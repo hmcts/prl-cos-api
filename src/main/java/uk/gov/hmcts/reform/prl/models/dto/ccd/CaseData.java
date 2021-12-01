5b432f8a775a48b6df4e26236f79c78a36d6e244
pete-lowe <petemlowe@gmail.com>
2021-12-01 15:48:03 +0000

    updated with changes to data model

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
@@ -20,108 +20,64 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
 @Setter
-@Builder(toBuilder = true)
+@Builder
 public class CaseData {
 
-    private final String state;
-    private final Long id;
-    //private final String taskList;
+    private final long id;
 
     @JsonProperty("LanguagePreferenceWelsh")
     private final YesOrNo languagePreferenceWelsh;
-    private final String applicantName;
-    private final String childName;
-    private final List<Element<ContactOrderDocument>> contactOrderDocumentsUploaded;
-    private final List<Element<C8Document>> c8FormDocumentsUploaded;
-    private final List<Element<OtherDocument>> otherDocumentsUploaded;
-    private final YesOrNo isWelshNeeded;
-    private final List<Element<WelshNeeds>> welshNeeds;
-    private final YesOrNo isInterpreterNeeded;
-    private final List<Element<InterpreterNeed>> interpreterNeeds;
-    private final YesOrNo isDisabilityPresent;
-    private final String adjustmentsRequired;
-    private final YesOrNo isSpecialArrangementsRequired;
-    private final String specialArrangementsRequired;
-    private final YesOrNo isIntermediaryNeeded;
-    private final String reasonsForIntermediary;
+
+    /**
+     * Case name
+     */
+    private final String applicantCaseName;
+
+    /**
+     * Type of application
+     */
     private final List<OrderTypeEnum> ordersApplyingFor;
     private final childArrangementOrderTypeEnum typeOfChildArrangementsOrder;
     private final String natureOfOrder;
+    private final YesOrNo consentOrder;
+    private final ConsentOrderDocument draftConsentOrderFile;
     private final PermissionRequiredEnum applicationPermissionRequired;
     private final String applicationPermissionRequiredReason;
     private final String applicationDetails;
-    private final YesOrNo isApplicationUrgent;
-    private final YesOrNo isApplicationConsideredWithoutNotice;
-    private final YesOrNo isHearingWithoutNoticeRequiredNotPossible;
-    private final YesOrNo isHearingWithoutNoticeRequiredRespondentWillFrustrate;
-    private final String applicationUrgencyOrders;
-    private final String applicationReasonsForUrgency;
-    private final DaysAndHours applicationConsideredInDaysAndHours;
-    private final String applicationNoticeEfforts;
-    private final String applicationWithoutNoticeReasons;
-    private final String applicationWithoutNoticeNotPossibleReasons;
-    private final String applicationWithoutNoticeRespondentWillFrustrateReasons;
-    private final YesOrNo domesticAbuse;
-    private final YesOrNo childAbduction;
-    private final YesOrNo childAbuse;
-    private final YesOrNo drugsAlcoholSubstanceAbuse;
-    private final YesOrNo safetyWelfareConcerns;
-    private final YesOrNo childAtRiskOfAbduction;
-    private final YesOrNo policeNotified;
-    private final YesOrNo childHasPassport;
-    private final YesOrNo childAbductedBefore;
-    private final YesOrNo childHasMultiplePassports;
-    private final List<PassportPossessionEnum> childPassportPossession;
-    private final String childPassportPossessionOtherDetails;
-    private final String childAbductionDetails;
-    private final YesOrNo abductionPoliceInvolved;
-    private final String abductionPoliceInvolvedDetails;
-    private final String childAtRiskOfAbductionReason;
-    private final String childWhereabouts;
-    private final YesOrNo childAbuseSexually;
-    private final String childAbuseSexuallyDetails;
-    private final LocalDate childAbuseSexuallyStartDate;
-    private final YesOrNo childAbuseSexuallyOngoing;
-    private final String childAbuseSexuallyHelpSought;
-    private final YesOrNo childAbusePhysically;
-    private final String childAbusePhysicallyDetails;
-    private final LocalDate childAbusePhysicallyStartDate;
-    private final YesOrNo childAbusePhysicallyOngoing;
-    private final String childAbusePhysicallyHelpSought;
-    private final YesOrNo childAbuseFinancially;
-    private final String childAbuseFinanciallyDetails;
-    private final LocalDate childAbuseFinanciallyStartDate;
-    private final YesOrNo childAbuseFinanciallyOngoing;
-    private final String childAbuseFinanciallyHelpSought;
-    private final YesOrNo childAbuseDomestic;
-    private final String childAbuseDomesticDetails;
-    private final LocalDate childAbuseDomesticStartDate;
-    private final YesOrNo childAbuseDomesticOngoing;
-    private final String childAbuseDomesticHelpSought;
-    private final YesOrNo childDrugsAlcoholSubstanceAbuse;
-    private final String childDrugsAlcoholSubstanceAbuseDetails;
-    private final LocalDate childDrugsAlcoholSubstanceAbuseStartDate;
-    private final YesOrNo childDrugsAlcoholSubstanceAbuseOngoing;
-    private final String childDrugsAlcoholSubstanceAbuseHelpSought;
-    private final YesOrNo otherSafetyOrWelfareConcerns;
-    private final String otherSafetyOrWelfareConcernsDetails;
-    private final Address childrenAddress;
-    private final List<Element<Child>> children;
-    private final YesNoDontKnow isChildrenKnownToAuthority;
-    private final String childAndLocalAuthority;
-    private final YesNoDontKnow isChildrenUnderChildProtection;
-    private final YesNoDontKnow isChildrenWithSameParents;
-    private final String parentsAndTheirChildren;
-    private final String parentalResponsibilities;
-    private final WhoChildrenLiveWith whoChildrenLiveWith;
-    private final String childAddressAndAdultsLivingWith;
-    private final YesOrNo isExistingProceedings;
-    private final String childrenInProceeding;
-    private final List<Element<ProceedingDetails>> existingProceedings;
+
+    /**
+     * Hearing urgency
+     */
+    private final YesOrNo isCaseUrgent;
+    private final String caseUrgencyTimeAndReason;
+    private final String effortsMadeWithRespondents;
+    private final YesOrNo doYouNeedAWithoutNoticeHearing;
+    private final String reasonsForApplicationWithoutNotice;
+    private final YesOrNo doYouRequireAHearingWithReducedNotice;
+    private final String setOutReasonsBelow;
+    private final YesOrNo areRespondentsAwareOfProceedings;
+
+    /**
+     * Applicant details
+     */
     private final List<Element<PartyDetails>> applicants;
+
+    /**
+     * Child details
+     */
+    private final List<Element<Child>> children;
+    private final YesNoDontKnow childrenKnownToLocalAuthority;
+    private final String childrenKnownToLocalAuthorityTextArea;
+    private final YesNoDontKnow childrenSubjectOfChildProtectionPlan;
+
+    /**
+     * Respondent details
+     */
     private final List<Element<PartyDetails>> respondents;
-    private final List<Element<PartyDetails>> othersToNotify;
-    private final List<Element<Child>> otherChildren;
+
+    /**
+     * MIAM
+     */
     private final YesOrNo applicantAttendedMIAM;
     private final YesOrNo claimingExemptionMIAM;
     private final YesOrNo familyMediatorMIAM;
@@ -139,19 +95,124 @@ public class CaseData {
     private final String familyMediatorServiceName1;
     private final String soleTraderName1;
     private final MIAMDocument miamCertificationDocumentUpload1;
-    private final YesOrNo consentOrder;
-    private final ConsentOrderDocument draftConsentOrderFile;
-    private final YesOrNo isThisCaseUrgent;
-    private final String caseUrgencyTimeAndReason;
-    private final String effortsMadeWithRespondents;
-    private final YesOrNo doYouNeedAWithoutNoticeHearing;
-    private final String reasonsForApplicationWithoutNotice;
-    private final YesOrNo doYouRequireAHearingWithReducedNotice;
-    //TODO: field below should have id linked to previous field
-    private final String setOutReasonsBelow;
-    private final YesOrNo areRespondentsAwareOfProceedings;
-    private final String applicantCaseName;
 
+    /**
+     * Allegations of harm
+     */
+    private final YesOrNo allegationsOfHarmYesNo;
+    private final YesOrNo allegationsOfHarmDomesticAbuseYesNo;
+    private final List<ApplicantOrChildren> physicalAbuseVictim;
+    private final List<ApplicantOrChildren> emotionalAbuseVictim;
+    private final List<ApplicantOrChildren> psychologicalAbuseVictim;
+    private final List<ApplicantOrChildren> sexualAbuseVictim;
+    private final List<ApplicantOrChildren> financialAbuseVictim;
+    private final YesOrNo allegationsOfHarmChildAbductionYesNo;
+    private final String childAbductionReasons;
+    private final YesOrNo previousAbductionThreats;
+    private final String previousAbductionThreatsDetails;
+    private final String childrenLocationNow;
+    private final YesOrNo abductionPassportOfficeNotified;
+    private final YesOrNo abductionPreviousPoliceInvolvement;
+    private final String abductionPreviousPoliceInvolvementDetails;
+    private final YesOrNo abductionOtherSafetyConcerns;
+    private final String abductionOtherSafetyConcernsDetails;
+    private final String abductionCourtStepsRequested;
+    private final List<Element<Behaviours>> behaviours;
+    private final YesOrNo ordersNonMolestation;
+    private final YesOrNo ordersOccupation;
+    private final YesOrNo ordersForcedMarriageProtection;
+    private final YesOrNo ordersRestraining;
+    private final YesOrNo ordersOtherInjunctive;
+    private final YesOrNo ordersUndertakingInPlace;
+    private final LocalDate ordersNonMolestationDateIssued;
+    private final LocalDate ordersNonMolestationEndDate;
+    private final YesOrNo ordersNonMolestationCurrent;
+    private final String ordersNonMolestationCourtName;
+    private final OtherDocument ordersNonMolestationDocument;
+    private final LocalDate ordersOccupationDateIssued;
+    private final LocalDate ordersOccupationEndDate;
+    private final YesOrNo ordersOccupationCurrent;
+    private final String ordersOccupationCourtName;
+    private final OtherDocument ordersOccupationDocument;
+    private final LocalDate ordersForcedMarriageProtectionDateIssued;
+    private final LocalDate ordersForcedMarriageProtectionEndDate;
+    private final YesOrNo ordersForcedMarriageProtectionCurrent;
+    private final String ordersForcedMarriageProtectionCourtName;
+    private final OtherDocument ordersForcedMarriageProtectionDocument;
+    private final LocalDate ordersRestrainingDateIssued;
+    private final LocalDate ordersRestrainingEndDate;
+    private final YesOrNo ordersRestrainingCurrent;
+    private final String ordersRestrainingCourtName;
+    private final OtherDocument ordersRestrainingDocument;
+    private final LocalDate ordersOtherInjunctiveDateIssued;
+    private final LocalDate ordersOtherInjunctiveEndDate;
+    private final YesOrNo ordersOtherInjunctiveCurrent;
+    private final String ordersOtherInjunctiveCourtName;
+    private final OtherDocument ordersOtherInjunctiveDocument;
+    private final LocalDate ordersUndertakingInPlaceDateIssued;
+    private final LocalDate ordersUndertakingInPlaceEndDate;
+    private final YesOrNo ordersUndertakingInPlaceCurrent;
+    private final String ordersUndertakingInPlaceCourtName;
+    private final OtherDocument ordersUndertakingInPlaceDocument;
+    private final YesOrNo allegationsOfHarmOtherConcerns;
+    private final String allegationsOfHarmOtherConcernsDetails;
+    private final String allegationsOfHarmOtherConcernsCourtActions;
+
+    /**
+     * Other people in the case
+     */
+    private final List<Element<PartyDetails>> othersToNotify;
+
+    /**
+     * Other proceedings
+     */
+
+    /**
+     * Attending the hearing
+     */
+    private final YesOrNo isWelshNeeded;
+    private final List<Element<WelshNeeds>> welshNeeds;
+    private final YesOrNo isInterpreterNeeded;
+    private final List<Element<InterpreterNeed>> interpreterNeeds;
+    private final YesOrNo isDisabilityPresent;
+    private final String adjustmentsRequired;
+    private final YesOrNo isSpecialArrangementsRequired;
+    private final String specialArrangementsRequired;
+    private final YesOrNo isIntermediaryNeeded;
+    private final String reasonsForIntermediary;
+
+    /**
+     * International element
+     */
+    private final YesOrNo habitualResidentInOtherState;
+    private final YesOrNo jurisdictionIssue;
+    private final YesOrNo requestToForeignAuthority;
+    private final String habitualResidentInOtherStateGiveReason;
+    private final String jurisdictionIssueGiveReason;
+    private final String requestToForeignAuthorityGiveReason;
+
+    /**
+     * Litigation capacity
+     */
+    private final String litigationCapacityFactors;
+    private final String litigationCapacityReferrals;
+    private final YesOrNo litigationCapacityOtherFactors;
+    private final String litigationCapacityOtherFactorsDetails;
+
+    /**
+     * Welsh language requirements
+     */
+    private final YesOrNo welshLanguageRequirement;
+    private final LanguagePreference welshLanguageRequirementApplication;
+    private final YesOrNo languageRequirementApplicationNeedWelsh;
+    private final YesOrNo welshLanguageRequirementApplicationNeedEnglish;
+
+
+    /**
+     * Upload documents
+     */
+    private final List<Element<ContactOrderDocument>> contactOrderDocumentsUploaded;
+    private final List<Element<C8Document>> c8FormDocumentsUploaded;
+    private final List<Element<OtherDocument>> otherDocumentsUploaded;
 
 }
-
