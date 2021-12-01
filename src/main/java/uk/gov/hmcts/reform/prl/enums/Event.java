696429d3a0d84cb9bb83b591edac86e67371792f
pete-lowe <petemlowe@gmail.com>
2021-12-01 14:37:35 +0000

    updated event enum

--- src/main/java/uk/gov/hmcts/reform/prl/enums/Event.java
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/Event.java
@@ -11,11 +11,11 @@ public enum Event {
     ATTENDING_THE_HEARING("attendingTheHearing", "Attending the hearing"),
     HEARING_URGENCY("hearingUrgency",  "Hearing urgency"),
     APPLICANT_DETAILS("applicantsDetails", "Applicant details"),
-    CHILD_DETAILS("childDetails", "Child Details"),
-    RESPONDENT_DETAILS("respondentDetails", "Respondent Details"),
+    CHILD_DETAILS("childDetails", "Child details"),
+    RESPONDENT_DETAILS("respondentsDetails", "Respondent details"),
     MIAM("miam", "MIAM"),
-    CASE_NAME("caseName", "Case Name"),
-    ALLEGATIONS_OF_HARM("allegationsOfHarm", "Allegations of Harm");
+    CASE_NAME("caseName", "Case name"),
+    ALLEGATIONS_OF_HARM("allegationsOfHarm", "Allegations of harm");
 
     private final String id;
     private final String name;
