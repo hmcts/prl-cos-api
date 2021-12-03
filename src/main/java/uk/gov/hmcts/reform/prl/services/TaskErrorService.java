0fe9408ef4c194341dc49f0e11b86f8cd02c781e
pete-lowe <petemlowe@gmail.com>
2021-12-03 15:50:10 +0000

    fixed other people in the case validaiton and added start of error checking

---
+++ src/main/java/uk/gov/hmcts/reform/prl/services/TaskErrorService.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.reform.prl.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class TaskErrorService {
+
+
+
+
+}
