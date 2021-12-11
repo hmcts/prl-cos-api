90dd8d4e8d55e09b78effd3685e54b28fc5086f5
Yogendra Upasani <yogendra.upasani@hmcts.net>
2021-12-11 18:19:09 +0000

    PRL-444 fixed application start up problem

--- src/main/java/uk/gov/hmcts/reform/prl/services/SystemUserService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/SystemUserService.java
@@ -5,20 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
+import uk.gov.hmcts.reform.prl.config.SystemUserConfiguration;
 
 @Service
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class SystemUserService {
 
-    @Value("${prl.system-update.username}")
-    private final String username;
-    @Value("${prl.system-update.password}")
-    private final String password;
+
+    private final SystemUserConfiguration userConfig;
 
     private final IdamClient idamClient;
 
     public String getSysUserToken() {
-        return idamClient.getAccessToken(username, password);
+        return idamClient.getAccessToken(userConfig.getUserName(), userConfig.getPassword());
     }
 
     public String getUserId(String userToken) {
