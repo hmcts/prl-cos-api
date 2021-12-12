2145cdc4225c22716fe93166385e3bf1804eccdc
Yogendra Upasani <yogendra.upasani@hmcts.net>
2021-12-12 00:12:12 +0000

    PRL-444 fixed application start up problem

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -67,6 +67,21 @@ data "azurerm_key_vault_secret" "prl-cos-idam-client-secret" {
   key_vault_id = data.azurerm_key_vault.prl_key_vault.id
 }
 
+data "azurerm_key_vault_secret" "prl_cos_idam_client_secret" {
+  key_vault_id = "${data.azurerm_key_vault.prl_key_vault.id}"
+  name = "prl-cos-idam-client-secret"
+}
+
+resource "azurerm_key_vault_secret" "prl_idam_secret" {
+  name         = "prl-cos-idam-client-secret"
+  value        = data.azurerm_key_vault_secret.prl_cos_idam_client_secret.value
+  key_vault_id = module.key-vault.key_vault_id
+
+  depends_on = [
+    module.key-vault
+  ]
+}
+
 data "azurerm_key_vault" "s2s_vault" {
   name                = "s2s-${var.env}"
   resource_group_name = "rpe-service-auth-provider-${var.env}"
