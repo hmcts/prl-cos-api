c28e13d1dcf827ef2a7f350e0f19f0de101d61d2
pete-lowe <petemlowe@gmail.com>
2021-12-13 14:57:42 +0000

    fixed checkstyle and failing test

--- src/main/java/uk/gov/hmcts/reform/prl/services/SystemUserService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/SystemUserService.java
@@ -13,8 +13,8 @@ import uk.gov.hmcts.reform.prl.config.SystemUserConfiguration;
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class SystemUserService {
 
-    @Autowired
-    OAuth2Configuration oAuth2Configuration;
+
+    private final OAuth2Configuration auth;
 
     private final SystemUserConfiguration userConfig;
 
@@ -23,10 +23,10 @@ public class SystemUserService {
     public String getSysUserToken() {
 
         log.info("*******************************************************");
-        log.info(oAuth2Configuration.getClientId());
-        log.info(oAuth2Configuration.getClientScope());
-        log.info(oAuth2Configuration.getRedirectUri());
-        log.info(oAuth2Configuration.getClientSecret());
+        log.info(auth.getClientId());
+        log.info(auth.getClientScope());
+        log.info(auth.getRedirectUri());
+        log.info(auth.getClientSecret());
         log.info(userConfig.getUserName());
         log.info("*******************************************************");
 
