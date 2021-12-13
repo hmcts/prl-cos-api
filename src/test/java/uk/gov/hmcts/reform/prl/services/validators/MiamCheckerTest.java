4503a9130c11aa3b13ff243ccacb91448c447028
pete-lowe <petemlowe@gmail.com>
2021-12-13 15:15:23 +0000

    added start of miam validator unit tests

--- src/test/java/uk/gov/hmcts/reform/prl/services/validators/MiamCheckerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/validators/MiamCheckerTest.java
@@ -1,4 +1,37 @@
 package uk.gov.hmcts.reform.prl.services.validators;
 
+import org.junit.Test;
+import uk.gov.hmcts.reform.prl.enums.YesOrNo;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+
+import static uk.gov.hmcts.reform.prl.enums.YesOrNo.NO;
+import static uk.gov.hmcts.reform.prl.enums.YesOrNo.YES;
+
 public class MiamCheckerTest {
+
+    @Test
+    public void whenNoCaseDataThenIsStartedReturnsFalse() {
+        CaseData caseData = CaseData.builder().build();
+
+        MiamChecker miamChecker = new MiamChecker();
+
+        assert !miamChecker.isStarted(caseData);
+    }
+
+    @Test
+    public void whenBasicMiamCaseDataPresentThenIsStartedReturnsTrue() {
+        CaseData caseData = CaseData.builder()
+            .applicantAttendedMIAM(YES)
+            .claimingExemptionMIAM(NO)
+            .familyMediatorMIAM(NO)
+            .build();
+
+        MiamChecker miamChecker = new MiamChecker();
+
+        assert miamChecker.isStarted(caseData);
+    }
+
+
+
+
 }
