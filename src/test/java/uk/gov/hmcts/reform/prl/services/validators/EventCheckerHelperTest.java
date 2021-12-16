f3b4851d98950e92051fdd813424654248519406
pete-lowe <petemlowe@gmail.com>
2021-12-16 09:50:38 +0000

    added more units tests and refactoring

---
+++ src/test/java/uk/gov/hmcts/reform/prl/services/validators/EventCheckerHelperTest.java
@@ -0,0 +1,42 @@
+package uk.gov.hmcts.reform.prl.services.validators;
+
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static uk.gov.hmcts.reform.prl.services.validators.EventCheckerHelper.allNonEmpty;
+
+class EventCheckerHelperTest {
+
+    @Nested
+    class AllNonEmpty {
+
+        @Test
+        void testEmptyArguments() {
+            assertThat(allNonEmpty()).isTrue();
+        }
+
+        @Test
+        void testOneEmpty() {
+            assertThat(allNonEmpty("")).isFalse();
+        }
+
+        @Test
+        void testOneNull() {
+            assertThat(allNonEmpty(new Object[] {null})).isFalse();
+        }
+
+        @Test
+        void testMultipleAndOneEmpty() {
+            assertThat(allNonEmpty("", "x")).isFalse();
+        }
+
+        @Test
+        void testNonEmpty() {
+            assertThat(allNonEmpty("x")).isTrue();
+        }
+
+    }
+
+}
