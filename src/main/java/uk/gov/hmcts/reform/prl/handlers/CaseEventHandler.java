7feb1b9b48b3b9e325c2aeee4a9c3c95be4f218c
pete-lowe <petemlowe@gmail.com>
2021-12-20 15:41:15 +0000

    removed unused import

--- src/main/java/uk/gov/hmcts/reform/prl/handlers/CaseEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/prl/handlers/CaseEventHandler.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.prl.handlers;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
