d41d312b2a185cb51d8d16bb2621dab2b36c1acd
pete-lowe <petemlowe@gmail.com>
2021-12-21 09:48:26 +0000

    added further task list renderer test

--- src/test/java/uk/gov/hmcts/reform/prl/services/TaskListRendererTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/TaskListRendererTest.java
@@ -5,6 +5,7 @@ import uk.gov.hmcts.reform.prl.models.EventValidationErrors;
 import uk.gov.hmcts.reform.prl.models.tasklist.Task;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -29,6 +30,8 @@ import static uk.gov.hmcts.reform.prl.enums.Event.VIEW_PDF_DOCUMENT;
 import static uk.gov.hmcts.reform.prl.enums.Event.WELSH_LANGUAGE_REQUIREMENTS;
 import static uk.gov.hmcts.reform.prl.enums.EventErrorsEnum.ALLEGATIONS_OF_HARM_ERROR;
 import static uk.gov.hmcts.reform.prl.enums.EventErrorsEnum.ATTENDING_THE_HEARING_ERROR;
+import static uk.gov.hmcts.reform.prl.models.tasklist.TaskState.FINISHED;
+import static uk.gov.hmcts.reform.prl.models.tasklist.TaskState.IN_PROGRESS;
 import static uk.gov.hmcts.reform.prl.models.tasklist.TaskState.NOT_STARTED;
 
 public class TaskListRendererTest {
@@ -47,12 +50,12 @@ public class TaskListRendererTest {
         Task.builder().event(CHILD_DETAILS).state(NOT_STARTED).build(),
         Task.builder().event(RESPONDENT_DETAILS).state(NOT_STARTED).build(),
         Task.builder().event(MIAM).state(NOT_STARTED).build(),
-        Task.builder().event(ALLEGATIONS_OF_HARM).state(NOT_STARTED).build(),
+        Task.builder().event(ALLEGATIONS_OF_HARM).state(IN_PROGRESS).build(),
         Task.builder().event(OTHER_PEOPLE_IN_THE_CASE).state(NOT_STARTED).build(),
         Task.builder().event(OTHER_PROCEEDINGS).state(NOT_STARTED).build(),
         Task.builder().event(ATTENDING_THE_HEARING).state(NOT_STARTED).build(),
-        Task.builder().event(INTERNATIONAL_ELEMENT).state(NOT_STARTED).build(),
-        Task.builder().event(LITIGATION_CAPACITY).state(NOT_STARTED).build(),
+        Task.builder().event(INTERNATIONAL_ELEMENT).state(FINISHED).build(),
+        Task.builder().event(LITIGATION_CAPACITY).state(FINISHED).build(),
         Task.builder().event(WELSH_LANGUAGE_REQUIREMENTS).state(NOT_STARTED).build(),
         Task.builder().event(VIEW_PDF_DOCUMENT).state(NOT_STARTED).build(),
         Task.builder().event(SUBMIT_AND_PAY).state(NOT_STARTED).build());
@@ -84,6 +87,30 @@ public class TaskListRendererTest {
         assert expectedTaskList.equals(actualTaskList);
 
     }
+
+    @Test
+    public void shouldRenderTaskListWithNoErrors() throws IOException {
+
+        List<EventValidationErrors> emptyErrors = Collections.emptyList();
+
+        BufferedReader taskListMarkDown = new BufferedReader(new FileReader("src/test/resources/task-list-no-errors.md"));
+
+        List<String> lines = new ArrayList<>();
+
+        String line = taskListMarkDown.readLine();
+        while (line != null) {
+            lines.add(line);
+            line = taskListMarkDown.readLine();
+        }
+
+        String expectedTaskList = String.join("\n", lines);
+        String actualTaskList = taskListRenderer.render(tasks, emptyErrors);
+
+        assert expectedTaskList.equals(actualTaskList);
+
+    }
+
+
 
 
 }
