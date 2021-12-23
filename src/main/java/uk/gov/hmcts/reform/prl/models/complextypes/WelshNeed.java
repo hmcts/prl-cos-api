de57abc32bf4c34e064f232598e849084b2a97a5
pete-lowe <63454814+pete-lowe@users.noreply.github.com>
2021-12-23 12:31:18 +0000

    welsh need error fixed

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/WelshNeed.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/WelshNeed.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Data
 @Builder
-public class WelshNeeds {
+public class WelshNeed {
 
     private final String whoNeedsWelsh;
     private final List<SpokenOrWrittenWelshEnum> spokenOrWritten;
