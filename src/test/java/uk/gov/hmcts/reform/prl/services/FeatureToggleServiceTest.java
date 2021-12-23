18d5814b035c44217672a0429de01d74690ef332
Swaroopa <pendyalaswaroopa86@gmail.com>
2021-12-23 09:58:01 +0000

    checkstyle issue fixed

--- src/test/java/uk/gov/hmcts/reform/prl/services/FeatureToggleServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/FeatureToggleServiceTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.prl.services;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
