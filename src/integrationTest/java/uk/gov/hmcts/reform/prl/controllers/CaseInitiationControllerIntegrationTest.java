5e27ebc83203fcbaf10403e6b51deabb02b89742
pete-lowe <petemlowe@gmail.com>
2021-12-23 14:29:43 +0000

    checkstyle change

--- src/integrationTest/java/uk/gov/hmcts/reform/prl/controllers/CaseInitiationControllerIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/prl/controllers/CaseInitiationControllerIntegrationTest.java
@@ -5,7 +5,6 @@ import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
