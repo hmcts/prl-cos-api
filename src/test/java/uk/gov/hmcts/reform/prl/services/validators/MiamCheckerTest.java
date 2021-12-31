197457b57a3a0a80a8f1fdb69a2b8262f9f9fe7d
bharadwajsairam <bharadwajsairam001@gmail.com>
2021-12-31 16:39:23 +0000

    "MIAM to Miam"

--- src/test/java/uk/gov/hmcts/reform/prl/services/validators/MiamCheckerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/validators/MiamCheckerTest.java
@@ -4,13 +4,13 @@ import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
 import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.models.documents.Document;
-import uk.gov.hmcts.reform.prl.models.documents.MIAMDocument;
+import uk.gov.hmcts.reform.prl.models.documents.MiamDocument;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 
 import java.util.Collections;
 
-import static uk.gov.hmcts.reform.prl.enums.MIAMDomesticViolenceChecklistEnum.MIAMDomesticViolenceChecklistEnum_Value_1;
-import static uk.gov.hmcts.reform.prl.enums.MIAMExemptionsChecklistEnum.domesticViolence;
+import static uk.gov.hmcts.reform.prl.enums.MiamDomesticViolenceChecklistEnum.MiamDomesticViolenceChecklistEnum_Value_1;
+import static uk.gov.hmcts.reform.prl.enums.MiamExemptionsChecklistEnum.domesticViolence;
 import static uk.gov.hmcts.reform.prl.enums.YesOrNo.NO;
 import static uk.gov.hmcts.reform.prl.enums.YesOrNo.YES;
 
@@ -28,9 +28,9 @@ public class MiamCheckerTest {
     @Test
     public void whenBasicMiamCaseDataPresentThenIsStartedReturnsTrue() {
         CaseData caseData = CaseData.builder()
-            .applicantAttendedMIAM(YES)
-            .claimingExemptionMIAM(NO)
-            .familyMediatorMIAM(NO)
+            .applicantAttendedMiam(YES)
+            .claimingExemptionMiam(NO)
+            .familyMediatorMiam(NO)
             .build();
 
         MiamChecker miamChecker = new MiamChecker();
@@ -59,11 +59,11 @@ public class MiamCheckerTest {
     @Test
     public void whenApplicantHasAttendedMiamAndDetailsProvidedIsFinishedReturnsTrue() {
         CaseData caseData = CaseData.builder()
-            .applicantAttendedMIAM(YES)
+            .applicantAttendedMiam(YES)
             .mediatorRegistrationNumber("123456")
             .familyMediatorServiceName("Test Name")
             .soleTraderName("Trade Sole")
-            .mIAMCertificationDocumentUpload(MIAMDocument.builder().build())
+            .miamCertificationDocumentUpload(MiamDocument.builder().build())
             .build();
 
         MiamChecker miamChecker = new MiamChecker();
@@ -74,13 +74,13 @@ public class MiamCheckerTest {
     @Test
     public void whenApplicantHasNotAttendedMiamButHasApprovedExemptionIsFinishedReturnsTrue() {
         CaseData caseData = CaseData.builder()
-            .applicantAttendedMIAM(NO)
-            .claimingExemptionMIAM(YES)
-            .familyMediatorMIAM(YES)
+            .applicantAttendedMiam(NO)
+            .claimingExemptionMiam(YES)
+            .familyMediatorMiam(YES)
             .mediatorRegistrationNumber1("123456")
             .familyMediatorServiceName1("Test Name")
             .soleTraderName1("Trade Sole")
-            .miamCertificationDocumentUpload1(MIAMDocument.builder().build())
+            .miamCertificationDocumentUpload1(MiamDocument.builder().build())
             .build();
 
         MiamChecker miamChecker = new MiamChecker();
@@ -91,11 +91,11 @@ public class MiamCheckerTest {
     @Test
     public void whenApplicantHasNotAttendedMiamButHasCompletedExemptionsSectionIsFinishedReturnsTrue() {
         CaseData caseData = CaseData.builder()
-            .applicantAttendedMIAM(NO)
-            .claimingExemptionMIAM(YES)
-            .familyMediatorMIAM(NO)
+            .applicantAttendedMiam(NO)
+            .claimingExemptionMiam(YES)
+            .familyMediatorMiam(NO)
             .miamExemptionsChecklist(Collections.singletonList(domesticViolence))
-            .miamDomesticViolenceChecklist(Collections.singletonList(MIAMDomesticViolenceChecklistEnum_Value_1))
+            .miamDomesticViolenceChecklist(Collections.singletonList(MiamDomesticViolenceChecklistEnum_Value_1))
             .build();
 
             MiamChecker miamChecker = new MiamChecker();
