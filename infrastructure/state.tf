16b10de25b51f77b042121fc1f535ca8c6078785
Aloknath Datta <alokdatta.uit@gmail.com>
2022-01-17 19:00:18 +0000

    updated terraform

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -2,13 +2,12 @@ terraform {
   backend "azurerm" {}
 
   required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "1.6.0"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.85.0"
+      version = "~> 2.92.0"
+    }
+    random = {
+      source = "hashicorp/random"
     }
   }
 }
