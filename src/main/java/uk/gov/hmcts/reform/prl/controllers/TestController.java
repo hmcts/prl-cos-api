e53b607acc21b456be9031c8ef75bc1b23242884
Aloknath Datta <alokdatta.uit@gmail.com>
2022-01-17 20:17:46 +0000

    Accessing Secrets

---
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/TestController.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.prl.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.prl.services.TestService;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    TestService testService;
+
+    @GetMapping("/test")
+    public ResponseEntity<String> testSecrets() {
+
+        return ok(testService.testMethod());
+    }
+}
