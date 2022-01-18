d7f8f4f13732a2d28645a0a2eae3c41aafed3259
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-01-18 16:25:36 +0000

    Modfified the attribute mapping for restrict checkbox

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/OtherDocuments.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/OtherDocuments.java
@@ -4,11 +4,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.prl.enums.DocTypeOtherDocumentsEnum;
-import uk.gov.hmcts.reform.prl.enums.FurtherEvidenceDocumentType;
 import uk.gov.hmcts.reform.prl.enums.RestrictToCafcassHmcts;
-import uk.gov.hmcts.reform.prl.models.documents.CorrespondenceDocument;
-import uk.gov.hmcts.reform.prl.models.documents.FurtherEvidenceDocument;
 import uk.gov.hmcts.reform.prl.models.documents.OtherDocument;
+
+import java.util.List;
 
 @Data
 @Builder
@@ -19,5 +18,5 @@ public class OtherDocuments {
     private final String notes;
     private final OtherDocument documentOther;
     private final DocTypeOtherDocumentsEnum docTypeOtherDocumentsEnum;
-    private final RestrictToCafcassHmcts restrictCheckboxOtherDocuments;
+    private final List<RestrictToCafcassHmcts> restrictCheckboxOtherDocuments;
 }
