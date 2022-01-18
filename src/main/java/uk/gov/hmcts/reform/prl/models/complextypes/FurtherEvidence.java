ffa64450a1a9991b12d8a4d83d8961f08c9d3518
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-01-18 16:25:36 +0000

    Modfified the attribute mapping for restrict checkbox

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/FurtherEvidence.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/FurtherEvidence.java
@@ -5,10 +5,12 @@ import lombok.Data;
 import uk.gov.hmcts.reform.prl.enums.*;
 import uk.gov.hmcts.reform.prl.models.documents.FurtherEvidenceDocument;
 
+import java.util.List;
+
 @Data
 @Builder
 public class FurtherEvidence {
     private final FurtherEvidenceDocumentType typeOfDocumentFurtherEvidence;
     private final FurtherEvidenceDocument documentFurtherEvidence;
-    private final RestrictToCafcassHmcts restrictCheckboxFurtherEvidence;
+    private final List<RestrictToCafcassHmcts> restrictCheckboxFurtherEvidence;
 }
