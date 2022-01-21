b7ac14fa15a217647d22a930946352551cec0f23
pete-lowe <petemlowe@gmail.com>
2022-01-21 15:57:07 +0000

    checkstyle

--- src/main/java/uk/gov/hmcts/reform/prl/enums/YesOrNo.java
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/YesOrNo.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
