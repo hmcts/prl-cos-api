c2cd3486f538f32644e3dbaf0717dfea193eb4a2
CatherineChowWingMei <wingmeicatherine.chow@hmcts.net>
2022-01-24 19:14:00 +0000

    Fix Coverage

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/ReturnApplicationReturnMessageControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/ReturnApplicationReturnMessageControllerTest.java
@@ -77,18 +77,19 @@ public class ReturnApplicationReturnMessageControllerTest {
     }
 
     @Test
+    public void whenHasApplicantWithNullRepresentativeNameGetLegalFullNameReturnsFalse() {
+        casedata = CaseData.builder().applicants(null).build();
+
+        Assertions.assertEquals("[Legal representative name]",returnApplicationReturnMessageController.getLegalFullName(casedata));
+    }
+
+    @Test
     public void whenNoApplicantGetLegalFullNameReturnConstantString() {
         casedata = CaseData.builder().build();
 
         Assertions.assertEquals("[Legal representative name]",returnApplicationReturnMessageController.getLegalFullName(casedata));
     }
 
-    @Test
-    public void whenHasApplicantNoRepresentativeNameGetLegalFullNameReturnConstantString() {
-        casedata = CaseData.builder().applicants(null).build();
-
-        Assertions.assertEquals("[Legal representative name]",returnApplicationReturnMessageController.getLegalFullName(casedata));
-    }
 
     @Test
     public void whenHasApplicantRepresentativeNameGetLegalFullNameReturnLegalRepresentativeFullName() {
@@ -128,6 +129,48 @@ public class ReturnApplicationReturnMessageControllerTest {
                                            .applicantCaseName(caseName)
                                            .applicants(applicantList)
                                            .rejectReason(Collections.singletonList(consentOrderNotProvided))
+                                           .returnMessage(returnMsgStr.toString())
+                                           .build())
+                             .build())
+            .build();
+
+
+        when(userService.getUserDetails(authToken)).thenReturn(userDetails);
+
+        returnApplicationReturnMessageController.returnApplicationReturnMessage(authToken, callbackRequest);
+
+        verify(userService).getUserDetails(authToken);
+        verifyNoMoreInteractions(userService);
+
+
+    }
+
+    @Test
+    public void shouldDoNothingReturnApplicationReturnMessageWithoutRejectReason() throws Exception {
+        PartyDetails applicant = PartyDetails.builder().representativeFirstName("John").representativeLastName("Smith").build();
+        Element<PartyDetails> wrappedApplicant = Element.<PartyDetails>builder().value(applicant).build();
+        List<Element<PartyDetails>> applicantList = Collections.singletonList(wrappedApplicant);
+
+        StringBuilder returnMsgStr = new StringBuilder();
+
+        returnMsgStr.append("Subject line: Application returned: " + caseName + "\n")
+            .append("Case name: " + caseName + "\n")
+            .append("Reference code: " + ccdId + "\n\n")
+            .append("Dear " + legalName + ",\n\n")
+            .append("Thank you for your application."
+                        + " Your application has been reviewed and is being returned for the following reasons:" + "\n\n")
+            .append(consentOrderNotProvided.getReturnMsgText());
+
+        returnMsgStr.append("Please resolve these issues and resubmit your application.\n\n")
+            .append("Kind regards,\n")
+            .append("solicitor@example.com Solicitor");
+
+        callbackRequest = CallbackRequest.builder()
+            .caseDetails(uk.gov.hmcts.reform.prl.models.dto.ccd.CaseDetails.builder()
+                             .caseId(ccdId)
+                             .caseData(CaseData.builder()
+                                           .applicantCaseName(caseName)
+                                           .applicants(applicantList)
                                            .returnMessage(returnMsgStr.toString())
                                            .build())
                              .build())
