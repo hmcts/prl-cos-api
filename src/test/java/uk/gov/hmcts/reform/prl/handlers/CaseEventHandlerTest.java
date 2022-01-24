1f7881e7ee80f0addca22c446d27caa756590e7b
saravanakumar-palanichamy <saravanakumar.palanichamy@HMCTS.NET>
2022-01-24 09:02:10 +0000

    resolved sonar issues

--- src/test/java/uk/gov/hmcts/reform/prl/handlers/CaseEventHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/handlers/CaseEventHandlerTest.java
@@ -20,9 +20,11 @@ import java.util.Map;
 
 import static org.apache.commons.lang3.RandomUtils.nextLong;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.C100_CASE_TYPE;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.CASE_TYPE;
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.FL401_CASE_TYPE;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.JURISDICTION;
 import static uk.gov.hmcts.reform.prl.enums.Event.CASE_NAME;
 import static uk.gov.hmcts.reform.prl.enums.Event.SUBMIT_AND_PAY;
@@ -78,5 +80,26 @@ public class CaseEventHandlerTest {
             "internal-update-task-list",
             Map.of("taskList", renderedTaskLists)
         );
+    }
+
+    @Test
+    public void shouldNotUpdateTaskListForCasesInOpenState() {
+        final CaseData caseData = CaseData.builder()
+            .id(nextLong())
+            .caseTypeOfApplication(FL401_CASE_TYPE)
+            .build();
+        final CaseDataChanged caseDataChanged = new CaseDataChanged(caseData);
+        final List<Task> tasks = List.of(
+            Task.builder().event(CASE_NAME).state(FINISHED).build(),
+            Task.builder().event(SUBMIT_AND_PAY).state(NOT_STARTED).build());
+
+        final String renderedTaskLists = "<h1>Task 1</h1><h2>Task 2</h2>";
+
+        final List<EventValidationErrors> eventsErrors = Collections.emptyList();
+
+        caseEventHandler.handleCaseDataChange(caseDataChanged);
+
+        verifyNoInteractions(taskListService);
+        verifyNoInteractions(taskListRenderer);
     }
 }
