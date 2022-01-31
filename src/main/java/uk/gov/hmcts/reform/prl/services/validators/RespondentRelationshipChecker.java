55b38526a8c797a860db4b638048e0f1d4d4aeaf
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-01-31 08:55:57 +0000

    added test case for respondent relationship checker

--- src/main/java/uk/gov/hmcts/reform/prl/services/validators/RespondentRelationshipChecker.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/validators/RespondentRelationshipChecker.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.prl.services.validators;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.prl.models.complextypes.RespondentRelationDateInfo;
 import uk.gov.hmcts.reform.prl.models.complextypes.RespondentRelationObjectType;
 import uk.gov.hmcts.reform.prl.models.complextypes.RespondentRelationOptionsInfo;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
@@ -27,9 +26,7 @@ public class RespondentRelationshipChecker implements EventChecker {
         boolean finished;
 
         Optional<RespondentRelationObjectType> respondentRelationObjectType = ofNullable(caseData.getRespondentRelationObjectType());
-        Optional<RespondentRelationDateInfo> respondentRelationDateInfo = ofNullable(caseData.getRespondentRelationDateInfo());
         Optional<RespondentRelationOptionsInfo> respondentRelationOptionsInfo = ofNullable(caseData.getRespondentRelationOptionsInfo());
-
 
         if (respondentRelationObjectType.isPresent()) {
 
---
+++ src/test/java/uk/gov/hmcts/reform/prl/services/validators/RespondentRelationshipCheckerTest.java
@@ -0,0 +1,100 @@
+package uk.gov.hmcts.reform.prl.services.validators;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.prl.enums.ApplicantRelationshipEnum;
+import uk.gov.hmcts.reform.prl.enums.ApplicantRelationshipOptionsEnum;
+import uk.gov.hmcts.reform.prl.models.complextypes.RespondentRelationObjectType;
+import uk.gov.hmcts.reform.prl.models.complextypes.RespondentRelationOptionsInfo;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+import uk.gov.hmcts.reform.prl.services.TaskErrorService;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RespondentRelationshipCheckerTest {
+
+    @Mock
+    TaskErrorService taskErrorService;
+
+    @Mock
+    RespondentRelationObjectType respondentRelationObjectType;
+    @InjectMocks
+    RespondentRelationshipChecker respondentRelationshipChecker;
+
+    @Test
+    public void whenNoCaseDataPresentThenIsStartedReturnsFalse() {
+        CaseData caseData = CaseData.builder().build();
+
+        assert !respondentRelationshipChecker.isStarted(caseData);
+
+    }
+
+    @Test
+    public void whenEmptyRelationshipDataPresentThenIsStartedReturnsTrue() {
+        CaseData caseData = CaseData.builder()
+            .respondentRelationObjectType(RespondentRelationObjectType.builder().build())
+            .build();
+
+        assert respondentRelationshipChecker.isStarted(caseData);
+    }
+
+    @Test
+    public void whenSomeRelationshipDataPresentThenIsStartedReturnsTrue() {
+        CaseData caseData = CaseData.builder()
+            .respondentRelationObjectType(RespondentRelationObjectType.builder()
+                                              .applicantRelationshipEnum(ApplicantRelationshipEnum.noneOfTheAbove)
+                                              .build())
+            .build();
+        assert respondentRelationshipChecker.isStarted(caseData);
+    }
+
+    @Test
+    public void whenNoCaseDataPresentThenIsFinishedReturnsFalse() {
+        CaseData caseData = CaseData.builder().build();
+
+        assert !respondentRelationshipChecker.isFinished(caseData);
+
+    }
+
+    @Test
+    public void whenSomeRealtionshipDataPresentThenIsFinishedReturnsFalse() {
+        CaseData caseData = CaseData.builder()
+            .respondentRelationObjectType(RespondentRelationObjectType.builder()
+                                              .applicantRelationshipEnum(ApplicantRelationshipEnum.noneOfTheAbove)
+                                              .build())
+            .build();
+
+        assert !respondentRelationshipChecker.isFinished(caseData);
+
+    }
+
+    @Test
+    public void whenAllRelationshipDataPresentThenIsFinishedReturnsTrue() {
+
+        CaseData caseData = CaseData.builder()
+            .respondentRelationObjectType(RespondentRelationObjectType.builder()
+                                              .applicantRelationshipEnum(ApplicantRelationshipEnum.noneOfTheAbove)
+                                              .build())
+            .respondentRelationOptionsInfo(RespondentRelationOptionsInfo.builder()
+                                               .applicantRelationshipOptionsEnum(ApplicantRelationshipOptionsEnum.aunt)
+                                               .build())
+            .build();
+        assert respondentRelationshipChecker.isFinished(caseData);
+    }
+
+    @Test
+    public void whenRelationshipDataSelectedNotNoneOfTheAboveThenIsFinishedReturnsTrue() {
+
+        CaseData caseData = CaseData.builder()
+            .respondentRelationObjectType(RespondentRelationObjectType.builder()
+                                              .applicantRelationshipEnum(ApplicantRelationshipEnum.formerlyEngagedOrProposed)
+                                              .build())
+            .build();
+        assert respondentRelationshipChecker.isFinished(caseData);
+    }
+
+}
