ba46439543bc1b4695322edff15ea29ece80f63b
Yogendra Upasani <yogendra.upasani@hmcts.net>
2022-02-01 00:58:40 +0000

    PRL-612 CTSC staff email

--- src/test/java/uk/gov/hmcts/reform/prl/services/SolicitorEmailServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/SolicitorEmailServiceTest.java
@@ -16,6 +16,7 @@ import uk.gov.hmcts.reform.prl.models.dto.notify.EmailTemplateVars;
 import uk.gov.hmcts.reform.prl.models.dto.notify.SolicitorEmail;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -29,14 +30,14 @@ public class SolicitorEmailServiceTest {
     private static final String manageCaseUrl = null;
     public static final String EMAIL_TEMPLATE_ID_1 = "111";
     public static final String EMAIL_TEMPLATE_ID_2 = "222";
-    public static final SolicitorEmail expectedEmailVars =  SolicitorEmail.builder()
+    public static final SolicitorEmail expectedEmailVars = SolicitorEmail.builder()
         .caseReference("123")
         .caseName("Case 123")
         .applicantName("applicantName")
-            .courtName("court name")
-            .fullName("Full name")
+        .courtName("court name")
+        .fullName("Full name")
         .courtEmail("C@justice.gov.uk")
-            .caseLink("http://localhost:3333/")
+        .caseLink("http://localhost:3333/")
         .build();
 
 
@@ -97,5 +98,50 @@ public class SolicitorEmailServiceTest {
         assertEquals(solicitorEmailService.buildEmail(caseDetails), email);
 
     }
+
+    @Test
+    public void sendEmailSuccessfully() {
+        PartyDetails applicant = PartyDetails.builder()
+            .firstName("TestFirst")
+            .lastName("TestLast")
+            .build();
+
+        String applicantNames = "TestFirst TestLast";
+
+        Element<PartyDetails> wrappedApplicants = Element.<PartyDetails>builder().value(applicant).build();
+        List<Element<PartyDetails>> listOfApplicants = Collections.singletonList(wrappedApplicants);
+
+        CaseData caseData = CaseData.builder()
+            .id(12345L)
+            .applicantCaseName("TestCaseName")
+            .applicantSolicitorEmailAddress("test@test.com")
+            .applicants(listOfApplicants)
+            .build();
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("applicantSolicitorEmailAddress", "test@test.com");
+
+        CaseDetails caseDetails = CaseDetails.builder()
+            .id(caseData.getId())
+            .data(data)
+            .build();
+        UserDetails userDetails = UserDetails.builder()
+            .forename("userFirst")
+            .surname("userLast")
+            .build();
+
+        when(emailService.getCaseData(caseDetails)).thenReturn(caseData);
+
+        EmailTemplateVars email = SolicitorEmail.builder()
+            .caseReference(String.valueOf(caseData.getId()))
+            .caseName(emailService.getCaseData(caseDetails).getApplicantCaseName())
+            .applicantName(applicantNames)
+            .caseLink(manageCaseUrl + caseDetails.getId())
+            .build();
+
+        solicitorEmailService.sendEmail(caseDetails);
+        assertEquals(caseDetails.getData().get("applicantSolicitorEmailAddress").toString(), "test@test.com");
+    }
+
 }
 
