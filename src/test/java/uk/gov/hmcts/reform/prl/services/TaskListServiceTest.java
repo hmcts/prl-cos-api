021feccef10aef1912971305b26a01fda8bbc8ee
CatherineChowWingMei <wingmeicatherine.chow@hmcts.net>
2022-02-02 14:33:14 +0000

    Fix Coverage

--- src/test/java/uk/gov/hmcts/reform/prl/services/TaskListServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/TaskListServiceTest.java
@@ -14,22 +14,7 @@ import uk.gov.hmcts.reform.prl.services.validators.EventsChecker;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static uk.gov.hmcts.reform.prl.enums.Event.ALLEGATIONS_OF_HARM;
-import static uk.gov.hmcts.reform.prl.enums.Event.APPLICANT_DETAILS;
-import static uk.gov.hmcts.reform.prl.enums.Event.ATTENDING_THE_HEARING;
-import static uk.gov.hmcts.reform.prl.enums.Event.CASE_NAME;
-import static uk.gov.hmcts.reform.prl.enums.Event.CHILD_DETAILS;
-import static uk.gov.hmcts.reform.prl.enums.Event.HEARING_URGENCY;
-import static uk.gov.hmcts.reform.prl.enums.Event.INTERNATIONAL_ELEMENT;
-import static uk.gov.hmcts.reform.prl.enums.Event.LITIGATION_CAPACITY;
-import static uk.gov.hmcts.reform.prl.enums.Event.MIAM;
-import static uk.gov.hmcts.reform.prl.enums.Event.OTHER_PEOPLE_IN_THE_CASE;
-import static uk.gov.hmcts.reform.prl.enums.Event.OTHER_PROCEEDINGS;
-import static uk.gov.hmcts.reform.prl.enums.Event.RESPONDENT_DETAILS;
-import static uk.gov.hmcts.reform.prl.enums.Event.SUBMIT_AND_PAY;
-import static uk.gov.hmcts.reform.prl.enums.Event.TYPE_OF_APPLICATION;
-import static uk.gov.hmcts.reform.prl.enums.Event.VIEW_PDF_DOCUMENT;
-import static uk.gov.hmcts.reform.prl.enums.Event.WELSH_LANGUAGE_REQUIREMENTS;
+import static uk.gov.hmcts.reform.prl.enums.Event.*;
 import static uk.gov.hmcts.reform.prl.models.tasklist.TaskState.NOT_STARTED;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -63,6 +48,27 @@ public class TaskListServiceTest {
             Task.builder().event(WELSH_LANGUAGE_REQUIREMENTS).state(NOT_STARTED).build(),
             Task.builder().event(VIEW_PDF_DOCUMENT).state(NOT_STARTED).build(),
             Task.builder().event(SUBMIT_AND_PAY).state(NOT_STARTED).build());
+
+        List<Task> actualTasks = taskListService.getTasksForOpenCase(caseData);
+
+        assertThat(expectedTasks).isEqualTo(actualTasks);
+
+    }
+
+    @Test
+    public void getTasksShouldReturnFl401ListOfTasks() {
+
+        CaseData caseData = CaseData.builder().caseTypeOfApplication(PrlAppsConstants.FL401_CASE_TYPE).build();
+
+        List<Task> expectedTasks = List.of(
+            Task.builder().event(FL401_CASE_NAME).state(NOT_STARTED).build(),
+            Task.builder().event(FL401_APPLICANT_FAMILY_DETAILS).state(NOT_STARTED).build(),
+            Task.builder().event(APPLICANT_DETAILS).state(NOT_STARTED).build(),
+            Task.builder().event(RESPONDENT_DETAILS).state(NOT_STARTED).build(),
+            Task.builder().event(OTHER_PROCEEDINGS).state(NOT_STARTED).build(),
+            Task.builder().event(INTERNATIONAL_ELEMENT).state(NOT_STARTED).build(),
+            Task.builder().event(WELSH_LANGUAGE_REQUIREMENTS).state(NOT_STARTED).build(),
+            Task.builder().event(VIEW_PDF_DOCUMENT).state(NOT_STARTED).build());
 
         List<Task> actualTasks = taskListService.getTasksForOpenCase(caseData);
 
