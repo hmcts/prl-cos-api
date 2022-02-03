054c1ac1137c208de1c91bf571cf92a1ab125e48
saravanakumar-palanichamy <saravanakumar.palanichamy@HMCTS.NET>
2022-02-03 11:55:03 +0000

    added test cases

--- src/test/java/uk/gov/hmcts/reform/prl/services/TaskListServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/TaskListServiceTest.java
@@ -19,6 +19,7 @@ import static uk.gov.hmcts.reform.prl.enums.Event.APPLICANT_DETAILS;
 import static uk.gov.hmcts.reform.prl.enums.Event.ATTENDING_THE_HEARING;
 import static uk.gov.hmcts.reform.prl.enums.Event.CASE_NAME;
 import static uk.gov.hmcts.reform.prl.enums.Event.CHILD_DETAILS;
+import static uk.gov.hmcts.reform.prl.enums.Event.FL401_CASE_NAME;
 import static uk.gov.hmcts.reform.prl.enums.Event.HEARING_URGENCY;
 import static uk.gov.hmcts.reform.prl.enums.Event.INTERNATIONAL_ELEMENT;
 import static uk.gov.hmcts.reform.prl.enums.Event.LITIGATION_CAPACITY;
@@ -70,4 +71,25 @@ public class TaskListServiceTest {
 
     }
 
+    @Test
+    public void getTasksShouldReturnFl401ListOfTasks() {
+
+        CaseData caseData = CaseData.builder().caseTypeOfApplication(PrlAppsConstants.FL401_CASE_TYPE).build();
+
+        List<Task> expectedTasks = List.of(
+            Task.builder().event(FL401_CASE_NAME).state(NOT_STARTED).build(),
+            Task.builder().event(APPLICANT_DETAILS).state(NOT_STARTED).build(),
+            Task.builder().event(RESPONDENT_DETAILS).state(NOT_STARTED).build(),
+            Task.builder().event(OTHER_PROCEEDINGS).state(NOT_STARTED).build(),
+            Task.builder().event(INTERNATIONAL_ELEMENT).state(NOT_STARTED).build(),
+            Task.builder().event(WELSH_LANGUAGE_REQUIREMENTS).state(NOT_STARTED).build(),
+            Task.builder().event(VIEW_PDF_DOCUMENT).state(NOT_STARTED).build());
+
+        List<Task> actualTasks = taskListService.getTasksForOpenCase(caseData);
+
+        assertThat(expectedTasks).isEqualTo(actualTasks);
+
+    }
+
 }
+
