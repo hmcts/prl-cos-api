5910a74df72f62d4b7359956765b4e9cb4a93356
pete-lowe <petemlowe@gmail.com>
2022-02-08 16:38:58 +0000

    Fixed checkstyle issues

--- src/main/java/uk/gov/hmcts/reform/prl/services/SendAndReplyService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/SendAndReplyService.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.prl.services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.prl.enums.sendmessages.MessageStatus;
