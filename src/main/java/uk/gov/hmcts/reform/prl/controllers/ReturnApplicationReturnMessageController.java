66afdfc751e0b51082d55bf7ef35f92a420741bb
CatherineChowWingMei <wingmeicatherine.chow@hmcts.net>
2022-02-08 18:05:09 +0000

    Unit test case

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ReturnApplicationReturnMessageController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ReturnApplicationReturnMessageController.java
@@ -5,77 +5,35 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.idam.client.models.UserDetails;
-import uk.gov.hmcts.reform.prl.enums.RejectReasonEnum;
 import uk.gov.hmcts.reform.prl.framework.exceptions.WorkflowException;
-import uk.gov.hmcts.reform.prl.models.Element;
-import uk.gov.hmcts.reform.prl.models.complextypes.PartyDetails;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.services.CaseWorkerEmailService;
+import uk.gov.hmcts.reform.prl.services.ReturnApplicationService;
 import uk.gov.hmcts.reform.prl.services.UserService;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
-import static java.util.Optional.ofNullable;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-import static uk.gov.hmcts.reform.prl.services.validators.EventCheckerHelper.allNonEmpty;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class ReturnApplicationReturnMessageController {
 
     @Autowired
-    private UserService userService;
+    UserService userService;
+    @Autowired
+    private ReturnApplicationService returnApplicationService;
+    @Autowired
     private final ObjectMapper objectMapper;
+    @Autowired
     private final CaseWorkerEmailService caseWorkerEmailService;
-
-    public boolean noRejectReasonSelected(CaseData caseData) {
-
-        boolean noOptionSelected = true;
-
-        boolean hasSelectedOption = allNonEmpty(caseData.getRejectReason());
-
-        if (hasSelectedOption) {
-            noOptionSelected = false;
-        }
-
-        return noOptionSelected;
-    }
-
-    public String getLegalFullName(CaseData caseData) {
-
-        String legalName = "[Legal representative name]";
-
-        Optional<List<Element<PartyDetails>>> applicantsWrapped = ofNullable(caseData.getApplicants());
-
-        if (applicantsWrapped.isPresent() && applicantsWrapped.get().size() == 1) {
-            List<PartyDetails> applicants = applicantsWrapped.get()
-                .stream()
-                .map(Element::getValue)
-                .collect(Collectors.toList());
-
-            String legalFirstName = applicants.get(0).getRepresentativeFirstName();
-            String legalLastName = applicants.get(0).getRepresentativeLastName();
-
-            legalName = legalFirstName + " " + legalLastName;
-
-        } else {
-            legalName = caseData.getSolicitorName();
-        }
-        return legalName;
-    }
 
     @PostMapping(path = "/return-application-return-message", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
     @ApiOperation(value = "Callback to get return message of the return application ")
@@ -87,45 +45,18 @@ public class ReturnApplicationReturnMessageController {
         @RequestBody uk.gov.hmcts.reform.ccd.client.model.CallbackRequest callbackRequest
     ) throws WorkflowException {
 
+        UserDetails userDetails = userService.getUserDetails(authorisation);
+        Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
+
+        CaseData caseData1 = objectMapper.convertValue(callbackRequest.getCaseDetails().getData(), CaseData.class);
+
         CaseData caseData = objectMapper.convertValue(callbackRequest.getCaseDetails().getData(), CaseData.class)
             .toBuilder()
             .id(callbackRequest.getCaseDetails().getId())
             .build();
 
-        UserDetails userDetails = userService.getUserDetails(authorisation);
-        Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
-
-        if (noRejectReasonSelected(caseData)) {
-            log.info("There are no reject reason selected, therefore no return message is needed");
-        } else {
-            log.info("Preparing pre-filled text for return message");
-            String caseName = caseData.getApplicantCaseName();
-            String ccdId = String.valueOf(caseData.getId());
-
-
-            String legalName = getLegalFullName(caseData);
-            String caseWorkerName = userDetails.getFullName();
-
-            List<RejectReasonEnum> listOfReasons = caseData.getRejectReason();
-
-            StringBuilder returnMsgStr = new StringBuilder();
-
-            returnMsgStr//.append("Subject line: Application returned: " + caseName + "\n")
-                .append("Case name: " + caseName + "\n")
-                .append("Reference code: " + ccdId + "\n\n")
-                .append("Dear " + legalName + ",\n\n")
-                .append("Thank you for your application."
-                            + " Your application has been reviewed and is being returned for the following reasons:" + "\n\n");
-
-            for (RejectReasonEnum reasonEnum : listOfReasons) {
-                returnMsgStr.append(reasonEnum.getReturnMsgText());
-            }
-
-            returnMsgStr.append("Please resolve these issues and resubmit your application.\n\n")
-                .append("Kind regards,\n")
-                .append(caseWorkerName);
-
-            caseDataUpdated.put("returnMessage", returnMsgStr.toString());
+        if (!returnApplicationService.noRejectReasonSelected(caseData)) {
+            caseDataUpdated.put("returnMessage", returnApplicationService.getReturnMessage(caseData,userDetails));
         }
         return AboutToStartOrSubmitCallbackResponse.builder().data(caseDataUpdated).build();
     }
@@ -133,7 +64,6 @@ public class ReturnApplicationReturnMessageController {
     @PostMapping(path = "/return-application-notification", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
     @ApiOperation(value = "Callback to send return application email notification")
     public AboutToStartOrSubmitCallbackResponse returnApplicationEmailNotification(
-        @RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation,
         @RequestBody uk.gov.hmcts.reform.ccd.client.model.CallbackRequest callbackRequest) throws Exception {
 
         caseWorkerEmailService.sendReturnApplicationEmailToSolicitor(callbackRequest.getCaseDetails());
