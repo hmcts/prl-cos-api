796b7a40b5060ce00ccbb13166ecd9affc4f030e
Swaroopa <pendyalaswaroopa86@gmail.com>
2022-02-08 10:49:26 +0000

    pr comments were resolved

--- src/integrationTest/java/uk/gov/hmcts/reform/prl/controllers/CallbackControllerIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/prl/controllers/CallbackControllerIntegrationTest.java
@@ -7,7 +7,6 @@ import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Ignore;
 import org.junit.Test;
