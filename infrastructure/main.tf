72c0b21fcac3745252e1310d8d6afc90e21a223d
Khurshed Khan <khurshed.khan@hmcts.net>
2022-02-11 09:47:44 +0000

    PRL-835: reverted mai.tf to previous version

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,8 +1,3 @@
 provider "azurerm" {
   features {}
 }
-
-data "azurerm_key_vault_secret" "launch_darkly_sdk_key" {
-  name = "launchDarkly-sdk-key"
-  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
-}
