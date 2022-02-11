b6250d6f3e945a145f9fe6ac773af2ae3f95a940
pete-lowe <petemlowe@gmail.com>
2022-02-11 16:46:20 +0000

    Removed dgs from this branch

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/TaskListController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/TaskListController.java
@@ -1,11 +1,6 @@
 package uk.gov.hmcts.reform.prl.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.prl.events.CaseDataChanged;
-import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.services.ApplicationsTabService;
-import uk.gov.hmcts.reform.prl.utils.DgsCustomSerializer;
-
-import java.util.Map;
 
 @Api
 @RestController
@@ -34,28 +25,7 @@ public class TaskListController extends AbstractCallbackController {
 
     @PostMapping("/submitted")
     public void handleSubmitted(@RequestBody CallbackRequest callbackRequest,
-                                @RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation) throws JsonProcessingException {
-
-
-        CaseData caseData = getCaseData(callbackRequest.getCaseDetails());
-
-        String m = callbackRequest.getCaseDetails().getData().toString();
-
-        ObjectMapper objectMapper = new ObjectMapper()
-            .registerModule(new ParameterNamesModule())
-            .registerModule(new Jdk8Module())
-            .registerModule(new JavaTimeModule());
-
-        SimpleModule module = new SimpleModule();
-        module.addSerializer(CaseData.class, new DgsCustomSerializer());
-        objectMapper.registerModule(module);
-
-        String serliazed = objectMapper.writeValueAsString(caseData);
-
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> map = mapper.readValue(serliazed, Map.class);
-        String new1 = map.toString();
-
+                                @RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation) {
 
         applicationsTabService.updateApplicationTabData(getCaseData(callbackRequest.getCaseDetails()));
 
