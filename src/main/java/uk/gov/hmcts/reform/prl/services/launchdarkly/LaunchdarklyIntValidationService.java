cc68812e8880fbfe86935cdb1e20ba38e8b101e7
Khurshed Khan <khurshed.khan@hmcts.net>
2022-02-11 09:45:14 +0000

    PRL-835 : Integration Laaunch darkly in pro-cos-api

---
+++ src/main/java/uk/gov/hmcts/reform/prl/services/launchdarkly/LaunchdarklyIntValidationService.java
@@ -0,0 +1,28 @@
+package uk.gov.hmcts.reform.prl.documentgenerator.service.launchdarkly;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.prl.documentgenerator.config.launchdarkly.LaunchDarklyClient;
+
+@Service
+public class LaunchdarklyIntValidationService {
+
+    LaunchDarklyClient launchDarklyClient;
+
+    @Autowired
+    public LaunchdarklyIntValidationService(LaunchDarklyClient launchDarklyClient) {
+        this.launchDarklyClient = launchDarklyClient;
+    }
+
+    public  String checkFeatureFlag(String flag) {
+
+        String result;
+        if (launchDarklyClient.isFeatureEnabled(flag)) {
+            result = "Feature flag is on";
+        } else {
+            result = "Feature flag is off";
+        }
+        return result;
+    }
+
+}
