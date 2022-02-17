1a72afe8436887eed80d45c8422ade985be73462
Swaroopa <pendyalaswaroopa86@gmail.com>
2022-02-17 12:20:28 +0000

    checkstyle issues due to merge conflicts are resolved

--- src/main/java/uk/gov/hmcts/reform/prl/services/OrganisationService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/OrganisationService.java
@@ -102,38 +102,37 @@ public class OrganisationService {
             log.info("Respondents length {}", respondents.size());
 
             for (PartyDetails respondent : respondents) {
-
-                log.info("*** Count **** ");
                 if (respondent.getDoTheyHaveLegalRepresentation().equals(YesNoDontKnow.yes)
                     && respondent.getSolicitorOrg() != null) {
-                        String organisationID = respondent.getSolicitorOrg().getOrganisationID();
-                        if (organisationID != null) {
-                            log.info("Organisation Id : {}",organisationID);
-                            log.info("*** Before api call organisation **** ");
-                            organisations = organisationApi.findOrganisation(userToken, authTokenGenerator.generate(), organisationID);
-                            log.info("*** After api call organisation **** {}",organisations);
 
-                            respondent.toBuilder()
-                                .organisationAddress1(Optional.ofNullable(organisations.getContactInformation().get(0).getAddressLine1()).isPresent()
+                    String organisationID = respondent.getSolicitorOrg().getOrganisationID();
+                    if (organisationID != null) {
+                        log.info("Organisation Id : {}",organisationID);
+                        log.info("*** Before api call organisation **** ");
+                        organisations = organisationApi.findOrganisation(userToken, authTokenGenerator.generate(), organisationID);
+                        log.info("*** After api call organisation **** {}",organisations);
+
+                        respondent.toBuilder()
+                            .organisationAddress1(Optional.ofNullable(organisations.getContactInformation().get(0).getAddressLine1()).isPresent()
                                                           ? organisations.getContactInformation().get(0).getAddressLine1()
                                                           : "")
-                                .organisationAddress2(Optional.ofNullable(organisations.getContactInformation().get(0).getAddressLine2()).isPresent()
+                            .organisationAddress2(Optional.ofNullable(organisations.getContactInformation().get(0).getAddressLine2()).isPresent()
                                                           ? organisations.getContactInformation().get(0).getAddressLine2()
                                                           : "")
-                                .organisationAddress3(Optional.ofNullable(organisations.getContactInformation().get(0).getAddressLine3()).isPresent()
+                            .organisationAddress3(Optional.ofNullable(organisations.getContactInformation().get(0).getAddressLine3()).isPresent()
                                                           ? organisations.getContactInformation().get(0).getAddressLine3()
                                                           : "")
-                                .organisationCountry(Optional.ofNullable(organisations.getContactInformation().get(0).getCountry()).isPresent()
+                            .organisationCountry(Optional.ofNullable(organisations.getContactInformation().get(0).getCountry()).isPresent()
                                                          ? organisations.getContactInformation().get(0).getCountry()
                                                          : "")
-                                .organisationCounty(Optional.ofNullable(organisations.getContactInformation().get(0).getCounty()).isPresent()
+                            .organisationCounty(Optional.ofNullable(organisations.getContactInformation().get(0).getCounty()).isPresent()
                                                         ? organisations.getContactInformation().get(0).getCounty()
                                                         : "")
-                                .organisationPostcode(Optional.ofNullable(organisations.getContactInformation().get(0).getPostCode()).isPresent()
+                            .organisationPostcode(Optional.ofNullable(organisations.getContactInformation().get(0).getPostCode()).isPresent()
                                                           ? organisations.getContactInformation().get(0).getPostCode()
                                                           : "");
-                            applicantsWithOrganisationDetails.add(Element.<PartyDetails>builder().value(respondent).build());
-                            log.info("***** Respondent with Organisation address **** {} ", applicantsWithOrganisationDetails);
+                        applicantsWithOrganisationDetails.add(Element.<PartyDetails>builder().value(respondent).build());
+                        log.info("***** Respondent with Organisation address **** {} ", applicantsWithOrganisationDetails);
                     }
                 }
             }
