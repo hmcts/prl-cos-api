4d771e7825525897aacc495c2644ba2ef743c026
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-02-17 11:22:02 +0000

    updating casedata with organisation details

--- src/main/java/uk/gov/hmcts/reform/prl/services/OrganisationService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/OrganisationService.java
@@ -14,6 +14,7 @@ import uk.gov.hmcts.reform.prl.models.complextypes.Organisations;
 import uk.gov.hmcts.reform.prl.models.complextypes.PartyDetails;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 
+import javax.servlet.http.Part;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -31,85 +32,33 @@ public class OrganisationService {
     private final AuthTokenGenerator authTokenGenerator;
     private final SystemUserService systemUserService;
     private List<Element<PartyDetails>> applicantsWithOrganisationDetails = new ArrayList<>();
-    private  PartyDetails partyDetails = PartyDetails.builder().build();
     @Autowired
     private ObjectMapper objectMapper;
 
     public CaseData getApplicantOrganisationDetails(CaseData caseData) throws NotFoundException {
 
-        String userToken = systemUserService.getSysUserToken();
+        if(Optional.ofNullable(caseData.getApplicants()).isPresent()){
+            String userToken = systemUserService.getSysUserToken();
 
-        List<PartyDetails> applicants = caseData
-            .getApplicants()
-            .stream()
-            .map(Element::getValue)
-            .collect(Collectors.toList());
+            List<PartyDetails> applicants = caseData
+                .getApplicants()
+                .stream()
+                .map(Element::getValue)
+                .collect(Collectors.toList());
 
-        log.info("applicants length {}",applicants.stream().count());
+            log.info("applicants length {}",applicants.stream().count());
 
-        for (PartyDetails applicant : applicants) {
+            for (PartyDetails applicant : applicants) {
 
-            log.info("*** Count **** ");
-            if (applicant.getSolicitorOrg() != null) {
-                String organisationID = applicant.getSolicitorOrg().getOrganisationID();
-                if (organisationID != null) {
-                    log.info("Organisation Id : {}",organisationID);
-                    log.info("*** Before api call organisation **** ");
-                    organisations = organisationApi.findOrganisation(userToken, authTokenGenerator.generate(), organisationID);
-                    log.info("*** After api call organisation **** {}",organisations);
-                    applicant.toBuilder()
-                        .organisationAddress1(Optional.ofNullable(organisations.getContactInformation().get(0).getAddressLine1()).isPresent()
-                                                  ? organisations.getContactInformation().get(0).getAddressLine1()
-                                                  : "")
-                        .organisationAddress2(Optional.ofNullable(organisations.getContactInformation().get(0).getAddressLine2()).isPresent()
-                                                  ? organisations.getContactInformation().get(0).getAddressLine2()
-                                                  : "")
-                        .organisationAddress3(Optional.ofNullable(organisations.getContactInformation().get(0).getAddressLine3()).isPresent()
-                                                  ? organisations.getContactInformation().get(0).getAddressLine3()
-                                                  : "")
-                        .organisationCountry(Optional.ofNullable(organisations.getContactInformation().get(0).getCountry()).isPresent()
-                                                 ? organisations.getContactInformation().get(0).getCountry()
-                                                 : "")
-                        .organisationCounty(Optional.ofNullable(organisations.getContactInformation().get(0).getCounty()).isPresent()
-                                                ? organisations.getContactInformation().get(0).getCounty()
-                                                : "")
-                        .organisationPostcode(Optional.ofNullable(organisations.getContactInformation().get(0).getPostCode()).isPresent()
-                                                  ? organisations.getContactInformation().get(0).getPostCode()
-                                                  : "")
-                                          .build();
-
-                    log.info("***** Applicant with Organisation address **** {}", applicantsWithOrganisationDetails);
-                }
-            }
-        }
-        return caseData;
-    }
-
-    public CaseData getRespondentOrganisationDetails(CaseData caseData) throws NotFoundException {
-
-        String userToken = systemUserService.getSysUserToken();
-
-        List<PartyDetails> respondents = caseData
-            .getRespondents()
-            .stream()
-            .map(Element::getValue)
-            .collect(Collectors.toList());
-
-        log.info("applicants length {}",respondents.stream().count());
-
-        for (PartyDetails respondent : respondents) {
-
-            log.info("*** Count **** ");
-            if (respondent.getDoTheyHaveLegalRepresentation().equals(YesNoDontKnow.yes)) {
-                if (respondent.getSolicitorOrg() != null) {
-                    String organisationID = respondent.getSolicitorOrg().getOrganisationID();
+                log.info("*** Count **** ");
+                if (applicant.getSolicitorOrg() != null) {
+                    String organisationID = applicant.getSolicitorOrg().getOrganisationID();
                     if (organisationID != null) {
                         log.info("Organisation Id : {}",organisationID);
                         log.info("*** Before api call organisation **** ");
                         organisations = organisationApi.findOrganisation(userToken, authTokenGenerator.generate(), organisationID);
                         log.info("*** After api call organisation **** {}",organisations);
-
-                        respondent.toBuilder()
+                        applicant.toBuilder()
                             .organisationAddress1(Optional.ofNullable(organisations.getContactInformation().get(0).getAddressLine1()).isPresent()
                                                       ? organisations.getContactInformation().get(0).getAddressLine1()
                                                       : "")
@@ -127,12 +76,70 @@ public class OrganisationService {
                                                     : "")
                             .organisationPostcode(Optional.ofNullable(organisations.getContactInformation().get(0).getPostCode()).isPresent()
                                                       ? organisations.getContactInformation().get(0).getPostCode()
-                                                      : "");
-
-                        log.info("***** Respondent with Organisation address **** ");
+                                                      : "")
+                            .build();
+                        applicantsWithOrganisationDetails.add(Element.<PartyDetails>builder().value(applicant).build());
+                        log.info("***** Applicant with Organisation address **** {}", applicantsWithOrganisationDetails);
                     }
                 }
             }
+            caseData.toBuilder().applicants(applicantsWithOrganisationDetails).build();
+        }
+
+        return caseData;
+    }
+
+    public CaseData getRespondentOrganisationDetails(CaseData caseData) throws NotFoundException {
+
+        if (Optional.ofNullable(caseData.getRespondents()).isPresent()) {
+            String userToken = systemUserService.getSysUserToken();
+            applicantsWithOrganisationDetails.clear();
+            List<PartyDetails> respondents = caseData
+                .getRespondents()
+                .stream()
+                .map(Element::getValue)
+                .collect(Collectors.toList());
+
+            log.info("applicants length {}",respondents.stream().count());
+
+            for (PartyDetails respondent : respondents) {
+
+                log.info("*** Count **** ");
+                if (respondent.getDoTheyHaveLegalRepresentation().equals(YesNoDontKnow.yes)) {
+                    if (respondent.getSolicitorOrg() != null) {
+                        String organisationID = respondent.getSolicitorOrg().getOrganisationID();
+                        if (organisationID != null) {
+                            log.info("Organisation Id : {}",organisationID);
+                            log.info("*** Before api call organisation **** ");
+                            organisations = organisationApi.findOrganisation(userToken, authTokenGenerator.generate(), organisationID);
+                            log.info("*** After api call organisation **** {}",organisations);
+
+                            respondent.toBuilder()
+                                .organisationAddress1(Optional.ofNullable(organisations.getContactInformation().get(0).getAddressLine1()).isPresent()
+                                                          ? organisations.getContactInformation().get(0).getAddressLine1()
+                                                          : "")
+                                .organisationAddress2(Optional.ofNullable(organisations.getContactInformation().get(0).getAddressLine2()).isPresent()
+                                                          ? organisations.getContactInformation().get(0).getAddressLine2()
+                                                          : "")
+                                .organisationAddress3(Optional.ofNullable(organisations.getContactInformation().get(0).getAddressLine3()).isPresent()
+                                                          ? organisations.getContactInformation().get(0).getAddressLine3()
+                                                          : "")
+                                .organisationCountry(Optional.ofNullable(organisations.getContactInformation().get(0).getCountry()).isPresent()
+                                                         ? organisations.getContactInformation().get(0).getCountry()
+                                                         : "")
+                                .organisationCounty(Optional.ofNullable(organisations.getContactInformation().get(0).getCounty()).isPresent()
+                                                        ? organisations.getContactInformation().get(0).getCounty()
+                                                        : "")
+                                .organisationPostcode(Optional.ofNullable(organisations.getContactInformation().get(0).getPostCode()).isPresent()
+                                                          ? organisations.getContactInformation().get(0).getPostCode()
+                                                          : "");
+                            applicantsWithOrganisationDetails.add(Element.<PartyDetails>builder().value(respondent).build());
+                            log.info("***** Respondent with Organisation address **** ");
+                        }
+                    }
+                }
+            }
+            caseData.toBuilder().respondents(applicantsWithOrganisationDetails).build();
         }
         return caseData;
     }
