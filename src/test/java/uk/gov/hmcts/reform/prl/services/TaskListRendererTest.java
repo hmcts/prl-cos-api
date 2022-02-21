31e5730071a22a6a6b71f92616597161f6bb6a7c
Aloknath Datta <alokdatta.uit@gmail.com>
2022-02-21 11:58:12 +0000

    build issue fix

--- src/test/java/uk/gov/hmcts/reform/prl/services/TaskListRendererTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/TaskListRendererTest.java
@@ -12,7 +12,8 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static uk.gov.hmcts.reform.prl.enums.Event.ALLEGATIONS_OF_HARM;
 import static uk.gov.hmcts.reform.prl.enums.Event.APPLICANT_DETAILS;
 import static uk.gov.hmcts.reform.prl.enums.Event.ATTENDING_THE_HEARING;
