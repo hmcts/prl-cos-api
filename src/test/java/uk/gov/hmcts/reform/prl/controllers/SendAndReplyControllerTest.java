eae74271d2ed0805d3e2dc44982105c511175178
pete-lowe <petemlowe@gmail.com>
2022-02-23 09:33:04 +0000

    Removed unused import

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/SendAndReplyControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/SendAndReplyControllerTest.java
@@ -29,7 +29,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
