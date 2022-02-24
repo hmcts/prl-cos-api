d63986e5fc93b792d5df4568fdcdc8857bde9715
pete-lowe <petemlowe@gmail.com>
2022-02-24 15:56:37 +0000

    removed import

--- src/main/java/uk/gov/hmcts/reform/prl/services/TaskListRenderer.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/TaskListRenderer.java
@@ -13,7 +13,6 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
