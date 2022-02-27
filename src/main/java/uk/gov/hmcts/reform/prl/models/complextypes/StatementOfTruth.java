f0faceeaf6134791df3f792602ab7373a383df97
Swaroopa <pendyalaswaroopa86@gmail.com>
2022-02-27 00:53:51 +0000

    values.preview.template.yaml is updated

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/StatementOfTruth.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/StatementOfTruth.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.prl.models.complextypes;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
-import uk.gov.hmcts.reform.prl.enums.FL401Consent;
 
 import java.time.LocalDate;
 
@@ -11,7 +10,6 @@ import java.time.LocalDate;
 @Builder
 @Jacksonized
 public class StatementOfTruth {
-    private final FL401Consent applicantConsent;
     private final LocalDate date;
     private final String fullname;
     private final String nameOfFirm;
