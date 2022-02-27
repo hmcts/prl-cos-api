f7616bc3d6b03dfe292495af02e3a1b48c707014
Swaroopa <pendyalaswaroopa86@gmail.com>
2022-02-27 00:07:44 +0000

    new callback request is added for sending email notifications

--- src/main/java/uk/gov/hmcts/reform/prl/enums/FL401Consent.java
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/FL401Consent.java
@@ -1,9 +1,26 @@
 package uk.gov.hmcts.reform.prl.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public enum FL401Consent {
 
     @JsonProperty("fl401Consent")
-    fl401Consent;
+    fl401Consent("The applicant believes that the facts stated in this form and any continuation "
+                     + "sheets are true. I am authorised by the applicant to sign this statement. ");
+
+    private final String displayedValue;
+
+    @JsonValue
+    public String getDisplayedValue() {
+        return displayedValue;
+    }
+
+    @JsonCreator
+    public static Fl401ConfidentialConsent getValue(String key) {
+        return Fl401ConfidentialConsent.valueOf(key);
+    }
 }
--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
@@ -35,7 +35,6 @@ import uk.gov.hmcts.reform.prl.models.complextypes.ApplicantChild;
 import uk.gov.hmcts.reform.prl.models.complextypes.ApplicantFamilyDetails;
 import uk.gov.hmcts.reform.prl.models.complextypes.Behaviours;
 import uk.gov.hmcts.reform.prl.models.complextypes.Child;
-import uk.gov.hmcts.reform.prl.models.complextypes.ConfidentialityCheck;
 import uk.gov.hmcts.reform.prl.models.complextypes.Correspondence;
 import uk.gov.hmcts.reform.prl.models.complextypes.FurtherEvidence;
 import uk.gov.hmcts.reform.prl.models.complextypes.InterpreterNeed;
@@ -439,7 +438,5 @@ public class CaseData implements MappableObject {
      */
     @JsonProperty("fl401StmtOfTruth")
     private final StatementOfTruth fl401StmtOfTruth;
-    @JsonProperty("fl401ConfidentialityCheck")
-    private final ConfidentialityCheck fl401ConfidentialityCheck;
 
 }
