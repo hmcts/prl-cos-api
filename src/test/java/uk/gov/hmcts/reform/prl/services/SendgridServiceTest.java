09aeaf1aa7bf428c8947121d7958f0223e5dc460
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-03-08 15:27:47 +0000

    unit test for sendgridservice added

---
+++ src/test/java/uk/gov/hmcts/reform/prl/services/SendgridServiceTest.java
@@ -0,0 +1,44 @@
+package uk.gov.hmcts.reform.prl.services;
+
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.prl.rpa.mappers.C100JsonMapper;
+import uk.gov.hmcts.reform.prl.rpa.mappers.json.NullAwareJsonObjectBuilder;
+
+
+import javax.json.JsonObject;
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class SendgridServiceTest {
+
+    @InjectMocks
+    private SendgridService sendgridService;
+
+    @Mock
+    private SendGrid sendGrid;
+
+    @Mock
+    private C100JsonMapper c100JsonMapper;
+    @Test
+    public void TestSendEmail() throws IOException {
+        Response response = new Response();
+        response.setStatusCode(200);
+        JsonObject jsonObject = new NullAwareJsonObjectBuilder()
+            .add("applicantCaseName","hello")
+            .build();
+        sendgridService.sendEmail(jsonObject);
+        //assertThrows(sendgridService.sendEmail(jsonObject),IOException);
+        //assertNotEquals(verify(sendGrid.api(Mockito.any(Request.class))).getStatusCode(),202);
+    }
+}
