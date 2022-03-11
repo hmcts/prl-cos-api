e966d8233183604bfb1c4a5cd72a2ba320ec8058
BharadwajSairam <75499453+BharadwajSairam@users.noreply.github.com>
2022-03-11 11:21:09 +0000

    Update PartyDetails.java

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/PartyDetails.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/PartyDetails.java
@@ -59,7 +59,7 @@ public class PartyDetails {
     private String phoneNumber;
     private String email;
     private Address address;
-    @JsonIgnore
+   
     private final Organisations organisations;
     private final String solicitorTelephone;
     @JsonIgnore
