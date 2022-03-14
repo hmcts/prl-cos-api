a21eb5387e6c980f40e1e0d05e5dc9b00a8fddef
Aloknath Datta <alokdatta.uit@gmail.com>
2022-03-14 20:32:51 +0000

    Compilation error fixed

--- src/main/java/uk/gov/hmcts/reform/prl/services/validators/EventsChecker.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/validators/EventsChecker.java
@@ -138,7 +138,7 @@ public class EventsChecker {
         eventStatus.put(RESPONDENT_BEHAVIOUR, respondentBehaviourChecker);
         eventStatus.put(WITHOUT_NOTICE_ORDER, withoutNoticeOrderChecker);
         eventStatus.put(FL401_APPLICANT_FAMILY_DETAILS, fl401ApplicantFamilyChecker);
-        eventStatus.put(FL401_UPLOAD_DOCUMENTS, uploadDocumentChecker);
+        eventStatus.put(FL401_UPLOAD_DOCUMENTS, pdfChecker);
         eventStatus.put(FL401_OTHER_PROCEEDINGS, fl401OtherProceedingsChecker);
         eventStatus.put(FL401_SOT_AND_SUBMIT, fl401StatementOfTruthAndSubmitChecker);
 
