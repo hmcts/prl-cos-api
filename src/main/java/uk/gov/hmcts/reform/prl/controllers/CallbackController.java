cae3ee01beb36a51a88417371609988978c812eb
pete-lowe <petemlowe@gmail.com>
2022-03-14 12:57:23 +0000

    changed field names

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/CallbackController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/CallbackController.java
@@ -146,8 +146,8 @@ public class CallbackController {
             caseData = organisationService.getApplicantOrganisationDetailsForFL401(caseData);
         }
         CaseData updatedCaseData = getUpdatedCaseDataWithDoc(authorisation, caseData);
-        updatedCaseData.setSubmitAndPayDownloadApplicationLinkText(
-            PrlAppsConstants.C100_CASE_TYPE.equalsIgnoreCase(caseData.getCaseTypeOfApplication()) ? "C100 draft" : "FL401");
+        updatedCaseData.setViewPDFlinkLabelText(
+            PrlAppsConstants.C100_CASE_TYPE.equalsIgnoreCase(caseData.getCaseTypeOfApplication()) ? "C100 draft" : "FL401 draft");
         return uk.gov.hmcts.reform.prl.models.dto.ccd.CallbackResponse
             .builder()
             .data(updatedCaseData)
--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
@@ -513,7 +513,7 @@ public class CaseData implements MappableObject {
     private final StatementOfTruth fl401StmtOfTruth;
 
 
-    private String submitAndPayDownloadApplicationLinkText;
+    private String viewPDFlinkLabelText;
 
 
 
