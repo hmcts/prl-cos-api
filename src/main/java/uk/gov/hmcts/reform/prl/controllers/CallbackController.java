4366e2d3603241a6b1c337f4450180dc87e7249a
pete-lowe <petemlowe@gmail.com>
2022-03-15 08:42:57 +0000

    aupdated label and some more code smell fixes

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/CallbackController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/CallbackController.java
@@ -147,7 +147,7 @@ public class CallbackController {
         }
         CaseData updatedCaseData = getUpdatedCaseDataWithDoc(authorisation, caseData);
         updatedCaseData.setViewPdfLinkLabelText(
-            PrlAppsConstants.C100_CASE_TYPE.equalsIgnoreCase(caseData.getCaseTypeOfApplication()) ? "C100 draft" : "FL401 draft");
+            PrlAppsConstants.C100_CASE_TYPE.equalsIgnoreCase(caseData.getCaseTypeOfApplication()) ? "### C100 draft" : "### FL401 draft");
         return uk.gov.hmcts.reform.prl.models.dto.ccd.CallbackResponse
             .builder()
             .data(updatedCaseData)
