6aed8a3028486208d4285cc07a803a29becad73d
CatherineChowWingMei <wingmeicatherine.chow@hmcts.net>
2022-03-18 18:43:45 +0000

    Fix code smell

--- src/main/java/uk/gov/hmcts/reform/prl/services/validators/HomeChecker.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/validators/HomeChecker.java
@@ -126,9 +126,12 @@ public class HomeChecker implements EventChecker {
     }
 
     public boolean verifyAddressPresent(Optional<Home> home) {
-        return ofNullable(home.get().getAddress()).isPresent()
-            && ofNullable(home.get().getAddress().getAddressLine1()).isPresent()
-            && !home.get().getAddress().getAddressLine1().isBlank();
+       if (home.isPresent()) {
+           return ofNullable(home.get().getAddress()).isPresent()
+               && ofNullable(home.get().getAddress().getAddressLine1()).isPresent()
+               && !home.get().getAddress().getAddressLine1().isBlank();
+       }
+       return false;
     }
 
     private boolean mandatoryChildDetailsAreCompleted(Optional<List<Element<ChildrenLiveAtAddress>>> doAnyChildrenLiveAtAddressYes) {
