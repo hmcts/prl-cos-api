ea50d26e10787732fe84e0e9cc1ddc20fea3b85d
CatherineChowWingMei <wingmeicatherine.chow@hmcts.net>
2022-03-18 11:40:29 +0000

    Fix code smell

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/PrePopulateFeeAndSolicitorNameControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/PrePopulateFeeAndSolicitorNameControllerTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.prl.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -41,7 +40,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
