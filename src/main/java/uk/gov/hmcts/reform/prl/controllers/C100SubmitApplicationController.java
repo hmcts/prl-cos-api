6712b6d3ec5df83090260f6500d463024584240a
saravanakumar-palanichamy <saravanakumar.palanichamy@HMCTS.NET>
2022-03-21 15:45:47 +0000

    added controller for resubmitting

---
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/C100SubmitApplicationController.java
@@ -0,0 +1,150 @@
+package uk.gov.hmcts.reform.prl.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.bytebuddy.asm.Advice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.ccd.client.model.CaseEventDetail;
+import uk.gov.hmcts.reform.idam.client.models.UserDetails;
+import uk.gov.hmcts.reform.prl.enums.FL401OrderTypeEnum;
+import uk.gov.hmcts.reform.prl.enums.State;
+import uk.gov.hmcts.reform.prl.mapper.AppObjectMapper;
+import uk.gov.hmcts.reform.prl.models.complextypes.TypeOfApplicationOrders;
+import uk.gov.hmcts.reform.prl.models.court.Court;
+import uk.gov.hmcts.reform.prl.models.court.CourtEmailAddress;
+import uk.gov.hmcts.reform.prl.models.documents.Document;
+import uk.gov.hmcts.reform.prl.models.dto.GeneratedDocumentInfo;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.CallbackResponse;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+import uk.gov.hmcts.reform.prl.models.language.DocumentLanguage;
+import uk.gov.hmcts.reform.prl.services.*;
+import uk.gov.hmcts.reform.prl.services.validators.FL401StatementOfTruthAndSubmitChecker;
+import uk.gov.hmcts.reform.prl.utils.CaseUtils;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+import static java.util.Optional.ofNullable;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.*;
+import static uk.gov.hmcts.reform.prl.enums.YesOrNo.Yes;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class C100SubmitApplicationController {
+
+    @Autowired
+    private CourtFinderService courtFinderService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private SolicitorEmailService solicitorEmailService;
+
+    @Autowired
+    private CaseWorkerEmailService caseWorkerEmailService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private DgsService dgsService;
+
+    @Autowired
+    private CaseEventService caseEventService;
+
+    @Autowired
+    private DocumentLanguageService documentLanguageService;
+
+    @Autowired
+    OrganisationService organisationService;
+
+    @PostMapping(path = "/resubmit-application", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
+    @ApiOperation(value = "Callback to change the state and document generation and submit application. ")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Resubmission completed"),
+        @ApiResponse(code = 400, message = "Bad Request")})
+    public AboutToStartOrSubmitCallbackResponse resubmitApplication(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation,
+        @RequestBody CallbackRequest callbackRequest) throws Exception {
+
+        CaseDetails caseDetails = callbackRequest.getCaseDetails();
+
+        CaseData caseData = CaseUtils.getCaseData(callbackRequest.getCaseDetails(), objectMapper);
+
+        // REtunr--> case_issue --- > Retunr --> submitted --> hearing- atypeljksjdkf-
+        // Retunr --> case_issue --> hearing- atypeljksjdkf-
+
+        List<CaseEventDetail> eventsForCase = caseEventService.findEventsForCase(String.valueOf(caseData.getId()));
+        String result = eventsForCase.stream().map(CaseEventDetail::getStateId)
+            .skip(eventsForCase.indexOf(State.AWAITING_RESUBMISSION_TO_HMCTS) + 1)
+            .filter((previousState) -> State.SUBMITTED_PAID.equals(previousState)
+                || State.CASE_ISSUE.equals(previousState) )
+            .findFirst()
+            .orElse("");
+
+        Map<String, Object> stringObjectMap = caseData.toMap(AppObjectMapper.getObjectMapper());
+        stringObjectMap.put("state", State.CASE_ISSUE);
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(stringObjectMap)
+            .build();
+    }
+
+    private Document generateDocumentField(String fileName,GeneratedDocumentInfo generatedDocumentInfo) {
+        if (null == generatedDocumentInfo) {
+            return null;
+        }
+        return Document.builder()
+            .documentUrl(generatedDocumentInfo.getUrl())
+            .documentBinaryUrl(generatedDocumentInfo.getBinaryUrl())
+            .documentHash(generatedDocumentInfo.getHashToken())
+            .documentFileName(fileName).build();
+    }
+
+    private GeneratedDocumentInfo generateDocument(String authorisation, String template, CaseData caseData,
+                                                   boolean isWelsh)
+        throws Exception {
+        log.info("Generating the {} document for case id {} ", template, caseData.getId());
+        GeneratedDocumentInfo generatedDocumentInfo = null;
+        if (isWelsh) {
+            generatedDocumentInfo = dgsService.generateWelshDocument(
+                authorisation,
+                uk.gov.hmcts.reform.prl.models.dto.ccd.CaseDetails.builder().caseData(caseData).build(),
+                template
+            );
+        } else {
+            generatedDocumentInfo = dgsService.generateDocument(
+                authorisation,
+                uk.gov.hmcts.reform.prl.models.dto.ccd.CaseDetails.builder().caseData(caseData).build(),
+                template
+            );
+        }
+        if (null != generatedDocumentInfo) {
+            caseData = caseData.toBuilder().isDocumentGenerated("Yes").build();
+        } else {
+            caseData = caseData.toBuilder().isDocumentGenerated("No").build();
+        }
+
+        log.info("Genereated the {} document for case id {} ", template, caseData.getId());
+        return generatedDocumentInfo;
+    }
+
+}
