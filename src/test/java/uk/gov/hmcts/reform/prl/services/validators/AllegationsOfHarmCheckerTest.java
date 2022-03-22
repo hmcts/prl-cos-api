69546c585e634a8401402f0a685b0f218fb0038f
pete-lowe <petemlowe@gmail.com>
2022-03-22 18:11:33 +0000

    fixed test

--- src/test/java/uk/gov/hmcts/reform/prl/services/validators/AllegationsOfHarmCheckerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/validators/AllegationsOfHarmCheckerTest.java
@@ -122,7 +122,7 @@ public class AllegationsOfHarmCheckerTest {
     @Test
     public void whenOtherConcernsPresentThenValidateOtherConcernsTrue() {
         CaseData caseData = CaseData.builder()
-            .allegationsOfHarmOtherConcernsYesNo(Yes)
+            .allegationsOfHarmOtherConcerns(Yes)
             .allegationsOfHarmOtherConcernsDetails("Details")
             .allegationsOfHarmOtherConcernsCourtActions("Court actions")
             .build();
