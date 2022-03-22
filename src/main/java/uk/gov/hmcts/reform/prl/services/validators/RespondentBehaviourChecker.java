985e0fa2ed093736f0753425d8cfdd6b5f13e4d7
CatherineChowWingMei <wingmeicatherine.chow@hmcts.net>
2022-03-22 17:20:29 +0000

    coverage

--- src/main/java/uk/gov/hmcts/reform/prl/services/validators/RespondentBehaviourChecker.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/validators/RespondentBehaviourChecker.java
@@ -33,10 +33,9 @@ public class RespondentBehaviourChecker implements EventChecker {
         }
 
         Optional<String>  otherReason = ofNullable(respondentBehaviourData.getOtherReasonApplicantWantToStopFromRespondentDoing());
-        boolean otherReasonCompleted = (otherReason.isPresent() && !(otherReason.get().isBlank()));
         Optional<List<ApplicantStopFromRespondentDoingEnum>> applicantStopRespondentList
             = ofNullable(respondentBehaviourData.getApplicantWantToStopFromRespondentDoing());
-        if (otherReasonCompleted && applicantStopRespondentList.isPresent()) {
+        if (otherReason.isPresent() && applicantStopRespondentList.isPresent()) {
             taskErrorService.removeError(RESPONDENT_BEHAVIOUR_ERROR);
             return true;
         } else {
@@ -55,20 +54,18 @@ public class RespondentBehaviourChecker implements EventChecker {
             return false;
         }
         Optional<String>  otherReason = ofNullable(respondentBehaviourData.getOtherReasonApplicantWantToStopFromRespondentDoing());
-        boolean otherReasonCompleted = (otherReason.isPresent() && !(otherReason.get().isBlank()));
         Optional<List<ApplicantStopFromRespondentDoingEnum>> applicantStopRespondentList
             = ofNullable(respondentBehaviourData.getApplicantWantToStopFromRespondentDoing());
         Optional<List<ApplicantStopFromRespondentDoingToChildEnum>> applicantStopFromRespondentDoingToChildList
             = ofNullable(respondentBehaviourData.getApplicantWantToStopFromRespondentDoingToChild());
         boolean anyStarted = false;
 
-        if (otherReasonCompleted || applicantStopRespondentList.isPresent() || applicantStopFromRespondentDoingToChildList.isPresent()) {
+        if (otherReason.isPresent() || applicantStopRespondentList.isPresent() || applicantStopFromRespondentDoingToChildList.isPresent()) {
             anyStarted = true;
         }
 
         return  anyStarted;
     }
-
 
     @Override
     public boolean hasMandatoryCompleted(CaseData caseData) {
@@ -77,11 +74,9 @@ public class RespondentBehaviourChecker implements EventChecker {
             return false;
         }
         Optional<String>  otherReason = ofNullable(respondentBehaviourData.getOtherReasonApplicantWantToStopFromRespondentDoing());
-        boolean otherReasonCompleted = (otherReason.isPresent() && !(otherReason.get().isBlank()));
         Optional<List<ApplicantStopFromRespondentDoingEnum>> applicantStopRespondentList
             = ofNullable(respondentBehaviourData.getApplicantWantToStopFromRespondentDoing());
-        return otherReasonCompleted
-            && applicantStopRespondentList.isPresent() && !applicantStopRespondentList.isEmpty();
+        return otherReason.isPresent() && applicantStopRespondentList.isPresent();
     }
 
 }
