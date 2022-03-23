db997a4ad56e428f26fda2984155f1e04e244fa5
pete-lowe <petemlowe@gmail.com>
2022-03-23 15:42:29 +0000

    added document mocking

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/C100ReSubmitApplicationControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/C100ReSubmitApplicationControllerTest.java
@@ -8,20 +8,24 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Value;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.CaseEventDetail;
 import uk.gov.hmcts.reform.prl.enums.State;
+import uk.gov.hmcts.reform.prl.models.dto.GeneratedDocumentInfo;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.models.language.DocumentLanguage;
 import uk.gov.hmcts.reform.prl.services.CaseEventService;
 import uk.gov.hmcts.reform.prl.services.CaseWorkerEmailService;
+import uk.gov.hmcts.reform.prl.services.DgsService;
 import uk.gov.hmcts.reform.prl.services.DocumentLanguageService;
 import uk.gov.hmcts.reform.prl.services.OrganisationService;
 import uk.gov.hmcts.reform.prl.services.SolicitorEmailService;
 import uk.gov.hmcts.reform.prl.services.tab.alltabs.AllTabServiceImpl;
 
+import javax.annotation.processing.Generated;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -37,6 +41,42 @@ import static uk.gov.hmcts.reform.prl.enums.YesOrNo.Yes;
 
 @RunWith(MockitoJUnitRunner.class)
 public class C100ReSubmitApplicationControllerTest {
+
+    @Value("${document.templates.c100.c100_final_template}")
+    protected String c100FinalTemplate;
+
+    @Value("${document.templates.c100.c100_final_filename}")
+    protected String c100FinalFilename;
+
+    @Value("${document.templates.c100.c100_c8_template}")
+    protected String c100C8Template;
+
+    @Value("${document.templates.c100.c100_c8_filename}")
+    protected String c100C8Filename;
+
+    @Value("${document.templates.c100.c100_c1a_template}")
+    protected String c100C1aTemplate;
+
+    @Value("${document.templates.c100.c100_c1a_filename}")
+    protected String c100C1aFilename;
+
+    @Value("${document.templates.c100.c100_final_welsh_template}")
+    protected String c100FinalWelshTemplate;
+
+    @Value("${document.templates.c100.c100_final_welsh_filename}")
+    protected String c100FinalWelshFilename;
+
+    @Value("${document.templates.c100.c100_c8_welsh_template}")
+    protected String c100C8WelshTemplate;
+
+    @Value("${document.templates.c100.c100_c8_welsh_filename}")
+    protected String c100C8WelshFilename;
+
+    @Value("${document.templates.c100.c100_c1a_welsh_template}")
+    protected String c100C1aWelshTemplate;
+
+    @Value("${document.templates.c100.c100_c1a_welsh_filename}")
+    protected String c100C1aWelshFilename;
 
     @InjectMocks
     C100ReSubmitApplicationController c100ReSubmitApplicationController;
@@ -62,6 +102,9 @@ public class C100ReSubmitApplicationControllerTest {
     @Mock
     ObjectMapper objectMapper;
 
+    @Mock
+    DgsService dgsService;
+
     private CallbackRequest callbackRequest;
     private CaseDetails caseDetails;
     private CaseData caseData;
@@ -69,7 +112,7 @@ public class C100ReSubmitApplicationControllerTest {
 
 
     @Before
-    public void init() {
+    public void init() throws Exception {
         caseData = CaseData.builder()
             .id(12345L)
             .allegationsOfHarmYesNo(Yes)
@@ -82,6 +125,27 @@ public class C100ReSubmitApplicationControllerTest {
         callbackRequest = CallbackRequest.builder()
             .caseDetails(caseDetails)
             .build();
+
+        uk.gov.hmcts.reform.prl.models.dto.ccd.CaseDetails prlCaseDetails = uk.gov.hmcts.reform.prl.models.dto.ccd.CaseDetails.builder()
+            .caseData(caseData)
+            .build();
+
+        GeneratedDocumentInfo englishFinal = GeneratedDocumentInfo.builder().build();
+        GeneratedDocumentInfo welshFinal = GeneratedDocumentInfo.builder().build();
+        GeneratedDocumentInfo englishC1a = GeneratedDocumentInfo.builder().build();
+        GeneratedDocumentInfo welshC1a = GeneratedDocumentInfo.builder().build();
+        GeneratedDocumentInfo englishC8 = GeneratedDocumentInfo.builder().build();
+        GeneratedDocumentInfo welshC8 = GeneratedDocumentInfo.builder().build();
+
+        when(dgsService.generateDocument(auth, prlCaseDetails, c100FinalTemplate)).thenReturn(englishFinal);
+        when(dgsService.generateDocument(auth, prlCaseDetails, c100FinalWelshTemplate)).thenReturn(welshFinal);
+        when(dgsService.generateDocument(auth, prlCaseDetails, c100C1aTemplate)).thenReturn(englishC1a);
+        when(dgsService.generateDocument(auth, prlCaseDetails, c100C1aWelshTemplate)).thenReturn(welshC1a);
+        when(dgsService.generateDocument(auth, prlCaseDetails, c100C8Template)).thenReturn(englishC8);
+        when(dgsService.generateDocument(auth, prlCaseDetails, c100C8WelshTemplate)).thenReturn(welshC8);
+
+        when(organisationService.getApplicantOrganisationDetails(caseData)).thenReturn(caseData);
+        when(organisationService.getRespondentOrganisationDetails(caseData)).thenReturn(caseData);
 
     }
 
@@ -121,8 +185,6 @@ public class C100ReSubmitApplicationControllerTest {
         when(objectMapper.convertValue(caseDetails.getData(), CaseData.class)).thenReturn(caseData);
         when(caseEventService.findEventsForCase(String.valueOf(caseData.getId()))).thenReturn(caseEvents);
         when(documentLanguageService.docGenerateLang(caseData)).thenReturn(documentLanguage);
-        when(organisationService.getApplicantOrganisationDetails(caseData)).thenReturn(caseData);
-        when(organisationService.getRespondentOrganisationDetails(caseData)).thenReturn(caseData);
         AboutToStartOrSubmitCallbackResponse response = c100ReSubmitApplicationController.resubmitApplication(auth, callbackRequest);
 
         assertEquals(response.getData().get("state"), State.CASE_ISSUE.getValue());
@@ -157,8 +219,7 @@ public class C100ReSubmitApplicationControllerTest {
         when(objectMapper.convertValue(caseDetails.getData(), CaseData.class)).thenReturn(caseDataNoAllegations);
         when(caseEventService.findEventsForCase(String.valueOf(caseDataNoAllegations.getId()))).thenReturn(caseEvents);
         when(documentLanguageService.docGenerateLang(caseDataNoAllegations)).thenReturn(documentLanguage);
-        when(organisationService.getApplicantOrganisationDetails(caseDataNoAllegations)).thenReturn(caseDataNoAllegations);
-        when(organisationService.getRespondentOrganisationDetails(caseDataNoAllegations)).thenReturn(caseDataNoAllegations);
+
         AboutToStartOrSubmitCallbackResponse response = c100ReSubmitApplicationController.resubmitApplication(auth, callbackRequest);
 
         assertEquals(response.getData().get("state"), State.CASE_ISSUE.getValue());
