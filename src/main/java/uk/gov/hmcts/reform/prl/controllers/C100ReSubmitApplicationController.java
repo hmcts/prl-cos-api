e189f415dd233a4de6d1cf3f155205b17c91d671
pete-lowe <petemlowe@gmail.com>
2022-03-25 12:22:51 +0000

    added start of functional test

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/C100ReSubmitApplicationController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/C100ReSubmitApplicationController.java
@@ -42,7 +42,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.DOCUMENT_FIELD_C1A;
