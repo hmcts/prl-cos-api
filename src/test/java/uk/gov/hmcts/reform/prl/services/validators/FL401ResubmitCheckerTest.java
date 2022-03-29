ca0d9f03f869e3b08c202e872d3cb9817bc66472
pete-lowe <petemlowe@gmail.com>
2022-03-29 13:45:47 +0100

    fixed tests

--- src/test/java/uk/gov/hmcts/reform/prl/services/validators/FL401ResubmitCheckerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/validators/FL401ResubmitCheckerTest.java
@@ -1,14 +1,17 @@
 package uk.gov.hmcts.reform.prl.services.validators;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class FL401ResubmitCheckerTest {
 
     @Mock
