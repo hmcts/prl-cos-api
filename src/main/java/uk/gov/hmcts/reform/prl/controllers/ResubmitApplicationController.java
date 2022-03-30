b63b4db7b6c2ff7a50947272d14a3bbedb0ea1bd
pete-lowe <petemlowe@gmail.com>
2022-03-30 09:13:50 +0100

    added tab refresh

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ResubmitApplicationController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ResubmitApplicationController.java
@@ -167,6 +167,7 @@ public class ResubmitApplicationController {
                 caseDataUpdated.put("isNotificationSent", "No");
             }
         }
+        caseDataUpdated.putAll(allTabService.getAllTabsFields(caseData));
         return AboutToStartOrSubmitCallbackResponse.builder()
             .data(caseDataUpdated)
             .build();
