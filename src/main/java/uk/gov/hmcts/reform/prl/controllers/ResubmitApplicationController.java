2c84cbe7c5cb9e950cc6f117f2b0fe14641c0f84
pete-lowe <petemlowe@gmail.com>
2022-03-30 10:13:00 +0100

    added document generation

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ResubmitApplicationController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ResubmitApplicationController.java
@@ -167,6 +167,7 @@ public class ResubmitApplicationController {
                 caseDataUpdated.put("isNotificationSent", "No");
             }
         }
+        caseDataUpdated.putAll(documentGenService.generateDocuments(authorisation, caseData));
         caseDataUpdated.putAll(allTabService.getAllTabsFields(caseData));
         return AboutToStartOrSubmitCallbackResponse.builder()
             .data(caseDataUpdated)
