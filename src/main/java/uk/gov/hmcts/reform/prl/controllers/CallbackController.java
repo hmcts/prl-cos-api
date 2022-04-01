aa934b730836e250d970f9eccd6ba4831beaf521
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-04-01 14:15:42 +0100

    initial commit

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/CallbackController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/CallbackController.java
@@ -498,4 +498,28 @@ public class CallbackController {
 
         return AboutToStartOrSubmitCallbackResponse.builder().data(caseDataUpdated).build();
     }
+
+    @PostMapping(path = "/copy-manage-docs-for-tabs", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
+    @ApiOperation(value = "Copy fl401 case name to C100 Case name")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Callback processed.", response = uk.gov.hmcts.reform.prl.models.dto.ccd.CallbackResponse.class),
+        @ApiResponse(code = 400, message = "Bad Request")})
+    public AboutToStartOrSubmitCallbackResponse copyManageDocsForTabs(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation,
+        @RequestBody uk.gov.hmcts.reform.ccd.client.model.CallbackRequest callbackRequest
+    ) {
+        Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
+
+        if (caseDataUpdated.get("furtherEvidences") != null) {
+            caseDataUpdated.put("mainAppDocForTabDisplay", caseDataUpdated.get("furtherEvidences"));
+        }
+        if (caseDataUpdated.get("correspondence") != null) {
+            caseDataUpdated.put("correspondenceForTabDisplay", caseDataUpdated.get("correspondence"));
+        }
+        if (caseDataUpdated.get("otherDocuments") != null) {
+            caseDataUpdated.put("otherDocumentsTabDisplay", caseDataUpdated.get("otherDocuments"));
+        }
+        log.info("*** Check Documents *** {}",caseDataUpdated);
+        return AboutToStartOrSubmitCallbackResponse.builder().data(caseDataUpdated).build();
+    }
 }
