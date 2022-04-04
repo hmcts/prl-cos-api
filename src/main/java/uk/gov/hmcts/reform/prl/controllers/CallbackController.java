04190a9e100829b2bfd9cd383a9e680bcfa76f5b
swaroopa86 <pendyalaswaroopa86@gmail.com>
2022-04-04 07:28:41 +0100

    withdrawal request flag is added to show retrun application event

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/CallbackController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/CallbackController.java
@@ -457,6 +457,7 @@ public class CallbackController {
         if ((withdrawApplication.isPresent() && Yes.equals(withdrawApplication.get()))) {
 
             if (previousState.isPresent() && !stateList.contains(previousState.get())) {
+                caseData = caseData.toBuilder().isWithdrawRequestSent("Yes").build();
                 if (PrlAppsConstants.C100_CASE_TYPE.equalsIgnoreCase(caseData.getCaseTypeOfApplication())) {
                     solicitorEmailService.sendWithDrawEmailToSolicitorAfterIssuedState(caseDetails, userDetails);
                     Optional<List<Element<LocalCourtAdminEmail>>> localCourtAdmin = ofNullable(caseData.getLocalCourtAdmin());
--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
@@ -392,7 +392,7 @@ public class CaseData implements MappableObject {
      */
     private final String familymanCaseNumber;
     private final String fl401FamilymanCaseNumber;
-  
+
     /**
      * Manage Documents.
      */
@@ -556,4 +556,8 @@ public class CaseData implements MappableObject {
     private String isDocumentGenerated;
     private String isNotificationSent;
 
+    /**
+     *  Withdraw request flag.
+     */
+    private String isWithdrawRequestSent;
 }
