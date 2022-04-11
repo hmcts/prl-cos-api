408f4d753c253827a67b0047455054c44508fb78
M_2167254 <m_2167254@ambgb000499.home>
2022-04-11 17:40:52 +0100

    upload order changes

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersController.java
@@ -89,15 +89,15 @@ public class ManageOrdersController {
 
         StringBuilder slectedOrder = new StringBuilder();
         slectedOrder.append(caseData.getApplicantCaseName());
-        slectedOrder.append("\n");
+        slectedOrder.append("\n\n");
         slectedOrder.append(caseData.getCaseTypeOfApplication().equalsIgnoreCase(FL401_CASE_TYPE) ?
                                     String.format("Family Man ID: ",caseData.getFl401FamilymanCaseNumber())
                                     : String.format("Family Man ID: ",caseData.getFamilymanCaseNumber()));
-        slectedOrder.append("\n");
+        slectedOrder.append("\n\n");
         slectedOrder.append(caseData.getManageOrdersOptions() == ManageOrdersOptionsEnum.createAnOrder
                                 ? caseData.getCreateSelectOrderOptions().getDisplayedValue()
                                 :caseData.getChildArrangementOrders().getDisplayedValue());
-        slectedOrder.append("\n");
+        slectedOrder.append("\n\n");
         CaseData caseDataInput = CaseData.builder().childrenList(builder.toString())
             .selectedOrder(slectedOrder.toString()).build();
 
