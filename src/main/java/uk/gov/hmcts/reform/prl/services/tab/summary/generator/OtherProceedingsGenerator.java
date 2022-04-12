a618f2a0b5e32b50fecd14d4870163079d8c7155
M_776008 <m_776008@ambgb000496.broadband>
2022-04-12 17:21:45 +0100

    Changed the unused import

--- src/main/java/uk/gov/hmcts/reform/prl/services/tab/summary/generator/OtherProceedingsGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/tab/summary/generator/OtherProceedingsGenerator.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.prl.services.tab.summary.generator;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.prl.enums.TypeOfOrderEnum;
 import uk.gov.hmcts.reform.prl.enums.YesNoDontKnow;
