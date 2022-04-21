273330fca989e97619c221c8347e9e70d349ff03
pete-lowe <petemlowe@gmail.com>
2022-04-21 10:28:39 +0100

    changed court name

--- src/main/java/uk/gov/hmcts/reform/prl/enums/CourtDetailsPilotEnum.java
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/CourtDetailsPilotEnum.java
@@ -7,10 +7,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum CourtDetailsPilotEnum {
 
-    swanseaCountyCourt("family.swansea.countycourt@justice.gov.uk", "Swansea County Court", "344"),
-    southamptonCountyCourt("family.southampton.countycourt@justice.gov.uk", "Southampton County Court", "328"),
-    exeterCountyCourt("family.exeter.countycourt@justice.gov.uk", "Exeter County Court", "198"),
-    gloucesterCountyCourt("family.gloucester.countycourt@justice.gov.uk", "Gloucester County Court", "203");
+    swanseaCountyCourt("family.swansea.countycourt@justice.gov.uk", "Swansea Family Court", "344"),
+    southamptonCountyCourt("family.southampton.countycourt@justice.gov.uk", "Southampton Family Court", "328"),
+    exeterCountyCourt("family.exeter.countycourt@justice.gov.uk", "Exeter Family Court", "198"),
+    gloucesterCountyCourt("family.gloucester.countycourt@justice.gov.uk", "Gloucester Family Court", "203");
 
     private final String courtEmail;
     private final String courtName;
