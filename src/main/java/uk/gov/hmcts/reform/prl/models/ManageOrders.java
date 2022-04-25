1836eeff2b6bfde7988f0897a6b9e47098adf2a4
swaroopa86 <pendyalaswaroopa86@gmail.com>
2022-04-25 14:49:42 +0100

    casedata is updated

---
+++ src/main/java/uk/gov/hmcts/reform/prl/models/ManageOrders.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.reform.prl.models;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+import uk.gov.hmcts.reform.prl.enums.ChildArrangementOrderTypeEnum;
+import uk.gov.hmcts.reform.prl.enums.OrderTypeEnum;
+import uk.gov.hmcts.reform.prl.enums.YesOrNo;
+
+import java.util.List;
+
+@Data
+@Builder
+@Jacksonized
+public class ManageOrders {
+    private final List<OrderTypeEnum> childArrangementsOrdersToIssue;
+    private final ChildArrangementOrderTypeEnum selectChildArrangementsOrder;
+    private final YesOrNo doesOrderClosesCase;
+
+}
