1a8a448278f9d5147593f67347821a8e97bf9a0e
pete-lowe <petemlowe@gmail.com>
2022-05-03 20:30:39 +0100

    added further functional tests

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/FeeAndPayServiceRequestControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/FeeAndPayServiceRequestControllerFunctionalTest.java
@@ -1,24 +1,19 @@
 package uk.gov.hmcts.reform.prl.controllers;
 
-import io.restassured.RestAssured;
-import io.restassured.specification.RequestSpecification;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 import uk.gov.hmcts.reform.prl.Application;
 import uk.gov.hmcts.reform.prl.ResourceLoader;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CallbackRequest;
+import uk.gov.hmcts.reform.prl.models.dto.payment.PaymentServiceResponse;
 import uk.gov.hmcts.reform.prl.services.PaymentRequestService;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -31,18 +26,6 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = { Application.class })
 public class FeeAndPayServiceRequestControllerFunctionalTest {
-
-//    private final String userToken = "Bearer testToken";
-//
-//    private static final String VALID_REQUEST_BODY = "controller/valid-request-body.json";
-//
-//    private final String targetInstance =
-//        StringUtils.defaultIfBlank(
-//            System.getenv("TEST_URL"),
-//            "http://localhost:4044"
-//        );
-//
-//    private final RequestSpecification request = RestAssured.given().relaxedHTTPSValidation().baseUri(targetInstance);
 
     private MockMvc mockMvc;
 
@@ -60,35 +43,24 @@ public class FeeAndPayServiceRequestControllerFunctionalTest {
     }
 
     @Test
-    public void givenMiamAttendance_whenPostRequestToMiamValidatation_then200ResponseAndNoErrors() throws Exception {
+    public void givenC100Case_whenPostRequestToCreateServiceRequest_then200ResponseAndNoErrors() throws Exception {
         String requestBody = ResourceLoader.loadJson(CREATE_SERVICE_REQUEST);
 
-        PaymentRequestService paymentRequestService = new PaymentRequestService();
+        PaymentServiceResponse paymentServiceResponse = PaymentServiceResponse.builder().serviceRequestReference("C100Test")
+            .build();
 
-        when(paymentRequestService.createServiceRequest(any(CallbackRequest.class), any(String.class))).thenReturn()
+        when(paymentRequestService.createServiceRequest(any(CallbackRequest.class), any(String.class)))
+            .thenReturn(paymentServiceResponse);
 
 
-
-                                                        mockMvc.perform(post("/create-payment-service-request")
+        mockMvc.perform(post("/create-payment-service-request")
                             .contentType(MediaType.APPLICATION_JSON)
                             .header("Authorization", "auth")
                             .content(requestBody)
                             .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
-//            .andExpect(jsonPath("errors").isEmpty())
+            .andExpect(jsonPath("data.paymentServiceRequestReferenceNumber").value("C100Test"))
             .andReturn();
 
     }
-
-//    @Test
-//    public void givenNoRequestBodyReturn400() throws Exception {
-//        String requestBody = ResourceLoader.loadJson(VALID_REQUEST_BODY);
-//        request
-//            .header("Authorization", userToken)
-//            .when()
-//            .contentType("application/json")
-//            .post("/create-payment-service-request")
-//            .then()
-//            .assertThat().statusCode(400);
-//    }
 }
