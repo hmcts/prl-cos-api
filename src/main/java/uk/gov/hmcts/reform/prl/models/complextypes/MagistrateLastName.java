dcddd99888521717cb172fecec08a4b686b6d657
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-05-16 14:06:25 +0100

    updated magistrate last name

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/MagistrateLastName.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/MagistrateLastName.java
@@ -1,12 +1,18 @@
 package uk.gov.hmcts.reform.prl.models.complextypes;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-@AllArgsConstructor
 public class MagistrateLastName {
+    @JsonProperty("lastName")
     private final String lastName;
+
+    @JsonCreator
+    public MagistrateLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
