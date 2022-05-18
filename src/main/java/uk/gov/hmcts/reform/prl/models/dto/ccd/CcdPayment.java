3e1cfdf56360904b8b907510ed0584a849a0b1b5
Aloknath Datta <alokdatta.uit@gmail.com>
2022-05-18 15:51:48 +0100

    Fixed CCD Payment

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CcdPayment.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CcdPayment.java
@@ -1,21 +1,9 @@
 package uk.gov.hmcts.reform.prl.models.dto.ccd;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.extern.jackson.Jacksonized;
-import uk.gov.hmcts.reform.prl.enums.YesOrNo;
-import uk.gov.hmcts.reform.prl.enums.manageorders.JudgeOrMagistrateTitleEnum;
-import uk.gov.hmcts.reform.prl.enums.manageorders.UnderTakingEnum;
-import uk.gov.hmcts.reform.prl.models.Address;
-import uk.gov.hmcts.reform.prl.models.Element;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @Data
 @Builder
