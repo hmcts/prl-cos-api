2a9ebef77cc1c7c8f5138e764b580a1fc0e7bbbd
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-05-24 13:53:41 +0100

    Added respondent email notification when not represented by solicitor

---
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/notify/serviceofapplication/RespondentSolicitorEmail.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.reform.prl.models.dto.notify.serviceofapplication;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+import uk.gov.hmcts.reform.prl.models.dto.notify.EmailTemplateVars;
+
+import java.time.LocalDate;
+
+@Data
+public class RespondentSolicitorEmail extends EmailTemplateVars {
+
+    @JsonProperty("caseName")
+    private final String caseName;
+    @JsonProperty("issueDate")
+    private final LocalDate issueDate;
+    @JsonProperty("solicitorName")
+    private final String solicitorName;
+    @JsonProperty("caseLink")
+    private final String caseLink;
+
+    @Builder
+    public RespondentSolicitorEmail(String caseReference,
+                                                        String caseName,
+                                                        LocalDate issueDate,
+                                                        String solicitorName,
+                                                        String caseLink) {
+        super(caseReference);
+        this.caseName = caseName;
+        this.issueDate = issueDate;
+        this.solicitorName = solicitorName;
+        this.caseLink = caseLink;
+    }
+}
