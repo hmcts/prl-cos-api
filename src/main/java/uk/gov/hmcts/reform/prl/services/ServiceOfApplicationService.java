a8ebddaf3a1849c4e368c8fbdc4c7f7f793eadb0
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-05-24 11:22:32 +0100

    Added email service for fl401 nad c100 flows for Service of Application

--- src/main/java/uk/gov/hmcts/reform/prl/services/ServiceOfApplicationService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ServiceOfApplicationService.java
@@ -1,17 +1,17 @@
 package uk.gov.hmcts.reform.prl.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
-import uk.gov.hmcts.reform.prl.services.time.Time;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.C100_CASE_TYPE;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.FL401_CASE_TYPE;
 
 @Service
@@ -22,11 +22,7 @@ public class ServiceOfApplicationService {
     public static final String FAMILY_MAN_ID = "Family Man ID: ";
 
     @Autowired
-    private final DgsService dgsService;
-
-    private final Time dateTime;
-
-    private final ObjectMapper objectMapper;
+    private final ServiceOfApplicationEmailService serviceOfApplicationEmailService;
 
     public Map<String, Object> populateHeader(CaseData caseData, Map<String,Object> caseDataUpdated) {
         caseDataUpdated.put("serviceOfApplicationHeader", getHeaderInfo(caseData));
@@ -56,7 +52,6 @@ public class ServiceOfApplicationService {
         }
         return caseData;
     }
-
 
     private Map<String,Object> getUpdatedCaseData(String selectedOrder, Map<String,Object> caseData) {
 
@@ -122,8 +117,6 @@ public class ServiceOfApplicationService {
         return caseData;
     }
 
-
-
     private String getHeaderInfo(CaseData caseData) {
         StringBuilder headerInfo = new StringBuilder();
         headerInfo.append("Case Name: " + caseData.getApplicantCaseName());
@@ -140,5 +133,13 @@ public class ServiceOfApplicationService {
         return FL401_CASE_TYPE.equalsIgnoreCase(caseData.getCaseTypeOfApplication())
             ? FAMILY_MAN_ID + caseData.getFl401FamilymanCaseNumber()
             : FAMILY_MAN_ID + caseData.getFamilymanCaseNumber();
+    }
+
+    public void sendEmail(CaseDetails caseDetails) throws Exception {
+        if (C100_CASE_TYPE.equalsIgnoreCase(caseDetails.getData().get("caseTypeOfApplication").toString())) {
+            serviceOfApplicationEmailService.sendEmailC100(caseDetails);
+        } else {
+            serviceOfApplicationEmailService.sendEmailFL401(caseDetails);
+        }
     }
 }
