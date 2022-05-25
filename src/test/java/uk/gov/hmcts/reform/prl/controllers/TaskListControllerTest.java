5706dc19ee28b6d8ca8db2b89de11e1ecc4e271c
Sairam Kantheti <m_2167254@ambgb000499.home>
2022-05-25 16:13:42 +0100

    included test class

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/TaskListControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/TaskListControllerTest.java
@@ -1,16 +1,29 @@
 package uk.gov.hmcts.reform.prl.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.reform.prl.enums.LanguagePreference;
+import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.events.CaseDataChanged;
+import uk.gov.hmcts.reform.prl.models.Element;
+import uk.gov.hmcts.reform.prl.models.complextypes.PartyDetails;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.services.EventService;
+import uk.gov.hmcts.reform.prl.services.tab.alltabs.AllTabServiceImpl;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 public class TaskListControllerTest {
@@ -19,7 +32,17 @@ public class TaskListControllerTest {
     TaskListController taskListController;
 
     @Mock
+    ObjectMapper objectMapper;
+
+    @Mock
     EventService eventPublisher;
+
+    @Mock
+    AllTabServiceImpl allTabService;
+
+
+    public static final String authToken = "Bearer TestAuthToken";
+
 
     @Test
     public void testHandleSubmitted() {
@@ -28,5 +51,93 @@ public class TaskListControllerTest {
         taskListController.publishEvent(caseDataChanged);
 
         verify(eventPublisher, times(1)).publishEvent(caseDataChanged);
+    }
+
+    @Test
+    public void testHandleSubmittedfl401() {
+
+
+
+
+        PartyDetails fl401Applicant = PartyDetails.builder()
+            .canYouProvideEmailAddress(YesOrNo.No)
+            .isAddressConfidential(YesOrNo.No)
+            .isPhoneNumberConfidential(YesOrNo.No)
+            .firstName("test")
+            .lastName("test")
+            .build();
+
+        String applicantNames = "TestFirst TestLast";
+
+        CaseData caseData = CaseData.builder()
+            .id(12345L)
+            .applicantCaseName("TestCaseName")
+            .applicantsFL401(fl401Applicant)
+            .courtEmailAddress("localcourt@test.com")
+            .dateSubmitted(String.valueOf("22-02-2022"))
+            .caseTypeOfApplication("FL401")
+            .welshLanguageRequirementApplication(LanguagePreference.english)
+            .languageRequirementApplicationNeedWelsh(YesOrNo.Yes)
+            .build();
+
+
+
+        Map<String, Object> stringObjectMap = caseData.toMap(new ObjectMapper());
+
+        uk.gov.hmcts.reform.ccd.client.model.CallbackRequest callbackRequest = uk.gov.hmcts.reform.ccd.client.model
+            .CallbackRequest.builder()
+            .caseDetails(uk.gov.hmcts.reform.ccd.client.model.CaseDetails.builder()
+                             .id(12345L)
+                             .data(stringObjectMap)
+                             .build())
+            .build();
+        when(objectMapper.convertValue(stringObjectMap, CaseData.class)).thenReturn(caseData);
+        doNothing().when(allTabService).updateAllTabsIncludingConfTab(Mockito.any(CaseData.class));
+        taskListController.handleSubmitted(callbackRequest,authToken);
+
+
+    }
+
+
+    @Test
+    public void testHandleSubmittedc100() {
+
+
+        PartyDetails partyDetails = PartyDetails.builder()
+            .canYouProvideEmailAddress(YesOrNo.No)
+            .isAddressConfidential(YesOrNo.No)
+            .isPhoneNumberConfidential(YesOrNo.No)
+            .firstName("test")
+            .lastName("test")
+            .build();
+
+        String applicantNames = "TestFirst TestLast";
+
+        Element<PartyDetails> partyDetailsElement = Element.<PartyDetails>builder().value(partyDetails).build();
+        List<Element<PartyDetails>> applicants = Collections.singletonList(partyDetailsElement);
+
+        CaseData caseData = CaseData.builder()
+            .id(12345L)
+            .applicantCaseName("TestCaseName")
+            .applicants(applicants)
+            .courtEmailAddress("localcourt@test.com")
+            .dateSubmitted(String.valueOf("22-02-2022"))
+            .caseTypeOfApplication("C100")
+            .welshLanguageRequirementApplication(LanguagePreference.english)
+            .languageRequirementApplicationNeedWelsh(YesOrNo.Yes)
+            .build();
+
+        Map<String, Object> stringObjectMap = caseData.toMap(new ObjectMapper());
+
+        uk.gov.hmcts.reform.ccd.client.model.CallbackRequest callbackRequest = uk.gov.hmcts.reform.ccd.client.model
+            .CallbackRequest.builder()
+            .caseDetails(uk.gov.hmcts.reform.ccd.client.model.CaseDetails.builder()
+                             .id(12345L)
+                             .data(stringObjectMap)
+                             .build())
+            .build();
+        when(objectMapper.convertValue(stringObjectMap, CaseData.class)).thenReturn(caseData);
+        doNothing().when(allTabService).updateAllTabsIncludingConfTab(Mockito.any(CaseData.class));
+        taskListController.handleSubmitted(callbackRequest,authToken);
     }
 }
