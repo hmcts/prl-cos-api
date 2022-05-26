a179b9e6fe7c4299c3efa16d8cfad7c658fcf4e5
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-05-26 11:53:11 +0100

    modified fetch of orderstoSA in post service

--- src/main/java/uk/gov/hmcts/reform/prl/services/ServiceOfApplicationPostService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ServiceOfApplicationPostService.java
@@ -3,22 +3,27 @@ package uk.gov.hmcts.reform.prl.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.prl.enums.YesNoDontKnow;
 import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.models.Element;
+import uk.gov.hmcts.reform.prl.models.complextypes.PartyDetails;
 import uk.gov.hmcts.reform.prl.models.complextypes.serviceofapplication.OrdersToServeSA;
 import uk.gov.hmcts.reform.prl.models.documents.Document;
 import uk.gov.hmcts.reform.prl.models.dto.GeneratedDocumentInfo;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.services.document.DocumentGenService;
 
+import javax.servlet.http.Part;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.YES;
 import static uk.gov.hmcts.reform.prl.utils.DocumentUtils.toGeneratedDocumentInfo;
 
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.DOCUMENT_COVER_SHEET_HINT;
+import static uk.gov.hmcts.reform.prl.utils.ElementUtils.element;
 
 @Service
 @RequiredArgsConstructor
@@ -28,25 +33,51 @@ public class ServiceOfApplicationPostService {
     private BulkPrintService bulkPrintService;
 
     @Autowired
-    private OrdersToServeSA orders;
-
-    @Autowired
     private DocumentGenService documentGenService;
 
     private final static String LETTER_TYPE = "RespondentServiceOfApplication";
 
 
     public void send(CaseData caseData, String authorisation) throws Exception {
-        bulkPrintService.send(String.valueOf(caseData.getId()), authorisation, LETTER_TYPE, getListOfDocumentInfo(authorisation, caseData));
+        // Sends post to the respondents who are not represented by a solicitor
+        caseData.getRespondents().stream()
+            .map(Element::getValue)
+            .filter(partyDetails -> !YesNoDontKnow.yes.equals(partyDetails.getDoTheyHaveLegalRepresentation()))
+            .filter(partyDetails -> YesOrNo.Yes.equals(partyDetails.getIsCurrentAddressKnown()))
+            .forEach(partyDetails -> {
+
+            try {
+                bulkPrintService.send(
+                    String.valueOf(caseData.getId()),
+                    authorisation,
+                    LETTER_TYPE,
+                    getListOfDocumentInfo(authorisation, caseData, partyDetails)
+                );
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
-    private List<GeneratedDocumentInfo> getListOfDocumentInfo(String auth, CaseData caseData) throws Exception {
+    private List<GeneratedDocumentInfo> getListOfDocumentInfo(String auth, CaseData caseData, PartyDetails partyDetails) throws Exception {
         List<GeneratedDocumentInfo> docs = new ArrayList<>();
-        docs.add(generateCoverSheet(auth, caseData));
+
+        docs.add(generateCoverSheet(auth, getRespondentCaseData(partyDetails,caseData)));
         docs.add(getFinalDocument(caseData));
         getC1aDocument(caseData).ifPresent(docs::add);
         docs.addAll(getSelectedOrders(caseData));
+
+
         return docs;
+    }
+
+    private CaseData getRespondentCaseData(PartyDetails partyDetails, CaseData caseData) {
+            CaseData respondentCaseData = CaseData
+                .builder()
+                .id(caseData.getId())
+                .respondents(List.of(element(partyDetails)))
+                .build();
+        return respondentCaseData;
     }
 
     private GeneratedDocumentInfo getFinalDocument(CaseData caseData) {
@@ -78,7 +109,7 @@ public class ServiceOfApplicationPostService {
     private List<GeneratedDocumentInfo> getSelectedOrders(CaseData caseData) {
         List<GeneratedDocumentInfo> docs = new ArrayList<>();
 
-        List<String> orderNames = orders.getSelectedOrders().stream()
+        List<String> orderNames = caseData.getServiceOfApplicationScreen1().getSelectedOrders().stream()
             .map(this::getSelectedOrderTypes)
             .collect(Collectors.toList());
 
