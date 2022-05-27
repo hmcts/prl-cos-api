bd74ed86213464900ad7e4fc9b19752ea53b23b3
pete-lowe <petemlowe@gmail.com>
2022-05-27 16:28:59 +0100

    removed import

--- src/main/java/uk/gov/hmcts/reform/prl/services/ServiceOfApplicationPostService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ServiceOfApplicationPostService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.CannotCreateTransactionException;
 import uk.gov.hmcts.reform.prl.enums.YesNoDontKnow;
 import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.enums.manageorders.CreateSelectOrderOptionsEnum;
