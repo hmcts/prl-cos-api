2770ba58037b7ad2f7a566925b1974c600ebcc8a
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-05-31 12:48:06 +0100

    checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/prl/utils/HashGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/prl/utils/HashGenerator.java
@@ -13,10 +13,11 @@ public final class HashGenerator {
         final MessageDigest digest = MessageDigest.getInstance("SHA-256");
         final byte[] hash = digest.digest(base.getBytes(StandardCharsets.UTF_8));
         final StringBuilder hexString = new StringBuilder();
-        for (int i = 0; i < hash.length; i++) {
-            final String hex = Integer.toHexString(0xff & hash[i]);
-            if(hex.length() == 1)
+        for (byte b : hash) {
+            final String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
                 hexString.append('0');
+            }
             hexString.append(hex);
         }
         return hexString.toString();
