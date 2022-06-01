694b88c234c04ace757d6328db6537f181ed69aa
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-06-01 13:45:03 +0100

    added privacy notice to respondent solicitor template

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/serviceofapplication/OrdersToServeSA.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/serviceofapplication/OrdersToServeSA.java
@@ -30,25 +30,24 @@ import java.util.stream.Stream;
 @Builder
 public class OrdersToServeSA {
 
-
     private final List<StandardDirectionsOrderEnum> standardDirectionsOrderOption;
-    private final List<BlankOrderOrDirectionsEnum>    blankOrderOrDirectionsOption;
+    private final List<BlankOrderOrDirectionsEnum> blankOrderOrDirectionsOption;
     private final List<BlankOrderOrDirectionsWithdrawEnum> blankOrderOrDirectionsWithdrawOption;
-    private final List<ChildArrangementsSpecificProhibitedOrderEnum>    childArrangementSpecificOrderOption;
+    private final List<ChildArrangementsSpecificProhibitedOrderEnum> childArrangementSpecificOrderOption;
     private final List<ParentalResponsibilityEnum> parentalResponsibilityOption;
-    private final List<SpecialGuardianShipEnum>    specialGuardianShipOption;
+    private final List<SpecialGuardianShipEnum> specialGuardianShipOption;
     private final List<NoticeOfProceedingsPartiesEnum> noticeOfProceedingsPartiesOption;
-    private final List<NoticeOfProceedingsNonPartiesEnum>    noticeOfProceedingsNonPartiesOption;
+    private final List<NoticeOfProceedingsNonPartiesEnum> noticeOfProceedingsNonPartiesOption;
     private final List<TransferOfCaseToAnotherCourtEnum> transferOfCaseToAnotherCourtOption;
-    private final List<AppointmentOfGuardianEnum>    appointmentOfGuardianOption;
+    private final List<AppointmentOfGuardianEnum> appointmentOfGuardianOption;
     private final List<NonMolestationEnum> nonMolestationOption;
-    private final List<OccupationEnum>    occupationOption;
+    private final List<OccupationEnum> occupationOption;
     private final List<PowerOfArrestEnum> powerOfArrestOption;
-    private final List<AmendDischargedVariedEnum>    amendDischargedVariedOption;
+    private final List<AmendDischargedVariedEnum> amendDischargedVariedOption;
     private final List<BlankOrderEnum> blankOrderEnumOption;
-    private final List<GeneralFormUndertakingEnum>    generalFormUndertakingOption;
+    private final List<GeneralFormUndertakingEnum> generalFormUndertakingOption;
     private final List<NoticeOfProceedingsEnum> noticeOfProceedingsEnumOption;
-    private final List<OtherUploadAnOrderEnum>    otherUploadAnOrderOption;
+    private final List<OtherUploadAnOrderEnum> otherUploadAnOrderOption;
 
     public List<String> getSelectedOrders() {
         return Stream.of(OrdersToServeSA.class.getDeclaredFields()).filter(Objects::nonNull)
@@ -64,5 +63,4 @@ public class OrdersToServeSA {
             .map(s -> s.substring(1, s.length() - 1))
             .collect(Collectors.toList());
     }
-
 }
