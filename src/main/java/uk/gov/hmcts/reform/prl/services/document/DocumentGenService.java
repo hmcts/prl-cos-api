3df2fb1b3e55e801a757e4903d3efbeb2d83bf04
Sairam Kantheti <sai.kantheti@hmcts.net>
2022-06-08 16:04:52 +0100

    SNI-953 changes

--- src/main/java/uk/gov/hmcts/reform/prl/services/document/DocumentGenService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/document/DocumentGenService.java
@@ -172,7 +172,7 @@ public class DocumentGenService {
                 && YesOrNo.Yes.equals(caseData.getAllegationsOfHarmYesNo())) {
                 updatedCaseData.put(DOCUMENT_FIELD_C1A, getDocument(authorisation, caseData, C1A_HINT, false));
             }
-            if (!State.SUBMITTED_PAID.equals(caseData.getState())) {
+            if (!State.CASE_ISSUE.equals(caseData.getState())) {
                 updatedCaseData.put(DOCUMENT_FIELD_FINAL, getDocument(authorisation, caseData, FINAL_HINT, false));
             }
         }
