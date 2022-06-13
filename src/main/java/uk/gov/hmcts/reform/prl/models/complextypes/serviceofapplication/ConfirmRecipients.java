91419cbc00bc595ebe137b4147b1fb20d38e115c
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-06-13 17:50:40 +0100

    enum corrected

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/serviceofapplication/ConfirmRecipients.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/serviceofapplication/ConfirmRecipients.java
@@ -2,9 +2,10 @@ package uk.gov.hmcts.reform.prl.models.complextypes.serviceofapplication;
 
 import lombok.Builder;
 import lombok.Data;
-import uk.gov.hmcts.reform.prl.enums.RestrictToCafcassHmcts;
 import uk.gov.hmcts.reform.prl.enums.manageorders.OrderRecipientsEnum;
+import uk.gov.hmcts.reform.prl.enums.manageorders.OtherEnum;
 import uk.gov.hmcts.reform.prl.enums.manageorders.OtherOrderRecipientsEnum;
+import uk.gov.hmcts.reform.prl.enums.serviceofapplication.CafcassServiceApplicationEnum;
 import uk.gov.hmcts.reform.prl.models.Element;
 
 import java.util.List;
@@ -15,8 +16,8 @@ public class ConfirmRecipients {
     private final List<OrderRecipientsEnum> applicantsList;
     private final List<OtherOrderRecipientsEnum> otherPeopleList;
 
-    private final List<RestrictToCafcassHmcts> cafcassEmailOptionChecked;
-    private final List<RestrictToCafcassHmcts> otherEmailOptionChecked;
+    private final List<CafcassServiceApplicationEnum> cafcassEmailOptionChecked;
+    private final List<OtherEnum> otherEmailOptionChecked;
 
     private final List<Element<String>> cafcassEmailAddressList;
     private final List<Element<String>> otherEmailAddressList;
