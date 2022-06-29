922f309a352096fcf1c705075865f7d51d417877
Khurshed Khan <khurshed.khan@hmcts.net>
2022-06-29 15:43:15 +0100

    reverted chabges to make manage order work

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersController.java
@@ -31,7 +31,6 @@ import uk.gov.hmcts.reform.prl.services.UserService;
 import uk.gov.hmcts.reform.prl.utils.CaseUtils;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -42,7 +41,6 @@ import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.joining;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
-import static org.reflections.Reflections.log;
 import static uk.gov.hmcts.reform.prl.enums.manageorders.ManageOrdersOptionsEnum.amendOrderUnderSlipRule;
 
 @Slf4j
@@ -145,11 +143,8 @@ public class ManageOrdersController {
             callbackRequest.getCaseDetails().getData(),
             CaseData.class
         );
-        Map<String, Object> caseDataMap = new HashMap<>();
-        caseDataMap.putAll(manageOrderService.clearManageOrdersFields(caseData));
-        caseDataMap.putAll(manageOrderService.populateHeader(caseData));
         return AboutToStartOrSubmitCallbackResponse.builder()
-            .data(caseDataMap)
+            .data(manageOrderService.populateHeader(caseData))
             .build();
     }
 
