f12d93e54b569667d82b1d4cbe19d224c4d2dc41
Khurshed Khan <khurshed.khan@hmcts.net>
2022-06-29 16:52:30 +0100

    reverted chabges to make manage order work

--- src/main/java/uk/gov/hmcts/reform/prl/services/ManageOrderService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ManageOrderService.java
@@ -27,7 +27,6 @@ import uk.gov.hmcts.reform.prl.models.dto.ccd.ManageOrders;
 import uk.gov.hmcts.reform.prl.services.time.Time;
 import uk.gov.hmcts.reform.prl.utils.ElementUtils;
 
-import java.lang.reflect.Field;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -201,14 +200,6 @@ public class ManageOrderService {
         return headerMap;
     }
 
-    public Map<String, Object> clearManageOrdersFields(CaseData caseData) {
-        Map<String, Object> emptyManageOrdersMap = new HashMap<>();
-        for (Field field : caseData.getManageOrders().getClass().getDeclaredFields()) {
-            emptyManageOrdersMap.put(field.getName(), null);
-        }
-        emptyManageOrdersMap.put("previewOrderDoc", null);
-        return emptyManageOrdersMap;
-    }
 
     public CaseData getUpdatedCaseData(CaseData caseData) {
         return caseData.toBuilder().childrenList(getChildInfoFromCaseData(caseData))
