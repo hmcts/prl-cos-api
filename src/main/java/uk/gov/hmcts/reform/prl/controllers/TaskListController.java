ad61cc1e84e448b1905c1b673655e21edcb075da
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2022-06-30 11:54:56 +0100

    Adding coverage and fixed code smell

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/TaskListController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/TaskListController.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.prl.controllers;
 
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpHeaders;
@@ -26,7 +27,9 @@ import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.ISSUED_STATE;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.ROLES;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.SUBMITTED_STATE;
 
+
 @Api
+@Slf4j
 @RestController
 @RequestMapping("/update-task-list")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -52,13 +55,13 @@ public class TaskListController extends AbstractCallbackController {
         tabService.updateAllTabsIncludingConfTab(caseData);
         UserDetails userDetails = userService.getUserDetails(authorisation);
         List<String> roles = userDetails.getRoles();
-        boolean isCourtStaff = roles.stream().anyMatch(element -> ROLES.contains(element));
+        boolean isCourtStaff = roles.stream().anyMatch(ROLES::contains);
         String state = caseData.getState().getValue();
         if (isCourtStaff && (SUBMITTED_STATE.equalsIgnoreCase(state) || ISSUED_STATE.equalsIgnoreCase(state))) {
             try {
                 caseDataUpdated = dgsService.generateDocuments(authorisation, caseData);
             } catch (Exception e) {
-                e.printStackTrace();
+                log.error("Error regenerating the document", e);
             }
         }
         return AboutToStartOrSubmitCallbackResponse.builder().data(caseDataUpdated).build();
