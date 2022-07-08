98b4b58b1af0048480e31240a3df4b4df0bf0221
pete-lowe <petemlowe@gmail.com>
2022-07-08 13:55:45 +0100

    checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersController.java
@@ -31,7 +31,6 @@ import uk.gov.hmcts.reform.prl.services.UserService;
 import uk.gov.hmcts.reform.prl.utils.CaseUtils;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
