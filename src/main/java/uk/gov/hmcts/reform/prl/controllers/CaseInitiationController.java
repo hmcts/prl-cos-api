42cd383d96d085eb1d67707ecd376da8f1827c93
Sairam Kantheti <sai.kantheti@hmcts.net>
2022-07-11 12:25:14 +0100

    FPET-62 changes

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/CaseInitiationController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/CaseInitiationController.java
@@ -2,27 +2,39 @@ package uk.gov.hmcts.reform.prl.controllers;
 
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.prl.events.CaseDataChanged;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+import uk.gov.hmcts.reform.prl.services.caseaccess.AssignCaseAccessService;
 
 @Api
 @RestController
 @RequestMapping("/case-initiation")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Slf4j
 public class CaseInitiationController extends AbstractCallbackController {
 
-    @PostMapping("/submitted")
-    public void handleSubmitted(@RequestBody CallbackRequest callbackRequest) {
 
+    private  final AssignCaseAccessService assignCaseAccessService;
+
+    @PostMapping("/submitted")
+    public void handleSubmitted(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation,
+                                @RequestBody CallbackRequest callbackRequest) {
+
+        log.info("authorisation token: {}",authorisation);
         final CaseDetails caseDetails = callbackRequest.getCaseDetails();
         final CaseData caseData = getCaseData(caseDetails);
+
+        assignCaseAccessService.assignCaseAccess(authorisation, caseDetails.getId().toString());
 
         publishEvent(new CaseDataChanged(caseData));
 
