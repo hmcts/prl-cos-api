24870cc641fd129e8ca2ff5f32192c48dc89490e
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2022-07-11 16:42:57 +0100

    try

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersController.java
@@ -154,7 +154,7 @@ public class ManageOrdersController {
         );
 
         Map<String, Object> updatedCaseData = manageOrderService.populateHeader(caseData);
-        //updatedCaseData.putAll(manageOrderService.clearManageOrdersFields(caseData));
+        updatedCaseData.putAll(manageOrderService.clearManageOrdersFields(caseData));
         updatedCaseData.putAll(manageOrderService.getChildOptionList(caseData));
         return AboutToStartOrSubmitCallbackResponse.builder()
             .data(updatedCaseData)
