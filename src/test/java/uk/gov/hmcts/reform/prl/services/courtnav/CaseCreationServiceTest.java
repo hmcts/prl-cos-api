122e10813e3fd30174039fa9d67a126db0796adc
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2022-07-25 17:50:22 +0100

    Refactored for testing both citizen and caseworker flow

--- src/test/java/uk/gov/hmcts/reform/prl/services/courtnav/CaseCreationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/courtnav/CaseCreationServiceTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.prl.services.courtnav;
 
 import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -11,11 +10,9 @@ import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
 import uk.gov.hmcts.reform.idam.client.models.UserInfo;
-import uk.gov.hmcts.reform.prl.constants.PrlAppsConstants;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
