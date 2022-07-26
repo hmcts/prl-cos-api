abdb1b7e974853c12a4bee256b03697c6521275c
Khurshed Khan <khurshed.khan@hmcts.net>
2022-07-26 17:23:41 +0100

    PRL-1757:added changes for retrieving and link cases for Respondent

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseController.java
@@ -7,17 +7,13 @@ import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CallbackResponse;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.services.citizen.CaseService;
 
+import java.util.List;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
@@ -66,6 +62,29 @@ public class CaseController {
                                                                 s2sToken,
                                                                 caseId,
                                                                 eventId).getData(), CaseData.class);
+    }
+
+    @GetMapping(path = "/citizen/{role}/retrieve-cases/{userId}", produces = APPLICATION_JSON)
+    @ApiOperation(value = "Frontend to fetch case list")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Case details returned", response = CallbackResponse.class),
+        @ApiResponse(code = 400, message = "Bad Request")})
+    public List<CaseData> retrieveCases(
+        @PathVariable("role") String role,
+        @PathVariable("userId") String userId,
+        @RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation,
+        @RequestHeader("serviceAuthorization") String s2sToken
+    ) {
+        return caseService.retrieveCases(authorisation, s2sToken, role, userId);
+    }
+
+    @PutMapping("/citizen/link")
+    @ApiOperation("Links citizen to case")
+    public void linkDefendantToClaim(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation,
+                                     @RequestHeader(value = "caseId", required = false) String caseId,
+                                     @RequestHeader("serviceAuthorization") String s2sToken,
+                                     @RequestHeader("accessCode") String accessCode) {
+        caseService.linkCitizenToCase(authorisation, s2sToken, accessCode ,caseId);
     }
 
 }
