10cc8ae39b79317a19bcb0d0a58fa76e5c292537
Khurshed Khan <khurshed.khan@hmcts.net>
2022-07-27 10:18:51 +0100

    PRL-1765: fixed build issue

--- src/main/java/uk/gov/hmcts/reform/prl/utils/CaseDetailsConverter.java
+++ src/main/java/uk/gov/hmcts/reform/prl/utils/CaseDetailsConverter.java
@@ -1,20 +1,19 @@
 package uk.gov.hmcts.reform.prl.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 
 @Component
+@RequiredArgsConstructor
 public class CaseDetailsConverter {
 
     @Autowired
-    ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper;
 
-    public CaseDetailsConverter(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
 
     public CaseData extractCase(CaseDetails caseDetails) {
         return objectMapper.convertValue(caseDetails, CaseData.class);
