2a40835c1c936b5004bf083ec7d1ec085505a65b
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2022-08-02 17:45:06 +0100

    unused import removed

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/courtnav/CaseControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/courtnav/CaseControllerFunctionalTest.java
@@ -9,7 +9,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.prl.ResourceLoader;
