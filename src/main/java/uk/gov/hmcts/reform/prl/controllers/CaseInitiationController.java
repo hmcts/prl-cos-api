f43ebc165f4b455219527cb177c9140dfa789bcd
Praneeth G <praneethkumar.gadhe@hmcts.net>
2022-08-09 13:17:29 +0100

    added supplemetary data

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/CaseInitiationController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/CaseInitiationController.java
@@ -10,11 +10,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.prl.events.CaseDataChanged;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.services.caseaccess.AssignCaseAccessService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Tag(name = "case-initiation-controller")
 @RestController
@@ -26,6 +31,10 @@ public class CaseInitiationController extends AbstractCallbackController {
 
     private  final AssignCaseAccessService assignCaseAccessService;
 
+    private final CoreCaseDataApi coreCaseDataApi;
+
+    private final AuthTokenGenerator authTokenGenerator;
+
     @PostMapping("/submitted")
     public void handleSubmitted(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation,
                                 @RequestBody CallbackRequest callbackRequest) {
@@ -36,6 +45,14 @@ public class CaseInitiationController extends AbstractCallbackController {
         assignCaseAccessService.assignCaseAccess(caseDetails.getId().toString(),authorisation);
 
         publishEvent(new CaseDataChanged(caseData));
+
+        // setting supplementary data updates to enable global search
+        String caseId = String.valueOf(caseData.getId());
+        Map<String, Map<String, Map<String, Object>>> supplementaryData = new HashMap<>();
+        supplementaryData.put("supplementary_data_updates",
+            Map.of("$set", Map.of("HMCTSServiceId", "ABA5")));
+        coreCaseDataApi.submitSupplementaryData(authorisation, authTokenGenerator.generate(), caseId,
+            supplementaryData);
 
     }
 }
