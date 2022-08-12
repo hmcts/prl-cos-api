fc214a7c4b5eaf52f10d25399de28710a053015f
Aloknath Datta <alokdatta.uit@gmail.com>
2022-08-12 20:53:34 +0100

    Revert "Updated validate Access Code config"
    This reverts commit 45de407e

--- src/main/java/uk/gov/hmcts/reform/prl/services/citizen/CaseService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/citizen/CaseService.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Iterables;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.CaseAccessApi;
@@ -48,6 +47,7 @@ public class CaseService {
     @Autowired
     CaseAccessApi caseAccessApi;
 
+
     @Autowired
     IdamClient idamClient;
 
@@ -59,12 +59,6 @@ public class CaseService {
 
     @Autowired
     ObjectMapper objectMapper;
-
-    @Value("${idam.systemupdate.username}")
-    private String systemUpdateUsername;
-
-    @Value("${idam.systemupdate.password}")
-    private String systemUpdatePassword;
 
     public CaseDetails updateCase(CaseData caseData, String authToken, String s2sToken, String caseId, String eventId) {
 
@@ -256,12 +250,12 @@ public class CaseService {
             .build();
     }
 
-    public String validateAccessCode(String s2sToken, String caseId, String accessCode) {
+    public String validateAccessCode(String userToken, String s2sToken, String caseId, String accessCode) {
         log.info("validateAccessCode");
         log.info("parameters are :" + caseId + " and " + accessCode);
-        String accessCodeStatus = "invalid";
+        String accessCodeStatus = "Invalid access code";
         CaseData caseData = objectMapper.convertValue(
-            coreCaseDataApi.getCase(idamClient.getAccessToken(systemUpdateUsername, systemUpdatePassword), s2sToken, caseId).getData(),
+            coreCaseDataApi.getCase(userToken, s2sToken, caseId).getData(),
             CaseData.class
         );
 
@@ -277,10 +271,10 @@ public class CaseService {
         if (matchingCaseInvite.size() > 0) {
             for (CaseInvite caseInvite : matchingCaseInvite) {
                 if ("Yes".equals(caseInvite.getHasLinked())) {
-                    accessCodeStatus = "linked";
+                    accessCodeStatus = "Access code already linked";
                 }
             }
-            accessCodeStatus = "valid";
+            accessCodeStatus = "Access code is valid";
         }
         log.info("accessCodeStatus" + accessCodeStatus);
         return accessCodeStatus;
