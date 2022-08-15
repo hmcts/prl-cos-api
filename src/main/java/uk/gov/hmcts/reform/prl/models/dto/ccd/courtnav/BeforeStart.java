103bee1bd7960f855babcf58ce2a3e9dad18ec90
swaroopa86 <pendyalaswaroopa86@gmail.com>
2022-08-15 10:25:05 +0100

    @jacksonized property is added to beforestart section

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/courtnav/BeforeStart.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/courtnav/BeforeStart.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.jackson.Jacksonized;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.courtnav.enums.ApplicantAge;
 
 @Data
@@ -12,6 +13,7 @@ import uk.gov.hmcts.reform.prl.models.dto.ccd.courtnav.enums.ApplicantAge;
 @Getter
 @Setter
 @AllArgsConstructor
+@Jacksonized
 public class BeforeStart {
     private final ApplicantAge applicantHowOld;
 
