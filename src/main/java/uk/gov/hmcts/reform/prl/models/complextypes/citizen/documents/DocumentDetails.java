876e49ca08f837f578ba5e533f2602af83f561ac
sahaSonali <sonali.saha@hmcts.net>
2022-08-15 18:22:21 +0100

    commit for citizen document uplaod feature

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/documents/DocumentDetails.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/documents/DocumentDetails.java
@@ -3,11 +3,10 @@ package uk.gov.hmcts.reform.prl.models.complextypes.citizen.documents;
 import lombok.Builder;
 import lombok.Data;
 
-import java.util.Date;
 
 @Data
 @Builder
 public class DocumentDetails {
     private final String documentName;
-    private final Date documentUploadedDate;
+    private final String documentUploadedDate;
 }
