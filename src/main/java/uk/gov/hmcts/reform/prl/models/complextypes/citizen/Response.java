6e309713b659e66a0d6e95cff8cd43e649da9ac8
Aloknath Datta <alokdatta.uit@gmail.com>
2022-08-17 16:10:33 +0100

    updated citizen details for response

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/Response.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/Response.java
@@ -4,8 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.prl.enums.YesOrNo;
-import uk.gov.hmcts.reform.prl.models.complextypes.PartyDetails;
 import uk.gov.hmcts.reform.prl.models.complextypes.applicationtab.Miam;
+import uk.gov.hmcts.reform.prl.models.complextypes.citizen.common.CitizenDetails;
 import uk.gov.hmcts.reform.prl.models.complextypes.citizen.response.confidentiality.KeepDetailsPrivate;
 import uk.gov.hmcts.reform.prl.models.complextypes.citizen.response.consent.Consent;
 import uk.gov.hmcts.reform.prl.models.complextypes.citizen.response.internationalelements.CitizenInternationalElements;
@@ -18,7 +18,7 @@ public class Response {
     private final YesOrNo legalRepresentation;
     private final Consent consent;
     private final KeepDetailsPrivate confidentiality;
-    private final PartyDetails respondentDetails;
+    private final CitizenDetails citizenDetails;
     // TODO: Add support you need during your case here
     private final Miam miam;
     private final CurrentOrPreviousProceedings currentOrPreviousProceedings;
