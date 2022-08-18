90b537dc1232e6ed24c2f6c78d36beb078a49d36
sahaSonali <sonali.saha@hmcts.net>
2022-08-18 11:44:42 +0100

    adding @constructor in GenerateAndUploadDocumentRequest

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/citizen/GenerateAndUploadDocumentRequest.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/citizen/GenerateAndUploadDocumentRequest.java
@@ -1,18 +1,25 @@
 package uk.gov.hmcts.reform.prl.models.dto.citizen;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.Value;
 
 import java.util.Map;
 
-@Value
-@Schema(description = "Request body model for Document Generation Request")
 @Data
-@Builder
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Schema(description = "Request body model for Document Generation Request")
+@Getter
+@Setter
+@Builder(toBuilder = true)
 public class GenerateAndUploadDocumentRequest {
     @JsonProperty(value = "values", required = true)
     @Schema(description = "Placeholder key / value pairs", required = true)
