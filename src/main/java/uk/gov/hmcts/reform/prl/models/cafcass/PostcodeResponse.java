2e764d2d6327fdfe55436fbdc53d5bfcf9676448
Mohammed Murshad Haque <murshad.haque@HMCTS.NET>
2022-08-22 11:54:48 +0100

    ISDB-351: JUnit test

--- src/main/java/uk/gov/hmcts/reform/prl/models/cafcass/PostcodeResponse.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/cafcass/PostcodeResponse.java
@@ -1,12 +1,17 @@
 package uk.gov.hmcts.reform.prl.models.cafcass;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.Data;
+import lombok.ToString;
 
+import java.io.Serializable;
 import java.util.List;
 
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @Builder
-public class PostcodeResponse {
+public class PostcodeResponse implements java.io.Serializable {
     private List<PostcodeResult> results;
 }
--- src/test/java/uk/gov/hmcts/reform/prl/services/cafcass/postcode/PostcodeLookupServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/cafcass/postcode/PostcodeLookupServiceTest.java
@@ -1,11 +1,15 @@
 package uk.gov.hmcts.reform.prl.services.cafcass.postcode;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.internal.matchers.Any;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
@@ -13,7 +17,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import uk.gov.hmcts.reform.prl.config.cafcass.PostcodeLookupConfiguration;
+import uk.gov.hmcts.reform.prl.models.cafcass.AddressDetails;
+import uk.gov.hmcts.reform.prl.models.cafcass.PostcodeResponse;
+import uk.gov.hmcts.reform.prl.models.cafcass.PostcodeResult;
 import uk.gov.hmcts.reform.prl.services.cafcass.PostcodeLookupService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -32,6 +42,9 @@ public class PostcodeLookupServiceTest {
 
     @Mock
     PostcodeLookupConfiguration postcodeLookupConfiguration;
+
+    @Mock
+    ObjectMapper objectMapper;
 
     @Before
     public void setup() {
@@ -118,5 +131,45 @@ public class PostcodeLookupServiceTest {
         assertThrows(
                 RuntimeException.class, () -> postcodeLookupService
                         .isValidNationalPostCode("IG11 7YL", ENGLAND_POSTCODE_COUNTRYCODE));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenStatusIsOK() throws JsonProcessingException {
+        AddressDetails address = AddressDetails.builder()
+            .address("59 Middle Lane")
+            .postcode("N8 8PE")
+            .status(HttpStatus.OK.toString())
+            .countryCode("E")
+            .build();
+        PostcodeResult result = PostcodeResult.builder().dpa(address).build();
+        List<PostcodeResult> resultLst = new ArrayList<>();
+        resultLst.add(result);
+//ObjectA myobjectA = new ObjectA();
+//        //define the entity you want the exchange to return
+//        ResponseEntity<List<ObjectA>> myEntity = new ResponseEntity<List<ObjectA>>(HttpStatus.ACCEPTED);
+//        Mockito.when(restTemplate.exchange(
+//            Matchers.eq("/objects/get-objectA"),
+//            Matchers.eq(HttpMethod.POST),
+//            Matchers.<HttpEntity<List<ObjectA>>>any(),
+//            Matchers.<ParameterizedTypeReference<List<ObjectA>>>any())
+//        ).thenReturn(myEntity);
+
+        ResponseEntity<String> responseEntity =
+            new ResponseEntity<String>(PostcodeResponse.builder().results(resultLst).build().toString(), HttpStatus.OK);
+        when(postcodeLookupConfiguration.getUrl()).thenReturn("https://api.os.uk/search/places/v1");
+        when(postcodeLookupConfiguration.getAccessKey()).thenReturn("dummy");
+
+
+        when(restTemplate.exchange(
+            ArgumentMatchers.anyString(),
+            ArgumentMatchers.any(HttpMethod.class),
+            ArgumentMatchers.<HttpEntity<PostcodeResponse>>any(),
+            ArgumentMatchers.<Class<String>>any()))
+            .thenReturn(responseEntity);
+        when(objectMapper.readValue(PostcodeResponse.builder().results(resultLst).build().toString(), PostcodeResponse.class)).thenReturn(PostcodeResponse.builder().results(resultLst).build());
+
+        assertThat(postcodeLookupService
+                       .isValidNationalPostCode("N8 8PE", ENGLAND_POSTCODE_COUNTRYCODE))
+            .isTrue();
     }
 }
