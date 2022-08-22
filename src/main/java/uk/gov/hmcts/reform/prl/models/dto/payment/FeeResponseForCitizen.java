5924b8043d6c703a0086fe2cdad86e1856e22754
Yogendra Upasani <yogendra.upasani@hmcts.net>
2022-08-22 23:42:46 +0100

    PRLC100-69 - Fetch Fee details to be displayed on Citizen Rebuild

---
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/payment/FeeResponseForCitizen.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.reform.prl.models.dto.payment;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder(toBuilder = true)
+public class FeeResponseForCitizen {
+    @JsonProperty(value = "feeAmountForC100Application")
+    private String amount;
+
+    @JsonProperty(value = "errorRetrievingResponse")
+    private String errorRetrievingResponse;
+}
