520b05616822a8407a9e0ab64d7d046584f14b3a
sahaSonali <sonali.saha@hmcts.net>
2022-08-22 18:41:52 +0100

    changes for updating cases in ccd for upload

--- src/main/java/uk/gov/hmcts/reform/prl/services/DgsService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/DgsService.java
@@ -12,6 +12,7 @@ import uk.gov.hmcts.reform.prl.mapper.AppObjectMapper;
 import uk.gov.hmcts.reform.prl.mapper.welshlang.WelshLangMapper;
 import uk.gov.hmcts.reform.prl.models.dto.GenerateDocumentRequest;
 import uk.gov.hmcts.reform.prl.models.dto.GeneratedDocumentInfo;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseDetails;
 import uk.gov.hmcts.reform.prl.models.dto.citizen.GenerateAndUploadDocumentRequest;
 
@@ -84,12 +85,17 @@ public class DgsService {
                                                          String templateName) throws Exception {
 
         Map<String, Object> tempCaseDetails = new HashMap<>();
-        Object documentDetails = null;
+        String documentDetails = null;
         if (generateAndUploadDocumentRequest.getValues() != null
             && generateAndUploadDocumentRequest.getValues().containsKey("freeTextStatements")) {
             documentDetails = generateAndUploadDocumentRequest.getValues().get("freeTextStatements");
         }
-        tempCaseDetails.put("caseDetails", AppObjectMapper.getObjectMapper().convertValue(documentDetails, Map.class));
+        String caseId = generateAndUploadDocumentRequest.getValues().get("caseId");
+        CaseDetails caseDetails = CaseDetails.builder().caseId(caseId).state("ISSUE")
+                                        .caseData(CaseData.builder().id(Long.valueOf(caseId)).build()).build();
+        tempCaseDetails.put("caseDetails", AppObjectMapper.getObjectMapper().convertValue(caseDetails, Map.class));
+
+
         GeneratedDocumentInfo generatedDocumentInfo = null;
         try {
             generatedDocumentInfo =
@@ -98,7 +104,7 @@ public class DgsService {
                 );
 
         } catch (Exception ex) {
-            log.error("Error generating and storing document for case {}", generateAndUploadDocumentRequest.getValues().get("caseId"));
+            log.error("Error generating and storing document for case {}", caseId);
             throw new DocumentGenerationException(ex.getMessage(), ex);
         }
         return generatedDocumentInfo;
