48aa87b923f4d8392fa4c7e20681125007b6bf60
Adetola Opanuga <2109031@cognizant.com>
2022-08-22 13:38:52 +0100

    Added unit test for successful run with correct postcode and nation

--- src/test/java/uk/gov/hmcts/reform/prl/services/cafcass/postcode/PostcodeLookupServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/cafcass/postcode/PostcodeLookupServiceTest.java
@@ -9,8 +9,6 @@ import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.internal.matchers.Any;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
@@ -46,9 +44,6 @@ public class PostcodeLookupServiceTest {
 
     @Mock
     PostcodeLookupConfiguration postcodeLookupConfiguration;
-
-    @Mock
-    ObjectMapper objectMapper;
 
     @Before
     public void setup() {
@@ -176,45 +171,5 @@ public class PostcodeLookupServiceTest {
         assertThrows(
                 RuntimeException.class, () -> postcodeLookupService
                         .isValidNationalPostCode("IG11 7YL", ENGLAND_POSTCODE_NATIONALCODE));
-    }
-
-    @Test
-    public void shouldReturnTrueWhenStatusIsOK() throws JsonProcessingException {
-        AddressDetails address = AddressDetails.builder()
-            .address("59 Middle Lane")
-            .postcode("N8 8PE")
-            .status(HttpStatus.OK.toString())
-            .countryCode("E")
-            .build();
-        PostcodeResult result = PostcodeResult.builder().dpa(address).build();
-        List<PostcodeResult> resultLst = new ArrayList<>();
-        resultLst.add(result);
-//ObjectA myobjectA = new ObjectA();
-//        //define the entity you want the exchange to return
-//        ResponseEntity<List<ObjectA>> myEntity = new ResponseEntity<List<ObjectA>>(HttpStatus.ACCEPTED);
-//        Mockito.when(restTemplate.exchange(
-//            Matchers.eq("/objects/get-objectA"),
-//            Matchers.eq(HttpMethod.POST),
-//            Matchers.<HttpEntity<List<ObjectA>>>any(),
-//            Matchers.<ParameterizedTypeReference<List<ObjectA>>>any())
-//        ).thenReturn(myEntity);
-
-        ResponseEntity<String> responseEntity =
-            new ResponseEntity<String>(PostcodeResponse.builder().results(resultLst).build().toString(), HttpStatus.OK);
-        when(postcodeLookupConfiguration.getUrl()).thenReturn("https://api.os.uk/search/places/v1");
-        when(postcodeLookupConfiguration.getAccessKey()).thenReturn("dummy");
-
-
-        when(restTemplate.exchange(
-            ArgumentMatchers.anyString(),
-            ArgumentMatchers.any(HttpMethod.class),
-            ArgumentMatchers.<HttpEntity<PostcodeResponse>>any(),
-            ArgumentMatchers.<Class<String>>any()))
-            .thenReturn(responseEntity);
-        when(objectMapper.readValue(PostcodeResponse.builder().results(resultLst).build().toString(), PostcodeResponse.class)).thenReturn(PostcodeResponse.builder().results(resultLst).build());
-
-        assertThat(postcodeLookupService
-                       .isValidNationalPostCode("N8 8PE", ENGLAND_POSTCODE_COUNTRYCODE))
-            .isTrue();
     }
 }
