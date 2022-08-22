a4c25eec20ffa8fcc91f36ee0431c00880ef6b31
Mohammed Murshad Haque <murshad.haque@HMCTS.NET>
2022-08-22 11:54:48 +0100

    ISDB-351: JUnit test

--- src/main/java/uk/gov/hmcts/reform/prl/models/cafcass/AddressDetails.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/cafcass/AddressDetails.java
@@ -1,12 +1,19 @@
 package uk.gov.hmcts.reform.prl.models.cafcass;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
+import lombok.ToString;
 
+import java.io.Serializable;
+
+
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @Builder
-public class AddressDetails {
+public class AddressDetails implements Serializable {
     @JsonProperty("ADDRESS")
     private String address;
 
