23a6b50d573f489847ff2661b0f8c9a35b594d5c
parvezhmcts <parvez.qureshi@hmcts.net>
2022-08-22 12:07:27 +0100

    ISDB-349
    - Integration with CCD search case API

---
+++ src/main/java/uk/gov/hmcts/reform/prl/services/cafcass/CafcassCcdDataStoreService.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.reform.prl.services.cafcass;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
+import uk.gov.hmcts.reform.ccd.client.model.SearchResult;
+import uk.gov.hmcts.reform.prl.constants.PrlAppsConstants;
+
+@Service
+public class CafcassCcdDataStoreService {
+
+    @Autowired
+    private CoreCaseDataApi coreCaseDataApi;
+
+    /**
+     *  This method will call CCD searchCases API
+     *  and return the result.
+     *
+     * @param authorisation Authorisation header
+     * @param searchString json input for search
+     * @param serviceAuthorisation
+     * @return SearchResult object
+     */
+    public SearchResult searchCases(String authorisation, String searchString, String serviceAuthorisation) {
+
+        return coreCaseDataApi.searchCases(authorisation, serviceAuthorisation, PrlAppsConstants.CASE_TYPE,
+                                           searchString);
+
+    }
+}
