d6fc5ffa4f952c27355802055eb8f27d14367f07
Yogendra Upasani <yogendra.upasani@hmcts.net>
2022-08-23 18:59:47 +0100

    PRLC100-69 Citizen Rebuild - HWF Changes

---
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/FeesAndPaymentCitizenController.java
@@ -0,0 +1,61 @@
+package uk.gov.hmcts.reform.prl.controllers.citizen;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.prl.models.FeeResponse;
+import uk.gov.hmcts.reform.prl.models.FeeType;
+import uk.gov.hmcts.reform.prl.models.dto.payment.FeeResponseForCitizen;
+import uk.gov.hmcts.reform.prl.services.AuthorisationService;
+import uk.gov.hmcts.reform.prl.services.FeeService;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class FeesAndPaymentCitizenController {
+
+
+    private final String serviceAuth = "ServiceAuthorization";
+
+    @Autowired
+    private AuthorisationService authorisationService;
+
+    @Autowired
+    private FeeService feeService;
+
+    @GetMapping(path = "/getC100ApplicationFees", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
+    @Operation(description = "Frontend to fetch the Fees data")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Fee amount returned"),
+        @ApiResponse(responseCode = "400", description = "Bad Request")})
+    public FeeResponseForCitizen fetchFeesAmount(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation,
+        @RequestHeader(serviceAuth) String serviceAuthorization
+    ) {
+        FeeResponse feeResponse = null;
+        try {
+            if (Boolean.TRUE.equals(authorisationService.authoriseUser(authorisation)) && Boolean.TRUE.equals(
+                authorisationService.authoriseService(serviceAuthorization))) {
+                feeResponse = feeService.fetchFeeDetails(FeeType.C100_SUBMISSION_FEE);
+            } else {
+                throw (new RuntimeException("Invalid Client"));
+            }
+        } catch (Exception e) {
+            return FeeResponseForCitizen.builder()
+                .errorRetrievingResponse(e.getMessage())
+                .build();
+        }
+        return FeeResponseForCitizen.builder()
+            .amount(feeResponse.getAmount().toString()).build();
+    }
+}
