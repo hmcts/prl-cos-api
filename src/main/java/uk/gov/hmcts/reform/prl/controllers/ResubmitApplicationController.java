ffa093667358e4ba143ac67fd6d33b6922bf2356
Praneeth G <praneethkumar.gadhe@hmcts.net>
2022-08-30 11:40:21 +0100

    updated new field for submitted case

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ResubmitApplicationController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ResubmitApplicationController.java
@@ -44,6 +44,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.CASE_DATE_AND_TIME_SUBMITTED_FIELD;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.COURT_ID_FIELD;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.COURT_NAME_FIELD;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.DATE_AND_TIME_SUBMITTED_FIELD;
@@ -127,6 +128,7 @@ public class ResubmitApplicationController {
                 caseDataUpdated.put(DATE_SUBMITTED_FIELD, caseData.getDateSubmitted());
                 caseDataUpdated.put(DATE_AND_TIME_SUBMITTED_FIELD,
                                     DateTimeFormatter.ofPattern("d MMM yyyy, hh:mm:ssa", Locale.UK).format(zonedDateTime).toUpperCase());
+                caseDataUpdated.put(CASE_DATE_AND_TIME_SUBMITTED_FIELD, DateTimeFormatter.ISO_OFFSET_DATE_TIME.format(zonedDateTime));
                 caseWorkerEmailService.sendEmail(caseDetails);
                 solicitorEmailService.sendEmail(caseDetails);
             }
@@ -183,6 +185,7 @@ public class ResubmitApplicationController {
                 DATE_AND_TIME_SUBMITTED_FIELD,
                 DateTimeFormatter.ISO_LOCAL_DATE.format(zonedDateTime)
             );
+            caseDataUpdated.put(CASE_DATE_AND_TIME_SUBMITTED_FIELD, DateTimeFormatter.ISO_OFFSET_DATE_TIME.format(zonedDateTime));
         }
         if (previousStates.isPresent() && State.CASE_ISSUE.getValue().equalsIgnoreCase(previousStates.get())) {
             caseData = caseData.toBuilder().state(State. CASE_ISSUE).build();
