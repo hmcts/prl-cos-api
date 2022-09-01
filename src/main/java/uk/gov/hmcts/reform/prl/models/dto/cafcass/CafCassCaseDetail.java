04a52eba8ab110a2930576a047d914554d785266
Mohammed Murshad Haque <murshad.haque@HMCTS.NET>
2022-09-01 17:45:53 +0100

    ISDB-350: Removed unused import

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/cafcass/CafCassCaseDetail.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/cafcass/CafCassCaseDetail.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.prl.models.dto.cafcass;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
