18d93a89e3ab4c6a53bab96dc3fe108a666563a2
Rahul Sanjava <rahul.sanjava@HMCTS.NET>
2022-09-02 17:53:54 +0100

    fix functional test case failures

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseControllerFunctionalTest.java
@@ -3,7 +3,7 @@ package uk.gov.hmcts.reform.prl.controllers.citizen;
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import org.apache.commons.lang3.StringUtils;
-import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -34,25 +34,25 @@ public class CaseControllerFunctionalTest {
 
     private final RequestSpecification request = RestAssured.given().relaxedHTTPSValidation().baseUri(targetInstance);
 
-    @Ignore
+    @Test
     public void createCaseInCcd() throws Exception {
         String requestBody = ResourceLoader.loadJson(CASE_DATA_INPUT);
         request
-                .header("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJraWQiOiIxZXIwV1J3"
-                        + "Z0lPVEFGb2pFNHJDL2ZiZUt1M0k9IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiJyYWh1bC5zYW5qY"
-                        + "XZhQGhtY3RzLm5ldCIsImN0cyI6Ik9BVVRIMl9TVEFURUxFU1NfR1JBTlQiLCJhdXRoX2xldmVsIjow"
-                        + "LCJhdWRpdFRyYWNraW5nSWQiOiI5MTlmOTdkOS0zZDgyLTQzOGMtOGI1YS1hN2ViNjcwYjFiYWQtMTc2N"
-                        + "DA0NjcwIiwiaXNzIjoiaHR0cHM6Ly9mb3JnZXJvY2stYW0uc2VydmljZS5jb3JlLWNvbXB1dGUtaWRhbS1h"
-                        + "YXQyLmludGVybmFsOjg0NDMvb3BlbmFtL29hdXRoMi9yZWFsbXMvcm9vdC9yZWFsbXMvaG1jdHMiLCJ0b2tlb"
-                        + "k5hbWUiOiJhY2Nlc3NfdG9rZW4iLCJ0b2tlbl90eXBlIjoiQmVhcmVyIiwiYXV0aEdyYW50SWQiOiJ1ZnFvMTVXe"
-                        + "VpvZmkwbXUwdFhPUEtTanRkS0UiLCJhdWQiOiJjbWNfY2l0aXplbiIsIm5iZiI6MTY1ODg0ODM0MSwiZ3JhbnRfdHl"
-                        + "wZSI6ImF1dGhvcml6YXRpb25fY29kZSIsInNjb3BlIjpbIm9wZW5pZCIsInByb2ZpbGUiLCJyb2xlcyJdLCJhdXRoX3"
-                        + "RpbWUiOjE2NTg4NDgzNDEsInJlYWxtIjoiL2htY3RzIiwiZXhwIjoxNjU4ODc3MTQxLCJpYXQiOjE2NTg4NDgzNDEsIm"
-                        + "V4cGlyZXNfaW4iOjI4ODAwLCJqdGkiOiJZT215NHQ3NWhCXzlpaC12Vkg5b3ZkLXhaUEEifQ.xQL_vGcoxUMPy88nzm"
-                        + "piY5Hbjp7QmHXSLt-Oxi-yFbQ-d3rUw7MlQtp8NrCetr-OgipgJguL7Sx_E1aiOcOEEjuU_axvS8eVdb143gnwO_vy7e"
-                        + "MBfewrT9PDR7ehU_7gVzCqLlTegPp4SMaW6ffANvQXaVBUOrABC8xFpE_RD1kcyo-nOslKH-MEW5rnXhQZjeRhCA2CUwfj"
-                        + "DP5Qkc7oT5ZO3wIq2EngnDPl9WxhJiKltzavVA64AcAsdy5zdjPRJ-kMPvpZtkC1oUonXslddxeHSQQHaQYxlmiUr1M3f_DL"
-                        + "6c4JC8DjQGWfitWXcIXBGG1kHRJuLOgUKz0NODzF4w")
+                .header("Authorization", "eyJ0eXAiOiJKV1QiLCJraWQiOiIxZXIwV1J3Z0lPVEFGb2p"
+                        + "FNHJDL2ZiZUt1M0k9IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiJyYWh1bC5zYW5qYXZhQGhtY3RzLm5ldCIsIm"
+                        + "N0cyI6Ik9BVVRIMl9TVEFURUxFU1NfR1JBTlQiLCJhdXRoX2xldmVsIjowLCJhdWRpdFRyYWNraW5nSWQiOiIyN"
+                        + "GJmODNhOS02MDVkLTQzZGYtYjY2YS1hMWY3NTY4ODUwNjAtMzg5Njc2MTQiLCJpc3MiOiJodHRwczovL2Zvcmdl"
+                        + "cm9jay1hbS5zZXJ2aWNlLmNvcmUtY29tcHV0ZS1pZGFtLWFhdDIuaW50ZXJuYWw6ODQ0My9vcGVuYW0vb2F1dGg"
+                        + "yL3JlYWxtcy9yb290L3JlYWxtcy9obWN0cyIsInRva2VuTmFtZSI6ImFjY2Vzc190b2tlbiIsInRva2VuX3R5cGUi"
+                        + "OiJCZWFyZXIiLCJhdXRoR3JhbnRJZCI6IjgxY1VhaW9PbVhQQm5MeWgyR2xCY1BTX1dhQSIsImF1ZCI6ImNtY19ja"
+                        + "XRpemVuIiwibmJmIjoxNjYyMTM3NDU1LCJncmFudF90eXBlIjoiYXV0aG9yaXphdGlvbl9jb2RlIiwic2NvcGUiOls"
+                        + "ib3BlbmlkIiwicHJvZmlsZSIsInJvbGVzIl0sImF1dGhfdGltZSI6MTY2MjEzNzQ1NSwicmVhbG0iOiIvaG1jdHMiL"
+                        + "CJleHAiOjE2NjIxNjYyNTUsImlhdCI6MTY2MjEzNzQ1NSwiZXhwaXJlc19pbiI6Mjg4MDAsImp0aSI6IkRqbFlJX01"
+                        + "vSURXVGxGYVhYZnNKcXJKa05wYyJ9.x9nmsqV5Np2fYXRfKV1h9hvB303qzFyT8d9Tx6RGYMo_G7wlsEo7YMrec3W8n"
+                        + "rOR7ksvf4AoTJxSCYXGMQ1Ezi7-SX5Vct9L6rGIAOeH595SrM4AkponlRP5jWD43DP76z62GpfmBcm_0tHDyWDiQ23"
+                        + "ZExl1SKJ9OUkDcunSV0jh_BV2DWyWU-SxN8L8cy5UBa5CnEJ4_tzuI4epAJq7QnV35dNyNiq-fseCMAH4GOQAzuoFL"
+                        + "18nBprrmqREK3auzQLAYXqQRdaOf33663oABZg7Hxi29AJ2SBtJbMHpqGhhk0WQ7Kns3djASt0UJUL2VWv6jbllGv"
+                        + "a9y3wgL-OXbg")
                 .header("serviceAuthorization", serviceAuthenticationGenerator.generate())
                 .body(requestBody)
                 .when()
