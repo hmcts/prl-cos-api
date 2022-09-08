46b9d53b8839530141b02aefc8718ba8000e3e37
Yogendra Upasani <yogendra.upasani@hmcts.net>
2022-09-08 19:17:09 +0100

    Added create case feature

--- src/main/java/uk/gov/hmcts/reform/prl/constants/PrlAppsConstants.java
+++ src/main/java/uk/gov/hmcts/reform/prl/constants/PrlAppsConstants.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PrlAppsConstants {
+    public static final String CITIZEN_PRL_CREATE_EVENT = "solicitorCreate";
     public static final String JURISDICTION = "PRIVATELAW";
     public static final String CASE_TYPE = "PRLAPPS";
 
