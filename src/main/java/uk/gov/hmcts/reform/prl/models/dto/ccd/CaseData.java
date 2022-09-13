d2d34eefd96fb01ca7b07d25930ca6ff6f0c53e6
channa-patil <channabasappagouda.patil@hmcts.net>
2022-09-13 17:14:54 +0100

    C100 rebuild - added new fields for c100 rebuild features

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
@@ -647,4 +647,8 @@ public class CaseData implements MappableObject {
 
     private String citizenUploadedStatement;
 
+    // C100 Rebuild
+    private String c100RebuildConfidentiality;
+    private String c100RebuildInternationalElements;
+
 }
