c744d19886c1e852c62ea46f036e0755e1ec1394
praveenrach <praveen.rachapally@HMCTS.NET>
2022-09-14 19:18:47 +0100

    updated the miam import in Response

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/Response.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/Response.java
@@ -4,14 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.prl.enums.YesOrNo;
-import uk.gov.hmcts.reform.prl.models.complextypes.applicationtab.Miam;
 import uk.gov.hmcts.reform.prl.models.complextypes.citizen.common.CitizenDetails;
 import uk.gov.hmcts.reform.prl.models.complextypes.citizen.common.CitizenFlags;
 import uk.gov.hmcts.reform.prl.models.complextypes.citizen.response.confidentiality.KeepDetailsPrivate;
 import uk.gov.hmcts.reform.prl.models.complextypes.citizen.response.consent.Consent;
 import uk.gov.hmcts.reform.prl.models.complextypes.citizen.response.internationalelements.CitizenInternationalElements;
+import uk.gov.hmcts.reform.prl.models.complextypes.citizen.response.miam.Miam;
 import uk.gov.hmcts.reform.prl.models.complextypes.citizen.response.proceedings.CurrentOrPreviousProceedings;
-
 
 @Data
 @Builder(toBuilder = true)
