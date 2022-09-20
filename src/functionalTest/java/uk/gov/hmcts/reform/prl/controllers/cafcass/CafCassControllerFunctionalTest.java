7c0c212b9fac84e4a984c181641a421ebe860ce5
Adetola Opanuga <2109031@cognizant.com>
2022-09-21 00:26:34 +0100

    Completed pact test

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/cafcass/CafCassControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/cafcass/CafCassControllerFunctionalTest.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.prl.controllers;
+package uk.gov.hmcts.reform.prl.controllers.cafcass;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -18,20 +18,24 @@ import org.springframework.web.context.WebApplicationContext;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 import uk.gov.hmcts.reform.ccd.client.model.SearchResult;
 import uk.gov.hmcts.reform.prl.mapper.CcdObjectMapper;
+import uk.gov.hmcts.reform.prl.models.dto.cafcass.CafCassResponse;
 import uk.gov.hmcts.reform.prl.services.cafcass.PostcodeLookupService;
 import uk.gov.hmcts.reform.prl.utils.TestResourceUtil;
 
-import java.util.Map;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.C100_CASE_TYPE;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.CASE_TYPE;
+import static uk.gov.hmcts.reform.prl.utils.TestConstants.AUTHORISATION_HEADER;
+import static uk.gov.hmcts.reform.prl.utils.TestConstants.CREATE_SERVICE_RESPONSE;
+import static uk.gov.hmcts.reform.prl.utils.TestConstants.SEARCH_CASE_ENDPOINT;
+import static uk.gov.hmcts.reform.prl.utils.TestConstants.SERVICE_AUTHORISATION_HEADER;
+import static uk.gov.hmcts.reform.prl.utils.TestConstants.TEST_AUTH_TOKEN;
+import static uk.gov.hmcts.reform.prl.utils.TestConstants.TEST_SERVICE_AUTH_TOKEN;
 
 @Slf4j
 @SpringBootTest
@@ -50,12 +54,6 @@ public class CafCassControllerFunctionalTest {
     @MockBean
     private PostcodeLookupService postcodeLookupService;
 
-    private static final String CREATE_SERVICE_RESPONSE = "classpath:response/cafcass-search-response.json";
-    private static final String SEARCH_CASE_URL = "/searchCases";
-    private static final String AUTH_TEST_TOKEN = "authorisation";
-    private static final String SERVICE_AUTH_TEST_TOKEN = "serviceauthorisation";
-    private static final String CASE_TYPE_OF_APPLICATION_FIELD = "caseTypeOfApplication";
-
     @Before
     public void setUp() {
         this.mockMvc = webAppContextSetup(webApplicationContext).build();
@@ -66,15 +64,15 @@ public class CafCassControllerFunctionalTest {
         String cafcassResponseStr = TestResourceUtil.readFileFrom(CREATE_SERVICE_RESPONSE);
         ObjectMapper objectMapper = CcdObjectMapper.getObjectMapper();
 
-        SearchResult expectedCafcassResponse = objectMapper.readValue(cafcassResponseStr, SearchResult.class);
+        SearchResult expectedSearchResult = objectMapper.readValue(cafcassResponseStr, SearchResult.class);
 
         Mockito.when(postcodeLookupService.isValidNationalPostCode(anyString(), anyString())).thenReturn(true);
-        Mockito.when(coreCaseDataApi.searchCases(anyString(), anyString(), anyString(), anyString())).thenReturn(expectedCafcassResponse);
+        Mockito.when(coreCaseDataApi.searchCases(anyString(), anyString(), anyString(), anyString())).thenReturn(expectedSearchResult);
 
-        MvcResult mvcResult = mockMvc.perform(get(SEARCH_CASE_URL)
+        MvcResult mvcResult = mockMvc.perform(get(SEARCH_CASE_ENDPOINT)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .header("authorisation", AUTH_TEST_TOKEN)
-                        .header("serviceAuthorisation", SERVICE_AUTH_TEST_TOKEN)
+                        .header(AUTHORISATION_HEADER, TEST_AUTH_TOKEN)
+                        .header(SERVICE_AUTHORISATION_HEADER, TEST_SERVICE_AUTH_TOKEN)
                         .queryParam("start_date", "2022-08-22T10:44:43.49")
                         .queryParam("end_date", "2022-08-26T11:00:54.055")
                         .accept(MediaType.APPLICATION_JSON))
@@ -83,37 +81,24 @@ public class CafCassControllerFunctionalTest {
 
         String contentAsString = mvcResult.getResponse().getContentAsString();
 
-        SearchResult actualCafcassResponse = objectMapper.readValue(contentAsString, SearchResult.class);
+        CafCassResponse actualCafcassResponse = objectMapper.readValue(contentAsString, CafCassResponse.class);
 
-        assertEquals(expectedCafcassResponse.getTotal(), actualCafcassResponse.getTotal());
-        assertEquals(expectedCafcassResponse.getCases().size(), actualCafcassResponse.getCases().size());
-        assertEquals(expectedCafcassResponse.getTotal(), actualCafcassResponse.getCases().size());
+        assertEquals(expectedSearchResult.getTotal(), actualCafcassResponse.getTotal());
+        assertEquals(expectedSearchResult.getCases().size(), actualCafcassResponse.getCases().size());
+        assertEquals(expectedSearchResult.getTotal(), actualCafcassResponse.getCases().size());
 
-        if (0 != expectedCafcassResponse.getTotal() || 0 != actualCafcassResponse.getTotal()) {
+        if (0 != actualCafcassResponse.getTotal()) {
             for (int responseCaseCnt = 0; responseCaseCnt < actualCafcassResponse.getTotal(); responseCaseCnt++) {
                 assertNotNull(actualCafcassResponse.getCases().get(responseCaseCnt).getState());
                 assertNotNull(actualCafcassResponse.getCases().get(responseCaseCnt).getCaseTypeId());
                 assertNotNull(actualCafcassResponse.getCases()
-                        .get(responseCaseCnt).getData().get(CASE_TYPE_OF_APPLICATION_FIELD));
+                        .get(responseCaseCnt).getCaseTypeOfApplication());
 
                 assertEquals(actualCafcassResponse.getCases().get(responseCaseCnt).getCaseTypeId(),
-                        expectedCafcassResponse.getCases().get(responseCaseCnt).getCaseTypeId());
+                        expectedSearchResult.getCases().get(responseCaseCnt).getCaseTypeId());
                 assertEquals(CASE_TYPE, actualCafcassResponse.getCases().get(responseCaseCnt).getCaseTypeId());
-                assertTrue(isValidCaseType(actualCafcassResponse.getCases().get(responseCaseCnt).getData()));
+                assertEquals(C100_CASE_TYPE, actualCafcassResponse.getCases().get(responseCaseCnt).getCaseTypeOfApplication());
             }
         }
-    }
-
-    private boolean isValidCaseType(Map<String, Object> inputDataMap) {
-        for (Map.Entry<String, Object> detailedData : inputDataMap.entrySet()) {
-            if (detailedData.getKey().equalsIgnoreCase(CASE_TYPE_OF_APPLICATION_FIELD)) {
-                if (detailedData.getValue().toString().equalsIgnoreCase(C100_CASE_TYPE)) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }
-        return false;
     }
 }
---
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/utils/TestConstants.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.reform.prl.utils;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class TestConstants {
+
+    public static final String CREATE_SERVICE_RESPONSE = "classpath:response/cafcass-search-response.json";
+    public static final String SEARCH_CASE_ENDPOINT = "/searchCases";
+    public static final String TEST_AUTH_TOKEN = "authorisation";
+    public static final String TEST_SERVICE_AUTH_TOKEN = "serviceauthorisation";
+    public static final String AUTHORISATION_HEADER = "authorisation";
+    public static final String SERVICE_AUTHORISATION_HEADER = "serviceauthorisation";
+}
