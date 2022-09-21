208e9936c040f47bb068260d7a2a399015c3cf95
swaroopa86 <pendyalaswaroopa86@gmail.com>
2022-09-21 10:35:14 +0100

    Revert "custom exceptions are added for courtnav withoutnotice order section"
    This reverts commit 6c764ec8e3a81831d6100af7ebb4164601c357da.

--- src/main/java/uk/gov/hmcts/reform/prl/exception/CourtNavDataValidationException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.reform.prl.exception;
-
-public class CourtNavDataValidationException extends RuntimeException {
-
-    public CourtNavDataValidationException(String message) {
-        super(message);
-    }
-}
