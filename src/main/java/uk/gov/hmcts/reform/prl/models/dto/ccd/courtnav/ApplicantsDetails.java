5d55b50406ebaf462a96d69772601dcc7e821494
swaroopa86 <pendyalaswaroopa86@gmail.com>
2022-09-21 10:35:06 +0100

    Revert "Null checkcs and empty string checks are added for courtnav applicant details"
    This reverts commit 873c09202a36aa0067125f6e73018bf2a3924b16.

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/courtnav/ApplicantsDetails.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/courtnav/ApplicantsDetails.java
@@ -9,8 +9,6 @@ import uk.gov.hmcts.reform.prl.models.dto.ccd.courtnav.enums.ApplicantGenderEnum
 import uk.gov.hmcts.reform.prl.models.dto.ccd.courtnav.enums.PreferredContactEnum;
 
 import java.util.List;
-import javax.validation.Valid;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -21,7 +19,6 @@ import javax.validation.constraints.NotNull;
 @Getter
 @Setter
 @AllArgsConstructor
-@Valid
 public class ApplicantsDetails {
 
     @NotNull
@@ -41,14 +38,9 @@ public class ApplicantsDetails {
     private final boolean shareContactDetailsWithRespondent;
     @NotBlank
     private String applicantPhoneNumber;
-    @NotNull
-    @Email
     private String applicantEmailAddress;
-
-    @NotEmpty
     private String applicantContactInstructions;
     private CourtnavAddress applicantAddress;
-    @NotNull
     private List<PreferredContactEnum> applicantPreferredContact;
     private final String legalRepresentativeFirm;
     private final CourtnavAddress legalRepresentativeAddress;
