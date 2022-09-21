df661ecdb22b7d3deacd3e3c88c21d920428a06f
swaroopa86 <pendyalaswaroopa86@gmail.com>
2022-09-21 09:07:21 +0100

    Null checkcs and empty string checks are added for courtnav applicant details

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/courtnav/Situation.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/courtnav/Situation.java
@@ -9,6 +9,10 @@ import uk.gov.hmcts.reform.prl.enums.FL401OrderTypeEnum;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.courtnav.enums.WithoutNoticeReasonEnum;
 
 import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Builder(toBuilder = true)
@@ -20,15 +24,33 @@ public class Situation {
     /**
      * type of application.
      */
+
+    @Valid
+    @NotNull
+    @NotEmpty
     private final List<FL401OrderTypeEnum> ordersAppliedFor;
 
     /**
      * without notice order.
      */
+
+    @NotNull(message = "Orders applied without notice cannot be null")
+    @NotEmpty
     private final boolean ordersAppliedWithoutNotice;
+
+    @Valid
+    @NotNull
     private final List<WithoutNoticeReasonEnum> ordersAppliedWithoutNoticeReason;
+    @NotBlank
     private final String ordersAppliedWithoutNoticeReasonDetails;
+
+    @Valid
+    @NotNull
+    @NotEmpty
     private final boolean bailConditionsOnRespondent;
+
+    @Valid
+    @NotBlank
     private final CourtNavDate bailConditionsEndDate;
     private final String additionalDetailsForCourt;
 
