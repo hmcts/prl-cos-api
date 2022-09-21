0decc6b538d11f8e0031c82d84c1fd795b86267d
swaroopa86 <pendyalaswaroopa86@gmail.com>
2022-09-21 13:50:43 +0100

    @model attribute is chnaged to requestBody in uploadCitizendocument

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseDocumentController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseDocumentController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
