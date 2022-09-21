5cf5ad724158ba7cb28dbb105543eec12da31d63
swaroopa86 <pendyalaswaroopa86@gmail.com>
2022-09-21 12:19:08 +0100

    Revert "validation for OrdersAppliedWithoutNoticeReason is added"
    This reverts commit ef7a846375d6b9b924e5c48946cd6583e303ffe4.

--- src/main/java/uk/gov/hmcts/reform/prl/exception/CourtNavCaseDataValidationException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.reform.prl.exception;
-
-public class CourtNavCaseDataValidationException extends RuntimeException {
-
-    public CourtNavCaseDataValidationException(String message) {
-        super(message);
-    }
-}
