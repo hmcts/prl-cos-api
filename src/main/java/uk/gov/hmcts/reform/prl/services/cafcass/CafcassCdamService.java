b7647b1ea572473bdf78cc65e3e2df91ff80ba5e
parvezhmcts <parvez.qureshi@hmcts.net>
2022-09-21 17:20:49 +0100

    ISDB-380
    - Integration with CDAM get document API

---
+++ src/main/java/uk/gov/hmcts/reform/prl/services/cafcass/CafcassCdamService.java
@@ -0,0 +1,31 @@
+package uk.gov.hmcts.reform.prl.services.cafcass;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.document.am.feign.CaseDocumentClient;
+
+import java.util.UUID;
+
+@Service
+public class CafcassCdamService {
+
+    @Autowired
+    CaseDocumentClient caseDocumentClient;
+
+
+    /**
+     * This method will call CDAM getDocument API
+     * and return the result.
+     * @param authToken
+     * @param s2sToken
+     * @param documentId
+     * @return
+     */
+    public ResponseEntity<Resource> getDocument(String authToken, String s2sToken, UUID documentId) {
+
+        return caseDocumentClient.getDocumentBinary(authToken,s2sToken, documentId);
+    }
+
+}
