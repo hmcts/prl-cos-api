99c8cc1e5fbcc301fbaed4adf4daf2f47d263f43
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2022-09-21 15:12:23 +0100

    Checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/prl/models/OrderDetails.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/OrderDetails.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import lombok.extern.slf4j.XSlf4j;
 import uk.gov.hmcts.reform.prl.models.documents.Document;
 
 import java.time.LocalDateTime;
