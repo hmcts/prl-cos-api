30a1cc76e04cef611fb0f9eec32a9ca1f2f77176
Adetola Opanuga <2109031@cognizant.com>
2022-09-23 00:58:01 +0100

    Remove unwanted class

--- src/main/java/uk/gov/hmcts/reform/prl/exception/cafcass/CafcassDocumentDownloadException.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.prl.exception.cafcass;
-
-public class CafcassDocumentDownloadException extends RuntimeException {
-    private static final long serialVersionUID = 1242994124480111078L;
-
-    public CafcassDocumentDownloadException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
