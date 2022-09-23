7ed41576eeb09d72eb5409537f82a27efe09e407
hmcts-kodatala <purushotham.kodatala@hmcts.net>
2022-09-23 18:54:32 +0100

    updted for log

--- src/main/java/uk/gov/hmcts/reform/prl/services/AuthorisationService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/AuthorisationService.java
@@ -25,10 +25,13 @@ public class AuthorisationService {
 
     public Boolean authoriseService(String serviceAuthHeader) {
         String callingService;
+        log.info(" s2sAuthorisedServices:::"+s2sAuthorisedServices);
         try {
             callingService = serviceAuthorisationApi.getServiceName(serviceAuthHeader);
             if (callingService != null && Arrays.asList(s2sAuthorisedServices.split(","))
                 .contains(callingService)) {
+                log.info(" callingService::::"+callingService);
+
                 return true;
             }
         } catch (Exception ex) {
@@ -42,6 +45,7 @@ public class AuthorisationService {
         try {
             UserInfo userInfo = idamClient.getUserInfo(authorisation);
             if (null != userInfo) {
+                log.info(" authoriseUser::::"+userInfo.getName());
                 return true;
             }
         } catch (Exception ex) {
