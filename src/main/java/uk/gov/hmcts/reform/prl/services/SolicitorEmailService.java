f876eb57aeb02b0011a092983630cb90d86741c4
Khurshed Khan <khurshed.khan@hmcts.net>
2022-09-26 11:30:02 +0100

    PRL-2187:Fixed review comment

--- src/main/java/uk/gov/hmcts/reform/prl/services/SolicitorEmailService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/SolicitorEmailService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 import uk.gov.hmcts.reform.prl.config.EmailTemplatesConfig;
+import uk.gov.hmcts.reform.prl.constants.PrlAppsConstants;
 import uk.gov.hmcts.reform.prl.enums.LanguagePreference;
 import uk.gov.hmcts.reform.prl.models.Element;
 import uk.gov.hmcts.reform.prl.models.complextypes.PartyDetails;
@@ -84,7 +85,8 @@ public class SolicitorEmailService {
 
     public void sendEmail(CaseDetails caseDetails) {
         log.info("Sending the email to solicitor for caseId {}", caseDetails.getId());
-        String applicantSolicitorEmailAddress = caseDetails.getData().get("applicantSolicitorEmailAddress").toString();
+        String applicantSolicitorEmailAddress = caseDetails.getData()
+            .get(PrlAppsConstants.APPLICANT_SOLICITOR_EMAIL_ADDRESS).toString();
         emailService.send(
             applicantSolicitorEmailAddress,
             EmailTemplateNames.SOLICITOR,
@@ -96,7 +98,8 @@ public class SolicitorEmailService {
 
     public void sendReSubmitEmail(CaseDetails caseDetails) {
         log.info("Sending case resubmission email to solicitor for caseId {}", caseDetails.getId());
-        String applicantSolicitorEmailAddress = caseDetails.getData().get("applicantSolicitorEmailAddress").toString();
+        String applicantSolicitorEmailAddress = caseDetails.getData()
+            .get(PrlAppsConstants.APPLICANT_SOLICITOR_EMAIL_ADDRESS).toString();
         emailService.send(
             applicantSolicitorEmailAddress,
             EmailTemplateNames.SOLICITOR_RESUBMIT_EMAIL,
