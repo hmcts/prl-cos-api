ff9cc8d1a9c793d9a34df064f3bec0dd043390fa
swaroopa86 <pendyalaswaroopa86@gmail.com>
2022-09-27 20:12:28 +0100

    loggers are added for UploadDocumentService

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/citizen/UploadedDocumentRequest.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/citizen/UploadedDocumentRequest.java
@@ -1,6 +1,9 @@
 package uk.gov.hmcts.reform.prl.models.dto.citizen;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,12 +11,21 @@ import java.util.List;
 
 @AllArgsConstructor
 @Getter
+@Data
+@Builder
 public class UploadedDocumentRequest {
+    @JsonProperty("caseId")
     private final String caseId;
+    @JsonProperty("parentDocumentType")
     private final String parentDocumentType;
+    @JsonProperty("documentType")
     private final String documentType;
+    @JsonProperty("partyId")
     private final String partyId;
+    @JsonProperty("partyName")
     private final String partyName;
+    @JsonProperty("isApplicant")
     private final String isApplicant;
+    @JsonProperty("files")
     private List<MultipartFile> files;
 }
