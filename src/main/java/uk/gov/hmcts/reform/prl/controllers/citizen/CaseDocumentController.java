3c17bf0dd5aa778dac4dad31ad777ddf7a5bbfdc
swaroopa86 <pendyalaswaroopa86@gmail.com>
2022-09-27 19:08:21 +0100

    upload document request is changed as @modelattribute

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseDocumentController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseDocumentController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -158,7 +159,7 @@ public class CaseDocumentController {
     })
     public ResponseEntity uploadCitizenStatementDocument(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation,
                                                          @RequestHeader("serviceAuthorization") String s2sToken,
-                                                         @RequestBody UploadedDocumentRequest uploadedDocumentRequest) {
+                                                         @ModelAttribute UploadedDocumentRequest uploadedDocumentRequest) {
 
         log.info("Uploaded doc request: {}", uploadedDocumentRequest);
         String caseId = uploadedDocumentRequest.getCaseId();
