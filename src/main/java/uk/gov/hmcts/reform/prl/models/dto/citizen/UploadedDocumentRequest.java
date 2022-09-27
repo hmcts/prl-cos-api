614c4bfb7ae2477df448b4ceca285ba77463c698
swaroopa86 <pendyalaswaroopa86@gmail.com>
2022-09-27 18:11:03 +0100

    upload document request is changed

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/citizen/UploadedDocumentRequest.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/citizen/UploadedDocumentRequest.java
@@ -1,25 +1,19 @@
 package uk.gov.hmcts.reform.prl.models.dto.citizen;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Map;
+import java.util.List;
 
-@Data
-@NoArgsConstructor
 @AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonIgnoreProperties(ignoreUnknown = true)
-@Schema(description = "Request body model for Document Storing Request")
-@Builder(toBuilder = true)
+@Getter
 public class UploadedDocumentRequest {
-
-    @JsonProperty("values")
-    private Map<String, Object> values;
+    private final String caseId;
+    private final String parentDocumentType;
+    private final String documentType;
+    private final String partyId;
+    private final String partyName;
+    private final String isApplicant;
+    private List<MultipartFile> files;
 }
