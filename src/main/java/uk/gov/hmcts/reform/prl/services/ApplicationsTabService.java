548d98767cb257a831930b39ed104a597b970c61
swaroopa86 <pendyalaswaroopa86@gmail.com>
2022-09-29 14:42:41 +0100

    CVE suppressions are added

--- src/main/java/uk/gov/hmcts/reform/prl/services/ApplicationsTabService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ApplicationsTabService.java
@@ -927,7 +927,7 @@ public class ApplicationsTabService implements TabService {
             .livingSituation(String.join(", ", livingSituationEnum))
             .isThereMortgageOnProperty(home.getIsThereMortgageOnProperty());
 
-        if (home.getMortgages() != null) {
+        if (home.getMortgages() != null && home.getMortgages().getMortgageNamedAfter() != null) {
             Mortgage mortgage = home.getMortgages();
 
             List<String> mortgageNameAft = mortgage.getMortgageNamedAfter().stream()
@@ -940,7 +940,7 @@ public class ApplicationsTabService implements TabService {
                 .mortgageLenderName(mortgage.getMortgageLenderName());
         }
         log.info("Landlord object infor::: {}",home.getLandlords());
-        if (home.getLandlords() != null) {
+        if (home.getLandlords() != null && home.getLandlords().getMortgageNamedAfterList() != null) {
             log.info("Entering inside if loop");
 
             Landlord landlord = home.getLandlords();
