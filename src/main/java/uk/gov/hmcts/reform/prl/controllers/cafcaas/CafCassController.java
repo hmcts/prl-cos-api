45a78c34c6b2cdf68a2ed4fb77e635d5b2b0fe73
“jegan-maharajan1” <maharajan.jegan1@hmcts.net>
2022-10-03 14:04:23 +0100

    CVE fix updated

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/cafcaas/CafCassController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/cafcaas/CafCassController.java
@@ -51,7 +51,7 @@ public class CafCassController extends AbstractCallbackController {
 
         if (Boolean.TRUE.equals(authorisationService.authoriseUser(authorisation)) && Boolean.TRUE.equals(
             authorisationService.authoriseService(serviceAuthorisation))) {
-            log.info("processing  request after authorization");
+            log.info("processing request after authorization");
             return ResponseEntity.ok(caseDataService.getCaseData(authorisation, serviceAuthorisation, startDate, endDate));
 
         } else {
