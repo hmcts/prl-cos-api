65fad3409a8d11026e71e79a62e9690e7344bd23
hmcts-kodatala <purushotham.kodatala@hmcts.net>
2022-10-05 17:43:56 +0100

    updted the test cases

--- src/main/java/uk/gov/hmcts/reform/prl/exception/cafcass/UnauthorizedException.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.prl.exception.cafcass;
-
-public class UnauthorizedException extends RuntimeException {
-    private static final long serialVersionUID = -948721106877408028L;
-
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
