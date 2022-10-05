c00d3c3539bd88e297683faf603ce554ff47ca39
hmcts-kodatala <purushotham.kodatala@hmcts.net>
2022-10-05 17:31:59 +0100

    updted the test cases

--- src/main/java/uk/gov/hmcts/reform/prl/exception/cafcass/exceptionhandlers/ApiError.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.prl.exception.cafcass.exceptionhandlers;
-
-public class ApiError {
-    public final String error;
-
-    public ApiError(String error) {
-        this.error = error;
-    }
-}
