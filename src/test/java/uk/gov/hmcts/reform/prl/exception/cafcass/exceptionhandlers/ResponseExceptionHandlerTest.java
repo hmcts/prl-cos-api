92fbf54883a88359127e48e810a9c578bed45452
hmcts-kodatala <purushotham.kodatala@hmcts.net>
2022-10-05 16:07:32 +0100

    updted the test cases

--- src/test/java/uk/gov/hmcts/reform/prl/exception/cafcass/exceptionhandlers/ResponseExceptionHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/exception/cafcass/exceptionhandlers/ResponseExceptionHandlerTest.java
@@ -1,46 +1,50 @@
 package uk.gov.hmcts.reform.prl.exception.cafcass.exceptionhandlers;
 
 import com.sun.jdi.InternalException;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.authorisation.exceptions.InvalidTokenException;
 import uk.gov.hmcts.reform.prl.exception.cafcass.ForbiddenException;
 import uk.gov.hmcts.reform.prl.exception.cafcass.UnauthorizedException;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-class ResponseExceptionHandlerTest {
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class ResponseExceptionHandlerTest {
 
     ResponseExceptionHandler responseExceptionHandler;
 
-    @BeforeEach
-    void setUp() {
-        responseExceptionHandler = new ResponseExceptionHandler();
-    }
-
     @Test
-    void handleInvalidTokenException() {
+    public  void handleInvalidTokenException() {
+        responseExceptionHandler = new ResponseExceptionHandler();
         assertNotNull(
             responseExceptionHandler.handleInvalidTokenException(
                 new InvalidTokenException("Test Exception")));
     }
 
     @Test
-    void handleUnAuthorizedException() {
+    public void handleUnAuthorizedException() {
+        responseExceptionHandler = new ResponseExceptionHandler();
+
         assertNotNull(
             responseExceptionHandler.handleUnAuthorizedException(
                 new UnauthorizedException("Test Exception")));
     }
 
     @Test
-    void handleForbiddenException() {
+    public  void handleForbiddenException() {
+        responseExceptionHandler = new ResponseExceptionHandler();
+
         assertNotNull(
             responseExceptionHandler.handleForbiddenException(
                 new ForbiddenException("Test Exception")));
     }
 
     @Test
-    void handleInternalException() {
+    public void handleInternalException() {
+        responseExceptionHandler = new ResponseExceptionHandler();
+
         assertNotNull(
             responseExceptionHandler.handleInternalException(
                 new InternalException("Test Exception")));
