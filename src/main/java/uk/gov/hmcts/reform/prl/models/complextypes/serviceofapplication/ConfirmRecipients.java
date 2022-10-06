b2433579ccc122f98e3457ff9209067b40687570
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-10-06 15:03:34 +0100

    added applicant solicitor respondents and respondent solicitors list to SoApplication

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/serviceofapplication/ConfirmRecipients.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/serviceofapplication/ConfirmRecipients.java
@@ -2,23 +2,23 @@ package uk.gov.hmcts.reform.prl.models.complextypes.serviceofapplication;
 
 import lombok.Builder;
 import lombok.Data;
-import uk.gov.hmcts.reform.prl.enums.manageorders.OrderRecipientsEnum;
 import uk.gov.hmcts.reform.prl.enums.manageorders.OtherEnum;
-import uk.gov.hmcts.reform.prl.enums.manageorders.OtherOrderRecipientsEnum;
 import uk.gov.hmcts.reform.prl.enums.serviceofapplication.CafcassServiceApplicationEnum;
 import uk.gov.hmcts.reform.prl.models.Element;
+import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicMultiSelectList;
 
 import java.util.List;
 
 @Data
 @Builder
 public class ConfirmRecipients {
-    private final List<OrderRecipientsEnum> applicantsList;
-    private final List<OtherOrderRecipientsEnum> otherPeopleList;
-
+    private final DynamicMultiSelectList applicantsList;
+    private final DynamicMultiSelectList respondentsList;
+    private final DynamicMultiSelectList applicantSolicitorList;
+    private final DynamicMultiSelectList respondentSolicitorList;
+    private final DynamicMultiSelectList otherPeopleList;
     private final List<CafcassServiceApplicationEnum> cafcassEmailOptionChecked;
     private final List<OtherEnum> otherEmailOptionChecked;
-
     private final List<Element<String>> cafcassEmailAddressList;
     private final List<Element<String>> otherEmailAddressList;
 }
