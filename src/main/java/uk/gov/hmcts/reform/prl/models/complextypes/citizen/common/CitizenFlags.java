1bc1263b05bafd488bbd7391cffcf4b30e5ff9ac
sahaSonali <sonali.saha@hmcts.net>
2022-10-06 14:27:00 +0100

    Set Flag for allDocumentsViewed

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/common/CitizenFlags.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/common/CitizenFlags.java
@@ -14,5 +14,5 @@ public class CitizenFlags {
 
     private final YesOrNo isApplicationViewed;
     private final YesOrNo isAllegationOfHarmViewed;
-    private final YesOrNo isAllDocumentsViewed;;
+    private final YesOrNo isAllDocumentsViewed;
 }
