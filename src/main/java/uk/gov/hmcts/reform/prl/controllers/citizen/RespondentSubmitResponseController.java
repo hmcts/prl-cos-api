883e1f9a747179912d25afc7433d8573e466ae5f
Khurshed Khan <khurshed.khan@hmcts.net>
2022-10-11 17:20:56 +0100

    added changes for controller

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/RespondentSubmitResponseController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/RespondentSubmitResponseController.java
@@ -2,12 +2,10 @@ package uk.gov.hmcts.reform.prl.controllers.citizen;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
 import uk.gov.hmcts.reform.prl.models.Element;
 import uk.gov.hmcts.reform.prl.models.complextypes.citizen.documents.UploadedDocuments;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
@@ -22,6 +20,8 @@ import javax.validation.constraints.NotNull;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static uk.gov.hmcts.reform.prl.utils.ElementUtils.element;
 
+@Slf4j
+@RestController
 public class RespondentSubmitResponseController {
 
     @Autowired
