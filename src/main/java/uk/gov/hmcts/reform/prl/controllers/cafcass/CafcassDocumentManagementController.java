c5579de53008a7179be958ab764f87aedc72346e
parvezhmcts <parvez.qureshi@hmcts.net>
2022-10-11 17:01:10 +0100

    - Exception handling for cafcass apis

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/cafcass/CafcassDocumentManagementController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/cafcass/CafcassDocumentManagementController.java
@@ -1,11 +1,11 @@
 package uk.gov.hmcts.reform.prl.controllers.cafcass;
 
+import feign.FeignException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +14,16 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import uk.gov.hmcts.reform.prl.exception.cafcass.exceptionhandlers.ApiError;
 import uk.gov.hmcts.reform.prl.services.AuthorisationService;
 import uk.gov.hmcts.reform.prl.services.cafcass.CafcassCdamService;
 
 import java.util.UUID;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.ResponseEntity.status;
 import static uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi.SERVICE_AUTHORIZATION;
 
 @Slf4j
@@ -40,16 +44,25 @@ public class CafcassDocumentManagementController {
             @ApiResponse(responseCode = "401", description = "Provided Authorization token is missing or invalid"),
             @ApiResponse(responseCode = "500", description = "Internal Server Error")
     })
-    public ResponseEntity<Resource> downloadDocument(@RequestHeader(AUTHORIZATION) String authorisation,
+    public ResponseEntity<?> downloadDocument(@RequestHeader(AUTHORIZATION) String authorisation,
                                                      @RequestHeader(SERVICE_AUTHORIZATION) String serviceAuthorisation,
                                                      @PathVariable UUID documentId) {
-        if (Boolean.TRUE.equals(authorisationService.authoriseUser(authorisation)) && Boolean.TRUE.equals(
-            authorisationService.authoriseService(serviceAuthorisation))) {
-            log.info("processing  request after authorization");
-            return cafcassCdamService.getDocument(authorisation, serviceAuthorisation, documentId);
+        try {
+            if (Boolean.TRUE.equals(authorisationService.authoriseUser(authorisation)) && Boolean.TRUE.equals(
+                authorisationService.authoriseService(serviceAuthorisation))) {
+                log.info("processing  request after authorization");
+                return cafcassCdamService.getDocument(authorisation, serviceAuthorisation, documentId);
 
-        } else {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+            } else {
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+            }
+        } catch (ResponseStatusException e) {
+            return status(FORBIDDEN).body(new ApiError(e.getMessage()));
+        } catch (
+            FeignException feignException) {
+            return status(feignException.status()).body(new ApiError(feignException.getMessage()));
+        } catch (Exception e) {
+            return status(INTERNAL_SERVER_ERROR).body(new ApiError(e.getMessage()));
         }
     }
 }
--- src/test/java/uk/gov/hmcts/reform/prl/controllers/cafcass/CafcassDocumentManagementControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/cafcass/CafcassDocumentManagementControllerTest.java
@@ -1,6 +1,10 @@
 package uk.gov.hmcts.reform.prl.controllers.cafcass;
 
+import feign.FeignException;
+import feign.Request;
+import feign.Response;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -9,9 +13,9 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.server.ResponseStatusException;
 import uk.gov.hmcts.reform.prl.services.AuthorisationService;
 import uk.gov.hmcts.reform.prl.services.cafcass.CafcassCdamService;
 
@@ -21,10 +25,13 @@ import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Map;
 import java.util.UUID;
 
+import static feign.Request.HttpMethod.GET;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.UUID.randomUUID;
-import static org.junit.Assert.assertThrows;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -35,7 +42,9 @@ import static org.springframework.http.HttpStatus.OK;
 import static uk.gov.hmcts.reform.prl.utils.TestConstants.CAFCASS_TEST_AUTHORISATION_TOKEN;
 import static uk.gov.hmcts.reform.prl.utils.TestConstants.CAFCASS_TEST_SERVICE_AUTHORISATION_TOKEN;
 import static uk.gov.hmcts.reform.prl.utils.TestConstants.EMPTY_STRING;
+import static uk.gov.hmcts.reform.prl.utils.TestConstants.TEST_AUTHORIZATION;
 import static uk.gov.hmcts.reform.prl.utils.TestConstants.TEST_CAFCASS_DOWNLOAD_FILENAME;
+import static uk.gov.hmcts.reform.prl.utils.TestConstants.TEST_SERVICE_AUTHORIZATION;
 
 @PropertySource(value = "classpath:application.yaml")
 @RunWith(MockitoJUnitRunner.Silent.class)
@@ -52,14 +61,19 @@ public class CafcassDocumentManagementControllerTest {
 
     private static final int TEST_DOWNLOAD_FILE_CONTENT_LENGTH = 10000;
 
+    private UUID documentId;
+
+    @BeforeEach
+    public void setUp() {
+        documentId = randomUUID();
+    }
+
     @Test
     @DisplayName("Successful download of document through CDAM Service")
     public void testCdamDocumentDownloadServiceResponseStatusOk() {
         Resource documentResource = createNewResource();
 
         ResponseEntity<Resource> expectedResponse = ResponseEntity.status(OK).contentType(MediaType.APPLICATION_PDF).body(documentResource);
-
-        final UUID documentId = randomUUID();
         when(authorisationService.authoriseService(any())).thenReturn(true);
         when(authorisationService.authoriseUser(any())).thenReturn(true);
 
@@ -77,7 +91,6 @@ public class CafcassDocumentManagementControllerTest {
     @Test
     @DisplayName("Failed download of document through CDAM Service")
     public void testGetDocumentBinary() {
-        final UUID documentId = randomUUID();
         when(authorisationService.authoriseService(any())).thenReturn(true);
         when(authorisationService.authoriseUser(any())).thenReturn(true);
 
@@ -95,28 +108,63 @@ public class CafcassDocumentManagementControllerTest {
 
     }
 
-
     @Test
-    @DisplayName("Download Document API ThrowsException")
-    public void testDownloadDocumentThrowsException() {
-        final UUID documentId = randomUUID();
+    public void testInvalidServicAuth_403Forbidden() {
         when(authorisationService.authoriseService(any())).thenReturn(false);
         when(authorisationService.authoriseUser(any())).thenReturn(false);
-
-        Mockito.when(cafcassCdamService.getDocument(CAFCASS_TEST_AUTHORISATION_TOKEN, CAFCASS_TEST_SERVICE_AUTHORISATION_TOKEN, documentId))
-            .thenReturn(new ResponseEntity<Resource>(
-                BAD_REQUEST));
-
-        assertThrows(ResponseStatusException.class, () -> {
-            cafcassDocumentManagementController.downloadDocument(
-                CAFCASS_TEST_AUTHORISATION_TOKEN,
-                CAFCASS_TEST_SERVICE_AUTHORISATION_TOKEN,
-                documentId);
-        });
-
+        final ResponseEntity response = cafcassDocumentManagementController.downloadDocument(
+            CAFCASS_TEST_AUTHORISATION_TOKEN,
+            CAFCASS_TEST_SERVICE_AUTHORISATION_TOKEN,
+            documentId);
+        assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
     }
 
 
+    @Test
+    public void testFeignExceptionBadRequest() throws IOException {
+        when(authorisationService.authoriseService(any())).thenReturn(true);
+        when(authorisationService.authoriseUser(any())).thenReturn(true);
+        when(cafcassCdamService.getDocument(TEST_AUTHORIZATION, TEST_SERVICE_AUTHORIZATION, documentId)).thenThrow(feignException(HttpStatus.BAD_REQUEST.value(), "Not found"));
+        final ResponseEntity response = cafcassDocumentManagementController.downloadDocument(
+            TEST_AUTHORIZATION,
+            TEST_SERVICE_AUTHORIZATION,
+            documentId
+        );
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void testFeignExceptionUnAuthorised() throws IOException {
+        when(authorisationService.authoriseService(any())).thenReturn(true);
+        when(authorisationService.authoriseUser(any())).thenReturn(true);
+        when(cafcassCdamService.getDocument(TEST_AUTHORIZATION, TEST_SERVICE_AUTHORIZATION, documentId)).thenThrow(feignException(HttpStatus.UNAUTHORIZED.value(), "Unauthorized"));
+        final ResponseEntity response = cafcassDocumentManagementController.downloadDocument(
+            TEST_AUTHORIZATION,
+            TEST_SERVICE_AUTHORIZATION,
+            documentId
+        );
+        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+    }
+
+    @Test
+    public void testExceptionInternalServerError() throws IOException {
+        when(authorisationService.authoriseService(any())).thenReturn(true);
+        when(authorisationService.authoriseUser(any())).thenReturn(true);
+        when(cafcassCdamService.getDocument(TEST_AUTHORIZATION, TEST_SERVICE_AUTHORIZATION, documentId)).thenThrow(new RuntimeException());
+        final ResponseEntity response = cafcassDocumentManagementController.downloadDocument(
+            TEST_AUTHORIZATION,
+            TEST_SERVICE_AUTHORIZATION,
+            documentId
+        );
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR , response.getStatusCode());
+    }
+
+    public static FeignException feignException(int status, String message) {
+        return FeignException.errorStatus(message, Response.builder()
+            .status(status)
+            .request(Request.create(GET, EMPTY, Map.of(), new byte[]{}, UTF_8, null))
+            .build());
+    }
 
     private Resource createNewResource() {
         Resource documentResource = new Resource() {
