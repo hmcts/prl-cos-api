d47a40f786f1306a1d14affe2500750b97abb571
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-10-14 17:25:07 +0100

    uncommented draft to check aks deploy

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseApplicationResponseController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseApplicationResponseController.java
@@ -13,9 +13,9 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-//import uk.gov.hmcts.reform.prl.enums.YesOrNo;
-//import uk.gov.hmcts.reform.prl.models.Element;
-//import uk.gov.hmcts.reform.prl.models.complextypes.PartyDetails;
+import uk.gov.hmcts.reform.prl.enums.YesOrNo;
+import uk.gov.hmcts.reform.prl.models.Element;
+import uk.gov.hmcts.reform.prl.models.complextypes.PartyDetails;
 //import uk.gov.hmcts.reform.prl.models.complextypes.citizen.documents.ResponseDocuments;
 import uk.gov.hmcts.reform.prl.models.documents.Document;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
@@ -25,13 +25,14 @@ import uk.gov.hmcts.reform.prl.utils.CaseUtils;
 
 //import java.time.LocalDate;
 //import java.util.ArrayList;
-//import java.util.List;
-//import java.util.stream.Collectors;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 //import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.C7_FINAL_ENGLISH;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.DOCUMENT_C7_BLANK_HINT;
 //import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.REVIEW_AND_SUBMIT;
-//import static uk.gov.hmcts.reform.prl.utils.ElementUtils.element;
+import static uk.gov.hmcts.reform.prl.utils.ElementUtils.element;
 
 @Slf4j
 @RestController
@@ -64,7 +65,7 @@ public class CaseApplicationResponseController {
         CaseDetails caseDetails = coreCaseDataApi.getCase(authorisation, s2sToken, caseId);
         log.info("Case Data retrieved for id : " + caseDetails.getId().toString());
         CaseData caseData = CaseUtils.getCaseData(caseDetails, objectMapper);
-        //caseData = updateCurrentRespondent(caseData, YesOrNo.Yes, partyId);
+        caseData = updateCurrentRespondent(caseData, YesOrNo.Yes, partyId);
         log.info("BEFORE call to generate Document {} {}",caseData, partyId);
 
         Document document = documentGenService.generateSingleDocument(
@@ -138,16 +139,16 @@ public class CaseApplicationResponseController {
     //        );
     //    }
 
-    //    private CaseData updateCurrentRespondent(CaseData caseData, YesOrNo currentRespondent, String partyId) {
-    //        List<Element<PartyDetails>> partyDetails = caseData.getRespondents().stream().map(respondent -> {
-    //            if (respondent.getId().toString().equalsIgnoreCase(partyId)) {
-    //                return element(respondent.getValue().toBuilder().currentRespondent(currentRespondent).build());
-    //            } else {
-    //                return respondent;
-    //            }
-    //        }).collect(Collectors.toList());
-    //        caseData = caseData.toBuilder().respondents(partyDetails).build();
-    //        return caseData;
-    //    }
+    private CaseData updateCurrentRespondent(CaseData caseData, YesOrNo currentRespondent, String partyId) {
+        List<Element<PartyDetails>> partyDetails = caseData.getRespondents().stream().map(respondent -> {
+            if (respondent.getId().toString().equalsIgnoreCase(partyId)) {
+                return element(respondent.getValue().toBuilder().currentRespondent(currentRespondent).build());
+            } else {
+                return respondent;
+            }
+        }).collect(Collectors.toList());
+        caseData = caseData.toBuilder().respondents(partyDetails).build();
+        return caseData;
+    }
 }
 
