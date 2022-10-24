8c7115f0764fefe4a316cd2b83aea28f3b580eb2
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-10-24 15:56:14 +0100

    added case data mappings for respondent solicitor for Confirm or edit your contact details event

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
@@ -50,6 +50,7 @@ import uk.gov.hmcts.reform.prl.models.complextypes.AppointedGuardianFullName;
 import uk.gov.hmcts.reform.prl.models.complextypes.Behaviours;
 import uk.gov.hmcts.reform.prl.models.complextypes.Child;
 import uk.gov.hmcts.reform.prl.models.complextypes.ConfidentialityDisclaimer;
+import uk.gov.hmcts.reform.prl.models.complextypes.ConfirmEditDetails;
 import uk.gov.hmcts.reform.prl.models.complextypes.Correspondence;
 import uk.gov.hmcts.reform.prl.models.complextypes.FL401OtherProceedingDetails;
 import uk.gov.hmcts.reform.prl.models.complextypes.FurtherEvidence;
@@ -681,6 +682,6 @@ public class CaseData implements MappableObject {
     private final Behaviours respondentChildAbuseBehaviour;
     private final RespondentChildAbduction respondentChildAbduction;
     private final RespondentOtherConcerns respondentOtherConcerns;
-
-
+    /** Confirm or Edit your contact details. **/
+    private final ConfirmEditDetails rsConfirmEditContactDetails;
 }
