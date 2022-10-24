88a2ca74d07b54196070b1e591d39d52b8726051
parvezhmcts <parvez.qureshi@hmcts.net>
2022-10-24 11:36:28 +0100

    trying to fix sonar failure

--- src/main/java/uk/gov/hmcts/reform/prl/constants/cafcass/CafcassAppConstants.java
+++ src/main/java/uk/gov/hmcts/reform/prl/constants/cafcass/CafcassAppConstants.java
@@ -1,11 +1,9 @@
 package uk.gov.hmcts.reform.prl.constants.cafcass;
 
-public interface CafcassAppConstants {
+public abstract class CafcassAppConstants {
 
-    String ENGLAND_POSTCODE_NATIONALCODE = "E";
+    public static final String ENGLAND_POSTCODE_NATIONALCODE = "E";
 
-    String CAFCASS_DOCUMENT_UPLOAD_EVENT_ID = "cafcass-document-upload";
-    String[] ALLOWED_FILE_TYPES = {"pdf"};
-    String[] ALLOWED_TYPE_OF_DOCS = {"FL401", "C8", "WITNESS_STATEMENT", "EXHIBITS_EVIDENCE", "EXHIBITS_COVERSHEET"};
-    String CAFCASS_USER_ROLE = "caseworker-privatelaw-cafcass";
+    public static final String CAFCASS_DOCUMENT_UPLOAD_EVENT_ID = "cafcass-document-upload";
+    public static final String CAFCASS_USER_ROLE = "caseworker-privatelaw-cafcass";
 }
