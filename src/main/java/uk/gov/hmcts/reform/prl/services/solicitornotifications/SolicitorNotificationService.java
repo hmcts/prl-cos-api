d23c50b59785da47b084bc7e34302fc392041b5f
Khurshed Khan <khurshed.khan@hmcts.net>
2022-10-31 17:17:01 +0000

    PRL-2444: Added changes for mail integration of for applicant and respondent solicitor

--- src/main/java/uk/gov/hmcts/reform/prl/services/solicitornotifications/SolicitorNotificationService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/solicitornotifications/SolicitorNotificationService.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.prl.services.solicitornotifications;
 
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 
 public interface SolicitorNotificationService {
 
