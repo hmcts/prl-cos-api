157bc1a8edf04bee2d0a5093ed625ff9aae3993d
Rahul Sanjava <rahul.sanjava@HMCTS.NET>
2022-11-01 12:38:44 +0000

    feat: implement applicant fields mapping (PRLC100-238)

---
+++ src/main/java/uk/gov/hmcts/reform/prl/models/c100rebuild/ApplicantDto.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.reform.prl.models.c100rebuild;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.gov.hmcts.reform.prl.enums.YesOrNo;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ApplicantDto {
+
+    private String id;
+    private String applicantFirstName;
+    private String applicantLastName;
+    private String[] contactDetailsPrivate;
+    private String[] contactDetailsPrivateAlternative;
+    private String applicantPreviousName;
+    private String applicantGender;
+    private DateOfBirth applicantDateOfBirth;
+    private String applicantPlaceOfBirth;
+    private String applicantAddressPostcode;
+    private String applicantAddress1;
+    private String applicantAddress2;
+    private String applicantAddressTown;
+    private String applicantAddressCounty;
+    private YesOrNo applicantAddressHistory;
+    private String applicantProvideDetailsOfPreviousAddresses;
+}
