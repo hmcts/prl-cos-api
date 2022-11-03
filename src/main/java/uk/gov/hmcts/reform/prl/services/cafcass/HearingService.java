8da4aa48594a8fcad2dbd414bd0cc70d7058e816
RonanCog <ronan.behan@HMCTS.NET>
2022-11-03 15:54:28 +0000

    Imported Hearings Pojos from Shajahan and worked on the Hearing Service with Purushotham

---
+++ src/main/java/uk/gov/hmcts/reform/prl/services/cafcass/HearingService.java
@@ -0,0 +1,31 @@
+package uk.gov.hmcts.reform.prl.services.cafcass;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.prl.clients.Cafcass.HearingApiClient;
+import uk.gov.hmcts.reform.prl.models.cafcass.Hearing.Hearings;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class HearingService {
+
+    Hearings hearingDetails = null;
+
+    private final AuthTokenGenerator authTokenGenerator;
+
+    @Autowired
+    HearingApiClient hearingApiClient;
+
+    public Hearings getHearings(String userToken, String caseReferenceNumber){
+        try {
+            hearingDetails = hearingApiClient.getHearingDetails(userToken, authTokenGenerator.generate(), caseReferenceNumber);
+        }catch (Exception e){
+            log.error(e.getMessage());
+        }
+        return hearingDetails;
+    }
+}
