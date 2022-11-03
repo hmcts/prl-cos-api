0b5bfce1cc4d4b08735858ee5614138241f88a20
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2022-11-03 16:42:36 +0000

    fix build temp

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/manageorders/FL404.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/manageorders/FL404.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.prl.models.Address;
+import uk.gov.hmcts.reform.prl.models.Element;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -82,5 +83,8 @@ public class FL404 {
     private final String fl404bOccupationTime2;
     private final String fl404bDateOfNextHearing;
     private final String fl404bTimeOfNextHearing;
+
+    //Draft order changes
+    private final List<Element<String>> addDirections;
 
 }
