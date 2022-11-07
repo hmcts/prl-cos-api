9ad1b5eb76a4104dcfc820516e1434d0de0841ef
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2022-11-07 01:10:43 +0000

    Populate fields

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/DraftAnOrderController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/DraftAnOrderController.java
@@ -17,13 +17,16 @@ import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.prl.constants.PrlAppsConstants;
 import uk.gov.hmcts.reform.prl.mapper.CcdObjectMapper;
+import uk.gov.hmcts.reform.prl.models.complextypes.manageorders.FL404;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.ManageOrders;
 import uk.gov.hmcts.reform.prl.services.ManageOrderService;
 import uk.gov.hmcts.reform.prl.utils.CaseUtils;
 
 import java.time.LocalDate;
 import java.util.Map;
 
+import static java.util.Optional.ofNullable;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.COURT_NAME;
 
@@ -90,7 +93,6 @@ public class DraftAnOrderController {
         @RequestBody CallbackRequest callbackRequest
     ) throws Exception {
         CaseData caseData = CaseUtils.getCaseData(callbackRequest.getCaseDetails(), objectMapper);
-        log.info("**********callbackRequest**********{}", callbackRequest);
         if (callbackRequest
             .getCaseDetailsBefore() != null && callbackRequest
             .getCaseDetailsBefore().getData().get(COURT_NAME) != null) {
@@ -98,7 +100,24 @@ public class DraftAnOrderController {
                                       .getCaseDetailsBefore().getData().get(COURT_NAME).toString());
         }
         log.info("Case data before prepopulate: {}", caseData.getManageOrders().getFl404CustomFields());
-        caseData = manageOrderService.populateCustomOrderFields(caseData);
+        FL404 fl404CustomFields = caseData.getManageOrders().getFl404CustomFields();
+        fl404CustomFields.toBuilder().fl404bApplicantName(String.format(PrlAppsConstants.FORMAT,
+                                                                        caseData.getApplicantsFL401().getFirstName(),
+                                                                        caseData.getApplicantsFL401().getLastName()
+        ))
+            .fl404bRespondentName(String.format(PrlAppsConstants.FORMAT, caseData.getRespondentsFL401().getFirstName(),
+                                                caseData.getRespondentsFL401().getLastName()
+            )).build();
+        if (ofNullable(caseData.getRespondentsFL401().getAddress()).isPresent()) {
+            fl404CustomFields = fl404CustomFields.toBuilder()
+                .fl404bRespondentAddress(caseData.getRespondentsFL401().getAddress()).build();
+        }
+        if (ofNullable(caseData.getRespondentsFL401().getDateOfBirth()).isPresent()) {
+            fl404CustomFields = fl404CustomFields.toBuilder()
+                .fl404bRespondentDob(caseData.getRespondentsFL401().getDateOfBirth()).build();
+        }
+        caseData = caseData.toBuilder()
+            .manageOrders(ManageOrders.builder().fl404CustomFields(fl404CustomFields).build()).build();
         log.info("Case data after prepopulate: {}", caseData.getManageOrders().getFl404CustomFields());
         Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
         caseDataUpdated = manageOrderService.getCaseData(authorisation, caseData);
