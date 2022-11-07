6031214473cc8c70700a6f85aa7661ad92a348cb
RonanCog <ronan.behan@HMCTS.NET>
2022-11-07 20:13:52 +0000

    Commit after creating stream

--- src/main/java/uk/gov/hmcts/reform/prl/models/cafcass/Hearing/Hearings.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/cafcass/Hearing/Hearings.java
@@ -6,7 +6,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @Setter
--- src/test/java/uk/gov/hmcts/reform/prl/services/cafcass/HearingServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/cafcass/HearingServiceTest.java
@@ -1,19 +1,24 @@
 package uk.gov.hmcts.reform.prl.services.cafcass;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import org.springframework.http.HttpStatus;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.prl.clients.Cafcass.HearingApiClient;
+import uk.gov.hmcts.reform.prl.models.cafcass.Hearing.CaseHearing;
 import uk.gov.hmcts.reform.prl.models.cafcass.Hearing.Hearings;
 
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -21,7 +26,6 @@ public class HearingServiceTest {
 
     @Mock
     private AuthTokenGenerator authTokenGenerator;
-
     private String authToken;
     private String userToken;
 
@@ -30,7 +34,7 @@ public class HearingServiceTest {
     @Mock
     HearingApiClient hearingApiClient;
 
-    HttpStatus ok = HttpStatus.OK;
+    Hearings hearings = null;
 
     @InjectMocks
     private HearingService hearingService;
@@ -41,17 +45,29 @@ public class HearingServiceTest {
         authToken = "Authorization";
         userToken = "ServiceAuthorization";
         caseReferenceNumber = "caseReference";
-        when(authTokenGenerator.generate()).thenReturn(userToken);
+
+        List<CaseHearing> caseHearings = null;
+        CaseHearing mock1 = Mockito.mock(CaseHearing.class);
+        caseHearings.add(mock1);
+        hearings = new Hearings();
+        hearings.setCaseRef("caseReference");
+        hearings.setHmctsServiceCode("Authorization");
+        hearings.setCaseHearings(caseHearings);
     }
 
     @Test
     @DisplayName("test case for HearingService.")
-    public void getHearingsTest(){
+    public void getHearingsTestSuccess(){
+        when(authTokenGenerator.generate()).thenReturn(userToken);
+        when(hearingApiClient.getHearingDetails(anyString(), anyString(), anyString())).thenReturn(hearings);
 
-        //final ResponseEntity<Resource> response = hearingService.getHearings(authToken,userToken,caseReferenceNumber);
+        //when the test runs it says there is no instance of the following vars authToken, userToken, caseReferenceNumber
+        //I think it's not able to read the setUp method
+        //hearings is also null
+        Hearings response =
+            hearingService.getHearings(authToken, userToken, caseReferenceNumber);
 
-        when(hearingService.getHearings(authToken,userToken,caseReferenceNumber)).thenReturn(new Hearings(ok));
-
+        Assert.assertEquals(hearings, response);
     }
 
 }
