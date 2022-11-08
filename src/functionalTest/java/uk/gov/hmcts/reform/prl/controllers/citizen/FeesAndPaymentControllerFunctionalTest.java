f5476c46eaf384cc3730ab87e9ce8ad5d5d98e47
Yogendra Upasani <yogendra.upasani@hmcts.net>
2022-11-08 23:33:31 +0000

    Fixed Functional tests for c100 rebuild

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/citizen/FeesAndPaymentControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/citizen/FeesAndPaymentControllerFunctionalTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.prl.ResourceLoader;
 import uk.gov.hmcts.reform.prl.utils.IdamTokenGenerator;
@@ -16,6 +17,12 @@ import uk.gov.hmcts.reform.prl.utils.ServiceAuthenticationGenerator;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @ContextConfiguration
+@TestPropertySource(
+    properties = {
+        "idam.client.secret=${CITIZEN_IDAM_CLIENT_SECRET}",
+        "idam.client.id=prl-citizen-frontend"
+    }
+)
 public class FeesAndPaymentControllerFunctionalTest {
 
     @Autowired
