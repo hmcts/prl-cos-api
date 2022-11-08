cf6554d4aa56600652e0dd2ac7ff9e83ae404e16
Saranya Vinayagam <saranyavinayagam@Saranyas-MacBook-Pro.local>
2022-11-08 16:07:11 +0000

    PRL100-292 other person details data mapping

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/mapper/CaseDataOtherPersonsElementsMapper.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/mapper/CaseDataOtherPersonsElementsMapper.java
@@ -1,27 +1,27 @@
 package uk.gov.hmcts.reform.prl.controllers.citizen.mapper;
 
+import uk.gov.hmcts.reform.prl.constants.PrlAppsConstants;
 import uk.gov.hmcts.reform.prl.enums.Gender;
-import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.models.Address;
 import uk.gov.hmcts.reform.prl.models.Element;
-import uk.gov.hmcts.reform.prl.models.c100rebuild.ApplicantDto;
 import uk.gov.hmcts.reform.prl.models.c100rebuild.C100RebuildOtherPersonDetailsElements;
+import uk.gov.hmcts.reform.prl.models.c100rebuild.ChildRelationship;
 import uk.gov.hmcts.reform.prl.models.c100rebuild.DateofBirth;
+import uk.gov.hmcts.reform.prl.models.c100rebuild.OtherPersonAddress;
 import uk.gov.hmcts.reform.prl.models.c100rebuild.OtherPersonDetail;
 import uk.gov.hmcts.reform.prl.models.c100rebuild.PersonalDetails;
+import uk.gov.hmcts.reform.prl.models.c100rebuild.RelationshipDetails;
 import uk.gov.hmcts.reform.prl.models.complextypes.PartyDetails;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
 
 public class CaseDataOtherPersonsElementsMapper {
-    private static final String ADDRESS_FIELD = "address";
-    private static final String EMAIL_FIELD = "email";
-    private static final String TELEPHONE_FIELD = "telephone";
 
     public static void updateOtherPersonDetailsElementsForCaseData(CaseData.CaseDataBuilder caseDataBuilder,
                                                  C100RebuildOtherPersonDetailsElements c100RebuildOtherPersonDetailsElements) {
@@ -48,28 +48,38 @@ public class CaseDataOtherPersonsElementsMapper {
                 .firstName(otherPersonDetail.getFirstName())
                 .lastName(otherPersonDetail.getLastName())
                 .previousName(personalDetails.getPreviousFullName())
-                .gender(Gender.valueOf(personalDetails.getGender()))
-                .dateOfBirth("No".equalsIgnoreCase(personalDetails.getIsDateOfBirthUnknown())
+                .gender(Gender.getDisplayedValueFromEnumString(personalDetails.getGender()))
+                .dateOfBirth(PrlAppsConstants.NO.equalsIgnoreCase(personalDetails.getIsDateOfBirthUnknown())
                         ? buildDateOfBirth(personalDetails.getDateOfBirth())
                         : buildDateOfBirth(personalDetails.getApproxDateOfBirth()))
-                //TODO
-//                .address(buildAddress())
-                .build();
+                .address(buildAddress(otherPersonDetail.getOtherPersonAddress()))
+                .relationshipToChildren(buildChildRelationship(otherPersonDetail.getRelationshipDetails()))
+        .build();
     }
 
-    private static Address buildAddress(ApplicantDto applicantDto) {
+    private static String buildChildRelationship(RelationshipDetails relationshipDetails) {
+        Optional<ChildRelationship> childRelationship = relationshipDetails.getRelationshipToChildren().stream().findFirst();
+        if (childRelationship.isPresent()) {
+            ChildRelationship relationship = childRelationship.get();
+            if (PrlAppsConstants.OTHER.equalsIgnoreCase(relationship.getRelationshipType())) {
+                return relationship.getOtherRelationshipTypeDetails();
+            }
+            return relationship.getRelationshipType();
+        }
+        return null;
+    }
+
+    private static Address buildAddress(OtherPersonAddress address) {
         return Address
                 .builder()
-                .addressLine1(applicantDto.getApplicantAddress1())
-                .addressLine2(applicantDto.getApplicantAddress2())
-                .postTown(applicantDto.getApplicantAddressTown())
-                .county(applicantDto.getApplicantAddressCounty())
-                .postCode(applicantDto.getApplicantAddressPostcode())
+                .addressLine1(address.getAddressLine1())
+                .addressLine2(address.getAddressLine2())
+                .addressLine3(address.getAddressLine3())
+                .postTown(address.getPostTown())
+                .county(address.getCounty())
+                .postCode(address.getPostCode())
+                .country(address.getCountry())
                 .build();
-    }
-
-    private static YesOrNo buildConfidentialField(List<String> contactDetailsPrivateList, String field) {
-        return contactDetailsPrivateList.contains(field) ? YesOrNo.Yes : YesOrNo.No;
     }
 
     private static LocalDate buildDateOfBirth(DateofBirth date) {
