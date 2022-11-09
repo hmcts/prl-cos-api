35eb725359a5821b52a5569bc6abd38d152eed44
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2022-11-09 15:07:12 +0000

    Checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/prl/services/DraftAnOrderService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/DraftAnOrderService.java
@@ -22,8 +22,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static java.util.Optional.ofNullable;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.APPLICANT_SOLICITOR;
