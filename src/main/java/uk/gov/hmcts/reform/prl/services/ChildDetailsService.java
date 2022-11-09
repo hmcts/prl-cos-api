1260187a43d08d1a621b4b6c71833f182ae8dbe7
Praneeth G <praneethkumar.gadhe@hmcts.net>
2022-11-09 22:17:22 +0000

    changes are in progress

--- src/main/java/uk/gov/hmcts/reform/prl/services/ChildDetailsService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ChildDetailsService.java
@@ -7,12 +7,10 @@ import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.prl.models.Element;
 import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicList;
 import uk.gov.hmcts.reform.prl.models.complextypes.ApplicantRelatedToChild;
-import uk.gov.hmcts.reform.prl.models.complextypes.Child;
 import uk.gov.hmcts.reform.prl.models.complextypes.PartyDetails;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.utils.ElementUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -25,11 +23,8 @@ public class ChildDetailsService {
 
 
     public Map<String, Object> getApplicantDetails(CaseData caseData) {
-        List<ApplicantRelatedToChild> lists = new ArrayList<>();
-        lists.add(ApplicantRelatedToChild.builder().selectedApplicantName(getApplicantsAsDynamicList(caseData)).build());
-        Child child = Child.builder()
-                        .applicantsRelatedToChild(lists).build();
-        return  objectMapper.convertValue(child, Map.class);
+        return  objectMapper.convertValue(ApplicantRelatedToChild.builder()
+                .selectedApplicantName(getApplicantsAsDynamicList(caseData)).build(), Map.class);
     }
 
     public DynamicList getApplicantsAsDynamicList(CaseData caseData) {
