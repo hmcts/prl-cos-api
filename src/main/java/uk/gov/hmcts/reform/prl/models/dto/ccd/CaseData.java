ac53f07acb5c34e18ace4f6be852a9028115ad1c
Rahul Sanjava <rahul.sanjava@HMCTS.NET>
2022-11-09 17:34:57 +0000

    feat: add remaining fields in caseData for C100 rebuild journey

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
@@ -649,13 +649,25 @@ public class CaseData implements MappableObject {
     private final String paymentReferenceNumber;
 
     // C100 Rebuild
-    private String c100RebuildConfidentiality;
     private String c100RebuildInternationalElements;
     private String c100RebuildReasonableAdjustments;
     private String c100RebuildTypeOfOrder;
     private String c100RebuildHearingWithoutNotice;
+    private String c100RebuildHearingUrgency;
     private String c100RebuildOtherProceedings;
     private String c100RebuildReturnUrl;
     private String c100RebuildMaim;
-    private String c100RebuildHearingUrgency;
+    private String c100RebuildChildDetails;
+    private String c100RebuildApplicantDetails;
+    private String c100RebuildOtherChildrenDetails;
+    private String c100RebuildRespondentDetails;
+    private String c100RebuildOtherPersonsDetails;
+
+    private String c100RebuildSafetyConcerns;
+    private String c100RebuildScreeningQuestions;
+    private String c100RebuildHelpWithFeesDetails;
+    private String c100RebuildStatementOfTruth;
+    private String helpWithFeesReferenceNumber;
+    private String c100RebuildChildPostCode;
+    private String c100RebuildConsentOrderDetails;
 }
