253d4d20a147776a8469b0cca1462b200ff66f2a
2219956 <ramesh.meripe@hmcts.net>
2022-11-09 12:29:35 +0000

    added logs for prod issue

--- src/main/java/uk/gov/hmcts/reform/prl/services/CourtFinderService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/CourtFinderService.java
@@ -16,10 +16,8 @@ import uk.gov.hmcts.reform.prl.models.court.CourtEmailAddress;
 import uk.gov.hmcts.reform.prl.models.court.ServiceArea;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 
-
 import java.util.Optional;
 import java.util.stream.Collectors;
-
 import static java.util.Optional.ofNullable;
 import static uk.gov.hmcts.reform.prl.enums.LiveWithEnum.anotherPerson;
 import static uk.gov.hmcts.reform.prl.enums.LiveWithEnum.applicant;
@@ -87,9 +85,9 @@ public class CourtFinderService {
             return getPostcodeFromWrappedParty(caseData.getRespondents().get(0));
         } else if (child.getChildLiveWith().contains(anotherPerson) && ofNullable(getFirstOtherPerson(child)).isPresent()) {
             if (ofNullable(getFirstOtherPerson(child).getAddress().getPostCode()).isEmpty()) {
-                caseData.getApplicants().stream().forEach(x->log.info("addressLine1 :::",x.getValue().getAddress().getAddressLine1()));
-                caseData.getApplicants().stream().forEach(x->log.info("addressLine2 :::",x.getValue().getAddress().getAddressLine2()));
-                caseData.getApplicants().stream().forEach(x->log.info("PostCode :::",x.getValue().getAddress().getPostCode()));
+                caseData.getApplicants().stream().forEach(x->{log.info("addressLine1 :::",x.getValue().getAddress().getAddressLine1())});
+                caseData.getApplicants().stream().forEach(x->{log.info("addressLine2 :::",x.getValue().getAddress().getAddressLine2())});
+                caseData.getApplicants().stream().forEach(x->{log.info("PostCode :::",x.getValue().getAddress().getPostCode())});
                 return getPostcodeFromWrappedParty(caseData.getApplicants().get(0));
             }
             return getFirstOtherPerson(child).getAddress().getPostCode();
