e54fc12ed290b9c0e18155fcdf7fa54eb1137a1e
Yogendra Upasani <yogendra.upasani@hmcts.net>
2022-11-09 10:17:57 +0000

    Fixed Functional tests for c100 rebuild

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/citizen/FeesAndPaymentControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/citizen/FeesAndPaymentControllerFunctionalTest.java
@@ -4,7 +4,6 @@ import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Ignore;
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
