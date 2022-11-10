cdd391e4a0edbb62e3a960995de51538f0abb130
Praneeth G <praneethkumar.gadhe@hmcts.net>
2022-11-10 09:33:40 +0000

    changes are in progress

--- src/main/java/uk/gov/hmcts/reform/prl/services/ChildDetailsService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ChildDetailsService.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.prl.services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.launchdarkly.shaded.com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,6 +24,7 @@ public class ChildDetailsService {
 
 
     public Map<String, Object> getApplicantDetails(CaseData caseData) {
+        log.info("child data : {}",new Gson().toJson(getApplicantsAsDynamicList(caseData)));
         return  objectMapper.convertValue(ApplicantRelatedToChild.builder()
                 .selectedApplicantName(getApplicantsAsDynamicList(caseData)).build(), Map.class);
     }
