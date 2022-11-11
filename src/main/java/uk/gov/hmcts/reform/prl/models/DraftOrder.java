9b82cfd96b9309270349b07e658bfe11a133c6cd
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2022-11-11 01:44:19 +0000

    Fix checkstyle

--- src/main/java/uk/gov/hmcts/reform/prl/models/DraftOrder.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/DraftOrder.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.prl.models;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
