e0fec8b0e724896e98ce164c00c77e3c8ae709d2
praveenrach <praveen.rachapally@HMCTS.NET>
2022-11-11 07:46:37 +0000

    updated caseservice for citizen update case

--- src/main/java/uk/gov/hmcts/reform/prl/enums/CaseEvent.java
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/CaseEvent.java
@@ -3,7 +3,8 @@ package uk.gov.hmcts.reform.prl.enums;
 import java.util.Arrays;
 
 public enum CaseEvent {
-    LINK_CITIZEN("linkCitizenAccount");
+    LINK_CITIZEN("linkCitizenAccount"),
+    CITIZEN_INTERNAL_CASE_UPDATE("citizen-internal-case-update");
 
     private final String value;
 
