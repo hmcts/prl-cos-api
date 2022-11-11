86bccdaed06e4134cc21b620fdb8f285ee633ee5
parvezhmcts <parvez.qureshi@hmcts.net>
2022-11-11 14:05:26 +0000

    ISDB-448
    - comment code temporarily to fix submit issue

--- src/main/java/uk/gov/hmcts/reform/prl/services/SearchCasesDataService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/SearchCasesDataService.java
@@ -57,8 +57,8 @@ public class SearchCasesDataService {
 
             }
             // set applicant and respondent case flag
-            setApplicantFlag(caseData, caseDetails);
-            setRespondentFlag(caseData, caseDetails);
+            //    setApplicantFlag(caseData, caseDetails);
+            //    setRespondentFlag(caseData, caseDetails);
         }
 
         return caseDetails;
