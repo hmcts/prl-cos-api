9fabdfa7bdf58337b3bf7952c630953ca3bef9c5
Aloknath Datta <alokdatta.uit@gmail.com>
2022-11-21 17:12:45 +0000

    update

--- src/cftlib/java/uk/gov/hmcts/reform/prl/CftLibConfig.java
+++ src/cftlib/java/uk/gov/hmcts/reform/prl/CftLibConfig.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.rse.ccd.lib.api.CFTLib;
 import uk.gov.hmcts.rse.ccd.lib.api.CFTLibConfigurer;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 @Component
 public class CftLibConfig implements CFTLibConfigurer {
     @Override
