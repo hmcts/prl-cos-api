09a8c7dd7a49a1894eff1d94984d659efcdf639d
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2022-11-23 19:26:40 +0000

    Adding final order changes for admin

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/manageorders/DirectionDetails.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/manageorders/DirectionDetails.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.prl.models.complextypes.manageorders;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
+import uk.gov.hmcts.reform.prl.enums.BoldEnum;
 
 import java.util.List;
 
@@ -11,7 +12,7 @@ import java.util.List;
 public class DirectionDetails {
 
     @JsonProperty("isBold")
-    private final List<String> isBold;
+    private final List<BoldEnum> isBold;
     @JsonProperty("directionText")
     private final String directionText;
 
