cf3ba5e1f3187dc4a6fd1281ad485271748e0332
2219956 <ramesh.meripe@hmcts.net>
2022-11-23 14:13:58 +0000

    AllegationOfHarmRevised cos api changes : TypeOfAbuseEnum changed key and value

--- src/main/java/uk/gov/hmcts/reform/prl/enums/TypeOfAbuseEnum.java
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/TypeOfAbuseEnum.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.prl.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
@@ -10,26 +11,31 @@ import lombok.RequiredArgsConstructor;
 public enum TypeOfAbuseEnum {
 
     @JsonProperty("TypeOfAbuseEnum_value_1")
-    TypeOfAbuseEnum_value_1("Physical abuse"),
+    TypeOfAbuseEnum_value_1("TypeOfAbuseEnum_value_1","Physical abuse"),
 
     @JsonProperty("TypeOfAbuseEnum_value_2")
-    TypeOfAbuseEnum_value_2("Psychological abuse"),
+    TypeOfAbuseEnum_value_2("TypeOfAbuseEnum_value_2","Psychological abuse"),
 
     @JsonProperty("TypeOfAbuseEnum_value_3")
-    TypeOfAbuseEnum_value_3("Sexual abuse"),
+    TypeOfAbuseEnum_value_3("TypeOfAbuseEnum_value_3","Sexual abuse"),
 
     @JsonProperty("TypeOfAbuseEnum_value_4")
-    TypeOfAbuseEnum_value_4("Emotional abuse"),
+    TypeOfAbuseEnum_value_4("TypeOfAbuseEnum_value_4","Emotional abuse"),
 
     @JsonProperty("TypeOfAbuseEnum_value_5")
-    TypeOfAbuseEnum_value_5("Financial  abuse");
+    TypeOfAbuseEnum_value_5("TypeOfAbuseEnum_value_5","Financial  abuse");
 
-
+    private final String id;
     private final String displayedValue;
 
     @JsonValue
     public String getDisplayedValue() {
         return displayedValue;
+    }
+
+    @JsonCreator
+    public static TypeOfAbuseEnum getValue(String key) {
+        return TypeOfAbuseEnum.valueOf(key);
     }
 
 }
