eb7c999a4e9f672e33f5979dd69b29151f32af9a
Yogendra Upasani <yogendra.upasani@hmcts.net>
2022-11-29 16:05:54 +0000

    Updated c100 rebuild code

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/mapper/CaseDataUrgencyElementsMapper.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/mapper/CaseDataUrgencyElementsMapper.java
@@ -4,7 +4,6 @@ import uk.gov.hmcts.reform.prl.enums.citizen.UrgentHearingReasonEnum;
 import uk.gov.hmcts.reform.prl.models.c100rebuild.C100RebuildUrgencyElements;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,27 +21,25 @@ public class CaseDataUrgencyElementsMapper {
     private static final String CASE_URGENCY_REASONS = " Case Urgency Reasons - ";
 
     public static void updateUrgencyElementsForCaseData(CaseData.CaseDataBuilder caseDataBuilder,
-                                                      C100RebuildUrgencyElements c100RebuildUrgencyElements) {
+                                                        C100RebuildUrgencyElements c100RebuildUrgencyElements) {
         caseDataBuilder
-                .isCaseUrgent(c100RebuildUrgencyElements.getUrgentHearingRequired())
-                .caseUrgencyTimeAndReason(buildCaseUrgencyTimeAndReason(c100RebuildUrgencyElements))
-                .areRespondentsAwareOfProceedings(c100RebuildUrgencyElements.getHearingWithNext48HrsDetails())
-                .effortsMadeWithRespondents(c100RebuildUrgencyElements.getHearingWithNext48HrsMsg());
+            .isCaseUrgent(c100RebuildUrgencyElements.getUrgentHearingRequired())
+            .caseUrgencyTimeAndReason(buildCaseUrgencyTimeAndReason(c100RebuildUrgencyElements))
+            .areRespondentsAwareOfProceedings(c100RebuildUrgencyElements.getHearingWithNext48HrsDetails())
+            .effortsMadeWithRespondents(c100RebuildUrgencyElements.getHearingWithNext48HrsMsg());
     }
 
     private static String buildCaseUrgencyTimeAndReason(C100RebuildUrgencyElements c100RebuildUrgencyElements) {
         List<String> riskList = nonNull(c100RebuildUrgencyElements.getReasonOfUrgentHearing())
-                ? Arrays.stream(c100RebuildUrgencyElements.getReasonOfUrgentHearing())
-                        .map(value -> value.replace(" ", ""))
-                        .collect(Collectors.toList()) : Collections.emptyList();
+            ? List.of(c100RebuildUrgencyElements.getReasonOfUrgentHearing()) : Collections.emptyList();
 
         List<String> riskList1 = riskList.stream().map(value -> UrgentHearingReasonEnum.valueOf(value)
-                .getDisplayedValue()).collect(Collectors.toList());
+            .getDisplayedValue()).collect(Collectors.toList());
 
         if (isNotEmpty(c100RebuildUrgencyElements.getOtherRiskDetails())) {
             riskList1.add(c100RebuildUrgencyElements.getOtherRiskDetails());
         }
         return CASE_URGENCY_TIME + c100RebuildUrgencyElements.getTimeOfHearingDetails()
-                + CASE_URGENCY_REASONS + String.join(COMMA_SEPARATOR, riskList1);
+            + CASE_URGENCY_REASONS + String.join(COMMA_SEPARATOR, riskList1);
     }
 }
