528596bb1a99cc403cd5d4a877b5636f9f9847d3
sahaSonali <sonali.saha@hmcts.net>
2022-12-06 12:14:19 +0000

    Changes for Serve Order

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/manageorders/serveorders/ServeOrderAdditionalDocument.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/manageorders/serveorders/ServeOrderAdditionalDocument.java
@@ -1,11 +1,13 @@
 package uk.gov.hmcts.reform.prl.models.complextypes.manageorders.serveorders;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.prl.models.documents.Document;
 
 @Data
 @Builder(toBuilder = true)
+@AllArgsConstructor
 public class ServeOrderAdditionalDocument {
 
     private final Document serveOrderDocument;
