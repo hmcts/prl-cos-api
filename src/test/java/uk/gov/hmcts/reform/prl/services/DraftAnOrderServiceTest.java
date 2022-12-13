d3f6f989fe89dab7605524e864d983a124a68bef
swaroopa86 <pendyalaswaroopa86@gmail.com>
2022-12-13 18:02:49 +0000

    test cases are added for DraftOrderService

--- src/test/java/uk/gov/hmcts/reform/prl/services/DraftAnOrderServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/DraftAnOrderServiceTest.java
@@ -9,14 +9,22 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.prl.enums.ChildArrangementOrderTypeEnum;
+import uk.gov.hmcts.reform.prl.enums.OrderTypeEnum;
+import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.enums.manageorders.ChildArrangementOrdersEnum;
 import uk.gov.hmcts.reform.prl.enums.manageorders.CreateSelectOrderOptionsEnum;
+import uk.gov.hmcts.reform.prl.enums.manageorders.JudgeOrMagistrateTitleEnum;
 import uk.gov.hmcts.reform.prl.enums.manageorders.ManageOrdersOptionsEnum;
 import uk.gov.hmcts.reform.prl.enums.manageorders.OrderRecipientsEnum;
+import uk.gov.hmcts.reform.prl.enums.manageorders.SelectTypeOfOrderEnum;
+import uk.gov.hmcts.reform.prl.enums.manageorders.YesNoNotRequiredEnum;
 import uk.gov.hmcts.reform.prl.models.Address;
+import uk.gov.hmcts.reform.prl.models.DraftOrder;
 import uk.gov.hmcts.reform.prl.models.Element;
 import uk.gov.hmcts.reform.prl.models.OrderDetails;
 import uk.gov.hmcts.reform.prl.models.Organisation;
+import uk.gov.hmcts.reform.prl.models.OtherDraftOrderDetails;
 import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicList;
 import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicListElement;
 import uk.gov.hmcts.reform.prl.models.complextypes.ApplicantChild;
@@ -24,6 +32,7 @@ import uk.gov.hmcts.reform.prl.models.complextypes.AppointedGuardianFullName;
 import uk.gov.hmcts.reform.prl.models.complextypes.Child;
 import uk.gov.hmcts.reform.prl.models.complextypes.ChildrenLiveAtAddress;
 import uk.gov.hmcts.reform.prl.models.complextypes.Home;
+import uk.gov.hmcts.reform.prl.models.complextypes.MagistrateLastName;
 import uk.gov.hmcts.reform.prl.models.complextypes.PartyDetails;
 import uk.gov.hmcts.reform.prl.models.complextypes.manageorders.FL404;
 import uk.gov.hmcts.reform.prl.models.documents.Document;
@@ -37,6 +46,7 @@ import uk.gov.hmcts.reform.prl.utils.ElementUtils;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -80,14 +90,21 @@ public class DraftAnOrderServiceTest {
     @Mock
     private DocumentLanguageService documentLanguageService;
 
+
     @Before
     public void setup() {
         DocumentLanguage documentLanguage = DocumentLanguage.builder().isGenEng(true).isGenWelsh(true).build();
         when(documentLanguageService.docGenerateLang(Mockito.any(CaseData.class))).thenReturn(documentLanguage);
+
+        generatedDocumentInfo = GeneratedDocumentInfo.builder()
+            .url("TestUrl")
+            .binaryUrl("binaryUrl")
+            .hashToken("testHashToken")
+            .build();
     }
 
     @Test
-    public void getUpdatedCaseDataCaTest() {
+    public void testGenerateDraftOrderCollection() {
         Child child = Child.builder()
             .firstName("Test")
             .lastName("Name")
@@ -101,6 +118,59 @@ public class DraftAnOrderServiceTest {
         Element<Child> wrappedChildren = Element.<Child>builder().value(child).build();
         List<Element<Child>> listOfChildren = Collections.singletonList(wrappedChildren);
 
+        MagistrateLastName magistrateLastName = MagistrateLastName.builder()
+            .lastName("Magistrate last")
+            .build();
+
+        Element<MagistrateLastName> magistrateLastNameElement = Element.<MagistrateLastName>builder().value(magistrateLastName).build();
+        List<Element<MagistrateLastName>> magistrateElementList = Collections.singletonList(magistrateLastNameElement);
+
+        List<OrderTypeEnum> orderType = new ArrayList<>();
+        orderType.add(OrderTypeEnum.childArrangementsOrder);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
+
+        DraftOrder draftOrder = DraftOrder.builder()
+            .typeOfOrder(SelectTypeOfOrderEnum.interim.getDisplayedValue())
+            .orderTypeId(CreateSelectOrderOptionsEnum.childArrangementsSpecificProhibitedOrder.getDisplayedValue())
+            .orderDocument(Document.builder()
+                               .documentUrl(generatedDocumentInfo.getUrl())
+                               .documentBinaryUrl(generatedDocumentInfo.getBinaryUrl())
+                               .documentHash(generatedDocumentInfo.getHashToken())
+                               .documentFileName("c100DraftFilename.pdf")
+                               .build())
+            .orderDocumentWelsh(Document.builder()
+                                    .documentUrl(generatedDocumentInfo.getUrl())
+                                    .documentBinaryUrl(generatedDocumentInfo.getBinaryUrl())
+                                    .documentHash(generatedDocumentInfo.getHashToken())
+                                    .documentFileName("c100DraftWelshFilename")
+                                    .build())
+            .otherDetails(OtherDraftOrderDetails.builder()
+                              .createdBy("test title")
+                              .dateCreated(LocalDateTime.parse(dtf.format(now)))
+                              .status("Draft").build())
+            .isTheOrderByConsent(YesOrNo.Yes)
+            .wasTheOrderApprovedAtHearing(YesOrNo.Yes)
+            .judgeOrMagistrateTitle(JudgeOrMagistrateTitleEnum.circuitJudge)
+            .judgeOrMagistratesLastName("judge last")
+            .justiceLegalAdviserFullName("Judge full")
+            .magistrateLastName(magistrateElementList)
+            .recitalsOrPreamble("test preamble")
+            .isTheOrderAboutAllChildren(YesNoNotRequiredEnum.yes)
+            .orderDirections("test order")
+            .furtherDirectionsIfRequired("test further order")
+            .childArrangementsOrdersToIssue(orderType)
+            .selectChildArrangementsOrder(ChildArrangementOrderTypeEnum.liveWithOrder)
+            .build();
+
+        Element<DraftOrder> draftOrderElement = Element.<DraftOrder>builder()
+            .id(UUID.randomUUID())
+            .value(draftOrder)
+            .build();
+        List<Element<DraftOrder>> draftOrderList = Collections.singletonList(draftOrderElement);
+
         CaseData caseData = CaseData.builder()
             .id(12345L)
             .caseTypeOfApplication("C100")
@@ -108,11 +178,43 @@ public class DraftAnOrderServiceTest {
             .familymanCaseNumber("familyman12345")
             .children(listOfChildren)
             .childArrangementOrders(ChildArrangementOrdersEnum.blankOrderOrDirections)
+            .createSelectOrderOptions(CreateSelectOrderOptionsEnum.childArrangementsSpecificProhibitedOrder)
+            .selectedOrder("test order")
+            .selectTypeOfOrder(SelectTypeOfOrderEnum.general)
+            .previewOrderDoc(Document.builder()
+                                 .documentUrl(generatedDocumentInfo.getUrl())
+                                 .documentBinaryUrl(generatedDocumentInfo.getBinaryUrl())
+                                 .documentHash(generatedDocumentInfo.getHashToken())
+                                 .documentFileName("draftSolicitorOrderFilename.pdf")
+                                 .build())
+            .previewOrderDocWelsh(Document.builder()
+                                      .documentUrl(generatedDocumentInfo.getUrl())
+                                      .documentBinaryUrl(generatedDocumentInfo.getBinaryUrl())
+                                      .documentHash(generatedDocumentInfo.getHashToken())
+                                      .documentFileName("draftWelshSolicitorFilename")
+                                      .build())
+            .manageOrders(ManageOrders.builder()
+                              .isTheOrderByConsent(YesOrNo.Yes)
+                              .recitalsOrPreamble("test recitals")
+                              .orderDirections("test orders")
+                              .furtherDirectionsIfRequired("test further directions")
+                              .judgeOrMagistrateTitle(JudgeOrMagistrateTitleEnum.circuitJudge)
+                              .childArrangementsOrdersToIssue(orderType)
+                              .selectChildArrangementsOrder(ChildArrangementOrderTypeEnum.liveWithOrder)
+                              .build())
+            .judgeOrMagistratesLastName("judge last")
+            .justiceLegalAdviserFullName("Judge full")
+            .magistrateLastName(magistrateElementList)
+            .isTheOrderAboutAllChildren(YesNoNotRequiredEnum.yes)
+            .wasTheOrderApprovedAtHearing(YesOrNo.No)
+            .draftOrderCollection(draftOrderList)
             .build();
 
-        CaseData caseData1 = manageOrderService.getUpdatedCaseData(caseData);
-        assertEquals("Child 1: Test Name\n", caseData1.getChildrenList());
-        assertNotNull(caseData1.getSelectedOrder());
+        when(dateTime.now()).thenReturn(LocalDateTime.now());
+
+        Map<String, Object> stringObjectMap = new HashMap<>();
+        stringObjectMap = draftAnOrderService.generateDraftOrderCollection(caseData);
+        assertNotNull(stringObjectMap.get("draftOrderCollection"));
     }
 
     @Test
