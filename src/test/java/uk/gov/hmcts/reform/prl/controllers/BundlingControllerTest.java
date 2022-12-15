61aa335211aeca1ac9d6d6f87a19ed1aad0ec6eb
GayatriDeviCh <GayatriDevi.Chadaram@hmcts.net>
2022-12-15 10:53:35 +0000

    CheckStyle Issue Fix

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/BundlingControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/BundlingControllerTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
