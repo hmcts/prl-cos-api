9f325c4ab3b6f32c701384ad3f1e0a8a7339628b
Khurshed Khan <khurshed.khan@hmcts.net>
2022-12-21 10:50:54 +0000

    PRL-2810:Added backend changes ford solicitor draft order

--- src/main/java/uk/gov/hmcts/reform/prl/enums/serveorder/CafcassCymruDocumentsEnum.java
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/serveorder/CafcassCymruDocumentsEnum.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import uk.gov.hmcts.reform.prl.enums.CustomEnumSerializer;
 
