ab0255dc3d125fe0e1ef675a0e5938b7129dbe98
Aloknath Datta <alokdatta.uit@gmail.com>
2022-12-22 13:02:35 +0000

    finding solicitor role

--- src/main/java/uk/gov/hmcts/reform/prl/models/caseaccess/FindUserCaseRolesResponse.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/caseaccess/FindUserCaseRolesResponse.java
@@ -1,15 +1,17 @@
 package uk.gov.hmcts.reform.prl.models.caseaccess;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Builder
 @Getter
-@EqualsAndHashCode
+@Data
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FindUserCaseRolesResponse {
     @JsonProperty("case_users")
     private List<CaseUser> caseUsers;
