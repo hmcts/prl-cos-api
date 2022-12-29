949f7839dba6cf0c50476236527b0ba537e1fe2f
Khurshed Khan <khurshed.khan@hmcts.net>
2022-12-29 22:26:45 +0000

    PRL-2494:Changes for proceedings order

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/response/proceedings/OtherProceedingDetails.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/response/proceedings/OtherProceedingDetails.java
@@ -1,11 +1,14 @@
 package uk.gov.hmcts.reform.prl.models.complextypes.citizen.response.proceedings;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.models.c100rebuild.Document;
 import uk.gov.hmcts.reform.prl.models.c100rebuild.OrderDate;
+
+import java.time.LocalDate;
 
 @Data
 @Builder(toBuilder = true)
@@ -14,9 +17,15 @@ public class OtherProceedingDetails {
     @JsonProperty("caseNo")
     private final String caseNumber;
     private final OrderDate orderDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private final LocalDate proceedingOrderDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private final LocalDate proceedingOrderEndDate;
     @JsonProperty("currentOrder")
     private final YesOrNo isCurrentOrder;
     private final OrderDate orderEndDate;
     private final YesOrNo orderCopy;
     private final Document orderDocument;
+    private final uk.gov.hmcts.reform.prl.models.documents.Document proceedingOrderDocument;
+
 }
--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/response/proceedings/Proceedings.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/response/proceedings/Proceedings.java
@@ -2,13 +2,48 @@ package uk.gov.hmcts.reform.prl.models.complextypes.citizen.response.proceedings
 
 import lombok.Builder;
 import lombok.Data;
-import uk.gov.hmcts.reform.prl.enums.TypeOfOrderEnum;
+import uk.gov.hmcts.reform.prl.models.c100rebuild.OrderDate;
+import uk.gov.hmcts.reform.prl.models.documents.Document;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
 
 @Data
 @Builder(toBuilder = true)
 public class Proceedings {
-    private final TypeOfOrderEnum orderType;
+    private final String orderType;
     private final List<OtherProceedingDetails> proceedingDetails;
+
+    public List<OtherProceedingDetails> getProceedingDetails() {
+        for (OtherProceedingDetails proceedingDetails: proceedingDetails
+             ) {
+            proceedingDetails.toBuilder()
+                .proceedingOrderDate(buildDate(proceedingDetails.getOrderDate()))
+                .proceedingOrderEndDate(buildDate(proceedingDetails.getOrderEndDate()))
+                .proceedingOrderDocument(buildDocument(proceedingDetails.getOrderDocument()))
+                .build();
+        }
+        return proceedingDetails;
+    }
+
+    private static LocalDate buildDate(OrderDate date) {
+        if (isNotEmpty(date.getYear()) &&  isNotEmpty(date.getMonth()) && isNotEmpty(date.getDay())) {
+            return LocalDate.of(Integer.parseInt(date.getYear()), Integer.parseInt(date.getMonth()),
+                                Integer.parseInt(date.getDay()));
+        }
+        return null;
+    }
+
+    public static Document buildDocument(uk.gov.hmcts.reform.prl.models.c100rebuild.Document orderDocument) {
+        if (isNotEmpty(orderDocument)) {
+            return Document.builder()
+                .documentUrl(orderDocument.getUrl())
+                .documentBinaryUrl(orderDocument.getBinaryUrl())
+                .documentFileName(orderDocument.getFilename())
+                .build();
+        }
+        return null;
+    }
 }
