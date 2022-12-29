fd5b5d95fd4aedf3e98314f4d3f67c5e9f647161
Khurshed Khan <khurshed.khan@hmcts.net>
2022-12-29 18:00:32 +0000

    PRL-2494:Changes for proceedings order

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/response/proceedings/OtherProceedingDetails.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/response/proceedings/OtherProceedingDetails.java
@@ -1,13 +1,11 @@
 package uk.gov.hmcts.reform.prl.models.complextypes.citizen.response.proceedings;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.prl.enums.YesOrNo;
+import uk.gov.hmcts.reform.prl.models.c100rebuild.OrderDate;
 import uk.gov.hmcts.reform.prl.models.documents.Document;
-
-import java.time.LocalDate;
 
 @Data
 @Builder(toBuilder = true)
@@ -15,12 +13,10 @@ public class OtherProceedingDetails {
     private final String orderDetail;
     @JsonProperty("caseNo")
     private final String caseNumber;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private final LocalDate orderDate;
+    private final OrderDate orderDate;
     @JsonProperty("currentOrder")
     private final YesOrNo isCurrentOrder;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private final LocalDate orderEndDate;
+    private final OrderDate orderEndDate;
     private final YesOrNo orderCopy;
     private final Document orderDocument;
 }
