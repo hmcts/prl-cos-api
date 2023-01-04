35d38b3fd90020a20c55cc3407714ff4a385152b
Khurshed Khan <khurshed.khan@hmcts.net>
2023-01-04 12:42:03 +0000

    PRL-2494:Changes for proceedings order

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/response/proceedings/CurrentOrPreviousProceedings.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/response/proceedings/CurrentOrPreviousProceedings.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.prl.enums.YesOrNo;
+import uk.gov.hmcts.reform.prl.models.Element;
 
 import java.util.List;
 
@@ -15,5 +16,5 @@ public class CurrentOrPreviousProceedings {
     private final YesOrNo haveChildrenBeenInvolvedInCourtCase;
     private final YesOrNo courtOrderMadeForProtection;
     @JsonProperty("proceedingsList")
-    private List<Proceedings> proceedingsList;
+    private List<Element<Proceedings>> proceedingsList;
 }
