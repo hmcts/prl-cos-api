0a40b7b2589567df13799b5beaa4c29970420de1
Malik <hamza.malik@hmcts.net>
2023-01-05 13:53:04 +0000

    changing instances of CASE_HEARING to PREPARE_FOR_HEARING_CONDUCT_HEARING

--- src/test/java/uk/gov/hmcts/reform/prl/services/ServiceOfApplicationEmailServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/ServiceOfApplicationEmailServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.prl.enums.YesNoDontKnow;
+import uk.gov.hmcts.reform.prl.models.complextypes.OtherPersonRelationshipToChild;
 import uk.gov.hmcts.reform.prl.models.complextypes.PartyDetails;
 import uk.gov.hmcts.reform.prl.models.dto.GeneratedDocumentInfo;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
@@ -55,13 +56,20 @@ public class ServiceOfApplicationEmailServiceTest {
                                             .solicitorEmail("test@gmail.com")
                                             .representativeLastName("LastName")
                                             .representativeFirstName("FirstName")
+                                            .firstName("test")
+                                            .lastName("test")
+                                            .email("test@gmail.com")
                                             .build())))
             .respondents(List.of(element(PartyDetails.builder()
-                                            .solicitorEmail("test@gmail.com")
-                                            .representativeLastName("LastName")
-                                            .representativeFirstName("FirstName")
+                                             .solicitorEmail("test@gmail.com")
+                                             .representativeLastName("LastName")
+                                             .representativeFirstName("FirstName")
+                                             .firstName("test")
+                                             .lastName("test")
+                                             .email("test@gmail.com")
                                              .doTheyHaveLegalRepresentation(YesNoDontKnow.yes)
-                                            .build())))
+                                             .build())))
+
             .build();
         CaseDetails caseDetails = CaseDetails.builder().build();
         when(emailService.getCaseData(Mockito.any(CaseDetails.class))).thenReturn(caseData);
