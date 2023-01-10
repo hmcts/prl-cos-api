f689767e5496f54e7dd9d66edacd394c439ec6c0
swaroopa86 <pendyalaswaroopa86@gmail.com>
2023-01-10 11:25:13 +0000

    merge issues resolved

--- src/main/java/uk/gov/hmcts/reform/prl/services/DraftAnOrderService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/DraftAnOrderService.java
@@ -5,9 +5,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.prl.constants.PrlAppsConstants;
 import uk.gov.hmcts.reform.prl.enums.YesNoDontKnow;
+import uk.gov.hmcts.reform.prl.enums.dio.DioCafcassOrCymruEnum;
+import uk.gov.hmcts.reform.prl.enums.dio.DioHearingsAndNextStepsEnum;
+import uk.gov.hmcts.reform.prl.enums.dio.DioOtherEnum;
+import uk.gov.hmcts.reform.prl.enums.dio.DioPreamblesEnum;
 import uk.gov.hmcts.reform.prl.enums.manageorders.OrderRecipientsEnum;
 import uk.gov.hmcts.reform.prl.enums.sdo.SdoCourtEnum;
 import uk.gov.hmcts.reform.prl.enums.sdo.SdoDocumentationAndEvidenceEnum;
@@ -646,4 +651,123 @@ public class DraftAnOrderService {
         List<DynamicListElement> courtList = locationRefDataService.getCourtLocations(authorisation);
         return courtList;
     }
+
+    public static boolean checkDirectionOnIssueOptionsSelected(CaseData caseData) {
+        return !(caseData.getDirectionOnIssue().getDioPreamblesList().isEmpty()
+            && caseData.getDirectionOnIssue().getDioHearingsAndNextStepsList().isEmpty()
+            && caseData.getDirectionOnIssue().getDioCafcassOrCymruList().isEmpty()
+            && caseData.getDirectionOnIssue().getDioLocalAuthorityList().isEmpty()
+            && caseData.getDirectionOnIssue().getDioCourtList().isEmpty()
+            && caseData.getDirectionOnIssue().getDioOtherList().isEmpty());
+    }
+    public void populateDirectionOnIssueFields(String authorisation, CaseData caseData, Map<String, Object> caseDataUpdated) {
+
+        if (!caseData.getDirectionOnIssue().getDioPreamblesList().isEmpty()
+            && caseData.getDirectionOnIssue().getDioPreamblesList().contains(DioPreamblesEnum.rightToAskCourt)) {
+            caseDataUpdated.put(
+                "dioRightToAskCourt",
+                "As the direction has been made without hearing may ask the court to reconsider this order. "
+                    + "You must do that within seven days of receiving the order by writing to the court"
+                    + "(and notifying any other party) and asking the court to reconsider. "
+                    + "Alternatively, the court may reconsider the directions at the first hearing"
+            );
+        }
+        if (!caseData.getDirectionOnIssue().getDioHearingsAndNextStepsList().isEmpty()
+            && caseData.getDirectionOnIssue().getDioHearingsAndNextStepsList().contains(
+            DioHearingsAndNextStepsEnum.permissionHearing)) {
+            caseDataUpdated.put(
+                "dioCaseReviewAtSecondGateKeeping",
+                "As these directions have been made without a hearing you may ask the court to reconsider this order"
+                    + ".  You must do that within seven days of receiving this order by writing to the court"
+                    + " (and notifying any other party) and asking the court to reconsider.  Alternatively, "
+                    + "the court may reconsider the directions at the first hearing"
+            );
+        }
+        if (!caseData.getDirectionOnIssue().getDioHearingsAndNextStepsList().isEmpty()
+            && caseData.getDirectionOnIssue().getDioHearingsAndNextStepsList().contains(
+            DioHearingsAndNextStepsEnum.updateContactDetails)) {
+            caseDataUpdated.put(
+                "dioUpdateContactDetails",
+                "The parties must, if their contact details have changed or are missing from "
+                    + "the application, contact CAFCASS or CAFCASS Cymru quoting the case number "
+                    + "at  [CafcassCymruCAT@gov.wales / privatelawapplications@cafcass.gov.uk] "
+                    + "The email must include telephone contact details and email address so that "
+                    + "they may be contacted for safeguarding purposes. Alternatively if any party "
+                    + "is managing their case using the online dashboard, they can update their "
+                    + "contact details on the dashboard and do not have to also contact CAFCASS or CAFCASS Cymru."
+            );
+        }
+        if (!caseData.getDirectionOnIssue().getDioCafcassOrCymruList().isEmpty()
+            && caseData.getDirectionOnIssue().getDioCafcassOrCymruList().contains(
+            DioCafcassOrCymruEnum.cafcassSafeguarding)) {
+            caseDataUpdated.put(
+                "dioCafcassSafeguardingIssue",
+                "The Court directs that Cafcass shall carry out safeguarding checks in relation "
+                    + "to the child(ren) and parties and file a safeguarding letter within "
+                    + "[number of working days)/by(insert date or time period]  of receipt of this order.  "
+                    + "A copy of the letter shall also be served on the parties unless, in the opinion "
+                    + "of the letter's author, this would create a risk of harm to a party or child."
+                    + "A copy of this order to be served on CAFCASS. The court has asked Cafcass "
+                    + "to undertake some enquires to see if there are risk issues about a child "
+                    + "that the court needs to consider when making any decisions.  "
+                    + "Cafcass will contact you by telephone to discuss your child's circumstances "
+                    + "and talk about any such risk issues, before writing a short safeguarding letter "
+                    + "to the court to provide further advice.  Cafcass can advise on risk issues only "
+                    + "at this stage and so may not be able to discuss all aspects of your case, "
+                    + "and they won't be able to talk to your child(ren) at this stage.\n"
+                    + "More information about Cafcass and the work they do can be found on their website:\n"
+                    + "www.cafcass.gov.uk"
+            );
+        }
+        if (!caseData.getDirectionOnIssue().getDioCafcassOrCymruList().isEmpty()
+            && caseData.getDirectionOnIssue().getDioCafcassOrCymruList().contains(
+            DioCafcassOrCymruEnum.cafcassCymruSafeguarding)) {
+            caseDataUpdated.put(
+                "dioCafcassCymruSafeguardingIssue",
+                "The Court directs that Cafcass Cymru shall carry out safeguarding checks in "
+                    + "relation to the child(ren) and parties and file a safeguarding letter within "
+                    + "[number working days})/by(insert date or time period] of receipt of this order.  "
+                    + "A copy of the letter shall also be served on the parties unless, in the opinion "
+                    + "of the letter's author, this would create a risk of harm to a party or child.\n"
+                    + "A copy of this order to be served on CAFCASS The court has asked Cafcass to "
+                    + "undertake some enquires to see if there are risk issues about a child that "
+                    + "the court needs to consider when making any decisions.  Cafcass will contact you "
+                    + "by telephone to discuss your child's circumstances and talk about any such risk issues, "
+                    + "before writing a short safeguarding letter to the court to provide further advice.  "
+                    + "Cafcass can advise on risk issues only at this stage and so may not be able to "
+                    + "discuss all aspects of your case, and they won't be able to talk to your child(ren) at this stage.\n"
+                    + "More information about Cafcass and the work they do can be found on their website:\n"
+                    + "www.cafcass.gov.uk"
+
+            );
+        }
+        if (!caseData.getDirectionOnIssue().getDioOtherList().isEmpty()
+            && caseData.getDirectionOnIssue().getDioOtherList().contains(
+            DioOtherEnum.parentWithCare)) {
+            caseDataUpdated.put(
+                "dioParentWithCare",
+                "The application is transferred to the Family Court at [place].The reason for transfer "
+                    + "is another court is in the area where the child usually lives / there are on-going "
+                    + "proceedings in another court / free-text box reason "
+
+            );
+        }
+
+        List<DynamicListElement> courtList = getCourtDynamicList(authorisation);
+        populateDioCourtDynamicList(courtList, caseDataUpdated);
+
+        log.info("Case data updated map {}", caseDataUpdated);
+    }
+
+    private void populateDioCourtDynamicList(List<DynamicListElement> courtList, Map<String, Object> caseDataUpdated) {
+        DynamicList courtDynamicList =  DynamicList.builder().value(DynamicListElement.EMPTY).listItems(courtList)
+            .build();
+        caseDataUpdated.put(
+            "dioFhdraCourtDynamicList", courtDynamicList);
+        caseDataUpdated.put(
+            "dioPermissionHearingCourtDynamicList", courtDynamicList);
+        caseDataUpdated.put(
+            "dioTransferApplicationCourtDynamicList", courtDynamicList);
+    }
+
 }
