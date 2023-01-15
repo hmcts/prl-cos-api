65cf728c45ca44cbb17d268101eb2910800298ec
sahaSonali <sonali.saha@hmcts.net>
2023-01-15 21:13:45 +0000

    add unit test case & functional test case for draft order

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/DraftOrdersControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/DraftOrdersControllerFunctionalTest.java
@@ -1,16 +1,29 @@
 package uk.gov.hmcts.reform.prl.controllers;
 
-import io.restassured.RestAssured;
-import io.restassured.specification.RequestSpecification;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 import uk.gov.hmcts.reform.prl.ResourceLoader;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+import uk.gov.hmcts.reform.prl.services.ManageOrderService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.FL401_CASE_TYPE;
 
 @Slf4j
 @SpringBootTest
@@ -19,63 +32,102 @@ import uk.gov.hmcts.reform.prl.ResourceLoader;
 public class DraftOrdersControllerFunctionalTest {
 
     private final String userToken = "Bearer testToken";
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @MockBean
+    private ManageOrderService manageOrderService;
+
+
 
     private static final String VALID_REQUEST_BODY = "requests/call-back-controller.json";
-    private static final String VALID_MANAGE_ORDER_REQUEST_BODY = "requests/manage-order-fetch-children-request.json";
+    private static final String VALID_DRAFT_ORDER_REQUEST_BODY = "requests/draft-order-sdo-with-options-request.json";
 
 
-    private final String targetInstance =
-        StringUtils.defaultIfBlank(
-            System.getenv("TEST_URL"),
-            "http://localhost:4044"
-        );
-
-    private final RequestSpecification request = RestAssured.given().relaxedHTTPSValidation().baseUri(targetInstance);
+    @Before
+    public void setUp() {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
 
     @Test
     public void givenRequestBody_whenReset_fields_then200Response() throws Exception {
         String requestBody = ResourceLoader.loadJson(VALID_REQUEST_BODY);
-        request
-            .header("Authorization", userToken)
-            .body(requestBody)
-            .when()
-            .contentType("application/json")
-            .post("/reset-fields")
-            .then().assertThat().statusCode(200);
+        mockMvc.perform(post("/reset-fields")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .header("Authorization", "auth")
+                            .content(requestBody)
+                            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
     }
 
     @Test
     public void givenRequestBody_whenSelected_order_then200Response() throws Exception {
-        String requestBody = ResourceLoader.loadJson(VALID_MANAGE_ORDER_REQUEST_BODY);
-        request
-            .body(requestBody)
-            .when()
-            .contentType("application/json")
-            .post("/selected-order")
-            .then().assertThat().statusCode(200);
+        String requestBody = ResourceLoader.loadJson(VALID_DRAFT_ORDER_REQUEST_BODY);
+        mockMvc.perform(post("/selected-order")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .header("Authorization", "auth")
+                            .content(requestBody)
+                            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
     }
 
     @Test
-    @Ignore
     public void givenRequestBody_whenPopulate_draft_order_fields_then200Response() throws Exception {
-        String requestBody = ResourceLoader.loadJson(VALID_MANAGE_ORDER_REQUEST_BODY);
-        request
-            .body(requestBody)
-            .when()
-            .contentType("application/json")
-            .post("/populate-draft-order-fields")
-            .then().assertThat().statusCode(200);
+        String requestBody = ResourceLoader.loadJson(VALID_DRAFT_ORDER_REQUEST_BODY);
+        CaseData caseData = CaseData.builder()
+            .caseTypeOfApplication(FL401_CASE_TYPE)
+            .build();
+        when(manageOrderService.populateCustomOrderFields(caseData)).thenReturn(caseData);
+        mockMvc.perform(post("/populate-draft-order-fields")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .header("Authorization", "auth")
+                            .content(requestBody)
+                            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
     }
 
     @Test
     public void givenRequestBody_whenPopulate_standard_direction_order_fields() throws Exception {
-        String requestBody = ResourceLoader.loadJson(VALID_MANAGE_ORDER_REQUEST_BODY);
+        String requestBody = ResourceLoader.loadJson(VALID_DRAFT_ORDER_REQUEST_BODY);
+        mockMvc.perform(post("/populate-standard-direction-order-fields")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .header("Authorization", "auth")
+                            .content(requestBody)
+                            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
+    }
 
-        request.header("Authorization", userToken)
-            .body(requestBody)
-            .when()
-            .contentType("application/json")
-            .post("/populate-standard-direction-order-fields")
-            .then().assertThat().statusCode(500);
+    @Test
+    public void givenRequestBody_whenAbout_to_submit() throws Exception {
+        String requestBody = ResourceLoader.loadJson(VALID_DRAFT_ORDER_REQUEST_BODY);
+        mockMvc.perform(post("/about-to-submit")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .header("Authorization", "auth")
+                            .content(requestBody)
+                            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
+    }
+
+    @Test
+    public void givenRequestBody_whenGenerate_doc() throws Exception {
+        String requestBody = ResourceLoader.loadJson(VALID_DRAFT_ORDER_REQUEST_BODY);
+        CaseData caseData = CaseData.builder()
+            .caseTypeOfApplication(FL401_CASE_TYPE)
+            .build();
+        Map<String, Object> caseDataMap = new HashMap<>();
+        when(manageOrderService.getCaseData("test", caseData)).thenReturn(caseDataMap);
+
+        mockMvc.perform(post("/generate-doc")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .header("Authorization", "auth")
+                            .content(requestBody)
+                            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
     }
 }
