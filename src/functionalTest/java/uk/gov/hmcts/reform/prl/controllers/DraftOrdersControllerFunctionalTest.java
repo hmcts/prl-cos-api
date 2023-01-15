d7491762f4fda9244b071c2635dbf4b4fe7dd0b7
sahaSonali <sonali.saha@hmcts.net>
2023-01-15 00:05:24 +0000

    initial check-in

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/DraftOrdersControllerFunctionalTest.java
@@ -0,0 +1,79 @@
+package uk.gov.hmcts.reform.prl.controllers;
+
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.reform.prl.ResourceLoader;
+
+@Slf4j
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+public class DraftOrdersControllerFunctionalTest {
+
+    private final String userToken = "Bearer testToken";
+
+    private static final String VALID_REQUEST_BODY = "requests/call-back-controller.json";
+    private static final String VALID_MANAGE_ORDER_REQUEST_BODY = "requests/manage-order-fetch-children-request.json";
+
+
+    private final String targetInstance =
+        StringUtils.defaultIfBlank(
+            System.getenv("TEST_URL"),
+            "http://localhost:4044"
+        );
+
+    private final RequestSpecification request = RestAssured.given().relaxedHTTPSValidation().baseUri(targetInstance);
+
+    @Test
+    public void givenRequestBody_whenReset_fields_then200Response() throws Exception {
+        String requestBody = ResourceLoader.loadJson(VALID_REQUEST_BODY);
+        request
+            .header("Authorization", userToken)
+            .body(requestBody)
+            .when()
+            .contentType("application/json")
+            .post("/reset-fields")
+            .then().assertThat().statusCode(200);
+    }
+
+    @Test
+    public void givenRequestBody_whenSelected_order_then200Response() throws Exception {
+        String requestBody = ResourceLoader.loadJson(VALID_MANAGE_ORDER_REQUEST_BODY);
+        request
+            .body(requestBody)
+            .when()
+            .contentType("application/json")
+            .post("/selected-order")
+            .then().assertThat().statusCode(200);
+    }
+
+    @Test
+    public void givenRequestBody_whenPopulate_draft_order_fields_then200Response() throws Exception {
+        String requestBody = ResourceLoader.loadJson(VALID_MANAGE_ORDER_REQUEST_BODY);
+        request
+            .body(requestBody)
+            .when()
+            .contentType("application/json")
+            .post("/populate-draft-order-fields")
+            .then().assertThat().statusCode(200);
+    }
+
+    @Test
+    public void givenRequestBody_whenPopulate_standard_direction_order_fields() throws Exception {
+        String requestBody = ResourceLoader.loadJson(VALID_MANAGE_ORDER_REQUEST_BODY);
+
+        request.header("Authorization", userToken)
+            .body(requestBody)
+            .when()
+            .contentType("application/json")
+            .post("/populate-standard-direction-order-fields")
+            .then().assertThat().statusCode(500);
+    }
+}
