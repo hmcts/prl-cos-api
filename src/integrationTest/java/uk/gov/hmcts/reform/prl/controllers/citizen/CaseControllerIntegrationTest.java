e88a284483c9724e65628a89dabb68db1c9a0c59
sahaSonali <sonali.saha@hmcts.net>
2023-01-19 13:04:21 +0000

    Added integration test CaseControllerIntegrationTest

--- src/integrationTest/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseControllerIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseControllerIntegrationTest.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.prl.controllers.citizen;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -30,6 +31,10 @@ public class CaseControllerIntegrationTest {
 
     private final String createCaseEndpoint = "/case/create";
 
+    private final String getCaseEndpoint = "/1234567";
+
+    private final String updateCaseEndpoint = "1234567/{eventId}/update-case";
+
     private final String validBody = "controller/valid-casedata-input.json";
 
     @Autowired
@@ -43,6 +48,31 @@ public class CaseControllerIntegrationTest {
 
     @Test
     public void testCreateCaseEndpoint() throws Exception {
+        String requestBody = ResourceLoader.loadJson(validBody);
+        HttpPost httpPost = new HttpPost(serviceUrl + createCaseEndpoint);
+        httpPost.addHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+        httpPost.addHeader("serviceAuthorization", serviceAuthenticationGenerator.generate());
+        httpPost.addHeader(AUTHORIZATION, idamTokenGenerator.generateIdamTokenForSystem());
+        StringEntity body = new StringEntity(requestBody);
+        httpPost.setEntity(body);
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(httpPost);
+        assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode());
+    }
+
+    @Test
+    public void testGetCaseEndpoint() throws Exception {
+        HttpGet httpGet = new HttpGet(serviceUrl + updateCaseEndpoint);
+        httpGet.addHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+        httpGet.addHeader("serviceAuthorization", serviceAuthenticationGenerator.generate());
+        httpGet.addHeader(AUTHORIZATION, idamTokenGenerator.generateIdamTokenForSystem());
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(httpGet);
+        assertEquals(
+            HttpStatus.SC_NOT_FOUND,
+            httpResponse.getStatusLine().getStatusCode());
+    }
+
+    @Test
+    public void testUpdateCaseEndpoint() throws Exception {
         String requestBody = ResourceLoader.loadJson(validBody);
         HttpPost httpPost = new HttpPost(serviceUrl + createCaseEndpoint);
         httpPost.addHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE);
