12d3621e0d65659aec8c34b590beb5c2e19626cf
JamesCutts00 <James.Cutts@HMCTS.ORG>
2023-01-19 16:59:48 +0000

    Completed test cases for respondentsolicitorservice

---
+++ src/test/java/uk/gov/hmcts/reform/prl/services/c100respondentsolicitor/C100RespondentSolicitorServiceTest.java
@@ -0,0 +1,201 @@
+package uk.gov.hmcts.reform.prl.services.c100respondentsolicitor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.prl.enums.Gender;
+import uk.gov.hmcts.reform.prl.enums.YesNoDontKnow;
+import uk.gov.hmcts.reform.prl.enums.citizen.ConfidentialityListEnum;
+import uk.gov.hmcts.reform.prl.models.Address;
+import uk.gov.hmcts.reform.prl.models.Element;
+import uk.gov.hmcts.reform.prl.models.Organisation;
+import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicList;
+import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicListElement;
+import uk.gov.hmcts.reform.prl.models.complextypes.PartyDetails;
+import uk.gov.hmcts.reform.prl.models.complextypes.citizen.response.confidentiality.KeepDetailsPrivate;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertNotNull;
+import static uk.gov.hmcts.reform.prl.enums.YesOrNo.Yes;
+
+public class C100RespondentSolicitorServiceTest {
+
+    @Mock
+    C100RespondentSolicitorService respondentSolicitorService;
+
+    CaseData caseData;
+
+    public static final String authToken = "Bearer TestAuthToken";
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        List<ConfidentialityListEnum> confidentialityListEnums = new ArrayList<>();
+
+        confidentialityListEnums.add(ConfidentialityListEnum.email);
+        confidentialityListEnums.add(ConfidentialityListEnum.phoneNumber);
+
+        PartyDetails respondent = PartyDetails.builder().representativeFirstName("Abc")
+            .representativeLastName("Xyz")
+            .gender(Gender.male)
+            .email("abc@xyz.com")
+            .phoneNumber("1234567890")
+            .canYouProvideEmailAddress(Yes)
+            .isEmailAddressConfidential(Yes)
+            .isPhoneNumberConfidential(Yes)
+            .solicitorOrg(Organisation.builder().organisationID("ABC").organisationName("XYZ").build())
+            .solicitorAddress(Address.builder().addressLine1("ABC").postCode("AB1 2MN").build())
+            .doTheyHaveLegalRepresentation(YesNoDontKnow.yes)
+            .build();
+
+        Element<PartyDetails> wrappedRespondents = Element.<PartyDetails>builder().value(respondent).build();
+        List<Element<PartyDetails>> respondentList = Collections.singletonList(wrappedRespondents);
+
+        DynamicListElement dynamicListElement = DynamicListElement.builder().code(String.valueOf(0)).build();
+        DynamicList chooseRespondent = DynamicList.builder().value(dynamicListElement).build();
+
+        caseData = CaseData.builder().respondents(respondentList)
+            .chooseRespondentDynamicList(chooseRespondent)
+            .keepContactDetailsPrivateOther(KeepDetailsPrivate.builder()
+                                           .confidentiality(Yes)
+                                           .confidentialityList(confidentialityListEnums)
+                                           .build())
+            .build();
+    }
+
+    @Test
+    public void populateAboutToStartCaseDataTest() {
+
+        List<String> errorList = new ArrayList<>();
+        Map<String, Object> stringObjectMap = caseData.toMap(new ObjectMapper());
+
+        CallbackRequest callbackRequest = uk.gov.hmcts.reform.ccd.client.model
+            .CallbackRequest.builder()
+            .caseDetails(uk.gov.hmcts.reform.ccd.client.model.CaseDetails.builder()
+                             .id(123L)
+                             .data(stringObjectMap)
+                             .build())
+            .build();
+
+        Map<String, Object> response = respondentSolicitorService.populateAboutToStartCaseData(
+            callbackRequest, authToken, errorList
+        );
+
+        assertNotNull(response);
+    }
+
+    @Test
+    public void populateAboutToSubmitCaseDataTest() {
+
+        List<String> errorList = new ArrayList<>();
+
+        Map<String, Object> stringObjectMap = caseData.toMap(new ObjectMapper());
+
+        CallbackRequest callbackRequest = uk.gov.hmcts.reform.ccd.client.model
+            .CallbackRequest.builder()
+            .caseDetails(uk.gov.hmcts.reform.ccd.client.model.CaseDetails.builder()
+                             .id(123L)
+                             .data(stringObjectMap)
+                             .build())
+            .build();
+
+        Map<String, Object> response = respondentSolicitorService.populateAboutToSubmitCaseData(
+            callbackRequest, authToken, errorList
+        );
+
+        assertNotNull(response);
+    }
+
+    @Test
+    public void populateSolicitorRespondentListTest() {
+
+        Map<String, Object> stringObjectMap = caseData.toMap(new ObjectMapper());
+
+        CallbackRequest callbackRequest = uk.gov.hmcts.reform.ccd.client.model
+            .CallbackRequest.builder()
+            .caseDetails(uk.gov.hmcts.reform.ccd.client.model.CaseDetails.builder()
+                             .id(123L)
+                             .data(stringObjectMap)
+                             .build())
+            .build();
+
+        Map<String, Object> response = respondentSolicitorService.populateSolicitorRespondentList(
+            callbackRequest, authToken
+        );
+
+        assertNotNull(response);
+
+    }
+
+    @Test
+    public void updateActiveRespondentSelectionBySolicitor() {
+        Map<String, Object> stringObjectMap = caseData.toMap(new ObjectMapper());
+
+        CallbackRequest callbackRequest = uk.gov.hmcts.reform.ccd.client.model
+            .CallbackRequest.builder()
+            .caseDetails(uk.gov.hmcts.reform.ccd.client.model.CaseDetails.builder()
+                             .id(123L)
+                             .data(stringObjectMap)
+                             .build())
+            .build();
+
+        Map<String, Object> response = respondentSolicitorService.updateActiveRespondentSelectionBySolicitor(
+            callbackRequest, authToken
+        );
+
+        assertNotNull(response);
+    }
+
+    @Test
+    public void generateConfidentialityDynamicSelectionDisplayTest() {
+        Map<String, Object> stringObjectMap = caseData.toMap(new ObjectMapper());
+
+        CallbackRequest callbackRequest = uk.gov.hmcts.reform.ccd.client.model
+            .CallbackRequest.builder()
+            .caseDetails(uk.gov.hmcts.reform.ccd.client.model.CaseDetails.builder()
+                             .id(123L)
+                             .data(stringObjectMap)
+                             .build())
+            .build();
+
+        Map<String, Object> response = respondentSolicitorService.generateConfidentialityDynamicSelectionDisplay(
+            callbackRequest
+        );
+
+        assertNotNull(response);
+    }
+
+    @Test
+    public void validateActiveRespondentResponse() {
+
+        List<String> errorList = new ArrayList<>();
+
+        Map<String, Object> stringObjectMap = caseData.toMap(new ObjectMapper());
+
+        CallbackRequest callbackRequest = uk.gov.hmcts.reform.ccd.client.model
+            .CallbackRequest.builder()
+            .caseDetails(uk.gov.hmcts.reform.ccd.client.model.CaseDetails.builder()
+                             .id(123L)
+                             .data(stringObjectMap)
+                             .build())
+            .build();
+
+        Map<String, Object> response = respondentSolicitorService.validateActiveRespondentResponse(
+            callbackRequest, authToken, errorList
+        );
+
+        assertNotNull(response);
+
+    }
+
+}
