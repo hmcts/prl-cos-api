0bccc3bde09c4ee32b458683c12cc1ea733f7f5b
sahaSonali <sonali.saha@hmcts.net>
2023-01-21 18:35:18 +0000

    Added CitizenCallbackControllerFunctionalTest

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/citizen/CitizenCallbackControllerFunctionalTest.java
@@ -0,0 +1,66 @@
+package uk.gov.hmcts.reform.prl.controllers.citizen;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
+import uk.gov.hmcts.reform.prl.ResourceLoader;
+import uk.gov.hmcts.reform.prl.services.SystemUserService;
+import uk.gov.hmcts.reform.prl.services.citizen.CitizenEmailService;
+import uk.gov.hmcts.reform.prl.services.tab.alltabs.AllTabServiceImpl;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@Slf4j
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+public class CitizenCallbackControllerFunctionalTest {
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @MockBean
+    private AllTabServiceImpl allTabsService;
+    @MockBean
+    private CoreCaseDataApi coreCaseDataApi;
+    @MockBean
+    private AuthTokenGenerator authTokenGenerator;
+    @MockBean
+    private SystemUserService systemUserService;
+    @MockBean
+    private CitizenEmailService citizenEmailService;
+
+
+    private static final String VALID_REQUEST_BODY = "requests/call-back-controller.json";
+
+
+    @Before
+    public void setUp() {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void givenRequestBody_whenUpdate_citizen_application_then200Response() throws Exception {
+
+        String requestBody = ResourceLoader.loadJson(VALID_REQUEST_BODY);
+        mockMvc.perform(post("/update-citizen-application")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .header("Authorization", "auth")
+                            .content(requestBody)
+                            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
+    }
+}
