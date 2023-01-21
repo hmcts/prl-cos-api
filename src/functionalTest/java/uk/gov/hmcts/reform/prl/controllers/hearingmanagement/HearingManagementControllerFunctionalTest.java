cbfbda1a692d74b99af77c654bf698ee2a98013a
sahaSonali <sonali.saha@hmcts.net>
2023-01-21 14:05:44 +0000

    Added HearingManagementControllerFunctionalTest

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/hearingmanagement/HearingManagementControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/hearingmanagement/HearingManagementControllerFunctionalTest.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.prl.controllers.noticeofchnage;
+package uk.gov.hmcts.reform.prl.controllers.hearingmanagement;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
@@ -6,14 +6,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 import uk.gov.hmcts.reform.prl.ResourceLoader;
+import uk.gov.hmcts.reform.prl.services.AuthorisationService;
+import uk.gov.hmcts.reform.prl.services.hearingmanagement.HearingManagementService;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
@@ -21,13 +26,13 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @ContextConfiguration
-public class NoticeOfChangeControllerFunctionalTest {
+public class HearingManagementControllerFunctionalTest {
 
     private MockMvc mockMvc;
     @Autowired
     private WebApplicationContext webApplicationContext;
 
-    private static final String VALID_REQUEST_BODY = "requests/call-back-controller.json";
+    private static final String VALID_HEARING_MANAGEMENT_REQUEST_BODY = "requests/hearing-management-controller.json";
 
 
     @Before
@@ -35,24 +40,19 @@ public class NoticeOfChangeControllerFunctionalTest {
         this.mockMvc = webAppContextSetup(webApplicationContext).build();
     }
 
-    @Test
-    public void givenRequestBody_whenAboutToSubmitNoCRequest_then200Response() throws Exception {
-        String requestBody = ResourceLoader.loadJson(VALID_REQUEST_BODY);
-        mockMvc.perform(post("/noc/aboutToSubmitNoCRequest")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .header("Authorization", "auth")
-                            .content(requestBody)
-                            .accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andReturn();
-    }
+    @MockBean
+    private HearingManagementService hearingManagementService;
+
+    @MockBean
+    private AuthorisationService authorisationService;
 
     @Test
-    public void givenRequestBody_whenSubmittedNoCRequest_then200Response() throws Exception {
-        String requestBody = ResourceLoader.loadJson(VALID_REQUEST_BODY);
-        mockMvc.perform(post("/noc/submittedNoCRequest")
+    public void givenRequestBody_whenHearing_management_state_update_then200Response() throws Exception {
+        String requestBody = ResourceLoader.loadJson(VALID_HEARING_MANAGEMENT_REQUEST_BODY);
+        when(authorisationService.authoriseService(anyString())).thenReturn(Boolean.TRUE);
+        mockMvc.perform(put("/hearing-management-state-update")
                             .contentType(MediaType.APPLICATION_JSON)
-                            .header("Authorization", "auth")
+                            .header("serviceAuthorization", "auth")
                             .content(requestBody)
                             .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
