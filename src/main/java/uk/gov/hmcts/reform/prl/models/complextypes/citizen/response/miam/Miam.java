8a8cf8c0499fbc70d8b00024e6b0071b461d4267
Khurshed Khan <khurshed.khan@hmcts.net>
2023-01-23 16:12:55 +0000

    added  code to add only respondent who doesn't have c7 response submitted

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/response/miam/Miam.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/response/miam/Miam.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.prl.models.complextypes.citizen.response.miam;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +10,10 @@ import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 @Builder(toBuilder = true)
 @AllArgsConstructor
 public class Miam {
+    @JsonProperty("attendedMiam")
     private final YesOrNo attendedMiam;
+    @JsonProperty("willingToAttendMiam")
     private final YesOrNo willingToAttendMiam;
+    @JsonProperty("reasonNotAttendingMiam")
     private final String reasonNotAttendingMiam;
 }
