fa00ee9b080661afa7b7812d9fc1ad729cb6966f
sahaSonali <sonali.saha@hmcts.net>
2023-01-23 15:35:31 +0000

    Added integration testcase

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseApplicationReponseControllerIntegrationTest.java
@@ -0,0 +1,63 @@
+package uk.gov.hmcts.reform.prl.controllers.citizen;
+
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import uk.gov.hmcts.reform.prl.Application;
+import uk.gov.hmcts.reform.prl.services.citizen.CaseService;
+import uk.gov.hmcts.reform.prl.util.IdamTokenGenerator;
+import uk.gov.hmcts.reform.prl.util.ServiceAuthenticationGenerator;
+
+import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringIntegrationSerenityRunner.class)
+@SpringBootTest(classes = {Application.class, CaseApplicationReponseControllerIntegrationTest.class})
+public class CaseApplicationReponseControllerIntegrationTest {
+
+    @Value("${case.orchestration.service.base.uri}")
+    protected String serviceUrl;
+
+    private final String generateC7documentEndpoint = "/generate-c7document";
+
+    private final String generateC7documentFinalEndpoint = "/generate-c7document-final";
+
+    @Autowired
+    CaseService caseService;
+
+    @Autowired
+    IdamTokenGenerator idamTokenGenerator;
+
+    @Autowired
+    ServiceAuthenticationGenerator serviceAuthenticationGenerator;
+
+    @Test
+    public void testGenerateC7documentEndpoint() throws Exception {
+        HttpPost httpPost = new HttpPost(serviceUrl + generateC7documentEndpoint);
+        httpPost.addHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+        httpPost.addHeader("serviceAuthorization", serviceAuthenticationGenerator.generate());
+        httpPost.addHeader(AUTHORIZATION, idamTokenGenerator.generateIdamTokenForSystem());
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(httpPost);
+        assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode());
+    }
+
+    @Test
+    public void testGenerateC7documentFinalEndpoint() throws Exception {
+        HttpPost httpPost = new HttpPost(serviceUrl + generateC7documentFinalEndpoint);
+        httpPost.addHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+        httpPost.addHeader("serviceAuthorization", serviceAuthenticationGenerator.generate());
+        httpPost.addHeader(AUTHORIZATION, idamTokenGenerator.generateIdamTokenForSystem());
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(httpPost);
+        assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode());
+    }
+
+
+}
