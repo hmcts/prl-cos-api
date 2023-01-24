9f5a470bab51a72b9b9e83d51efd1d9b8f2f68ca
sahaSonali <sonali.saha@hmcts.net>
2023-01-24 18:31:11 +0000

    changes for upload additional application tab

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/UploadAdditionalApllicationControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/UploadAdditionalApllicationControllerTest.java
@@ -2,49 +2,43 @@ package uk.gov.hmcts.reform.prl.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javassist.NotFoundException;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.context.annotation.PropertySource;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
-import uk.gov.hmcts.reform.idam.client.models.UserDetails;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.prl.constants.PrlAppsConstants;
 import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.models.Element;
+import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicList;
 import uk.gov.hmcts.reform.prl.models.complextypes.Child;
 import uk.gov.hmcts.reform.prl.models.complextypes.PartyDetails;
-import uk.gov.hmcts.reform.prl.models.dto.GeneratedDocumentInfo;
+import uk.gov.hmcts.reform.prl.models.complextypes.uploadadditionalapplication.AdditionalApplicationsBundle;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
-import uk.gov.hmcts.reform.prl.models.dto.ccd.WorkflowResult;
-import uk.gov.hmcts.reform.prl.services.DgsService;
-import uk.gov.hmcts.reform.prl.services.LocationRefDataService;
-import uk.gov.hmcts.reform.prl.services.SolicitorEmailService;
-import uk.gov.hmcts.reform.prl.services.UserService;
-import uk.gov.hmcts.reform.prl.services.tab.summary.CaseSummaryTabService;
+import uk.gov.hmcts.reform.prl.services.UploadAdditionalApplicationService;
 import uk.gov.hmcts.reform.prl.utils.ApplicantsListGenerator;
 import uk.gov.hmcts.reform.prl.workflows.ApplicationConsiderationTimetableValidationWorkflow;
 import uk.gov.hmcts.reform.prl.workflows.ValidateMiamApplicationOrExemptionWorkflow;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.when;
-import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.DOCUMENT_FIELD_C1A_WELSH;
-import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.DOCUMENT_FIELD_C8_WELSH;
-import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.DOCUMENT_FIELD_FINAL_WELSH;
-import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.ROLES;
 import static uk.gov.hmcts.reform.prl.enums.Gender.female;
 import static uk.gov.hmcts.reform.prl.enums.LiveWithEnum.anotherPerson;
 import static uk.gov.hmcts.reform.prl.enums.OrderTypeEnum.childArrangementsOrder;
 import static uk.gov.hmcts.reform.prl.enums.RelationshipsEnum.father;
 import static uk.gov.hmcts.reform.prl.enums.RelationshipsEnum.specialGuardian;
+import static uk.gov.hmcts.reform.prl.utils.ElementUtils.element;
 
 
 @RunWith(MockitoJUnitRunner.Silent.class)
@@ -61,77 +55,15 @@ public class UploadAdditionalApllicationControllerTest {
     private UploadAdditionalApplicationController uploadAdditionalApplicationController;
 
     @Mock
-    private UserService userService;
-
-    @Mock
-    private WorkflowResult workflowResult;
-
-    @Mock
-    private CaseSummaryTabService caseSummaryTab;
-
-    @Mock
-    private DgsService dgsService;
-
-    @Mock
-    private GeneratedDocumentInfo generatedDocumentInfo;
-
-    @Mock
-    private SolicitorEmailService solicitorEmailService;
-
-    @Mock
-    private UserDetails userDetails;
-
-    @Mock
     private ObjectMapper objectMapper;
 
     @Mock
     private ApplicantsListGenerator applicantsListGenerator;
     @Mock
-    private LocationRefDataService locationRefDataService;
+    private UploadAdditionalApplicationService uploadAdditionalApplicationService;
 
 
     public static final String authToken = "Bearer TestAuthToken";
-
-    private static final Map<String, Object> c100DraftMap = new HashMap<>();
-    private static final Map<String, Object> c100DocsMap = new HashMap<>();
-
-    private static final Map<String, Object> fl401DraftMap = new HashMap<>();
-    private static final Map<String, Object> fl401DocsMap = new HashMap<>();
-
-    @Before
-    public void setUp() {
-
-        userDetails = UserDetails.builder()
-            .forename("solicitor@example.com")
-            .surname("Solicitor")
-            .roles(ROLES)
-            .build();
-        generatedDocumentInfo = GeneratedDocumentInfo.builder()
-            .url("TestUrl")
-            .binaryUrl("binaryUrl")
-            .hashToken("testHashToken")
-            .build();
-
-        c100DraftMap.put(PrlAppsConstants.DRAFT_DOCUMENT_FIELD, "test");
-        c100DraftMap.put(PrlAppsConstants.DRAFT_DOCUMENT_WELSH_FIELD, "test");
-
-        c100DocsMap.put(PrlAppsConstants.DOCUMENT_FIELD_C8, "test");
-        c100DocsMap.put(PrlAppsConstants.DOCUMENT_FIELD_C1A, "test");
-        c100DocsMap.put(PrlAppsConstants.DOCUMENT_FIELD_FINAL, "test");
-        c100DocsMap.put(DOCUMENT_FIELD_C8_WELSH, "test");
-        c100DocsMap.put(DOCUMENT_FIELD_C1A_WELSH, "test");
-        c100DocsMap.put(DOCUMENT_FIELD_FINAL_WELSH, "test");
-
-        fl401DraftMap.put(PrlAppsConstants.DRAFT_DOCUMENT_FIELD, "test");
-        fl401DraftMap.put(PrlAppsConstants.DRAFT_DOCUMENT_WELSH_FIELD, "test");
-
-        fl401DocsMap.put(PrlAppsConstants.DOCUMENT_FIELD_C8, "test");
-        fl401DocsMap.put(PrlAppsConstants.DOCUMENT_FIELD_FINAL, "test");
-        fl401DocsMap.put(DOCUMENT_FIELD_C8_WELSH, "test");
-        fl401DocsMap.put(DOCUMENT_FIELD_FINAL_WELSH, "test");
-        when(locationRefDataService.getCourtDetailsFromEpimmsId(Mockito.anyString(),Mockito.anyString()))
-            .thenReturn("test-test-test-test-test-test");
-    }
 
     @Test
     public void testPrePopulateApplicants() throws NotFoundException {
@@ -174,6 +106,7 @@ public class UploadAdditionalApllicationControllerTest {
             .CallbackRequest.builder().caseDetails(uk.gov.hmcts.reform.ccd.client.model.CaseDetails.builder().id(123L)
                                                        .data(caseDataUpdated).build()).build();
 
+        when(applicantsListGenerator.buildApplicantsList(caseData)).thenReturn(DynamicList.builder().build());
 
         AboutToStartOrSubmitCallbackResponse aboutToStartOrSubmitCallbackResponse =
             uploadAdditionalApplicationController.prePopulateApplicants(
@@ -202,4 +135,61 @@ public class UploadAdditionalApllicationControllerTest {
         Map<String, Object> caseDetailsRespnse = aboutToStartOrSubmitCallbackResponse.getData();
         assertNull(caseDetailsRespnse.get("additionalApplicantsList"));
     }
+
+    @Test
+    public void testcreateUploadAdditionalApplicationBundle() {
+
+        CaseData caseData = CaseData.builder()
+            .caseTypeOfApplication(PrlAppsConstants.C100_CASE_TYPE)
+            .build();
+
+        Map<String, Object> caseDataUpdated = new HashMap<>();
+        caseDataUpdated.put("caseTypeOfApplication", "C100");
+
+        when(objectMapper.convertValue(caseDataUpdated, CaseData.class)).thenReturn(caseData);
+
+        List<Element<AdditionalApplicationsBundle>> additionalApplicationsBundle = new ArrayList<>();
+        additionalApplicationsBundle.add(element(AdditionalApplicationsBundle.builder().build()));
+        when(uploadAdditionalApplicationService.getAdditionalApplicationElements("test", caseData)).thenReturn(
+            additionalApplicationsBundle);
+        CallbackRequest callbackRequest = uk.gov.hmcts.reform.ccd.client.model
+            .CallbackRequest.builder()
+            .caseDetails(CaseDetails.builder()
+                             .id(1L)
+                             .data(caseDataUpdated).build()).build();
+        assertNotNull(uploadAdditionalApplicationController.createUploadAdditionalApplicationBundle(
+            "test",
+            callbackRequest
+        ));
+    }
+
+    @Test
+    public void testcreateUploadAdditionalApplicationBundleWithCaseDataMap() {
+        Map<String, Object> caseDataUpdated = new HashMap<>();
+        caseDataUpdated.put("temporaryOtherApplicationsBundle", "test");
+        caseDataUpdated.put("temporaryC2Document", "test");
+        caseDataUpdated.put("additionalApplicantsList", "test");
+        caseDataUpdated.put("typeOfC2Application", "test");
+        caseDataUpdated.put("additionalApplicationsApplyingFor", "test");
+        CaseData caseData = CaseData.builder()
+            .caseTypeOfApplication(PrlAppsConstants.C100_CASE_TYPE)
+            .build();
+        when(objectMapper.convertValue(caseDataUpdated, CaseData.class)).thenReturn(caseData);
+
+        List<Element<AdditionalApplicationsBundle>> additionalApplicationsBundle = new ArrayList<>();
+        additionalApplicationsBundle.add(element(AdditionalApplicationsBundle.builder().build()));
+        when(uploadAdditionalApplicationService.getAdditionalApplicationElements("test", caseData)).thenReturn(
+            additionalApplicationsBundle);
+        CallbackRequest callbackRequest = uk.gov.hmcts.reform.ccd.client.model
+            .CallbackRequest.builder()
+            .caseDetails(CaseDetails.builder()
+                             .id(1L)
+                             .data(caseDataUpdated).build()).build();
+
+        assertNotNull(uploadAdditionalApplicationController.createUploadAdditionalApplicationBundle(
+            "test",
+            callbackRequest
+        ));
+    }
+
 }
