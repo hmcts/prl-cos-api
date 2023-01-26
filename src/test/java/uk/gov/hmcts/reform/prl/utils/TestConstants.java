68ce7e8afcc5ba554af6acad15dc38598b22f53c
bharadwajsairam <bharadwajsairam001@gmail.com>
2023-01-26 10:51:05 +0000

    revert demo changes

--- src/test/java/uk/gov/hmcts/reform/prl/utils/TestConstants.java
+++ src/test/java/uk/gov/hmcts/reform/prl/utils/TestConstants.java
@@ -14,13 +14,5 @@ public class TestConstants {
     public static final String TEST_RESPONDENT_FIRST_NAME = "Zdzislaw";
     public static final String TEST_RESPONDENT_LAST_NAME = "Nowakowski";
     public static final String TEST_RESPONDENT_NAME = TEST_RESPONDENT_FIRST_NAME + " " + TEST_RESPONDENT_LAST_NAME;
-    public static final String WALES_POSTCODE_NATIONALCODE = "W";
-    public static final String PRL_CASE_TYPE = "PRLAPPS";
-    public static final String CAFCASS_TEST_AUTHORISATION_TOKEN = "authorisationToken";
-    public static final String CAFCASS_TEST_SERVICE_AUTHORISATION_TOKEN = "serviceAuthorisationToken";
-    public static final String EMPTY_STRING = "";
-    public static final String TEST_AUTHORIZATION = "testAuthorisation";
-    public static final String TEST_CAFCASS_DOWNLOAD_FILENAME = "CafcassDownloadFile.doc";
-    public static final String TEST_SERVICE_AUTHORIZATION = "testServiceAuthorisation";
 
 }
