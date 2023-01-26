49649fadab39f253cde5cf71ad5f732021c95c67
bharadwajsairam <bharadwajsairam001@gmail.com>
2023-01-26 10:11:03 +0000

    checkstyle fix

--- src/test/java/uk/gov/hmcts/reform/prl/services/citizen/CitizenCoreCaseDataServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/citizen/CitizenCoreCaseDataServiceTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.prl.services.citizen;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
