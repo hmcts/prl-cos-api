822e4f69bd4e4f7b3a9378cca2ca14a0ec96ede5
bharadwajsairam <bharadwajsairam001@gmail.com>
2023-01-26 10:51:05 +0000

    revert demo changes

--- src/main/java/uk/gov/hmcts/reform/prl/constants/cafcass/CafcassAppConstants.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.prl.constants.cafcass;
-
-public class CafcassAppConstants {
-
-    public static final String ENGLAND_POSTCODE_NATIONALCODE = "E";
-
-    private CafcassAppConstants() {
-    }
-}
