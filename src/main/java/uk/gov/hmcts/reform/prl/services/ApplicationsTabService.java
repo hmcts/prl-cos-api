9920bef09a066df42d4320befd5f484705a7b785
sahaSonali <sonali.saha@hmcts.net>
2023-01-27 20:39:26 +0000

    Changes for add cafcass officer

--- src/main/java/uk/gov/hmcts/reform/prl/services/ApplicationsTabService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ApplicationsTabService.java
@@ -184,6 +184,7 @@ public class ApplicationsTabService implements TabService {
             .collect(Collectors.toList());
         for (Child child : childList) {
             ChildDetails c = mapChildDetails(child);
+            log.info("ChildDetails ==========" + c);
             Element<ChildDetails> res = Element.<ChildDetails>builder().value(c).build();
             childFinalList.add(res);
         }
