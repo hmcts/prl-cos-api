a5cf994ae78fb920052e27df0dea6a9fd4fed714
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2023-01-27 02:12:20 +0000

    Test

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/FeeAndPayServiceRequestController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/FeeAndPayServiceRequestController.java
@@ -70,10 +70,8 @@ public class FeeAndPayServiceRequestController extends AbstractCallbackControlle
         log.info("Before {}", callbackRequest);
         return ok(SubmittedCallbackResponse.builder().confirmationHeader("# You are seeing confimration page").confirmationBody(
             "#### What happens next You can review this note in the case notes tab."
-                + "\n\n"
-                + "</br><a href='/cases/case-details/'" + callbackRequest.getCaseDetails().getCaseId() + "#Service%20Request"
-                + "\n\n"
-                + "/cases/case-details/" + callbackRequest.getCaseDetails().getCaseId() + "#Service%20Request").build());
+                + "\n\n <a href='/cases/case-details/'" + callbackRequest.getCaseDetails().getCaseId() + "'#Service%20Request'>click here to pay</a>"
+        ).build());
     }
 
     @PostMapping(path = "/generate-document-submit-application-test", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
