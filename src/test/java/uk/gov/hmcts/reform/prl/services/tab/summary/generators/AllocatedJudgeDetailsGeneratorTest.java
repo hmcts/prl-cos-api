7654e1b017e3e8b969cb83ca560405bcd42a6eea
GayatriDeviCh <GayatriDevi.Chadaram@hmcts.net>
2023-01-27 11:30:29 +0000

    Issue fixes for summaryTab

--- src/test/java/uk/gov/hmcts/reform/prl/services/tab/summary/generators/AllocatedJudgeDetailsGeneratorTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/tab/summary/generators/AllocatedJudgeDetailsGeneratorTest.java
@@ -1,10 +1,6 @@
 package uk.gov.hmcts.reform.prl.services.tab.summary.generators;
 
 import org.junit.Test;
-import uk.gov.hmcts.reform.prl.enums.YesOrNo;
-import uk.gov.hmcts.reform.prl.enums.gatekeeping.AllocatedJudgeTypeEnum;
-import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicList;
-import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicListElement;
 import uk.gov.hmcts.reform.prl.models.complextypes.tab.summarytab.CaseSummary;
 import uk.gov.hmcts.reform.prl.models.complextypes.tab.summarytab.summary.AllocatedJudge;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
@@ -19,19 +15,15 @@ public class AllocatedJudgeDetailsGeneratorTest {
     @Test
     public void testGenerate() {
         CaseSummary caseSummary = generator.generate(CaseData.builder()
-                                                         .courtName("Test Court")
-            .allocatedJudge(uk.gov.hmcts.reform.prl.models.dto.gatekeeping.AllocatedJudge
-                .builder().isJudgeOrLegalAdviser(AllocatedJudgeTypeEnum.JUDGE).isSpecificJudgeOrLegalAdviserNeeded(YesOrNo.Yes)
-                .judgeList(DynamicList.builder().value(DynamicListElement.builder().code("test1(test1@xxx.com)")
-                    .label("test1(test1@xxx.com)").build()).build()).build())
-                                                             .build());
+            .courtName("Test Court")
+            .build());
 
         assertThat(caseSummary).isEqualTo(CaseSummary.builder()
-                                              .allocatedJudgeDetails(AllocatedJudge.builder()
-                                                      .judgeTitle(" ").emailAddress("test1@xxx.com").lastName("test1")
-                                                      .courtName("Test Court")
-                                                                         .build())
-                                                               .build());
+            .allocatedJudgeDetails(AllocatedJudge.builder()
+                .judgeTitle(" ").emailAddress(" ").lastName(" ")
+                .courtName("Test Court")
+                .build())
+            .build());
 
     }
 }
