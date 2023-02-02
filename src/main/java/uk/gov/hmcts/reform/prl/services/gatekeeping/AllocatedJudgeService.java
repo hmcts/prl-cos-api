e0cbec9dceb8cde14ccf1badf6781ce0af75f2f8
GayatriDeviCh <GayatriDevi.Chadaram@hmcts.net>
2023-02-02 18:43:06 +0000

    Checkstyle issue fix

--- src/main/java/uk/gov/hmcts/reform/prl/services/gatekeeping/AllocatedJudgeService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/gatekeeping/AllocatedJudgeService.java
@@ -64,7 +64,6 @@ public class AllocatedJudgeService {
                     } catch (Exception e) {
                         throw new RuntimeException(e);
                     }
-                    //log.info("*** ********PersonalCode for the selected judge id : {}", null != personalCodes ? personalCodes.length : personalCodes);
                     log.info("*** ********UserName retrieved from environment to get idam token: {}", refDataIdamUsername);
                     log.info("*** ********Password retrieved from environment to get idam token: {}", refDataIdamPassword);
                     List<JudicialUsersApiResponse> judgeDetails = judicialUserInfoService.getAllJudicialUserDetails(JudicialUsersApiRequest.builder()
