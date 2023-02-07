8e34419f488292d916c059f09ec16b71fc86bc21
parvezhmcts <parvez.qureshi@hmcts.net>
2023-02-07 15:05:22 +0000

    ISDB-577
    -

---
+++ src/main/java/uk/gov/hmcts/reform/prl/models/caseflags/flagdetails/Data.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.reform.prl.models.caseflags.flagdetails;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@lombok.Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@Getter
+@AllArgsConstructor
+public class Data {
+    public List<Path> path;
+    public String hearingRelevant;
+    public Date dateTimeCreated;
+    public String flagComment;
+    public Object subTypeKey;
+    public String flagCode;
+    public String name;
+    public Object subTypeValue;
+    public Object otherDescription;
+    public String status;
+}
