d0fdf9f77b9bc3cae5de4a69e3dfa476501316bb
sangeetha <sangeetha.ponnusamy@hmcts.net>
2023-02-07 11:57:40 +0000

    feat(FPET-312) Added Junits for AllocateJudgeService

--- src/test/java/uk/gov/hmcts/reform/prl/services/gatekeeping/AllocatedJudgeServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/gatekeeping/AllocatedJudgeServiceTest.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.reform.prl.services.gatekeeping;
 
-/*
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Map;
 import org.junit.Test;
@@ -10,9 +10,17 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.prl.constants.PrlAppsConstants;
+import uk.gov.hmcts.reform.prl.enums.YesOrNo;
+import uk.gov.hmcts.reform.prl.enums.gatekeeping.AllocatedJudgeTypeEnum;
+import uk.gov.hmcts.reform.prl.enums.gatekeeping.TierOfJudiciaryEnum;
+import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicList;
+import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicListElement;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+import uk.gov.hmcts.reform.prl.models.dto.gatekeeping.AllocatedJudge;
 import uk.gov.hmcts.reform.prl.services.RefDataUserService;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.Silent.class)
@@ -28,23 +36,28 @@ public class AllocatedJudgeServiceTest {
     ObjectMapper objectMapper;
 
     @Test
-    public void testMapAllocatedJudge(){
+    public void testWhenTierOfJudiciaryDetailsProvided() {
         CaseData caseData = CaseData.builder()
-            .caseTypeOfApplication(PrlAppsConstants.C100_CASE_TYPE)
-            .build();
+            .caseTypeOfApplication(PrlAppsConstants.C100_CASE_TYPE).build();
 
         Map<String, Object> stringObjectMap = caseData.toMap(new ObjectMapper());
+        stringObjectMap.put("tierOfJudiciary","circuitJudge");
         when(objectMapper.convertValue(stringObjectMap, CaseData.class)).thenReturn(caseData);
-        CallbackRequest callbackRequest = uk.gov.hmcts.reform.ccd.client.model
-            .CallbackRequest.builder()
-            .caseDetails(uk.gov.hmcts.reform.ccd.client.model.CaseDetails.builder()
-                             .id(123L)
-                             .data(stringObjectMap)
-                             .build())
-            .build();
-
+        AllocatedJudge expectedResponse = allocatedJudgeService.getAllocatedJudgeDetails(stringObjectMap,null,null);
+        assertEquals(expectedResponse.getTierOfJudiciary().getDisplayedValue(),TierOfJudiciaryEnum.CIRCUIT_JUDGE.getDisplayedValue());
 
     }
+    @Test
+    public void testWhenLegalAdvisorDetailsProvided() {
+        CaseData caseData = CaseData.builder()
+            .caseTypeOfApplication(PrlAppsConstants.C100_CASE_TYPE).build();
 
+        Map<String, Object> stringObjectMap = caseData.toMap(new ObjectMapper());
+        stringObjectMap.put("isJudgeOrLegalAdviser", AllocatedJudgeTypeEnum.LEGAL_ADVISER);
+        when(objectMapper.convertValue(stringObjectMap, CaseData.class)).thenReturn(caseData);
+        DynamicList legalAdviserList =DynamicList.builder().value(DynamicListElement.builder().code("test1(test1@test.com)").label("test1(test1@test.com)").build()).build();
+        AllocatedJudge expectedResponse = allocatedJudgeService.getAllocatedJudgeDetails(stringObjectMap,legalAdviserList,null);
+        assertEquals(AllocatedJudgeTypeEnum.LEGAL_ADVISER,expectedResponse.getIsJudgeOrLegalAdviser());
+        assertNotNull(expectedResponse.getLegalAdviserList());
+    }
 }
-*/
