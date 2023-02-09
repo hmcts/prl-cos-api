57bf78416cb9218b108ebe6b9e770755189504d4
BharadwajSairam <75499453+BharadwajSairam@users.noreply.github.com>
2023-02-09 10:30:06 +0000

    Update DraftAnOrderController.java

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/DraftAnOrderController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/DraftAnOrderController.java
@@ -201,10 +201,7 @@ public class DraftAnOrderController {
     public AboutToStartOrSubmitCallbackResponse prepareDraftOrderCollection(
         @RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation,
         @RequestBody CallbackRequest callbackRequest) {
-        CaseData caseData = objectMapper.convertValue(
-            callbackRequest.getCaseDetails().getData(),
-            CaseData.class
-        );
+        CaseData caseData = CaseUtils.getCaseData(callbackRequest.getCaseDetails(), objectMapper);
         Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
         log.info("*** Case type of application in draft orders submission before: {}", caseData.getSelectedCaseTypeID());
         caseDataUpdated.put("caseTypeOfApplication", caseData.getSelectedCaseTypeID());
