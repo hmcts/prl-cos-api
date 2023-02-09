d671cf198b37f53dd6717182d6d6aaa44c07d6ea
Khurshed Khan <khurshed.khan@hmcts.net>
2023-02-09 17:08:32 +0000

    PRL-3005:Added changes to save the order

--- src/main/java/uk/gov/hmcts/reform/prl/services/AmendOrderService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/AmendOrderService.java
@@ -5,18 +5,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.document.am.model.Document;
 import uk.gov.hmcts.reform.prl.constants.PrlAppsConstants;
+import uk.gov.hmcts.reform.prl.enums.YesOrNo;
+import uk.gov.hmcts.reform.prl.enums.serveorder.WhatToDoWithOrderEnum;
+import uk.gov.hmcts.reform.prl.models.DraftOrder;
 import uk.gov.hmcts.reform.prl.models.Element;
 import uk.gov.hmcts.reform.prl.models.OrderDetails;
+import uk.gov.hmcts.reform.prl.models.OtherDraftOrderDetails;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.ManageOrders;
 import uk.gov.hmcts.reform.prl.services.time.Time;
 
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 
 import static uk.gov.hmcts.reform.prl.utils.ElementUtils.element;
@@ -62,25 +69,70 @@ public class AmendOrderService {
         UUID selectedOrderId = caseData.getManageOrders().getAmendOrderDynamicList().getValueCodeAsUuid();
         List<Element<OrderDetails>> orders = caseData.getOrderCollection();
 
-        orders.stream()
+        if (YesOrNo.Yes.equals(caseData.getServeOrderData().getDoYouWantToServeOrder())
+            || WhatToDoWithOrderEnum.finalizeSaveToServeLater
+                .equals(caseData.getServeOrderData().getWhatDoWithOrder())) {
+            orders.stream()
+                .filter(order -> Objects.equals(order.getId(), selectedOrderId))
+                .findFirst()
+                .ifPresent(order -> {
+                    OrderDetails amended = order.getValue().toBuilder()
+                        .orderDocument(amendedDocument)
+                        .orderType(order.getValue().getOrderType())
+                        .typeOfOrder(order.getValue().getTypeOfOrder())
+                        .otherDetails(order.getValue().getOtherDetails().toBuilder()
+                                          .orderAmendedDate(time.now().format(DateTimeFormatter.ofPattern(
+                                              PrlAppsConstants.D_MMMM_YYYY,
+                                              Locale.UK
+                                          )))
+                                          .build())
+                        .build();
+
+                    orders.addAll(List.of(element(amended)));
+                });
+            return Map.of("orderCollection", orders);
+        } else {
+            return  setDraftOrderCollection(caseData, amendedDocument);
+        }
+
+    }
+
+    public Map<String, Object> setDraftOrderCollection(CaseData caseData, uk.gov.hmcts.reform.prl.models.documents.Document amendedDocument) {
+        List<Element<DraftOrder>> draftOrderList = new ArrayList<>();
+        Element<DraftOrder> draftOrderElement = element(getCurrentDraftOrderDetails(caseData,amendedDocument));
+        if (caseData.getDraftOrderCollection() != null) {
+            draftOrderList.addAll(caseData.getDraftOrderCollection());
+            draftOrderList.add(draftOrderElement);
+        } else {
+            draftOrderList.add(draftOrderElement);
+        }
+        draftOrderList.sort(Comparator.comparing(
+            m -> m.getValue().getOtherDetails().getDateCreated(),
+            Comparator.reverseOrder()
+        ));
+        return Map.of("draftOrderCollection", draftOrderList
+        );
+    }
+
+    private DraftOrder getCurrentDraftOrderDetails(CaseData caseData,
+                                                   uk.gov.hmcts.reform.prl.models.documents.Document amendedDocument) {
+        UUID selectedOrderId = caseData.getManageOrders().getAmendOrderDynamicList().getValueCodeAsUuid();
+        List<Element<OrderDetails>> orders = caseData.getOrderCollection();
+        Optional<Element<OrderDetails>> orderDetails  = orders.stream()
             .filter(order -> Objects.equals(order.getId(), selectedOrderId))
-            .findFirst()
-            .ifPresent(order -> {
-                OrderDetails amended = order.getValue().toBuilder()
-                    .orderDocument(amendedDocument)
-                    .orderType(order.getValue().getOrderType())
-                    .typeOfOrder(order.getValue().getTypeOfOrder())
-                    .otherDetails(order.getValue().getOtherDetails().toBuilder()
-                                      .orderAmendedDate(time.now().format(DateTimeFormatter.ofPattern(
-                                          PrlAppsConstants.D_MMMM_YYYY,
-                                          Locale.UK
-                                      )))
-                                      .build())
-                    .build();
+            .findFirst();
+        String orderType = orderDetails.get().getValue().getOrderType();
 
-                orders.add(element(order.getId(), amended));
-            });
-        return Map.of("orderCollection", orders);
-
+        return DraftOrder.builder()
+            .typeOfOrder(orderType)
+            .orderTypeId(orderType)
+            .orderDocument(amendedDocument)
+            .otherDetails(OtherDraftOrderDetails.builder()
+                              .createdBy(caseData.getJudgeOrMagistratesLastName())
+                              .dateCreated(time.now())
+                              .status("Draft").build())
+            .dateOrderMade(caseData.getDateOrderMade())
+            .orderSelectionType(caseData.getManageOrdersOptions())
+            .build();
     }
 }
