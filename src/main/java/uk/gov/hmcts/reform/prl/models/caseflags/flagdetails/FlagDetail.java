5f6e56a0577591b5e7acb351938266692703f42c
parvezhmcts <parvez.qureshi@hmcts.net>
2023-02-10 17:52:33 +0000

    - modified datetimecreated

--- src/main/java/uk/gov/hmcts/reform/prl/models/caseflags/flagdetails/FlagDetail.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/caseflags/flagdetails/FlagDetail.java
@@ -1,9 +1,12 @@
 package uk.gov.hmcts.reform.prl.models.caseflags.flagdetails;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @lombok.Data
 @Builder(toBuilder = true)
@@ -14,7 +17,8 @@ public class FlagDetail {
     //  public List<Element<Path>> path;
     public String hearingRelevant;
 
-    //  public LocalDateTime dateTimeCreated;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
+    public LocalDateTime dateTimeCreated;
     public String flagComment;
     // public Object subTypeKey;
     public String flagCode;
