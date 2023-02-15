537213b8a140dd0558824aa8c956721377951ef4
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2023-02-15 18:37:47 +0000

    NHD Cos changes 1

--- src/test/java/uk/gov/hmcts/reform/prl/services/hearingmanagement/HearingManagementServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/hearingmanagement/HearingManagementServiceTest.java
@@ -717,6 +717,7 @@ public class HearingManagementServiceTest {
                                                          caseType, nextHearingDateRequest.getCaseRef(), true,
                                                          buildCaseDataContentForNhd("updateNextHearingDate", eventToken,nextHearingDetails)
         );
+
         assertTrue(true);
     }
 }
