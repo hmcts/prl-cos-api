2e604ac3c2957f34afa30905283753958df96485
sangeetha <sangeetha.ponnusamy@hmcts.net>
2023-02-15 13:55:30 +0000

    Object Structure for Hearing

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
@@ -764,4 +764,7 @@ public class CaseData implements MappableObject {
     private AllocatedJudge allocatedJudge;
     @JsonProperty("gatekeepingDetails")
     private GatekeepingDetails gatekeepingDetails;
+
+
+    private final List<Element<HearingData>> hearingData;
 }
