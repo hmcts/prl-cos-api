d84a7ea57f947b1255ede11d7fee4ceec0bb0c3b
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2023-02-15 18:45:34 +0000

    NHD Cos changes 2

--- src/main/java/uk/gov/hmcts/reform/prl/models/caseflags/flagdetails/Data.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/caseflags/flagdetails/Data.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
 import java.time.LocalDateTime;
 import java.util.List;
 
--- src/main/java/uk/gov/hmcts/reform/prl/models/caseflags/flagdetails/FlagDetail.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/caseflags/flagdetails/FlagDetail.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 @lombok.Data
