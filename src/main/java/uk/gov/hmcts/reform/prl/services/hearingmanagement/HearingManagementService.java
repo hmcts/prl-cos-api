d6eee082a4c4508317431796bebb48d38db754d7
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2023-02-16 10:01:48 +0000

    NHD Cos changes 2

--- src/main/java/uk/gov/hmcts/reform/prl/services/hearingmanagement/HearingManagementService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/hearingmanagement/HearingManagementService.java
@@ -93,7 +93,6 @@ public class HearingManagementService {
             hearingRequest.getCaseRef()
         );
 
-        log.info("HHHHHHHH {}",caseDetails.getData());
         CaseData caseData = CaseUtils.getCaseData(caseDetails, objectMapper);
 
         String hmcStatus = hearingRequest.getHearingUpdate().getHmcStatus();
