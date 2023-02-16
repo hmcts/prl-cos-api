359b2219efb861ad6404505d5a45a33e3a7300a8
2219956 <ramesh.meripe@hmcts.net>
2023-02-16 09:54:27 +0000

    Code smells fix

--- src/main/java/uk/gov/hmcts/reform/prl/services/ApplicationsTabServiceHelper.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ApplicationsTabServiceHelper.java
@@ -110,7 +110,7 @@ public class ApplicationsTabServiceHelper {
         List<Element<ChildAndApplicantRelation>> applicants = new ArrayList<>();
         Optional<List<Element<ChildrenAndApplicantRelation>>> checkApplicants = ofNullable(caseData.getChildAndApplicantRelations());
 
-        if (checkApplicants.isEmpty() || (checkApplicants.get().isEmpty() && checkApplicants.get().size() == 0)) {
+        if (checkApplicants.isEmpty() || checkApplicants.get().isEmpty()) {
             ChildAndApplicantRelation a = ChildAndApplicantRelation.builder().build();
             Element<ChildAndApplicantRelation> app = Element.<ChildAndApplicantRelation>builder().value(a).build();
             applicants.add(app);
@@ -135,7 +135,7 @@ public class ApplicationsTabServiceHelper {
         List<Element<ChildAndRespondentRelation>> respondents = new ArrayList<>();
         Optional<List<Element<ChildrenAndRespondentRelation>>> checkRespondents = ofNullable(caseData.getChildAndRespondentRelations());
 
-        if (checkRespondents.isEmpty() || (checkRespondents.get().isEmpty() && checkRespondents.get().size() == 0)) {
+        if (checkRespondents.isEmpty() || checkRespondents.get().isEmpty()) {
             ChildAndRespondentRelation a = ChildAndRespondentRelation.builder().build();
             Element<ChildAndRespondentRelation> app = Element.<ChildAndRespondentRelation>builder().value(a).build();
             respondents.add(app);
@@ -159,7 +159,7 @@ public class ApplicationsTabServiceHelper {
         List<Element<ChildAndOtherPeopleRelation>> otherPeopleRelations = new ArrayList<>();
         Optional<List<Element<ChildrenAndOtherPeopleRelation>>> checkRespondents = ofNullable(caseData.getChildAndOtherPeopleRelations());
 
-        if (checkRespondents.isEmpty() || (checkRespondents.get().isEmpty() && checkRespondents.get().size() == 0)) {
+        if (checkRespondents.isEmpty() || checkRespondents.get().isEmpty()) {
             ChildAndOtherPeopleRelation a = ChildAndOtherPeopleRelation.builder().build();
             Element<ChildAndOtherPeopleRelation> app = Element.<ChildAndOtherPeopleRelation>builder().value(a).build();
             otherPeopleRelations.add(app);
@@ -170,9 +170,9 @@ public class ApplicationsTabServiceHelper {
             .collect(Collectors.toList());
 
         for (ChildrenAndOtherPeopleRelation otherPeople : currentApplicants) {
-                ChildAndOtherPeopleRelation a = objectMapper.convertValue(otherPeople, ChildAndOtherPeopleRelation.class);
-                Element<ChildAndOtherPeopleRelation> app = Element.<ChildAndOtherPeopleRelation>builder().value(a).build();
-                otherPeopleRelations.add(app);
+            ChildAndOtherPeopleRelation a = objectMapper.convertValue(otherPeople, ChildAndOtherPeopleRelation.class);
+            Element<ChildAndOtherPeopleRelation> app = Element.<ChildAndOtherPeopleRelation>builder().value(a).build();
+            otherPeopleRelations.add(app);
         }
         log.info("-->getChildAndOtherPeopleRelationsTable()--->End");
         return otherPeopleRelations;
