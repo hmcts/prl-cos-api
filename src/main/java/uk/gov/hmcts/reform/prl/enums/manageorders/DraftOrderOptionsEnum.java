545c0ca46cc13079ce28883d0b9e3bf8b614ad38
sahaSonali <sonali.saha@hmcts.net>
2023-02-19 00:47:30 +0000

    Changes for create order

---
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/manageorders/DraftOrderOptionsEnum.java
@@ -0,0 +1,31 @@
+package uk.gov.hmcts.reform.prl.enums.manageorders;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.RequiredArgsConstructor;
+import uk.gov.hmcts.reform.prl.enums.CustomEnumSerializer;
+
+@RequiredArgsConstructor
+@JsonSerialize(using = CustomEnumSerializer.class)
+public enum DraftOrderOptionsEnum {
+
+    @JsonProperty("createAnOrder")
+    createAnOrder("createAnOrder", "Create an order"),
+    @JsonProperty("uploadAnOrder")
+    uploadAnOrder("uploadAnOrder", "Upload an order");
+
+    private final String id;
+    private final String displayedValue;
+
+    @JsonValue
+    public String getDisplayedValue() {
+        return displayedValue;
+    }
+
+    @JsonCreator
+    public static DraftOrderOptionsEnum getValue(String key) {
+        return DraftOrderOptionsEnum.valueOf(key);
+    }
+}
