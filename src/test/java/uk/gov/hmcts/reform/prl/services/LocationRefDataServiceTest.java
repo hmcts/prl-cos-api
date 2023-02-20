6a5b3807be0212b6da991dc8e824b19193900b7c
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2023-02-20 20:00:18 +0000

    Adding test coverage

--- src/test/java/uk/gov/hmcts/reform/prl/services/LocationRefDataServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/LocationRefDataServiceTest.java
@@ -45,6 +45,7 @@ public class LocationRefDataServiceTest {
     public void setUp() {
         when(authTokenGenerator.generate()).thenReturn("");
         ReflectionTestUtils.setField(locationRefDataService,"courtsToFilter", "1:email,2:email,3:email,4:email");
+        ReflectionTestUtils.setField(locationRefDataService,"dacourtsToFilter", "1:email,2:email,3:email,4:email");
     }
 
     @Test
@@ -56,10 +57,26 @@ public class LocationRefDataServiceTest {
     }
 
     @Test
+    public void testDaGetCourtDetailsWithNullCourtDetails() {
+        when(locationRefDataApi.getCourtDetailsByService(Mockito.anyString(),Mockito.anyString(),Mockito.anyString()))
+            .thenReturn(null);
+        List<DynamicListElement> courtLocations = locationRefDataService.getDaCourtLocations("test");
+        assertTrue(courtLocations.isEmpty());
+    }
+
+    @Test
     public void testgetCourtDetailsWithException() {
         when(locationRefDataApi.getCourtDetailsByService(Mockito.anyString(),Mockito.anyString(),Mockito.anyString()))
             .thenThrow(NullPointerException.class);
         List<DynamicListElement> courtLocations = locationRefDataService.getCourtLocations("test");
+        assertNull(courtLocations.get(0).getCode());
+    }
+
+    @Test
+    public void testDaGetCourtDetailsWithException() {
+        when(locationRefDataApi.getCourtDetailsByService(Mockito.anyString(),Mockito.anyString(),Mockito.anyString()))
+            .thenThrow(NullPointerException.class);
+        List<DynamicListElement> courtLocations = locationRefDataService.getDaCourtLocations("test");
         assertNull(courtLocations.get(0).getCode());
     }
 
@@ -76,6 +93,20 @@ public class LocationRefDataServiceTest {
         assertFalse(courtLocations.isEmpty());
     }
 
+
+    @Test
+    public void testDaGetCourtDetailsWithData() {
+        when(locationRefDataApi.getCourtDetailsByService(Mockito.anyString(),Mockito.anyString(),Mockito.anyString()))
+            .thenReturn(CourtDetails.builder()
+                            .courtVenues(List.of(CourtVenue.builder().region("r").regionId("id").courtName("1")
+                                                     .region("test").siteName("test")
+                                                     .courtEpimmsId("2")
+                                                     .courtTypeId(FAMILY_COURT_TYPE_ID).build()))
+                            .build());
+        List<DynamicListElement> courtLocations = locationRefDataService.getDaCourtLocations("test");
+        assertFalse(courtLocations.isEmpty());
+    }
+
     @Test
     public void testgetCourtDetailsWithNoData() {
         when(locationRefDataApi.getCourtDetailsByService(Mockito.anyString(),Mockito.anyString(),Mockito.anyString()))
@@ -87,6 +118,20 @@ public class LocationRefDataServiceTest {
                             .build());
         ReflectionTestUtils.setField(locationRefDataService,"courtsToFilter", "");
         List<DynamicListElement> courtLocations = locationRefDataService.getCourtLocations("test");
+        assertFalse(courtLocations.isEmpty());
+    }
+
+    @Test
+    public void testDaGetCourtDetailsWithNoData() {
+        when(locationRefDataApi.getCourtDetailsByService(Mockito.anyString(),Mockito.anyString(),Mockito.anyString()))
+            .thenReturn(CourtDetails.builder()
+                            .courtVenues(List.of(CourtVenue.builder().region("r").regionId("id").courtName("1")
+                                                     .region("test").siteName("test")
+                                                     .courtEpimmsId("2")
+                                                     .courtTypeId(FAMILY_COURT_TYPE_ID).build()))
+                            .build());
+        ReflectionTestUtils.setField(locationRefDataService,"dacourtsToFilter", "");
+        List<DynamicListElement> courtLocations = locationRefDataService.getDaCourtLocations("test");
         assertFalse(courtLocations.isEmpty());
     }
 
