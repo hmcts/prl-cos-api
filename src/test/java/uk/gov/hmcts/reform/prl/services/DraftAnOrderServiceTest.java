2b6c920686dec000cc09f118e19d1269ae29f54d
sahaSonali <sonali.saha@hmcts.net>
2023-02-24 14:09:18 +0000

    Changes for serve order

--- src/test/java/uk/gov/hmcts/reform/prl/services/DraftAnOrderServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/DraftAnOrderServiceTest.java
@@ -247,7 +247,7 @@ public class DraftAnOrderServiceTest {
     public void testToGetDraftOrderDynamicList() {
 
         Map<String, Object> stringObjectMap = new HashMap<>();
-        stringObjectMap = draftAnOrderService.getDraftOrderDynamicList(caseData, "editAndApproveAnOrder");
+        stringObjectMap = draftAnOrderService.getDraftOrderDynamicList(caseData, "adminEditAndApproveAnOrder");
 
         assertNotNull(stringObjectMap.get("draftOrdersDynamicList"));
         assertNotNull(stringObjectMap.get("caseTypeOfApplication"));
@@ -372,7 +372,7 @@ public class DraftAnOrderServiceTest {
     @Test
     public void testGetDraftOrderDynamicList() {
         List<Element<DraftOrder>> draftOrderCollection = new ArrayList<>();
-        Map<String, Object> caseDataMap = draftAnOrderService.getDraftOrderDynamicList(caseData, "editAndApproveAnOrder");
+        Map<String, Object> caseDataMap = draftAnOrderService.getDraftOrderDynamicList(caseData, "adminEditAndApproveAnOrder");
         assertEquals("C100", caseDataMap.get("caseTypeOfApplication"));
     }
 
