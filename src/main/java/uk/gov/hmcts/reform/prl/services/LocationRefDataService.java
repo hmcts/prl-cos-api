6050f2485fc9b544340879a71b5afb8a69c72e7e
sahaSonali <sonali.saha@hmcts.net>
2023-02-26 09:58:29 +0000

    Fix for Cafcass/Cafcass Cymru flag

--- src/main/java/uk/gov/hmcts/reform/prl/services/LocationRefDataService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/LocationRefDataService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.prl.clients.LocationRefDataApi;
+import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicListElement;
 import uk.gov.hmcts.reform.prl.models.court.CourtDetails;
 import uk.gov.hmcts.reform.prl.models.court.CourtVenue;
@@ -52,10 +53,7 @@ public class LocationRefDataService {
                 if (courtList.length == 1) {
                     return true;
                 }
-                List<String> ids = Arrays.stream(courtList).map(ele -> Arrays.stream(ele.split(":")).toArray()[0]
-                        .toString())
-                    .collect(Collectors.toList());
-                return ids.contains(location.getCourtEpimmsId());
+                return Arrays.asList(courtList).contains(location.getCourtEpimmsId());
             })
             .map(this::getDisplayEntry).collect(Collectors.toList()));
     }
@@ -63,32 +61,22 @@ public class LocationRefDataService {
     private DynamicListElement getDisplayEntry(CourtVenue location) {
         String value = concat(concat(concat(location.getSiteName(), " - "), concat(location.getCourtAddress(), " - ")),
                               location.getPostcode());
-        String key = location.getCourtEpimmsId() + ":";
-        if (courtsToFilter.length() > 1) {
-            Optional<String> code = Arrays.stream(courtsToFilter.split(",")).filter(ele -> Arrays.stream(ele.split(":")).toArray()[0]
-                .toString().equalsIgnoreCase(location.getCourtEpimmsId())).findFirst();
-            if (code.isPresent()) {
-                key += Arrays.stream(code.get().split(":")).toArray().length > 1
-                    ? Arrays.stream(code.get().split(":")).toArray()[1] : "";
-                log.info("** Court venue key: {}, value: {} ", key, value);
-            }
-        }
+        String key = location.getCourtEpimmsId();
         return DynamicListElement.builder().code(key).label(value).build();
     }
 
     public String getCourtDetailsFromEpimmsId(String baseLocationId, String authToken) {
-        CourtDetails courtDetails = locationRefDataApi.getCourtDetailsByService(
-            authToken,
-            authTokenGenerator.generate(),
-            SERVICE_ID
-        );
+        CourtDetails courtDetails = locationRefDataApi.getCourtDetailsByService(authToken,
+                                                                                authTokenGenerator.generate(),
+                                                                                SERVICE_ID);
         log.info("courtDetails====> " + courtDetails);
         log.info("baseLocationId====> " + baseLocationId);
-        Optional<CourtVenue> courtVenue = courtDetails.getCourtVenues().stream().filter(location -> !"Scotland".equals(
-                location.getRegion()))
+
+        Optional<CourtVenue> courtVenue = courtDetails.getCourtVenues().stream().filter(location -> !"Scotland".equals(location.getRegion()))
             .filter(location -> FAMILY_COURT_TYPE_ID.equalsIgnoreCase(location.getCourtTypeId()))
             .filter(location -> baseLocationId.equalsIgnoreCase(location.getCourtEpimmsId()))
             .findFirst();
+        log.info("courtVenue====> " + courtVenue);
 
         return courtVenue.map(venue -> venue.getCourtEpimmsId() + "-" + venue.getRegionId()
             + "-" + venue.getCourtName() + "-" + venue.getPostcode() + "-" + venue.getRegion()
