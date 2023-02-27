e364bab1db7513b07c86a8873d5b12a6f3243e1f
priteshkr <pritesh.kumar@hmcts.net>
2023-02-27 15:48:31 +0000

    Removed authorisation

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ServiceRequestUpdateCallbackController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ServiceRequestUpdateCallbackController.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
