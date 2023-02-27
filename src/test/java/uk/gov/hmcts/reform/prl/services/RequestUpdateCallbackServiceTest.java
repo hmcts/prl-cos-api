0dca77677dc2f4d870d4c00699fadb6e702cdfb3
REMYA MARY JAMES <RemyaMary.James@hmcts.net>
2023-02-27 12:46:11 +0000

    updated tests for RequestUpdateCallbackService

--- src/test/java/uk/gov/hmcts/reform/prl/services/RequestUpdateCallbackServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/RequestUpdateCallbackServiceTest.java
@@ -13,13 +13,18 @@ import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.Event;
+import uk.gov.hmcts.reform.ccd.client.model.EventRequestData;
 import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
+import uk.gov.hmcts.reform.prl.clients.ccd.CoreCaseDataService;
+import uk.gov.hmcts.reform.prl.enums.CaseEvent;
 import uk.gov.hmcts.reform.prl.models.court.Court;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.models.dto.payment.PaymentDto;
 import uk.gov.hmcts.reform.prl.models.dto.payment.ServiceRequestUpdateDto;
 import uk.gov.hmcts.reform.prl.services.tab.alltabs.AllTabServiceImpl;
+import uk.gov.hmcts.reform.prl.utils.CaseUtils;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
@@ -27,6 +32,8 @@ import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.CASE_TYPE;
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.JURISDICTION;
 
 @RunWith(MockitoJUnitRunner.Silent.class)
 public class RequestUpdateCallbackServiceTest {
@@ -40,6 +47,10 @@ public class RequestUpdateCallbackServiceTest {
     private final String serviceAuthToken = "Bearer testServiceAuth";
     private final String systemUserId = "systemUserID";
     private final String eventToken = "eventToken";
+
+    private final String userToken = "Bearer testToken";
+
+    private String bearerToken;
 
     @Mock
     private AuthTokenGenerator authTokenGenerator;
@@ -70,6 +81,9 @@ public class RequestUpdateCallbackServiceTest {
 
     @Mock
     private CourtFinderService courtFinderService;
+
+    @Mock
+    CoreCaseDataService coreCaseDataService;
 
     @InjectMocks
     RequestUpdateCallbackService requestUpdateCallbackService;
@@ -134,17 +148,15 @@ public class RequestUpdateCallbackServiceTest {
     @Test
     public void shouldProcessCallback() throws Exception {
         CaseData caseData = CaseData.builder().id(1L).build();
-        CaseDetails caseDetails = CaseDetails.builder().id(Long.valueOf("123")).data(Map.of("id", 1)).build();
+        Map<String, Object> stringObjectMap = caseData.toMap(new ObjectMapper());
+        CaseDetails caseDetails = CaseDetails.builder().id(1L).data(stringObjectMap)
+                .createdDate(LocalDateTime.now()).lastModified(LocalDateTime.now()).build();
         when(coreCaseDataApi.getCase(authToken, serviceAuthToken, caseId.toString())).thenReturn(caseDetails);
         when(objectMapper.convertValue(caseDetails.getData(), CaseData.class)).thenReturn(caseData);
-        doNothing().when(allTabService).updateAllTabsIncludingConfTab(Mockito.any(CaseData.class));
-        when(coreCaseDataApi.startEventForCaseWorker(authToken, serviceAuthToken, systemUserId, jurisdiction,
-                                                     caseType, Long.toString(caseId), eventName
-        ))
-            .thenReturn(buildStartEventResponse(eventName, eventToken));
+
         when(courtFinderService.getNearestFamilyCourt(caseData)).thenReturn(court);
         serviceRequestUpdateDto = ServiceRequestUpdateDto.builder()
-            .ccdCaseNumber(caseId.toString())
+            .ccdCaseNumber(caseDetails.getId().toString())
             .payment(PaymentDto.builder()
                          .paymentAmount("123")
                          .paymentMethod("cash")
@@ -154,6 +166,59 @@ public class RequestUpdateCallbackServiceTest {
                          .build())
             .serviceRequestStatus("Paid")
             .build();
+        CaseEvent caseEvent = CaseEvent.PAYMENT_SUCCESS_CALLBACK;
+        EventRequestData eventRequestData = EventRequestData.builder()
+            .eventId(caseEvent.getValue())
+            .caseTypeId(CASE_TYPE)
+            .ignoreWarning(true)
+            .jurisdictionId(JURISDICTION)
+            .userId(systemUserId)
+            .userToken(userToken)
+            .build();
+        EventRequestData allTabsUpdateEventRequestData = EventRequestData.builder()
+            .eventId(CaseEvent.UPDATE_ALL_TABS.getValue())
+            .caseTypeId(CASE_TYPE)
+            .ignoreWarning(true)
+            .jurisdictionId(JURISDICTION)
+            .userId(systemUserId)
+            .userToken(userToken)
+            .build();
+        when(systemUserService.getSysUserToken()).thenReturn(userToken);
+        when(systemUserService.getUserId(userToken)).thenReturn(systemUserId);
+        when(coreCaseDataService.eventRequest(caseEvent, systemUserId)).thenReturn(eventRequestData);
+        when(coreCaseDataService.eventRequest(CaseEvent.UPDATE_ALL_TABS, systemUserId)).thenReturn(allTabsUpdateEventRequestData);
+
+
+        StartEventResponse startEventResponse = StartEventResponse.builder()
+            .caseDetails(caseDetails)
+            .token(bearerToken).build();
+        when(coreCaseDataService.startUpdate(
+            userToken,eventRequestData, String.valueOf(caseData.getId()),true))
+            .thenReturn(startEventResponse);
+        when(objectMapper.convertValue(caseDetails.getData(), CaseData.class)).thenReturn(caseData);
+
+        CaseData caseDataUpdated = CaseUtils.getCaseDataFromStartUpdateEventResponse(startEventResponse, objectMapper);
+
+        CaseDataContent caseDataContent = CaseDataContent.builder()
+            .data(stringObjectMap)
+            .build();
+        when(coreCaseDataService.createCaseDataContent(startEventResponse,caseDataUpdated)).thenReturn(caseDataContent);
+        when(coreCaseDataService.submitUpdate(userToken, eventRequestData, caseDataContent,String.valueOf(caseData.getId()), true))
+            .thenReturn(caseDetails);
+
+        StartEventResponse allTabsUpdateStartEventResponse = StartEventResponse.builder()
+            .caseDetails(caseDetails)
+            .token(userToken).build();
+        CaseData caseDataUpdatedforAllTabs = CaseUtils.getCaseDataFromStartUpdateEventResponse(allTabsUpdateStartEventResponse, objectMapper);
+        when(coreCaseDataService.startUpdate(
+            userToken,allTabsUpdateEventRequestData, String.valueOf(caseData.getId()),true))
+            .thenReturn(allTabsUpdateStartEventResponse);
+
+        doNothing().when(allTabService).updateAllTabsIncludingConfTabRefactored(userToken,
+                                                                                String.valueOf(caseData.getId()),
+                                                                                allTabsUpdateStartEventResponse,
+                                                                                allTabsUpdateEventRequestData,
+                                                                                caseData);
 
         requestUpdateCallbackService.processCallback(serviceRequestUpdateDto);
         assertEquals(coreCaseDataApi.getCase(authToken, serviceAuthToken, caseId.toString()), caseDetails);
@@ -163,17 +228,15 @@ public class RequestUpdateCallbackServiceTest {
     @Test
     public void shouldProcessPendingCallback() throws Exception {
         CaseData caseData = CaseData.builder().id(1L).build();
-        CaseDetails caseDetails = CaseDetails.builder().id(Long.valueOf("123")).data(Map.of("id", 1)).build();
+        Map<String, Object> stringObjectMap = caseData.toMap(new ObjectMapper());
+        CaseDetails caseDetails = CaseDetails.builder().id(1L).data(stringObjectMap).build();
         when(coreCaseDataApi.getCase(authToken, serviceAuthToken, caseId.toString())).thenReturn(caseDetails);
         when(objectMapper.convertValue(caseDetails.getData(), CaseData.class)).thenReturn(caseData);
         doNothing().when(allTabService).updateAllTabsIncludingConfTab(Mockito.any(CaseData.class));
-        when(coreCaseDataApi.startEventForCaseWorker(authToken, serviceAuthToken, systemUserId, jurisdiction,
-                                                     caseType, Long.toString(caseId), eventFailureName
-        ))
-            .thenReturn(buildStartEventResponse(eventName, eventToken));
+
         when(courtFinderService.getNearestFamilyCourt(caseData)).thenReturn(court);
         serviceRequestUpdateDto = ServiceRequestUpdateDto.builder()
-            .ccdCaseNumber(caseId.toString())
+            .ccdCaseNumber(caseDetails.getId().toString())
             .payment(PaymentDto.builder()
                          .paymentAmount("123")
                          .paymentMethod("cash")
@@ -183,6 +246,61 @@ public class RequestUpdateCallbackServiceTest {
                          .build())
             .serviceRequestStatus("Paidn")
             .build();
+
+        CaseEvent caseEvent = CaseEvent.PAYMENT_FAILURE_CALLBACK;
+        EventRequestData eventRequestData = EventRequestData.builder()
+            .eventId(caseEvent.getValue())
+            .caseTypeId(CASE_TYPE)
+            .ignoreWarning(true)
+            .jurisdictionId(JURISDICTION)
+            .userId(systemUserId)
+            .userToken(userToken)
+            .build();
+        EventRequestData allTabsUpdateEventRequestData = EventRequestData.builder()
+            .eventId(CaseEvent.UPDATE_ALL_TABS.getValue())
+            .caseTypeId(CASE_TYPE)
+            .ignoreWarning(true)
+            .jurisdictionId(JURISDICTION)
+            .userId(systemUserId)
+            .userToken(userToken)
+            .build();
+        when(systemUserService.getSysUserToken()).thenReturn(userToken);
+        when(systemUserService.getUserId(userToken)).thenReturn(systemUserId);
+        when(coreCaseDataService.eventRequest(caseEvent, systemUserId)).thenReturn(eventRequestData);
+        when(coreCaseDataService.eventRequest(CaseEvent.UPDATE_ALL_TABS, systemUserId)).thenReturn(allTabsUpdateEventRequestData);
+
+
+        StartEventResponse startEventResponse = StartEventResponse.builder()
+            .caseDetails(caseDetails)
+            .token(bearerToken).build();
+        when(coreCaseDataService.startUpdate(
+            userToken,eventRequestData, String.valueOf(caseData.getId()),true))
+            .thenReturn(startEventResponse);
+        when(objectMapper.convertValue(caseDetails.getData(), CaseData.class)).thenReturn(caseData);
+
+        CaseData caseDataUpdated = CaseUtils.getCaseDataFromStartUpdateEventResponse(startEventResponse, objectMapper);
+
+        CaseDataContent caseDataContent = CaseDataContent.builder()
+            .data(stringObjectMap)
+            .build();
+        when(coreCaseDataService.createCaseDataContent(startEventResponse,caseDataUpdated)).thenReturn(caseDataContent);
+        when(coreCaseDataService.submitUpdate(userToken, eventRequestData, caseDataContent,String.valueOf(caseData.getId()), true))
+            .thenReturn(caseDetails);
+
+        StartEventResponse allTabsUpdateStartEventResponse = StartEventResponse.builder()
+            .caseDetails(caseDetails)
+            .token(userToken).build();
+        CaseData caseDataUpdatedforAllTabs = CaseUtils.getCaseDataFromStartUpdateEventResponse(allTabsUpdateStartEventResponse, objectMapper);
+        when(coreCaseDataService.startUpdate(
+            userToken,allTabsUpdateEventRequestData, String.valueOf(caseData.getId()),true))
+            .thenReturn(allTabsUpdateStartEventResponse);
+
+        doNothing().when(allTabService).updateAllTabsIncludingConfTabRefactored(userToken,
+                                                                                String.valueOf(caseData.getId()),
+                                                                                allTabsUpdateStartEventResponse,
+                                                                                allTabsUpdateEventRequestData,
+                                                                                caseData);
+
 
         requestUpdateCallbackService.processCallback(serviceRequestUpdateDto);
         assertEquals(coreCaseDataApi.getCase(authToken, serviceAuthToken, caseId.toString()), caseDetails);
