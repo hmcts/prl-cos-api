7ead31892a540cf2ebafd5f75cbc1ee75731d565
Aloknath Datta <alokdatta.uit@gmail.com>
2023-02-27 13:30:00 +0000

    updated test case.

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/FeeAndPayServiceRequestControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/FeeAndPayServiceRequestControllerTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.prl.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -21,7 +20,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
-@Ignore
 @PropertySource(value = "classpath:application.yaml")
 @RunWith(SpringRunner.class)
 public class FeeAndPayServiceRequestControllerTest {
