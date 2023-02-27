0cd226e412cd6412b1a83f530642e655d443a0c7
Yogendra Upasani <yogendra.upasani@hmcts.net>
2023-02-27 23:44:18 +0000

    ISDB-595 Get Next Hearing date CCD callback changes

--- src/main/java/uk/gov/hmcts/reform/prl/clients/HearingApiClient.java
+++ src/main/java/uk/gov/hmcts/reform/prl/clients/HearingApiClient.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.cloud.openfeign.FeignClientProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import uk.gov.hmcts.reform.prl.models.dto.hearingmanagement.NextHearingDetails;
 import uk.gov.hmcts.reform.prl.models.dto.hearings.Hearings;
 
 @FeignClient(
@@ -15,6 +16,13 @@ public interface HearingApiClient {
 
     @GetMapping(path = "/hearings")
     Hearings getHearingDetails(
+        @RequestHeader("Authorization") String authorisation,
+        @RequestHeader("ServiceAuthorization") String serviceAuthorization,
+        @RequestHeader("caseReference") String caseReference
+    );
+
+    @GetMapping(path = "/getNextHearingDate")
+    NextHearingDetails getNextHearingDate(
         @RequestHeader("Authorization") String authorisation,
         @RequestHeader("ServiceAuthorization") String serviceAuthorization,
         @RequestHeader("caseReference") String caseReference
