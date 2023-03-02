d441c3bbcfb8e0ad989dd255a862cb2983bc54cf
2219956 <ramesh.meripe@hmcts.net>
2023-03-02 18:31:29 +0000

    junit Issue fix

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/listwithoutnotice/ListWithoutNoticeControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/listwithoutnotice/ListWithoutNoticeControllerTest.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.reform.prl.controllers.listwithoutnotice;
 
-/*
+
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -9,20 +9,28 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.prl.enums.HearingDateConfirmOptionEnum;
+import uk.gov.hmcts.reform.prl.enums.YesOrNo;
+import uk.gov.hmcts.reform.prl.enums.dio.DioBeforeAEnum;
+import uk.gov.hmcts.reform.prl.models.Element;
+import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicList;
 import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicListElement;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.HearingData;
 import uk.gov.hmcts.reform.prl.services.HearingDataService;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.when;
-
-
 
 @Slf4j
 @RunWith(MockitoJUnitRunner.Silent.class)
@@ -36,6 +44,9 @@ public class ListWithoutNoticeControllerTest {
 
     @Mock
     private ObjectMapper objectMapper;
+
+    @Mock
+    AuthTokenGenerator authTokenGenerator;
 
     public static final String authToken = "Bearer TestAuthToken";
     public static final String serviceAuth = "serviceAuth";
@@ -68,6 +79,80 @@ public class ListWithoutNoticeControllerTest {
     @Test
     public void shouldSeeHearingDetailswithoutListWithNotice() throws Exception {
 
+        DynamicListElement dynamicListElement2 = DynamicListElement.builder()
+            .code("INTER")
+            .label("In Person")
+            .build();
+        List<DynamicListElement> dynamicListElementsList = new ArrayList<>();
+        dynamicListElementsList.add(dynamicListElement2);
+        DynamicList dynamicList = DynamicList.builder()
+            .listItems(dynamicListElementsList)
+            .build();
+        HearingData hearingData = HearingData.builder()
+            .hearingTypes(dynamicList)
+            .confirmedHearingDates(dynamicList)
+            .hearingChannels(dynamicList)
+            .applicantHearingChannel(dynamicList)
+            .hearingChannelDynamicRadioList(dynamicList)
+            .hearingVideoChannels(dynamicList)
+            .hearingTelephoneChannels(dynamicList)
+            .courtList(dynamicList)
+            .localAuthorityHearingChannel(dynamicList)
+            .hearingListedLinkedCases(dynamicList)
+            .applicantSolicitorHearingChannel(dynamicList)
+            .respondentHearingChannel(dynamicList)
+            .respondentSolicitorHearingChannel(dynamicList)
+            .cafcassHearingChannel(dynamicList)
+            .cafcassCymruHearingChannel(dynamicList)
+            .applicantHearingChannel(dynamicList)
+            .hearingDateConfirmOptionEnum(HearingDateConfirmOptionEnum.dateConfirmedInHearingsTab)
+            .additionalHearingDetails("Test")
+            .instructionsForRemoteHearing("Test")
+            .hearingEstimatedHours(5)
+            .hearingEstimatedMinutes(40)
+            .hearingEstimatedDays(15)
+            .allPartiesAttendHearingSameWayYesOrNo(YesOrNo.Yes)
+            .hearingAuthority(DioBeforeAEnum.circuitJudge)
+            .hearingJudgePersonalCode("test")
+            .hearingJudgeLastName("test")
+            .hearingJudgeEmailAddress("Test")
+            .mainApplicantName("Test")
+            .build();
+
+        Element<HearingData> childElement = Element.<HearingData>builder().value(hearingData).build();
+        List<Element<HearingData>> listWithoutNoticeHearingDetails = Collections.singletonList(childElement);
+        Map<String, Object> caseDataUpdated = new HashMap<>();
+        caseDataUpdated.put("listWithoutNoticeHearingDetails",listWithoutNoticeHearingDetails);
+
+
+        CaseData caseData = CaseData.builder()
+            .courtName("testcourt")
+            .listWithoutNoticeHearingDetails(listWithoutNoticeHearingDetails)
+            .build();
+        Map<String, Object> stringObjectMap = caseData.toMap(new ObjectMapper());
+        when(objectMapper.convertValue(stringObjectMap, CaseData.class)).thenReturn(caseData);
+
+        CaseDetails caseDetails = CaseDetails.builder()
+            .id(123L)
+            .data(stringObjectMap)
+            .build();
+
+        CallbackRequest callbackRequest = CallbackRequest.builder()
+            .caseDetails(caseDetails)
+            .build();
+
+        when(hearingPrePopulateService.prePopulateHearingType(authToken)).thenReturn(List.of(DynamicListElement.builder()
+                                                                                                 .build()));
+
+
+        AboutToStartOrSubmitCallbackResponse response = listWithoutNoticeController.prePopulateHearingPageData(authToken,callbackRequest);
+        assertNotNull(response.getData().containsKey("listWithoutNoticeHearingDetails"));
+    }
+
+
+    @Test
+    public void testListWithoutNoticeSubmission() throws Exception {
+
         CaseData caseData = CaseData.builder()
             .courtName("testcourt")
             .build();
@@ -89,9 +174,9 @@ public class ListWithoutNoticeControllerTest {
         caseDataUpdated.put("listWithoutNoticeHearingDetails",List.of(DynamicListElement.builder()
                                                                           .build()));
 
-        AboutToStartOrSubmitCallbackResponse response = listWithoutNoticeController.prePopulateHearingPageData(authToken,callbackRequest);
+        AboutToStartOrSubmitCallbackResponse response = listWithoutNoticeController.listWithoutNoticeSubmission(authToken,callbackRequest);
         assertNotNull(response.getData().containsKey("listWithoutNoticeHearingDetails"));
     }
 
 }
-*/
+
