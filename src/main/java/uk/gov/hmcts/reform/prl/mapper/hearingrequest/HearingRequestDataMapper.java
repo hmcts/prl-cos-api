40a4fe8c7361f46a46d5f9c7195a94c656e9076a
GayatriDeviCh <GayatriDevi.Chadaram@hmcts.net>
2023-03-03 21:35:38 +0000

    issue fixes

--- src/main/java/uk/gov/hmcts/reform/prl/mapper/hearingrequest/HearingRequestDataMapper.java
+++ src/main/java/uk/gov/hmcts/reform/prl/mapper/hearingrequest/HearingRequestDataMapper.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicList;
+import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicListElement;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.HearingData;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.HearingDataPrePopulatedDynamicLists;
 
@@ -16,8 +17,7 @@ public class HearingRequestDataMapper {
     public void mapHearingData(HearingData hearingData, HearingDataPrePopulatedDynamicLists hearingDataPrePopulatedDynamicLists) {
         log.info("Inside Request mapper hearing data****hearingDataPrePopulatedDynamicLists  {}", hearingDataPrePopulatedDynamicLists);
         boolean isHearingDynamicListItemsNullifyReq = (null != hearingDataPrePopulatedDynamicLists) ? false  : true;
-        mapDynamicListItems(hearingData.getHearingTypes(),
-            isHearingDynamicListItemsNullifyReq ? null : hearingDataPrePopulatedDynamicLists.getRetrievedHearingTypes());
+        mapHearingTypesListItems(hearingData,isHearingDynamicListItemsNullifyReq,hearingDataPrePopulatedDynamicLists);
         mapDynamicListItems(hearingData.getConfirmedHearingDates(),
             isHearingDynamicListItemsNullifyReq ? null : hearingDataPrePopulatedDynamicLists.getRetrievedHearingDates());
         mapDynamicListItems(hearingData.getHearingChannels(),
@@ -35,6 +35,23 @@ public class HearingRequestDataMapper {
         mapOtherPartyHearingChannelsMapping(hearingData, isHearingDynamicListItemsNullifyReq
             ? null : hearingDataPrePopulatedDynamicLists.getRetrievedHearingChannels());
         log.info("Inside Request mapper hearing data****  {}", hearingData);
+    }
+
+    private void mapHearingTypesListItems(HearingData hearingData, boolean isHearingDynamicListItemsNullifyReq,
+                                          HearingDataPrePopulatedDynamicLists hearingDataPrePopulatedDynamicLists) {
+        if (null != hearingData.getHearingTypes()) {
+            mapDynamicListItems(hearingData.getHearingTypes(),
+                isHearingDynamicListItemsNullifyReq ? null : hearingDataPrePopulatedDynamicLists.getRetrievedHearingTypes());
+        } else {
+            mapDynamicListItemsForHearingTypesNotSelected(hearingData, hearingDataPrePopulatedDynamicLists);
+        }
+    }
+
+    private void mapDynamicListItemsForHearingTypesNotSelected(HearingData hearingData,
+                                                               HearingDataPrePopulatedDynamicLists hearingDataPrePopulatedDynamicLists) {
+        hearingData.toBuilder().hearingTypes(DynamicList.builder()
+            .value(DynamicListElement.EMPTY)
+            .listItems(hearingDataPrePopulatedDynamicLists.getRetrievedHearingTypes().getListItems()).build()).build();
     }
 
     private void mapOtherPartyHearingChannelsMapping(HearingData hearingData, DynamicList retrievedHearingChannels) {
