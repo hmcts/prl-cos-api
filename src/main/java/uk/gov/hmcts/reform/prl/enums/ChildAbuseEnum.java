78d1cee9ca0bfb740065ea0c7f8a0ff336457278
Praneeth G <praneethkumar.gadhe@hmcts.net>
2023-03-03 14:33:53 +0000

    updated logs

---
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/ChildAbuseEnum.java
@@ -0,0 +1,41 @@
+package uk.gov.hmcts.reform.prl.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@JsonSerialize(using = CustomEnumSerializer.class)
+public enum ChildAbuseEnum {
+
+    @JsonProperty("physicalAbuse")
+    physicalAbuse("physicalAbuse","Physical abuse"),
+
+    @JsonProperty("psychologicalAbuse")
+    psychologicalAbuse("psychologicalAbuse","Psychological abuse"),
+
+    @JsonProperty("sexualAbuse")
+    sexualAbuse("sexualAbuse","Sexual abuse"),
+
+    @JsonProperty("emotionalAbuse")
+    emotionalAbuse("emotionalAbuse","Emotional abuse"),
+
+    @JsonProperty("financialAbuse")
+    financialAbuse("financialAbuse","Financial  abuse");
+
+    private final String id;
+    private final String displayedValue;
+
+    @JsonValue
+    public String getDisplayedValue() {
+        return displayedValue;
+    }
+
+    @JsonCreator
+    public static ChildAbuseEnum getValue(String key) {
+        return ChildAbuseEnum.valueOf(key);
+    }
+
+}
