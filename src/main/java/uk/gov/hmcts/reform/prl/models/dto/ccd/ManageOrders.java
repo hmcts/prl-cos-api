9980dbd6216f5e014bdd375f42b6cd1e9a69cdaf
swaroopa86 <pendyalaswaroopa86@gmail.com>
2023-03-05 17:34:14 +0000

    cafcass fields removed from create orders generic screen

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/ManageOrders.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/ManageOrders.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import uk.gov.hmcts.reform.prl.enums.CafcassReportsEnum;
 import uk.gov.hmcts.reform.prl.enums.ChildArrangementOrderTypeEnum;
 import uk.gov.hmcts.reform.prl.enums.OrderTypeEnum;
 import uk.gov.hmcts.reform.prl.enums.YesOrNo;
@@ -167,11 +166,6 @@ public class ManageOrders {
 
     @JsonProperty("fl404bCustomFields")
     private final FL404b fl404bCustomFields;
-
-    @JsonProperty("isOrderDrawnForCafcass")
-    private final YesOrNo isOrderDrawnForCafcass;
-    @JsonProperty("cafcassReports")
-    private final List<CafcassReportsEnum> cafcassReports;
 
     @Builder.Default
     @JsonProperty("childOption")
