e68645ef5181bf0318111eb5a7354dec528748ea
Aloknath Datta <alokdatta.uit@gmail.com>
2023-03-07 00:46:10 +0000

    CourtNav fixes

--- src/main/java/uk/gov/hmcts/reform/prl/constants/PrlAppsConstants.java
+++ src/main/java/uk/gov/hmcts/reform/prl/constants/PrlAppsConstants.java
@@ -328,4 +328,7 @@ public class PrlAppsConstants {
     public static final String ADJOURNED = "ADJOURNED";
     public static final String POSTPONED = "POSTPONED";
     public static final String COMPLETED = "COMPLETED";
+
+    public static final String COURTNAV = "COURTNAV";
+    public static final String NA_COURTNAV = "NA_COURTNAV";
 }
