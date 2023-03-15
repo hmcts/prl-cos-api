1d76a302187975aec4dbffffb207300cfcf38fb0
2219956 <ramesh.meripe@hmcts.net>
2023-03-15 14:10:20 +0000

    Internal fix

--- src/main/java/uk/gov/hmcts/reform/prl/mapper/hearingrequest/HearingRequestDataMapper.java
+++ src/main/java/uk/gov/hmcts/reform/prl/mapper/hearingrequest/HearingRequestDataMapper.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.prl.enums.HearingDateConfirmOptionEnum;
 import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicList;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.HearingData;
