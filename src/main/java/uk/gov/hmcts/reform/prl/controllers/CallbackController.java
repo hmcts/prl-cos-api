be6b20fc8ca67b78b12b0e9e04ec60b9a9e4a482
sangeetha <sangeetha.ponnusamy@hmcts.net>
2023-03-16 10:21:44 +0000

    PRL -3184 _Updated code _ added Logger

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/CallbackController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/CallbackController.java
@@ -326,6 +326,7 @@ public class CallbackController {
 
         Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
         List<DynamicListElement> courtList = locationRefDataService.getCourtLocations(authorisation);
+        log.info("***CourtList {} ****",courtList);
         caseDataUpdated.put("courtList", DynamicList.builder().value(DynamicListElement.EMPTY).listItems(courtList)
             .build());
         return AboutToStartOrSubmitCallbackResponse.builder().data(caseDataUpdated).build();
