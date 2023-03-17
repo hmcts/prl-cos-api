1b11bf95411f57b722da12f2506494da95e760b2
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2023-03-17 15:29:17 +0000

    ListOfCaseIds call optimaztion in serachCases api

--- src/main/java/uk/gov/hmcts/reform/prl/services/cafcass/CaseDataService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/cafcass/CaseDataService.java
@@ -92,6 +92,7 @@ public class CaseDataService {
 
             cafCassFilter.filter(cafCassResponse);
             getHearingDetails(authorisation, cafCassResponse);
+            getHearingDetailsForAllCases(authorisation, cafCassResponse);
             updateHearingResponse(authorisation, authTokenGenerator.generate(), cafCassResponse);
 
         } else {
@@ -140,6 +141,30 @@ public class CaseDataService {
                                                                                       String.valueOf(cafCassCaseDetail.getId())));
         }
     }
+
+    private void getHearingDetailsForAllCases(String authorisation, CafCassResponse cafCassResponse) {
+
+        List<String> caseIds =
+            cafCassResponse.getCases().stream()
+                .map(
+                    eachCafCassCaseDetail ->
+                        eachCafCassCaseDetail.getId().toString())
+                .collect(Collectors.toList());
+
+        List<Hearings> listOfHearingDetails = hearingService.getHearingsForAllCases(authorisation,caseIds);
+
+        if (!listOfHearingDetails.isEmpty()) {
+            for (CafCassCaseDetail cafCassCaseDetail : cafCassResponse.getCases()) {
+                for (Hearings hearing : listOfHearingDetails) {
+                    if (hearing.getCaseRef().equals(String.valueOf(cafCassCaseDetail.getId()))) {
+                        cafCassCaseDetail.getCaseData().setHearingData(hearing);
+                    }
+                }
+            }
+        }
+
+    }
+
 
     private void updateHearingResponse(String authorisation, String s2sToken, CafCassResponse cafCassResponse) {
 
