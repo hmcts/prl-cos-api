4a203f704685ad4ffd74e2b59744d8fe41f6b06c
parvezhmcts <parvez.qureshi@hmcts.net>
2023-03-21 07:55:29 +0000

    ISDB-649 perf improvment
    - Moved case type of applivation filter to CCD elastic search query

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/request/Bool.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/request/Bool.java
@@ -1,10 +1,13 @@
 package uk.gov.hmcts.reform.prl.models.dto.ccd.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -16,4 +19,9 @@ public class Bool {
     private Filter filter;
 
     private Must must;
+
+    private List<Should> should;
+
+    @JsonProperty("minimum_should_match")
+    public int minimumShouldMatch;
 }
