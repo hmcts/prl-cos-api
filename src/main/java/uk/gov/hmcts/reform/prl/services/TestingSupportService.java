5ffc96c967289d2bd19bdfa78b7a12465c46fef3
sahaSonali <sonali.saha@hmcts.net>
2023-03-21 16:10:08 +0000

    Changes for XUI testing support

--- src/main/java/uk/gov/hmcts/reform/prl/services/TestingSupportService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/TestingSupportService.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 import uk.gov.hmcts.reform.prl.constants.PrlAppsConstants;
-import uk.gov.hmcts.reform.prl.events.CaseDataChanged;
-import uk.gov.hmcts.reform.prl.models.documents.Document;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.services.document.DocumentGenService;
 import uk.gov.hmcts.reform.prl.services.tab.alltabs.AllTabServiceImpl;
@@ -19,12 +16,7 @@ import uk.gov.hmcts.reform.prl.utils.CaseUtils;
 import uk.gov.hmcts.reform.prl.utils.ResourceLoader;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.ISSUED_STATE;
-import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.ROLES;
-import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.SUBMITTED_STATE;
 
 @Slf4j
 @Service
@@ -65,34 +57,6 @@ public class TestingSupportService {
                 .lastModified(initialCaseDetails.getLastModified()).build();
             caseDataUpdated = updatedCaseDetails.getData();
         }
-        return caseDataUpdated;
-    }
-
-    public Map<String, Object> submittedCaseCreation(String authorisation, CallbackRequest callbackRequest) {
-        CaseData data = CaseUtils.getCaseData(callbackRequest.getCaseDetails(), objectMapper);
-        eventPublisher.publishEvent(new CaseDataChanged(data));
-        UserDetails userDetails = userService.getUserDetails(authorisation);
-        List<String> roles = userDetails.getRoles();
-        boolean isCourtStaff = roles.stream().anyMatch(ROLES::contains);
-        String state = callbackRequest.getCaseDetails().getState();
-        Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
-        if (isCourtStaff && (SUBMITTED_STATE.equalsIgnoreCase(state) || ISSUED_STATE.equalsIgnoreCase(state))) {
-            try {
-                log.info("Generating documents for the amended details");
-                caseDataUpdated.putAll(dgsService.generateDocuments(authorisation, data));
-            } catch (Exception e) {
-                log.error("Error regenerating the document", e);
-            }
-        }
-        data = data.toBuilder()
-            .c8Document((Document) caseDataUpdated.get("c8Document"))
-            .c1ADocument((Document) caseDataUpdated.get("c1ADocument"))
-            .c8WelshDocument((Document) caseDataUpdated.get("c8WelshDocument"))
-            .finalDocument((Document) caseDataUpdated.get("finalDocument"))
-            .finalWelshDocument((Document) caseDataUpdated.get("finalWelshDocument"))
-            .c1AWelshDocument((Document) caseDataUpdated.get("c1AWelshDocument"))
-            .build();
-        tabService.updateAllTabsIncludingConfTab(data);
         return caseDataUpdated;
     }
 }
