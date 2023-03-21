24310468be5ffcc772c051b73b60beb730660df0
Malik <hamza.malik@hmcts.net>
2023-03-21 21:10:29 +0000

    revert

--- src/main/java/uk/gov/hmcts/reform/prl/enums/LanguagePreference.java
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/LanguagePreference.java
@@ -26,6 +26,6 @@ public enum LanguagePreference {
             .map(YesOrNo.Yes::equals)
             .orElse(false);
 
-        return preferredLanguageIsWelsh ? LanguagePreference.welsh : LanguagePreference.welsh;
+        return preferredLanguageIsWelsh ? LanguagePreference.welsh : LanguagePreference.english;
     }
 }
