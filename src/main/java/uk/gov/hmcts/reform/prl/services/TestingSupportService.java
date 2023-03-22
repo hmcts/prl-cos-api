a89da14c6c04e08d663a9071d2ee1bb4af2d93b7
sealDebolina <debolina.seal@hmcts.net>
2023-03-22 15:49:06 +0000

    controller and service

--- src/main/java/uk/gov/hmcts/reform/prl/services/TestingSupportService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/TestingSupportService.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.prl.services;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.prl.constants.PrlAppsConstants;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+import uk.gov.hmcts.reform.prl.repositories.CaseRepository;
 import uk.gov.hmcts.reform.prl.services.document.DocumentGenService;
 import uk.gov.hmcts.reform.prl.services.tab.alltabs.AllTabServiceImpl;
 import uk.gov.hmcts.reform.prl.utils.CaseUtils;
@@ -34,9 +36,11 @@ public class TestingSupportService {
     private final UserService userService;
     @Autowired
     private final DocumentGenService dgsService;
+    @Autowired
+    CaseRepository caseRepository;
 
     private static final String VALID_C100_INPUT_JSON = "C100_Dummy_CaseDetails.json";
-
+    private static final String VALID_C100_CITIZEN_INPUT_JSON = "C100_citizen_Dummy_CaseDetails.json";
     private static final String VALID_FL401_INPUT_JSON = "FL401_Dummy_CaseDetails.json";
 
     public Map<String, Object> initiateCaseCreation(CallbackRequest callbackRequest) throws Exception {
@@ -58,5 +62,13 @@ public class TestingSupportService {
             caseDataUpdated = updatedCaseDetails.getData();
         }
         return caseDataUpdated;
+    }
+
+    public CaseDetails initiateCaseCreation_citizen()throws Exception {
+        String requestBody;
+        Map<String, Object> caseDataUpdated = new HashMap<>();
+        requestBody=ResourceLoader.loadJson(VALID_C100_CITIZEN_INPUT_JSON);
+        CaseDetails dummyCaseDetails = objectMapper.readValue(requestBody, CaseDetails.class);
+     return dummyCaseDetails;
     }
 }
