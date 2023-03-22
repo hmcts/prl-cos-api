5863ef3aaa8797a6ed254f343452df4734eb8cda
sahaSonali <sonali.saha@hmcts.net>
2023-03-22 21:38:33 +0000

    Changes for XUI testing support

--- src/main/java/uk/gov/hmcts/reform/prl/services/TestingSupportService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/TestingSupportService.java
@@ -10,6 +10,7 @@ import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.prl.constants.PrlAppsConstants;
 import uk.gov.hmcts.reform.prl.events.CaseDataChanged;
+import uk.gov.hmcts.reform.prl.models.complextypes.StatementOfTruth;
 import uk.gov.hmcts.reform.prl.models.documents.Document;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.services.document.DocumentGenService;
@@ -17,9 +18,15 @@ import uk.gov.hmcts.reform.prl.services.tab.alltabs.AllTabServiceImpl;
 import uk.gov.hmcts.reform.prl.utils.CaseUtils;
 import uk.gov.hmcts.reform.prl.utils.ResourceLoader;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.DATE_SUBMITTED_FIELD;
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.ISSUE_DATE_FIELD;
 import static uk.gov.hmcts.reform.prl.enums.Event.TS_SOLICITOR_APPLICATION;
 
 @Slf4j
@@ -49,13 +56,14 @@ public class TestingSupportService {
 
     private static final String VALID_C100_GATEKEEPING_INPUT_JSON = "C100_Dummy_Gatekeeping_CaseDetails.json";
 
-    private static final String VALID_FL401_GATEKEEPING_INPUT_JSON = "FL401_Dummy_Draft_CaseDetails.json";
+    private static final String VALID_FL401_GATEKEEPING_INPUT_JSON = "FL401_Dummy_Gatekeeping_CaseDetails.json";
 
     public Map<String, Object> initiateCaseCreation(CallbackRequest callbackRequest) throws Exception {
         String requestBody;
         CaseDetails initialCaseDetails = callbackRequest.getCaseDetails();
         CaseData initialCaseData = CaseUtils.getCaseData(callbackRequest.getCaseDetails(), objectMapper);
         Map<String, Object> caseDataUpdated = new HashMap<>();
+        boolean adminCreateApplication = false;
         if (TS_SOLICITOR_APPLICATION.getId().equalsIgnoreCase(callbackRequest.getEventId())) {
             if (PrlAppsConstants.C100_CASE_TYPE.equalsIgnoreCase(initialCaseData.getCaseTypeOfApplication())) {
                 requestBody = ResourceLoader.loadJson(VALID_C100_DRAFT_INPUT_JSON);
@@ -68,14 +76,29 @@ public class TestingSupportService {
             } else {
                 requestBody = ResourceLoader.loadJson(VALID_FL401_GATEKEEPING_INPUT_JSON);
             }
+            adminCreateApplication = true;
         }
         CaseDetails dummyCaseDetails = objectMapper.readValue(requestBody, CaseDetails.class);
         if (dummyCaseDetails != null) {
+            CaseData dummyCaseData = CaseUtils.getCaseData(dummyCaseDetails, objectMapper);
             CaseDetails updatedCaseDetails = dummyCaseDetails.toBuilder()
                 .id(initialCaseDetails.getId())
                 .createdDate(initialCaseDetails.getCreatedDate())
-                .lastModified(initialCaseDetails.getLastModified()).build();
+                .lastModified(initialCaseDetails.getLastModified())
+                .build();
             caseDataUpdated = updatedCaseDetails.getData();
+            if (adminCreateApplication) {
+                caseDataUpdated.put(
+                    DATE_SUBMITTED_FIELD,
+                    DateTimeFormatter.ISO_LOCAL_DATE.format(ZonedDateTime.now(ZoneId.of("Europe/London")))
+                );
+                caseDataUpdated.put(ISSUE_DATE_FIELD, LocalDate.now());
+                if (PrlAppsConstants.FL401_CASE_TYPE.equalsIgnoreCase(initialCaseData.getCaseTypeOfApplication())
+                    && null != dummyCaseData.getFl401StmtOfTruth()) {
+                    StatementOfTruth statementOfTruth = dummyCaseData.getFl401StmtOfTruth().toBuilder().date(LocalDate.now()).build();
+                    caseDataUpdated.put("fl401StmtOfTruth", statementOfTruth);
+                }
+            }
         }
         return caseDataUpdated;
     }
