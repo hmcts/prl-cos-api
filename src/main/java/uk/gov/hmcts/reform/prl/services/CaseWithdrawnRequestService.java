815f217d8b24cea987bdb343d542c57921e3fc43
sahaSonali <sonali.saha@hmcts.net>
2023-03-27 14:49:35 +0100

    Fix for Case status not changing to Withdrawn

---
+++ src/main/java/uk/gov/hmcts/reform/prl/services/CaseWithdrawnRequestService.java
@@ -0,0 +1,67 @@
+package uk.gov.hmcts.reform.prl.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
+import uk.gov.hmcts.reform.prl.enums.State;
+import uk.gov.hmcts.reform.prl.enums.YesOrNo;
+import uk.gov.hmcts.reform.prl.models.complextypes.WithdrawApplication;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+import uk.gov.hmcts.reform.prl.utils.CaseUtils;
+
+import java.util.Optional;
+
+import static java.util.Optional.ofNullable;
+import static uk.gov.hmcts.reform.prl.enums.YesOrNo.Yes;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CaseWithdrawnRequestService {
+
+    private final ObjectMapper objectMapper;
+    public static final String APPLICATION_WITHDRAWN_SUCCESS_LABEL = "# Application withdrawn";
+    public static final String APPLICATION_WITHDRAWN_STATUS_LABEL = "### What happens next \n\n This case will now display as “withdrawn” in "
+        + "your case list.";
+
+    public static final String APPLICATION_WITHDRAWN_REQUEST_LABEL = "# Requested Application Withdrawal";
+    public static final String APPLICATION_WITHDRAWN_REQUEST_STATUS_LABEL = "### What happens next \n\n The court will consider your "
+        + "withdrawal request.";
+
+    public static final String APPLICATION_NOT_WITHDRAWN_REQUEST_LABEL = "# Application not withdrawn";
+
+    public SubmittedCallbackResponse caseWithdrawnRequestSubmitted(CallbackRequest callbackRequest) {
+
+        CaseData caseData = CaseUtils.getCaseData(callbackRequest.getCaseDetails(), objectMapper);
+        log.info("CaseWithdrawnRequestController CaseData ==> " + caseData);
+        WithdrawApplication withDrawApplicationData = caseData.getWithDrawApplicationData();
+        Optional<YesOrNo> withdrawApplication = ofNullable(withDrawApplicationData.getWithDrawApplication());
+        String caseWithdrawnConfirmationHeader;
+        String caseWithdrawnConfirmationBodyPrefix;
+        log.info("withdrawApplication ==> " + withdrawApplication);
+        log.info("state ==> " + caseData.getState());
+        if ((withdrawApplication.isPresent() && Yes.equals(withdrawApplication.get()))) {
+            if (State.CASE_ISSUE.equals(caseData.getState())
+                || State.AWAITING_RESUBMISSION_TO_HMCTS.equals(caseData.getState()) || State.GATE_KEEPING.equals(caseData.getState())) {
+                caseWithdrawnConfirmationHeader = APPLICATION_WITHDRAWN_REQUEST_LABEL;
+                caseWithdrawnConfirmationBodyPrefix = APPLICATION_WITHDRAWN_REQUEST_STATUS_LABEL;
+            } else {
+                caseWithdrawnConfirmationHeader = APPLICATION_WITHDRAWN_SUCCESS_LABEL;
+                caseWithdrawnConfirmationBodyPrefix = APPLICATION_WITHDRAWN_STATUS_LABEL;
+            }
+        } else {
+            caseWithdrawnConfirmationHeader = APPLICATION_NOT_WITHDRAWN_REQUEST_LABEL;
+            caseWithdrawnConfirmationBodyPrefix = " \n\n ";
+        }
+        log.info("caseWithdrawnConfirmationHeader ==> " + caseWithdrawnConfirmationHeader);
+        log.info("caseWithdrawnConfirmationBodyPrefix ==> " + caseWithdrawnConfirmationBodyPrefix);
+
+        return SubmittedCallbackResponse.builder().confirmationHeader(
+            caseWithdrawnConfirmationHeader).confirmationBody(
+            caseWithdrawnConfirmationBodyPrefix
+        ).build();
+    }
+}
