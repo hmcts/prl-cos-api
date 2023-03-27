d74c046a53b1e011603eca8eebda17f01097f4fd
sahaSonali <sonali.saha@hmcts.net>
2023-03-27 01:12:15 +0100

    Fix for Case status not changing to Withdrawn

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/CallbackController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/CallbackController.java
@@ -394,10 +394,9 @@ public class CallbackController {
                 } else if (PrlAppsConstants.FL401_CASE_TYPE.equalsIgnoreCase(caseData.getCaseTypeOfApplication())) {
                     solicitorEmailService.sendWithDrawEmailToFl401Solicitor(caseDetails, userDetails);
                 }
-                log.info("previousState ==> " + previousState);
-
-                if (previousState.isPresent() && !RETURN_STATE.equalsIgnoreCase(previousState.get())) {
-                    log.info("inside case state update ===== ");
+                log.info("state ==> " + caseData.getState());
+                if (!State.AWAITING_RESUBMISSION_TO_HMCTS.equals(caseData.getState())) {
+                    log.info("inside case state update as withdrawn ===== ");
                     caseDataUpdated.put("state", WITHDRAWN_STATE);
                     caseData = caseData.toBuilder().state(State.CASE_WITHDRAWN).build();
                     caseDataUpdated.putAll(caseSummaryTab.updateTab(caseData));
