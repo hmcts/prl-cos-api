10e0a7ca205298297d825a5c987812324491bbd9
GayatriDeviCh <GayatriDevi.Chadaram@hmcts.net>
2023-03-31 17:19:51 +0100

    PRL-3312 initial code changes

---
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/gatekeeping/ListOnNoticeController.java
@@ -0,0 +1,55 @@
+package uk.gov.hmcts.reform.prl.controllers.gatekeeping;
+
+import java.util.List;
+import java.util.Map;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static uk.gov.hmcts.reform.prl.enums.manageorders.ManageOrdersOptionsEnum.servedSavedOrders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.prl.enums.YesOrNo;
+import uk.gov.hmcts.reform.prl.enums.gatekeeping.ListOnNoticeReasonsEnum;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+import uk.gov.hmcts.reform.prl.utils.CaseUtils;
+
+@Slf4j
+@RestController
+@SecurityRequirement(name = "Bearer Authentication")
+public class ListOnNoticeController {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @PostMapping(path = "/listOnNotice/reasonUpdation/mid-event", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
+    @Operation(description = "Callback to amend order mid-event")
+    @SecurityRequirement(name = "Bearer Authentication")
+    public AboutToStartOrSubmitCallbackResponse serveOrderMidEvent(
+        @RequestHeader(org.springframework.http.HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
+        @RequestBody CallbackRequest callbackRequest) {
+        CaseData caseData = CaseUtils.getCaseData(callbackRequest.getCaseDetails(), objectMapper);
+        log.info("*** mid event triggered for List ON Notice : {}", caseData.getId());
+        Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
+        if (null != caseData.getListOnNoticeReasonsEnum()) {
+            log.info("*** inside null check for reasons Enum : {}", getReasonsSelected(caseData.getListOnNoticeReasonsEnum()));
+            //caseDataUpdated.put("additionalReasonsForListOnNotice", getReasonsSelected(caseData.getListOnNoticeReasonsEnum()));
+        }
+        return AboutToStartOrSubmitCallbackResponse.builder().data(caseDataUpdated).build();
+    }
+
+    private String getReasonsSelected(List<ListOnNoticeReasonsEnum> listOnNoticeReasonsEnum) {
+        String reasonsSelected = "";
+        listOnNoticeReasonsEnum.stream().forEach(reason -> reasonsSelected.concat(reason.getDisplayedValue()+"\n"));
+        return reasonsSelected;
+    }
+}
