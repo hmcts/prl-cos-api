738ae442aa5e66b7c83b091dbe9bfb62c31dcd49
GayatriDeviCh <GayatriDevi.Chadaram@hmcts.net>
2023-04-04 10:59:56 +0100

    checkstyle issue fix

--- src/main/java/uk/gov/hmcts/reform/prl/enums/gatekeeping/ListOnNoticeReasonsEnum.java
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/gatekeeping/ListOnNoticeReasonsEnum.java
@@ -16,13 +16,18 @@ public enum ListOnNoticeReasonsEnum {
     @JsonProperty("noEvidenceOfImmediateRiskOfHarmToTheChildren")
     noEvidenceOfImmediateRiskOfHarmToTheChildren("There is no evidence of immediate risk of harm to the child[ren]"),
     @JsonProperty("bothPartiesInformationAndSafeguardingNecessaryToCourt")
-    bothPartiesInformationAndSafeguardingNecessaryToCourt("Information from both parties and safegaurding is necessary to enable the court to determine the long-term arrangements."),
+    bothPartiesInformationAndSafeguardingNecessaryToCourt(
+        "Information from both parties and safegaurding is necessary to enable the court to determine the long-term arrangements."),
     @JsonProperty("childrenResideWithApplicantAndBothProtectedByNonMolestationOrder")
-    childrenResideWithApplicantAndBothProtectedByNonMolestationOrder("The child[ren] reside with applicant and both are protected by a Non-Molestation Order"),
+    childrenResideWithApplicantAndBothProtectedByNonMolestationOrder(
+        "The child[ren] reside with applicant and both are protected by a Non-Molestation Order"),
     @JsonProperty("noEvidenceOnRespondentSeeksToRemovetheChildrenFromApplicantsCare")
-    noEvidenceOnRespondentSeeksToRemovetheChildrenFromApplicantsCare("There is no evidence to suggest that the respondent seeks to remove the child[ren] from the applicant's care and therefore there is no genuine emergency"),
+    noEvidenceOnRespondentSeeksToRemovetheChildrenFromApplicantsCare(
+        "There is no evidence to suggest that the respondent seeks to remove the child[ren] from the applicant's care and therefore "
+            + "there is no genuine emergency"),
     @JsonProperty("noEvidenceOnRespondentSeekToFrustrateTheProcessIfTheyWereGivenNotice")
-    noEvidenceOnRespondentSeekToFrustrateTheProcessIfTheyWereGivenNotice("There is no evidence to suggest that the respondent would seek to frustrate the process if they were given notice"),
+    noEvidenceOnRespondentSeekToFrustrateTheProcessIfTheyWereGivenNotice(
+        "There is no evidence to suggest that the respondent would seek to frustrate the process if they were given notice"),
     @JsonProperty("itsNotWithoutNoticeButItsUrgent")
     itsNotWithoutNoticeButItsUrgent("It is not without notice but it is urgent");
 
