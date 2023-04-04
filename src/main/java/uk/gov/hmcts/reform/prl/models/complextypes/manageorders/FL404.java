2956ca291497d312e3c90bfb6f5cd7e8667ed0e7
JamesCutts00 <James.Cutts@HMCTS.ORG>
2023-04-04 09:21:26 +0100

    Added new field from prl-3194

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/manageorders/FL404.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/manageorders/FL404.java
@@ -50,6 +50,7 @@ public class FL404 {
     private final String fl404bAddMoreDetailsProperty;
     private final String fl404bAddSchool;
     private final String fl404bAddMoreDetailsSchool;
+    private final String fl404bAddFurtherDetailsSchool;
     private final String fl404bCourtName1;
     private final Address fl404bOtherCourtAddress;
     private final String fl404bCostOfApplication;
