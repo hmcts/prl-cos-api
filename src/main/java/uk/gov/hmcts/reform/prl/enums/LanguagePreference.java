f63591b9b3c4cff288f45b602ff0cfd59bd8b28d
SasankAchyuta9 <VenkataSaiMadhavaSasank.Achyuta@HMCTS.NET>
2023-04-05 12:37:24 +0100

    made changes for email service for sending in fee and pay controller.

--- src/main/java/uk/gov/hmcts/reform/prl/enums/LanguagePreference.java
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/LanguagePreference.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
-import uk.gov.hmcts.reform.prl.repositories.CcdCaseApi;
 
 import java.util.Optional;
 
@@ -21,27 +18,17 @@ public enum LanguagePreference {
     @JsonProperty("welsh")
     welsh("Welsh");
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CcdCaseApi.class);
     private final String displayedValue;
 
     public static LanguagePreference getLanguagePreference(CaseData caseData) {
         boolean preferredLanguageIsWelsh = Optional.ofNullable(caseData.getLanguagePreferenceWelsh())
             .map(YesOrNo.Yes::equals)
             .orElse(false);
-        LOGGER.info("****************The Language preference "
-                        + "for email I received is ***************: "
-                        + "{}", caseData.getLanguagePreferenceWelsh());
 
         return preferredLanguageIsWelsh ? LanguagePreference.welsh : LanguagePreference.english;
     }
 
     public static LanguagePreference getPreferenceLanguage(CaseData caseData) {
-        LOGGER.info("****************The doc in welsh "
-                        + "for email I received is ***************: "
-                        + "{}", caseData.getWelshLanguageRequirement());
-        LOGGER.info("****************The language for completion "
-                        + "for email I received is ***************: "
-                        + "{}", caseData.getWelshLanguageRequirementApplication());
         return YesOrNo.Yes.equals(caseData.getWelshLanguageRequirement())
             && welsh.equals(caseData.getWelshLanguageRequirementApplication()) ? LanguagePreference.welsh : LanguagePreference.english;
     }
