788b7ecbd2ebbc4614434c854f69b2110d1d7c23
parvezhmcts <parvez.qureshi@hmcts.net>
2023-04-05 17:33:56 +0100

    ISDB-319 C21 draft order changes
    - added logger for debug

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/DraftAnOrderController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/DraftAnOrderController.java
@@ -119,7 +119,7 @@ public class DraftAnOrderController {
         Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
         caseDataUpdated.put("caseTypeOfApplication", CaseUtils.getCaseTypeOfApplication(caseData));
 
-        log.info("C21 Draft order options in callback:: {}", (null != caseData.getManageOrders())
+        log.info("C21 Draft order options in callback in populate-draft-order-fields:: {}", (null != caseData.getManageOrders())
             ? caseData.getManageOrders().getC21OrderOptions() : null);
 
         if (null != caseData.getCreateSelectOrderOptions()
