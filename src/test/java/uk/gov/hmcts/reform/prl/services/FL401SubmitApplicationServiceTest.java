86a1308cabf30eb0980c6314ea03921c7fa9d0d1
bharadwajsairam <bharadwajsairam001@gmail.com>
2023-04-06 17:43:11 +0100

    checkstyle fix

--- src/test/java/uk/gov/hmcts/reform/prl/services/FL401SubmitApplicationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/FL401SubmitApplicationServiceTest.java
@@ -38,7 +38,6 @@ import uk.gov.hmcts.reform.prl.models.court.CourtVenue;
 import uk.gov.hmcts.reform.prl.models.court.ServiceArea;
 import uk.gov.hmcts.reform.prl.models.documents.Document;
 import uk.gov.hmcts.reform.prl.models.dto.GeneratedDocumentInfo;
-import uk.gov.hmcts.reform.prl.models.dto.ccd.CallbackResponse;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseDetails;
 import uk.gov.hmcts.reform.prl.models.language.DocumentLanguage;
