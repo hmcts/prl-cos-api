0a8c9c15a00dbe9e8100a14b9fdee62c5d87db24
parvezhmcts <parvez.qureshi@hmcts.net>
2023-04-12 18:03:05 +0100

    PRL_3188 C21 draft order hearing screen changes
    - updating logger

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/DraftAnOrderController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/DraftAnOrderController.java
@@ -226,6 +226,9 @@ public class DraftAnOrderController {
     public AboutToStartOrSubmitCallbackResponse prepareDraftOrderCollection(
         @RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation,
         @RequestBody CallbackRequest callbackRequest) {
+        log.info("verifying hearing data while submitting************* {} ", objectMapper.convertValue(
+            callbackRequest.getCaseDetails().getData(),
+            CaseData.class));
         return AboutToStartOrSubmitCallbackResponse.builder().data(draftAnOrderService.prepareDraftOrderCollection(
             authorisation,
             callbackRequest
