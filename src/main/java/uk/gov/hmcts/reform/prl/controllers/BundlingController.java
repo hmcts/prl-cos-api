b89d4e5f08bbcd8fb87c0deba87a73f877df8b4a
REMYA MARY JAMES <RemyaMary.James@hmcts.net>
2023-04-13 17:21:49 +0100

    updated

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/BundlingController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/BundlingController.java
@@ -34,6 +34,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static java.util.Objects.nonNull;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
