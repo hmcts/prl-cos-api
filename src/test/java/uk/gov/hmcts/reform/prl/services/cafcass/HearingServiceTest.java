2f67ee94640c384a366bf3849afa4d7a8a16b978
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2023-04-18 15:17:07 +0100

    Code Coverage for Hearing Service

--- src/test/java/uk/gov/hmcts/reform/prl/services/cafcass/HearingServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/cafcass/HearingServiceTest.java
@@ -9,7 +9,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.HttpServerErrorException;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.prl.clients.cafcass.HearingApiClient;
 import uk.gov.hmcts.reform.prl.models.cafcass.hearing.CaseHearing;
@@ -18,8 +20,11 @@ import uk.gov.hmcts.reform.prl.models.cafcass.hearing.Hearings;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.Silent.class)
@@ -62,7 +67,8 @@ public class HearingServiceTest {
     @Test
     @DisplayName("test case for HearingService.")
     public void getHearingsTestException() {
-        hearingApiClient = null;
+
+        when(hearingApiClient.getHearingDetails(any(),any(),any())).thenThrow(new HttpServerErrorException(HttpStatus.BAD_GATEWAY));
 
         Hearings response =
             hearingService.getHearings(authToken, caseReferenceNumber);
@@ -77,10 +83,10 @@ public class HearingServiceTest {
         final List<CaseHearing> caseHearings = new ArrayList();
 
         final CaseHearing caseHearing = CaseHearing.caseHearingWith().hearingID(Long.valueOf("1234"))
-                .hmcStatus("LISTED").hearingType("ABA5-APL").hearingTypeValue("Appeal").hearingDaySchedule(
+            .hmcStatus("LISTED").hearingType("ABA5-APL").hearingTypeValue("Appeal").hearingDaySchedule(
                 List.of(
                     HearingDaySchedule.hearingDayScheduleWith().hearingVenueName("BRENTFORD COUNTY COURT AND FAMILY COURT")
-                            .hearingStartDateTime(LocalDateTime.parse("2023-01-24T13:00:00")).hearingEndDateTime(LocalDateTime.parse(
+                        .hearingStartDateTime(LocalDateTime.parse("2023-01-24T13:00:00")).hearingEndDateTime(LocalDateTime.parse(
                             "2023-01-24T16:00:00")).build())).build();
 
         caseHearings.add(caseHearing);
@@ -105,10 +111,10 @@ public class HearingServiceTest {
         final List<CaseHearing> caseHearings = new ArrayList();
 
         final CaseHearing caseHearing = CaseHearing.caseHearingWith().hearingID(Long.valueOf("1234"))
-                .hmcStatus("EXCEPTION").hearingType("ABA5-APL").hearingTypeValue("Appeal").hearingDaySchedule(
+            .hmcStatus("EXCEPTION").hearingType("ABA5-APL").hearingTypeValue("Appeal").hearingDaySchedule(
                 List.of(
                     HearingDaySchedule.hearingDayScheduleWith().hearingVenueName("BRENTFORD COUNTY COURT AND FAMILY COURT")
-                            .hearingStartDateTime(LocalDateTime.parse("2023-01-24T13:00:00")).hearingEndDateTime(LocalDateTime.parse(
+                        .hearingStartDateTime(LocalDateTime.parse("2023-01-24T13:00:00")).hearingEndDateTime(LocalDateTime.parse(
                             "2023-01-24T16:00:00")).build())).build();
 
         caseHearings.add(caseHearing);
@@ -124,6 +130,54 @@ public class HearingServiceTest {
             hearingService.getHearings(authToken, caseReferenceNumber);
 
         Assert.assertNull(response);
+    }
+
+    @Test
+    @DisplayName("test case for HearingService to get all the hearings for all the caseIds.")
+    public void getHearingsForAllCasesTestSuccess() {
+
+        Map<String, String> caseIdWithRegionIdMap = new HashMap<>();
+        caseIdWithRegionIdMap.put("testCaseId", "testRegionId");
+
+        final List<CaseHearing> caseHearings = new ArrayList();
+
+        final CaseHearing caseHearing = CaseHearing.caseHearingWith().hearingID(Long.valueOf("1234"))
+            .hmcStatus("LISTED").hearingType("ABA5-APL").hearingTypeValue("Appeal").hearingDaySchedule(
+                List.of(
+                    HearingDaySchedule.hearingDayScheduleWith().hearingVenueName("BRENTFORD COUNTY COURT AND FAMILY COURT")
+                        .hearingStartDateTime(LocalDateTime.parse("2023-01-24T13:00:00")).hearingEndDateTime(LocalDateTime.parse(
+                            "2023-01-24T16:00:00")).build())).build();
+
+        caseHearings.add(caseHearing);
+
+        hearings = new Hearings();
+        hearings.setCaseRef("caseReference");
+        hearings.setCaseHearings(caseHearings);
+        List<Hearings> listOfHearingDetails = new ArrayList<>();
+        listOfHearingDetails.add(hearings);
+        ReflectionTestUtils.setField(hearingService, "hearingStatusList", List.of("LISTED"));
+        when(authTokenGenerator.generate()).thenReturn(s2sToken);
+        when(hearingApiClient.getHearingDetailsForAllCaseIds(authToken, authTokenGenerator.generate(),caseIdWithRegionIdMap)).thenReturn(listOfHearingDetails);
+        List<Hearings> response =
+            hearingService.getHearingsForAllCases(authToken, caseIdWithRegionIdMap);
+
+        Assert.assertNotNull(response);
+
+    }
+
+    @Test
+    @DisplayName("test case for HearingService getHearingsForAllCases Exception.")
+    public void getHearingsForAllCasesTestException() {
+
+        Map<String, String> caseIdWithRegionIdMap = new HashMap<>();
+        caseIdWithRegionIdMap.put("testCaseId", "testRegionId");
+
+        when(hearingApiClient.getHearingDetailsForAllCaseIds(any(),any(),any())).thenThrow(new HttpServerErrorException(HttpStatus.BAD_GATEWAY));
+
+        List<Hearings> response =
+            hearingService.getHearingsForAllCases(authToken, caseIdWithRegionIdMap);
+
+        Assert.assertEquals(null, response);
     }
 
 
