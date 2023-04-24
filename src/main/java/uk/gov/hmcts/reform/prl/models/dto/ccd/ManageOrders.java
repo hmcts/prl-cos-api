30330c8cc9715e4a996238e27de21f27b6caed97
parvezhmcts <parvez.qureshi@hmcts.net>
2023-04-24 17:45:12 +0100

    Revert "removing as its not a wrapper class"
    This reverts commit 6742a89f8cac7b7b1ca4cf740275c421d4c79b3b.

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/ManageOrders.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/ManageOrders.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.prl.models.dto.ccd;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -252,9 +253,12 @@ public class ManageOrders {
     private final List<Element<PostalInformation>> postalInformationCaOnlyC47a;
 
     @JsonProperty("ordersHearingDetails")
+    @JsonUnwrapped
+    @Builder.Default
     private final List<Element<HearingData>> ordersHearingDetails;
 
-    @JsonProperty("solicitorOrdersHearingDetails")
+    @JsonUnwrapped
+    @Builder.Default
     private final List<Element<HearingData>> solicitorOrdersHearingDetails;
 
     private YesOrNo isOrderCreatedBySolicitor;
