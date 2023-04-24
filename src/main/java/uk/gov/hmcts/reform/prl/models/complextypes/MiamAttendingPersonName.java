2ca487bb0775e749b93da5797985cd1d05f26285
sahaSonali <sonali.saha@hmcts.net>
2023-04-24 21:00:08 +0100

    Changes to save SDO order in CCD

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/MiamAttendingPersonName.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/MiamAttendingPersonName.java
@@ -1,11 +1,13 @@
 package uk.gov.hmcts.reform.prl.models.complextypes;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class MiamAttendingPersonName {
 
     @JsonProperty("miamAttendingPersonName")
