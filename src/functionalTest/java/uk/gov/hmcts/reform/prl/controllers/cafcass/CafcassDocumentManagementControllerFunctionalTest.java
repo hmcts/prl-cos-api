2a6a1a2c49c640b6043898d92c3869caa76dbdc1
Yogendra Upasani <yogendra.upasani@hmcts.net>
2023-04-26 22:24:26 +0100

    PRL-3557 Cafcass Response to include court type id and name

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/cafcass/CafcassDocumentManagementControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/cafcass/CafcassDocumentManagementControllerFunctionalTest.java
@@ -18,8 +18,11 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.WebApplicationContext;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.document.am.feign.CaseDocumentClient;
+import uk.gov.hmcts.reform.idam.client.models.UserInfo;
 import uk.gov.hmcts.reform.prl.services.AuthorisationService;
+import uk.gov.hmcts.reform.prl.services.SystemUserService;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 import static java.util.UUID.randomUUID;
@@ -32,11 +35,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
-import static uk.gov.hmcts.reform.prl.utils.TestConstants.AUTHORISATION_HEADER;
-import static uk.gov.hmcts.reform.prl.utils.TestConstants.CAFCASS_DOCUMENT_DOWNLOAD_ENDPOINT;
-import static uk.gov.hmcts.reform.prl.utils.TestConstants.SERVICE_AUTHORISATION_HEADER;
-import static uk.gov.hmcts.reform.prl.utils.TestConstants.TEST_AUTH_TOKEN;
-import static uk.gov.hmcts.reform.prl.utils.TestConstants.TEST_SERVICE_AUTH_TOKEN;
+import static uk.gov.hmcts.reform.prl.constants.cafcass.CafcassAppConstants.CAFCASS_USER_ROLE;
+import static uk.gov.hmcts.reform.prl.utils.TestConstants.*;
 
 @Slf4j
 @SpringBootTest
@@ -55,6 +55,9 @@ public class CafcassDocumentManagementControllerFunctionalTest {
     private AuthorisationService authorisationService;
 
     @MockBean
+    private SystemUserService systemUserService;
+
+    @MockBean
     private AuthTokenGenerator authTokenGenerator;
 
     @Before
@@ -65,12 +68,16 @@ public class CafcassDocumentManagementControllerFunctionalTest {
     @Test
     public void givenValidUuidDownloadFileWith200Response() throws Exception {
         final UUID documentId = randomUUID();
+        UserInfo userInfo = UserInfo.builder()
+            .roles(Arrays.asList(CAFCASS_USER_ROLE))
+            .build();
 
         final ResponseEntity<Resource> response = ResponseEntity.status(HttpStatus.OK).contentType(APPLICATION_PDF).build();
         Mockito.when(authorisationService.authoriseService(any())).thenReturn(true);
         Mockito.when(authorisationService.authoriseUser(any())).thenReturn(true);
+        Mockito.when(authorisationService.getUserInfo()).thenReturn(userInfo);
         Mockito.when(authTokenGenerator.generate()).thenReturn(TEST_SERVICE_AUTH_TOKEN);
-
+        Mockito.when(systemUserService.getSysUserToken()).thenReturn(TEST_AUTH_TOKEN);
         Mockito.when(caseDocumentClient.getDocumentBinary(anyString(), anyString(), any(UUID.class)))
                 .thenReturn(response);
 
@@ -87,11 +94,15 @@ public class CafcassDocumentManagementControllerFunctionalTest {
     @Test
     public void givenInvalidUuidDownloadFileWith400Response() throws Exception {
         final UUID documentId = randomUUID();
-
+        UserInfo userInfo = UserInfo.builder()
+            .roles(Arrays.asList(CAFCASS_USER_ROLE))
+            .build();
         final ResponseEntity<Resource> response = ResponseEntity.status(BAD_REQUEST).build();
         Mockito.when(authorisationService.authoriseService(any())).thenReturn(true);
         Mockito.when(authorisationService.authoriseUser(any())).thenReturn(true);
+        Mockito.when(authorisationService.getUserInfo()).thenReturn(userInfo);
         Mockito.when(authTokenGenerator.generate()).thenReturn(TEST_SERVICE_AUTH_TOKEN);
+        Mockito.when(systemUserService.getSysUserToken()).thenReturn(TEST_AUTH_TOKEN);
 
         Mockito.when(caseDocumentClient.getDocumentBinary(anyString(), anyString(), any(UUID.class)))
                 .thenReturn(response);
