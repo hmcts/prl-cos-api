bda0a5ac07e45b617f7376034fc4ce8b3da35d65
Khurshed Khan <khurshed.khan@hmcts.net>
2023-04-27 17:28:41 +0100

    PRL-3571:Fixed issue during testing

--- src/main/java/uk/gov/hmcts/reform/prl/models/HearingDateTimeOption.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/HearingDateTimeOption.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.prl.models;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 public class HearingDateTimeOption {
 
     @JsonProperty("hearingDateTimeOption")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     private LocalDateTime hearingDateTimeOption;
 
     @JsonCreator
