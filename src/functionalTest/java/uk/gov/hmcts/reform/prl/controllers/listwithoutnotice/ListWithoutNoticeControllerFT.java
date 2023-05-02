8b5ca49cc0012cc3b6a90e13c0a51bb62d21bd1b
swaroopa86 <pendyalaswaroopa86@gmail.com>
2023-05-02 17:24:10 +0100

    listwithoutnotice functional test cases are fixed

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/listwithoutnotice/ListWithoutNoticeControllerFT.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/listwithoutnotice/ListWithoutNoticeControllerFT.java
@@ -7,7 +7,6 @@ import io.restassured.specification.RequestSpecification;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
