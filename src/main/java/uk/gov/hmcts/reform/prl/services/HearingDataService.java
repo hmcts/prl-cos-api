4d99a76377e994ad0630bb1b7bbc07662334376e
parvezhmcts <parvez.qureshi@hmcts.net>
2023-05-03 17:21:41 +0100

    PRL-3453 - Send and Reply upgrade- message type- variations - Save Messages to Open Messages
    - checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/prl/services/HearingDataService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/HearingDataService.java
@@ -32,7 +32,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
