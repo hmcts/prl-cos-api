a2bd01b2f795de7ede096c288ff677e43ff40f00
parvezhmcts <parvez.qureshi@hmcts.net>
2023-05-10 10:18:56 +0100

    PRL-3453 - Send and Reply upgrade- message type- variations - Save Messages to Open Messages
    - hard coded case id for preview testing
    - added logger
    - removed external message related code.

--- src/main/java/uk/gov/hmcts/reform/prl/models/sendandreply/Message.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/sendandreply/Message.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.prl.models.sendandreply;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -10,10 +9,8 @@ import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.enums.sendmessages.MessageStatus;
-import uk.gov.hmcts.reform.prl.models.complextypes.sendandreply.SelectedExternalPartyDocument;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import static java.util.Optional.ofNullable;
 
@@ -52,9 +49,6 @@ public class Message extends MessageMetaData {
     private String selectedSubmittedDocumentCode;
     private String selectedSubmittedDocumentValue;
     private String selectedExternalParties;
-
-    @JsonProperty("selectedExternalPartyDocuments")
-    private List<SelectedExternalPartyDocument> selectedExternalPartyDocuments;
 
     @JsonIgnore
     public String getLabelForDynamicList() {
