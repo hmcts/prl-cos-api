b0835c6232ed1e4ccdbf2649bf5bfb5e5c9eaa6e
JamesCutts00 <James.Cutts@HMCTS.ORG>
2023-05-10 14:53:26 +0100

    Added logger to capture respondent list

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/c100respondentsolicitor/C100RespondentSolicitorController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/c100respondentsolicitor/C100RespondentSolicitorController.java
@@ -67,6 +67,7 @@ public class C100RespondentSolicitorController {
         @RequestBody CallbackRequest callbackRequest) throws Exception {
         log.info("handleAboutToSubmit: Callback for about-to-submit");
         List<String> errorList = new ArrayList<>();
+        log.info("callBackRequest {}", callbackRequest.getCaseDetails().getData().toString());
         return AboutToStartOrSubmitCallbackResponse
             .builder()
             .data(respondentSolicitorService.populateAboutToSubmitCaseData(
