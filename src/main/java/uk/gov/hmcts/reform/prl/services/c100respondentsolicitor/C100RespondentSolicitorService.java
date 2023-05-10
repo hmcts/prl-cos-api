0d8fb0a5de43dc924b90d2580c59abf965af7139
JamesCutts00 <James.Cutts@HMCTS.ORG>
2023-05-10 11:07:03 +0100

    Fixed bug in international elements

--- src/main/java/uk/gov/hmcts/reform/prl/services/c100respondentsolicitor/C100RespondentSolicitorService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/c100respondentsolicitor/C100RespondentSolicitorService.java
@@ -330,8 +330,7 @@ public class C100RespondentSolicitorService {
                                                       .childrenLiveOutsideOfEnWl(caseData
                                                                                      .getInternationalElementChild()
                                                                                      .getChildrenLiveOutsideOfEnWl())
-                                                      .childrenLiveOutsideOfEnWlDetails(YesOrNo
-                                                                                            .No
+                                                      .childrenLiveOutsideOfEnWlDetails(Yes
                                                                                             .equals(caseData
                                                                                                         .getInternationalElementChild()
                                                                                                         .getChildrenLiveOutsideOfEnWl()
@@ -341,8 +340,7 @@ public class C100RespondentSolicitorService {
                                                       .parentsAnyOneLiveOutsideEnWl(caseData
                                                                                         .getInternationalElementChild()
                                                                                         .getParentsAnyOneLiveOutsideEnWl())
-                                                      .parentsAnyOneLiveOutsideEnWlDetails(YesOrNo
-                                                                                               .No
+                                                      .parentsAnyOneLiveOutsideEnWlDetails(Yes
                                                                                                .equals(caseData
                                                                                                            .getInternationalElementChild()
                                                                                                            .getParentsAnyOneLiveOutsideEnWl()
@@ -353,8 +351,7 @@ public class C100RespondentSolicitorService {
                                                       .anotherPersonOrderOutsideEnWl(caseData
                                                                                          .getInternationalElementChild()
                                                                                          .getAnotherPersonOrderOutsideEnWl())
-                                                      .anotherPersonOrderOutsideEnWlDetails(YesOrNo
-                                                                                                .No
+                                                      .anotherPersonOrderOutsideEnWlDetails(Yes
                                                                                                 .equals(caseData
                                                                                                             .getInternationalElementChild()
                                                                                                             .getAnotherPersonOrderOutsideEnWl()
@@ -365,8 +362,7 @@ public class C100RespondentSolicitorService {
                                                       .anotherCountryAskedInformation(caseData
                                                                                           .getInternationalElementChild()
                                                                                           .getAnotherCountryAskedInformation())
-                                                      .anotherCountryAskedInformationDetaails(YesOrNo
-                                                                                                  .No
+                                                      .anotherCountryAskedInformationDetaails(Yes
                                                                                                   .equals(caseData
                                                                                                               .getInternationalElementChild()
                                                                                                               .getAnotherCountryAskedInformation()
