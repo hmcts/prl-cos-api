c15b69e04290624ad811c52fe37dc74b625e8407
JamesCutts00 <James.Cutts@HMCTS.ORG>
2023-05-11 14:05:09 +0100

    Testing support for respondent tasklist

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/testingsupport/TestingSupportController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/testingsupport/TestingSupportController.java
@@ -50,6 +50,23 @@ public class TestingSupportController {
         )).build();
     }
 
+    @PostMapping(path = "/fillRespondentTaskList", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
+    @Operation(description = "Initiate the respondent tasklist creation for testing support")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Callback processed.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AboutToStartOrSubmitCallbackResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)})
+    @SecurityRequirement(name = "Bearer Authentication")
+    public AboutToStartOrSubmitCallbackResponse fillRespondentTaskList(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
+        @RequestBody CallbackRequest callbackRequest
+    ) throws Exception {
+        return AboutToStartOrSubmitCallbackResponse.builder().data(testingSupportService.initiateTaskListCreation(
+            authorisation,
+            callbackRequest
+        )).build();
+    }
+
     @PostMapping(path = "/submitted-case-creation", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
     @Operation(description = "Initiate the case creation for testing support")
     @ApiResponses(value = {
