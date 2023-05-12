27febb58e9b2252feb8c3110c9c7bfb31487dbd9
bharadwajsairam <bharadwajsairam001@gmail.com>
2023-05-12 20:41:01 +0100

    added about to submit callback

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ReviewDocumentsController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ReviewDocumentsController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.prl.enums.YesNoDontKnow;
 import uk.gov.hmcts.reform.prl.models.Element;
 import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicList;
 import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicListElement;
@@ -60,7 +61,7 @@ public class ReviewDocumentsController {
             schema = @Schema(implementation = AboutToStartOrSubmitCallbackResponse.class))),
         @ApiResponse(responseCode = "400", description = "Bad Request")})
     @SecurityRequirement(name = "Bearer Authentication")
-    public AboutToStartOrSubmitCallbackResponse populateDocsForReview(
+    public AboutToStartOrSubmitCallbackResponse handleAboutToStart(
         @RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
         @RequestBody CallbackRequest callbackRequest
     ) throws Exception {
@@ -84,7 +85,7 @@ public class ReviewDocumentsController {
     @PostMapping(path = "/review-documents/mid-event", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
     @Operation(description = "Callback to amend order mid-event")
     @SecurityRequirement(name = "Bearer Authentication")
-    public AboutToStartOrSubmitCallbackResponse populateOrderToAmendDownloadLink(
+    public AboutToStartOrSubmitCallbackResponse handleMidEvent(
         @RequestHeader(org.springframework.http.HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
         @RequestBody CallbackRequest callbackRequest) {
         CaseData caseData = CaseUtils.getCaseData(callbackRequest.getCaseDetails(), objectMapper);
@@ -93,8 +94,11 @@ public class ReviewDocumentsController {
             .getReviewDocsDynamicList().getValue()) {
             UUID uuid = UUID.fromString(caseData.getReviewDocuments().getReviewDocsDynamicList().getValue().getCode());
             log.info("** uuid ** {}", uuid);
-            Optional<Element<QuarentineLegalDoc>> quarentineLegalDocElement = caseData.getLegalProfQuarentineDocsList().stream()
-                .filter(element -> element.getId().equals(uuid)).findFirst();
+            Optional<Element<QuarentineLegalDoc>> quarentineLegalDocElement = Optional.empty();
+            if (null != caseData.getLegalProfQuarentineDocsList()) {
+                quarentineLegalDocElement = caseData.getLegalProfQuarentineDocsList().stream()
+                    .filter(element -> element.getId().equals(uuid)).findFirst();
+            }
             Optional<Element<UploadedDocuments>> quarentineCitizenDocElement = Optional.empty();
             if (null != caseData.getCitizenUploadQuarentineDocsList()) {
                 quarentineCitizenDocElement = caseData.getCitizenUploadQuarentineDocsList().stream()
@@ -128,6 +132,77 @@ public class ReviewDocumentsController {
                 caseDataUpdated.put("reviewDoc", doc.getCitizenDocument());
             }
         }
+        return AboutToStartOrSubmitCallbackResponse.builder().data(caseDataUpdated).build();
+    }
+
+    @PostMapping(path = "/review-documents/about-to-submit", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Callback processed.", content = @Content(mediaType = "application/json",
+            schema = @Schema(implementation = AboutToStartOrSubmitCallbackResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Bad Request")})
+    @SecurityRequirement(name = "Bearer Authentication")
+    public AboutToStartOrSubmitCallbackResponse handleAboutToSubmit(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
+        @RequestBody CallbackRequest callbackRequest
+    ) throws Exception {
+
+        CaseDetails caseDetails = callbackRequest.getCaseDetails();
+        Map<String, Object> caseDataUpdated = caseDetails.getData();
+        CaseData caseData = CaseUtils.getCaseData(caseDetails, objectMapper);
+        UUID uuid = UUID.fromString(caseData.getReviewDocuments().getReviewDocsDynamicList().getValue().getCode());
+        if (YesNoDontKnow.yes.equals(caseData.getReviewDocuments().getReviewDecisionYesOrNo())) {
+            Optional<Element<QuarentineLegalDoc>> quarentineLegalDocElement = caseData.getLegalProfQuarentineDocsList().stream()
+                .filter(element -> element.getId().equals(uuid)).findFirst();
+            if (quarentineLegalDocElement.isPresent()) {
+                Element<QuarentineLegalDoc> docDetails = caseData.getLegalProfQuarentineDocsList()
+                    .remove(caseData.getLegalProfQuarentineDocsList().indexOf(quarentineLegalDocElement.get()));
+                if (null != caseData.getReviewDocuments().getLegalProfUploadDocListConfTab()) {
+                    caseData.getReviewDocuments().getLegalProfUploadDocListConfTab().add(docDetails);
+                    caseDataUpdated.put("legalProfUploadDocListConfTab", caseData.getReviewDocuments().getLegalProfUploadDocListConfTab());
+                } else {
+                    caseDataUpdated.put("legalProfUploadDocListConfTab", List.of(docDetails));
+                }
+            }
+            Optional<Element<UploadedDocuments>> quarentineCitizenDocElement = caseData.getCitizenUploadQuarentineDocsList().stream()
+                .filter(element -> element.getId().equals(uuid)).findFirst();
+            if (quarentineCitizenDocElement.isPresent()) {
+                Element<UploadedDocuments> docDetails = caseData.getCitizenUploadQuarentineDocsList()
+                    .remove(caseData.getCitizenUploadQuarentineDocsList().indexOf(quarentineCitizenDocElement.get()));
+                if (null != caseData.getReviewDocuments().getCitizenUploadDocListConfTab()) {
+                    caseData.getReviewDocuments().getCitizenUploadDocListConfTab().add(docDetails);
+                    caseDataUpdated.put("citizenUploadDocListConfTab", caseData.getReviewDocuments().getCitizenUploadDocListConfTab());
+                } else {
+                    caseDataUpdated.put("citizenUploadDocListConfTab", List.of(docDetails));
+                }
+            }
+        } else if (YesNoDontKnow.no.equals(caseData.getReviewDocuments().getReviewDecisionYesOrNo())) {
+            Optional<Element<QuarentineLegalDoc>> quarentineLegalDocElement = caseData.getLegalProfQuarentineDocsList().stream()
+                .filter(element -> element.getId().equals(uuid)).findFirst();
+            if (quarentineLegalDocElement.isPresent()) {
+                Element<QuarentineLegalDoc> docDetails = caseData.getLegalProfQuarentineDocsList()
+                    .remove(caseData.getLegalProfQuarentineDocsList().indexOf(quarentineLegalDocElement.get()));
+                if (null != caseData.getReviewDocuments().getLegalProfUploadDocListDocTab()) {
+                    caseData.getReviewDocuments().getLegalProfUploadDocListDocTab().add(docDetails);
+                    caseDataUpdated.put("legalProfUploadDocListDocTab", caseData.getReviewDocuments().getLegalProfUploadDocListDocTab());
+                } else {
+                    caseDataUpdated.put("legalProfUploadDocListDocTab", List.of(docDetails));
+                }
+            }
+            Optional<Element<UploadedDocuments>> quarentineCitizenDocElement = caseData.getCitizenUploadQuarentineDocsList().stream()
+                .filter(element -> element.getId().equals(uuid)).findFirst();
+            if (quarentineCitizenDocElement.isPresent()) {
+                Element<UploadedDocuments> docDetails = caseData.getCitizenUploadQuarentineDocsList()
+                    .remove(caseData.getCitizenUploadQuarentineDocsList().indexOf(quarentineCitizenDocElement.get()));
+                if (null != caseData.getReviewDocuments().getCitizenUploadedDocListDocTab()) {
+                    caseData.getReviewDocuments().getCitizenUploadedDocListDocTab().add(docDetails);
+                    caseDataUpdated.put("citizenUploadedDocListDocTab", caseData.getReviewDocuments().getCitizenUploadedDocListDocTab());
+                } else {
+                    caseDataUpdated.put("citizenUploadedDocListDocTab", List.of(docDetails));
+                }
+            }
+        }
+        caseDataUpdated.put("legalProfQuarentineDocsList", caseData.getLegalProfQuarentineDocsList());
+        caseDataUpdated.put("citizenUploadQuarentineDocsList", caseData.getCitizenUploadQuarentineDocsList())
         return AboutToStartOrSubmitCallbackResponse.builder().data(caseDataUpdated).build();
     }
 }
