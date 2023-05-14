72d7a40827747a0a6ca5368c06bf22ead6a2b294
sahaSonali <sonali.saha@hmcts.net>
2023-05-14 15:31:21 +0100

    Changes to remove a legal representative- emails

--- src/main/java/uk/gov/hmcts/reform/prl/handlers/NoticeOfChangeEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/prl/handlers/NoticeOfChangeEventHandler.java
@@ -38,16 +38,16 @@ public class NoticeOfChangeEventHandler {
         sendEmailToLitigant(caseData, event, EmailTemplateNames.CA_DA_APPLICANT_RESPONDENT_NOC);
 
         //PRL-3211 - notify applicants/respondents other parties except litigant
-        sendEmailToApplicantsRespondents(caseData, event);
+        sendEmailToApplicantsRespondents(caseData, event, EmailTemplateNames.CA_DA_OTHER_PARTIES_NOC);
 
         //PRL-3211 - notify other persons if any
-        sendEmailToOtherParties(caseData, event);
+        sendEmailToOtherParties(caseData, event, EmailTemplateNames.CA_DA_OTHER_PARTIES_NOC);
 
         //PRL-3211 - notify applicants/respondents LRs
-        sendEmailToAppRespSolicitors(caseData, event);
+        sendEmailToAppRespSolicitors(caseData, event, EmailTemplateNames.CA_DA_OTHER_PARTIES_NOC);
     }
 
-    private void sendEmailToAppRespSolicitors(CaseData caseData, NoticeOfChangeEvent event) {
+    private void sendEmailToAppRespSolicitors(CaseData caseData, NoticeOfChangeEvent event, EmailTemplateNames emailTemplateNames) {
         Map<String, String> solicitorsToNotify = new HashMap<>();
         solicitorsToNotify.putAll(CaseUtils.getApplicantSolicitorsToNotify(caseData));
         solicitorsToNotify.putAll(CaseUtils.getRespondentSolicitorsToNotify(caseData));
@@ -55,27 +55,27 @@ public class NoticeOfChangeEventHandler {
             solicitorsToNotify.forEach(
                 (key, value) -> emailService.send(
                     key,
-                    EmailTemplateNames.CA_DA_OTHER_PARTIES_NOC,
+                    emailTemplateNames,
                     noticeOfChangeContentProvider.buildNocEmailSolicitor(caseData, event.getSolicitorName()),
                     LanguagePreference.getPreferenceLanguage(caseData)
                 ));
         }
     }
 
-    private void sendEmailToOtherParties(CaseData caseData, NoticeOfChangeEvent event) {
+    private void sendEmailToOtherParties(CaseData caseData, NoticeOfChangeEvent event, EmailTemplateNames emailTemplateNames) {
         Map<String, String> othersToNotify = CaseUtils.getOthersToNotify(caseData);
         if (!othersToNotify.isEmpty()) {
             othersToNotify.forEach(
                 (key, value) -> emailService.send(
                     key,
-                    EmailTemplateNames.CA_DA_OTHER_PARTIES_NOC,
+                    emailTemplateNames,
                     noticeOfChangeContentProvider.buildNocEmailCitizen(caseData, event.getSolicitorName(), value, true, ""),
                     LanguagePreference.getPreferenceLanguage(caseData)
                 ));
         }
     }
 
-    private void sendEmailToApplicantsRespondents(CaseData caseData, NoticeOfChangeEvent event) {
+    private void sendEmailToApplicantsRespondents(CaseData caseData, NoticeOfChangeEvent event, EmailTemplateNames emailTemplateNames) {
         Element<PartyDetails> partyElement = getLitigantParty(caseData, event);
         Map<String, String> applicantsRespondentsToNotify = new HashMap<>();
         applicantsRespondentsToNotify.putAll(CaseUtils.getApplicantsToNotify(caseData, null != partyElement ? partyElement.getId() : null));
@@ -84,7 +84,7 @@ public class NoticeOfChangeEventHandler {
             applicantsRespondentsToNotify.forEach(
                 (key, value) -> emailService.send(
                     key,
-                    EmailTemplateNames.CA_DA_OTHER_PARTIES_NOC,
+                    emailTemplateNames,
                     noticeOfChangeContentProvider.buildNocEmailCitizen(caseData, event.getSolicitorName(), value, false, ""),
                     LanguagePreference.getPreferenceLanguage(caseData)
                 ));
@@ -141,7 +141,16 @@ public class NoticeOfChangeEventHandler {
         //PRL-3215 - notify old LR
         sendEmailToSolicitor(caseData, event, EmailTemplateNames.CA_DA_REMOVE_SOLICITOR_NOC);
 
-        //PRL-3211 - notify LiP
+        //PRL-3215 - notify LiP
         sendEmailToLitigant(caseData, event, EmailTemplateNames.CA_DA_APPLICANT_REMOVE_RESPONDENT_NOC);
+
+        //PRL-3215 - notify applicants/respondents other parties except litigant
+        sendEmailToApplicantsRespondents(caseData, event, EmailTemplateNames.CA_DA_OTHER_PARTIES_REMOVE_NOC);
+
+        //PRL-3215 - notify other persons if any
+        sendEmailToOtherParties(caseData, event, EmailTemplateNames.CA_DA_OTHER_PARTIES_REMOVE_NOC);
+
+        //PRL-3215 - notify applicants/respondents LRs
+        sendEmailToAppRespSolicitors(caseData, event, EmailTemplateNames.CA_DA_OTHER_PARTIES_REMOVE_NOC);
     }
 }
