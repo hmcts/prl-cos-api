f6a31f9b7ff823462d00fe2343dc4001b472c0ba
sahaSonali <sonali.saha@hmcts.net>
2023-05-15 20:21:10 +0100

    Changes to remove a legal representative

--- src/test/java/uk/gov/hmcts/reform/prl/services/tab/alltabs/AllTabServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/tab/alltabs/AllTabServiceImplTest.java
@@ -143,27 +143,27 @@ public class AllTabServiceImplTest {
 
     @Test
     public void testUpdatePartyDetailsForNocC100Applicant() {
-        allTabService.updatePartyDetailsForNoc(CASE_DATA, Optional.of(SolicitorRole.C100APPLICANTSOLICITOR1));
+        allTabService.updatePartyDetailsForNoc(CASE_DATA, Optional.of(SolicitorRole.C100APPLICANTSOLICITOR1), null);
         verify(coreCaseDataService).triggerEvent(anyString(), anyString(),anyLong(), anyString(), anyMap());
     }
 
     @Test
     public void testUpdatePartyDetailsForNocC100Respondent() {
-        allTabService.updatePartyDetailsForNoc(CASE_DATA, Optional.of(SolicitorRole.C100RESPONDENTSOLICITOR1));
+        allTabService.updatePartyDetailsForNoc(CASE_DATA, Optional.of(SolicitorRole.C100RESPONDENTSOLICITOR1), null);
         verify(coreCaseDataService).triggerEvent(anyString(), anyString(),anyLong(), anyString(), anyMap());
     }
 
     @Test
     public void testUpdatePartyDetailsForNocFL401Applicant() {
         when(CASE_DATA.getCaseTypeOfApplication()).thenReturn("FL401");
-        allTabService.updatePartyDetailsForNoc(CASE_DATA, Optional.of(SolicitorRole.FL401APPLICANTSOLICITOR));
+        allTabService.updatePartyDetailsForNoc(CASE_DATA, Optional.of(SolicitorRole.FL401APPLICANTSOLICITOR), null);
         verify(coreCaseDataService).triggerEvent(anyString(), anyString(),anyLong(), anyString(), anyMap());
     }
 
     @Test
     public void testUpdatePartyDetailsForNocFL401Respondent() {
         when(CASE_DATA.getCaseTypeOfApplication()).thenReturn("FL401");
-        allTabService.updatePartyDetailsForNoc(CASE_DATA, Optional.of(SolicitorRole.FL401RESPONDENTSOLICITOR));
+        allTabService.updatePartyDetailsForNoc(CASE_DATA, Optional.of(SolicitorRole.FL401RESPONDENTSOLICITOR), null);
         verify(coreCaseDataService).triggerEvent(anyString(), anyString(),anyLong(), anyString(), anyMap());
     }
 }
