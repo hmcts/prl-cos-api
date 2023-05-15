630baff7b92c5f3140616d541d8f7a7d78c5567a
swaroopa86 <pendyalaswaroopa86@gmail.com>
2023-05-15 12:55:42 +0100

    Revert "add casenumber functional test is fixed"
    This reverts commit a77f1d0e18f2dbd81597d3fdfb19dea96f19716f.

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/CallbackControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/CallbackControllerFunctionalTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.prl.ResourceLoader;
-import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.utils.IdamTokenGenerator;
 
 import java.time.LocalDate;
@@ -158,8 +157,7 @@ public class CallbackControllerFunctionalTest {
             .post("/fl401-add-case-number")
             .then()
             .body("data.issueDate", equalTo(LocalDate.now().toString()),
-                  "data.isAddCaseNumberAdded", YesOrNo.Yes
-            )
+                  "data.isAddCaseNumberAdded", "Yes")
             .assertThat().statusCode(200);
     }
 
