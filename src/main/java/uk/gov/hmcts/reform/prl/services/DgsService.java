483d7525bff92121692ad503b302da3ea3310d15
JamesCutts00 <James.Cutts@HMCTS.ORG>
2023-05-15 10:15:53 +0100

    Added overloaded methods

--- src/main/java/uk/gov/hmcts/reform/prl/services/DgsService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/DgsService.java
@@ -29,6 +29,24 @@ public class DgsService {
     private static final String CASE_DETAILS_STRING = "caseDetails";
     private static final String ERROR_MESSAGE = "Error generating and storing document for case {}";
 
+    public GeneratedDocumentInfo generateDocument(String authorisation, CaseDetails caseDetails, String templateName,
+                                                  Map<String, Object> respondentDetails) throws Exception {
+        Map<String, Object> tempCaseDetails = new HashMap<>();
+        tempCaseDetails.put(CASE_DETAILS_STRING, AppObjectMapper.getObjectMapper().convertValue(caseDetails, Map.class));
+        GeneratedDocumentInfo generatedDocumentInfo = null;
+        try {
+            generatedDocumentInfo =
+                dgsApiClient.generateDocument(authorisation, GenerateDocumentRequest
+                    .builder().template(templateName).values(tempCaseDetails).build()
+                );
+
+        } catch (Exception ex) {
+            log.error(ERROR_MESSAGE, caseDetails.getCaseId());
+            throw new DocumentGenerationException(ex.getMessage(), ex);
+        }
+        return generatedDocumentInfo;
+    }
+
     public GeneratedDocumentInfo generateDocument(String authorisation, CaseDetails caseDetails, String templateName) throws Exception {
         Map<String, Object> tempCaseDetails = new HashMap<>();
         tempCaseDetails.put(CASE_DETAILS_STRING, AppObjectMapper.getObjectMapper().convertValue(caseDetails, Map.class));
@@ -40,6 +58,43 @@ public class DgsService {
                 );
 
         } catch (Exception ex) {
+            log.error(ERROR_MESSAGE, caseDetails.getCaseId());
+            throw new DocumentGenerationException(ex.getMessage(), ex);
+        }
+        return generatedDocumentInfo;
+    }
+
+    public GeneratedDocumentInfo generateWelshDocument(String authorisation, CaseDetails caseDetails, String templateName,
+                                                       Map<String, Object> respondentDetails) throws Exception {
+
+        Map<String, Object> tempCaseDetails = new HashMap<>();
+        // Get the Welsh Value of each object using Welsh Mapper
+        Map<String, Object> caseDataMap = AppObjectMapper.getObjectMapper().convertValue(caseDetails, Map.class);
+        Map<String, Object> caseDataValues = (Map<String, Object>) caseDataMap.get("case_data");
+        caseDataValues.forEach((k, v) -> {
+            if (v != null) {
+                Object updatedWelshObj = WelshLangMapper.applyWelshTranslation(k, v,
+                                                                               PrlAppsConstants.C100_CASE_TYPE
+                                                                                   .equalsIgnoreCase(
+                                                                                       caseDetails.getCaseData()
+                                                                                           .getCaseTypeOfApplication()
+                                                                                   )
+                );
+                caseDataValues.put(k, updatedWelshObj);
+            }
+        });
+        caseDataMap.put("case_data", caseDataValues);
+        tempCaseDetails.put(CASE_DETAILS_STRING, caseDataMap);
+
+        GeneratedDocumentInfo generatedDocumentInfo = null;
+        try {
+            generatedDocumentInfo =
+                dgsApiClient.generateDocument(authorisation, GenerateDocumentRequest
+                    .builder().template(templateName).values(tempCaseDetails).build()
+                );
+
+        } catch (Exception ex) {
+            log.error(ERROR_MESSAGE, caseDetails.getCaseId());
             throw new DocumentGenerationException(ex.getMessage(), ex);
         }
         return generatedDocumentInfo;
