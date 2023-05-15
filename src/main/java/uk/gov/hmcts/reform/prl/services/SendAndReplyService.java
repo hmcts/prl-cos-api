2c13bcd7c1893ae6291813dd8bb23a6eadf5a7f3
channa-patil <channabasappagouda.patil@hmcts.net>
2023-05-15 23:53:53 +0100

    PRL-3458 - Populate judicial & ctsc email list for replyMessageObject

--- src/main/java/uk/gov/hmcts/reform/prl/services/SendAndReplyService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/SendAndReplyService.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.prl.services;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
