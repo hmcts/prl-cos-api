0bf001947f14e17e0fd993a3e3cc679fc24622e9
Yogendra Upasani <yogendra.upasani@hmcts.net>
2023-05-17 10:06:17 +0100

    PRL-3613 Cafcass Cymru Email Notifications

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/WelshCourtEmail.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/WelshCourtEmail.java
@@ -2,8 +2,10 @@ package uk.gov.hmcts.reform.prl.models.dto.ccd;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
 @Data
+@Configuration
 public class WelshCourtEmail {
     @Value("${welsh.court.email-mapping}")
     protected String welshCourtEmailMapping;
