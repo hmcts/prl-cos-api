50a7eea007f2e99f30f938fe96ec007d54ad5d45
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2023-05-17 21:17:25 +0100

    Functional Test impl

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
@@ -800,8 +800,6 @@ public class CaseData implements MappableObject {
 
     private final ChangeOrganisationRequest changeOrganisationRequestField;
     //PRL-3454 - send and reply message enhancements
-    @JsonUnwrapped
-    @Builder.Default
     private SendOrReplyMessage sendOrReplyMessage;
 
 }
