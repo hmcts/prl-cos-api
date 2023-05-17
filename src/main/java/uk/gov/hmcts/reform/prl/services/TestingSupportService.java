38da5c632fcc2de7935fec02eab1bc0ea5b85ce2
sahaSonali <sonali.saha@hmcts.net>
2023-05-17 11:17:24 +0100

    Changes to remove a legal representative

--- src/main/java/uk/gov/hmcts/reform/prl/services/TestingSupportService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/TestingSupportService.java
@@ -327,6 +327,8 @@ public class TestingSupportService {
     }
 
     private boolean isAuthorized(String authorisation) {
+        log.info("TESTING_SUPPORT_LD_FLAG_ENABLED ==> " + launchDarklyClient.isFeatureEnabled(TESTING_SUPPORT_LD_FLAG_ENABLED));
+        log.info("authorisationService.authoriseUser(authorisation) ==> " + authorisationService.authoriseUser(authorisation));
         return launchDarklyClient.isFeatureEnabled(TESTING_SUPPORT_LD_FLAG_ENABLED)
             && Boolean.TRUE.equals(authorisationService.authoriseUser(authorisation));
     }
