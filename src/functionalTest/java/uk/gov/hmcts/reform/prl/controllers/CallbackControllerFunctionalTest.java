185769e61172ebb2e4b8f295a7615dcd0090518d
swaroopa86 <pendyalaswaroopa86@gmail.com>
2023-05-17 11:39:54 +0100

    functional test case is updated

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/CallbackControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/CallbackControllerFunctionalTest.java
@@ -12,10 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.prl.ResourceLoader;
-import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.utils.IdamTokenGenerator;
-
-import java.time.LocalDate;
 
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
