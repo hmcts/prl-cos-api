ccea3ebfee50934ca0b840760daa32fdf9400d75
JamesCutts00 <James.Cutts@HMCTS.ORG>
2023-05-22 14:46:21 +0100

    Added c8 document

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
@@ -720,6 +720,7 @@ public class CaseData implements MappableObject {
     private final String isEngC7DocGen;
     private final Document draftC7ResponseDoc;
     private final Document finalC7ResponseDoc;
+    private final Document draftC8Doc;
 
     private final List<SubmitConsentEnum> respondentAgreeStatement;
 
