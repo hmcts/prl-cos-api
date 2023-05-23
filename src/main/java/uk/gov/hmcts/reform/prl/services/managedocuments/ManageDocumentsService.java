693f9672030ffc2d5bcb6ccc244d47270825efd6
channa-patil <channabasappagouda.patil@hmcts.net>
2023-05-24 00:23:55 +0100

    PRL-3562 - Added changes to populate document categories & sub categories

---
+++ src/main/java/uk/gov/hmcts/reform/prl/services/managedocuments/ManageDocumentsService.java
@@ -0,0 +1,76 @@
+package uk.gov.hmcts.reform.prl.services.managedocuments;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
+import uk.gov.hmcts.reform.ccd.client.model.CategoriesAndDocuments;
+import uk.gov.hmcts.reform.ccd.client.model.Category;
+import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicList;
+import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicListElement;
+import uk.gov.hmcts.reform.prl.models.complextypes.managedocuments.ManageDocuments;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+import uk.gov.hmcts.reform.prl.utils.CaseUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static uk.gov.hmcts.reform.prl.utils.ElementUtils.element;
+import static uk.gov.hmcts.reform.prl.utils.ElementUtils.nullSafeCollection;
+
+
+@Slf4j
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class ManageDocumentsService {
+
+    @Autowired
+    private final CoreCaseDataApi coreCaseDataApi;
+
+    @Autowired
+    private final AuthTokenGenerator authTokenGenerator;
+
+    public CaseData populateDocumentCategories(String authorization, CaseData caseData) {
+
+        ManageDocuments manageDocuments = ManageDocuments.builder()
+            .documentCategories(getCategoriesSubcategories(authorization, String.valueOf(caseData.getId())))
+            .build();
+
+        return caseData.toBuilder()
+            .manageDocuments(Arrays.asList(element(manageDocuments)))
+            .build();
+    }
+
+    private DynamicList getCategoriesSubcategories(String authorisation, String caseReference) {
+        try {
+            CategoriesAndDocuments categoriesAndDocuments = coreCaseDataApi.getCategoriesAndDocuments(
+                authorisation,
+                authTokenGenerator.generate(),
+                caseReference
+            );
+
+            if (null != categoriesAndDocuments) {
+                List<Category> parentCategories = nullSafeCollection(categoriesAndDocuments.getCategories())
+                    .stream()
+                    .sorted(Comparator.comparing(Category::getCategoryName))
+                    .collect(Collectors.toList());
+
+                List<DynamicListElement> dynamicListElementList = new ArrayList<>();
+                CaseUtils.createCategorySubCategoryDynamicList(parentCategories, dynamicListElementList);
+
+                return DynamicList.builder().value(DynamicListElement.EMPTY)
+                    .listItems(dynamicListElementList).build();
+            }
+        } catch (Exception e) {
+            log.error("Error in getCategoriesAndDocuments method", e);
+        }
+        return DynamicList.builder()
+            .value(DynamicListElement.EMPTY).build();
+    }
+}
