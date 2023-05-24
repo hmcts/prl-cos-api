80e6901204adcfdd9758fc2e29a007f72c8c6105
channa-patil <channabasappagouda.patil@hmcts.net>
2023-05-24 12:00:32 +0100

    PRL-3562 - added json tags for manage documents field

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/managedocuments/ManageDocuments.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/managedocuments/ManageDocuments.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.prl.models.complextypes.managedocuments;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import uk.gov.hmcts.reform.prl.enums.RestrictToCafcassHmcts;
 import uk.gov.hmcts.reform.prl.enums.managedocuments.DocumentPartyEnum;
 import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicList;
@@ -9,6 +10,7 @@ import uk.gov.hmcts.reform.prl.models.documents.Document;
 
 import java.util.List;
 
+@Data
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class ManageDocuments {
