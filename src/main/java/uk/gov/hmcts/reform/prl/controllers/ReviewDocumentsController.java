17c0b0f9af19c3d92a87d5691ce756844c57fb65
Khurshed Khan <khurshed.khan@hmcts.net>
2023-05-25 16:09:47 +0100

    PRL-3565:Added logger

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ReviewDocumentsController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ReviewDocumentsController.java
@@ -120,11 +120,12 @@ public class ReviewDocumentsController {
 
                 String doctobereviewed = String
                     .join(format("<h3 class='govuk-heading-s'>Submitted by</h3><label class='govuk-label' for='more-detail'><li>%s</li></label>",
-                                 "Legal professional"),
-                          format("<h3 class='govuk-heading-s'>Document category</h3><label class='govuk-label' for='more-detail'><li>%s</li></label>",
-                                 doc.getCategory()),
-                          format("<h3 class='govuk-heading-s'>Details/comments</h3><label class='govuk-label' for='more-detail'><li>%s</li></label>",
-                         doc.getNotes()), "<br/>");
+                                "Legal professional"),
+                         format("<h3 class='govuk-heading-s'>Document category</h3><label class='govuk-label' for='more-detail'><li>%s</li></label>",
+                                doc.getCategory()),
+                         format("<h3 class='govuk-heading-s'>Details/comments</h3><label class='govuk-label' for='more-detail'><li>%s</li></label>",
+                                doc.getNotes()));
+
                 log.info("docTobeReviewed {}", doctobereviewed);
                 caseDataUpdated.put("docToBeReviewed", doctobereviewed);
                 caseDataUpdated.put("reviewDoc", doc.getDocument());
