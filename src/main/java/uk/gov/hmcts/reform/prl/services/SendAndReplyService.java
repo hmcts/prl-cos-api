7b253109bd296e1c03e5c17b6fffccc34aea7934
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2023-05-26 15:19:34 +0100

    table style9

--- src/main/java/uk/gov/hmcts/reform/prl/services/SendAndReplyService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/SendAndReplyService.java
@@ -758,7 +758,7 @@ public class SendAndReplyService {
                 .map(Element::getValue)
                 .forEach(history -> {
                     lines.add("<h4>Message</h4>");
-                    lines.add("<div class='width-57' style='background: green; width: 48%;' style=\"background: red; width: 48%;\">");
+                    lines.add("<div class='width-50'");
                     lines.add("<table>");
                     addRowToMessageTable(lines, "From", history.getMessageFrom());
                     addRowToMessageTable(lines, "To", history.getMessageTo());
@@ -791,7 +791,7 @@ public class SendAndReplyService {
 
         //latest message
         lines.add("<h2>Message</h2>");
-        lines.add("<div class='width-30'>");
+        lines.add("<div class='width-50'>");
         lines.add("<table>");
         addRowToMessageTable(lines, "From", message.getSenderEmail());
         addRowToMessageTable(lines, "Date of the message", message.getDateSent());
