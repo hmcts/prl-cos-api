5c6d6549fe7a0927e93b27c001f31e3b65985629
Yogendra Upasani <yogendra.upasani@hmcts.net>
2023-06-04 19:36:10 +0100

    Other applications lable

--- src/main/java/uk/gov/hmcts/reform/prl/models/sendandreply/Message.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/sendandreply/Message.java
@@ -77,6 +77,7 @@ public class Message extends MessageMetaData {
 
     private String judgeEmail;
     private String senderNameAndRole;
+    private String otherApplicationsLinkText;
 
     @JsonIgnore
     public String getLabelForDynamicList() {
