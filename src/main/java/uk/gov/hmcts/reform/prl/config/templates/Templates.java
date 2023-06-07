29157c86bed17662ef01fde49966912c8398963e
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2023-06-07 19:49:57 +0100

    changing the name

--- src/main/java/uk/gov/hmcts/reform/prl/config/templates/Templates.java
+++ src/main/java/uk/gov/hmcts/reform/prl/config/templates/Templates.java
@@ -3,7 +3,7 @@ package uk.gov.hmcts.reform.prl.config.templates;
 
 public class Templates {
     public static String ANNEX_ENG_Y = "PRL-ENG-ANNEX-Y.docx";
-    public static String ANNEX_ENG_Z = "PRL-ENG-ANNEX-Y.docx";
+    public static String ANNEX_ENG_Z = "PRL-ENG-ANNEX-Z.docx";
     public static String MEDIATION_VOUCHER_ENG = "PRL-ENG-MEDIATION-VOUCHER.docx";
     public static String SAFETY_PROTECTION_ENG = "PRL-ENG-SAFETY-PROTECTION.docx";
     public static String FL416_ENG = "PRL-ENG-FL416.docx";
