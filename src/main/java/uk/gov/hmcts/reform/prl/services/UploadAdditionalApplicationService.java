09eb5d42f34236d03e6d47164a6f0adcd563affc
sahaSonali <sonali.saha@hmcts.net>
2023-06-07 13:41:19 +0100

    Changes for AwP : Configure fee lookup API

--- src/main/java/uk/gov/hmcts/reform/prl/services/UploadAdditionalApplicationService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/UploadAdditionalApplicationService.java
@@ -187,6 +187,7 @@ public class UploadAdditionalApplicationService {
             authorisation
         );
         if (null != paymentServiceResponse) {
+            log.info("PaymentServiceResponse ===> " + paymentServiceResponse);
             caseDataUpdated.put(
                 "paymentServiceRequestReferenceNumber",
                 paymentServiceResponse.getServiceRequestReference()
