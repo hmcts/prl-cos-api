5603bd1b4bd0699ce940875c6a9096b7f10637ef
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2023-06-07 22:04:28 +0100

    Adding loggers

--- src/main/java/uk/gov/hmcts/reform/prl/services/ServiceOfApplicationService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ServiceOfApplicationService.java
@@ -359,6 +359,7 @@ public class ServiceOfApplicationService {
                 generatePinAndSendNotificationEmailForCitizen(caseData);
             }
         }
+        log.info("emailNotificationDetails {}" , emailNotificationDetails);
         return ServedApplicationDetails.builder().emailNotificationDetails(emailNotificationDetails)
             .servedBy(userService.getUserDetails(authorization).getFullName())
             .bulkPrintDetails(bulkPrintDetails).build();
