f0cbe22400a75b3c6e59e168c6b126d54b69ab74
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2023-06-08 00:20:58 +0100

    Adding email changes

--- src/main/java/uk/gov/hmcts/reform/prl/config/templates/Templates.java
+++ src/main/java/uk/gov/hmcts/reform/prl/config/templates/Templates.java
@@ -11,17 +11,17 @@ public class Templates {
 
 
     //Emails
-    public static String EMAIL_BODY = "" +
-        "Case name: %s \n" +
-        "Case number: %s\n" +
-        "\n" +
-        "\n" +
-        "Dear %s,\n" +
-        "\n" +
-        "A final order has been issued for this case and is attached to this email. \n" +
-        "\n" +
-        "You can also view the final order when you sign into your MyHMCTS account. A sign in link will be sent in a separate email. \n" +
-        " \n" +
-        "HM Courts and Tribunals Service (HMCTS) \n";
+    public static String EMAIL_BODY = ""
+        + "Case name: %s \n"
+        + "Case number: %s\n"
+        + "\n"
+        + "\n"
+        + "Dear %s,\n"
+        + "\n"
+        + "A final order has been issued for this case and is attached to this email. \n"
+        + "\n"
+        + "You can also view the final order when you sign into your MyHMCTS account. A sign in link will be sent in a separate email. \n"
+        + " \n"
+        + "HM Courts and Tribunals Service (HMCTS) \n";
 
 }
