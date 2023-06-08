d2db48d2ef3b82ec9090f84a461c88de9fef7f9e
bharadwajsairam <bharadwajsairam001@gmail.com>
2023-06-08 15:48:27 +0100

    C8 TEMPLATE ADDED FOR Respondent Solicitor

--- src/main/java/uk/gov/hmcts/reform/prl/constants/PrlAppsConstants.java
+++ src/main/java/uk/gov/hmcts/reform/prl/constants/PrlAppsConstants.java
@@ -100,6 +100,8 @@ public class PrlAppsConstants {
     public static final String C8_HINT = "C8";
     public static final String C1A_HINT = "C1A";
     public static final String C8_DRAFT_HINT = "C8_DRAFT";
+    public static final String C8_RESP_DRAFT_HINT = "C8_RESPONDENT_DRAFT";
+    public static final String C8_RESP_FINAL_HINT = "C8_RESPONDENT_FINAL";
     public static final String C1A_DRAFT_HINT = "C1A_DRAFT";
     public static final String FINAL_HINT = "FINAL";
     public static final String DRAFT_HINT = "DRAFT";
