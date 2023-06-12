53b15abd6576f68eacf8b5ca4caaadc0b7364a06
sahaSonali <sonali.saha@hmcts.net>
2023-06-12 15:00:38 +0100

    Changes for AwP : Configure fee lookup api

--- src/main/java/uk/gov/hmcts/reform/prl/enums/uploadadditionalapplication/UploadAdditionalApplicationsFieldsEnum.java
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/uploadadditionalapplication/UploadAdditionalApplicationsFieldsEnum.java
@@ -6,7 +6,8 @@ public enum UploadAdditionalApplicationsFieldsEnum {
     temporaryC2Document("temporaryC2Document"),
     additionalApplicantsList("additionalApplicantsList"),
     typeOfC2Application("typeOfC2Application"),
-    additionalApplicationsApplyingFor("additionalApplicationsApplyingFor");
+    additionalApplicationsApplyingFor("additionalApplicationsApplyingFor"),
+    additionalApplicationFeesToPay("additionalApplicationFeesToPay");
 
     private final String value;
 
