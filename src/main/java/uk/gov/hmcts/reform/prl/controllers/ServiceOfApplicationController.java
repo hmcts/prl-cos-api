a31f01d927dc39d5fa494d5db8f9f6f9cc4155dd
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2023-06-15 15:40:59 +0100

    refactor

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ServiceOfApplicationController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ServiceOfApplicationController.java
@@ -31,7 +31,6 @@ import uk.gov.hmcts.reform.prl.services.tab.alltabs.AllTabServiceImpl;
 import uk.gov.hmcts.reform.prl.utils.CaseUtils;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
