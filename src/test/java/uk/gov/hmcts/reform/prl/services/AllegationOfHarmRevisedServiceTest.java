c05609e0581cddd78b58be0aa627ac604f1bce53
REMYA MARY JAMES <RemyaMary.James@hmcts.net>
2023-06-15 11:59:20 +0100

    AOHChildUpdates

--- src/test/java/uk/gov/hmcts/reform/prl/services/AllegationOfHarmRevisedServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/AllegationOfHarmRevisedServiceTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.prl.services;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -54,6 +55,7 @@ public class AllegationOfHarmRevisedServiceTest {
         Map<String, Object> response = allegationOfHarmService.getPrePopulatedChildData(CaseData.builder()
                 .newChildDetails(List.of(childDetailsRevisedElement)).build());
         Assert.assertFalse(response.isEmpty());
+
 
     }
 
