c50ca40865d0fbf70162f9e001f481a9cbf29a12
JamesCutts00 <James.Cutts@HMCTS.ORG>
2023-06-16 14:18:07 +0100

    Fixed test case

--- src/test/java/uk/gov/hmcts/reform/prl/services/SolicitorEmailServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/SolicitorEmailServiceTest.java
@@ -225,7 +225,7 @@ public class SolicitorEmailServiceTest {
             .caseName(emailService.getCaseData(caseDetails).getApplicantCaseName())
             .applicantName(applicantNames)
             .courtName(court.getCourtName())
-            .caseLink(manageCaseUrl + "/" + caseDetails.getId() + "#Service%20Request")
+            .caseLink("<a href='" + manageCaseUrl + "/" + caseDetails.getId() + "#Service%20Request'>Pay and submit application</a>")
             .build();
 
         when(courtFinderService.getNearestFamilyCourt(caseData)).thenReturn(court);
