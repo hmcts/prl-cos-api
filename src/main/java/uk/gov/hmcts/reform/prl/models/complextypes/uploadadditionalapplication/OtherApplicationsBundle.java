88e5e56b9c6f053045ee187b5601321f0a08ea0d
sahaSonali <sonali.saha@hmcts.net>
2023-06-19 13:16:27 +0100

    Changes for AwP : Configure fee lookup api

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/uploadadditionalapplication/OtherApplicationsBundle.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/uploadadditionalapplication/OtherApplicationsBundle.java
@@ -1,12 +1,15 @@
 package uk.gov.hmcts.reform.prl.models.complextypes.uploadadditionalapplication;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
+import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.enums.uploadadditionalapplication.CaOtherApplicationType;
 import uk.gov.hmcts.reform.prl.enums.uploadadditionalapplication.DaOtherApplicationType;
 import uk.gov.hmcts.reform.prl.enums.uploadadditionalapplication.DocumentAcknowledge;
+import uk.gov.hmcts.reform.prl.enums.uploadadditionalapplication.OtherApplicationType;
 import uk.gov.hmcts.reform.prl.enums.uploadadditionalapplication.UrgencyTimeFrameType;
 import uk.gov.hmcts.reform.prl.models.Element;
 import uk.gov.hmcts.reform.prl.models.documents.Document;
@@ -15,6 +18,7 @@ import java.util.List;
 
 @Builder
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OtherApplicationsBundle {
     @JsonProperty("applicantName")
     private final String applicantName;
@@ -22,6 +26,8 @@ public class OtherApplicationsBundle {
     private final CaOtherApplicationType caApplicationType;
     @JsonProperty("daApplicationType")
     private final DaOtherApplicationType daApplicationType;
+    @JsonProperty("applicationType")
+    private final OtherApplicationType applicationType;
     @JsonProperty("document")
     private final Document document;
     @JsonProperty("documentAcknowledge")
@@ -32,7 +38,13 @@ public class OtherApplicationsBundle {
     private final List<Element<Supplement>> supplementsBundle;
     @JsonProperty("supportingEvidenceBundle")
     private final List<Element<SupportingEvidenceBundle>> supportingEvidenceBundle;
+    @JsonProperty("uploadedDateTime")
     private final String uploadedDateTime;
+    @JsonProperty("author")
     private final String author;
+    @JsonProperty("urgency")
+    private final Urgency urgency;
+    @JsonProperty("documentRelatedToCase")
+    private final YesOrNo documentRelatedToCase;
 
 }
