2a35f2c6b605b6d7e7d6f245eb4e5f1478956f23
Aloknath Datta <alokdatta.uit@gmail.com>
2023-06-19 21:10:17 +0100

    data map

--- src/main/java/uk/gov/hmcts/reform/prl/services/c100respondentsolicitor/C100RespondentSolicitorService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/c100respondentsolicitor/C100RespondentSolicitorService.java
@@ -892,6 +892,7 @@ public class C100RespondentSolicitorService {
         );
         dataMap.put("reasonableAdjustments", response.getSupportYouNeed().getReasonableAdjustments());
         dataMap.put("attendingTheCourt", response.getAttendToCourt());
+        log.info("Now the data map is :: " + dataMap);
         return dataMap;
     }
 
