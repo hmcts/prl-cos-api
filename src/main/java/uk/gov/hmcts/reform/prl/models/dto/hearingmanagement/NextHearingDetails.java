b4fd99ebf0708a0fb84954a6da797c1e70c8fcff
Yogendra Upasani <yogendra.upasani@hmcts.net>
2023-06-20 12:28:23 +0100

    Added Json format property for hearingDateTime which is set by CCD batch job

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/hearingmanagement/NextHearingDetails.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/hearingmanagement/NextHearingDetails.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.prl.models.dto.hearingmanagement;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,6 +18,7 @@ public class NextHearingDetails {
     @JsonProperty("hearingID")
     private final String hearingID;
     @JsonProperty("hearingDateTime")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
     private final LocalDateTime hearingDateTime;
 
 }
