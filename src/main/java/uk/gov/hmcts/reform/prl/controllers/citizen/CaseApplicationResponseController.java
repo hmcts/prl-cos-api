7d48b258b0c40ff646539d90df48177f5bbc52bf
sahaSonali <sonali.saha@hmcts.net>
2023-06-21 13:07:33 +0100

    Changes for citizen C8 & C1A document

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseApplicationResponseController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseApplicationResponseController.java
@@ -14,13 +14,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.idam.client.IdamClient;
+import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.models.Element;
 import uk.gov.hmcts.reform.prl.models.complextypes.PartyDetails;
 import uk.gov.hmcts.reform.prl.models.complextypes.citizen.documents.ResponseDocuments;
+import uk.gov.hmcts.reform.prl.models.complextypes.respondentsolicitor.documents.RespondentDocs;
 import uk.gov.hmcts.reform.prl.models.documents.Document;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+import uk.gov.hmcts.reform.prl.services.c100respondentsolicitor.C100RespondentSolicitorService;
 import uk.gov.hmcts.reform.prl.services.citizen.CaseService;
 import uk.gov.hmcts.reform.prl.services.citizen.CitizenResponseNotificationEmailService;
 import uk.gov.hmcts.reform.prl.services.document.DocumentGenService;
@@ -29,11 +34,17 @@ import uk.gov.hmcts.reform.prl.utils.CaseUtils;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
+import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.C7_FINAL_ENGLISH;
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.C8_RESP_FINAL_HINT;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.DOCUMENT_C7_DRAFT_HINT;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.REVIEW_AND_SUBMIT;
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.SOLICITOR_C1A_FINAL_DOCUMENT;
+import static uk.gov.hmcts.reform.prl.enums.YesOrNo.Yes;
 import static uk.gov.hmcts.reform.prl.utils.ElementUtils.element;
 
 @Slf4j
@@ -55,6 +66,10 @@ public class CaseApplicationResponseController {
 
     @Autowired
     CitizenResponseNotificationEmailService citizenResponseNotificationEmailService;
+    @Autowired
+    C100RespondentSolicitorService c100RespondentSolicitorService;
+    @Autowired
+    IdamClient idamClient;
 
 
     @PostMapping(path = "/{caseId}/{partyId}/generate-c7document", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
@@ -101,6 +116,12 @@ public class CaseApplicationResponseController {
         CaseData caseData = CaseUtils.getCaseData(caseDetails, objectMapper);
         updateCurrentRespondent(caseData, YesOrNo.Yes, partyId);
         log.info(" Generating C7 Final document for respondent ");
+        Optional<Element<PartyDetails>> currentRespondent = caseData.getRespondents().stream().filter(respondent -> YesOrNo.Yes.equals(
+            respondent.getValue().getCurrentRespondent())).findFirst();
+        log.info("currentRespondent citizen " + currentRespondent);
+        CallbackRequest callbackRequest = CallbackRequest.builder().caseDetails(caseDetails).build();
+        CaseDetails caseDetailsReturn = null;
+        List<Element<ResponseDocuments>> responseDocumentsList = new ArrayList<>();
         Document document = documentGenService.generateSingleDocument(
             authorisation,
             caseData,
@@ -109,8 +130,6 @@ public class CaseApplicationResponseController {
         );
         log.info("C7 Final document generated successfully for respondent ");
         updateCurrentRespondent(caseData, null, partyId);
-        CaseDetails caseDetailsReturn = null;
-        List<Element<ResponseDocuments>> responseDocumentsList = new ArrayList<>();
         if (document != null) {
             if (caseData.getCitizenResponseC7DocumentList() != null) {
                 responseDocumentsList.addAll(caseData.getCitizenResponseC7DocumentList());
@@ -122,14 +141,25 @@ public class CaseApplicationResponseController {
                 + partyDetails
                 .getLastName()).orElse("");
 
+            UserDetails userDetails = idamClient.getUserDetails(authorisation);
             Element<ResponseDocuments> responseDocumentElement = element(ResponseDocuments.builder()
                                                                              .partyName(partyName)
-                                                                             .createdBy(partyId)
+                                                                             .createdBy(userDetails.getFullName())
                                                                              .citizenDocument(document)
                                                                              .dateCreated(LocalDate.now())
                                                                              .build());
             responseDocumentsList.add(responseDocumentElement);
             caseData = caseData.toBuilder().citizenResponseC7DocumentList(responseDocumentsList).build();
+            caseData = generateOtherC1aAndC8Documents(
+                authorisation,
+                caseData,
+                currentRespondent,
+                callbackRequest,
+                document,
+                partyName,
+                userDetails
+            );
+
             caseDetailsReturn = caseService.updateCase(
                 caseData,
                 authorisation,
@@ -156,6 +186,106 @@ public class CaseApplicationResponseController {
             caseData,
             CaseData.class
         );
+    }
+
+    private CaseData generateOtherC1aAndC8Documents(String authorisation, CaseData caseData, Optional<Element<PartyDetails>> currentRespondent,
+                                                CallbackRequest callbackRequest, Document document, String partyName,
+                                                UserDetails userDetails) throws Exception {
+        Document c1aFinalDocument = null;
+        Document c8FinalDocument = null;
+        if (currentRespondent.isPresent()) {
+            Map<String, Object> dataMap = c100RespondentSolicitorService.populateDataMap(
+                callbackRequest,
+                currentRespondent.get()
+            );
+            if (isNotEmpty(currentRespondent.get().getValue().getResponse())
+                && isNotEmpty(currentRespondent.get().getValue().getResponse().getRespondentAllegationsOfHarmData())
+                && Yes.equals(currentRespondent.get().getValue().getResponse().getRespondentAllegationsOfHarmData().getRespAohYesOrNo())) {
+                c1aFinalDocument = documentGenService.generateSingleDocument(
+                    authorisation,
+                    caseData,
+                    SOLICITOR_C1A_FINAL_DOCUMENT,
+                    false,
+                    dataMap
+                );
+            }
+            RespondentDocs respondentDocs = RespondentDocs.builder().build();
+            if (dataMap.containsKey("isConfidentialDataPresent")) {
+                c8FinalDocument = documentGenService.generateSingleDocument(
+                    authorisation,
+                    caseData,
+                    C8_RESP_FINAL_HINT,
+                    false,
+                    dataMap
+                );
+            }
+            if (null != c1aFinalDocument) {
+                respondentDocs = respondentDocs
+                    .toBuilder()
+                    .c1aDocument(ResponseDocuments
+                                     .builder()
+                                     .partyName(partyName)
+                                     .createdBy(userDetails.getFullName())
+                                     .dateCreated(LocalDate.now())
+                                     .citizenDocument(c1aFinalDocument)
+                                     .build()
+                    )
+                    .build();
+            }
+            if (null != document) {
+                respondentDocs = respondentDocs
+                    .toBuilder()
+                    .c7Document(ResponseDocuments
+                                    .builder()
+                                    .partyName(partyName)
+                                    .createdBy(userDetails.getFullName())
+                                    .dateCreated(LocalDate.now())
+                                    .citizenDocument(document)
+                                    .build()
+                    )
+                    .build();
+            }
+            if (null != caseData.getRespondentDocsList()) {
+                caseData.getRespondentDocsList().add(element(respondentDocs));
+            } else {
+                caseData.setRespondentDocsList(List.of(element(respondentDocs)));
+            }
+            populateC8Documents(caseData, currentRespondent.get(), partyName, userDetails, c8FinalDocument);
+        }
+        return caseData;
+    }
+
+    private static void populateC8Documents(CaseData caseData, Element<PartyDetails> currentRespondent, String partyName,
+                                            UserDetails userDetails, Document c8FinalDocument) {
+        int partyIndex = caseData.getRespondents().indexOf(currentRespondent);
+        log.info("current respondent index " + partyIndex);
+        if (null != c8FinalDocument && partyIndex >= 0) {
+            ResponseDocuments c8ResponseDocuments = ResponseDocuments.builder()
+                .partyName(partyName)
+                .createdBy(userDetails.getFullName())
+                .dateCreated(LocalDate.now())
+                .citizenDocument(c8FinalDocument)
+                .build();
+            switch (partyIndex) {
+                case 0:
+                    caseData.setRespondentAc8(c8ResponseDocuments);
+                    break;
+                case 1:
+                    caseData.setRespondentBc8(c8ResponseDocuments);
+                    break;
+                case 2:
+                    caseData.setRespondentCc8(c8ResponseDocuments);
+                    break;
+                case 3:
+                    caseData.setRespondentDc8(c8ResponseDocuments);
+                    break;
+                case 4:
+                    caseData.setRespondentEc8(c8ResponseDocuments);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
     private CaseData updateCurrentRespondent(CaseData caseData, YesOrNo currentRespondent, String partyId) {
