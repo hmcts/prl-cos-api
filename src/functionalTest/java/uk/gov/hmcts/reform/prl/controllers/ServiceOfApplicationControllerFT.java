e1ea9f1bcc7e3e97c468fe9d3e87a2af52f3a2ad
parvezhmcts <parvez.qureshi@hmcts.net>
2023-06-26 09:48:05 +0100

    - fixed and added functional test for SOA

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/ServiceOfApplicationControllerFT.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/ServiceOfApplicationControllerFT.java
@@ -4,14 +4,25 @@ import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.prl.ResourceLoader;
+import uk.gov.hmcts.reform.prl.utils.IdamTokenGenerator;
 
+import static org.hamcrest.Matchers.equalTo;
 
-
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration
 public class ServiceOfApplicationControllerFT {
 
-    private static final String VALID_REQUEST_BODY = "requests/manage-order-fetch-children-request.json";
+    private static final String VALID_REQUEST_BODY = "requests/service-of-application.json";
 
+    @Autowired
+    protected IdamTokenGenerator idamTokenGenerator;
 
     private final String targetInstance =
         StringUtils.defaultIfBlank(
@@ -22,7 +33,7 @@ public class ServiceOfApplicationControllerFT {
     private final RequestSpecification request = RestAssured.given().relaxedHTTPSValidation().baseUri(targetInstance);
 
     @Test
-    public void givenRequestWithCaseData_ResponseContainsHeaderAndCollapsable() throws Exception {
+    public void givenRequestWithCaseData_AboutToStart() throws Exception {
 
         final String userToken = "Bearer testToken";
 
@@ -33,6 +44,23 @@ public class ServiceOfApplicationControllerFT {
             .when()
             .contentType("application/json")
             .post("/service-of-application/about-to-start")
+            .then()
+            .body("data.soaOtherPeoplePresentInCaseFlag", equalTo("Yes"))
+            .body("data.isCafcass", equalTo("No"))
+            .assertThat().statusCode(200);
+    }
+
+    @Test
+    public void givenRequestWithCaseData_Submitted() throws Exception {
+
+
+        String requestBody = ResourceLoader.loadJson(VALID_REQUEST_BODY);
+        request
+            .header("Authorization", idamTokenGenerator.generateIdamTokenForSystem())
+            .body(requestBody)
+            .when()
+            .contentType("application/json")
+            .post("/service-of-application/submitted")
             .then()
             .assertThat().statusCode(200);
     }
