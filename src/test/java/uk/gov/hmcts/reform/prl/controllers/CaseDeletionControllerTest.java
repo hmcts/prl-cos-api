e3f090b7a16e92b5ff7397e565a370fdcc788c6c
swaroopa86 <pendyalaswaroopa86@gmail.com>
2023-06-29 19:49:21 +0100

    additional test cases are added

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/CaseDeletionControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/CaseDeletionControllerTest.java
@@ -1,9 +1,11 @@
 package uk.gov.hmcts.reform.prl.controllers;
 
 import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
@@ -14,6 +16,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -46,6 +50,30 @@ public class CaseDeletionControllerTest {
             .handleAboutToSubmitEvent(authToken,s2sToken,callbackRequest);
 
         assertThat(callbackResponse.getData()).isEmpty();
+    }
+
+    @Test
+    public void testExceptionForHandleAboutToSubmitEvent() throws Exception {
+        Map<String, Object> caseDataMap = new HashMap<>();
+        caseDataMap.put("applicantCaseName", "testCaseName");
+
+        CaseDetails caseDetails = CaseDetails.builder()
+            .id(123L)
+            .data(caseDataMap)
+            .build();
+        CallbackRequest callbackRequest = CallbackRequest.builder()
+            .caseDetails(caseDetails)
+            .build();
+        Mockito.when(authorisationService.isAuthorized(authToken, s2sToken)).thenReturn(false);
+        assertExpectedException(() -> {
+            caseDeletionController.handleAboutToSubmitEvent(authToken,s2sToken,callbackRequest);
+        }, RuntimeException.class, "Invalid Client");
+    }
+
+    protected <T extends Throwable> void assertExpectedException(ThrowingRunnable methodExpectedToFail, Class<T> expectedThrowableClass,
+                                                                 String expectedMessage) {
+        T exception = assertThrows(expectedThrowableClass, methodExpectedToFail);
+        assertEquals(expectedMessage, exception.getMessage());
     }
 
 }
