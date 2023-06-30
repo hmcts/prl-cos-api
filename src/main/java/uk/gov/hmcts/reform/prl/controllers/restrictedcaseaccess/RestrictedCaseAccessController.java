673499e4adf0e01f5cf1aa9bb9e32740e59b8239
Aloknath Datta <alokdatta.uit@gmail.com>
2023-06-30 22:07:09 +0100

    Mark case as restricted API exposed

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/restrictedcaseaccess/RestrictedCaseAccessController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/restrictedcaseaccess/RestrictedCaseAccessController.java
@@ -10,11 +10,11 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 
