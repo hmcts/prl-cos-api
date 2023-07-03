21711bfd2fdcb8004b6cda227bc0c0b233da3b89
Aloknath Datta <alokdatta.uit@gmail.com>
2023-07-03 10:35:21 +0100

    Extended CCD API added

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/restrictedcaseaccess/RestrictedCaseAccessController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/restrictedcaseaccess/RestrictedCaseAccessController.java
@@ -184,13 +184,38 @@ public class RestrictedCaseAccessController {
         @RequestBody CallbackRequest callbackRequest) {
         log.info("markAsRestricted7");
         Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
-        caseDataUpdated.put("securityClassification", "RESTRICTED");
         log.info("caseDataUpdated::" + caseDataUpdated);
         Map<String, Object> dataClassification
             = caseDataService.getDataClassification(String.valueOf(callbackRequest.getCaseDetails().getId()));
         log.info("dataClassification::" + dataClassification);
         Map<String, Object> securityUpdated = new HashMap<>();
         securityUpdated.put("securityClassification", "RESTRICTED");
+        log.info("securityUpdated::" + securityUpdated);
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(caseDataUpdated)
+            .dataClassification(dataClassification)
+            .securityClassification(securityUpdated)
+            .build();
+    }
+
+    @PostMapping(path = "/mark-as-restricted-8", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
+    @Operation(description = "Mark case as restricted")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Callback processed.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AboutToStartOrSubmitCallbackResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)})
+    @SecurityRequirement(name = "Bearer Authentication")
+    public AboutToStartOrSubmitCallbackResponse markAsRestricted8(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
+        @RequestBody CallbackRequest callbackRequest) {
+        log.info("markAsRestricted7");
+        Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
+        log.info("caseDataUpdated::" + caseDataUpdated);
+        Map<String, Object> dataClassification
+            = caseDataService.getDataClassification(String.valueOf(callbackRequest.getCaseDetails().getId()));
+        log.info("dataClassification::" + dataClassification);
+        Map<String, Object> securityUpdated = new HashMap<>();
+        securityUpdated.put("security_classification", "RESTRICTED");
         log.info("securityUpdated::" + securityUpdated);
         return AboutToStartOrSubmitCallbackResponse.builder()
             .data(caseDataUpdated)
