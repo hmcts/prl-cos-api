b184ffde41f4e60155a65e9cc0a1f6c44cf2e8bd
Aloknath Datta <alokdatta.uit@gmail.com>
2023-07-04 12:48:04 +0100

    Bespoke AboutToStartOrSubmitCallbackResponse

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/restrictedcaseaccess/RestrictedCaseAccessController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/restrictedcaseaccess/RestrictedCaseAccessController.java
@@ -311,5 +311,24 @@ public class RestrictedCaseAccessController {
             .securityClassification("RESTRICTED")
             .build();
     }
+
+    @PostMapping(path = "/mark-as-restricted-12", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
+    @Operation(description = "Mark case as restricted")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Callback processed.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AboutToStartOrSubmitCallbackResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)})
+    @SecurityRequirement(name = "Bearer Authentication")
+    public uk.gov.hmcts.reform.prl.models.ccd.AboutToStartOrSubmitCallbackResponse markAsRestricted12(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
+        @RequestBody CallbackRequest callbackRequest) {
+        log.info("markAsRestricted7");
+        Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
+        log.info("caseDataUpdated::" + caseDataUpdated);
+        return uk.gov.hmcts.reform.prl.models.ccd.AboutToStartOrSubmitCallbackResponse.builder()
+            .data(caseDataUpdated)
+            .securityClassification("RESTRICTED")
+            .build();
+    }
 }
 
