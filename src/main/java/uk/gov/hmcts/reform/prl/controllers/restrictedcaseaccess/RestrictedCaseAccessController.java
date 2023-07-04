fd7c5b845a944320ee496a4ad351e629d5b2dd75
Aloknath Datta <alokdatta.uit@gmail.com>
2023-07-04 11:58:58 +0100

    Bespoke AboutToStartOrSubmitCallbackResponse

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/restrictedcaseaccess/RestrictedCaseAccessController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/restrictedcaseaccess/RestrictedCaseAccessController.java
@@ -288,5 +288,28 @@ public class RestrictedCaseAccessController {
             .securityClassification(securityUpdated)
             .build();
     }
+
+    @PostMapping(path = "/mark-as-restricted-11", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
+    @Operation(description = "Mark case as restricted")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Callback processed.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AboutToStartOrSubmitCallbackResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)})
+    @SecurityRequirement(name = "Bearer Authentication")
+    public uk.gov.hmcts.reform.prl.models.ccd.AboutToStartOrSubmitCallbackResponse markAsRestricted11(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
+        @RequestBody CallbackRequest callbackRequest) {
+        log.info("markAsRestricted7");
+        Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
+        log.info("caseDataUpdated::" + caseDataUpdated);
+        Map<String, Object> dataClassification
+            = caseDataService.getDataClassification(String.valueOf(callbackRequest.getCaseDetails().getId()));
+        log.info("dataClassification::" + dataClassification);
+        return uk.gov.hmcts.reform.prl.models.ccd.AboutToStartOrSubmitCallbackResponse.builder()
+            .data(caseDataUpdated)
+            .dataClassification(dataClassification)
+            .securityClassification("RESTRICTED")
+            .build();
+    }
 }
 
