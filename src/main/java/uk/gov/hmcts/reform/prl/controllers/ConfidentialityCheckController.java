bccea50b507f710755c99cb326eeb22a05d515be
parvezhmcts <parvez.qureshi@hmcts.net>
2023-07-05 09:24:40 +0100

    PRL-3884- Confidentiality check related changes
    - submitted event endpoint added.

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ConfidentialityCheckController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ConfidentialityCheckController.java
@@ -1,19 +1,26 @@
 package uk.gov.hmcts.reform.prl.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.reform.prl.services.ServiceOfApplicationService;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping("/confidentiality-check")
@@ -22,6 +29,10 @@ public class ConfidentialityCheckController {
 
     @Autowired
     private ServiceOfApplicationService serviceOfApplicationService;
+
+    public static final String CONFIDENTIAL_CONFIRMATION_NO_HEADER = "# The application will be reviewed for confidential details";
+    public static final String CONFIDENTIAL_CONFIRMATION_NO_BODY_PREFIX = "### What happens next \n\n The application cannot "
+        + "be served. The packs will be sent to the filling team to be redacted.";
 
 
     @PostMapping(path = "/about-to-start", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
@@ -34,6 +45,20 @@ public class ConfidentialityCheckController {
     ) {
         return AboutToStartOrSubmitCallbackResponse.builder().data(serviceOfApplicationService.getConfidentialPacks(
             callbackRequest.getCaseDetails())).build();
+    }
+
+    @PostMapping(path = "/submitted", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
+    @Operation(description = "Confidentiality check submitted event")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Callback processed."),
+        @ApiResponse(responseCode = "400", description = "Bad Request")})
+    @SecurityRequirement(name = "Bearer Authentication")
+    public ResponseEntity<SubmittedCallbackResponse> handleSubmitted(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
+        @RequestBody CallbackRequest callbackRequest) throws Exception {
+
+        return ok(SubmittedCallbackResponse.builder().confirmationBody(
+            CONFIDENTIAL_CONFIRMATION_NO_BODY_PREFIX).build());
     }
 
 }
