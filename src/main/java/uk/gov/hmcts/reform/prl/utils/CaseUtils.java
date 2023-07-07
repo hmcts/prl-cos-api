9a3611a20343beb370ff9314ae6e22a4319ee52e
Aloknath Datta <alokdatta.uit@gmail.com>
2023-07-07 13:05:20 +0100

    Prod issue check

--- src/main/java/uk/gov/hmcts/reform/prl/utils/CaseUtils.java
+++ src/main/java/uk/gov/hmcts/reform/prl/utils/CaseUtils.java
@@ -312,6 +312,7 @@ public class CaseUtils {
         }
 
         List<String> roles = userDetails.getRoles();
+        log.info("Vaidating prod issue: roles retrieved from IDAM: " + roles);
         if (roles.contains(SOLICITOR_ROLE)) {
             return SOLICITOR;
         } else if (roles.contains(CAFCASS_ROLE)) {
