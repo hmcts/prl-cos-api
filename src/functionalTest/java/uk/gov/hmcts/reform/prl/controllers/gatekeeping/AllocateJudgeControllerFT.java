2f14e57f365d8d6c675da4dcf63452f8f84efe85
Swaroopa <pendyalaswaroopa86@gmail.com>
2023-07-10 20:04:45 +0100

    manageOrdersControllerFT is updated

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/gatekeeping/AllocateJudgeControllerFT.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/gatekeeping/AllocateJudgeControllerFT.java
@@ -11,7 +11,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
