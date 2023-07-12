f9ec4ace94a707087d0fce03e9811dc92768535e
Swaroopa <pendyalaswaroopa86@gmail.com>
2023-07-12 09:57:46 +0100

    TaskListControllerTest is updated

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/TaskListControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/TaskListControllerTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.prl.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.function.ThrowingRunnable;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -15,7 +14,6 @@ import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 import uk.gov.hmcts.reform.prl.enums.State;
 import uk.gov.hmcts.reform.prl.events.CaseDataChanged;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
-import uk.gov.hmcts.reform.prl.services.AuthorisationService;
 import uk.gov.hmcts.reform.prl.services.EventService;
 import uk.gov.hmcts.reform.prl.services.UserService;
 import uk.gov.hmcts.reform.prl.services.tab.alltabs.AllTabServiceImpl;
@@ -24,9 +22,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -53,11 +48,7 @@ public class TaskListControllerTest {
     CaseDetails caseDetails;
     CaseData caseData;
     CallbackRequest callbackRequest;
-    @Mock
-    private AuthorisationService authorisationService;
-
-    public static final String authToken = "Bearer TestAuthToken";
-    public static final String s2sToken = "s2s AuthToken";
+    String auth = "authorisation";
 
     @Before
     public void setup() {
@@ -76,7 +67,6 @@ public class TaskListControllerTest {
             .build();
 
         when(objectMapper.convertValue(caseDetails.getData(), CaseData.class)).thenReturn(caseData);
-        when(authorisationService.isAuthorized(any(),any())).thenReturn(true);
     }
 
     @Test
@@ -91,38 +81,14 @@ public class TaskListControllerTest {
     @Test
     public void testHandleSubmittedWithoutCourtStaffRoles() {
         when(userService.getUserDetails(Mockito.anyString())).thenReturn(UserDetails.builder().roles(List.of("test role")).build());
-        taskListController.handleSubmitted(callbackRequest, authToken, s2sToken);
+        taskListController.handleSubmitted(callbackRequest, "testAuth");
         verify(tabService,times(1)).updateAllTabsIncludingConfTab(Mockito.any(CaseData.class));
     }
 
     @Test
     public void testHandleSubmittedWithCourtStaffRoles() {
         when(userService.getUserDetails(Mockito.anyString())).thenReturn(UserDetails.builder().roles(ROLES).build());
-        taskListController.handleSubmitted(callbackRequest, authToken, s2sToken);
+        taskListController.handleSubmitted(callbackRequest, "testAuth");
         verify(tabService,times(1)).updateAllTabsIncludingConfTab(Mockito.any(CaseData.class));
-    }
-
-    @Test
-    public void testExceptionForHandleSubmittedWithoutCourtStaffRoles() {
-        when(userService.getUserDetails(Mockito.anyString())).thenReturn(UserDetails.builder().roles(List.of("test role")).build());
-        Mockito.when(authorisationService.isAuthorized(authToken, s2sToken)).thenReturn(false);
-        assertExpectedException(() -> {
-            taskListController.handleSubmitted(callbackRequest, authToken, s2sToken);
-        }, RuntimeException.class, "Invalid Client");
-    }
-
-    @Test
-    public void testExceptionForHandleSubmittedWithCourtStaffRoles() {
-        when(userService.getUserDetails(Mockito.anyString())).thenReturn(UserDetails.builder().roles(ROLES).build());
-        Mockito.when(authorisationService.isAuthorized(authToken, s2sToken)).thenReturn(false);
-        assertExpectedException(() -> {
-            taskListController.handleSubmitted(callbackRequest, authToken, s2sToken);
-        }, RuntimeException.class, "Invalid Client");
-    }
-
-    protected <T extends Throwable> void assertExpectedException(ThrowingRunnable methodExpectedToFail, Class<T> expectedThrowableClass,
-                                                                 String expectedMessage) {
-        T exception = assertThrows(expectedThrowableClass, methodExpectedToFail);
-        assertEquals(expectedMessage, exception.getMessage());
     }
 }
