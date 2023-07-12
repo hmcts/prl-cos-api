92b1e6ecaff4c8f6ca5842a6e6e5c1aa02e08b88
Praneeth G <praneethkumar.gadhe@hmcts.net>
2023-07-12 09:43:26 +0100

    changes reverted

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/migration/MigrationController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/migration/MigrationController.java
@@ -13,15 +13,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.prl.controllers.AbstractCallbackController;
-import uk.gov.hmcts.reform.prl.enums.State;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.services.tab.alltabs.AllTabServiceImpl;
-
-import java.util.List;
-import java.util.Map;
 
 @Tag(name = "migration-controller")
 @Slf4j
@@ -36,49 +31,11 @@ public class MigrationController extends AbstractCallbackController {
     AllTabServiceImpl tabService;
 
     @PostMapping("/submitted")
-    public AboutToStartOrSubmitCallbackResponse handleSubmitted(@RequestBody CallbackRequest callbackRequest,
+    public void handleSubmitted(@RequestBody CallbackRequest callbackRequest,
                                 @RequestHeader(HttpHeaders.AUTHORIZATION)
                                 @Parameter(hidden = true) String authorisation) {
         CaseData caseData = getCaseData(callbackRequest.getCaseDetails());
-        Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
-
-        if (State.JUDICIAL_REVIEW.equals(caseData.getState()) // && closedCaseIdFromProd.contains(caseData.getId())
-        ) {
-            caseData = caseData.toBuilder().state(State.PREPARE_FOR_HEARING_CONDUCT_HEARING).build();
-
-            log.info("updating state via migration tool with  case id: {}",caseData.getId());
-            log.info("updating tabs migration data for the case id: {}",caseData.getId());
-            tabService.updateAllTabsIncludingConfTab(caseData);
-            return AboutToStartOrSubmitCallbackResponse.builder().data(caseDataUpdated)
-                    .state(State.PREPARE_FOR_HEARING_CONDUCT_HEARING.getValue()).build();
-
-        }
-
         log.info("updating tabs migration data for the case id: {}",caseData.getId());
         tabService.updateAllTabsIncludingConfTab(caseData);
-        return AboutToStartOrSubmitCallbackResponse.builder()
-                .build();
-    }
-
-    @PostMapping("/about-to-start")
-    public AboutToStartOrSubmitCallbackResponse handleAboutToSubmitted(@RequestBody CallbackRequest callbackRequest,
-                                                                @RequestHeader(HttpHeaders.AUTHORIZATION)
-                                @Parameter(hidden = true) String authorisation) {
-        List<Long> closedCaseIdFromProd = List.of(Long.valueOf("1686576623239199"));
-        CaseData caseData = getCaseData(callbackRequest.getCaseDetails());
-
-        if (State.JUDICIAL_REVIEW.equals(caseData.getState()) // && closedCaseIdFromProd.contains(caseData.getId())
-        ) {
-            Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
-            log.info("updating state via migration tool with  case id: {}",caseData.getId());
-            return AboutToStartOrSubmitCallbackResponse.builder().data(caseDataUpdated)
-                            .state(State.PREPARE_FOR_HEARING_CONDUCT_HEARING.getValue()).build();
-
-        }
-        log.info("triggered migration about to submit event for the case id: {}",caseData.getId());
-
-        return AboutToStartOrSubmitCallbackResponse.builder()
-                .build();
-
     }
 }
--- src/test/java/uk/gov/hmcts/reform/prl/controllers/migration/MigrationControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/migration/MigrationControllerTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.prl.controllers.migration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,7 +8,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.test.context.junit4.SpringRunner;
-import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.prl.enums.State;
@@ -48,11 +46,11 @@ public class MigrationControllerTest {
         caseDataMap = new HashMap<>();
         caseData = CaseData.builder()
                 .id(12345678L)
-                .state(State.JUDICIAL_REVIEW)
+                .state(State.SUBMITTED_PAID)
                 .build();
         caseDetails = CaseDetails.builder()
                 .id(12345678L)
-                .state(State.JUDICIAL_REVIEW.getValue())
+                .state(State.SUBMITTED_PAID.getValue())
                 .data(caseDataMap)
                 .build();
         callbackRequest = CallbackRequest.builder()
@@ -66,48 +64,5 @@ public class MigrationControllerTest {
     public void handleSubmitted() {
         migrationController.handleSubmitted(callbackRequest, "testAuth");
         verify(tabService,times(1)).updateAllTabsIncludingConfTab(Mockito.any(CaseData.class));
-    }
-
-    @Test
-    public void handleSubmittedStateRemains() {
-        caseData = CaseData.builder()
-                .id(12345678L)
-                .state(State.SUBMITTED_PAID)
-                .build();
-        caseDetails = CaseDetails.builder()
-                .id(12345678L)
-                .state(State.SUBMITTED_PAID.getValue())
-                .data(caseDataMap)
-                .build();
-        callbackRequest = CallbackRequest.builder()
-                .caseDetails(caseDetails)
-                .build();
-        migrationController.handleSubmitted(callbackRequest, "testAuth");
-        verify(tabService,times(1)).updateAllTabsIncludingConfTab(Mockito.any(CaseData.class));
-    }
-
-    @Test
-    public void handleAboutToSubmitted() {
-        AboutToStartOrSubmitCallbackResponse response = migrationController.handleAboutToSubmitted(callbackRequest, "testAuth");
-        Assert.assertNotNull(response.getState());
-    }
-
-    @Test
-    public void handleAboutToSubmittedNullState() {
-
-        caseData = CaseData.builder()
-                .id(12345678L)
-                .state(State.SUBMITTED_PAID)
-                .build();
-        caseDetails = CaseDetails.builder()
-                .id(12345678L)
-                .state(State.SUBMITTED_PAID.getValue())
-                .data(caseDataMap)
-                .build();
-        callbackRequest = CallbackRequest.builder()
-                .caseDetails(caseDetails)
-                .build();
-        AboutToStartOrSubmitCallbackResponse response = migrationController.handleAboutToSubmitted(callbackRequest, "testAuth");
-        Assert.assertNull(response.getState());
     }
 }
