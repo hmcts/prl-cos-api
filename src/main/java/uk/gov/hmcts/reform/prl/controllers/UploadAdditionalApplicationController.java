d1fec166230b00d048a13a112ae9fcf2e1d5012f
sahaSonali <sonali.saha@hmcts.net>
2023-07-13 11:37:01 +0100

    Merge with master

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/UploadAdditionalApplicationController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/UploadAdditionalApplicationController.java
@@ -16,12 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
+import uk.gov.hmcts.reform.prl.constants.PrlAppsConstants;
+import uk.gov.hmcts.reform.prl.services.AuthorisationService;
 import uk.gov.hmcts.reform.prl.services.UploadAdditionalApplicationService;
 
 import java.util.Map;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.springframework.http.ResponseEntity.ok;
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.INVALID_CLIENT;
 
 @RestController
 @Slf4j
@@ -30,16 +33,21 @@ public class UploadAdditionalApplicationController {
 
     @Autowired
     private final UploadAdditionalApplicationService uploadAdditionalApplicationService;
+    private final AuthorisationService authorisationService;
 
     @PostMapping(path = "/pre-populate-applicants", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
     @Operation(description = "Callback to Generate applicants")
     public AboutToStartOrSubmitCallbackResponse prePopulateApplicants(@RequestHeader("Authorization")
                                                                       @Parameter(hidden = true) String authorisation,
-                                                                      @RequestBody CallbackRequest callbackRequest) {
-        return AboutToStartOrSubmitCallbackResponse.builder().data(uploadAdditionalApplicationService.prePopulateApplicants(
-            callbackRequest, authorisation)).build();
+                                                                      @RequestBody CallbackRequest callbackRequest,
+                                                                      @RequestHeader(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER) String s2sToken) {
+        if (authorisationService.isAuthorized(authorisation, s2sToken)) {
+            return AboutToStartOrSubmitCallbackResponse.builder().data(uploadAdditionalApplicationService.prePopulateApplicants(
+                callbackRequest, authorisation)).build();
+        } else {
+            throw (new RuntimeException(INVALID_CLIENT));
+        }
     }
-
 
 
     @PostMapping(path = "/upload-additional-application/about-to-submit", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
@@ -48,14 +56,20 @@ public class UploadAdditionalApplicationController {
         @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)})
     public AboutToStartOrSubmitCallbackResponse createUploadAdditionalApplicationBundle(@RequestHeader("Authorization")
                                                                                         @Parameter(hidden = true) String authorisation,
-                                                                                        @RequestBody CallbackRequest callbackRequest) {
-        Map<String, Object> caseDataUpdated
-            = uploadAdditionalApplicationService.createUploadAdditionalApplicationBundle(
-            authorisation,
-            callbackRequest
-        );
+                                                                                        @RequestBody CallbackRequest callbackRequest,
+                                                                                        @RequestHeader(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER)
+                                                                                            String s2sToken) {
+        if (authorisationService.isAuthorized(authorisation, s2sToken)) {
+            Map<String, Object> caseDataUpdated
+                = uploadAdditionalApplicationService.createUploadAdditionalApplicationBundle(
+                authorisation,
+                callbackRequest
+            );
 
-        return AboutToStartOrSubmitCallbackResponse.builder().data(caseDataUpdated).build();
+            return AboutToStartOrSubmitCallbackResponse.builder().data(caseDataUpdated).build();
+        } else {
+            throw (new RuntimeException(INVALID_CLIENT));
+        }
     }
 
     @PostMapping(path = "/upload-additional-application/mid-event/calculate-fee", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
@@ -64,10 +78,16 @@ public class UploadAdditionalApplicationController {
         @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)})
     public AboutToStartOrSubmitCallbackResponse calculateAdditionalApplicationsFee(@RequestHeader("Authorization")
                                                                                    @Parameter(hidden = true) String authorisation,
-                                                                                   @RequestBody CallbackRequest callbackRequest) {
-        return AboutToStartOrSubmitCallbackResponse.builder().data(uploadAdditionalApplicationService.calculateAdditionalApplicationsFee(
-            callbackRequest
-        )).build();
+                                                                                   @RequestBody CallbackRequest callbackRequest,
+                                                                                   @RequestHeader(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER)
+                                                                                       String s2sToken) {
+        if (authorisationService.isAuthorized(authorisation, s2sToken)) {
+            return AboutToStartOrSubmitCallbackResponse.builder().data(uploadAdditionalApplicationService.calculateAdditionalApplicationsFee(
+                callbackRequest
+            )).build();
+        } else {
+            throw (new RuntimeException(INVALID_CLIENT));
+        }
     }
 
     @PostMapping(path = "/upload-additional-application/submitted", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
@@ -75,19 +95,31 @@ public class UploadAdditionalApplicationController {
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Bundle created"),
         @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)})
     public ResponseEntity<SubmittedCallbackResponse> uploadAdditionalApplicationSubmittedEvent(@RequestHeader("Authorization")
-                                                                                   @Parameter(hidden = true) String authorisation,
-                                                                                    @RequestBody CallbackRequest callbackRequest) {
-        return ok(uploadAdditionalApplicationService.uploadAdditionalApplicationSubmitted(callbackRequest));
+                                                                                               @Parameter(hidden = true) String authorisation,
+                                                                                               @RequestBody CallbackRequest callbackRequest,
+                                                                                               @RequestHeader
+                                                                                                       (PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER)
+                                                                                                   String s2sToken) {
+        if (authorisationService.isAuthorized(authorisation, s2sToken)) {
+            return ok(uploadAdditionalApplicationService.uploadAdditionalApplicationSubmitted(callbackRequest));
+        } else {
+            throw (new RuntimeException(INVALID_CLIENT));
+        }
     }
 
     @PostMapping(path = "/upload-additional-application/mid-event", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
     @Operation(description = "Callback to Generate applicants")
     public AboutToStartOrSubmitCallbackResponse populateHearingList(@RequestHeader("Authorization")
-                                                                        @Parameter(hidden = true) String authorisation,
-                                                                        @RequestBody CallbackRequest callbackRequest) {
-        return AboutToStartOrSubmitCallbackResponse.builder()
-            .data(uploadAdditionalApplicationService.populateHearingList(authorisation, callbackRequest))
-            .build();
+                                                                    @Parameter(hidden = true) String authorisation,
+                                                                    @RequestBody CallbackRequest callbackRequest,
+                                                                    @RequestHeader(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER) String s2sToken) {
+        if (authorisationService.isAuthorized(authorisation, s2sToken)) {
+            return AboutToStartOrSubmitCallbackResponse.builder()
+                .data(uploadAdditionalApplicationService.populateHearingList(authorisation, callbackRequest))
+                .build();
+        } else {
+            throw (new RuntimeException(INVALID_CLIENT));
+        }
     }
 
 }
