5dc695b9f9a421f64770eec5d4d6dfa1cc35e384
Aloknath Datta <alokdatta.uit@gmail.com>
2023-07-27 13:02:37 +0100

    test update for system update

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/restrictedcaseaccess/RestrictedCaseAccessController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/restrictedcaseaccess/RestrictedCaseAccessController.java
@@ -17,6 +17,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
+import uk.gov.hmcts.reform.ccd.client.model.EventRequestData;
+import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
+import uk.gov.hmcts.reform.prl.clients.ccd.CcdCoreCaseDataService;
+import uk.gov.hmcts.reform.prl.enums.CaseEvent;
+import uk.gov.hmcts.reform.prl.services.SystemUserService;
 import uk.gov.hmcts.reform.prl.services.extendedcasedataservice.ExtendedCaseDataService;
 
 import java.util.Map;
@@ -29,6 +35,8 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 @RequiredArgsConstructor
 public class RestrictedCaseAccessController {
     private final ExtendedCaseDataService caseDataService;
+    private final CcdCoreCaseDataService coreCaseDataService;
+    private final SystemUserService systemUserService;
 
     @PostMapping(path = "/mark-as-restricted", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
     @Operation(description = "Mark case as restricted")
@@ -51,6 +59,41 @@ public class RestrictedCaseAccessController {
             .dataClassification(dataClassification)
             .securityClassification("RESTRICTED")
             .build();
+    }
+
+    @PostMapping(path = "/mark-as-restricted-sys-update", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
+    @Operation(description = "Mark case as restricted")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Callback processed.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AboutToStartOrSubmitCallbackResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)})
+    @SecurityRequirement(name = "Bearer Authentication")
+    public void markAsRestrictedAsSysUpdate(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
+        @RequestBody CallbackRequest callbackRequest) {
+        String sysAuthorisation = systemUserService.getSysUserToken();
+        String systemUpdateUserId = systemUserService.getUserId(sysAuthorisation);
+        CaseEvent caseEvent = CaseEvent.UPDATE_ALL_TABS;
+        EventRequestData eventRequestData = coreCaseDataService.eventRequest(caseEvent, systemUpdateUserId);
+        StartEventResponse startEventResponse =
+            coreCaseDataService.startUpdate(
+                authorisation,
+                eventRequestData,
+                String.valueOf(callbackRequest.getCaseDetails().getId()),
+                true
+            );
+
+        CaseDataContent caseDataContent = coreCaseDataService.createCaseDataContentOnlyWithSecurityClassification(
+            startEventResponse
+        );
+
+        coreCaseDataService.submitUpdate(
+            authorisation,
+            eventRequestData,
+            caseDataContent,
+            String.valueOf(callbackRequest.getCaseDetails().getId()),
+            true
+        );
     }
 }
 
