697a7e615437b12fa26a5a743dec4fd109d29195
Aloknath Datta <alokdatta.uit@gmail.com>
2023-08-04 13:07:24 +0100

    HWF Fix for PRL-4158

--- src/main/java/uk/gov/hmcts/reform/prl/services/FeeAndPayServiceRequestService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/FeeAndPayServiceRequestService.java
@@ -7,21 +7,25 @@ import uk.gov.hmcts.reform.prl.enums.Event;
 import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CallbackRequest;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class FeeAndPayServiceRequestService {
-    public static final String HWF_PATTERN = "^\\w{3}-\\w{3}-\\w{3}$|^[A-Za-z]{2}\\d{2}-\\d{6}$";
+    public static final String HWF_SUPPRESSION_ERROR_MESSAGE
+        = "Help with Fees is not yet available in Family Private Law digital service. Select 'No' to continue with your application";
+    //public static final String HWF_PATTERN = "^\\w{3}-\\w{3}-\\w{3}$|^[A-Za-z]{2}\\d{2}-\\d{6}$";
 
-    public boolean validateHelpWithFeesNumber(CallbackRequest callbackRequest) {
+    //TODO: Validation is suppressed for the time being as system is not ready for HWF
+    /** public boolean validateHelpWithFeesNumber(CallbackRequest callbackRequest) {
         boolean invalidHwfNumber = false;
         Pattern pattern = Pattern.compile(HWF_PATTERN);
         Matcher matcher = null;
         if (Event.SUBMIT_AND_PAY.getId().equalsIgnoreCase(callbackRequest.getEventId())
             && YesOrNo.Yes.equals(callbackRequest.getCaseDetails().getCaseData().getHelpWithFees())) {
+
             matcher = pattern.matcher(callbackRequest.getCaseDetails().getCaseData().getHelpWithFeesNumber());
         } else if (Event.UPLOAD_ADDITIONAL_APPLICATIONS.getId().equalsIgnoreCase(callbackRequest.getEventId())
             && null != callbackRequest.getCaseDetails().getCaseData().getUploadAdditionalApplicationData()
@@ -32,9 +36,24 @@ public class FeeAndPayServiceRequestService {
                                           .getAdditionalApplicationsHelpWithFeesNumber());
         }
 
+
         if (null != matcher && !matcher.find()) {
             invalidHwfNumber = true;
         }
         return invalidHwfNumber;
+    } **/
+
+    public List<String> validateSuppressedHelpWithFeesCheck(CallbackRequest callbackRequest) {
+        List<String> errorList = new ArrayList<>();
+        if (Event.SUBMIT_AND_PAY.getId().equalsIgnoreCase(callbackRequest.getEventId())
+            && YesOrNo.Yes.equals(callbackRequest.getCaseDetails().getCaseData().getHelpWithFees())) {
+            errorList.add(HWF_SUPPRESSION_ERROR_MESSAGE);
+        } else if (Event.UPLOAD_ADDITIONAL_APPLICATIONS.getId().equalsIgnoreCase(callbackRequest.getEventId())
+            && null != callbackRequest.getCaseDetails().getCaseData().getUploadAdditionalApplicationData()
+            && YesOrNo.Yes.equals(callbackRequest.getCaseDetails().getCaseData().getUploadAdditionalApplicationData()
+                                      .getAdditionalApplicationsHelpWithFees())) {
+            errorList.add(HWF_SUPPRESSION_ERROR_MESSAGE);
+        }
+        return errorList;
     }
 }
