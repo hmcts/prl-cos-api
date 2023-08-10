a50e84d59b2a51bd276999e91da64ab5eded1f2b
REMYA MARY JAMES <RemyaMary.James@hmcts.net>
2023-08-10 15:02:23 +0100

    fixed checkstyle

--- src/main/java/uk/gov/hmcts/reform/prl/services/LocationRefDataService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/LocationRefDataService.java
@@ -10,7 +10,11 @@ import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicListElement;
 import uk.gov.hmcts.reform.prl.models.court.CourtDetails;
 import uk.gov.hmcts.reform.prl.models.court.CourtVenue;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static org.apache.logging.log4j.util.Strings.concat;
