460c0be2ba48c803c309f097b4da381322aff898
channa-patil <channabasappagouda.patil@hmcts.net>
2023-08-10 16:12:45 +0100

    fix - added loggers for debug submit citizen docs

--- src/main/java/uk/gov/hmcts/reform/prl/models/documents/Document.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/documents/Document.java
@@ -19,8 +19,8 @@ public class Document {
     String documentFileName;
     @JsonProperty("document_hash")
     String documentHash;
-    @JsonProperty("category_id")
-    String categoryId;
+    /*@JsonProperty("category_id")
+    String categoryId;*/
     @JsonProperty("document_creation_date")
     Date documentCreatedOn;
 
@@ -32,13 +32,13 @@ public class Document {
                     @JsonProperty("document_binary_url") String documentBinaryUrl,
                     @JsonProperty("document_filename") String documentFileName,
                     @JsonProperty("document_hash") String documentHash,
-                    @JsonProperty("category_id") String categoryId,
+                    //@JsonProperty("category_id") String categoryId,
                     Date documentCreatedOn) {
         this.documentUrl = documentUrl;
         this.documentBinaryUrl = documentBinaryUrl;
         this.documentFileName = documentFileName;
         this.documentHash = documentHash;
-        this.categoryId = categoryId;
+        //this.categoryId = categoryId;
         this.documentCreatedOn = documentCreatedOn;
 
     }
