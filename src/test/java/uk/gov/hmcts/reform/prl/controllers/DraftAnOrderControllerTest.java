8e2fcd9b08b77063a73a38df42b8328bf3cb10f9
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2023-08-11 17:50:29 +0100

    testcase-fix

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/DraftAnOrderControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/DraftAnOrderControllerTest.java
@@ -222,6 +222,7 @@ public class DraftAnOrderControllerTest {
         }
         Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
         when(authorisationService.isAuthorized(any(),any())).thenReturn(true);
+        when(draftAnOrderService.handlePopulateDraftOrderFields(any(),any())).thenReturn(caseDataUpdated);
         Assert.assertEquals(caseDataUpdated, draftAnOrderController.populateFl404Fields(authToken, s2sToken, callbackRequest).getData());
     }
 
@@ -256,6 +257,7 @@ public class DraftAnOrderControllerTest {
         }
         Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
         when(authorisationService.isAuthorized(any(),any())).thenReturn(true);
+        when(draftAnOrderService.handlePopulateDraftOrderFields(any(),any())).thenReturn(caseDataUpdated);
         Assert.assertEquals(caseDataUpdated, draftAnOrderController.populateFl404Fields(authToken,s2sToken,callbackRequest).getData());
     }
 
