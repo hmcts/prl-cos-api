610144528ed7e3742839e3c350c26566ddb10dd8
bharadwajsairam <bharadwajsairam001@gmail.com>
2023-08-11 13:43:47 +0100

    added Jsonproperty annotation

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/ServiceOfApplication.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/ServiceOfApplication.java
@@ -61,5 +61,6 @@ public class ServiceOfApplication {
     private final YesOrNo soaServeLocalAuthorityYesOrNo;
     private final String soaLaEmailAddress;
     private final YesOrNo soaServeC8ToLocalAuthorityYesOrNo;
+    @JsonProperty("soaDocumentDynamicListForLa")
     private final List<Element<DocumentListForLa>> soaDocumentDynamicListForLa;
 }
