f497f1a70df4300f6082a7ff6d0f34cfdd6cbb57
bharadwajsairam <bharadwajsairam001@gmail.com>
2023-08-11 13:32:54 +0100

    added serializer annotation

--- src/main/java/uk/gov/hmcts/reform/prl/models/serviceofapplication/DocumentListForLa.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/serviceofapplication/DocumentListForLa.java
@@ -1,15 +1,20 @@
 package uk.gov.hmcts.reform.prl.models.serviceofapplication;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import uk.gov.hmcts.reform.prl.enums.CustomEnumSerializer;
 import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicList;
 
 @Data
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Builder(toBuilder = true)
+@JsonSerialize(using = CustomEnumSerializer.class)
 public class DocumentListForLa {
+    @JsonProperty("documentsListForLa")
     private DynamicList documentsListForLa;
 }
