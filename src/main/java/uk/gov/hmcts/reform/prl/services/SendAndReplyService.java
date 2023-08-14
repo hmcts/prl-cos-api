c419cedfeb10437502931619d1d28174ce780aef
Yogendra Upasani <yogendra.upasani@hmcts.net>
2023-08-14 22:50:36 +0100

    PRL-4164 Update awp application status on Send and Reply message

--- src/main/java/uk/gov/hmcts/reform/prl/services/SendAndReplyService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/SendAndReplyService.java
@@ -473,19 +473,21 @@ public class SendAndReplyService {
             List<DynamicListElement> dynamicListElements = new ArrayList<>();
             additionalApplicationElements = caseData.getAdditionalApplicationsBundle();
             additionalApplicationElements.stream().forEach(additionalApplicationsBundleElement -> {
-                if (additionalApplicationsBundleElement.getValue().getOtherApplicationsBundle() != null) {
-                    dynamicListElements.add(DynamicListElement.builder().code("Other applications")
-                                                .label("Other applications - "
-                                                           .concat(additionalApplicationsBundleElement.getValue()
-                                                                       .getOtherApplicationsBundle().getUploadedDateTime()))
-                                                .build());
-                }
-                if (additionalApplicationsBundleElement.getValue().getC2DocumentBundle() != null) {
-                    dynamicListElements.add(DynamicListElement.builder().code("C2 application")
-                                                .label("C2 application - "
-                                                           .concat(additionalApplicationsBundleElement.getValue()
-                                                                       .getC2DocumentBundle().getUploadedDateTime()))
-                                                .build());
+                if (additionalApplicationsBundleElement.getValue().getApplicationStatus().equals("Submitted")) {
+                    if (additionalApplicationsBundleElement.getValue().getOtherApplicationsBundle() != null) {
+                        dynamicListElements.add(DynamicListElement.builder().code("Other applications")
+                                                    .label("Other applications - "
+                                                               .concat(additionalApplicationsBundleElement.getValue()
+                                                                           .getOtherApplicationsBundle().getUploadedDateTime()))
+                                                    .build());
+                    }
+                    if (additionalApplicationsBundleElement.getValue().getC2DocumentBundle() != null) {
+                        dynamicListElements.add(DynamicListElement.builder().code("C2 application")
+                                                    .label("C2 application - "
+                                                               .concat(additionalApplicationsBundleElement.getValue()
+                                                                           .getC2DocumentBundle().getUploadedDateTime()))
+                                                    .build());
+                    }
                 }
             });
             return getDynamicList(dynamicListElements);
