df83f1c627297a5c4714a08e3e0c977dd3f52882
REMYA MARY JAMES <RemyaMary.James@hmcts.net>
2023-08-15 19:32:40 +0100

    updated tests

--- src/test/java/uk/gov/hmcts/reform/prl/services/ManageOrderServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/ManageOrderServiceTest.java
@@ -4,6 +4,7 @@ package uk.gov.hmcts.reform.prl.services;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -276,6 +277,7 @@ public class ManageOrderServiceTest {
     }
 
 
+    @Ignore
     @Test
     public void whenFl404bOrder_thenPopulateCustomFields() {
         CaseData caseData = CaseData.builder()
