3c6ff5fffd684aedd8463be390fcb8c6491a8513
Yogendra Upasani <yogendra.upasani@hmcts.net>
2023-08-18 17:41:45 +0100

    PRL-4206 POC for converting doc to pdfs

--- src/main/java/uk/gov/hmcts/reform/prl/clients/DgsApiClient.java
+++ src/main/java/uk/gov/hmcts/reform/prl/clients/DgsApiClient.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.openfeign.FeignClientProperties;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 import uk.gov.hmcts.reform.prl.models.dto.GenerateDocumentRequest;
 import uk.gov.hmcts.reform.prl.models.dto.GeneratedDocumentInfo;
 
