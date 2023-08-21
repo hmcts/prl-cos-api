9fafa27e56ccc24b3f22186091064b61c6a8ea60
bharadwajsairam <bharadwajsairam001@gmail.com>
2023-08-21 16:07:00 +0100

    revert changes

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/EditAndApproveDraftOrderControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/EditAndApproveDraftOrderControllerTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.prl.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.function.ThrowingRunnable;
 import org.junit.runner.RunWith;
@@ -829,7 +828,6 @@ public class EditAndApproveDraftOrderControllerTest {
             .sendEmailWhenOrderIsServed(callbackRequest.getCaseDetails());
     }
 
-    @Ignore
     @Test
     public void testEditAndServeOrderMidEvent() {
         final String authorisation = "Bearer someAuthorisationToken";
