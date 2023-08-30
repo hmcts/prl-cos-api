b49f2cde8fa89f008d234272fec74af867489834
JamesCutts00 <James.Cutts@HMCTS.ORG>
2023-08-30 14:53:23 +0100

    Added controller for creating courtnav case

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/testingsupport/TestingSupportController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/testingsupport/TestingSupportController.java
@@ -60,6 +60,28 @@ public class TestingSupportController {
         }
     }
 
+    @PostMapping(path = "/about-to-submit-case-creation-courtNav", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
+    @Operation(description = "Initiate the case creation for testing support for courtNav")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Callback processed.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AboutToStartOrSubmitCallbackResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)})
+    @SecurityRequirement(name = "Bearer Authentication")
+    public AboutToStartOrSubmitCallbackResponse aboutToSubmitCaseCreationCourtNav(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
+        @RequestHeader(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER) String s2sToken,
+        @RequestBody CallbackRequest callbackRequest
+    ) throws Exception {
+        if (authorisationService.isAuthorized(authorisation,s2sToken)) {
+            return AboutToStartOrSubmitCallbackResponse.builder().data(testingSupportService.initiateCaseCreation(
+                authorisation,
+                callbackRequest
+            )).build();
+        } else {
+            throw (new RuntimeException(INVALID_CLIENT));
+        }
+    }
+
     @PostMapping(path = "/fillRespondentTaskList", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
     @Operation(description = "Fill the respondent C7 form via testing support")
     @ApiResponses(value = {
