b1860b22c546f4a973f7e2cfc2dc654c715f215a
praneethhm <101885945+praneethhm@users.noreply.github.com>
2023-08-30 14:36:39 +0100

    added s2s token for payment service with feature toggle

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ServiceRequestUpdateCallbackController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ServiceRequestUpdateCallbackController.java
@@ -11,14 +11,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+import uk.gov.hmcts.reform.prl.config.launchdarkly.LaunchDarklyClient;
+import uk.gov.hmcts.reform.prl.constants.PrlAppsConstants;
 import uk.gov.hmcts.reform.prl.framework.exceptions.WorkflowException;
 import uk.gov.hmcts.reform.prl.models.dto.payment.ServiceRequestUpdateDto;
+import uk.gov.hmcts.reform.prl.services.AuthorisationService;
 import uk.gov.hmcts.reform.prl.services.RequestUpdateCallbackService;
 import uk.gov.hmcts.reform.prl.services.tab.alltabs.AllTabsService;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.INVALID_CLIENT;
 
 @Slf4j
 @RestController
@@ -31,23 +36,40 @@ public class ServiceRequestUpdateCallbackController extends AbstractCallbackCont
     @Qualifier("allTabsService")
     AllTabsService tabService;
 
+    @Autowired
+    private final AuthorisationService authorisationService;
+
+    @Autowired
+    private final LaunchDarklyClient launchDarklyClient;
+
     @PutMapping(path = "/service-request-update", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
     @Operation(description = "Ways to pay will call this API and send the status of payment with other details")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Callback processed.",
-            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CallbackResponse.class))),
+        @ApiResponse(responseCode = "200", description = "Callback processed.", content = @Content(mediaType = "application/json",
+            schema = @Schema(implementation = CallbackResponse.class))),
         @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)})
     public void serviceRequestUpdate(
-        @RequestBody ServiceRequestUpdateDto serviceRequestUpdateDto
+            @RequestHeader(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER) String s2sToken,
+            @RequestBody ServiceRequestUpdateDto serviceRequestUpdateDto
     ) throws WorkflowException {
+        if (launchDarklyClient.isFeatureEnabled("payment-app-s2sToken")) {
+            if (Boolean.TRUE.equals(authorisationService.authoriseService(s2sToken))) {
+                log.info("s2s token from payment service validation is successful");
+            } else {
+                log.info("s2s token from payment service validation is unsuccessful");
+                throw (new RuntimeException(INVALID_CLIENT));
+            }
+        }
+
         try {
             requestUpdateCallbackService.processCallback(serviceRequestUpdateDto);
         } catch (Exception ex) {
             log.error(
-                "Payment callback is unsuccessful for the CaseID: {}",
-                serviceRequestUpdateDto.getCcdCaseNumber()
+                    "Payment callback is unsuccessful for the CaseID: {}",
+                    serviceRequestUpdateDto.getCcdCaseNumber()
             );
             throw new WorkflowException(ex.getMessage(), ex);
         }
     }
 }
+
