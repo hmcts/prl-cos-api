be16d0b8cfc4d4d543b87a2f881a9b3c09b5898c
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2023-08-30 14:44:58 +0100

    json annotation inclusion

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/tab/summarytab/summary/DateOfSubmission.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/tab/summarytab/summary/DateOfSubmission.java
@@ -1,10 +1,12 @@
 package uk.gov.hmcts.reform.prl.models.complextypes.tab.summarytab.summary;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
 @Builder
 @Data
 public class DateOfSubmission {
+    @JsonProperty("dateOfSubmission")
     private final String dateOfSubmission;
 }
--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
@@ -82,6 +82,15 @@ import uk.gov.hmcts.reform.prl.models.complextypes.confidentiality.ApplicantConf
 import uk.gov.hmcts.reform.prl.models.complextypes.confidentiality.ChildConfidentialityDetails;
 import uk.gov.hmcts.reform.prl.models.complextypes.managedocuments.ManageDocuments;
 import uk.gov.hmcts.reform.prl.models.complextypes.respondentsolicitor.documents.RespondentDocs;
+import uk.gov.hmcts.reform.prl.models.complextypes.tab.summarytab.summary.ApplicationTypeDetails;
+import uk.gov.hmcts.reform.prl.models.complextypes.tab.summarytab.summary.CaseStatus;
+import uk.gov.hmcts.reform.prl.models.complextypes.tab.summarytab.summary.ConfidentialDetails;
+import uk.gov.hmcts.reform.prl.models.complextypes.tab.summarytab.summary.DateOfSubmission;
+import uk.gov.hmcts.reform.prl.models.complextypes.tab.summarytab.summary.OrderAppliedFor;
+import uk.gov.hmcts.reform.prl.models.complextypes.tab.summarytab.summary.OtherProceedingEmptyTable;
+import uk.gov.hmcts.reform.prl.models.complextypes.tab.summarytab.summary.OtherProceedings;
+import uk.gov.hmcts.reform.prl.models.complextypes.tab.summarytab.summary.SpecialArrangements;
+import uk.gov.hmcts.reform.prl.models.complextypes.tab.summarytab.summary.Urgency;
 import uk.gov.hmcts.reform.prl.models.complextypes.uploadadditionalapplication.AdditionalApplicationsBundle;
 import uk.gov.hmcts.reform.prl.models.documents.Document;
 import uk.gov.hmcts.reform.prl.models.dto.bundle.BundlingInformation;
@@ -761,6 +770,21 @@ public class CaseData extends BaseCaseData implements MappableObject {
 
     private final List<Element<StmtOfServiceAddRecipient>> stmtOfServiceAddRecipient;
 
+
+
     private final uk.gov.hmcts.reform.prl.models.complextypes.tab.summarytab.summary.AllocatedJudge allocatedJudgeDetails;
+    private final ConfidentialDetails confidentialDetails;
+    private final Urgency urgencyDetails;
+    //private final Hearings hearings;
+    private final SpecialArrangements specialArrangement;
+    private final OrderAppliedFor summaryTabForOrderAppliedFor;
+    private final List<Element<OtherProceedings>> otherProceedingsForSummaryTab;
+    @JsonProperty("otherProceedingEmptyField")
+    private final OtherProceedingEmptyTable otherProceedingEmptyTable;
+    @JsonProperty("dateOfSubmission")
+    private final DateOfSubmission dateOfSubmission;
+    @JsonProperty("applicationTypeDetails")
+    private final ApplicationTypeDetails applicationTypeDetails;
+    private final CaseStatus caseStatus;
 
 }
