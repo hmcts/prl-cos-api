3ed5fcea32a091c00a324e717d82b126ee05d821
bharadwajsairam <bharadwajsairam001@gmail.com>
2023-08-31 10:41:12 +0100

    revert mid event change

--- src/main/java/uk/gov/hmcts/reform/prl/services/ManageOrderService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ManageOrderService.java
@@ -2071,7 +2071,8 @@ public class ManageOrderService {
 
     public Map<String, Object> checkOnlyC47aOrderSelectedToServe(CallbackRequest callbackRequest) {
         CaseData caseData = CaseUtils.getCaseData(callbackRequest.getCaseDetails(), objectMapper);
-        Map<String, Object> caseDataUpdated = new HashMap<>();
+        Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
+        log.info("*** 123 ** {}", caseDataUpdated.get("orderCollection"));
         List<DynamicMultiselectListElement> selectedServedOrderList = caseData.getManageOrders().getServeOrderDynamicList().getValue();
         if (selectedServedOrderList != null && selectedServedOrderList.size() == 1
             && selectedServedOrderList.get(0).getLabel().contains(C_47_A)) {
