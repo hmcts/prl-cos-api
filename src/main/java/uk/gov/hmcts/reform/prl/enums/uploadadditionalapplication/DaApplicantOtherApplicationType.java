d8075365b6eca730861e1514003286dbd4511361
alokDatta <aloknath.datta@hmcts.net>
2023-09-07 13:58:37 +0100

    Getter added and json value removed

--- src/main/java/uk/gov/hmcts/reform/prl/enums/uploadadditionalapplication/DaApplicantOtherApplicationType.java
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/uploadadditionalapplication/DaApplicantOtherApplicationType.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.prl.enums.uploadadditionalapplication;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
