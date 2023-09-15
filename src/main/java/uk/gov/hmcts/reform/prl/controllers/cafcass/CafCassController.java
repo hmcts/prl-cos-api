d46097620609d4c56714bafe7dc62f5e35a56c4b
priteshkr <pritesh.kumar@hmcts.net>
2023-09-15 14:58:45 +0100

    Refactored Variable

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/cafcass/CafCassController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/cafcass/CafCassController.java
@@ -23,7 +23,6 @@ import uk.gov.hmcts.reform.prl.services.AuthorisationService;
 import uk.gov.hmcts.reform.prl.services.cafcass.CaseDataService;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-import static org.springframework.cloud.openfeign.security.OAuth2FeignRequestInterceptor.BEARER;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
@@ -41,6 +40,8 @@ public class CafCassController extends AbstractCallbackController {
 
     @Autowired
     private AuthorisationService authorisationService;
+
+    private static final String BEARER = "Bearer ";
 
     @GetMapping(path = "/searchCases", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
     @Operation(description = "search case data")
