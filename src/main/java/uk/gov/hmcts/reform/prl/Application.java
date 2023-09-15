4b4775e9be5c6e3b975ce751aeb8f1d3306063eb
priteshkr <pritesh.kumar@hmcts.net>
2023-09-15 14:13:27 +0100

    Added mapper package dependency

--- src/main/java/uk/gov/hmcts/reform/prl/Application.java
+++ src/main/java/uk/gov/hmcts/reform/prl/Application.java
@@ -26,7 +26,8 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootConfiguration
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {  "uk.gov.hmcts.reform.prl", "uk.gov.hmcts.reform.prl.services",
-    "uk.gov.hmcts.reform.prl.config","uk.gov.hmcts.reform.ccd.document","uk.gov.hmcts.reform.prl.repositories"})
+    "uk.gov.hmcts.reform.prl.config","uk.gov.hmcts.reform.ccd.document","uk.gov.hmcts.reform.prl.repositories",
+     "uk.gov.hmcts.reform.prl.mapper"})
 @Slf4j
 public class Application {
 
