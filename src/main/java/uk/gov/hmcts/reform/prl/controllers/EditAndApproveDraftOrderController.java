3de728b095abb95e7b11d248e5212f3b5bb234c9
JamesCutts00 <James.Cutts@HMCTS.ORG>
2023-09-15 14:42:15 +0100

    Added extra condition to if statement so white space isn't accepted

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/EditAndApproveDraftOrderController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/EditAndApproveDraftOrderController.java
@@ -194,9 +194,9 @@ public class EditAndApproveDraftOrderController {
                 CaseData.class
             );
             if (caseData.getManageOrders() != null) {
-                if (caseData.getManageOrders().getJudgeOrMagistrateTitle() == JudgeOrMagistrateTitleEnum
-                        .justicesLegalAdviser && caseData.getJusticeLegalAdviserFullName() != null && !caseData
-                        .getJusticeLegalAdviserFullName().isBlank()) {
+                if ((caseData.getManageOrders().getJudgeOrMagistrateTitle() == JudgeOrMagistrateTitleEnum
+                        .justicesLegalAdviser) && (caseData.getJusticeLegalAdviserFullName() == null || caseData
+                        .getJusticeLegalAdviserFullName().isBlank())) {
                     List<String> errorList = new ArrayList<>();
                     errorList.add("Full name of Justices' Legal Advisor is mandatory, when the Judge's title is selected as Justices' Legal Adviser");
                     return AboutToStartOrSubmitCallbackResponse.builder().errors(errorList).build();
