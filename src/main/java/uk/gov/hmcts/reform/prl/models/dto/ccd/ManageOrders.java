894da07b8085f58eb0a180e6ac78ee92ad5461a9
bharadwajsairam <bharadwajsairam001@gmail.com>
2023-10-02 13:44:37 +0100

    added underTakingExpiryDateTime

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/ManageOrders.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/ManageOrders.java
@@ -139,7 +139,9 @@ public class ManageOrders implements MappableObject {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private final LocalDate underTakingDateExpiry;
     private final String underTakingExpiryTime;
-    private final String underTakingExpiryDateTime;
+    @JsonProperty("underTakingExpiryDateTime")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    private final LocalDateTime underTakingExpiryDateTime;
     private final YesOrNo underTakingFormSign;
 
     private final YesOrNo isTheOrderByConsent;
