e607456d5846a7e891baae74561bdfc3d25e50cc
sahaSonali <sonali.saha@hmcts.net>
2023-10-02 17:37:47 +0100

    Fix for power of arrest

--- src/main/java/uk/gov/hmcts/reform/prl/services/ManageOrderService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ManageOrderService.java
@@ -2105,6 +2105,7 @@ public class ManageOrderService {
         } else {
             caseDataUpdated.put("previewOrderDoc", caseData.getUploadOrderDoc());
         }
+        log.info("populatePreviewOrder caseDataUpdated ===> " + caseDataUpdated);
         return caseDataUpdated;
     }
 
