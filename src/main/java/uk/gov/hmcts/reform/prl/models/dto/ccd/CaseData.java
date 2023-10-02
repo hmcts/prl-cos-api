83211f63fb474486dfcd5ea8f9516efdfcb26ee5
channa-patil <channabasappagouda.patil@hmcts.net>
2023-10-02 17:30:13 +0100

    Added new debug fields for dynamic list render test

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/CaseData.java
@@ -778,4 +778,13 @@ public class CaseData extends BaseCaseData implements MappableObject {
     private ReviewDocuments reviewDocuments;
     private final List<Element<StmtOfServiceAddRecipient>> stmtOfServiceAddRecipient;
 
+    @JsonProperty("isApplicant1Present")
+    private final YesOrNo isApplicant1Present;
+    @JsonProperty("isApplicant4Present")
+    private final YesOrNo isApplicant4Present;
+    @JsonProperty("isApplicant1SolicitorPresent")
+    private final YesOrNo isApplicant1SolicitorPresent;
+    @JsonProperty("isApplicant4SolicitorPresent")
+    private final YesOrNo isApplicant4SolicitorPresent;
+
 }
