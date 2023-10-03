2bbab026dadfdd0bf1e2d7130d381ec28810b435
alokDatta <aloknath.datta@hmcts.net>
2023-10-03 13:42:50 +0100

    Pointing 1569

--- src/main/java/uk/gov/hmcts/reform/prl/services/extendedcasedataservice/ExtendedCaseDataService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/extendedcasedataservice/ExtendedCaseDataService.java
@@ -9,12 +9,12 @@ import uk.gov.hmcts.reform.prl.clients.ccd.ExtendedCaseDataApi;
 import uk.gov.hmcts.reform.prl.models.extendedcasedetails.ExtendedCaseDetails;
 import uk.gov.hmcts.reform.prl.services.SystemUserService;
 
-import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@RequiredArgsConstructor
 public class ExtendedCaseDataService {
 
     @Autowired
@@ -32,15 +32,11 @@ public class ExtendedCaseDataService {
     public Map<String, Object> getDataClassification(String caseId) {
         String sysUserToken = systemUserService.getSysUserToken();
         String authToken = authTokenGenerator.generate();
-        log.info("** Testing sysUserToken:: " + sysUserToken);
-        log.info("** Testing authToken:: " + authToken);
         ExtendedCaseDetails caseDetails = caseDataApi.getExtendedCaseDetails(
             sysUserToken,
             authToken, caseId
         );
 
-        log.info("** Testing case data:: " + caseDetails.getCaseData());
-        log.info("** Testing case data classification:: " + caseDetails.getDataClassification());
         return caseDetails.getDataClassification();
     }
 }
