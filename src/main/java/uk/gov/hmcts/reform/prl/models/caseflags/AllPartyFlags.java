e82b6e83555f54429077efd5c63c386a7372d9a0
alokDatta <aloknath.datta@hmcts.net>
2023-10-04 16:05:32 +0100

    New party flags

---
+++ src/main/java/uk/gov/hmcts/reform/prl/models/caseflags/AllPartyFlags.java
@@ -0,0 +1,46 @@
+package uk.gov.hmcts.reform.prl.models.caseflags;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Value;
+import uk.gov.hmcts.reform.prl.models.noticeofchange.NoticeOfChangeParties;
+
+@Value
+@Builder
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class AllPartyFlags {
+    private PartyFlags caApplicant1;
+    private PartyFlags caApplicant2;
+    private PartyFlags caApplicant3;
+    private PartyFlags caApplicant4;
+    private PartyFlags caApplicant5;
+    private PartyFlags caApplicantSolicitor1;
+    private PartyFlags caApplicantSolicitor2;
+    private PartyFlags caApplicantSolicitor3;
+    private PartyFlags caApplicantSolicitor4;
+    private PartyFlags caApplicantSolicitor5;
+    private PartyFlags caRespondent1;
+    private PartyFlags caRespondent2;
+    private PartyFlags caRespondent3;
+    private PartyFlags caRespondent4;
+    private PartyFlags caRespondent5;
+    private PartyFlags caRespondentSolicitor1;
+    private PartyFlags caRespondentSolicitor2;
+    private PartyFlags caRespondentSolicitor3;
+    private PartyFlags caRespondentSolicitor4;
+    private PartyFlags caRespondentSolicitor5;
+    private PartyFlags caOtherParty1;
+    private PartyFlags caOtherParty2;
+    private PartyFlags caOtherParty3;
+    private PartyFlags caOtherParty4;
+    private PartyFlags caOtherParty5;
+    private PartyFlags caOtherParty6;
+    private PartyFlags caOtherParty7;
+    private PartyFlags caOtherParty8;
+    private PartyFlags caOtherParty9;
+    private PartyFlags caOtherParty10;
+    private PartyFlags daApplicant;
+    private PartyFlags daApplicantSolicitor1;
+    private PartyFlags daRespondent;
+    private PartyFlags caRespondentSolicitor;
+}
---
+++ src/main/java/uk/gov/hmcts/reform/prl/utils/caseflags/PartyLevelCaseFlagsGenerator.java
@@ -0,0 +1,36 @@
+package uk.gov.hmcts.reform.prl.utils.caseflags;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.prl.models.caseflags.Flags;
+import uk.gov.hmcts.reform.prl.models.caseflags.PartyFlags;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class PartyLevelCaseFlagsGenerator {
+    public Map<String, Object> generateFlags(String partyName, String caseDataField, String roleOnCase) {
+        Map<String, Object> data = new HashMap<>();
+        final Flags partyInternalFlag = Flags
+                .builder()
+                .partyName(partyName)
+                .roleOnCase(roleOnCase)
+                .details(Collections.emptyList())
+                .build();
+        final Flags partyExternalFlag = Flags
+                .builder()
+                .partyName(partyName)
+                .roleOnCase(roleOnCase)
+                .details(Collections.emptyList())
+                .build();
+        PartyFlags partyFlags = PartyFlags.builder()
+                .partyInternalFlag(partyInternalFlag)
+                .partyExternalFlags(partyExternalFlag)
+                .build();
+
+        data.put(caseDataField, partyFlags);
+
+        return data;
+    }
+}
