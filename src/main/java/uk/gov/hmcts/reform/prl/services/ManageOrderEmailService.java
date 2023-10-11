0dff9fff667f76ae9a274d6653a0ef8bdf68e153
JamesCutts00 <James.Cutts@HMCTS.ORG>
2023-10-11 12:23:18 +0100

    fix(prl-4234/Serve_order-other_organization): Fixed checkstyle issue

--- src/main/java/uk/gov/hmcts/reform/prl/services/ManageOrderEmailService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ManageOrderEmailService.java
@@ -456,7 +456,7 @@ public class ManageOrderEmailService {
                     && DeliveryByEnum.email.equals(manageOrders.getDeliveryByOptionsCaOnlyC47a())) {
                 manageOrders.getEmailInformationCaOnlyC47a().stream().map(Element::getValue).forEach(value -> listOfOtherAndCafcassEmails
                         .add(value.getEmailAddress()));
-                }
+            }
 
             //PRL-4225 - send order & additional docs to other people via post only
             if (isNotEmpty(manageOrders.getOtherParties()) || isNotEmpty(manageOrders.getOtherPartiesOnlyC47a())) {
