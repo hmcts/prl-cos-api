c4ad3afcbbc4b18fa41f826f06387a5d712640e7
sahaSonali <sonali.saha@hmcts.net>
2023-10-17 11:11:44 +0100

    rebase with manage order base brnach

--- src/main/java/uk/gov/hmcts/reform/prl/constants/PrlAppsConstants.java
+++ src/main/java/uk/gov/hmcts/reform/prl/constants/PrlAppsConstants.java
@@ -209,7 +209,7 @@ public class PrlAppsConstants {
     public static final String FAMILY_COURT_TYPE_ID = "18";
     public static final String[] HEARING_PAGE_NEEDED_ORDER_IDS = {"noticeOfProceedings","childArrangementsSpecificProhibitedOrder",
         "occupation","nonMolestation","amendDischargedVaried","noticeOfProceedingsNonParties","noticeOfProceedingsParties", "blank"};
-   public static final String RIGHT_TO_ASK_COURT = "As the direction has been made without a hearing,"
+    public static final String RIGHT_TO_ASK_COURT = "As the direction has been made without a hearing,"
         + " you may ask the court to reconsider this order. "
         + "You must do that within 7 days of receiving this order by writing to the court"
         + "(and notifying any other party) and asking the court to reconsider."
