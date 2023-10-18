7b6897b72cb389e430df43794c823952818ff26b
bharadwajsairam <bharadwajsairam001@gmail.com>
2023-10-18 15:49:37 +0100

    datepicker complex type removed

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/ManageOrders.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/ManageOrders.java
@@ -23,7 +23,6 @@ import uk.gov.hmcts.reform.prl.enums.manageorders.ServingRespondentsEnum;
 import uk.gov.hmcts.reform.prl.enums.manageorders.UnderTakingEnum;
 import uk.gov.hmcts.reform.prl.enums.manageorders.WithDrawTypeOfOrderEnum;
 import uk.gov.hmcts.reform.prl.models.Address;
-import uk.gov.hmcts.reform.prl.models.DateTimePicker;
 import uk.gov.hmcts.reform.prl.models.Element;
 import uk.gov.hmcts.reform.prl.models.common.MappableObject;
 import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicList;
@@ -141,7 +140,8 @@ public class ManageOrders implements MappableObject {
     private final LocalDate underTakingDateExpiry;
     private final String underTakingExpiryTime;
     @JsonProperty("underTakingExpiryDateTime")
-    private final DateTimePicker underTakingExpiryDateTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    private final LocalDateTime underTakingExpiryDateTime;
     private final YesOrNo underTakingFormSign;
 
     private final YesOrNo isTheOrderByConsent;
