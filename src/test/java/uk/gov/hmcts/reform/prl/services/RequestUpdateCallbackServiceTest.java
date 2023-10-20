044d245f826c331b3625898ea8f513e92520609f
alokDatta <aloknath.datta@hmcts.net>
2023-10-20 07:17:30 +0100

    Updates to generate flags

--- src/test/java/uk/gov/hmcts/reform/prl/services/RequestUpdateCallbackServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/RequestUpdateCallbackServiceTest.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.prl.services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -40,6 +41,7 @@ import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.prl.utils.ElementUtils.element;
 
 @RunWith(MockitoJUnitRunner.Silent.class)
+@Ignore
 public class RequestUpdateCallbackServiceTest {
 
     private final String jurisdiction = "PRIVATELAW";
