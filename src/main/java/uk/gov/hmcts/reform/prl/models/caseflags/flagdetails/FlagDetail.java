76a5b1c81e936cee30dbfe1e2b438a73030b9ff6
alokDatta <aloknath.datta@hmcts.net>
2023-10-21 02:42:46 +0100

    Updated flag details pojo

--- src/main/java/uk/gov/hmcts/reform/prl/models/caseflags/flagdetails/FlagDetail.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/caseflags/flagdetails/FlagDetail.java
@@ -5,8 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import uk.gov.hmcts.reform.prl.enums.YesOrNo;
+import uk.gov.hmcts.reform.prl.models.Element;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @lombok.Data
 @Builder(toBuilder = true)
@@ -14,15 +17,23 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class FlagDetail {
-    //  public List<Element<Path>> path;
-    public String hearingRelevant;
+    public String name;
+    public String name_cy;
+    public String subTypeValue;
+    public String subTypeValue_cy;
+    public String subTypeKey;
+    public String otherDescription;
+    public String otherDescription_cy;
+    public String flagComment;
+    public String flagComment_cy;
+    public String flagUpdateComment;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     public LocalDateTime dateTimeCreated;
-    public String flagComment;
-    // public Object subTypeKey;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    public LocalDateTime dateTimeModified;
+    public List<Element<String>> path;
+    public YesOrNo hearingRelevant;
     public String flagCode;
-    public String name;
-    // public Object subTypeValue;
-    // public Object otherDescription;
     public String status;
+    public YesOrNo availableExternally;
 }
