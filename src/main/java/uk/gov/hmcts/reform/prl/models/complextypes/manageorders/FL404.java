d459f781fbb549f185fe134eb2bfd633590d041b
Khurshed Khan <khurshed.khan@hmcts.net>
2023-10-23 13:48:57 +0100

    removed formatting

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/manageorders/FL404.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/manageorders/FL404.java
@@ -36,7 +36,7 @@ public class FL404 {
     private final String fl404bPowerOfArrestParagraph;
     private final String fl404bRiskOfSignificantHarm;
     private final String fl404bDateOrderMade;
-   // @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    // @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     private String fl404bDateOrderEnd;
     private final String fl404bDateOrderEndTime;
 
