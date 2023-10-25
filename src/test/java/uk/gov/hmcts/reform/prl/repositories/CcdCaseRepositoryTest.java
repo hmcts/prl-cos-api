e22f703a79e8700200d66a962fe0b128bf2ffe7e
JamesCutts00 <James.Cutts@HMCTS.ORG>
2023-10-25 13:50:49 +0100

    coverage(feature/tech_debt_Increase_code_coverage_Part_3): Fixed Assert Statement Ib CcdCaseRepositoryTest

--- src/test/java/uk/gov/hmcts/reform/prl/repositories/CcdCaseRepositoryTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/repositories/CcdCaseRepositoryTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.prl.repositories;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,12 +11,14 @@ import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.EventRequestData;
 import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+import uk.gov.hmcts.reform.prl.services.citizen.CaseService;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.prl.enums.CaseEvent.LINK_CITIZEN;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CcdCaseRepositoryTest {
@@ -23,8 +26,14 @@ public class CcdCaseRepositoryTest {
     @InjectMocks
     CcdCaseRepository ccdCaseRepository;
 
+    @InjectMocks
+    CaseService caseService;
+
     @Mock
     CcdCaseApi ccdCaseApi;
+
+    @Mock
+    CaseRepository caseRepository;
 
     public static final String authToken = "Bearer TestAuthToken";
 
@@ -44,12 +53,16 @@ public class CcdCaseRepositoryTest {
     }
 
     @Test
-    public void testLinkDefendant() {
+    public void testLinkDefendant() throws JsonProcessingException {
+        Map<String, Object> map = new HashMap<>();
         EventRequestData eventRequestData = EventRequestData.builder().build();
         StartEventResponse startEventResponse = StartEventResponse.builder().build();
-        Map<String, Object> map = new HashMap<>();
+
+        when(caseRepository.updateCase(authToken, caseId, caseData, LINK_CITIZEN)).thenReturn(emptyCaseDetails);
 
         ccdCaseRepository.linkDefendant(authToken, s2sToken, caseId, eventRequestData, startEventResponse, map);
+        CaseDetails emptyCaseDetails1 = caseService.updateCase(caseData, authToken, s2sToken, caseId, LINK_CITIZEN.getValue(), "test");
+        assertEquals(emptyCaseDetails, emptyCaseDetails1);
     }
 
     @Test
