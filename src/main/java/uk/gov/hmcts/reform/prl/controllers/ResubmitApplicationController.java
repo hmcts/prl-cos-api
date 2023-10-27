01b47cd662a820a5c8df9d61b6e9febf83a65744
JamesCutts00 <James.Cutts@HMCTS.ORG>
2023-10-27 16:39:27 +0100

    code smells(feature/Tech_debt_S1488_S5826_S123_S1481_S1172_&_S1118): fixed build issues

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ResubmitApplicationController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ResubmitApplicationController.java
@@ -19,7 +19,6 @@ import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.CaseEventDetail;
-import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 import uk.gov.hmcts.reform.prl.constants.PrlAppsConstants;
 import uk.gov.hmcts.reform.prl.enums.State;
 import uk.gov.hmcts.reform.prl.enums.caseworkeremailnotification.CaseWorkerEmailNotificationEventEnum;
