2eeae6f56ada5150998478d003df08a7534123a7
bharadwajsairam <bharadwajsairam001@gmail.com>
2023-11-01 18:38:35 +0000

    logger and pattern updated to populate undertakingExpiryDateTime

--- src/main/java/uk/gov/hmcts/reform/prl/models/DraftOrder.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/DraftOrder.java
@@ -120,7 +120,7 @@ public class DraftOrder {
     private final LocalDate underTakingDateExpiry;
     private final String underTakingExpiryTime;
     @JsonProperty("underTakingExpiryDateTime")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     private final LocalDateTime underTakingExpiryDateTime;
     private final YesOrNo underTakingFormSign;
 
