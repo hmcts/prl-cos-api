3af90a3a51a8bf22e6da1e3d9bbb938f01a371c3
sairamkantheti1 <sai.kantheti@HMCTS.NET>
2023-11-30 14:12:50 +0000

    added logs

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/CallbackController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/CallbackController.java
@@ -579,6 +579,7 @@ public class CallbackController {
     ) {
         if (authorisationService.isAuthorized(authorisation, s2sToken)) {
             Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
+            log.info("case data updated before submit {}",caseDataUpdated);
             //Added for Case linking
             if (caseDataUpdated.get(APPLICANT_CASE_NAME) != null) {
                 caseDataUpdated.put("caseNameHmctsInternal", caseDataUpdated.get(APPLICANT_CASE_NAME));
@@ -725,6 +726,7 @@ public class CallbackController {
             UserDetails userDetails = userService.getUserDetails(authorisation);
             Optional<Organisations> userOrganisation = organisationService.findUserOrganisation(authorisation);
             caseDataUpdated.put("caseSolicitorName", userDetails.getFullName());
+            log.info("organisation details {}",userOrganisation);
             if (userOrganisation.isPresent()) {
                 log.info("Got the Org Details");
                 caseDataUpdated.put("caseSolicitorOrgName", userOrganisation.get().getName());
@@ -737,6 +739,7 @@ public class CallbackController {
                         .orgPolicyReference(caseData.getApplicantOrganisationPolicy().getOrgPolicyReference())
                         .orgPolicyCaseAssignedRole(caseData.getApplicantOrganisationPolicy().getOrgPolicyCaseAssignedRole())
                         .build();
+                    log.info("organisation policy {}",applicantOrganisationPolicy);
                     caseDataUpdated.put("applicantOrganisationPolicy", applicantOrganisationPolicy);
                 }
             }
--- src/main/java/uk/gov/hmcts/reform/prl/controllers/CaseInitiationController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/CaseInitiationController.java
@@ -52,6 +52,7 @@ public class CaseInitiationController extends AbstractCallbackController {
             final CaseDetails caseDetails = callbackRequest.getCaseDetails();
             final CaseData caseData = getCaseData(caseDetails).toBuilder().build();
 
+            log.info("case detail received from xui {}",caseDetails);
             assignCaseAccessService.assignCaseAccess(caseDetails.getId().toString(), authorisation);
 
             // setting supplementary data updates to enable global search
