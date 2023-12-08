4036c91ea2d5b9fc6dc80d6a06883f56c63e9341
sairamkantheti1 <sai.kantheti@HMCTS.NET>
2023-12-08 18:32:28 +0000

    Update RefDataUserService.java

--- src/main/java/uk/gov/hmcts/reform/prl/services/RefDataUserService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/RefDataUserService.java
@@ -4,7 +4,6 @@ package uk.gov.hmcts.reform.prl.services;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
