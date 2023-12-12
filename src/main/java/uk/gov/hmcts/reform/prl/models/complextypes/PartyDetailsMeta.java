50a6aefba7537b4ad3e66f59a1e7dfd1ee173383
alokDatta <aloknath.datta@hmcts.net>
2023-12-12 14:31:36 +0000

    Review comments incorporated

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/PartyDetailsMeta.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/PartyDetailsMeta.java
@@ -2,14 +2,10 @@ package uk.gov.hmcts.reform.prl.models.complextypes;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.extern.jackson.Jacksonized;
 import uk.gov.hmcts.reform.prl.enums.PartyEnum;
 
 @Data
-@Getter
-@Builder(toBuilder = true)
-@Jacksonized
+@Builder
 public class PartyDetailsMeta {
     private final PartyEnum partyType;
     private final Integer partyIndex;
