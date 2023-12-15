356c5a5f0a6241a6a62481d3336cfc0727689829
JamesCutts00 <James.Cutts@HMCTS.ORG>
2023-12-15 12:49:01 +0000

    feature(prl-4930/integration-with-am): Added request object

--- src/main/java/uk/gov/hmcts/reform/prl/models/roleassignment/request/RoleAssignmentRequest.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/roleassignment/request/RoleAssignmentRequest.java
@@ -1,12 +1,12 @@
-package uk.gov.hmcts.reform.prl.models.roleassignment.response;
+package uk.gov.hmcts.reform.prl.models.roleassignment.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import uk.gov.hmcts.reform.prl.models.roleassignment.RoleRequest;
 
 @Data
-@Schema(description = "The response object for RoleAssignment")
-public class RoleAssignmentResponse {
+@Schema(description = "The request object for RoleAssignment")
+public class RoleAssignmentRequest {
 
     private RoleRequest roleRequest;
     private RequestedRoles requestedRoles;
