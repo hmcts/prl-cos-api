744f8dd06e6535dc88dd9c05563ba341de99b5d1
JamesCutts00 <James.Cutts@HMCTS.ORG>
2023-12-15 14:52:34 +0000

    feature(prl-4930/integration-with-am): Updated Request and Response objects based off of the swagger

--- src/main/java/uk/gov/hmcts/reform/prl/enums/RoleType.java
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/RoleType.java
@@ -5,9 +5,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @JsonSerialize(using = CustomEnumSerializer.class)
-public enum Status {
-
-    LIVE,
-    REJECTED,
-    CREATE_REQUESTED
+public enum RoleType {
+    ORGANISATION,
+    CASE
 }
--- src/main/java/uk/gov/hmcts/reform/prl/models/roleassignment/RoleRequest.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/roleassignment/RoleRequest.java
@@ -1,12 +1,25 @@
 package uk.gov.hmcts.reform.prl.models.roleassignment;
 
 import lombok.Data;
+import uk.gov.hmcts.reform.prl.enums.Status;
+
+import java.time.LocalDateTime;
 
 @Data
 public class RoleRequest {
 
     private String assignerId;
+    private String authenticatedUserId;
+    private Boolean byPassOrgDroolRule;
+    private String clientId;
+    private String correlationId;
+    private LocalDateTime created;
+    private String id;
+    private String log;
     private String process;
     private String reference;
     private Boolean replaceExisting;
+    private String requestType;
+    private String roleAssignmentId;
+    private Status status;
 }
