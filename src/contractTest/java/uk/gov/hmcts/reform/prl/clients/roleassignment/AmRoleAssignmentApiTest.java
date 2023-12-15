e25bf4c034861f3b0a25f0c6fcca929ae2c9b407
JamesCutts00 <James.Cutts@HMCTS.ORG>
2023-12-15 10:35:46 +0000

    feature(prl-4930/integration-with-am): added TestPropertySource to amroleassignmentapitest

--- src/contractTest/java/uk/gov/hmcts/reform/prl/clients/roleassignment/AmRoleAssignmentApiTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/prl/clients/roleassignment/AmRoleAssignmentApiTest.java
@@ -6,6 +6,7 @@ import au.com.dius.pact.core.model.annotations.PactFolder;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.prl.clients.idam.IdamApiConsumerApplication;
 
@@ -15,6 +16,23 @@ import uk.gov.hmcts.reform.prl.clients.idam.IdamApiConsumerApplication;
 @PactTestFor(providerName = "role_assignment", port = "8881")
 @ContextConfiguration(
         classes = {AmRoleAssignmentApiApplication.class, IdamApiConsumerApplication.class}
+)
+@TestPropertySource(
+    properties = {"bundle.api.url=http://localhost:8899","idam.api.url=localhost:5000","commonData.api.url=localhost:5000",
+        "fis_hearing.api.url=localhost:5000",
+        "refdata.api.url=",
+        "courtfinder.api.url=",
+        "prl-dgs-api.url=",
+        "fees-register.api.url=",
+        "fis_hearing.api.url=",
+        "judicialUsers.api.url=",
+        "locationfinder.api.url=",
+        "rd_professional.api.url=",
+        "payments.api.url=",
+        "pba.validation.service.api.baseurl=",
+        "staffDetails.api.url=",
+        "amRoleAssignment.api.url=8881"
+    }
 )
 
 @PactFolder("pacts")
