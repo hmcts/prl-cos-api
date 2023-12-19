d2c5beddb4f43a160f470214f8cb970a0ced4fb4
sealDebolina <debolina.seal@hmcts.net>
2023-12-19 13:42:05 +0000

    removing document related to case

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/managedocuments/ManageDocuments.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/managedocuments/ManageDocuments.java
@@ -5,11 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.enums.managedocuments.DocumentPartyEnum;
-import uk.gov.hmcts.reform.prl.enums.managedocuments.DocumentRelatedToCase;
+//import uk.gov.hmcts.reform.prl.enums.managedocuments.DocumentRelatedToCase;
 import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicList;
 import uk.gov.hmcts.reform.prl.models.documents.Document;
 
-import java.util.List;
+//import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -28,5 +28,5 @@ public class ManageDocuments {
     private final YesOrNo isRestricted;
     private final String restrictedDetails;
     private String documentRelatedToCaseLabel;
-    private final List<DocumentRelatedToCase> documentRelatedToCaseCheckbox;
+    //private final List<DocumentRelatedToCase> documentRelatedToCaseCheckbox;
 }
