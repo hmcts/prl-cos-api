4ec0041ddada4a45715c7884a05789cb6ff5b940
JamesCutts00 <James.Cutts@HMCTS.ORG>
2023-12-20 13:21:49 +0000

    feature(feature/tech-debt-code-smell-fix): Removed code to see if issue is being cause by it

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/FL401SubmitApplicationControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/FL401SubmitApplicationControllerTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.prl.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.function.ThrowingRunnable;
 import org.junit.jupiter.api.Assertions;
@@ -400,7 +399,6 @@ public class FL401SubmitApplicationControllerTest {
     }
 
     @Test
-    @Ignore
     public void testFl401SendApplicationNotification() throws Exception {
 
         PartyDetails fl401Applicant = PartyDetails.builder()
