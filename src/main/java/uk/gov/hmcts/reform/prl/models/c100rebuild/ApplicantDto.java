6de93d258534eca0a14994f062d1f480a8d65c00
Vivek Sekhar <vivek.sekhar@hmcts.net>
2023-12-21 11:02:56 +0000

    feat(PRL-4853): Integrate API changes for C100 application creation

--- src/main/java/uk/gov/hmcts/reform/prl/models/c100rebuild/ApplicantDto.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/c100rebuild/ApplicantDto.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.prl.models.c100rebuild;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
