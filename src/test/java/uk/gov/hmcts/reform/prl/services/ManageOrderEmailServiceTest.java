33be9a0fa0820e70d2fa73e27cc6001a8c9cf816
bharadwajsairam <bharadwajsairam001@gmail.com>
2023-12-22 14:37:59 +0000

    checkstyle fix

--- src/test/java/uk/gov/hmcts/reform/prl/services/ManageOrderEmailServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/ManageOrderEmailServiceTest.java
@@ -63,7 +63,10 @@ import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.prl.utils.ElementUtils.element;
 
 @PropertySource(value = "classpath:application.yaml")
