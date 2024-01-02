cf55493ad1631d071bf407adfcc1f22a214e8389
Khurshed Khan <khurshed.khan@hmcts.net>
2024-01-02 12:57:08 +0000

    PRL-4616:Created functional test for sending email to cafcass

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersControllerFunctionalTest.java
@@ -4,23 +4,31 @@ import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.prl.ResourceLoader;
-import uk.gov.hmcts.reform.prl.services.ManageOrderService;
+import uk.gov.hmcts.reform.prl.enums.State;
 import uk.gov.hmcts.reform.prl.utils.IdamTokenGenerator;
 import uk.gov.hmcts.reform.prl.utils.ServiceAuthenticationGenerator;
+
+import static org.hamcrest.Matchers.equalTo;
 
 @Slf4j
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @ContextConfiguration
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ManageOrdersControllerFunctionalTest {
 
     private final String userToken = "Bearer testToken";
@@ -33,13 +41,12 @@ public class ManageOrdersControllerFunctionalTest {
     @Autowired
     protected ServiceAuthenticationGenerator serviceAuthenticationGenerator;
 
-    @MockBean
-    private ManageOrderService manageOrderService;
-
     private static final String VALID_INPUT_JSON = "CallBackRequest.json";
 
     private static final String VALID_CAFCASS_REQUEST_JSON
         = "requests/cafcass-cymru-send-email-request.json";
+
+    private static final String APPLICANT_CASE_NAME_REQUEST = "requests/call-back-controller-applicant-case-name.json";
 
     private final String targetInstance =
         StringUtils.defaultIfBlank(
@@ -47,7 +54,13 @@ public class ManageOrdersControllerFunctionalTest {
             "http://localhost:4044"
         );
 
+    private static CaseDetails caseDetails;
+
     private final RequestSpecification request = RestAssured.given().relaxedHTTPSValidation().baseUri(targetInstance);
+
+    @Before
+    public void setup(){
+    }
 
     @Test
     public void givenRequestBody_whenPostRequestToPopulatePreviewOrder_then200Response() throws Exception {
@@ -105,18 +118,42 @@ public class ManageOrdersControllerFunctionalTest {
             .then().assertThat().statusCode(500);
     }
 
+
     @Test
-    public void givenRequestBody_WhenPostRequestTestSendCafcassCymruOrderEmail() throws Exception {
+    public void createCCDTestCase() throws Exception{
 
         String requestBody = ResourceLoader.loadJson(VALID_CAFCASS_REQUEST_JSON);
-
-        request
-            .header("Authorization", idamTokenGenerator.generateIdamTokenForSolicitor())
+        caseDetails =  request
+            .header("Authorization", idamTokenGenerator.generateIdamTokenForSystem())
             .header("ServiceAuthorization", serviceAuthenticationGenerator.generateTokenForCcd())
             .body(requestBody)
             .when()
             .contentType("application/json")
+            .post("/testing-support/create-ccd-case-data")
+            .then()
+            .assertThat().statusCode(200)
+            .extract()
+            .as(CaseDetails.class);
+
+        Assert.assertNotNull(caseDetails);
+        Assert.assertNotNull(caseDetails.getData().get("id"));
+    }
+
+    @Test
+    public void givenRequestBody_WhenPostRequestTestSendCafcassCymruOrderEmail() {
+        CallbackRequest callbackRequest = CallbackRequest.builder()
+            .caseDetails(caseDetails.toBuilder()
+                             .id(Long.parseLong((String)caseDetails.getData().get("id")))
+                             .state(State.JUDICIAL_REVIEW.getLabel()).build()).build();
+        request
+            .header("Authorization", idamTokenGenerator.generateIdamTokenForSystem())
+            .header("ServiceAuthorization", serviceAuthenticationGenerator.generateTokenForCcd())
+            .body(callbackRequest)
+            .when()
+            .contentType("application/json")
             .post("/case-order-email-notification")
-            .then().assertThat().statusCode(500);
+            .then()
+            .body("data.id", equalTo(caseDetails.getData().get("id")))
+            .assertThat().statusCode(200);
     }
 }
