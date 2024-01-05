8175f15b0995c21969bae97a758eda1edb147097
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2024-01-05 11:24:31 +0000

    Debug

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ReviewDocumentsController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ReviewDocumentsController.java
@@ -97,7 +97,7 @@ public class ReviewDocumentsController {
         ObjectMapper om = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         String result = om.writeValueAsString(callbackRequest.getCaseDetails().getData());
-        System.out.println("REVIEWINGGGGGGG 1111" + result);
+        System.out.println("REVIEWINGGGGGGG 11111111" + result);
 
         CaseDetails caseDetails = callbackRequest.getCaseDetails();
         CaseData caseData = CaseUtils.getCaseData(caseDetails, objectMapper);
