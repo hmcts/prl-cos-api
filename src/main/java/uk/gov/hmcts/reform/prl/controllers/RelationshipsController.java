e48abcc8891a6b3f489344d3d9423e57a65d1ea5
sairamkantheti1 <sai.kantheti@HMCTS.NET>
2024-01-09 12:09:05 +0000

    FPET-817 fixed and seprated the call back for amend children and applicants events

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/RelationshipsController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/RelationshipsController.java
@@ -54,31 +54,60 @@ public class RelationshipsController {
         Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
         List<Element<ChildrenAndApplicantRelation>> applicantChildRelationsList = new ArrayList<>();
         CaseData caseData = CaseUtils.getCaseData(callbackRequest.getCaseDetails(), objectMapper);
-        List<Element<ChildrenAndApplicantRelation>> existingApplicantChildRelations = caseData.getRelations().getChildAndApplicantRelations();
         caseData.getApplicants().forEach(eachApplicant ->
                 caseData.getNewChildDetails().forEach(eachChild -> {
-                    ChildrenAndApplicantRelation existingRelation = CollectionUtils.isNotEmpty(
-                        existingApplicantChildRelations)
-                        ? existingApplicantChildRelations.stream().filter(
-                            childrenAndApplicantRelationElement -> StringUtils.equals(childrenAndApplicantRelationElement.getValue().getApplicantId(),
-                                                                                      String.valueOf(eachApplicant.getId()))
-                                && StringUtils.equals(childrenAndApplicantRelationElement.getValue().getChildId(),String.valueOf(eachChild.getId())))
-                        .findFirst().map(Element::getValue).orElse(null) : null;
                     ChildrenAndApplicantRelation applicantChildRelations = ChildrenAndApplicantRelation.builder()
                             .childFullName(String.format(PrlAppsConstants.FORMAT, eachChild.getValue().getFirstName(),
-                                                         eachChild.getValue().getLastName()
-                            ))
+                                    eachChild.getValue().getLastName()))
                             .childId(eachChild.getId().toString())
                             .applicantId(eachApplicant.getId().toString())
-                            .childAndApplicantRelation(Objects.nonNull(existingRelation) ? existingRelation.getChildAndApplicantRelation() : null)
-                            .childLivesWith(Objects.nonNull(existingRelation) ? existingRelation.getChildLivesWith() : null)
-                            .applicantFullName(String.format(PrlAppsConstants.FORMAT,
-                                                             eachApplicant.getValue().getFirstName(),
-                                                             eachApplicant.getValue().getLastName()
-                            )).build();
+                            .applicantFullName(String.format(PrlAppsConstants.FORMAT, eachApplicant.getValue().getFirstName(),
+                                    eachApplicant.getValue().getLastName())).build();
                     applicantChildRelationsList.add(Element.<ChildrenAndApplicantRelation>builder().value(applicantChildRelations).build());
-
                 })
+        );
+        caseDataUpdated.put("buffChildAndApplicantRelations", applicantChildRelationsList);
+        return AboutToStartOrSubmitCallbackResponse.builder().data(caseDataUpdated).build();
+    }
+
+    @PostMapping(path = "/pre-populate-amend-applicant-to-child-relation", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
+    @Operation(description = "pre populates applicant and child relations")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Callback processed.", content = @Content(mediaType = "application/json",
+            schema = @Schema(implementation = AboutToStartOrSubmitCallbackResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)})
+    @SecurityRequirement(name = "Bearer Authentication")
+    public AboutToStartOrSubmitCallbackResponse prePopulateAmendApplicantToChildRelation(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
+        @RequestBody uk.gov.hmcts.reform.ccd.client.model.CallbackRequest callbackRequest
+    ) {
+        Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
+        List<Element<ChildrenAndApplicantRelation>> applicantChildRelationsList = new ArrayList<>();
+        CaseData caseData = CaseUtils.getCaseData(callbackRequest.getCaseDetails(), objectMapper);
+        List<Element<ChildrenAndApplicantRelation>> existingApplicantChildRelations = caseData.getRelations().getChildAndApplicantRelations();
+        caseData.getApplicants().forEach(eachApplicant ->
+                 caseData.getNewChildDetails().forEach(eachChild -> {
+                     ChildrenAndApplicantRelation existingRelation = CollectionUtils.isNotEmpty(existingApplicantChildRelations)
+                         ? existingApplicantChildRelations.stream().filter(childrenAndApplicantRelationElement ->
+                             StringUtils.equals(childrenAndApplicantRelationElement.getValue().getApplicantId(),
+                                                             String.valueOf(eachApplicant.getId())
+                                                         ) && StringUtils.equals(childrenAndApplicantRelationElement.getValue().getChildId(),
+                                                                                   String.valueOf(eachChild.getId())))
+                                                     .findFirst().map(Element::getValue).orElse(null) : null;
+                     ChildrenAndApplicantRelation applicantChildRelations = ChildrenAndApplicantRelation.builder()
+                         .childFullName(String.format(PrlAppsConstants.FORMAT, eachChild.getValue().getFirstName(),
+                                                      eachChild.getValue().getLastName()))
+                         .childId(eachChild.getId().toString())
+                         .applicantId(eachApplicant.getId().toString())
+                         .childAndApplicantRelation(Objects.nonNull(existingRelation) ? existingRelation.getChildAndApplicantRelation() : null)
+                         .childLivesWith(Objects.nonNull(existingRelation) ? existingRelation.getChildLivesWith() : null)
+                         .applicantFullName(String.format(PrlAppsConstants.FORMAT,
+                                                         eachApplicant.getValue().getFirstName(),
+                                                         eachApplicant.getValue().getLastName()
+                                                     )).build();
+                     applicantChildRelationsList.add(Element.<ChildrenAndApplicantRelation>builder().value(
+                         applicantChildRelations).build());
+                 })
         );
         caseDataUpdated.put("buffChildAndApplicantRelations", applicantChildRelationsList);
         return AboutToStartOrSubmitCallbackResponse.builder().data(caseDataUpdated).build();
