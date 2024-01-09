e982b643e6fe37cd6f69b338a95d1d72b87a1f5e
hcarslaw <Hugh.Carslaw@HMCTS.NET>
2024-01-09 14:46:22 +0000

    FPET-826: Fixes for dont know values and other person revised

--- src/test/java/uk/gov/hmcts/reform/prl/services/ApplicationsTabServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/ApplicationsTabServiceTest.java
@@ -635,8 +635,8 @@ public class ApplicationsTabServiceTest {
             .gender("male") //the new POJOs use strings as the enums are causing errors
             .address(address)
             .canYouProvideEmailAddress(YesOrNo.Yes)
-            .isAtAddressLessThan5YearsWithDontKnow("Dont know")
-            .doTheyHaveLegalRepresentation("Dont know")
+            .isAtAddressLessThan5YearsWithDontKnow("Don't know")
+            .doTheyHaveLegalRepresentation("Don't know")
             .email("test@test.com")
             .build();
 
