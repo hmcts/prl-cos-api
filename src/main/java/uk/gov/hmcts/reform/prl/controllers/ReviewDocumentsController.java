e71efc1c7c5a60e4af20f78d946966f624c771f3
Yogendra Upasani <yogendra.upasani@hmcts.net>
2024-01-09 01:10:43 +0000

    refactor mg doc and rw doc

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ReviewDocumentsController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ReviewDocumentsController.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.prl.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.swagger.v3.oas.annotations.Operation;
@@ -52,6 +53,11 @@ public class ReviewDocumentsController {
     public AboutToStartOrSubmitCallbackResponse handleAboutToStart(
         @RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
         @RequestBody CallbackRequest callbackRequest) {
+        try {
+            log.info("/review documents/about-to-start::CallbackRequest -> {}", objectMapper.writeValueAsString(callbackRequest));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
 
         CaseDetails caseDetails = callbackRequest.getCaseDetails();
         CaseData caseData = CaseUtils.getCaseData(caseDetails, objectMapper);
@@ -77,6 +83,11 @@ public class ReviewDocumentsController {
     public AboutToStartOrSubmitCallbackResponse handleMidEvent(
         @RequestHeader(org.springframework.http.HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
         @RequestBody CallbackRequest callbackRequest) {
+        try {
+            log.info("/review docs/mid event::CallbackRequest -> {}", objectMapper.writeValueAsString(callbackRequest));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         CaseData caseData = CaseUtils.getCaseData(callbackRequest.getCaseDetails(), objectMapper);
         Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
         reviewDocumentService.getReviewedDocumentDetailsNew(caseData, caseDataUpdated);
@@ -94,7 +105,6 @@ public class ReviewDocumentsController {
         @RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
         @RequestBody CallbackRequest callbackRequest
     ) throws Exception {
-
         ObjectMapper om = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         String result = om.writeValueAsString(callbackRequest.getCaseDetails().getData());
