349b17be39be17087456c5c2913d8fad7891eb8d
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2024-01-15 13:00:52 +0000

    Revised

--- src/main/java/uk/gov/hmcts/reform/prl/utils/ManageOrdersUtils.java
+++ src/main/java/uk/gov/hmcts/reform/prl/utils/ManageOrdersUtils.java
@@ -56,7 +56,8 @@ public class ManageOrdersUtils {
     private static final String[] VALID_ORDER_IDS_FOR_FL401 = {"nonMolestation", "occupation", "amendDischargedVaried",
         "blank", "powerOfArrest", "generalForm", "noticeOfProceedings"};
 
-    public static final String[] PROHIBITED_C100_ORDER_IDS_FOR_SOLICITORS = {"noticeOfProceedingsParties", "noticeOfProceedingsNonParties"};
+    public static final String[] PROHIBITED_ORDER_IDS_FOR_SOLICITORS = {"noticeOfProceedings","noticeOfProceedingsParties",
+        "noticeOfProceedingsNonParties", "standardDirectionsOrder","directionOnIssue"};
 
     public static List<String> getHearingScreenValidations(List<Element<HearingData>> ordersHearingDetails,
                                                            CreateSelectOrderOptionsEnum selectedOrderType,
