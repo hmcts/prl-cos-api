271c92716ec7f881c75389a1f9e1ca0286259283
Khurshed Khan <khurshed.khan@hmcts.net>
2024-01-16 08:10:38 +0000

    added check to generate C8 in gatekeeping state

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/TaskListController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/TaskListController.java
@@ -72,7 +72,7 @@ public class TaskListController extends AbstractCallbackController {
         boolean isCourtStaff = roles.stream().anyMatch(ROLES::contains);
         String state = callbackRequest.getCaseDetails().getState();
         if (isCourtStaff && (SUBMITTED_STATE.equalsIgnoreCase(state) || ISSUED_STATE.equalsIgnoreCase(state)
-        || JUDICIAL_REVIEW_STATE.equalsIgnoreCase(state))) {
+            || JUDICIAL_REVIEW_STATE.equalsIgnoreCase(state))) {
             try {
                 caseDataUpdated.putAll(dgsService.generateDocuments(authorisation, caseData));
                 CaseData updatedCaseData = objectMapper.convertValue(caseDataUpdated, CaseData.class);
