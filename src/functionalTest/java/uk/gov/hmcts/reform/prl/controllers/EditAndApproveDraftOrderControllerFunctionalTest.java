c5cc0cf2e822b2380eea48c2a09b9d36406b28e1
Khurshed Khan <khurshed.khan@hmcts.net>
2024-01-18 21:15:05 +0000

    Added assertions for functional tests

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/EditAndApproveDraftOrderControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/EditAndApproveDraftOrderControllerFunctionalTest.java
@@ -1,9 +1,12 @@
 package uk.gov.hmcts.reform.prl.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -17,7 +20,12 @@ import uk.gov.hmcts.reform.prl.services.DraftAnOrderService;
 import uk.gov.hmcts.reform.prl.utils.IdamTokenGenerator;
 import uk.gov.hmcts.reform.prl.utils.ServiceAuthenticationGenerator;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
@@ -53,6 +61,13 @@ public class EditAndApproveDraftOrderControllerFunctionalTest {
 
     @Test
     public void givenRequestBody_whenPopulate_draft_order_dropdown_then200Response() throws Exception {
+        Map<String, Object> drafOrderMap = new HashMap<>();
+        drafOrderMap.put("draftOrder1", "SDO");
+        drafOrderMap.put("draftOrder2", "C21");
+        Mockito
+            .when(draftAnOrderService
+                      .getDraftOrderDynamicList(ArgumentMatchers.any(), ArgumentMatchers.any(), ArgumentMatchers.any()))
+                .thenReturn(drafOrderMap);
         String requestBody = ResourceLoader.loadJson(VALID_DRAFT_ORDER_REQUEST_BODY);
         mockMvc.perform(post("/populate-draft-order-dropdown")
                             .contentType(MediaType.APPLICATION_JSON)
@@ -61,11 +76,18 @@ public class EditAndApproveDraftOrderControllerFunctionalTest {
                             .content(requestBody)
                             .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
+            .andExpect(jsonPath("data.draftOrder1").value("SDO"))
+            .andExpect(jsonPath("data.draftOrder2").value("C21"))
             .andReturn();
     }
 
     @Test
     public void givenRequestBody_whenJudge_admin_populate_draft_order_then200Response() throws Exception {
+        Map<String, Object> caseDataMap = new HashMap<>();
+        caseDataMap.put("orderName", "C21");
+        caseDataMap.put("orderUploadedAsDraftFlag", "Yes");
+        Mockito.when(draftAnOrderService.populateDraftOrderDocument(ArgumentMatchers.any()))
+                .thenReturn(caseDataMap);
         String requestBody = ResourceLoader.loadJson(VALID_DRAFT_ORDER_REQUEST_BODY);
         mockMvc.perform(post("/judge-or-admin-populate-draft-order")
                             .contentType(MediaType.APPLICATION_JSON)
@@ -74,6 +96,8 @@ public class EditAndApproveDraftOrderControllerFunctionalTest {
                             .content(requestBody)
                             .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
+            .andExpect(jsonPath("data.orderName").value("C21"))
+            .andExpect(jsonPath("data.orderUploadedAsDraftFlag").value("Yes"))
             .andReturn();
     }
 
@@ -87,11 +111,18 @@ public class EditAndApproveDraftOrderControllerFunctionalTest {
                             .content(requestBody)
                             .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
+            .andExpect(jsonPath("data.applicantCaseName").value("John Smith"))
+            .andExpect(jsonPath("data.caseTypeOfApplication").value("FL401"))
             .andReturn();
     }
 
     @Test
     public void givenRequestBody_whenJudge_or_admin_populate_draft_order_custom_fields_then200Response() throws Exception {
+        Map<String, Object> caseDataMap = new HashMap<>();
+        caseDataMap.put("appointedGuardianName", "John");
+        caseDataMap.put("parentName", "Smith");
+        Mockito.when(draftAnOrderService.populateDraftOrderCustomFields(ArgumentMatchers.any()))
+            .thenReturn(caseDataMap);
         String requestBody = ResourceLoader.loadJson(VALID_DRAFT_ORDER_REQUEST_BODY);
         mockMvc.perform(post("/judge-or-admin-populate-draft-order-custom-fields")
                             .contentType(MediaType.APPLICATION_JSON)
@@ -100,11 +131,20 @@ public class EditAndApproveDraftOrderControllerFunctionalTest {
                             .content(requestBody)
                             .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
+            .andExpect(jsonPath("data.appointedGuardianName").value("John"))
+            .andExpect(jsonPath("data.parentName").value("Smith"))
             .andReturn();
     }
 
     @Test
     public void givenRequestBody_whenJudge_or_admin_populate_draft_order_common_fields_then200Response() throws Exception {
+
+        Map<String, Object> caseDataMap = new HashMap<>();
+        caseDataMap.put("orderType", "C21");
+        caseDataMap.put("isTheOrderByConsent", "Yes");
+        Mockito.when(draftAnOrderService
+                         .populateCommonDraftOrderFields(ArgumentMatchers.any(),ArgumentMatchers.any()))
+            .thenReturn(caseDataMap);
         String requestBody = ResourceLoader.loadJson(VALID_DRAFT_ORDER_REQUEST_BODY);
         mockMvc.perform(post("/judge-or-admin-populate-draft-order-common-fields")
                             .contentType(MediaType.APPLICATION_JSON)
@@ -113,6 +153,8 @@ public class EditAndApproveDraftOrderControllerFunctionalTest {
                             .content(requestBody)
                             .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
+            .andExpect(jsonPath("data.orderType").value("C21"))
+            .andExpect(jsonPath("data.isTheOrderByConsent").value("Yes"))
             .andReturn();
     }
 
@@ -126,6 +168,9 @@ public class EditAndApproveDraftOrderControllerFunctionalTest {
                             .content(requestBody)
                             .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
+            .andExpect(content()
+                           .string(Matchers
+                                       .containsString(EditAndApproveDraftOrderController.CONFIRMATION_HEADER)))
             .andReturn();
     }
 }
