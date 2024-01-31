932675e0ce1b564bfadcceeaf8c1b2ee3a9f6205
bharadwajsairam <bharadwajsairam001@gmail.com>
2024-01-31 11:21:56 +0000

    Respondent pack cleanup added for both ca and da cases from statement of service event

--- src/main/java/uk/gov/hmcts/reform/prl/services/ServiceOfApplicationService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ServiceOfApplicationService.java
@@ -136,9 +136,6 @@ public class ServiceOfApplicationService {
     public static final String EMAIL = "email";
     public static final String POST = "post";
     public static final String COURT = "Court";
-    public static final String BY_EMAIL = "By email";
-    public static final String BY_EMAIL_AND_POST = "By email and post";
-    public static final String BY_POST = "By post";
     public static final String DA_APPLICANT_NAME = "daApplicantName";
     public static final String PRL_COURT_ADMIN = "PRL Court admin";
     public static final String DASH_BOARD_LINK = "dashBoardLink";
@@ -326,7 +323,7 @@ public class ServiceOfApplicationService {
         return ServedApplicationDetails.builder().emailNotificationDetails(emailNotificationDetails)
             .servedBy(userService.getUserDetails(authorization).getFullName())
             .servedAt(formatter)
-            .modeOfService(getModeOfService(emailNotificationDetails, bulkPrintDetails))
+            .modeOfService(CaseUtils.getModeOfService(emailNotificationDetails, bulkPrintDetails))
             .whoIsResponsible(whoIsResponsibleForServing)
             .bulkPrintDetails(bulkPrintDetails).build();
     }
@@ -845,22 +842,6 @@ public class ServiceOfApplicationService {
         return bulkPrintDetails;
     }
 
-    private String getModeOfService(List<Element<EmailNotificationDetails>> emailNotificationDetails,
-                                    List<Element<BulkPrintDetails>> bulkPrintDetails) {
-        String temp = null;
-        if (null != emailNotificationDetails && !emailNotificationDetails.isEmpty()) {
-            temp = BY_EMAIL;
-        }
-        if (null != bulkPrintDetails && !bulkPrintDetails.isEmpty()) {
-            if (null != temp) {
-                temp = BY_EMAIL_AND_POST;
-            } else {
-                temp = BY_POST;
-            }
-        }
-        return temp;
-    }
-
     private List<Element<EmailNotificationDetails>> sendNotificationToFl401Solicitor(CaseData caseData, String authorization, List<Document> packA,
                                                                                      List<Document> packB) {
         List<Element<EmailNotificationDetails>> emailNotificationDetails = new ArrayList<>();
@@ -1136,8 +1117,7 @@ public class ServiceOfApplicationService {
                         .filter(d -> !d.getDocumentFileName().equalsIgnoreCase(
                             C1A_BLANK_DOCUMENT_FILENAME))
                         .filter(d -> !d.getDocumentFileName().equalsIgnoreCase(
-                            C7_BLANK_DOCUMENT_FILENAME))
-                        .collect(Collectors.toList()));
+                            C7_BLANK_DOCUMENT_FILENAME)).toList());
         return docs;
     }
 
@@ -1148,8 +1128,7 @@ public class ServiceOfApplicationService {
         docs.addAll(getSoaSelectedOrders(caseData));
         // Annex Y to be excluded
         docs.addAll(staticDocs.stream()
-            .filter(d -> !d.getDocumentFileName().equalsIgnoreCase(SOA_C9_PERSONAL_SERVICE_FILENAME))
-            .collect(Collectors.toList()));
+                        .filter(d -> !d.getDocumentFileName().equalsIgnoreCase(SOA_C9_PERSONAL_SERVICE_FILENAME)).toList());
         return docs;
     }
 
@@ -1955,7 +1934,7 @@ public class ServiceOfApplicationService {
         return ServedApplicationDetails.builder().emailNotificationDetails(emailNotificationDetails)
             .servedBy(userService.getUserDetails(authorization).getFullName())
             .servedAt(formatter)
-            .modeOfService(getModeOfService(emailNotificationDetails, bulkPrintDetails))
+            .modeOfService(CaseUtils.getModeOfService(emailNotificationDetails, bulkPrintDetails))
             .whoIsResponsible(COURT)
             .bulkPrintDetails(bulkPrintDetails).build();
     }
@@ -1976,38 +1955,6 @@ public class ServiceOfApplicationService {
             } catch (IOException e) {
                 log.error("Failed to serve application via email notification to La {}", e.getMessage());
             }
-        }
-    }
-
-    // Use this method once respondent packs personally served to respondents by court admin or bailiff
-    private void checkAndServeRespondentPacksCaOrBailiffPersonalService(CaseData caseData,
-                                                                        List<Element<EmailNotificationDetails>> emailNotificationDetails,
-                                                                        List<Element<BulkPrintDetails>> bulkPrintDetails,
-                                                                        SoaPack unServedRespondentPack) {
-        if (SoaSolicitorServingRespondentsEnum.courtAdmin.toString().equalsIgnoreCase(unServedRespondentPack.getPersonalServiceBy())) {
-            emailNotificationDetails.add(element(EmailNotificationDetails.builder()
-                                                     .emailAddress(caseData.getApplicants().get(0).getValue().getSolicitorEmail())
-                                                     .servedParty(PRL_COURT_ADMIN)
-                                                     .docs(unServedRespondentPack.getPackDocument())
-                                                     .attachedDocs(String.join(",", unServedRespondentPack
-                                                         .getPackDocument().stream()
-                                                         .map(Element::getValue)
-                                                         .map(Document::getDocumentFileName).toList()))
-                                                     .timeStamp(DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss")
-                                                                    .format(ZonedDateTime.now(ZoneId.of("Europe/London"))))
-                                                     .build()));
-        } else if (SoaSolicitorServingRespondentsEnum.courtBailiff.toString()
-            .equalsIgnoreCase(unServedRespondentPack.getPersonalServiceBy())) {
-            bulkPrintDetails.add(element(BulkPrintDetails.builder()
-                                             .servedParty(PRL_COURT_ADMIN)
-                                             .printedDocs(String.join(",", unServedRespondentPack
-                                                 .getPackDocument().stream()
-                                                 .map(Element::getValue)
-                                                 .map(Document::getDocumentFileName).toList()))
-                                             .printDocs(unServedRespondentPack.getPackDocument())
-                                             .timeStamp(DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss")
-                                                            .format(ZonedDateTime.now(ZoneId.of("Europe/London"))))
-                                             .build()));
         }
     }
 
