c4dd369aa2bb640d9dfc26c7b31780fe45a7c643
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2024-02-10 10:01:50 +0000

    added checker logic for start and finished event status

---
+++ src/test/java/uk/gov/hmcts/reform/prl/services/c100respondentsolicitor/validators/ResponseToAllegationsOfHarmCheckerTest.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.reform.prl.services.c100respondentsolicitor.validators;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(MockitoJUnitRunner.Silent.class)
+@Slf4j
+class ResponseToAllegationsOfHarmCheckerTest {
+
+    @AfterEach
+    void tearDown() {
+    }
+}
