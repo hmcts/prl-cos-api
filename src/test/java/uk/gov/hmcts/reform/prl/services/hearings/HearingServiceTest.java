074c1542060fb70fc10f515fe89a099435f6f346
sahaSonali <sonali.saha@hmcts.net>
2024-02-14 13:49:27 +0000

    add logs

--- src/test/java/uk/gov/hmcts/reform/prl/services/hearings/HearingServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/hearings/HearingServiceTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.prl.services.hearings;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
