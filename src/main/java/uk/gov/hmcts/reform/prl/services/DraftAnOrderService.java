a433fe29a8a911c676aeea122d5b0eb75d001908
sahaSonali <sonali.saha@hmcts.net>
2024-02-14 16:06:08 +0000

    Fix for final order genaration for SDO

--- src/main/java/uk/gov/hmcts/reform/prl/services/DraftAnOrderService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/DraftAnOrderService.java
@@ -349,7 +349,7 @@ public class DraftAnOrderService {
                                                                                .getHearingDataForSelectedHearing(caseData,
                                                                                                                  hearings,
                                                                                                                  authorisation));
-                    } else if (CreateSelectOrderOptionsEnum.standardDirectionsOrder.equals(caseData.getCreateSelectOrderOptions())) {
+                    } else if (CreateSelectOrderOptionsEnum.standardDirectionsOrder.equals(draftOrder.getOrderType())) {
                         caseData = manageOrderService.setHearingDataForSdo(caseData, hearings, authorisation);
                     }
                     draftOrder = getUpdatedDraftOrder(draftOrder, caseData, loggedInUserType, eventId);
