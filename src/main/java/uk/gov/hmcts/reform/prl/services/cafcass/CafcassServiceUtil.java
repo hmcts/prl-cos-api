ef79137d49bcb6c9f6223c1f5b65ba6b6ad8c6fd
hmcts <prudhvi.maddineni@hmcts.net>
2024-02-15 15:28:21 +0000

    PRL Cafcass upload Qurantine

--- src/main/java/uk/gov/hmcts/reform/prl/services/cafcass/CafcassServiceUtil.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/cafcass/CafcassServiceUtil.java
@@ -3,18 +3,14 @@ package uk.gov.hmcts.reform.prl.services.cafcass;
 import uk.gov.hmcts.reform.ccd.document.am.model.Document;
 import uk.gov.hmcts.reform.prl.models.Element;
 import uk.gov.hmcts.reform.prl.models.complextypes.QuarantineLegalDoc;
-import uk.gov.hmcts.reform.prl.models.complextypes.citizen.documents.DocumentDetails;
-import uk.gov.hmcts.reform.prl.models.complextypes.citizen.documents.UploadedDocuments;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.DocumentManagementDetails;
 import uk.gov.hmcts.reform.prl.utils.DocumentsHelper;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.CAFCASS_PARTY;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.PARTY_NAME;
 import static uk.gov.hmcts.reform.prl.utils.ElementUtils.element;
 
@@ -34,8 +30,7 @@ public class CafcassServiceUtil {
 
     public static CaseData getCaseDataWithUploadedDocs(String caseId, String fileName, String typeOfDocument,
                                                  CaseData caseData, Document document) {
-        String partyName = caseData.getApplicantCaseName() != null
-            ? caseData.getApplicantCaseName() : CAFCASS_PARTY;
+
         List<Element<QuarantineLegalDoc>> uploadedDocumentsList;
 
 
@@ -52,27 +47,15 @@ public class CafcassServiceUtil {
                                       .documentHash(document.hashToken)
                                       .documentFileName(fileName).build()).build());
 
-        Element<UploadedDocuments> uploadedDocsElement =
-            element(UploadedDocuments.builder().dateCreated(LocalDate.now())
-                        .documentType(typeOfDocument)
-                        .uploadedBy(CAFCASS_PARTY)
-                        .documentDetails(DocumentDetails.builder().documentName(fileName)
-                                             .documentUploadedDate(new Date().toString()).build())
-                        .partyName(partyName).isApplicant(CAFCASS_PARTY)
-                        .parentDocumentType("Safe_guarding_Letter")
-                        .cafcassDocument(uk.gov.hmcts.reform.prl.models.documents.Document.builder()
-                                             .documentUrl(document.links.self.href)
-                                             .documentBinaryUrl(document.links.binary.href)
-                                             .documentHash(document.hashToken)
-                                             .documentFileName(fileName).build()).build());
         if (caseData.getCafcassUploadedDocs() != null) {
-            uploadedDocumentsList = caseData.getCafcassQuarantineDocsList();
+            uploadedDocumentsList = caseData.getDocumentManagementDetails().getCafcassQuarantineDocsList();
             uploadedDocumentsList.add(quarantineLegalDoc);
         } else {
             uploadedDocumentsList = new ArrayList<>();
             uploadedDocumentsList.add(quarantineLegalDoc);
         }
-        return CaseData.builder().id(Long.parseLong(caseId)).cafcassQuarantineDocsList(uploadedDocumentsList).build();
+        return CaseData.builder().id(Long.parseLong(caseId)).documentManagementDetails(DocumentManagementDetails.builder().cafcassQuarantineDocsList(
+            uploadedDocumentsList).build()).build();
     }
 
     private CafcassServiceUtil() {
