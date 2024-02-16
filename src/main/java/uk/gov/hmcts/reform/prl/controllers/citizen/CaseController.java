32f69fc31129eeebae6ec665af645b68be588684
anshikanigam1 <158184017+anshikanigam1@users.noreply.github.com>
2024-02-16 13:36:20 +0000

    Update CaseController.java

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseController.java
@@ -220,8 +220,8 @@ public class CaseController {
     ) {
         CaseDetails caseDetails = null;
         if (isAuthorized(authorisation, s2sToken)) {
-            caseDetails = caseService.withdrawCase(caseData, caseId, authorisation);
-            caseDetails.getData().put("caseStatus", CaseStatus.builder().state("Withdrawn").build());
+            caseData.put("state", WITHDRAWN_STATE);
+            caseData = caseData.toBuilder().state(State.CASE_WITHDRAWN).build();
             return CaseUtils.getCaseData(caseDetails, objectMapper);
         } else {
             throw (new RuntimeException(INVALID_CLIENT));
