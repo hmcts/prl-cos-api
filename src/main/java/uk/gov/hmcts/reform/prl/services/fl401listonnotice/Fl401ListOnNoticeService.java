24dcb448fa39f876b5a2ce1858dc42eacce0be68
sahaSonali <sonali.saha@hmcts.net>
2024-02-20 11:31:28 +0000

    Changes for DA List on notice

--- src/main/java/uk/gov/hmcts/reform/prl/services/fl401listonnotice/Fl401ListOnNoticeService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/fl401listonnotice/Fl401ListOnNoticeService.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.reform.prl.enums.CaseNoteDetails;
 import uk.gov.hmcts.reform.prl.enums.LanguagePreference;
 import uk.gov.hmcts.reform.prl.models.complextypes.PartyDetails;
@@ -37,6 +39,7 @@ import java.util.Map;
 import java.util.UUID;
 
 import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
+import static org.springframework.http.ResponseEntity.ok;
 import static uk.gov.hmcts.reform.prl.config.templates.Templates.PRL_LET_ENG_LIST_WITHOUT_NOTICE;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.CASE_NOTES;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.CASE_TYPE;
@@ -91,6 +94,19 @@ public class Fl401ListOnNoticeService {
     @Autowired
     private BulkPrintService bulkPrintService;
 
+    public static final String CONFIRMATION_BODY_WITHOUT_NOTICE = """
+        ### What happens next
+        Admin will be notified to list the hearing on notice.\n
+        The reasons you have given to list the hearing on notice will be shared with the applicant.\n
+        Your request details will be saved in case notes.""";
+
+    public static final String CONFIRMATION_BODY_WITH_NOTICE = """
+        ### What happens next
+        Admin will be notified to list the hearing on notice.\n
+        Your request details will be saved in case notes.""";
+
+    public static final String CONFIRMATION_HEADER = "# Listing instructions sent to admin";
+
     public Map<String, Object> prePopulateHearingPageDataForFl401ListOnNotice(CaseData caseData) {
 
         Map<String, Object> caseDataUpdated = new HashMap<>();
@@ -125,7 +141,7 @@ public class Fl401ListOnNoticeService {
         return caseDataUpdated;
     }
 
-    public void sendNotification(Map<String, Object> caseDataUpdated, String authorisation) {
+    public ResponseEntity<SubmittedCallbackResponse> sendNotification(Map<String, Object> caseDataUpdated, String authorisation) {
         CaseData caseData = objectMapper.convertValue(
             caseDataUpdated,
             CaseData.class
@@ -134,6 +150,7 @@ public class Fl401ListOnNoticeService {
         String fl401ReasonsForListWithoutNoticeRequested = (String) caseDataUpdated.get(
             FL401_REASONS_FOR_LIST_WITHOUT_NOTICE_REQUESTED);
         log.info("fl401ReasonsForListWithoutNoticeRequested ===> " + fl401ReasonsForListWithoutNoticeRequested);
+        String confirmationBody;
         if (!StringUtils.isEmpty(fl401ReasonsForListWithoutNoticeRequested)) {
             UUID bulkPrintId = null;
             StringBuilder finalReasonsForListWithoutNoticeRequested
@@ -183,17 +200,21 @@ public class Fl401ListOnNoticeService {
                     fl401ReasonsForListWithoutNoticeRequested = fl401ReasonsForListWithoutNoticeRequested
                         + "\n Bulk Print Id: " + bulkPrintId;
                 }
-                CaseNoteDetails currentCaseNoteDetails = addCaseNoteService.getCurrentCaseNoteDetails(
-                    LIST_ON_NOTICE_HEARING_INSTRUCTIONS,
-                    fl401ReasonsForListWithoutNoticeRequested,
-                    userService.getUserDetails(authorisation)
-                );
-                caseDataUpdated.put(
-                    CASE_NOTES,
-                    addCaseNoteService.getCaseNoteDetails(caseData, currentCaseNoteDetails)
-                );
             }
+            CaseNoteDetails currentCaseNoteDetails = addCaseNoteService.getCurrentCaseNoteDetails(
+                LIST_ON_NOTICE_HEARING_INSTRUCTIONS,
+                fl401ReasonsForListWithoutNoticeRequested,
+                userService.getUserDetails(authorisation)
+            );
+            caseDataUpdated.put(
+                CASE_NOTES,
+                addCaseNoteService.getCaseNoteDetails(caseData, currentCaseNoteDetails)
+            );
+            confirmationBody = CONFIRMATION_BODY_WITHOUT_NOTICE;
+        } else {
+            confirmationBody = CONFIRMATION_BODY_WITH_NOTICE;
         }
+
         cleanUpListOnNoticeFields(caseDataUpdated);
         coreCaseDataService.triggerEvent(
             JURISDICTION,
@@ -202,6 +223,11 @@ public class Fl401ListOnNoticeService {
             "internal-update-all-tabs",
             caseDataUpdated
         );
+
+        return ok(SubmittedCallbackResponse.builder()
+                       .confirmationHeader(CONFIRMATION_HEADER)
+                       .confirmationBody(
+                           confirmationBody).build());
     }
 
     private UUID sendViaPost(String authorisation,
