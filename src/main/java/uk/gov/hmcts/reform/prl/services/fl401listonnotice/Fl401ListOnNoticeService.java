8836c617553e6f8c4c0077a4222f6d11a4da3102
sahaSonali <sonali.saha@hmcts.net>
2024-02-20 13:56:13 +0000

    Changes for DA List on notice

--- src/main/java/uk/gov/hmcts/reform/prl/services/fl401listonnotice/Fl401ListOnNoticeService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/fl401listonnotice/Fl401ListOnNoticeService.java
@@ -93,19 +93,18 @@ public class Fl401ListOnNoticeService {
     @Autowired
     private BulkPrintService bulkPrintService;
 
-    public static final String CONFIRMATION_BODY_WITHOUT_NOTICE = "### What happens next \n\n "
-        + System.lineSeparator()
-        + "Admin will be notified to list the hearing on notice."
-        + System.lineSeparator()
-        + "The reasons you have given to list the hearing on notice will be shared with the applicant."
-        + System.lineSeparator()
-        + "Your request details will be saved in case notes.";
+    public static final String CONFIRMATION_BODY_WITHOUT_NOTICE = """
+        ### What happens next
 
-    public static final String CONFIRMATION_BODY_WITH_NOTICE = "### What happens next \n\n "
-        + System.lineSeparator()
-        + "Admin will be notified to list the hearing on notice."
-        + System.lineSeparator()
-        + "Your request details will be saved in case notes.";
+        Admin will be notified to list the hearing on notice.
+        The reasons you have given to list the hearing on notice will be shared with the applicant.
+        Your request details will be saved in case notes....""";
+
+    public static final String CONFIRMATION_BODY_WITH_NOTICE = """
+        ### What happens next
+
+        Admin will be notified to list the hearing on notice.
+        Your request details will be saved in case notes....""";
 
     public static final String CONFIRMATION_HEADER = "# Listing instructions sent to admin";
 
