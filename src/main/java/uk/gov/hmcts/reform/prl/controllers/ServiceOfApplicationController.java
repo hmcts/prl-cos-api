51e4fa737c36f5a120bb10bcd3874cad00ba5c86
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2024-02-24 15:24:45 +0000

    Functional Tests

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ServiceOfApplicationController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ServiceOfApplicationController.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.prl.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -64,7 +63,7 @@ public class ServiceOfApplicationController {
         @RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
         @RequestHeader(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER) String s2sToken,
         @RequestBody CallbackRequest callbackRequest
-    ) throws JsonProcessingException {
+    ) {
         if (authorisationService.isAuthorized(authorisation,s2sToken)) {
             return AboutToStartOrSubmitCallbackResponse.builder().data(serviceOfApplicationService.handleAboutToSubmit(
                 callbackRequest)).build();
