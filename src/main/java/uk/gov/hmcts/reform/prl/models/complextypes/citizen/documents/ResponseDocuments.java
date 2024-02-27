789eb91d3a0986abb8099775483603dfdaeebcd8
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2024-02-27 13:35:24 +0000

    remv

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/documents/ResponseDocuments.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/documents/ResponseDocuments.java
@@ -14,7 +14,6 @@ public class ResponseDocuments {
     private final String createdBy;
     private final LocalDate dateCreated;
     private final Document citizenDocument;
-    private final Document citizenDocumentWelsh;
     private final Document respondentC8Document;
     private final Document respondentC8DocumentWelsh;
     private final LocalDateTime dateTimeCreated;
