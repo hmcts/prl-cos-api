096df461abdc334019231847278bb5ffd51133d1
priteshkr <pritesh.kumar@hmcts.net>
2024-02-29 10:42:06 +0000

    Handled the existing caseBundles

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/BundlingController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/BundlingController.java
@@ -17,7 +17,6 @@ import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.prl.models.dto.bundle.Bundle;
 import uk.gov.hmcts.reform.prl.models.dto.bundle.BundleCreateResponse;
-import uk.gov.hmcts.reform.prl.models.dto.bundle.BundleDetails;
 import uk.gov.hmcts.reform.prl.models.dto.bundle.BundlingInformation;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.services.AuthorisationService;
@@ -106,13 +105,13 @@ public class BundlingController extends AbstractCallbackController {
                 historicalBundles.addAll(existingBundleInformation.getHistoricalBundles());
             }
             if (nonNull(existingBundleInformation.getCaseBundles())) {
-                List<Bundle> existingCaseBundles = existingBundleInformation.getCaseBundles() ;
+                List<Bundle> existingCaseBundles = existingBundleInformation.getCaseBundles();
                 existingCaseBundles.forEach(existingBundle -> {
-                     existingBundle.getValue().toBuilder()
+                    existingBundle.getValue().toBuilder()
                          .historicalStitchedDocument(existingBundle.getValue().getStitchedDocument())
                          .stitchedDocument(null);
 
-                }) ;
+                });
                 historicalBundles.addAll(existingCaseBundles);
             }
             existingBundleInformation.setHistoricalBundles(historicalBundles);
