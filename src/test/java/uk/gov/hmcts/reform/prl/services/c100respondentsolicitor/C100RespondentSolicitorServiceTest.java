0fe042a3038372b25cf1573b7c62b05828612a1a
“sayali-salunkhe1” <Sayali.salunkhe1@HMCTS.net>
2024-03-01 15:41:38 +0000

    prl_2692_Respondent_Solicitor_C7_Docmosis_Welsh: Removed unused import

--- src/test/java/uk/gov/hmcts/reform/prl/services/c100respondentsolicitor/C100RespondentSolicitorServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/c100respondentsolicitor/C100RespondentSolicitorServiceTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.prl.services.c100respondentsolicitor;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.function.ThrowingRunnable;
 import org.junit.jupiter.api.Assertions;
