4f5a0abedd9d7e1c0c9d4e41d8dd1286a18e38aa
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2024-03-01 00:36:25 +0000

    Initial push for am role assignment

---
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/amroles/InternalCaseworkerAmRolesEnum.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.reform.prl.enums.amroles;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
+public enum InternalCaseworkerAmRolesEnum {
+    JUDGE("JUDGE", List.of("circuit-judge", "allocated-magistrate","allocated-judge","leadership-judge","judge")),
+    LEGAL_ADVISER("LEGAL_ADVISER", List.of("tribunal-caseworker", "senior-tribunal-caseworker")),
+    COURT_ADMIN("COURT_ADMIN", List.of("hearing-centre-admin", "ctsc-team-leader","ctsc")),
+    CAFCASS_CYMRU("CAFCASS_CYMRU", List.of("listed-hearing-viewer", "caseworker-privatelaw-externaluser-viewonly"));
+
+    private final String user;
+    private final List<String> roles;
+
+    public String getUser() {
+        return user;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+}
