4bd93c5d4b2dffe100d310a7860aab3adcb3907b
bharadwajsairam <bharadwajsairam001@gmail.com>
2024-03-05 11:33:42 +0000

    checkstyle fix

--- src/test/java/uk/gov/hmcts/reform/prl/services/ManageOrderEmailServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/ManageOrderEmailServiceTest.java
@@ -68,7 +68,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
