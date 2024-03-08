251c1d63b56fe7ae625b03a94e59bd8b3d18d35a
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2024-03-08 09:48:06 +0000

    checkstyle fix

--- src/test/java/uk/gov/hmcts/reform/prl/services/ServiceOfApplicationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/ServiceOfApplicationServiceTest.java
@@ -81,7 +81,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.BLANK_STRING;
