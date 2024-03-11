a06c5d8d890b18eb2f7ec49c0bf068f13df3c7c9
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2024-03-11 15:08:22 +0000

    remove logs

--- src/main/java/uk/gov/hmcts/reform/prl/services/DgsService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/DgsService.java
@@ -94,7 +94,7 @@ public class DgsService {
                 dataMap.put(k, updatedWelshObj);
             }
         });
-        log.info("MyMappppp {}", dataMap);
+
         return generateDocument(authorisation, caseId, templateName,
                                 dataMap
         );
