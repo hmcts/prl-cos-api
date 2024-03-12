cf714c8279c6a55491f7e9f1383768dbb18decf4
Yogendra Upasani <yogendra.upasani@hmcts.net>
2024-03-12 01:00:57 +0000

    Concurrency fix for Cafcass Uploaded documents

--- src/main/java/uk/gov/hmcts/reform/prl/enums/CaseEvent.java
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/CaseEvent.java
@@ -35,7 +35,8 @@ public enum CaseEvent {
     HMC_CASE_STATUS_UPDATE_TO_DECISION_OUTCOME("hmcCaseUpdDecOutcome"),
     CITIZEN_REMOVE_LEGAL_REPRESENTATIVE("citizenRemoveLegalRepresentative"),
     TS_ADMIN_APPLICATION_NOC("testingSupportDummyCase"),
-    CITIZEN_STATEMENT_OF_SERVICE("citizenStatementOfService");
+    CITIZEN_STATEMENT_OF_SERVICE("citizenStatementOfService"),
+    CAFCASS_DOCUMENT_UPLOAD("cafcass-document-upload");
 
     private final String value;
 
