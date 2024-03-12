4ff6c74a3f7a0a9675cf24d8a3fed94d9a2f499b
Khurshed Khan <khurshed.khan@hmcts.net>
2024-03-12 13:34:26 +0000

    PRL-5323:Fixed review comments

--- src/main/java/uk/gov/hmcts/reform/prl/models/DocumentRequest.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/DocumentRequest.java
@@ -1,10 +1,16 @@
 package uk.gov.hmcts.reform.prl.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Builder
 public class DocumentRequest {
 
