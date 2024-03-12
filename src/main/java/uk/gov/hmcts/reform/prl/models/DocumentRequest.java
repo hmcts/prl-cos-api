8bac0fec4d1837a4d5803e9e783e5cd011aee334
Khurshed Khan <khurshed.khan@hmcts.net>
2024-03-12 13:22:29 +0000

    PRL-5323:Fixed review comments

--- src/main/java/uk/gov/hmcts/reform/prl/models/DocumentRequest.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/DocumentRequest.java
@@ -1,10 +1,13 @@
 package uk.gov.hmcts.reform.prl.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class DocumentRequest {
+
+    @JsonProperty("isWelsh")
     private boolean isWelsh;
 }
