8f6eab0d7a9d8872226d4226e87406e868cdc173
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2024-03-13 14:25:58 +0000

    Functional

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/managedocuments/ManageDocumentsControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/managedocuments/ManageDocumentsControllerFunctionalTest.java
@@ -93,7 +93,6 @@ public class ManageDocumentsControllerFunctionalTest {
     private static CaseDetails caseDetails;
 
     @Test
-    @Ignore
     public void createCcdTestCase() throws Exception {
 
         String requestBody = ResourceLoader.loadJson(VALID_CAFCASS_REQUEST_JSON);
@@ -310,10 +309,11 @@ public class ManageDocumentsControllerFunctionalTest {
     @Test
     public void givenMangeDocs_whenCopyDocsNeitherConfNorRestricted_thenAppropriateCategoryForCafcass() throws Exception {
         String requestBody = ResourceLoader.loadJson(MANAGE_DOCUMENT_REQUEST_NEITHER_CONF_NOR_RESTRICTED);
-
+        String requestBodyRevised = requestBody
+            .replace("1687442033910904", caseDetails.getId().toString());
         request
             .header("Authorization", idamTokenGenerator.generateIdamTokenForCafcass())
-            .body(requestBody)
+            .body(requestBodyRevised)
             .when()
             .contentType("application/json")
             .post("/manage-documents/copy-manage-docs")
