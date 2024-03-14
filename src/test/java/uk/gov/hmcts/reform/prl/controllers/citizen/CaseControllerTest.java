61e59aa64cc0d04397523333950db435a11ce654
sahaSonali <sonali.saha@hmcts.net>
2024-03-14 17:56:28 +0000

    fix build issue

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseControllerTest.java
@@ -237,7 +237,7 @@ public class CaseControllerTest {
         when(authorisationService.authoriseUser(authToken)).thenReturn(true);
         when(authorisationService.authoriseService(servAuthToken)).thenReturn(true);
         when(caseService.updateCaseDetails(authToken, caseId, eventId, citizenUpdatedCaseData)).thenReturn(caseDetails);
-        doNothing().when(allTabsService).updateAllTabs(any(CaseData.class));
+        //doNothing().when(allTabsService).updateAllTabs(any(CaseData.class));
 
         CaseData caseData1 = caseController.caseUpdate(
             citizenUpdatedCaseData,
