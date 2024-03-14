d05f6de5c6df896ff8d5c04e17ba62e548f05fc8
yogendraU <66823759+yogendraU@users.noreply.github.com>
2024-03-14 10:59:44 +0000

    Update InternalCaseworkerAmRolesEnum.java

--- src/main/java/uk/gov/hmcts/reform/prl/enums/amroles/InternalCaseworkerAmRolesEnum.java
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/amroles/InternalCaseworkerAmRolesEnum.java
@@ -8,10 +8,10 @@ import java.util.List;
 @RequiredArgsConstructor
 @Getter
 public enum InternalCaseworkerAmRolesEnum {
-    JUDGE("JUDGE", List.of("circuit-judge", "allocated-magistrate","allocated-judge","leadership-judge","judge")),
+    JUDGE("JUDGE", List.of("allocated-magistrate","judge")),
     LEGAL_ADVISER("LEGAL_ADVISER", List.of("tribunal-caseworker", "senior-tribunal-caseworker")),
-    COURT_ADMIN("COURT_ADMIN", List.of("hearing-centre-admin", "ctsc-team-leader","ctsc")),
-    CAFCASS_CYMRU("CAFCASS_CYMRU", List.of("listed-hearing-viewer", "caseworker-privatelaw-externaluser-viewonly"));
+    COURT_ADMIN("COURT_ADMIN", List.of("hearing-centre-admin", "ctsc")),
+    CAFCASS_CYMRU("CAFCASS_CYMRU", List.of("caseworker-privatelaw-externaluser-viewonly"));
 
     private final String user;
     private final List<String> roles;
