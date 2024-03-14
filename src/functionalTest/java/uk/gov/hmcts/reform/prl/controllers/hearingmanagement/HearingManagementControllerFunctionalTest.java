d53d5ca86e5cbf48f73c88a2d7c9b7809c84edf3
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2024-03-14 23:19:15 +0000

    soa

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/hearingmanagement/HearingManagementControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/hearingmanagement/HearingManagementControllerFunctionalTest.java
@@ -1,9 +1,15 @@
 package uk.gov.hmcts.reform.prl.controllers.hearingmanagement;
 
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -12,9 +18,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.prl.ResourceLoader;
 import uk.gov.hmcts.reform.prl.services.AuthorisationService;
 import uk.gov.hmcts.reform.prl.services.hearingmanagement.HearingManagementService;
+import uk.gov.hmcts.reform.prl.utils.IdamTokenGenerator;
+import uk.gov.hmcts.reform.prl.utils.ServiceAuthenticationGenerator;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
@@ -26,11 +35,18 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @ContextConfiguration
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class HearingManagementControllerFunctionalTest {
 
     private MockMvc mockMvc;
     @Autowired
     private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    protected IdamTokenGenerator idamTokenGenerator;
+
+    @Autowired
+    protected ServiceAuthenticationGenerator serviceAuthenticationGenerator;
 
     private static final String VALID_HEARING_MANAGEMENT_REQUEST_BODY = "requests/hearing-management-controller.json";
 
@@ -46,6 +62,38 @@ public class HearingManagementControllerFunctionalTest {
 
     @MockBean
     private AuthorisationService authorisationService;
+
+    private static final String VALID_CAFCASS_REQUEST_JSON = "requests/cafcass-cymru-send-email-request.json";
+
+    private static CaseDetails caseDetails;
+
+    private final String targetInstance =
+        StringUtils.defaultIfBlank(
+            System.getenv("TEST_URL"),
+            "http://localhost:4044"
+        );
+
+    private final RequestSpecification request1 = RestAssured.given().relaxedHTTPSValidation().baseUri(targetInstance);
+
+    @Test
+    public void createCcdTestCase() throws Exception {
+
+        String requestBody = ResourceLoader.loadJson(VALID_CAFCASS_REQUEST_JSON);
+        caseDetails =  request1
+            .header("Authorization", idamTokenGenerator.generateIdamTokenForSystem())
+            .header("ServiceAuthorization", serviceAuthenticationGenerator.generateTokenForCcd())
+            .body(requestBody)
+            .when()
+            .contentType("application/json")
+            .post("/testing-support/create-ccd-case-data")
+            .then()
+            .assertThat().statusCode(200)
+            .extract()
+            .as(CaseDetails.class);
+
+        Assert.assertNotNull(caseDetails);
+        Assert.assertNotNull(caseDetails.getId());
+    }
 
     @Test
     public void givenRequestBody_whenHearing_management_state_update_then200Response() throws Exception {
@@ -63,16 +111,16 @@ public class HearingManagementControllerFunctionalTest {
     @Test
     public void givenRequestBody_whenHearing_management_next_hearing_details_update_then200Response() throws Exception {
         String requestBody = ResourceLoader.loadJson(VALID_NEXT_HEARING_DETAILS_REQUEST_BODY);
-        when(authorisationService.authoriseUser(anyString())).thenReturn(Boolean.TRUE);
-        when(authorisationService.authoriseService(anyString())).thenReturn(Boolean.TRUE);
-        mockMvc.perform(put("/hearing-management-next-hearing-date-update")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .header("authorization", "auth")
-                            .header("serviceAuthorization", "sauth")
-                            .content(requestBody)
-                            .accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andReturn();
+        request1
+            .header("authorization", idamTokenGenerator.generateIdamTokenForSolicitor())
+            .header("serviceAuthorization", serviceAuthenticationGenerator.generateTokenForCcd())
+            .body(requestBody)
+            .when().contentType(String.valueOf(MediaType.APPLICATION_JSON))
+            .accept(String.valueOf(MediaType.APPLICATION_JSON))
+            .put("/hearing-management-next-hearing-date-update")
+            .then()
+            .assertThat().statusCode(200);
+
     }
 
 }
