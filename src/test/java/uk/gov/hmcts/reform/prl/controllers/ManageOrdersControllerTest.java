50076b33a717f1c31d1cdea55552778120ef16ed
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2024-03-14 19:32:43 +0000

    remove ignore

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersControllerTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.prl.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.function.ThrowingRunnable;
 import org.junit.runner.RunWith;
@@ -98,7 +97,6 @@ import static uk.gov.hmcts.reform.prl.utils.ElementUtils.element;
 
 @PropertySource(value = "classpath:application.yaml")
 @RunWith(MockitoJUnitRunner.Silent.class)
-@Ignore
 public class ManageOrdersControllerTest {
 
     private MockMvc mockMvc;
