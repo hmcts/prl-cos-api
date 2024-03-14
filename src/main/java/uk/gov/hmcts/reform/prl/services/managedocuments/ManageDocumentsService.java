f46a402681ea0489ec42c1612be15c2052cd4ceb
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2024-03-14 16:05:59 +0000

    remove logs

--- src/main/java/uk/gov/hmcts/reform/prl/services/managedocuments/ManageDocumentsService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/managedocuments/ManageDocumentsService.java
@@ -706,14 +706,10 @@ public class ManageDocumentsService {
     }
 
     public List<String> getLoggedInUserType(String authorisation) {
-        log.info("=====STARTTTT");
         UserDetails userDetails = userService.getUserDetails(authorisation);
-        log.info("UUUUU {}",userDetails.getEmail());
-        log.info("ROLESSSS {}",userDetails.getRoles());
         List<String> roles = userDetails.getRoles();
         List<String> loggedInUserType = new ArrayList<>();
         if (launchDarklyClient.isFeatureEnabled("role-assignment-api-in-orders-journey")) {
-            log.info("RoleAssignnn {}", userDetails.getEmail());
             //This would check for roles from AM for Judge/Legal advisor/Court admin
             //if it doesn't find then it will check for idam roles for rest of the users
             RoleAssignmentServiceResponse roleAssignmentServiceResponse = roleAssignmentApi.getRoleAssignments(
@@ -734,7 +730,6 @@ public class ManageDocumentsService {
                 List<String> amRoles = roleAssignmentServiceResponse.getRoleAssignmentResponse()
                     .stream()
                     .map(role -> role.getRoleName()).toList();
-                log.info("amrolessss {}", amRoles);
                 if (amRoles.stream().anyMatch(InternalCaseworkerAmRolesEnum.JUDGE.getRoles()::contains)) {
                     loggedInUserType.add(COURT_STAFF);
                     loggedInUserType.add(JUDGE_ROLE);
@@ -751,7 +746,6 @@ public class ManageDocumentsService {
         } else {
             checkExistingIdamRoleConfig(roles, loggedInUserType);
         }
-        log.info("=====ENDDDDDD");
         return loggedInUserType;
     }
 
