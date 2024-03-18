7709486649cdd3f7fbf2a3539f88b47e666de7ae
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2024-03-18 16:59:00 +0000

    Suppressing temp

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/EditAndApproveDraftOrderControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/EditAndApproveDraftOrderControllerFunctionalTest.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.hamcrest.Matchers;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
