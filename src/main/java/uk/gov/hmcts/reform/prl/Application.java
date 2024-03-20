d4ba3ec26d9deffcee0ba031a8774e5e2da92e42
alokDatta <aloknath.datta@hmcts.net>
2024-03-20 13:51:46 +0000

    IDAM sys user caching

--- src/main/java/uk/gov/hmcts/reform/prl/Application.java
+++ src/main/java/uk/gov/hmcts/reform/prl/Application.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @EnableFeignClients(basePackages = {"uk.gov.hmcts.reform.prl.*", "uk.gov.hmcts.reform.prl.services", "uk.gov.hmcts.reform.idam.client",
@@ -26,6 +28,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
     )
  */
 @EnableAsync
+@EnableCaching
+@EnableScheduling
 @SpringBootConfiguration
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {  "uk.gov.hmcts.reform.prl", "uk.gov.hmcts.reform.prl.services",
