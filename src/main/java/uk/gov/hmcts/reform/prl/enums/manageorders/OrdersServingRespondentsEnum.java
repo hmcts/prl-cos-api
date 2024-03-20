d32d236d5e7399fc8be4e41894803aa56fa5e212
JamesCutts00 <James.Cutts@HMCTS.ORG>
2024-03-20 14:24:43 +0000

    content-changes-sprint4:Added enums in backend

---
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/manageorders/OrdersServingRespondentsEnum.java
@@ -0,0 +1,38 @@
+package uk.gov.hmcts.reform.prl.enums.manageorders;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.RequiredArgsConstructor;
+import uk.gov.hmcts.reform.prl.enums.CustomEnumSerializer;
+import uk.gov.hmcts.reform.prl.enums.serviceofapplication.SoaSolicitorServingRespondentsEnum;
+
+@RequiredArgsConstructor
+@JsonSerialize(using = CustomEnumSerializer.class)
+public enum OrdersServingRespondentsEnum {
+    @JsonProperty("applicantLegalRepresentative")
+    applicantLegalRepresentative("applicantLegalRepresentative", "Applicant's legal representative"),
+    @JsonProperty("courtBailiff")
+    courtBailiff("courtBailiff", "Court bailiff (you must arrange for them to serve the order)"),
+    @JsonProperty("courtAdmin")
+    courtAdmin("courtAdmin", "Court admin (you must serve the order)");
+
+    private final String id;
+    private final String displayedValue;
+
+    public String getId() {
+        return id;
+    }
+
+    @JsonValue
+    public String getDisplayedValue() {
+        return displayedValue;
+    }
+
+    @JsonCreator
+    public static SoaSolicitorServingRespondentsEnum getValue(String key) {
+        return SoaSolicitorServingRespondentsEnum.valueOf(key);
+    }
+}
+
