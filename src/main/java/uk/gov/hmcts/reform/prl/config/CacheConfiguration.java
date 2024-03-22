8286389f3f2797885ba9316142b03131be1f503b
alokDatta <aloknath.datta@hmcts.net>
2024-03-22 08:30:00 +0000

    Checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/prl/config/CacheConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/prl/config/CacheConfiguration.java
@@ -13,6 +13,7 @@ public class CacheConfiguration {
     public static final String SYS_USER_CACHE = "systemUserCache";
     public static final String USER_DETAILS_CACHE = "userDetailsCache";
     public static final String ORGANISATION_CACHE = "organisationCache";
+
     @Bean
     public CacheManagerCustomizer<CaffeineCacheManager> cacheManagerCustomizer() {
         return cacheManager -> cacheManager.setAllowNullValues(false);
