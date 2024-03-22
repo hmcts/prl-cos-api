cc433c054fe4eef49c2dd5fc467075aa9e3d060f
Yogendra Upasani <yogendra.upasani@hmcts.net>
2024-03-22 20:01:26 +0000

    pushed for citizen case submission

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseController.java
@@ -96,7 +96,6 @@ public class CaseController {
                 accessCode
             );
             CaseData updatedCaseData = CaseUtils.getCaseData(caseDetails, objectMapper);
-            updatedCaseData = confidentialDetailsMapper.mapConfidentialData(updatedCaseData, true);
             return updatedCaseData
                 .toBuilder().id(caseDetails.getId()).build();
         } else {
