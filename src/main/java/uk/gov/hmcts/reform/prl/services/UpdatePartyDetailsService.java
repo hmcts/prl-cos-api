670aac3f76b80fb795318318d4308440f32167b5
sahaSonali <sonali.saha@hmcts.net>
2024-03-26 11:07:19 +0000

    Restructure Citizen Case Update to fix concurrency issue

--- src/main/java/uk/gov/hmcts/reform/prl/services/UpdatePartyDetailsService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/UpdatePartyDetailsService.java
@@ -326,16 +326,14 @@ public class UpdatePartyDetailsService {
                     || CaseUtils.checkIfAddressIsChanged(respondent.getValue(), resp1.getValue())
                     || CaseUtils.isPhoneNumberChanged(respondent.getValue(), resp1.getValue())
                     || !StringUtils.equals(resp1.getValue().getLabelForDynamicList(), respondent.getValue()
-                    .getLabelForDynamicList())
-                    || CaseUtils.isThereAnyNewConfidentialDataPresent(respondent.getValue(), resp1.getValue()))).toList();
+                    .getLabelForDynamicList()))).toList();
         } else {
             PartyDetails respondentDetailsFL401 = caseDataBefore.getRespondentsFL401();
             if ((CaseUtils.isEmailAddressChanged(respondent.getValue(), respondentDetailsFL401))
                 || CaseUtils.checkIfAddressIsChanged(respondent.getValue(), respondentDetailsFL401)
                 || (CaseUtils.isPhoneNumberChanged(respondent.getValue(), respondentDetailsFL401))
                 || !StringUtils.equals(respondent.getValue().getLabelForDynamicList(), respondentDetailsFL401
-                .getLabelForDynamicList())
-                || CaseUtils.isThereAnyNewConfidentialDataPresent(respondent.getValue(), respondentDetailsFL401)) {
+                .getLabelForDynamicList())) {
                 log.info("respondent data changed for fl401");
                 return true;
             }
