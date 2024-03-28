85c36cbbcfcca44a72a7dc25a7597deef668b290
JamesCutts00 <James.Cutts@HMCTS.ORG>
2024-03-28 14:09:14 +0000

    fix/citizen-concurrency-link-case: fixed checkstyle

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersControllerTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.prl.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.function.ThrowingRunnable;
 import org.junit.runner.RunWith;
