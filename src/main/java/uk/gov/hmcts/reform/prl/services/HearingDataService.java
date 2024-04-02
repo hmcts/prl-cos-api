c0a33561bbf5eb43dc96936da026fcb356ad4c6a
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2024-04-02 15:39:52 +0100

    removed loggers

--- src/main/java/uk/gov/hmcts/reform/prl/services/HearingDataService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/HearingDataService.java
@@ -663,11 +663,6 @@ public class HearingDataService {
                         .build();
                 }
             }
-            //Populate C100 names
-            log.info("hearingData.getApplicantName1 : {}", hearingData.getApplicantName1());
-            log.info("hearingData.getRespondentName1 : {}", hearingData.getRespondentName1());
-            log.info("hearingData.getApplicantSolicitor1 : {}", hearingData.getApplicantSolicitor1());
-
             String order = Objects.nonNull(orderType) ? String.valueOf(orderType) : String.valueOf(caseData.getCreateSelectOrderOptions());
             boolean isDaOrderForCaCase = ManageOrdersUtils.isDaOrderSelectedForCaCase(order, caseData);
 
