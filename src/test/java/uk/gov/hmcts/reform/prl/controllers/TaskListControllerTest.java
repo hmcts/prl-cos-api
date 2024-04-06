3f023dad3db711d22cb44897a7662ce3ecb551d6
alokDatta <aloknath.datta@hmcts.net>
2024-04-06 11:08:16 +0100

    Logger added

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/TaskListControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/TaskListControllerTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.prl.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -29,6 +30,9 @@ public class TaskListControllerTest {
 
     @Mock
     private TaskListService taskListService;
+
+    @Mock
+    private ObjectMapper objectMapper;
 
     Map<String, Object> caseDataMap;
     CaseDetails caseDetails;
