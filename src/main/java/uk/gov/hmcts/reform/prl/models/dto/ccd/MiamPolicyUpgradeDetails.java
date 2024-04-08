d933d8c547c44bb4cc45ecc53d610550459c21c1
sahaSonali <sonali.saha@hmcts.net>
2024-04-08 14:38:37 +0100

    Revert "Changes for MIAM EX UI Policy Change - added logger for Checker"
    This reverts commit a85686b068821c328e38094f64bbb019b6d4423b.

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/MiamPolicyUpgradeDetails.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/MiamPolicyUpgradeDetails.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.prl.models.dto.ccd;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,7 +20,6 @@ import java.util.List;
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class MiamPolicyUpgradeDetails {
-    @JsonProperty("childInvolvedInMiam")
     private YesOrNo childInvolvedInMiam;
     private YesOrNo applicantAttendedMiam;
     private YesOrNo claimingExemptionMiam;
