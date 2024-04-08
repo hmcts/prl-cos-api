3dec6271f43798c8d8d314bd478e4d10e5e2a4e3
sahaSonali <sonali.saha@hmcts.net>
2024-04-08 20:04:02 +0100

    Update checker

--- src/main/java/uk/gov/hmcts/reform/prl/constants/PrlAppsConstants.java
+++ src/main/java/uk/gov/hmcts/reform/prl/constants/PrlAppsConstants.java
@@ -27,9 +27,9 @@ public class PrlAppsConstants {
     public static final String DAYS = "days";
     public static final String HOURS = "hours";
 
-    public static final String CHILD_INVOLVED_IN_MIAM = "childInvolvedInMiam";
-    public static final String APPLICANT_ATTENDED_MIAM = "applicantAttendedMiam";
-    public static final String CLAIMING_EXEMPTION_MIAM = "claimingExemptionMiam";
+    public static final String CHILD_INVOLVED_IN_MIAM = "mpuChildInvolved";
+    public static final String APPLICANT_ATTENDED_MIAM = "mpuApplicantAttended";
+    public static final String CLAIMING_EXEMPTION_MIAM = "mpuClaimingExemption";
 
     // Fee and Pay related constants
     public static final String FEE_VERSION = "1";
