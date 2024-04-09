dc7095415f2300b034dc5fd0931af3f56b90ce5c
Priyanka Singh <priyanka.singh@hmcts.net>
2024-04-09 17:22:00 +0100

    FPET-932: added log

--- src/main/java/uk/gov/hmcts/reform/prl/services/SendAndReplyService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/SendAndReplyService.java
@@ -1274,6 +1274,7 @@ public class SendAndReplyService {
 
     public void sendNotificationToExternalParties(CaseData caseData, String authorisation) {
         try {
+            log.info("----> sendNotificationToExternalParties caseData 1277 >>>> {} ", objectMapper.writeValueAsString(caseData));
             //get the latest message
             Message message = caseData.getSendOrReplyMessage().getSendMessageObject();
             log.info("----> sendNotificationToExternalParties 1274 >>>> {} ", message.getInternalOrExternalMessage());
