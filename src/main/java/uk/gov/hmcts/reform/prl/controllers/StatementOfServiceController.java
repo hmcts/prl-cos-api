59b2640c41175ecdb015c2232ef397d7e1d1653a
bharadwajsairam <bharadwajsairam001@gmail.com>
2024-04-10 13:42:39 +0100

    checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/StatementOfServiceController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/StatementOfServiceController.java
@@ -13,7 +13,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
