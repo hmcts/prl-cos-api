cf9bd1303b9de00749cfa9fbbf6025f67d7757ce
sahaSonali <sonali.saha@hmcts.net>
2024-04-10 17:19:46 +0100

    Add MiamPolicyUpgradeController

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/DomesticAbuseEvidenceDocument.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/DomesticAbuseEvidenceDocument.java
@@ -1,16 +1,20 @@
 package uk.gov.hmcts.reform.prl.models.complextypes;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.prl.models.documents.Document;
 
 @Data
 @Builder(toBuilder = true)
-@AllArgsConstructor
 public class DomesticAbuseEvidenceDocument {
 
     @JsonProperty("domesticAbuseEvidenceDocument")
     private final Document domesticAbuseEvidenceDocument;
+
+    @JsonCreator
+    public DomesticAbuseEvidenceDocument(Document domesticAbuseEvidenceDocument) {
+        this.domesticAbuseEvidenceDocument = domesticAbuseEvidenceDocument;
+    }
 }
