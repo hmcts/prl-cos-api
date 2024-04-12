866daccd3a072a0f9ba72aeca1d2aa5bbb14ab76
sealDebolina <debolina.seal@hmcts.net>
2024-04-12 16:43:24 +0100

    respondingCitizenAOH added to CitizenPartyDetailsMapper

--- src/main/java/uk/gov/hmcts/reform/prl/mapper/citizen/CitizenPartyDetailsMapper.java
+++ src/main/java/uk/gov/hmcts/reform/prl/mapper/citizen/CitizenPartyDetailsMapper.java
@@ -424,7 +424,8 @@ public class CitizenPartyDetailsMapper {
         return existingPartyDetails.toBuilder()
             .response(existingPartyDetails.getResponse()
                           .toBuilder()
-                          .respondentAllegationsOfHarmData(citizenProvidedPartyDetails.getResponse().getRespondentAllegationsOfHarmData())
+                          //.respondentAllegationsOfHarmData(citizenProvidedPartyDetails.getResponse().getRespondentAllegationsOfHarmData())
+                          .respondingCitizenAoH(citizenProvidedPartyDetails.getResponse().getRespondingCitizenAoH())
                           .build())
             .build();
     }
