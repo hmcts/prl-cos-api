787cdf2cd821a8c94efcc307d339480c51951a32
anshika-nigam <anshika.nigam1@hmcts.net>
2024-04-12 12:06:53 +0100

    Updated code

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/citizen/CaseControllerTest.java
@@ -157,7 +157,7 @@ public class CaseControllerTest {
             .applicantCaseName("test")
             .createdDate(LocalDateTime.now().minusDays(10))
             .build();
-        String caseId = "1234567891234568";
+        String caseId = "1234567891234567";
 
         when(authorisationService.isAuthorized(authToken, servAuthToken)).thenReturn(false);
         caseController.retrieveCaseWithHearing(caseId, "test", authToken, servAuthToken);
