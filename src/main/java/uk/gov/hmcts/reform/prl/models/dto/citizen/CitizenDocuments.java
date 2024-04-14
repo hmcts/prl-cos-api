9d569a903795199785669a9e1b9d39117719747f
channa-patil <channabasappagouda.patil@hmcts.net>
2024-04-14 11:00:10 +0100

    Populate isNew, isFinal & servedData fields for orders & sonar fixes

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/citizen/CitizenDocuments.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/citizen/CitizenDocuments.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.prl.models.dto.citizen;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +14,7 @@ import java.util.List;
 @Getter
 @Data
 @Builder(toBuilder = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CitizenDocuments {
 
     private String partyId;
@@ -30,5 +32,8 @@ public class CitizenDocuments {
     //private List<Document> unservedRespondentPack; // if personal service - CA/CB -> Either applicant pack //
     private List<Document> respondentSoaPack;
     private String servedParty;
-    private String wasCafcassServed;
+    private boolean wasCafcassServed;
+    private LocalDateTime servedDate;
+    private boolean isNew;
+    private boolean isFinal;
 }
