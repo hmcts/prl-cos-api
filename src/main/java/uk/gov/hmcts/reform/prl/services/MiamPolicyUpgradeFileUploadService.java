f772b755e2c2c0bdf6608f72f26e57589461bd4d
sahaSonali <sonali.saha@hmcts.net>
2024-04-15 15:18:47 +0100

    Changes for taskList version 3

--- src/main/java/uk/gov/hmcts/reform/prl/services/MiamPolicyUpgradeFileUploadService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/MiamPolicyUpgradeFileUploadService.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.prl.services;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,12 +14,14 @@ import uk.gov.hmcts.reform.prl.services.managedocuments.ManageDocumentsService;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.apache.commons.collections.CollectionUtils.isNotEmpty;
 import static uk.gov.hmcts.reform.prl.enums.YesOrNo.Yes;
 import static uk.gov.hmcts.reform.prl.enums.miampolicyupgrade.MiamExemptionsChecklistEnum.domesticAbuse;
 import static uk.gov.hmcts.reform.prl.enums.miampolicyupgrade.MiamExemptionsChecklistEnum.previousMiamAttendance;
 import static uk.gov.hmcts.reform.prl.enums.miampolicyupgrade.MiamPreviousAttendanceChecklistEnum.miamPolicyUpgradePreviousAttendance_Value_1;
 import static uk.gov.hmcts.reform.prl.enums.miampolicyupgrade.MiamPreviousAttendanceChecklistEnum.miamPolicyUpgradePreviousAttendance_Value_2;
 import static uk.gov.hmcts.reform.prl.enums.miampolicyupgrade.TypeOfMiamAttendanceEvidenceEnum.miamCertificate;
+import static uk.gov.hmcts.reform.prl.services.managedocuments.ManageDocumentsService.CONFIDENTIAL;
 import static uk.gov.hmcts.reform.prl.utils.ElementUtils.element;
 
 @Service
@@ -35,27 +36,36 @@ public class MiamPolicyUpgradeFileUploadService {
         log.info("Inside renameConfidentialDocumentForMiamPolicyUpgrade");
         if ((caseData.getMiamPolicyUpgradeDetails().getMpuExemptionReasons().contains(domesticAbuse))
             && Yes.equals(caseData.getMiamPolicyUpgradeDetails().getMpuIsDomesticAbuseEvidenceProvided())
-            && CollectionUtils.isNotEmpty(caseData.getMiamPolicyUpgradeDetails().getMpuDomesticAbuseEvidenceDocument())) {
+            && isNotEmpty(caseData.getMiamPolicyUpgradeDetails().getMpuDomesticAbuseEvidenceDocument())) {
             List<Element<DomesticAbuseEvidenceDocument>> mpuConfidentialDomesticAbuseEvidenceDocument = new ArrayList<>();
             caseData.getMiamPolicyUpgradeDetails().getMpuDomesticAbuseEvidenceDocument()
                 .stream().forEach(domesticAbuseEvidenceDocument -> {
                     log.info("Going to append Confidential prefix for Domestic Abuse Evidence Document");
-                    Document domesticAbuseDocument = manageDocumentsService.downloadAndDeleteDocument(
-                        domesticAbuseEvidenceDocument.getValue().getDomesticAbuseDocument(), systemAuthorisation);
-                    mpuConfidentialDomesticAbuseEvidenceDocument.add(element(DomesticAbuseEvidenceDocument.builder().domesticAbuseDocument(
-                        domesticAbuseDocument).build()));
+                    Document domesticAbuseDocument = domesticAbuseEvidenceDocument.getValue().getDomesticAbuseDocument();
+                    if (!domesticAbuseDocument.getDocumentFileName().startsWith(CONFIDENTIAL)) {
+                        domesticAbuseDocument = manageDocumentsService.downloadAndDeleteDocument(
+                            domesticAbuseDocument, systemAuthorisation);
+                        mpuConfidentialDomesticAbuseEvidenceDocument.add(element(DomesticAbuseEvidenceDocument.builder().domesticAbuseDocument(
+                            domesticAbuseDocument).build()));
+                    }
                 });
             caseData = caseData.toBuilder()
                 .miamPolicyUpgradeDetails(caseData.getMiamPolicyUpgradeDetails()
                                               .toBuilder()
-                                              .mpuDomesticAbuseEvidenceDocument(mpuConfidentialDomesticAbuseEvidenceDocument)
+                                              .mpuDomesticAbuseEvidenceDocument(isNotEmpty(
+                                                  mpuConfidentialDomesticAbuseEvidenceDocument)
+                                                                                    ? mpuConfidentialDomesticAbuseEvidenceDocument
+                                                                                    : caseData.getMiamPolicyUpgradeDetails()
+                                                  .getMpuDomesticAbuseEvidenceDocument())
                                               .build())
                 .build();
         }
         if (caseData.getMiamPolicyUpgradeDetails().getMpuExemptionReasons().contains(previousMiamAttendance)
             && ObjectUtils.isNotEmpty(caseData.getMiamPolicyUpgradeDetails().getMpuPreviousMiamAttendanceReason())) {
             if (miamPolicyUpgradePreviousAttendance_Value_1.equals(caseData.getMiamPolicyUpgradeDetails().getMpuPreviousMiamAttendanceReason())
-                && ObjectUtils.isNotEmpty(caseData.getMiamPolicyUpgradeDetails().getMpuDocFromDisputeResolutionProvider())) {
+                && ObjectUtils.isNotEmpty(caseData.getMiamPolicyUpgradeDetails().getMpuDocFromDisputeResolutionProvider())
+                && !caseData.getMiamPolicyUpgradeDetails().getMpuDocFromDisputeResolutionProvider().getDocumentFileName().startsWith(
+                CONFIDENTIAL)) {
                 log.info("Going to append Confidential prefix previous maim attendance document for option 1");
                 Document mpuDocFromDisputeResolutionProvider = manageDocumentsService.downloadAndDeleteDocument(
                     caseData.getMiamPolicyUpgradeDetails().getMpuDocFromDisputeResolutionProvider(),
@@ -64,12 +74,15 @@ public class MiamPolicyUpgradeFileUploadService {
                 caseData = caseData.toBuilder()
                     .miamPolicyUpgradeDetails(caseData.getMiamPolicyUpgradeDetails()
                                                   .toBuilder()
-                                                  .mpuDocFromDisputeResolutionProvider(mpuDocFromDisputeResolutionProvider)
+                                                  .mpuDocFromDisputeResolutionProvider(
+                                                      mpuDocFromDisputeResolutionProvider)
                                                   .build())
                     .build();
             } else if (miamPolicyUpgradePreviousAttendance_Value_2.equals(caseData.getMiamPolicyUpgradeDetails().getMpuPreviousMiamAttendanceReason())
                 && miamCertificate.equals(caseData.getMiamPolicyUpgradeDetails().getMpuTypeOfPreviousMiamAttendanceEvidence())
-                && ObjectUtils.isNotEmpty(caseData.getMiamPolicyUpgradeDetails().getMpuCertificateByMediator())) {
+                && ObjectUtils.isNotEmpty(caseData.getMiamPolicyUpgradeDetails().getMpuCertificateByMediator())
+                && !caseData.getMiamPolicyUpgradeDetails().getMpuCertificateByMediator().getDocumentFileName().startsWith(
+                CONFIDENTIAL)) {
                 log.info("Going to append Confidential prefix previous maim attendance document for option 2");
                 Document mpuCertificateByMediator = manageDocumentsService.downloadAndDeleteDocument(
                     caseData.getMiamPolicyUpgradeDetails().getMpuCertificateByMediator(),
