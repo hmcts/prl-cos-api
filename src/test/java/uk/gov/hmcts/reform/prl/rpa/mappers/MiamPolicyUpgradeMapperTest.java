71300eb5459525f7c4978c2e5924cc2897d9be24
sahaSonali <sonali.saha@hmcts.net>
2024-04-16 14:25:04 +0100

    Changes for

--- src/test/java/uk/gov/hmcts/reform/prl/rpa/mappers/MiamPolicyUpgradeMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/rpa/mappers/MiamPolicyUpgradeMapperTest.java
@@ -5,15 +5,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
-import uk.gov.hmcts.reform.prl.enums.MiamDomesticViolenceChecklistEnum;
-import uk.gov.hmcts.reform.prl.enums.MiamExemptionsChecklistEnum;
-import uk.gov.hmcts.reform.prl.enums.MiamOtherGroundsChecklistEnum;
-import uk.gov.hmcts.reform.prl.enums.MiamPreviousAttendanceChecklistEnum;
-import uk.gov.hmcts.reform.prl.enums.MiamUrgencyReasonChecklistEnum;
+import uk.gov.hmcts.reform.prl.enums.YesOrNo;
+import uk.gov.hmcts.reform.prl.enums.miampolicyupgrade.TypeOfMiamAttendanceEvidenceEnum;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
-import uk.gov.hmcts.reform.prl.models.dto.ccd.MiamDetails;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.MiamPolicyUpgradeDetails;
 
 import java.util.List;
+
+import static uk.gov.hmcts.reform.prl.enums.miampolicyupgrade.MiamChildProtectionConcernChecklistEnum.MIAMChildProtectionConcernChecklistEnum_value_1;
+import static uk.gov.hmcts.reform.prl.enums.miampolicyupgrade.MiamDomesticAbuseChecklistEnum.miamDomesticAbuseChecklistEnum_Value_1;
+import static uk.gov.hmcts.reform.prl.enums.miampolicyupgrade.MiamDomesticAbuseChecklistEnum.miamDomesticAbuseChecklistEnum_Value_2;
+import static uk.gov.hmcts.reform.prl.enums.miampolicyupgrade.MiamExemptionsChecklistEnum.childProtectionConcern;
+import static uk.gov.hmcts.reform.prl.enums.miampolicyupgrade.MiamExemptionsChecklistEnum.domesticAbuse;
+import static uk.gov.hmcts.reform.prl.enums.miampolicyupgrade.MiamExemptionsChecklistEnum.other;
+import static uk.gov.hmcts.reform.prl.enums.miampolicyupgrade.MiamExemptionsChecklistEnum.previousMiamAttendance;
+import static uk.gov.hmcts.reform.prl.enums.miampolicyupgrade.MiamExemptionsChecklistEnum.urgency;
+import static uk.gov.hmcts.reform.prl.enums.miampolicyupgrade.MiamOtherGroundsChecklistEnum.miamPolicyUpgradeOtherGrounds_Value_3;
+import static uk.gov.hmcts.reform.prl.enums.miampolicyupgrade.MiamPreviousAttendanceChecklistEnum.miamPolicyUpgradePreviousAttendance_Value_2;
+import static uk.gov.hmcts.reform.prl.enums.miampolicyupgrade.MiamUrgencyReasonChecklistEnum.miamPolicyUpgradeUrgencyReason_Value_1;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MiamPolicyUpgradeMapperTest {
@@ -23,7 +32,7 @@ public class MiamPolicyUpgradeMapperTest {
 
     @Test
     public void testMapWithEmptyMiamDetails() {
-        CaseData caseData = CaseData.builder().miamDetails(MiamDetails.builder().build()).build();
+        CaseData caseData = CaseData.builder().miamPolicyUpgradeDetails(MiamPolicyUpgradeDetails.builder().build()).build();
         Assert.assertNotNull(miamPolicyUpgradeMapper.map(caseData));
     }
 
@@ -31,18 +40,29 @@ public class MiamPolicyUpgradeMapperTest {
     public void testMap() {
         CaseData caseData = CaseData
             .builder()
-            .miamDetails(MiamDetails
-                .builder()
-                .miamExemptionsChecklist(List.of(MiamExemptionsChecklistEnum.domesticViolence))
-                .miamDomesticViolenceChecklist(List.of(MiamDomesticViolenceChecklistEnum
-                    .miamDomesticViolenceChecklistEnum_Value_1))
-                .miamUrgencyReasonChecklist(List.of(MiamUrgencyReasonChecklistEnum
-                    .miamUrgencyReasonChecklistEnum_Value_1))
-                .miamPreviousAttendanceChecklist(MiamPreviousAttendanceChecklistEnum
-                    .miamPreviousAttendanceChecklistEnum_Value_1)
-                .miamOtherGroundsChecklist(MiamOtherGroundsChecklistEnum
-                    .miamOtherGroundsChecklistEnum_Value_1)
-                .build())
+            .miamPolicyUpgradeDetails(MiamPolicyUpgradeDetails
+                                          .builder()
+                                          .mpuChildInvolvedInMiam(YesOrNo.No)
+                                          .mpuApplicantAttendedMiam(YesOrNo.No)
+                                          .mpuClaimingExemptionMiam(YesOrNo.Yes)
+                                          .mpuExemptionReasons(List.of(
+                                              domesticAbuse,
+                                              childProtectionConcern,
+                                              urgency,
+                                              previousMiamAttendance,
+                                              other
+                                          ))
+                                          .mpuDomesticAbuseEvidences(List.of(
+                                              miamDomesticAbuseChecklistEnum_Value_1,
+                                              miamDomesticAbuseChecklistEnum_Value_2
+                                          ))
+                                          .mpuIsDomesticAbuseEvidenceProvided(YesOrNo.Yes)
+                                          .mpuChildProtectionConcernReason(MIAMChildProtectionConcernChecklistEnum_value_1)
+                                          .mpuUrgencyReason(miamPolicyUpgradeUrgencyReason_Value_1)
+                                          .mpuPreviousMiamAttendanceReason(miamPolicyUpgradePreviousAttendance_Value_2)
+                                          .mpuTypeOfPreviousMiamAttendanceEvidence(TypeOfMiamAttendanceEvidenceEnum.miamAttendanceDetails)
+                                          .mpuOtherExemptionReasons(miamPolicyUpgradeOtherGrounds_Value_3)
+                                          .build())
             .build();
         Assert.assertNotNull(miamPolicyUpgradeMapper.map(caseData));
     }
