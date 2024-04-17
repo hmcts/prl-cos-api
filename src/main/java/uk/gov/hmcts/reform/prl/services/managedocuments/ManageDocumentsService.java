c59c87cb818ed1010360b57c76c12e332cc98f70
JamesCutts00 <James.Cutts@HMCTS.ORG>
2024-04-17 14:39:26 +0100

    prl-5569/fm5_form_solicitor_upload: Added loggers

--- src/main/java/uk/gov/hmcts/reform/prl/services/managedocuments/ManageDocumentsService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/managedocuments/ManageDocumentsService.java
@@ -223,7 +223,6 @@ public class ManageDocumentsService {
                     isWaTaskSetForFirstDocumentIteration = true;
                     setFlagsForWaTask(updatedCaseData, caseDataUpdated, userRole, quarantineLegalDoc);
                 }
-                log.info("Quartiane legal doc is: {}", quarantineLegalDoc);
                 moveDocumentsToQuarantineTab(quarantineLegalDoc, updatedCaseData, caseDataUpdated, userRole);
             }
         }
