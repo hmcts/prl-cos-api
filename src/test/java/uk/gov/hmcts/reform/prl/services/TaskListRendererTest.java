e693e839e36c262510c1b37d6bcc15929afd4914
JamesCutts00 <James.Cutts@HMCTS.ORG>
2024-04-17 12:25:13 +0100

    added coverage to tasklistrenderer

--- src/test/java/uk/gov/hmcts/reform/prl/services/TaskListRendererTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/TaskListRendererTest.java
@@ -21,9 +21,9 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.TASK_LIST_VERSION_V2;
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.TASK_LIST_VERSION_V3;
 import static uk.gov.hmcts.reform.prl.enums.Event.ALLEGATIONS_OF_HARM;
 import static uk.gov.hmcts.reform.prl.enums.Event.ALLEGATIONS_OF_HARM_REVISED;
 import static uk.gov.hmcts.reform.prl.enums.Event.APPLICANT_DETAILS;
@@ -46,6 +46,7 @@ import static uk.gov.hmcts.reform.prl.enums.Event.HEARING_URGENCY;
 import static uk.gov.hmcts.reform.prl.enums.Event.INTERNATIONAL_ELEMENT;
 import static uk.gov.hmcts.reform.prl.enums.Event.LITIGATION_CAPACITY;
 import static uk.gov.hmcts.reform.prl.enums.Event.MIAM;
+import static uk.gov.hmcts.reform.prl.enums.Event.MIAM_POLICY_UPGRADE;
 import static uk.gov.hmcts.reform.prl.enums.Event.OTHER_CHILDREN_NOT_PART_OF_THE_APPLICATION;
 import static uk.gov.hmcts.reform.prl.enums.Event.OTHER_PEOPLE_IN_THE_CASE;
 import static uk.gov.hmcts.reform.prl.enums.Event.OTHER_PEOPLE_IN_THE_CASE_REVISED;
@@ -126,6 +127,28 @@ public class TaskListRendererTest {
             Task.builder().event(WELSH_LANGUAGE_REQUIREMENTS).state(NOT_STARTED).build(),
             Task.builder().event(VIEW_PDF_DOCUMENT).state(NOT_STARTED).build(),
             Task.builder().event(SUBMIT_AND_PAY).state(NOT_STARTED).build());
+
+    private final List<Task> taskC100V3 = List.of(
+        Task.builder().event(CASE_NAME).state(NOT_STARTED).state(NOT_STARTED).build(),
+        Task.builder().event(TYPE_OF_APPLICATION).state(NOT_STARTED).build(),
+        Task.builder().event(HEARING_URGENCY).state(NOT_STARTED).build(),
+        Task.builder().event(CHILD_DETAILS_REVISED).state(NOT_STARTED).build(),
+        Task.builder().event(APPLICANT_DETAILS).state(NOT_STARTED).build(),
+        Task.builder().event(RESPONDENT_DETAILS).state(NOT_STARTED).build(),
+        Task.builder().event(OTHER_PEOPLE_IN_THE_CASE_REVISED).state(NOT_STARTED).build(),
+        Task.builder().event(OTHER_CHILDREN_NOT_PART_OF_THE_APPLICATION).state(NOT_STARTED).build(),
+        Task.builder().event(CHILDREN_AND_APPLICANTS).state(NOT_STARTED).build(),
+        Task.builder().event(CHILDREN_AND_RESPONDENTS).state(NOT_STARTED).build(),
+        Task.builder().event(CHILDREN_AND_OTHER_PEOPLE_IN_THIS_APPLICATION).state(NOT_STARTED).build(),
+        Task.builder().event(ALLEGATIONS_OF_HARM_REVISED).state(IN_PROGRESS).build(),
+        Task.builder().event(MIAM_POLICY_UPGRADE).state(NOT_STARTED).build(),
+        Task.builder().event(OTHER_PROCEEDINGS).state(NOT_STARTED).build(),
+        Task.builder().event(ATTENDING_THE_HEARING).state(NOT_STARTED).build(),
+        Task.builder().event(INTERNATIONAL_ELEMENT).state(FINISHED).build(),
+        Task.builder().event(LITIGATION_CAPACITY).state(FINISHED).build(),
+        Task.builder().event(WELSH_LANGUAGE_REQUIREMENTS).state(NOT_STARTED).build(),
+        Task.builder().event(VIEW_PDF_DOCUMENT).state(NOT_STARTED).build(),
+        Task.builder().event(SUBMIT_AND_PAY).state(NOT_STARTED).build());
 
 
     private final List<EventValidationErrors> errors = List.of(
@@ -386,5 +409,17 @@ public class TaskListRendererTest {
         String actualTaskList = taskListRenderer.render(taskC100V2, errors, true, caseData);
 
         assertThat(expectedTaskList).isEqualTo(actualTaskList);
+    }
+
+    @Test
+    public void shouldRenderC100V3TaskList() {
+        CaseData caseData = CaseData.builder()
+            .caseTypeOfApplication(PrlAppsConstants.C100_CASE_TYPE)
+            .state(State.AWAITING_SUBMISSION_TO_HMCTS)
+            .taskListVersion(TASK_LIST_VERSION_V3)
+            .build();
+        String actualTaskList = taskListRenderer.render(taskC100V3, errors, true, caseData);
+
+        assertNotNull(actualTaskList);
     }
 }
