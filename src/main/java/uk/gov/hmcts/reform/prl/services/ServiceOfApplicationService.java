b97a541c24a02cb6bc99485dfa1fb774b2d33c2c
channa-patil <channabasappagouda.patil@hmcts.net>
2024-04-18 18:24:05 +0100

    PRL-5300 - Added test case coverage for remove legal rep email/post

--- src/main/java/uk/gov/hmcts/reform/prl/services/ServiceOfApplicationService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ServiceOfApplicationService.java
@@ -2260,7 +2260,6 @@ public class ServiceOfApplicationService {
                                       CaseInvite caseInvite, String template) {
 
         Map<String, Object> dataMap = populateAccessCodeMap(caseData, party, caseInvite);
-        log.info("Data sent to DGS to generate document {}", dataMap);
         return fetchCoverLetter(authorisation, template, dataMap);
     }
 
