a35596dd4c499a0a2557af10944ecd897f6331e1
alokDatta <aloknath.datta@hmcts.net>
2024-04-25 13:15:20 +0100

    Build fix

--- src/main/java/uk/gov/hmcts/reform/prl/utils/UploadAdditionalApplicationUtils.java
+++ src/main/java/uk/gov/hmcts/reform/prl/utils/UploadAdditionalApplicationUtils.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.prl.enums.uploadadditionalapplication.AdditionalApplicationTypeEnum;
 import uk.gov.hmcts.reform.prl.enums.uploadadditionalapplication.OtherApplicationType;
+import uk.gov.hmcts.reform.prl.models.complextypes.uploadadditionalapplication.AdditionalApplicationsBundle;
 import uk.gov.hmcts.reform.prl.models.complextypes.uploadadditionalapplication.C2DocumentBundle;
 import uk.gov.hmcts.reform.prl.models.complextypes.uploadadditionalapplication.OtherApplicationsBundle;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
@@ -29,33 +30,77 @@ public class UploadAdditionalApplicationUtils {
 
     public String getAwPTaskName(CaseData caseData) {
         String awpTaskNameToBe = null;
+        log.info("Getting the AWP task name");
         if (caseData.getUploadAdditionalApplicationData() != null
             && caseData.getUploadAdditionalApplicationData().getAdditionalApplicationsApplyingFor() != null
             && isNotEmpty(caseData.getUploadAdditionalApplicationData().getAdditionalApplicationsApplyingFor())) {
+            log.info("Inside the if loop");
             boolean c2OrderListed = false;
             boolean otherOrderListed = false;
             String awpTaskNameForOtherOrder = null;
             if (caseData.getUploadAdditionalApplicationData().getAdditionalApplicationsApplyingFor().contains(
                 AdditionalApplicationTypeEnum.c2Order)) {
+                log.info("C2 order listed");
                 c2OrderListed = true;
             }
             if (caseData.getUploadAdditionalApplicationData()
                 .getAdditionalApplicationsApplyingFor().contains(AdditionalApplicationTypeEnum.otherOrder)
                 && caseData.getUploadAdditionalApplicationData().getTemporaryOtherApplicationsBundle() != null) {
+                log.info("other order listed");
                 otherOrderListed = true;
                 OtherApplicationType applicationType = getOtherApplicationType(caseData.getUploadAdditionalApplicationData()
                                                                                    .getTemporaryOtherApplicationsBundle());
                 awpTaskNameForOtherOrder = applicationType != null ? applicationType.getDisplayedValue() : "";
+                log.info("awpTaskNameForOtherOrder = " + awpTaskNameForOtherOrder);
             }
 
             if (c2OrderListed && otherOrderListed) {
                 awpTaskNameToBe = C2_AND + awpTaskNameForOtherOrder;
+                log.info("Inside c2 and other");
             } else if (c2OrderListed) {
+                log.info("Inside c2");
                 awpTaskNameToBe = C2_APPLICATION_WITHIN_PROCEEDINGS;
             } else if (otherOrderListed) {
+                log.info("Inside other");
                 awpTaskNameToBe = awpTaskNameForOtherOrder;
             }
+            log.info(" final awpTaskNameToBe = " + awpTaskNameForOtherOrder);
         }
+        return awpTaskNameToBe;
+    }
+
+    public String getAwPTaskNameWhenPaymentCompleted(AdditionalApplicationsBundle additionalApplicationsBundle) {
+        String awpTaskNameToBe = null;
+        String awpTaskNameForOtherOrder = null;
+        boolean c2OrderListed = false;
+        boolean otherOrderListed = false;
+
+        log.info("Getting the AWP task name");
+        if (additionalApplicationsBundle != null
+            && additionalApplicationsBundle.getC2DocumentBundle() != null) {
+            log.info("Inside the if loop for C2");
+            c2OrderListed = true;
+        }
+        if (additionalApplicationsBundle != null
+            && additionalApplicationsBundle.getOtherApplicationsBundle() != null) {
+            log.info("Inside the if loop for C2");
+            otherOrderListed = true;
+            OtherApplicationType applicationType = additionalApplicationsBundle.getOtherApplicationsBundle().getApplicationType();
+            awpTaskNameForOtherOrder = applicationType != null ? applicationType.getDisplayedValue() : "";
+        }
+
+
+        if (c2OrderListed && otherOrderListed) {
+            awpTaskNameToBe = C2_AND + awpTaskNameForOtherOrder;
+            log.info("Inside c2 and other");
+        } else if (c2OrderListed) {
+            log.info("Inside c2");
+            awpTaskNameToBe = C2_APPLICATION_WITHIN_PROCEEDINGS;
+        } else if (otherOrderListed) {
+            log.info("Inside other");
+            awpTaskNameToBe = awpTaskNameForOtherOrder;
+        }
+        log.info("final awpTaskNameToBe = " + awpTaskNameForOtherOrder);
         return awpTaskNameToBe;
     }
 
@@ -127,14 +172,19 @@ public class UploadAdditionalApplicationUtils {
     public OtherApplicationType getOtherApplicationType(OtherApplicationsBundle temporaryOtherApplicationsBundle) {
         OtherApplicationType applicationType = null;
         if (null != temporaryOtherApplicationsBundle.getCaApplicantApplicationType()) {
+            log.info("inside temporaryOtherApplicationsBundle.getCaApplicantApplicationType()");
             applicationType = OtherApplicationType.valueOf(temporaryOtherApplicationsBundle.getCaApplicantApplicationType().name());
         } else if (null != temporaryOtherApplicationsBundle.getCaRespondentApplicationType()) {
+            log.info("inside temporaryOtherApplicationsBundle.getCaRespondentApplicationType()");
             applicationType = OtherApplicationType.valueOf(temporaryOtherApplicationsBundle.getCaRespondentApplicationType().name());
         } else if (null != temporaryOtherApplicationsBundle.getDaApplicantApplicationType()) {
+            log.info("inside temporaryOtherApplicationsBundle.getDaApplicantApplicationType()");
             applicationType = OtherApplicationType.valueOf(temporaryOtherApplicationsBundle.getDaApplicantApplicationType().name());
         } else if (null != temporaryOtherApplicationsBundle.getDaRespondentApplicationType()) {
+            log.info("inside temporaryOtherApplicationsBundle.getDaRespondentApplicationType()");
             applicationType = OtherApplicationType.valueOf(temporaryOtherApplicationsBundle.getDaRespondentApplicationType().name());
         }
+        log.info("applicationType is::" + applicationType);
         return applicationType;
     }
 }
