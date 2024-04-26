e9a1edb82ae17cbdbb9bebf196e4742949da5881
vasthav-patta <112877261+vasthav-patta@users.noreply.github.com>
2024-04-26 14:44:10 +0100

    removing un used imports

--- src/main/java/uk/gov/hmcts/reform/prl/services/SystemUserService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/SystemUserService.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.prl.services;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.scheduling.annotation.Scheduled;
