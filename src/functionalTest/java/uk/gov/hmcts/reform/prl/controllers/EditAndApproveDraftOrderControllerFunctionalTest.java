10fcd023d154653f42bd57a5f338c0b84ed0b539
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2024-04-27 17:29:37 +0100

    test all EditAndApprove

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/EditAndApproveDraftOrderControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/EditAndApproveDraftOrderControllerFunctionalTest.java
@@ -8,7 +8,6 @@ import org.hamcrest.Matchers;
 import org.hamcrest.core.IsNull;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
@@ -41,7 +40,6 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @RunWith(SpringRunner.class)
 @ContextConfiguration
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-@Ignore
 public class EditAndApproveDraftOrderControllerFunctionalTest {
 
     private final String userToken = "Bearer testToken";
