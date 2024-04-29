3d58bb30a9657bbe45d659ce0b74bc489d5e7fe4
alokDatta <aloknath.datta@hmcts.net>
2024-04-29 13:47:51 +0100

    Case flags CTSC WA changes

---
+++ src/main/java/uk/gov/hmcts/reform/prl/services/caseflags/CaseFlagsWaService.java
@@ -0,0 +1,51 @@
+package uk.gov.hmcts.reform.prl.services.caseflags;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.idam.client.models.UserDetails;
+import uk.gov.hmcts.reform.prl.clients.RoleAssignmentApi;
+import uk.gov.hmcts.reform.prl.enums.amroles.InternalCaseworkerAmRolesEnum;
+import uk.gov.hmcts.reform.prl.models.roleassignment.getroleassignment.RoleAssignmentResponse;
+import uk.gov.hmcts.reform.prl.models.roleassignment.getroleassignment.RoleAssignmentServiceResponse;
+import uk.gov.hmcts.reform.prl.models.user.UserRoles;
+import uk.gov.hmcts.reform.prl.services.UserService;
+
+import java.util.List;
+import java.util.Map;
+
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.WA_PERFORMING_USER;
+
+@Component
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+@Slf4j
+public class CaseFlagsWaService {
+    private final UserService userService;
+    private final RoleAssignmentApi roleAssignmentApi;
+    private final AuthTokenGenerator authTokenGenerator;
+
+    public Map<String, Object> findIfCreatedByCTSC(String authorisation, CallbackRequest callbackRequest) {
+        Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
+        UserDetails userDetails = userService.getUserDetails(authorisation);
+        RoleAssignmentServiceResponse roleAssignmentServiceResponse = roleAssignmentApi.getRoleAssignments(
+            authorisation,
+            authTokenGenerator.generate(),
+            null,
+            userDetails.getId()
+        );
+
+        List<String> roles = roleAssignmentServiceResponse
+            .getRoleAssignmentResponse()
+            .stream()
+            .map(RoleAssignmentResponse::getRoleName)
+            .toList();
+
+        if (roles.stream().anyMatch(InternalCaseworkerAmRolesEnum.CTSC.getRoles()::contains)) {
+            caseDataUpdated.put(WA_PERFORMING_USER, UserRoles.CTSC.name());
+        }
+        return caseDataUpdated;
+    }
+}
