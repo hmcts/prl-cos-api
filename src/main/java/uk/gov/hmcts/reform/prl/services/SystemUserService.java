a9ae4dc1d2fc1f2a810a76b80f88fdb0dd690610
vasthav-patta <112877261+vasthav-patta@users.noreply.github.com>
2024-04-29 10:39:28 +0100

    using default cache config

--- src/main/java/uk/gov/hmcts/reform/prl/services/SystemUserService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/SystemUserService.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.prl.services;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,13 +11,12 @@ import uk.gov.hmcts.reform.idam.client.IdamClient;
 import uk.gov.hmcts.reform.idam.client.OAuth2Configuration;
 import uk.gov.hmcts.reform.prl.config.SystemUserConfiguration;
 
-import static uk.gov.hmcts.reform.prl.config.CacheConfiguration.SYS_USER_CACHE;
-
 @Service
 @Slf4j
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
-@CacheConfig(cacheNames = SYS_USER_CACHE)
 public class SystemUserService {
+
+    public static final String SYS_USER_CACHE = "systemUserCache";
 
     private final OAuth2Configuration auth;
 
@@ -26,13 +24,13 @@ public class SystemUserService {
 
     private final IdamClient idamClient;
 
-    @Cacheable(cacheNames = {SYS_USER_CACHE})
+    @Cacheable(cacheNames = SYS_USER_CACHE)
     public String getSysUserToken() {
         log.info("Fetching system user token");
         return idamClient.getAccessToken(userConfig.getUserName(), userConfig.getPassword());
     }
 
-    @CacheEvict(allEntries = true, cacheNames = {SYS_USER_CACHE})
+    @CacheEvict(allEntries = true, cacheNames = SYS_USER_CACHE)
     @Scheduled(fixedDelay = 1800000)
     public void cacheEvict() {
         log.info("Evicting system user cron cache");
