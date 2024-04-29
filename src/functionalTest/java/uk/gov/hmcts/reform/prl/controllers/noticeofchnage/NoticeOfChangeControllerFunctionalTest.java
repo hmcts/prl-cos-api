b0e5da044ec8ef351962409314938a9533ed992b
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2024-04-29 09:29:56 +0100

    nochange

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/noticeofchnage/NoticeOfChangeControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/noticeofchnage/NoticeOfChangeControllerFunctionalTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.prl.controllers.noticeofchnage;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +25,6 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @ContextConfiguration
-@Ignore
 public class NoticeOfChangeControllerFunctionalTest {
 
     private MockMvc mockMvc;
