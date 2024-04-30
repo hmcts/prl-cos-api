6300b31a3704248cc8c63e4115759cc399deecb2
alokDatta <aloknath.datta@hmcts.net>
2024-04-30 23:46:59 +0100

    Async handler

--- src/main/java/uk/gov/hmcts/reform/prl/handlers/CaseFlagsEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/prl/handlers/CaseFlagsEventHandler.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.prl.services.caseflags;
+package uk.gov.hmcts.reform.prl.handlers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -7,9 +7,10 @@ import com.google.common.collect.Maps;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
-import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 import uk.gov.hmcts.reform.prl.clients.RoleAssignmentApi;
 import uk.gov.hmcts.reform.prl.clients.ccd.records.StartAllTabsUpdateDataContent;
@@ -18,28 +19,31 @@ import uk.gov.hmcts.reform.prl.enums.amroles.InternalCaseworkerAmRolesEnum;
 import uk.gov.hmcts.reform.prl.events.CaseFlagsEvent;
 import uk.gov.hmcts.reform.prl.models.roleassignment.getroleassignment.RoleAssignmentResponse;
 import uk.gov.hmcts.reform.prl.models.roleassignment.getroleassignment.RoleAssignmentServiceResponse;
-import uk.gov.hmcts.reform.prl.services.EventService;
 import uk.gov.hmcts.reform.prl.services.UserService;
 import uk.gov.hmcts.reform.prl.services.tab.alltabs.AllTabServiceImpl;
 
 import java.util.List;
 import java.util.Map;
 
-@Component
-@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+
 @Slf4j
-public class CaseFlagsWaService {
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class CaseFlagsEventHandler {
     private final UserService userService;
     private final RoleAssignmentApi roleAssignmentApi;
     private final AuthTokenGenerator authTokenGenerator;
     private final ObjectMapper objectMapper;
     private final AllTabServiceImpl allTabService;
-    private final EventService eventPublisher;
 
-    public void setUpWaTaskForCaseFlags(String authorisation, String caseId) {
-        UserDetails userDetails = userService.getUserDetails(authorisation);
+    @Async
+    @EventListener
+    public void triggerDummyEventForCaseFlags(final CaseFlagsEvent event) {
+        log.info("Inside event handler");
+        String caseId = String.valueOf(event.callbackRequest().getCaseDetails().getId());
+        UserDetails userDetails = userService.getUserDetails(event.authorisation());
         RoleAssignmentServiceResponse roleAssignmentServiceResponse = roleAssignmentApi.getRoleAssignments(
-            authorisation,
+            event.authorisation(),
             authTokenGenerator.generate(),
             null,
             userDetails.getId()
@@ -56,45 +60,10 @@ public class CaseFlagsWaService {
             log.info("error");
         }
         if (roles.stream().anyMatch(InternalCaseworkerAmRolesEnum.CTSC.getRoles()::contains)) {
-            log.info("triggered by user having CTSC role");
-            StartAllTabsUpdateDataContent startAllTabsUpdateDataContent = allTabService.getStartUpdateForSpecificEvent(
-                caseId,
-                CaseEvent.CREATE_WA_TASK_FOR_CTSC_CASE_FLAGS.getValue()
-            );
-
-            allTabService.submitAllTabsUpdate(
-                startAllTabsUpdateDataContent.authorisation(),
-                caseId,
-                startAllTabsUpdateDataContent.startEventResponse(),
-                startAllTabsUpdateDataContent.eventRequestData(),
-                startAllTabsUpdateDataContent.caseDataMap()
-            );
-        }
-    }
-
-    public void setUpWaTaskForCaseFlagsCopy(String authorisation, CallbackRequest callbackRequest) {
-        String caseId = String.valueOf(callbackRequest.getCaseDetails().getId());
-        UserDetails userDetails = userService.getUserDetails(authorisation);
-        RoleAssignmentServiceResponse roleAssignmentServiceResponse = roleAssignmentApi.getRoleAssignments(
-            authorisation,
-            authTokenGenerator.generate(),
-            null,
-            userDetails.getId()
-        );
-
-        List<String> roles = roleAssignmentServiceResponse
-            .getRoleAssignmentResponse()
-            .stream()
-            .map(RoleAssignmentResponse::getRoleName)
-            .toList();
-        try {
-            log.info("roles retrieved ===>" + objectMapper.writeValueAsString(roles));
-        } catch (JsonProcessingException e) {
-            log.info("error");
-        }
-        if (roles.stream().anyMatch(InternalCaseworkerAmRolesEnum.CTSC.getRoles()::contains)) {
-            Map<String, Object> caseDetailsBeforeMap =  objectMapper.convertValue(callbackRequest.getCaseDetailsBefore(), Map.class);
-            Map<String, Object> caseDetailsAfterMap =  objectMapper.convertValue(callbackRequest.getCaseDetails(), Map.class);
+            Map<String, Object> caseDetailsBeforeMap
+                = objectMapper.convertValue(event.callbackRequest().getCaseDetailsBefore(), Map.class);
+            Map<String, Object> caseDetailsAfterMap
+                = objectMapper.convertValue(event.callbackRequest().getCaseDetails(), Map.class);
 
             if (!caseDetailsBeforeMap.equals(caseDetailsAfterMap)) {
                 MapDifference<String, Object> diff = Maps.difference(caseDetailsBeforeMap, caseDetailsAfterMap);
@@ -119,13 +88,5 @@ public class CaseFlagsWaService {
                 startAllTabsUpdateDataContent.caseDataMap()
             );
         }
-    }
-
-    public void setUpWaTaskForCaseFlagsEventHandler(String authorisation, CallbackRequest callbackRequest) {
-        CaseFlagsEvent caseFlagsEvent = CaseFlagsEvent.builder()
-            .authorisation(authorisation)
-            .callbackRequest(callbackRequest)
-            .build();
-        eventPublisher.publishEvent(caseFlagsEvent);
     }
 }
