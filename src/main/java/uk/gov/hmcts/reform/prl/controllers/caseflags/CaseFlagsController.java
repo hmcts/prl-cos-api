15bda1832f1e7cd9f548772fc1f425fec3606f81
alokDatta <aloknath.datta@hmcts.net>
2024-04-30 23:46:59 +0100

    Async handler

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/caseflags/CaseFlagsController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/caseflags/CaseFlagsController.java
@@ -102,4 +102,38 @@ public class CaseFlagsController {
             throw (new RuntimeException(INVALID_CLIENT));
         }
     }
+
+    @PostMapping(path = "/setup-wa-task-2", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
+    @Operation(description = "Callback to validate case creator to decide on the WA task")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Callback processed.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AboutToStartOrSubmitCallbackResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)})
+    @SecurityRequirement(name = "Bearer Authentication")
+    public void setUpWaTaskForCaseFlags2(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
+        @RequestHeader(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER) String s2sToken,
+        @RequestBody CallbackRequest callbackRequest
+    ) {
+        if (authorisationService.isAuthorized(authorisation, s2sToken)) {
+            try {
+                log.info("setUpWaTaskForCaseFlags in case controller CaseDetails start json after ===>"
+                             + objectMapper.writeValueAsString(callbackRequest.getCaseDetails()));
+            } catch (JsonProcessingException e) {
+                log.info("error");
+            }
+
+            try {
+                log.info("setUpWaTaskForCaseFlags in case controller CaseDetails start json before ===>"
+                             + objectMapper.writeValueAsString(callbackRequest.getCaseDetailsBefore()));
+            } catch (JsonProcessingException e) {
+                log.info("error");
+            }
+            caseFlagsWaService.setUpWaTaskForCaseFlagsEventHandler(authorisation,
+                                                           callbackRequest
+            );
+        } else {
+            throw (new RuntimeException(INVALID_CLIENT));
+        }
+    }
 }
