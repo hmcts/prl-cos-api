6d1618b0a2b02aa79c5cff3a6bbb880169b057dd
anshika-nigam <anshika.nigam1@hmcts.net>
2024-04-30 11:49:10 +0100

    Updated code

--- src/main/java/uk/gov/hmcts/reform/prl/services/managedocuments/ManageDocumentsService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/managedocuments/ManageDocumentsService.java
@@ -357,7 +357,7 @@ public class ManageDocumentsService {
             List<Element<QuarantineLegalDoc>> existingCaseDocuments = getQuarantineDocs(caseData, userRole, true);
             existingCaseDocuments.add(element(finalConfidentialDocument));
             updateQuarantineDocs(caseDataUpdated, existingCaseDocuments, userRole, true);
-            log.info(finalConfidentialDocument.document.documentFileName + "final******");
+            log.info(finalConfidentialDocument.getDocument().getDocumentFileName() + "final******");
             log.info(existingCaseDocuments + "**********existingCaseDocuments");
             log.info(finalConfidentialDocument + "**********finalConfidentialDocument");
             if (finalConfidentialDocument.fileName.equals("C7_Document.pdf")) {
