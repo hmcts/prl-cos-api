686562a64ea0edefdb76175995d1eb8266b8b03c
alokDatta <aloknath.datta@hmcts.net>
2024-05-01 13:19:39 +0100

    Code clean up

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/caseflags/CaseFlagsController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/caseflags/CaseFlagsController.java
@@ -1,7 +1,5 @@
 package uk.gov.hmcts.reform.prl.controllers.caseflags;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -10,7 +8,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,84 +23,14 @@ import uk.gov.hmcts.reform.prl.services.caseflags.CaseFlagsWaService;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.INVALID_CLIENT;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/caseflags")
 public class CaseFlagsController {
     private final AuthorisationService authorisationService;
     private final CaseFlagsWaService caseFlagsWaService;
-    private final ObjectMapper objectMapper;
 
     @PostMapping(path = "/setup-wa-task", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
-    @Operation(description = "Callback to validate case creator to decide on the WA task")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Callback processed.",
-            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AboutToStartOrSubmitCallbackResponse.class))),
-        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)})
-    @SecurityRequirement(name = "Bearer Authentication")
-    public void setUpWaTaskForCaseFlags(
-        @RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
-        @RequestHeader(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER) String s2sToken,
-        @RequestBody CallbackRequest callbackRequest
-    ) {
-        if (authorisationService.isAuthorized(authorisation, s2sToken)) {
-            try {
-                log.info("setUpWaTaskForCaseFlags in case controller CaseDetails start json after ===>"
-                             + objectMapper.writeValueAsString(callbackRequest.getCaseDetails()));
-            } catch (JsonProcessingException e) {
-                log.info("error");
-            }
-
-            try {
-                log.info("setUpWaTaskForCaseFlags in case controller CaseDetails start json before ===>"
-                             + objectMapper.writeValueAsString(callbackRequest.getCaseDetailsBefore()));
-            } catch (JsonProcessingException e) {
-                log.info("error");
-            }
-            caseFlagsWaService.setUpWaTaskForCaseFlags(authorisation,
-                                                       String.valueOf(callbackRequest.getCaseDetails().getId())
-            );
-        } else {
-            throw (new RuntimeException(INVALID_CLIENT));
-        }
-    }
-
-    @PostMapping(path = "/setup-wa-task-1", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
-    @Operation(description = "Callback to validate case creator to decide on the WA task")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Callback processed.",
-            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AboutToStartOrSubmitCallbackResponse.class))),
-        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)})
-    @SecurityRequirement(name = "Bearer Authentication")
-    public void setUpWaTaskForCaseFlags1(
-        @RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
-        @RequestHeader(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER) String s2sToken,
-        @RequestBody CallbackRequest callbackRequest
-    ) {
-        if (authorisationService.isAuthorized(authorisation, s2sToken)) {
-            try {
-                log.info("setUpWaTaskForCaseFlags in case controller CaseDetails start json after ===>"
-                             + objectMapper.writeValueAsString(callbackRequest.getCaseDetails()));
-            } catch (JsonProcessingException e) {
-                log.info("error");
-            }
-
-            try {
-                log.info("setUpWaTaskForCaseFlags in case controller CaseDetails start json before ===>"
-                             + objectMapper.writeValueAsString(callbackRequest.getCaseDetailsBefore()));
-            } catch (JsonProcessingException e) {
-                log.info("error");
-            }
-            caseFlagsWaService.setUpWaTaskForCaseFlagsCopy(authorisation,
-                                                       callbackRequest
-            );
-        } else {
-            throw (new RuntimeException(INVALID_CLIENT));
-        }
-    }
-
-    @PostMapping(path = "/setup-wa-task-2", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
     @Operation(description = "Callback to validate case creator to decide on the WA task")
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "Callback processed.",
@@ -116,21 +43,9 @@ public class CaseFlagsController {
         @RequestBody CallbackRequest callbackRequest
     ) {
         if (authorisationService.isAuthorized(authorisation, s2sToken)) {
-            try {
-                log.info("setUpWaTaskForCaseFlags in case controller CaseDetails start json after ===>"
-                             + objectMapper.writeValueAsString(callbackRequest.getCaseDetails()));
-            } catch (JsonProcessingException e) {
-                log.info("error");
-            }
-
-            try {
-                log.info("setUpWaTaskForCaseFlags in case controller CaseDetails start json before ===>"
-                             + objectMapper.writeValueAsString(callbackRequest.getCaseDetailsBefore()));
-            } catch (JsonProcessingException e) {
-                log.info("error");
-            }
-            caseFlagsWaService.setUpWaTaskForCaseFlagsEventHandler(authorisation,
-                                                           callbackRequest
+            caseFlagsWaService.setUpWaTaskForCaseFlagsEventHandler(
+                authorisation,
+                callbackRequest
             );
         } else {
             throw (new RuntimeException(INVALID_CLIENT));
--- src/main/java/uk/gov/hmcts/reform/prl/handlers/CaseFlagsEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/prl/handlers/CaseFlagsEventHandler.java
@@ -1,11 +1,7 @@
 package uk.gov.hmcts.reform.prl.handlers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.MapDifference;
-import com.google.common.collect.Maps;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
@@ -17,19 +13,13 @@ import uk.gov.hmcts.reform.prl.clients.ccd.records.StartAllTabsUpdateDataContent
 import uk.gov.hmcts.reform.prl.enums.CaseEvent;
 import uk.gov.hmcts.reform.prl.enums.amroles.InternalCaseworkerAmRolesEnum;
 import uk.gov.hmcts.reform.prl.events.CaseFlagsEvent;
-import uk.gov.hmcts.reform.prl.models.Element;
-import uk.gov.hmcts.reform.prl.models.caseflags.flagdetails.FlagDetail;
-import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.models.roleassignment.getroleassignment.RoleAssignmentResponse;
 import uk.gov.hmcts.reform.prl.models.roleassignment.getroleassignment.RoleAssignmentServiceResponse;
 import uk.gov.hmcts.reform.prl.services.UserService;
 import uk.gov.hmcts.reform.prl.services.tab.alltabs.AllTabServiceImpl;
-import uk.gov.hmcts.reform.prl.utils.CaseUtils;
 
 import java.util.List;
-import java.util.Map;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class CaseFlagsEventHandler {
@@ -42,7 +32,6 @@ public class CaseFlagsEventHandler {
     @Async
     @EventListener
     public void triggerDummyEventForCaseFlags(final CaseFlagsEvent event) {
-        log.info("Inside event handler");
         String caseId = String.valueOf(event.callbackRequest().getCaseDetails().getId());
         UserDetails userDetails = userService.getUserDetails(event.authorisation());
         RoleAssignmentServiceResponse roleAssignmentServiceResponse = roleAssignmentApi.getRoleAssignments(
@@ -57,43 +46,7 @@ public class CaseFlagsEventHandler {
             .stream()
             .map(RoleAssignmentResponse::getRoleName)
             .toList();
-        try {
-            log.info("roles retrieved ===>" + objectMapper.writeValueAsString(roles));
-        } catch (JsonProcessingException e) {
-            log.info("error");
-        }
         if (roles.stream().anyMatch(InternalCaseworkerAmRolesEnum.CTSC.getRoles()::contains)) {
-            Map<String, Object> caseDetailsBeforeMap
-                = objectMapper.convertValue(event.callbackRequest().getCaseDetailsBefore(), Map.class);
-            Map<String, Object> caseDetailsAfterMap
-                = objectMapper.convertValue(event.callbackRequest().getCaseDetails(), Map.class);
-            CaseData caseDataBefore = CaseUtils.getCaseData(
-                event.callbackRequest().getCaseDetailsBefore(),
-                objectMapper
-            );
-            CaseData caseDataAfter = CaseUtils.getCaseData(event.callbackRequest().getCaseDetails(), objectMapper);
-
-            if (caseDataBefore.getCaseFlags().getDetails().size() != caseDataAfter.getCaseFlags().getDetails().size()) {
-                Element<FlagDetail> flagDetailElement = caseDataAfter.getCaseFlags().getDetails()
-                    .get(caseDataAfter.getCaseFlags().getDetails().size() - 1);
-
-                if ("Requested".equalsIgnoreCase(flagDetailElement.getValue().status)) {
-                    log.info("Status found as Requested for case flag item:: " + flagDetailElement.getValue().name);
-                } else {
-                    log.info("Status found as different for case flag item:: " + flagDetailElement.getValue().name);
-                }
-            }
-
-            if (!caseDetailsBeforeMap.equals(caseDetailsAfterMap)) {
-                MapDifference<String, Object> diff = Maps.difference(caseDetailsBeforeMap, caseDetailsAfterMap);
-                try {
-                    log.info("difference in the map is ===>" + objectMapper.writeValueAsString(diff.entriesDiffering()));
-                } catch (JsonProcessingException e) {
-                    log.info("error");
-                }
-            }
-
-            log.info("triggered by user having CTSC role");
             StartAllTabsUpdateDataContent startAllTabsUpdateDataContent = allTabService.getStartUpdateForSpecificEvent(
                 caseId,
                 CaseEvent.CREATE_WA_TASK_FOR_CTSC_CASE_FLAGS.getValue()
