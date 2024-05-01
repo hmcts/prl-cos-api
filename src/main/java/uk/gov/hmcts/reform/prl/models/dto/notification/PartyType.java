bc3dfdfa744ab17439ead31ea4e8a4bbcf525d93
channa-patil <channabasappagouda.patil@hmcts.net>
2024-05-01 10:57:38 +0100

    PRL-5568 - Added a new field to update fm5 reminders sent already

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/notification/PartyType.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/notification/PartyType.java
@@ -1,5 +1,34 @@
 package uk.gov.hmcts.reform.prl.models.dto.notification;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.RequiredArgsConstructor;
+import uk.gov.hmcts.reform.prl.enums.CustomEnumSerializer;
+
+@RequiredArgsConstructor
+@JsonSerialize(using = CustomEnumSerializer.class)
 public enum PartyType {
-    APPLICANT, APPLICANT_SOLICITOR, RESPONDENT, RESPONDENT_SOLICITOR
+    @JsonProperty("APPLICANT")
+    APPLICANT("APPLICANT", "Applicant"),
+    @JsonProperty("APPLICANT_SOLICITOR")
+    APPLICANT_SOLICITOR("APPLICANT_SOLICITOR", "Applicant solicitor"),
+    @JsonProperty("RESPONDENT")
+    RESPONDENT("RESPONDENT", "Respondent"),
+    @JsonProperty("RESPONDENT_SOLICITOR")
+    RESPONDENT_SOLICITOR("RESPONDENT_SOLICITOR", "Respondent solicitor");
+
+    private final String code;
+    private final String displayedValue;
+
+    @JsonValue
+    public String getDisplayedValue() {
+        return displayedValue;
+    }
+
+    @JsonCreator
+    public static PartyType getValue(String key) {
+        return PartyType.valueOf(key);
+    }
 }
