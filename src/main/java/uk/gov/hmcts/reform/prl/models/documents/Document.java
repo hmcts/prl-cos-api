48d8e06309c01bbb9f011f673051b07ba458a68b
Yogendra Upasani <yogendra.upasani@hmcts.net>
2024-05-07 17:28:55 +0100

    Ignored upload timestamp field

--- src/main/java/uk/gov/hmcts/reform/prl/models/documents/Document.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/documents/Document.java
@@ -2,6 +2,8 @@ package uk.gov.hmcts.reform.prl.models.documents;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
@@ -13,6 +15,7 @@ import java.util.Date;
 @Value
 @Builder(toBuilder = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Document {
 
     @JsonProperty("document_url")
@@ -27,11 +30,6 @@ public class Document {
     String categoryId;
     @JsonProperty("document_creation_date")
     Date documentCreatedOn;
-    @JsonProperty("upload_timestamp")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    LocalDateTime uploadTimeStamp;
-
-
 
     @JsonCreator
     public Document(@JsonProperty("document_url") String documentUrl,
@@ -39,14 +37,13 @@ public class Document {
                     @JsonProperty("document_filename") String documentFileName,
                     @JsonProperty("document_hash") String documentHash,
                     @JsonProperty("category_id") String categoryId,
-                    Date documentCreatedOn, LocalDateTime uploadTimeStamp) {
+                    Date documentCreatedOn) {
         this.documentUrl = documentUrl;
         this.documentBinaryUrl = documentBinaryUrl;
         this.documentFileName = documentFileName;
         this.documentHash = documentHash;
         this.categoryId = categoryId;
         this.documentCreatedOn = documentCreatedOn;
-        this.uploadTimeStamp = uploadTimeStamp;
     }
 
     public static Document buildFromDocument(uk.gov.hmcts.reform.ccd.document.am.model.Document document) {
