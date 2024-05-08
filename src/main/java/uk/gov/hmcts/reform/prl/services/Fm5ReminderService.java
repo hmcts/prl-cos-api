4db7bcf8365ea0b9316d20d0f64e8a2ad655f1c3
hmcts <prudhvi.maddineni@hmcts.net>
2024-05-08 13:51:51 +0100

    coverage branch

--- src/main/java/uk/gov/hmcts/reform/prl/services/Fm5ReminderService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/Fm5ReminderService.java
@@ -1,8 +1,10 @@
 package uk.gov.hmcts.reform.prl.services;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
