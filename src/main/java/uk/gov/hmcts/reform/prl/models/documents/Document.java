81ebd4624d504e6dcebd69e5e621c1729869d9a5
channa-patil <channabasappagouda.patil@hmcts.net>
2024-05-08 11:25:01 +0100

    feat/PRL-5566 - Fixed fetching cases from elastic search

--- src/main/java/uk/gov/hmcts/reform/prl/models/documents/Document.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/documents/Document.java
@@ -1,15 +1,12 @@
 package uk.gov.hmcts.reform.prl.models.documents;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Value;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
 @Value
