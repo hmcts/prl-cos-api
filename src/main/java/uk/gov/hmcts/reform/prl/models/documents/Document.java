1b7e4ad43ea986dee07e1847652e557cddea1d97
sairamkantheti1 <sai.kantheti@HMCTS.NET>
2024-05-09 14:29:23 +0100

    FPET-939 upload time stamp changes

--- src/main/java/uk/gov/hmcts/reform/prl/models/documents/Document.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/documents/Document.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.prl.models.documents;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
@@ -13,6 +14,7 @@ import java.util.Date;
 @Value
 @Builder(toBuilder = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Document {
 
     @JsonProperty("document_url")
