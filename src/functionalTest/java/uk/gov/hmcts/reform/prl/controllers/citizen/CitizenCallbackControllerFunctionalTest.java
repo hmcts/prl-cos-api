d0ecd080dd43e8070b6ba0e555a85d732cb87e56
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2024-05-13 16:22:50 +0100

    func test

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/citizen/CitizenCallbackControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/citizen/CitizenCallbackControllerFunctionalTest.java
@@ -4,57 +4,24 @@ import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
-import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
-import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.prl.ResourceLoader;
-import uk.gov.hmcts.reform.prl.services.SystemUserService;
-import uk.gov.hmcts.reform.prl.services.citizen.CitizenEmailService;
-import uk.gov.hmcts.reform.prl.services.tab.alltabs.AllTabServiceImpl;
 import uk.gov.hmcts.reform.prl.utils.IdamTokenGenerator;
 import uk.gov.hmcts.reform.prl.utils.ServiceAuthenticationGenerator;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 import static uk.gov.hmcts.reform.prl.controllers.ManageOrdersControllerFunctionalTest.VALID_CAFCASS_REQUEST_JSON;
 
 @Slf4j
 @SpringBootTest
-@RunWith(SpringRunner.class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ContextConfiguration
 public class CitizenCallbackControllerFunctionalTest {
-    private MockMvc mockMvc;
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-    @MockBean
-    private AllTabServiceImpl allTabsService;
-    @MockBean
-    private CoreCaseDataApi coreCaseDataApi;
-    @MockBean
-    private AuthTokenGenerator authTokenGenerator;
-    @MockBean
-    private SystemUserService systemUserService;
-    @MockBean
-    private CitizenEmailService citizenEmailService;
-
-
-    private static final String VALID_REQUEST_BODY = "requests/call-back-controller.json";
-
-    private static CaseDetails caseDetails;
 
     private final String targetInstance =
         StringUtils.defaultIfBlank(
@@ -73,17 +40,13 @@ public class CitizenCallbackControllerFunctionalTest {
     @Autowired
     protected ServiceAuthenticationGenerator serviceAuthenticationGenerator;
 
-    @BeforeAll
-    public void setUp() {
-        this.mockMvc = webAppContextSetup(webApplicationContext).build();
-    }
 
     @Test
     public void givenRequestBody_whenUpdate_citizen_application_then200Response() throws Exception {
 
         String requestBody = ResourceLoader.loadJson(VALID_CAFCASS_REQUEST_JSON);
 
-        caseDetails =  request
+        CaseDetails caseDetails =  request
             .header("Authorization", idamTokenGenerator.generateIdamTokenForSystem())
             .header("ServiceAuthorization", serviceAuthenticationGenerator.generateTokenForCcd())
             .body(requestBody)
