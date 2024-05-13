3b35e6c786f96ac3aa6215cdd0c7648f01d705b1
sahaSonali <sonali.saha@hmcts.net>
2024-05-13 11:20:51 +0100

    Changes for Citizen Backend Mapping work

--- src/main/java/uk/gov/hmcts/reform/prl/models/c100rebuild/C100RebuildMiamElements.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/c100rebuild/C100RebuildMiamElements.java
@@ -15,26 +15,30 @@ import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class C100RebuildMiamElements {
 
-    @JsonProperty("miam_otherProceedings")
-    private YesOrNo miamOtherProceedings;
-    @JsonProperty("miam_consent")
-    private String miamConsent;
-    @JsonProperty("miam_attendance")
-    private YesOrNo miamAttendance;
+
     @JsonProperty("miam_mediatorDocument")
     private YesOrNo miamMediatorDocument;
+    @JsonProperty("miam_noMediatorAccessSubfields")
+    private String[] miamNoMediatorAccessSubfields;
+    @JsonProperty("miam_consent")
+    private String miamConsent;
+
+
+    //
+    @JsonProperty("miam_otherProceedings")
+    private YesOrNo miamOtherProceedings;
+    @JsonProperty("miam_attendance")
+    private YesOrNo miamAttendance;
     @JsonProperty("miam_validReason")
     private YesOrNo miamValidReason;
-    @JsonProperty("miam_haveDocSigned")
-    private YesOrNo miamHaveDocSigned;
-    @JsonProperty("miam_certificate")
-    private Document miamCertificate;
+
     @JsonProperty("miam_nonAttendanceReasons")
     private String[] miamNonAttendanceReasons;
+
     @JsonProperty("miam_domesticAbuse")
     private String[] miamDomesticAbuse;
-    @JsonProperty("miam_domesticabuse_involvement_subfields")
-    private String[] miamDomesticAbuseInvolvementSubfields;
+    @JsonProperty("miam_domesticAbuse_policeInvolvement_subfields")
+    private String[] miamDomesticAbusePoliceInvolvementSubfields;
     @JsonProperty("miam_domesticabuse_courtInvolvement_subfields")
     private String[] miamDomesticAbuseCourtInvolvementSubfields;
     @JsonProperty("miam_domesticabuse_letterOfBeingVictim_subfields")
@@ -43,14 +47,43 @@ public class C100RebuildMiamElements {
     private String[] miamDomesticAbuseLetterFromAuthoritySubfields;
     @JsonProperty("miam_domesticabuse_letterFromSupportService_subfields")
     private String[] miamDomesticAbuseLetterFromSupportServiceSubfields;
+    @JsonProperty("miam_canProvideDomesticAbuseEvidence")
+    private YesOrNo miamCanProvideDomesticAbuseEvidence;
+    @JsonProperty("miam_detailsOfDomesticAbuseEvidence")
+    private String miamDetailsOfDomesticAbuseEvidence;
+    @JsonProperty("miam_domesticAbuseEvidenceDocs")
+    private uk.gov.hmcts.reform.prl.models.documents.Document[] miamDomesticAbuseEvidenceDocs;
+
     @JsonProperty("miam_childProtectionEvidence")
-    private String[] miamChildProtectionEvidence;
+    private String miamChildProtectionEvidence;
+
     @JsonProperty("miam_urgency")
-    private String[] miamUrgency;
+    private String miamUrgency;
+
     @JsonProperty("miam_previousAttendance")
-    private String[] miamPreviousAttendance;
+    private String miamPreviousAttendance;
+    @JsonProperty("miam_previousAttendanceEvidenceDoc")
+    private uk.gov.hmcts.reform.prl.models.documents.Document miamPreviousAttendanceEvidenceDoc;
+    @JsonProperty("miam_haveDocSignedByMediatorForPrevAttendance")
+    private YesOrNo miamHaveDocSignedByMediatorForPrevAttendance;
+    @JsonProperty("miam_detailsOfEvidence")
+    private String miamDetailsOfEvidence;
+
     @JsonProperty("miam_notAttendingReasons")
-    private String[] miamNotAttendingReasons;
-    @JsonProperty("miam_noMediatorAccessSubfields")
-    private String[] miamNoMediatorAccessSubfields;
+    private String miamNotAttendingReasons;
+    @JsonProperty("miam_noMediatorReasons")
+    private String miamNoMediatorReasons;
+    @JsonProperty("miam_noAppointmentAvailableDetails")
+    private String miamNoAppointmentAvailableDetails;
+    @JsonProperty("miam_unableToAttainDueToDisablityDetails")
+    private String miamUnableToAttainDueToDisablityDetails;
+    @JsonProperty("miam_noMediatorIn15mileDetails")
+    private String miamNoMediatorIn15mileDetails;
+
+    @JsonProperty("miam_haveDocSigned")
+    private YesOrNo miamHaveDocSigned;
+    @JsonProperty("miam_certificate")
+    private Document miamCertificate;
+
+
 }
