62de91dffff7689c7063300e0e6cbcfc1323f29f
alokDatta <aloknath.datta@hmcts.net>
2024-05-17 15:56:35 +0100

    Checkstyle fixed

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/CallbackControllerFT.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/CallbackControllerFT.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
