299de562b51552e99a03ba36f4cb466253e520c5
JamesCutts00 <James.Cutts@HMCTS.ORG>
2024-05-21 10:23:26 +0100

    Fixed sonar issues in ReasonableAdjustmentsControllerTest

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/ServiceOfApplicationControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/ServiceOfApplicationControllerTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.prl.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.function.ThrowingRunnable;
