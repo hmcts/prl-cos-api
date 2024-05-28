319fd2dc7f9eacd4ab32579de786cc753322c2fb
velmurugan-mani <velmurugan.mani@hmcts.net>
2024-05-28 14:05:54 +0100

    PRL-1731 Sonar Issue Fix: testHandleSoaSubmittedConfidential is removed for duplicate code

--- src/test/java/uk/gov/hmcts/reform/prl/services/ServiceOfApplicationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/ServiceOfApplicationServiceTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.prl.services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
