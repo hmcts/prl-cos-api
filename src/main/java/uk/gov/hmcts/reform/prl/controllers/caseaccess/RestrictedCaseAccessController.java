8bc408cda44e46107da9bc7929d1c4f46ea6546d
alokDatta <aloknath.datta@hmcts.net>
2024-05-29 17:37:27 +0100

    changes to the event name

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/caseaccess/RestrictedCaseAccessController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/caseaccess/RestrictedCaseAccessController.java
@@ -5,11 +5,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,15 +24,20 @@ import uk.gov.hmcts.reform.ccd.client.model.Classification;
 import uk.gov.hmcts.reform.prl.clients.ccd.CcdCoreCaseDataService;
 import uk.gov.hmcts.reform.prl.clients.ccd.records.StartAllTabsUpdateDataContent;
 import uk.gov.hmcts.reform.prl.enums.CaseEvent;
+import uk.gov.hmcts.reform.prl.models.ccd.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.prl.services.extendedcasedataservice.ExtendedCaseDataService;
 import uk.gov.hmcts.reform.prl.services.tab.alltabs.AllTabServiceImpl;
+
+import java.util.Map;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Slf4j
 @RestController
 @RequestMapping("/case-access")
-@RequiredArgsConstructor
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class RestrictedCaseAccessController {
+    private final ExtendedCaseDataService caseDataService;
     private final AllTabServiceImpl allTabService;
     private final CcdCoreCaseDataService coreCaseDataService;
     private final ObjectMapper objectMapper;
@@ -49,7 +56,7 @@ public class RestrictedCaseAccessController {
         StartAllTabsUpdateDataContent startAllTabsUpdateDataContent =
             allTabService.getStartUpdateForSpecificEvent(
                 String.valueOf(callbackRequest.getCaseDetails().getId()),
-                CaseEvent.MARK_CASE_AS_RESTRICTED.getValue()
+                CaseEvent.CHANGE_CASE_ACCESS_AS_SYSUSER.getValue()
             );
 
         log.info("** markCaseAsRestricted event started");
@@ -87,7 +94,7 @@ public class RestrictedCaseAccessController {
         StartAllTabsUpdateDataContent startAllTabsUpdateDataContent =
             allTabService.getStartUpdateForSpecificEvent(
                 String.valueOf(callbackRequest.getCaseDetails().getId()),
-                CaseEvent.MARK_CASE_AS_PRIVATE.getValue()
+                CaseEvent.CHANGE_CASE_ACCESS_AS_SYSUSER.getValue()
             );
 
         log.info("** markCaseAsPrivate event started");
@@ -125,7 +132,7 @@ public class RestrictedCaseAccessController {
         StartAllTabsUpdateDataContent startAllTabsUpdateDataContent =
             allTabService.getStartUpdateForSpecificEvent(
                 String.valueOf(callbackRequest.getCaseDetails().getId()),
-                CaseEvent.MARK_CASE_AS_PUBLIC.getValue()
+                CaseEvent.CHANGE_CASE_ACCESS_AS_SYSUSER.getValue()
             );
 
         log.info("** markCaseAsPublic event started");
@@ -147,5 +154,30 @@ public class RestrictedCaseAccessController {
             true
         );
         log.info("** markCaseAsPublic submitUpdate done");
+    }
+
+    @PostMapping(path = "/change-case-access", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
+    @Operation(description = "Change case access")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Callback processed.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AboutToStartOrSubmitCallbackResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)})
+    @SecurityRequirement(name = "Bearer Authentication")
+    public AboutToStartOrSubmitCallbackResponse changeCaseAccess(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
+        @RequestBody CallbackRequest callbackRequest) throws JsonProcessingException {
+        log.info("Case details before:: " + objectMapper.writeValueAsString(callbackRequest));
+        Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
+        log.info("caseDataUpdated::" + caseDataUpdated);
+        Map<String, Object> dataClassification
+            = caseDataService.getDataClassification(String.valueOf(callbackRequest.getCaseDetails().getId()));
+        log.info("dataClassification::" + dataClassification);
+        AboutToStartOrSubmitCallbackResponse aboutToStartOrSubmitCallbackResponse = AboutToStartOrSubmitCallbackResponse.builder()
+            .data(caseDataUpdated)
+            .dataClassification(dataClassification)
+            .securityClassification("RESTRICTED")
+            .build();
+        log.info("Response after:: " + objectMapper.writeValueAsString(aboutToStartOrSubmitCallbackResponse));
+        return aboutToStartOrSubmitCallbackResponse;
     }
 }
