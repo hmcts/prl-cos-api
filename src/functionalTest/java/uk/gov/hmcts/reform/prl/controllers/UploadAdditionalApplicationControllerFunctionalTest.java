097fc82e9bbc65bde328e1cd3af95c939d312df7
Yogendra Upasani <yogendra.upasani@hmcts.net>
2024-06-12 19:08:21 +0100

    fixed build issue

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/UploadAdditionalApplicationControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/UploadAdditionalApplicationControllerFunctionalTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.web.context.WebApplicationContext;
 import uk.gov.hmcts.reform.prl.ResourceLoader;
