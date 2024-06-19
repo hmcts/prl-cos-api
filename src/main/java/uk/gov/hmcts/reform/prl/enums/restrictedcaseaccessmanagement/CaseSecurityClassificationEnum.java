bfddc8361323320ac403295f230713f3b5e89775
alokDatta <aloknath.datta@hmcts.net>
2024-06-19 01:39:38 +0100

    API changes

--- src/main/java/uk/gov/hmcts/reform/prl/enums/restrictedcaseaccessmanagement/CaseSecurityClassificationEnum.java
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/restrictedcaseaccessmanagement/CaseSecurityClassificationEnum.java
@@ -4,8 +4,23 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.RequiredArgsConstructor;
 import uk.gov.hmcts.reform.prl.enums.CustomEnumSerializer;
 
+import java.util.Arrays;
+
 @RequiredArgsConstructor
 @JsonSerialize(using = CustomEnumSerializer.class)
 public enum CaseSecurityClassificationEnum {
-   PUBLIC, PRIVATE, RESTRICTED
+    PUBLIC("public"), PRIVATE("private"), RESTRICTED("restricted");
+
+    private final String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public static CaseSecurityClassificationEnum fromValue(String value) {
+        return Arrays.stream(values())
+                .filter(event -> event.value.equals(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Unknown event name: " + value));
+    }
 }
