bb1a6b14ce5552ba98f1fb94f297a89f5dc04797
sairamkantheti1 <sai.kantheti@HMCTS.NET>
2024-06-27 12:06:53 +0100

    FPET-920 changes

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/SendAndReplyController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/SendAndReplyController.java
@@ -37,7 +37,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
