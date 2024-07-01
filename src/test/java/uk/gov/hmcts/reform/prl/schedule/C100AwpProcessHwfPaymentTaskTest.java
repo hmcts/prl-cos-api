ad5419bcae83195aec56cf78b043f2d5f380eb6f
sahaSonali <sonali.saha@hmcts.net>
2024-07-01 23:29:48 +0100

    Changes for HwF AWP processing- Cron job

---
+++ src/test/java/uk/gov/hmcts/reform/prl/schedule/C100AwpProcessHwfPaymentTaskTest.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.prl.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.prl.services.C100AwpProcessHwfPaymentService;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+
+@ExtendWith(SpringExtension.class)
+class C100AwpProcessHwfPaymentTaskTest {
+
+    @InjectMocks
+    C100AwpProcessHwfPaymentTask c100AwpProcessHwfPaymentTask;
+
+    @Mock
+    C100AwpProcessHwfPaymentService c100AwpProcessHwfPaymentService;
+
+    @Test
+    void runTaskWithHearingAwayDays() {
+        c100AwpProcessHwfPaymentTask.run();
+
+        verify(c100AwpProcessHwfPaymentService, times(1)).checkHwfPaymentStatusAndUpdateApplicationStatus();
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/prl/services/C100AwpProcessHwfPaymentServiceTest.java
@@ -0,0 +1,203 @@
+package uk.gov.hmcts.reform.prl.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.ccd.client.model.EventRequestData;
+import uk.gov.hmcts.reform.ccd.client.model.SearchResult;
+import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
+import uk.gov.hmcts.reform.prl.clients.PaymentApi;
+import uk.gov.hmcts.reform.prl.clients.ccd.records.StartAllTabsUpdateDataContent;
+import uk.gov.hmcts.reform.prl.enums.CaseCreatedBy;
+import uk.gov.hmcts.reform.prl.enums.uploadadditionalapplication.ApplicationStatus;
+import uk.gov.hmcts.reform.prl.enums.uploadadditionalapplication.PaymentStatus;
+import uk.gov.hmcts.reform.prl.models.Element;
+import uk.gov.hmcts.reform.prl.models.SearchResultResponse;
+import uk.gov.hmcts.reform.prl.models.complextypes.uploadadditionalapplication.AdditionalApplicationsBundle;
+import uk.gov.hmcts.reform.prl.models.complextypes.uploadadditionalapplication.C2DocumentBundle;
+import uk.gov.hmcts.reform.prl.models.complextypes.uploadadditionalapplication.OtherApplicationsBundle;
+import uk.gov.hmcts.reform.prl.models.complextypes.uploadadditionalapplication.Payment;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+import uk.gov.hmcts.reform.prl.models.dto.payment.ServiceRequestReferenceStatusResponse;
+import uk.gov.hmcts.reform.prl.services.tab.alltabs.AllTabServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.CASE_TYPE;
+import static uk.gov.hmcts.reform.prl.utils.ElementUtils.element;
+
+@Slf4j
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class C100AwpProcessHwfPaymentServiceTest {
+
+    private final String authToken = "authToken";
+    private final String s2sAuthToken = "s2sAuthToken";
+    private CaseDetails caseDetails;
+    private CaseData caseData;
+
+
+    @InjectMocks
+    private C100AwpProcessHwfPaymentService c100AwpProcessHwfPaymentService;
+
+    @Mock
+    ObjectMapper objectMapper;
+
+    @Mock
+    SystemUserService systemUserService;
+
+    @Mock
+    AuthTokenGenerator authTokenGenerator;
+
+    @Mock
+    CoreCaseDataApi coreCaseDataApi;
+
+    @Mock
+    AllTabServiceImpl allTabService;
+
+    @Mock
+    PaymentRequestService paymentRequestService;
+
+    @Mock
+    private PaymentApi paymentApi;
+
+
+    @Before
+    public void setUp() {
+        when(systemUserService.getSysUserToken()).thenReturn(authToken);
+        when(authTokenGenerator.generate()).thenReturn(s2sAuthToken);
+
+        AdditionalApplicationsBundle additionalApplicationsBundle1 = AdditionalApplicationsBundle.builder()
+            .payment(Payment.builder().status(PaymentStatus.HWF.getDisplayedValue())
+                         .paymentServiceRequestReferenceNumber("2024-12222222")
+                         .build())
+            .otherApplicationsBundle(OtherApplicationsBundle.builder()
+                                         .applicationStatus(ApplicationStatus.PENDING_ON_PAYMENT.getDisplayedValue())
+                                         .build())
+            .c2DocumentBundle(C2DocumentBundle.builder()
+                                  .applicationStatus(ApplicationStatus.PENDING_ON_PAYMENT.getDisplayedValue())
+                                  .build())
+            .build();
+
+        AdditionalApplicationsBundle additionalApplicationsBundle2 = AdditionalApplicationsBundle.builder()
+            .payment(Payment.builder().status(PaymentStatus.PAID.getDisplayedValue()).build())
+            .otherApplicationsBundle(OtherApplicationsBundle.builder().applicationStatus(ApplicationStatus.SUBMITTED.getDisplayedValue()).build())
+            .build();
+
+        AdditionalApplicationsBundle additionalApplicationsBundle3 = AdditionalApplicationsBundle.builder()
+            .payment(Payment.builder().status(PaymentStatus.PAID.getDisplayedValue()).build())
+            .c2DocumentBundle(C2DocumentBundle.builder().applicationStatus(ApplicationStatus.SUBMITTED.getDisplayedValue()).build())
+            .build();
+
+        AdditionalApplicationsBundle additionalApplicationsBundle4 = AdditionalApplicationsBundle.builder()
+            .payment(Payment.builder().status(PaymentStatus.HWF.getDisplayedValue())
+                         .paymentServiceRequestReferenceNumber("2024-12222223")
+                         .build())
+            .otherApplicationsBundle(OtherApplicationsBundle.builder()
+                                         .applicationStatus(ApplicationStatus.PENDING_ON_PAYMENT.getDisplayedValue())
+                                         .build())
+            .build();
+
+        AdditionalApplicationsBundle additionalApplicationsBundle5 = AdditionalApplicationsBundle.builder()
+            .payment(Payment.builder().status(PaymentStatus.HWF.getDisplayedValue())
+                         .paymentServiceRequestReferenceNumber("2024-12222224")
+                         .build())
+            .c2DocumentBundle(C2DocumentBundle.builder().applicationStatus(ApplicationStatus.PENDING_ON_PAYMENT.getDisplayedValue()).build())
+            .build();
+
+        List<Element<AdditionalApplicationsBundle>> additionalApplicationsBundle = new ArrayList<>();
+        additionalApplicationsBundle.add(element(additionalApplicationsBundle1));
+        additionalApplicationsBundle.add(element(additionalApplicationsBundle2));
+        additionalApplicationsBundle.add(element(additionalApplicationsBundle3));
+        additionalApplicationsBundle.add(element(additionalApplicationsBundle4));
+        additionalApplicationsBundle.add(element(additionalApplicationsBundle5));
+
+
+        caseData = CaseData.builder()
+            .id(123L)
+            .caseCreatedBy(CaseCreatedBy.CITIZEN)
+            .additionalApplicationsBundle(additionalApplicationsBundle)
+            .build();
+
+        caseDetails = CaseDetails.builder()
+            .id(123L)
+            .data(caseData.toMap(objectMapper))
+            .build();
+
+        SearchResult searchResult = SearchResult.builder()
+            .total(1)
+            .cases(List.of(caseDetails))
+            .build();
+        when(coreCaseDataApi.searchCases(authToken, s2sAuthToken, CASE_TYPE, null)).thenReturn(searchResult);
+
+        SearchResultResponse response = SearchResultResponse.builder()
+            .total(1)
+            .cases(List.of(caseDetails))
+            .build();
+        when(objectMapper.convertValue(searchResult, SearchResultResponse.class)).thenReturn(response);
+
+        when(objectMapper.convertValue(caseDetails.getData(), CaseData.class)).thenReturn(caseData);
+
+
+        StartAllTabsUpdateDataContent startAllTabsUpdateDataContent = new StartAllTabsUpdateDataContent(s2sAuthToken,
+                                                                                                        EventRequestData.builder().build(),
+                                                                                                        StartEventResponse.builder().build(),
+                                                                                                        caseData.toMap(
+                                                                                                            objectMapper),
+                                                                                                        caseData, null
+        );
+        when(allTabService.getStartUpdateForSpecificEvent(any(), any())).thenReturn(startAllTabsUpdateDataContent);
+        when(allTabService.submitAllTabsUpdate(
+            anyString(),
+            anyString(),
+            any(),
+            any(),
+            any()
+        )).thenReturn(CaseDetails.builder().build());
+
+        ServiceRequestReferenceStatusResponse serviceRequestReferenceStatusResponse = ServiceRequestReferenceStatusResponse.builder()
+            .serviceRequestStatus("Paid")
+            .build();
+        when(paymentRequestService.fetchServiceRequestReferenceStatus(anyString(), anyString()))
+            .thenReturn(serviceRequestReferenceStatusResponse);
+    }
+
+    @Test
+    public void testCheckHwfPaymentStatusAndUpdateApplicationStatus() {
+
+        c100AwpProcessHwfPaymentService.checkHwfPaymentStatusAndUpdateApplicationStatus();
+        verify(paymentRequestService, times(3))
+            .fetchServiceRequestReferenceStatus(anyString(), anyString());
+
+    }
+
+    @Test
+    public void testCheckHwfPaymentStatusAndUpdateCaseStateWithEmptyList() {
+        SearchResult searchResult = SearchResult.builder()
+            .total(0)
+            .build();
+        when(coreCaseDataApi.searchCases(authToken, s2sAuthToken, CASE_TYPE, null)).thenReturn(searchResult);
+        when(objectMapper.convertValue(searchResult, SearchResultResponse.class)).thenReturn(null);
+        when(objectMapper.convertValue(caseDetails.getData(), CaseData.class)).thenReturn(caseData);
+
+        c100AwpProcessHwfPaymentService.checkHwfPaymentStatusAndUpdateApplicationStatus();
+
+        //verify
+        verify(paymentRequestService, times(0))
+            .fetchServiceRequestReferenceStatus(anyString(), anyString());
+    }
+
+}
