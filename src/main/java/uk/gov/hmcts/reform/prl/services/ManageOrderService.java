86dbf4f6d861833ce9a96b58549185ef3b2a4a83
velmurugan-mani <velmurugan.mani@hmcts.net>
2024-07-04 09:21:40 +0100

    PRL-5604: loggedInUserType Update for all the user

--- src/main/java/uk/gov/hmcts/reform/prl/services/ManageOrderService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ManageOrderService.java
@@ -3142,10 +3142,11 @@ public class ManageOrderService {
         }
 
         //For Automated Hearing Check for Judge
+        /*
         String loggedInUserType = getLoggedInUserType(authorisation);
         log.info("isAutomatedHearingPresent: loggedInUserType: {}", loggedInUserType);
         caseDataUpdated.put("isAutomatedHearingPresent", UserRoles.JUDGE.name().equals(loggedInUserType) ? Yes : No);
-        log.info("isAutomatedHearingPresent: caseDataUpdated: {}", caseDataUpdated.get("isAutomatedHearingPresent"));
+        log.info("isAutomatedHearingPresent: caseDataUpdated: {}", caseDataUpdated.get("isAutomatedHearingPresent"));*/
     }
 
     public HearingData getHearingData(String authorization,
