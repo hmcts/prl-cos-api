703d78a0999d495f7a9c9d082f450f15d7b5cb95
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2024-08-05 15:17:50 +0100

    Try

--- src/main/java/uk/gov/hmcts/reform/prl/constants/PrlAppsConstants.java
+++ src/main/java/uk/gov/hmcts/reform/prl/constants/PrlAppsConstants.java
@@ -1031,5 +1031,5 @@ public class PrlAppsConstants {
 
     //Citizen dashboard notifications
     public static final String CAN_10_FM5 = "CAN_10";
-    public static final String CLIENT_CONTEXT_HEADER_PARAMETER = "Client-Context";
+    public static final String CLIENT_CONTEXT_HEADER_PARAMETER = "client-context";
 }
