d3605952060170fe85a79a7f9bf89a18e8bf0a71
shashi kumar <shashikumar.sannakariyappa@cognizant.com>
2024-08-07 13:11:05 +0100

    log

--- src/main/java/uk/gov/hmcts/reform/prl/services/DraftAnOrderService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/DraftAnOrderService.java
@@ -661,7 +661,8 @@ public class DraftAnOrderService {
     public Map<String, Object> populateDraftOrderDocument(CaseData caseData, String authorization, String draftOrderId) {
         Map<String, Object> caseDataMap = new HashMap<>();
         DraftOrder selectedOrder;
-        if (null != draftOrderId) {
+        if (caseData.getApplicantCaseName().equalsIgnoreCase("C100 WA LINKING")) {
+            log.info("***inside the logic***");
             selectedOrder = CaseUtils.getDraftOrderFromCollectionId(caseData.getDraftOrderCollection(), draftOrderId);
         } else {
             selectedOrder = getSelectedDraftOrderDetails(
