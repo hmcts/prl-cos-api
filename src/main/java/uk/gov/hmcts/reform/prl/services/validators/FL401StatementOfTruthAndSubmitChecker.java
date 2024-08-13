0123e2bbda62e557297fd7a677d842111c2e20f6
hcarslaw <Hugh.Carslaw@HMCTS.NET>
2024-08-13 17:52:40 +0100

    FPET-1104: Make eventsChecker field final

--- src/main/java/uk/gov/hmcts/reform/prl/services/validators/FL401StatementOfTruthAndSubmitChecker.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/validators/FL401StatementOfTruthAndSubmitChecker.java
@@ -34,7 +34,7 @@ import static uk.gov.hmcts.reform.prl.enums.Event.WITHOUT_NOTICE_ORDER;
 public class FL401StatementOfTruthAndSubmitChecker implements EventChecker {
 
     @Lazy
-    private EventsChecker eventsChecker;
+    private final EventsChecker eventsChecker;
 
     @Override
     public boolean isFinished(CaseData caseData) {
