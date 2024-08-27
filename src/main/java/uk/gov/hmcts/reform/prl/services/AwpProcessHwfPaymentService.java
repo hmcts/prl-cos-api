1e5971d0bfe83e8efe551d307b1851a442b6ff86
sahaSonali <sonali.saha@hmcts.net>
2024-08-27 12:56:18 +0100

    AwP - HwF Cron job changes for DA cases

--- src/main/java/uk/gov/hmcts/reform/prl/services/AwpProcessHwfPaymentService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/AwpProcessHwfPaymentService.java
@@ -103,7 +103,7 @@ public class AwpProcessHwfPaymentService {
         if (isNotEmpty(processedApplicationIds)) {
             Map<String, Object> caseDataUpdated = new HashMap<>();
             for (UUID processedApplicationId : processedApplicationIds) {
-                log.info("All AwP HwF Payment completed Application Ids " + processedApplicationIds);
+                log.info("Updating AwP payment & application status for application id " + processedApplicationId);
                 caseData.getAdditionalApplicationsBundle().stream().filter(additionalApplicationsBundleElement ->
                                                                                additionalApplicationsBundleElement.getId().equals(
                                                                                    processedApplicationId))
