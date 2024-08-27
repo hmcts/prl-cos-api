d3eebfedbc2c784ac74e6d3c60c2dcd73844b9b7
hcarslaw <Hugh.Carslaw@HMCTS.NET>
2024-08-27 14:26:52 +0100

    FPET-1094: Update FP25 fee

--- src/main/java/uk/gov/hmcts/reform/prl/models/FeeType.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/FeeType.java
@@ -29,7 +29,7 @@ public enum FeeType {
     public static final Map<String, FeeType> applicationToFeeMapForCitizen = Map.ofEntries(
         Map.entry("C100_FP25_APPLICANT", FP25_WITNESS_SUMMONS),
         Map.entry("C100_FP25_RESPONDENT", FP25_WITNESS_SUMMONS),
-        Map.entry("FL401_FP25_APPLICANT", FP25_WITNESS_SUMMONS),
+        Map.entry("FL401_FP25_APPLICANT", NO_FEE),
         Map.entry("FL401_FP25_RESPONDENT", FP25_WITNESS_SUMMONS),
 
         Map.entry("C100_N161_APPLICANT", N161_APPELLANT_NOTICE_CA),
