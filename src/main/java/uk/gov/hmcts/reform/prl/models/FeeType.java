506f32151b357f49d39926a87924796a764f1806
sahaSonali <sonali.saha@hmcts.net>
2024-08-28 12:39:26 +0100

    Changes for PRL-6181

--- src/main/java/uk/gov/hmcts/reform/prl/models/FeeType.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/FeeType.java
@@ -29,7 +29,7 @@ public enum FeeType {
     public static final Map<String, FeeType> applicationToFeeMapForCitizen = Map.ofEntries(
         Map.entry("C100_FP25_APPLICANT", FP25_WITNESS_SUMMONS),
         Map.entry("C100_FP25_RESPONDENT", FP25_WITNESS_SUMMONS),
-        Map.entry("FL401_FP25_APPLICANT", FP25_WITNESS_SUMMONS),
+        Map.entry("FL401_FP25_APPLICANT", NO_FEE),//Changed as part of PRL-6181
         Map.entry("FL401_FP25_RESPONDENT", FP25_WITNESS_SUMMONS),
 
         Map.entry("C100_N161_APPLICANT", N161_APPELLANT_NOTICE_CA),
