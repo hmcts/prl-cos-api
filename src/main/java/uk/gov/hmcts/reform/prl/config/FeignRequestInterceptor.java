b741831bda6941de6ae5fccb3bbea125d901e56b
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2024-08-30 10:19:37 +0100

    pact fixes

--- src/main/java/uk/gov/hmcts/reform/prl/config/FeignRequestInterceptor.java
+++ src/main/java/uk/gov/hmcts/reform/prl/config/FeignRequestInterceptor.java
@@ -2,28 +2,20 @@ package uk.gov.hmcts.reform.prl.config;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
-import org.slf4j.MDC;
 
-import java.util.UUID;
 import java.util.function.Supplier;
 
 public class FeignRequestInterceptor implements RequestInterceptor {
 
     private Supplier<String> nextRequestId;
 
-    public static final String REQUEST_ID = "Request-Id";
-    public static final String ROOT_REQUEST_ID = "Root-Request-Id";
-    public static final String ORIGIN_REQUEST_ID = "Origin-Request-Id";
-
     @Override
     public void apply(RequestTemplate template) {
-        template.header(REQUEST_ID, nextRequestId.get());
-        template.header(ROOT_REQUEST_ID, MDC.get("rootRequestId"));
-        template.header(ORIGIN_REQUEST_ID, MDC.get("requestId"));
+
     }
 
     public FeignRequestInterceptor() {
-        this(UUID.randomUUID()::toString);
+        //this(RequestIdGenerator::next);
     }
 
     private FeignRequestInterceptor(Supplier<String> nextRequestId) {
