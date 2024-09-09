e3531f55e262c7689c475b58972380f30f7cb599
sahaSonali <sonali.saha@hmcts.net>
2024-09-09 12:09:08 +0100

    fix for courtnav uploaded doc

--- src/main/java/uk/gov/hmcts/reform/prl/services/managedocuments/ManageDocumentsService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/managedocuments/ManageDocumentsService.java
@@ -302,6 +302,7 @@ public class ManageDocumentsService {
         QuarantineLegalDoc finalQuarantineDocument = objectMapper.convertValue(hashMap, QuarantineLegalDoc.class);
         return finalQuarantineDocument.toBuilder()
             .documentParty(quarantineLegalDoc.getDocumentParty())
+            .documentType(quarantineLegalDoc.getDocumentType())
             .documentUploadedDate(quarantineLegalDoc.getDocumentUploadedDate())
             .categoryId(quarantineLegalDoc.getCategoryId())
             .categoryName(quarantineLegalDoc.getCategoryName())
