21712927e29c248d4bb349e237a8e73bc5383b91
hcarslaw <119344120+hcarslaw@users.noreply.github.com>
2024-09-10 11:26:51 +0100

    FPET-1094: Update FP25 fee (#2661)
    * FPET-1094: Update FP25 fee
    
    * Update values.preview.template.yaml
    
    * FPET-1094: Update FP25 fee
    
    * FPET-1094: Fix sonar issue
    
    * FPET-1094: Add test cases
    
    ---------
    
    Co-authored-by: sairamkantheti1 <101887377+sairamkantheti1@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/prl/models/FeeType.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/FeeType.java
@@ -20,7 +20,6 @@ public enum FeeType {
     NO_FEE;
 
     public static final Map<String, FeeType> applicationToFeeMap = Map.of(
-        "FP25_WITNESS_SUMMONS", FP25_WITNESS_SUMMONS,
         "C1_CHILD_ORDER", CHILD_ARRANGEMENTS_ORDER,
         "C3_CHILD_ORDER", C3_SEARCH_AND_TAKING_CHARGE_OF_A_CHILD,
         "C4_CHILD_ORDER", C4_WHEREABOUTS_OF_A_MISSING_CHILD
