a2a186e1c33a2fc7beef5877115bb9adccf9a607
sahaSonali <sonali.saha@hmcts.net>
2024-09-16 20:34:38 +0100

    Closing cases changes

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/closingcase/DateFinalDecisionWasMade.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/closingcase/DateFinalDecisionWasMade.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.prl.models.complextypes.closingcase;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
@@ -11,7 +10,6 @@ import java.time.LocalDateTime;
 
 @Data
 @Builder(toBuilder = true)
-@AllArgsConstructor
 public class DateFinalDecisionWasMade {
     @JsonProperty("finalDecisionDate")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
