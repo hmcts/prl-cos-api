6d1e2246e06c05b1ce77eca49df75d41ab74d65d
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2024-09-20 16:02:43 +0100

    Update PrlAppsConstants.java

--- src/main/java/uk/gov/hmcts/reform/prl/constants/PrlAppsConstants.java
+++ src/main/java/uk/gov/hmcts/reform/prl/constants/PrlAppsConstants.java
@@ -1031,7 +1031,6 @@ public class PrlAppsConstants {
     //Citizen dashboard notifications
     public static final String CAN_10_FM5 = "CAN_10";
 
-    public static final String LINE_SEPERATOR = "\n"
-        + "System.lineSeparator()";
+    public static final String LINE_SEPERATOR = "\n";
 
 }
