49abc4b2ee51b5268b57a93024cace786bda0e6a
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2024-09-20 17:51:35 +0100

    removed line seperator

--- src/main/java/uk/gov/hmcts/reform/prl/constants/PrlAppsConstants.java
+++ src/main/java/uk/gov/hmcts/reform/prl/constants/PrlAppsConstants.java
@@ -1031,6 +1031,4 @@ public class PrlAppsConstants {
     //Citizen dashboard notifications
     public static final String CAN_10_FM5 = "CAN_10";
 
-    public static final String LINE_SEPERATOR = "\n\n\n" + "  s  " + "\n\n\n";
-
 }
