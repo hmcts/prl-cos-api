df6ae2f022dd2553e87ad58bf11d6e674b5eee3a
alokDatta <aloknath.datta@hmcts.net>
2024-10-16 12:42:48 +0100

    Revert "Respondent solicitor change"
    This reverts commit ef7053087454a056f08a647b1c31296c9ec38533.

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/common/CitizenDetails.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/common/CitizenDetails.java
@@ -4,9 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.models.Address;
-import uk.gov.hmcts.reform.prl.models.documents.Document;
 
 import java.time.LocalDate;
 
@@ -21,8 +19,6 @@ public class CitizenDetails {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private final LocalDate dateOfBirth;
     private final String placeOfBirth;
-    private YesOrNo liveInRefuge;
-    private Document refugeConfidentialityC8Form;
     private final Address address;
     private AddressHistory addressHistory;
     private Contact contact;
