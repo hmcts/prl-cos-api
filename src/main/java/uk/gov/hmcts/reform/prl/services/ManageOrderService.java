1c61ea8729fe8425be8f987d76153cdeee648c0d
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2024-10-22 10:39:45 +0100

    removed extra space

--- src/main/java/uk/gov/hmcts/reform/prl/services/ManageOrderService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ManageOrderService.java
@@ -1038,7 +1038,6 @@ public class ManageOrderService {
                                    .isOrderUploaded(Yes)
                                    .doesOrderDocumentNeedSeal(Yes)
                                    .build()));
-
         return newOrderDetails;
     }
 
