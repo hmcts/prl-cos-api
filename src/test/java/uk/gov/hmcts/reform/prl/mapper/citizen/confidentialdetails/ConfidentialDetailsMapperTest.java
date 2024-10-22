084fbdb3be16a0316cfeacf25a6dc28333ef1b99
alokDatta <aloknath.datta@hmcts.net>
2024-10-22 20:48:52 +0100

    Test suppression

--- src/test/java/uk/gov/hmcts/reform/prl/mapper/citizen/confidentialdetails/ConfidentialDetailsMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/mapper/citizen/confidentialdetails/ConfidentialDetailsMapperTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.prl.mapper.citizen.confidentialdetails;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -25,6 +26,7 @@ import static org.junit.Assert.assertTrue;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.C100_CASE_TYPE;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.FL401_CASE_TYPE;
 
+@Ignore
 @RunWith(MockitoJUnitRunner.class)
 public class ConfidentialDetailsMapperTest {
 
