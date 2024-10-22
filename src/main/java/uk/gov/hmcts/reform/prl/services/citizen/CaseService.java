2089a663bcd9765201967b2501654e155b1dc082
BharadwajSairam <75499453+BharadwajSairam@users.noreply.github.com>
2024-10-22 19:35:15 +0100

    remove jsoninclude for nonnull fields (#2809)
    * remove jsoninclude for nonnull fields
    
    * Update CaseService.java

--- src/main/java/uk/gov/hmcts/reform/prl/services/citizen/CaseService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/citizen/CaseService.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.prl.services.citizen;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -229,8 +228,6 @@ public class CaseService {
 
         UserDetails userDetails = idamClient.getUserDetails(authToken);
         List<CaseDetails> caseDetails = new ArrayList<>(performSearch(authToken, userDetails, searchCriteria, s2sToken));
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
         return caseDetails
             .stream()
             .map(caseDetail -> CaseUtils.getCaseData(caseDetail, objectMapper))
