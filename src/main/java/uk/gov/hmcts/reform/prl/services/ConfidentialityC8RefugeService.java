9d463a214532bb1393fdd7f44d8a87efd8b0ad5c
alokDatta <aloknath.datta@hmcts.net>
2024-10-30 22:54:06 +0000

    Respondent Lip Bug fixes

--- src/main/java/uk/gov/hmcts/reform/prl/services/ConfidentialityC8RefugeService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ConfidentialityC8RefugeService.java
@@ -416,13 +416,7 @@ public class ConfidentialityC8RefugeService {
     public RefugeConfidentialDocumentsRecord processC8RefugeDocumentsOnAmendForC100(CaseData caseDataBefore, CaseData caseData, String eventId) {
         log.info("Inside processC8RefugeDocumentsOnAmendForC100");
         boolean onlyForApplicant = CaseEvent.AMEND_APPLICANTS_DETAILS.getValue().equalsIgnoreCase(eventId);
-        boolean onlyForRespondent =
-            CaseEvent.AMEND_RESPONDENTS_DETAILS.getValue().equalsIgnoreCase(eventId)
-            || CaseEvent.RESPONDENT_SOLICITOR_A_SUBMIT.getValue().equalsIgnoreCase(eventId)
-            || CaseEvent.RESPONDENT_SOLICITOR_B_SUBMIT.getValue().equalsIgnoreCase(eventId)
-            || CaseEvent.RESPONDENT_SOLICITOR_C_SUBMIT.getValue().equalsIgnoreCase(eventId)
-            || CaseEvent.RESPONDENT_SOLICITOR_D_SUBMIT.getValue().equalsIgnoreCase(eventId)
-            || CaseEvent.RESPONDENT_SOLICITOR_E_SUBMIT.getValue().equalsIgnoreCase(eventId);
+        boolean onlyForRespondent = CaseEvent.AMEND_RESPONDENTS_DETAILS.getValue().equalsIgnoreCase(eventId);
         boolean onlyForOtherPeople = CaseEvent.AMEND_OTHER_PEOPLE_IN_THE_CASE_REVISED.getValue().equalsIgnoreCase(
             eventId);
         RefugeConfidentialDocumentsRecord refugeConfidentialDocumentsRecord;
