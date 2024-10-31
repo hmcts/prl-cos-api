6dc7849319da9c3c3ea3272db87f96ed88e86aa6
JamesCuttsHMCTS <James.Cutts1@HMCTS.NET>
2024-10-31 15:35:25 +0000

    Added additional coverage to confidentialityC8RefugeService

--- src/test/java/uk/gov/hmcts/reform/prl/services/ConfidentialityC8RefugeServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/ConfidentialityC8RefugeServiceTest.java
@@ -12,14 +12,19 @@ import uk.gov.hmcts.reform.prl.models.Element;
 import uk.gov.hmcts.reform.prl.models.complextypes.PartyDetails;
 import uk.gov.hmcts.reform.prl.models.complextypes.citizen.Response;
 import uk.gov.hmcts.reform.prl.models.complextypes.citizen.common.CitizenDetails;
+import uk.gov.hmcts.reform.prl.models.complextypes.refuge.RefugeConfidentialDocuments;
+import uk.gov.hmcts.reform.prl.models.documents.Document;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+import uk.gov.hmcts.reform.prl.models.refuge.RefugeConfidentialDocumentsRecord;
+import uk.gov.hmcts.reform.prl.models.refuge.RefugeDocumentHandlerParameters;
 
 import java.time.LocalDate;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.C100_CASE_TYPE;
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.FL401_CASE_TYPE;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ConfidentialityC8RefugeServiceTest {
@@ -133,6 +138,50 @@ public class ConfidentialityC8RefugeServiceTest {
     }
 
     @Test
+    public void testRefugeCleanupWithoutAddress() {
+
+        refugePartyDetails1 = refugePartyDetails1.toBuilder()
+            .liveInRefuge(YesOrNo.No)
+            .isCurrentAddressKnown(YesOrNo.No)
+            .build();
+        Element<PartyDetails> wrappedApplicants = Element.<PartyDetails>builder().value(refugePartyDetails1).build();
+        List<Element<PartyDetails>> partyDetailsWrappedList = Collections.singletonList(wrappedApplicants);
+
+        HashMap<String, Object> updatedCaseData = new HashMap<>();
+        confidentialityC8RefugeService.processForcePartiesConfidentialityIfLivesInRefugeForC100(
+            Optional.of(partyDetailsWrappedList),
+            updatedCaseData,
+            " ",
+            true
+        );
+
+        assertTrue(updatedCaseData.containsKey(" "));
+
+    }
+
+    @Test
+    public void testRefugeCleanupFalseWithoutAddress() {
+
+        refugePartyDetails1 = refugePartyDetails1.toBuilder()
+            .liveInRefuge(YesOrNo.No)
+            .isCurrentAddressKnown(YesOrNo.No)
+            .build();
+        Element<PartyDetails> wrappedApplicants = Element.<PartyDetails>builder().value(refugePartyDetails1).build();
+        List<Element<PartyDetails>> partyDetailsWrappedList = Collections.singletonList(wrappedApplicants);
+
+        HashMap<String, Object> updatedCaseData = new HashMap<>();
+        confidentialityC8RefugeService.processForcePartiesConfidentialityIfLivesInRefugeForC100(
+            Optional.of(partyDetailsWrappedList),
+            updatedCaseData,
+            " ",
+            false
+        );
+
+        assertTrue(updatedCaseData.containsKey(" "));
+
+    }
+
+    @Test
     public void processForcePartiesConfidentialityIfLivesInRefugeForFL401Null() {
         Optional<PartyDetails> partyDetails = Optional.empty();
         confidentialityC8RefugeService.processForcePartiesConfidentialityIfLivesInRefugeForFL401(
@@ -171,6 +220,40 @@ public class ConfidentialityC8RefugeServiceTest {
     }
 
     @Test
+    public void processForcePartiesConfidentialityIfLivesInRefugeForFL401WithKnownAddress() {
+        Optional<PartyDetails> partyDetails = Optional.ofNullable(PartyDetails
+            .builder()
+            .liveInRefuge(YesOrNo.Yes)
+            .isCurrentAddressKnown(YesOrNo.Yes)
+            .build());
+        HashMap<String, Object> updatedCaseData = new HashMap<>();
+        confidentialityC8RefugeService.processForcePartiesConfidentialityIfLivesInRefugeForFL401(
+            partyDetails,
+            updatedCaseData,
+            "applicant",
+            false
+        );
+        assertTrue(true);
+    }
+
+    @Test
+    public void processForcePartiesConfidentialityIfLivesInRefugeForFL401WithKnownAddressCleanUpFalse() {
+        Optional<PartyDetails> partyDetails = Optional.ofNullable(PartyDetails
+            .builder()
+            .liveInRefuge(YesOrNo.Yes)
+            .isCurrentAddressKnown(YesOrNo.No)
+            .build());
+        HashMap<String, Object> updatedCaseData = new HashMap<>();
+        confidentialityC8RefugeService.processForcePartiesConfidentialityIfLivesInRefugeForFL401(
+            partyDetails,
+            updatedCaseData,
+            "applicant",
+            true
+        );
+        assertTrue(true);
+    }
+
+    @Test
     public void processForcePartiesConfidentialityIfLivesInRefugeForFL401WithResponse() {
         Optional<PartyDetails> partyDetails = Optional.ofNullable(PartyDetails.builder()
                                                                       .liveInRefuge(YesOrNo.No)
@@ -201,6 +284,64 @@ public class ConfidentialityC8RefugeServiceTest {
             true
         );
         assertTrue(true);
+    }
+
+    @Test
+    public void testListRefugeDocumentsForConfidentialTabC100() {
+        PartyDetails applicant = PartyDetails
+            .builder()
+            .liveInRefuge(YesOrNo.Yes)
+            .refugeConfidentialityC8Form(Document.builder().build())
+            .build();
+        Element<PartyDetails> wrappedApplicant = Element.<PartyDetails>builder().value(applicant).build();
+        List<Element<PartyDetails>> applicantList = new ArrayList<>();
+        applicantList.add(wrappedApplicant);
+        CaseData caseData = CaseData
+            .builder()
+            .caseTypeOfApplication(C100_CASE_TYPE)
+            .applicants(applicantList)
+            .build();
+        RefugeDocumentHandlerParameters refugeDocumentHandlerParameters = RefugeDocumentHandlerParameters
+            .builder().forAllParties(true)
+            .build();
+        RefugeConfidentialDocuments refugeConfidentialDocuments = RefugeConfidentialDocuments.builder().build();
+        Element<RefugeConfidentialDocuments> element = Element.<RefugeConfidentialDocuments>builder().value(refugeConfidentialDocuments).build();
+        List<Element<RefugeConfidentialDocuments>> list = new ArrayList<>();
+        list.add(element);
+        RefugeConfidentialDocumentsRecord refugeConfidentialDocumentsRecord = new RefugeConfidentialDocumentsRecord(list, list);
+        RefugeConfidentialDocumentsRecord returnedRefuge = confidentialityC8RefugeService
+            .listRefugeDocumentsForConfidentialTab(caseData, refugeDocumentHandlerParameters, refugeConfidentialDocumentsRecord);
+        assertNotNull(returnedRefuge);
+    }
+
+    @Test
+    public void testListRefugeDocumentsForConfidentialTabFL401() {
+        CaseData caseData = CaseData
+            .builder()
+            .caseTypeOfApplication(FL401_CASE_TYPE)
+            .build();
+        RefugeDocumentHandlerParameters refugeDocumentHandlerParameters = new RefugeDocumentHandlerParameters();
+        RefugeConfidentialDocuments refugeConfidentialDocuments = RefugeConfidentialDocuments.builder().build();
+        Element<RefugeConfidentialDocuments> element = Element.<RefugeConfidentialDocuments>builder().value(refugeConfidentialDocuments).build();
+        RefugeConfidentialDocumentsRecord refugeConfidentialDocumentsRecord = new RefugeConfidentialDocumentsRecord(List.of(element), List.of(element));
+        RefugeConfidentialDocumentsRecord returnedRefuge = confidentialityC8RefugeService
+            .listRefugeDocumentsForConfidentialTab(caseData, refugeDocumentHandlerParameters, refugeConfidentialDocumentsRecord);
+        assertNotNull(returnedRefuge);
+    }
+
+    @Test
+    public void testListRefugeDocumentsForConfidentialTabNoCaseType() {
+        CaseData caseData = CaseData
+            .builder()
+            .build();
+        RefugeDocumentHandlerParameters refugeDocumentHandlerParameters = new RefugeDocumentHandlerParameters();
+        RefugeConfidentialDocuments refugeConfidentialDocuments = RefugeConfidentialDocuments.builder().build();
+        Element<RefugeConfidentialDocuments> element = Element.<RefugeConfidentialDocuments>builder().value(refugeConfidentialDocuments).build();
+        RefugeConfidentialDocumentsRecord refugeConfidentialDocumentsRecord = new RefugeConfidentialDocumentsRecord(List.of(element), List.of(element));
+        RefugeConfidentialDocumentsRecord returnedRefuge = confidentialityC8RefugeService
+            .listRefugeDocumentsForConfidentialTab(caseData, refugeDocumentHandlerParameters, refugeConfidentialDocumentsRecord);
+
+        assertNotNull(returnedRefuge);
     }
 
     @Test
