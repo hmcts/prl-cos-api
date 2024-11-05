e12a0e96e506aecb20225bef835096711e1699b0
praneethhm <101885945+praneethhm@users.noreply.github.com>
2024-11-05 11:48:47 +0000

    Revert "Fixed functional test case failures"
    This reverts commit 983c7a12c78e6de204e598546ac52b5a74ce53f8.

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/CaseWithdrawnRequestControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/CaseWithdrawnRequestControllerFunctionalTest.java
@@ -4,28 +4,25 @@ import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.junit.Assert;
-import org.junit.Ignore;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.prl.ResourceLoader;
 import uk.gov.hmcts.reform.prl.utils.IdamTokenGenerator;
 import uk.gov.hmcts.reform.prl.utils.ServiceAuthenticationGenerator;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.notNullValue;
-
 
 @Slf4j
 @SpringBootTest
+@RunWith(SpringRunner.class)
 @ContextConfiguration
-public class AddCafcassOfficerControllerFunctionalTest {
+public class CaseWithdrawnRequestControllerFunctionalTest {
 
-    private static final String VALID_REQUEST_BODY = "requests/add-cafcass-officer.json";
 
     @Autowired
     protected IdamTokenGenerator idamTokenGenerator;
@@ -33,40 +30,32 @@ public class AddCafcassOfficerControllerFunctionalTest {
     @Autowired
     protected ServiceAuthenticationGenerator serviceAuthenticationGenerator;
 
-    private static final String C100_SEND_TO_GATEKEEPERJUDGE = "requests/call-back-controller-send-to-gatekeeperForJudge.json";
 
+    private static final String VALID_REQUEST_BODY = "requests/call-back-controller.json";
 
     private final String targetInstance =
         StringUtils.defaultIfBlank(
             System.getenv("TEST_URL"),
             "http://localhost:4044"
         );
-
     private final RequestSpecification request = RestAssured.given().relaxedHTTPSValidation().baseUri(targetInstance);
 
     @Test
-    public void givenRequestBody_whenAdd_cafcass_officer_about_to_submit_then200Response() throws Exception {
+    public void givenRequestBody_whenIssue_and_send_to_local_court_then200Response() throws Exception {
         String requestBody = ResourceLoader.loadJson(VALID_REQUEST_BODY);
-
-        AboutToStartOrSubmitCallbackResponse response =  request
+        request
             .header("Authorization", idamTokenGenerator.generateIdamTokenForCafcass())
             .header("ServiceAuthorization", serviceAuthenticationGenerator.generateTokenForCcd())
             .body(requestBody)
             .when()
             .contentType("application/json")
-            .post("/add-cafcass-officer/about-to-submit")
+            .post("/case-withdrawn-email-notification")
             .then()
             .assertThat().statusCode(200)
-            .body("data.caseStatus.state", equalTo("Submitted"),
-                  "data.caseTypeOfApplication", equalTo("C100"),
-                  "data.newChildDetails.list_items", hasSize(1),
-                  "data.newChildDetails[0].value.cafcassOfficerName", notNullValue(),
-                  "data.newChildDetails[0].value.cafcassOfficerPhoneNo", notNullValue(),
-                  "data.newChildDetails[0].value.cafcassOfficerEmailAddress", notNullValue()
-            )
-            .extract()
-            .as(AboutToStartOrSubmitCallbackResponse.class);
+            .body("confirmation_header", equalTo("# Application withdrawn"),
+                  "confirmation_body", notNullValue()
+            );
 
-        Assert.assertNotNull(response.getData().get("newChildDetails"));
     }
+
 }
