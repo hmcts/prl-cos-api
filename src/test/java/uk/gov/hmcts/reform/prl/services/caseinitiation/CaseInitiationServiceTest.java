bf26e160c6123aa5f795e8b753bc0629e9260a24
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2024-11-14 15:20:26 +0000

    FPET-1130 (#2757)
    * Update values.preview.template.yaml
    
    * added a controller for generate court list
    
    * added endpoint for populating court list
    
    * added test cases
    
    * Update CaseInitiationService.java
    
    * added FPET-1054 changes
    
    ---------
    
    Co-authored-by: praneethhm <101885945+praneethhm@users.noreply.github.com>
    Co-authored-by: priteshkr <101888106+priteshkr@users.noreply.github.com>

--- src/test/java/uk/gov/hmcts/reform/prl/services/caseinitiation/CaseInitiationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/caseinitiation/CaseInitiationServiceTest.java
@@ -3,19 +3,23 @@ package uk.gov.hmcts.reform.prl.services.caseinitiation;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.prl.models.Element;
+import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicListElement;
 import uk.gov.hmcts.reform.prl.models.complextypes.PartyDetails;
 import uk.gov.hmcts.reform.prl.models.complextypes.citizen.User;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.services.EventService;
+import uk.gov.hmcts.reform.prl.services.LocationRefDataService;
 import uk.gov.hmcts.reform.prl.services.caseaccess.AssignCaseAccessService;
 
 import java.util.HashMap;
@@ -28,6 +32,8 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.C100_CASE_TYPE;
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.FL401_CASE_TYPE;
+import static uk.gov.hmcts.reform.prl.services.caseinitiation.CaseInitiationService.COURT_LIST;
 
 @RunWith(MockitoJUnitRunner.Silent.class)
 public class CaseInitiationServiceTest {
@@ -46,6 +52,9 @@ public class CaseInitiationServiceTest {
     private CoreCaseDataApi coreCaseDataApi;
     @Mock
     private AssignCaseAccessService assignCaseAccessService;
+
+    @Mock
+    private LocationRefDataService locationRefDataService;
 
     private CaseData caseData;
     private CaseDetails caseDetails;
@@ -73,6 +82,7 @@ public class CaseInitiationServiceTest {
             .user(User.builder().email("").idamId("").build())
             .build();
 
+        caseDataMap.put("caseTypeOfApplication",C100_CASE_TYPE);
         caseData = CaseData.builder()
             .caseTypeOfApplication(C100_CASE_TYPE)
             .applicants(List.of(Element.<PartyDetails>builder().value(partyDetailsApplicant).build()))
@@ -92,6 +102,30 @@ public class CaseInitiationServiceTest {
             CallbackRequest.builder().caseDetails(caseDetails).build());
 
         verify(eventPublisher, times(1)).publishEvent(any());
+    }
+
+    @Test
+    public void testHandlePrePopulateCourtDetailsC100() {
+        caseData = CaseData.builder()
+                .caseTypeOfApplication(C100_CASE_TYPE)
+                .build();
+        when(objectMapper.convertValue(caseDataMap,CaseData.class)).thenReturn(caseData);
+        when(locationRefDataService.getCourtLocations(Mockito.anyString())).thenReturn(List.of(DynamicListElement.EMPTY));
+        Assertions.assertTrue(caseInitiationService.prePopulateCourtDetails(AUTHORISATION, caseDataMap).containsKey(COURT_LIST));
+    }
+
+    @Test
+    public void testHandlePrePopulateCourtDetailsFL401() {
+        caseDataMap.put("caseTypeOfApplication",FL401_CASE_TYPE);
+        caseData = CaseData.builder()
+                .caseTypeOfApplication(FL401_CASE_TYPE)
+                .build();
+        List<DynamicListElement> courts = List.of(DynamicListElement.builder().label("123").build());
+        when(locationRefDataService.getDaCourtLocations(AUTHORISATION))
+                .thenReturn(courts);
+        when(objectMapper.convertValue(caseDataMap,CaseData.class)).thenReturn(caseData);
+        when(locationRefDataService.getDaCourtLocations(Mockito.anyString())).thenReturn(courts);
+        Assertions.assertTrue(caseInitiationService.prePopulateCourtDetails(AUTHORISATION, caseDataMap).containsKey(COURT_LIST));
     }
 
 }
