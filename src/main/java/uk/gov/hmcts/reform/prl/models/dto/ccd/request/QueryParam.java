52f5b0883e19d6d1947a4810ee04fe452ac223e8
sahaSonali <110189370+sahaSonali@users.noreply.github.com>
2024-11-18 16:44:06 +0000

    Cafcass additional changes (#2855)
    * Fix for cafcass performance issue
    
    * Fix for cafcass performance issue
    
    * Fix for cafcass performance issue
    
    * Fix for cafcass performance issue
    
    * Fix for cafcass performance issue
    
    * Fix for cafcass performance issue
    
    * Fix for cafcass performance issue
    
    * Fix for cafcass performance issue
    
    * Fix for cafcass performance issue
    
    * Fix for cafcass performance issue
    
    * Fix for cafcass performance issue
    
    * Fix for cafcass performance issue
    
    * Fix for cafcass performance issue
    
    * Fix to set Hearing id as null if it is empty
    
    * Fix for PRL-6577
    
    * Update values.preview.template.yaml
    
    * Fix for PRL-6577
    
    * Added Source param for elastic search
    
    * Remove ref data call for address population : PRL-6604
    
    * merge conflict fix
    
    * merge compilation fix
    
    * Update values.preview.template.yaml
    
    * updated logger
    
    * corrceted _source params
    
    * Updated loggers
    
    * Added empty check for hearingdaySchedule
    
    * loggers removed and codesmell fix
    
    * Update values.preview.template.yaml
    
    ---------
    
    Co-authored-by: bharadwajsairam <bharadwajsairam001@gmail.com>
    Co-authored-by: BharadwajSairam <75499453+BharadwajSairam@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/request/QueryParam.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/request/QueryParam.java
@@ -1,10 +1,13 @@
 package uk.gov.hmcts.reform.prl.models.dto.ccd.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -14,5 +17,7 @@ import lombok.NoArgsConstructor;
 public class QueryParam {
     private Query query;
     private String size;
+    @JsonProperty("_source")
+    private List<String> dataToReturn;
 }
 
