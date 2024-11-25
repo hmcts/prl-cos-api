caa82580e3359047b746b27680e47e20d88b0486
bharadwajsairam <bharadwajsairam001@gmail.com>
2024-11-25 14:27:18 +0000

    Added app insights module block to test end point resource name display

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,15 @@
 provider "azurerm" {
   features {}
 }
+
+module "application_insights" {
+  source = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
+
+  env     = var.env
+  product = var.product
+  name    = "${var.product}-${var.component}-appinsights"
+
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.appinsights_location
+  common_tags         = var.common_tags
+}
