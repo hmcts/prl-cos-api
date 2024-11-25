e1d048eff9b8f0d0d565f7d386ee2f2aa20ef2da
bharadwajsairam <bharadwajsairam001@gmail.com>
2024-11-25 12:37:54 +0000

    checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/cafcass/CafCassController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/cafcass/CafCassController.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
