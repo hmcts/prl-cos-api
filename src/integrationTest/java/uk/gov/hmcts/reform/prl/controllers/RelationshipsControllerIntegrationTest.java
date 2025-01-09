6da897dcda26822376e0e1ba86e0a326089d930c
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2025-01-09 15:32:59 +0000

    FPET-1223- added integration test (#2922)
    * added integration test
    
    * added integration test
    
    * Update CaseDocumentControllerIntegrationTest.java
    
    * removed ignored Integration tests
    
    * removed the ParameterNamesModule module from class and added in test
    
    * Update CallbackControllerIntegrationTest.java
    
    * Update values.preview.template.yaml
    
    ---------
    
    Co-authored-by: priteshkr <101888106+priteshkr@users.noreply.github.com>

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/prl/controllers/RelationshipsControllerIntegrationTest.java
@@ -0,0 +1,189 @@
+package uk.gov.hmcts.reform.prl.controllers;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import uk.gov.hmcts.reform.prl.ResourceLoader;
+import uk.gov.hmcts.reform.prl.constants.PrlAppsConstants;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import static uk.gov.hmcts.reform.prl.util.TestConstants.AUTHORISATION_HEADER;
+
+@Slf4j
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+public class RelationshipsControllerIntegrationTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+        objectMapper.registerModule(new ParameterNamesModule());
+    }
+
+    @Test
+    public void testPrePopulateApplicantToChildRelation() throws Exception {
+        String url = "/pre-populate-applicant-to-child-relation";
+        String jsonRequest = ResourceLoader.loadJson("requests/judge-draft-sdo-order-request.json");
+
+        mockMvc.perform(
+                post(url)
+                    .header(AUTHORISATION_HEADER, "testAuthToken")
+                    .header(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER, "testServiceAuthToken")
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isOk())
+            .andReturn();
+    }
+
+    @Test
+    public void testPrePopulateAmendApplicantToChildRelation() throws Exception {
+        String url = "/pre-populate-amend-applicant-to-child-relation";
+        String jsonRequest = ResourceLoader.loadJson("requests/judge-draft-sdo-order-request.json");
+
+        mockMvc.perform(
+                post(url)
+                    .header(AUTHORISATION_HEADER, "testAuthToken")
+                    .header(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER, "testServiceAuthToken")
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isOk())
+            .andReturn();
+    }
+
+    @Test
+    public void testPopulateApplicantToChildRelation() throws Exception {
+        String url = "/populate-applicant-to-child-relation";
+        String jsonRequest = ResourceLoader.loadJson("requests/judge-draft-sdo-order-request.json");
+
+        mockMvc.perform(
+                post(url)
+                    .header(AUTHORISATION_HEADER, "testAuthToken")
+                    .header(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER, "testServiceAuthToken")
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isOk())
+            .andReturn();
+    }
+
+    @Test
+    public void testPrePopulateRespondentToChildRelation() throws Exception {
+        String url = "/pre-populate-respondent-to-child-relation";
+        String jsonRequest = ResourceLoader.loadJson("requests/judge-draft-sdo-order-request.json");
+
+        mockMvc.perform(
+                post(url)
+                    .header(AUTHORISATION_HEADER, "testAuthToken")
+                    .header(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER, "testServiceAuthToken")
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isOk())
+            .andReturn();
+    }
+
+    @Test
+    public void testPrePopulateAmendRespondentToChildRelation() throws Exception {
+        String url = "/pre-populate-amend-respondent-to-child-relation";
+        String jsonRequest = ResourceLoader.loadJson("requests/judge-draft-sdo-order-request.json");
+
+        mockMvc.perform(
+                post(url)
+                    .header(AUTHORISATION_HEADER, "testAuthToken")
+                    .header(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER, "testServiceAuthToken")
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isOk())
+            .andReturn();
+    }
+
+    @Test
+    public void testPopulateRespondentToChildRelation() throws Exception {
+        String url = "/populate-respondent-to-child-relation";
+        String jsonRequest = ResourceLoader.loadJson("requests/judge-draft-sdo-order-request.json");
+
+        mockMvc.perform(
+                post(url)
+                    .header(AUTHORISATION_HEADER, "testAuthToken")
+                    .header(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER, "testServiceAuthToken")
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isOk())
+            .andReturn();
+    }
+
+    @Test
+    public void testPrePopulateOtherPeopleToChildRelation() throws Exception {
+        String url = "/pre-populate-other-people-to-child-relation";
+        String jsonRequest = ResourceLoader.loadJson("requests/judge-draft-sdo-order-request.json");
+
+        mockMvc.perform(
+                post(url)
+                    .header(AUTHORISATION_HEADER, "testAuthToken")
+                    .header(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER, "testServiceAuthToken")
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isOk())
+            .andReturn();
+    }
+
+    @Test
+    public void testPrePopulateAmendOtherPeopleToChildRelation() throws Exception {
+        String url = "/pre-populate-amend-other-people-to-child-relation";
+        String jsonRequest = ResourceLoader.loadJson("requests/judge-draft-sdo-order-request.json");
+
+        mockMvc.perform(
+                post(url)
+                    .header(AUTHORISATION_HEADER, "testAuthToken")
+                    .header(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER, "testServiceAuthToken")
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isOk())
+            .andReturn();
+    }
+
+
+    @Test
+    public void testPopulateOtherPeopleToChildRelation() throws Exception {
+        String url = "/populate-other-people-to-child-relation";
+        String jsonRequest = ResourceLoader.loadJson("requests/judge-draft-sdo-order-request.json");
+
+        mockMvc.perform(
+                post(url)
+                    .header(AUTHORISATION_HEADER, "testAuthToken")
+                    .header(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER, "testServiceAuthToken")
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isOk())
+            .andReturn();
+    }
+}
