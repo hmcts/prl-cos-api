5f186d04ea473cc55dfa7f8c1d70fea7bafb13c6
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2025-01-09 15:32:59 +0000

    FPET-1223- added integration test (#2922)
    * added integration test
    
    * added integration test
    
    * Update CaseDocumentControllerIntegrationTest.java
    
    * removed ignored Integration tests
    
    * removed the ParameterNamesModule module from class and added in test
    
    * Update CallbackControllerIntegrationTest.java
    
    * Update values.preview.template.yaml
    
    ---------
    
    Co-authored-by: priteshkr <101888106+priteshkr@users.noreply.github.com>

--- src/integrationTest/java/uk/gov/hmcts/reform/prl/controllers/ResetAccessCodeControllerIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/prl/controllers/ResetAccessCodeControllerIntegrationTest.java
@@ -1,50 +1,77 @@
 package uk.gov.hmcts.reform.prl.controllers;
 
-import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.junit.Ignore;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import uk.gov.hmcts.reform.prl.Application;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 import uk.gov.hmcts.reform.prl.ResourceLoader;
+import uk.gov.hmcts.reform.prl.constants.PrlAppsConstants;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+import uk.gov.hmcts.reform.prl.services.AuthorisationService;
 import uk.gov.hmcts.reform.prl.services.pin.CaseInviteManager;
 
-import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
-import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
-@Ignore
-@RunWith(SpringIntegrationSerenityRunner.class)
-@SpringBootTest(classes = {Application.class, ResetAccessCodeControllerIntegrationTest.class})
+@Slf4j
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration
 public class ResetAccessCodeControllerIntegrationTest {
 
-    @Value("${case.orchestration.service.base.uri}")
-    protected String serviceUrl;
-
-    private static final String VALID_INPUT_JSON = "controller/valid-request-casedata-body.json";
-
-    private final String regenerateAccessCodeEndpoint = "/regenerate-access-code";
+    private MockMvc mockMvc;
 
     @Autowired
-    private CaseInviteManager caseInviteManager;
+    private WebApplicationContext webApplicationContext;
+
+    @MockBean
+    CaseInviteManager caseInviteManager;
+
+    @MockBean
+    AuthorisationService authorisationService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+        objectMapper.registerModule(new ParameterNamesModule());
+    }
+
 
     @Test
-    public void testRegenerateAccessCodeEndpoint() throws Exception {
-        String requestBody = ResourceLoader.loadJson(VALID_INPUT_JSON);
-        HttpPost httpPost = new HttpPost(serviceUrl + regenerateAccessCodeEndpoint);
-        httpPost.addHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE);
-        httpPost.addHeader(AUTHORIZATION, "Bearer testauth");
-        httpPost.addHeader("serviceAuthorization", "s2sToken");
-        StringEntity body = new StringEntity(requestBody);
-        httpPost.setEntity(body);
-        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(httpPost);
-        assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode());
+    public void testResetAccessCode() throws Exception {
+        String url = "/regenerate-access-code";
+        String jsonRequest = ResourceLoader.loadJson("CallbackRequest.json");
+
+        when(authorisationService.isAuthorized(anyString(), anyString())).thenReturn(true);
+        when(caseInviteManager.reGeneratePinAndSendNotificationEmail(any())).thenReturn(CaseData.builder().build());
+
+        mockMvc.perform(
+                post(url)
+                    .header("Authorization", "testAuthToken")
+                    .header(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER, "testServiceAuthToken")
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isOk())
+            .andReturn();
     }
+
 }
