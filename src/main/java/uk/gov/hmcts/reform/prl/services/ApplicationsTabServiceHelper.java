f2a8c46fcdeb0d40665ceb39879bfef451d6eda1
sairamkantheti1 <sai.kantheti@HMCTS.NET>
2025-01-22 16:00:48 +0000

    FPET-1248 changes

--- src/main/java/uk/gov/hmcts/reform/prl/services/ApplicationsTabServiceHelper.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ApplicationsTabServiceHelper.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.prl.services;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -43,10 +44,7 @@ public class ApplicationsTabServiceHelper {
         List<Element<OtherPersonInTheCaseRevised>> otherPersonsInTheCase = new ArrayList<>();
 
         if (otherPeopleCheck.isEmpty() || otherPeopleCheck.get().isEmpty()) {
-            OtherPersonInTheCaseRevised op = OtherPersonInTheCaseRevised.builder().build();
-            Element<OtherPersonInTheCaseRevised> other = Element.<OtherPersonInTheCaseRevised>builder().value(op).build();
-            otherPersonsInTheCase.add(other);
-            return otherPersonsInTheCase;
+            return getEmptyOtherPeople(otherPersonsInTheCase);
         }
         List<PartyDetails> otherPeople = caseData.getOtherPartyInTheCaseRevised().stream().map(Element::getValue).toList();
         otherPeople = maskConfidentialDetails(otherPeople);
@@ -62,10 +60,16 @@ public class ApplicationsTabServiceHelper {
                 otherPersonsInTheCase.add(wrappedPerson);
             }
         }
-        return otherPersonsInTheCase;
+        return CollectionUtils.size(otherPersonsInTheCase) > 0 ? otherPersonsInTheCase : getEmptyOtherPeople(
+            otherPersonsInTheCase);
     }
 
-
+    private static List<Element<OtherPersonInTheCaseRevised>> getEmptyOtherPeople(List<Element<OtherPersonInTheCaseRevised>> otherPersonsInTheCase) {
+        OtherPersonInTheCaseRevised op = OtherPersonInTheCaseRevised.builder().build();
+        Element<OtherPersonInTheCaseRevised> other = Element.<OtherPersonInTheCaseRevised>builder().value(op).build();
+        otherPersonsInTheCase.add(other);
+        return otherPersonsInTheCase;
+    }
 
 
     public List<Element<ChildDetailsRevised>> getChildRevisedDetails(CaseData caseData) {
