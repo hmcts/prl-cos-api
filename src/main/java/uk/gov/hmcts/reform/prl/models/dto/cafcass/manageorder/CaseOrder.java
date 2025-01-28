1db6f6cb3ca3a3c8e3600e7460bfbb3a14881134
JamesCuttsHMCTS <James.Cutts1@HMCTS.NET>
2025-01-28 13:17:25 +0000

    Removed ServeOrderDetails.java

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/cafcass/manageorder/CaseOrder.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/cafcass/manageorder/CaseOrder.java
@@ -95,8 +95,6 @@ public class CaseOrder {
         this.hearingDetails = caseHearing;
     }
 
-    private ServeOrderDetails serveOrderDetails;
-
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String selectedHearingType;
 
@@ -137,14 +135,6 @@ public class CaseOrder {
         String path = url.getPath();
         String documentId = path.split("/")[path.split("/").length - 1];
         return documentId;
-    }
-
-    public void setServeOrderDetails(ServeOrderDetails serveOrderDetails) {
-        this.serveOrderDetails = serveOrderDetails;
-        if (this.serveOrderDetails != null) {
-            setOriginalFilingDate(serveOrderDetails.getWhenReportsMustBeFiled());
-            setCourtReportType(serveOrderDetails.getCafcassCymruDocuments());
-        }
     }
 
     @JsonProperty("originalFilingDate")
