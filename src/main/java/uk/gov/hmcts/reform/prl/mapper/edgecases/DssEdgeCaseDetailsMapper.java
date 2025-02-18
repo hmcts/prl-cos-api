c25b41a601a52fc6d829f2448ef5bdd5574649aa
channa-patil <channabasappagouda.patil@hmcts.net>
2025-02-18 17:32:08 +0000

    PRL-6865 - fixed build issues, removed unused imports

--- src/main/java/uk/gov/hmcts/reform/prl/mapper/edgecases/DssEdgeCaseDetailsMapper.java
+++ src/main/java/uk/gov/hmcts/reform/prl/mapper/edgecases/DssEdgeCaseDetailsMapper.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.prl.enums.Gender;
 import uk.gov.hmcts.reform.prl.models.Address;
 import uk.gov.hmcts.reform.prl.models.complextypes.PartyDetails;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
