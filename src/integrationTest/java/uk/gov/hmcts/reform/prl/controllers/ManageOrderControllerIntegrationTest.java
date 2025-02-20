a53e34eedc56e5139a4c3e3dbf38d434b0751969
GayatriDeviChadaram <Gayatri.Chadaram1@hmcts.net>
2025-02-20 16:29:26 +0000

    code refactoring changes

--- src/integrationTest/java/uk/gov/hmcts/reform/prl/controllers/ManageOrderControllerIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/prl/controllers/ManageOrderControllerIntegrationTest.java
@@ -310,18 +310,22 @@ public class ManageOrderControllerIntegrationTest {
             .andReturn();
     }
 
-    /*@Test
+    @Test
     public void testCaseOrderEmailNotificationEndpointForAutoHearing() throws Exception {
-        String requestBody = ResourceLoader.loadJson(VALID_MANAGE_ORDER_AUTOMATED_HEARING_REQUEST_BODY);
-        HttpPost httpPost = new HttpPost(serviceUrl + caseOrderEmailNotificationEndpoint);
-        httpPost.addHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE);
-        httpPost.addHeader(AUTHORIZATION, "Bearer testauth");
-        httpPost.addHeader("serviceAuthorization", "s2sToken");
-        StringEntity body = new StringEntity(requestBody);
-        httpPost.setEntity(body);
-        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(httpPost);
-        assertEquals(HttpStatus.SC_INTERNAL_SERVER_ERROR, httpResponse.getStatusLine().getStatusCode());
-    }*/
+        String url = "/caseOrderEmailNotificationEndpoint";
+        String jsonRequest = ResourceLoader.loadJson(VALID_MANAGE_ORDER_AUTOMATED_HEARING_REQUEST_BODY);
+        when(authorisationService.isAuthorized(anyString(), anyString())).thenReturn(true);
+
+        mockMvc.perform(
+                post(url)
+                    .header(AUTHORISATION_HEADER, "testAuthToken")
+                    .header(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER, "testServiceAuthToken")
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isOk())
+            .andReturn();
+    }
 
     @Test
     public void testManageOrdersServeOrderMidEvent() throws Exception {
@@ -359,18 +363,22 @@ public class ManageOrderControllerIntegrationTest {
             .andReturn();
     }
 
-    /*@Test
+    @Test
     public void testCaseOrderSubmittedEndPointForAutoHearing() throws Exception {
-        String requestBody = ResourceLoader.loadJson(VALID_MANAGE_ORDER_AUTOMATED_HEARING_REQUEST_BODY);
-        HttpPost httpPost = new HttpPost(serviceUrl + editAndApproveOrderSubmittedEndpoint);
-        httpPost.addHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE);
-        httpPost.addHeader(AUTHORIZATION, "Bearer testauth");
-        httpPost.addHeader("serviceAuthorization", "s2sToken");
-        StringEntity body = new StringEntity(requestBody);
-        httpPost.setEntity(body);
-        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(httpPost);
-        assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode());
-    }*/
+        String jsonRequest = ResourceLoader.loadJson(VALID_MANAGE_ORDER_AUTOMATED_HEARING_REQUEST_BODY);
+
+        when(authorisationService.isAuthorized(anyString(), anyString())).thenReturn(true);
+
+        mockMvc.perform(
+                post(editAndApproveOrderSubmittedEndpoint)
+                    .header(AUTHORISATION_HEADER, "testAuthToken")
+                    .header(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER, "testServiceAuthToken")
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isOk())
+            .andReturn();
+    }
 
     @Test
     public void testValidateAndPopulateHearingData() throws Exception {
