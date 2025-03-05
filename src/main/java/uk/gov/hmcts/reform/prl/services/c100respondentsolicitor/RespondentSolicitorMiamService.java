147453dc5ffc9c90b7cda87161b75178677519b7
JamesCuttsHMCTS <James.Cutts1@HMCTS.NET>
2025-03-05 11:48:10 +0000

    Removed duplications

--- src/main/java/uk/gov/hmcts/reform/prl/services/c100respondentsolicitor/RespondentSolicitorMiamService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/c100respondentsolicitor/RespondentSolicitorMiamService.java
@@ -75,19 +75,19 @@ public class RespondentSolicitorMiamService {
         collapsible.add("<p>A MIAM is a first meeting with a mediator. - welsh</p>");
         collapsible.add("<p>The MIAM will: - welsh");
         collapsible.add(PULTAG);
-        collapsible.add("<li>last about an hour</li>");
+        collapsible.add("<li>last about an hour - welsh</li>");
         collapsible.add("<li>give you the opportunity to tell the mediator about your situation, and the issues to be decided - welsh</li>");
         collapsible.add("<li>explain the mediation process, and other options for reaching agreements - welsh</li>");
         collapsible.add(ULPPTAG);
         collapsible.add("<p>At the end of the meeting, the mediator will tell you if your case is suitable for mediation."
-            + "You can then decide if you want to proceed, or explore other options.</p>");
-        collapsible.add("<p>The benefits of a MIAM are:");
+            + "You can then decide if you want to proceed, or explore other options. - welsh</p>");
+        collapsible.add("<p>The benefits of a MIAM are: - welsh");
         collapsible.add(PULTAG);
-        collapsible.add("<li>less stress and a faster process than going to court</li>");
-        collapsible.add("<li>help with making arrangements over parenting, property and money</li>");
-        collapsible.add("<li>more control over your family's future</li>");
-        collapsible.add("<li>help with putting your childrens' interests first</li>");
-        collapsible.add("<li>help with moving on to the next stage of your lives</li>");
+        collapsible.add("<li>less stress and a faster process than going to court - welsh</li>");
+        collapsible.add("<li>help with making arrangements over parenting, property and money - welsh</li>");
+        collapsible.add("<li>more control over your family's future - welsh</li>");
+        collapsible.add("<li>help with putting your childrens' interests first - welsh</li>");
+        collapsible.add("<li>help with moving on to the next stage of your lives - welsh</li>");
         collapsible.add(ULPPTAG);
         collapsible.add("<p>The MIAM process works, with agreement reached in over 70% of cases. - welsh</p>");
         collapsible.add(DIV);
@@ -140,29 +140,29 @@ public class RespondentSolicitorMiamService {
         collapsibleWillingnessToAttendMiam.add("<details class=\"govuk-details\" data-module=\"govuk-details\">");
         collapsibleWillingnessToAttendMiam.add(SUMMARYCLASS);
         collapsibleWillingnessToAttendMiam.add(SPANCLASS);
-        collapsibleWillingnessToAttendMiam.add("Help with MIAM costs and exemptions");
+        collapsibleWillingnessToAttendMiam.add("Help with MIAM costs and exemptions - welsh");
         collapsibleWillingnessToAttendMiam.add(SPAN);
         collapsibleWillingnessToAttendMiam.add(SUMMARY);
         collapsibleWillingnessToAttendMiam.add(DIVCLASS);
-        collapsibleWillingnessToAttendMiam.add("<p>The price of mediation will vary depending on:");
+        collapsibleWillingnessToAttendMiam.add("<p>The price of mediation will vary depending on: - welsh");
         collapsibleWillingnessToAttendMiam.add(PULTAG);
-        collapsibleWillingnessToAttendMiam.add("<li>where you live</li>");
-        collapsibleWillingnessToAttendMiam.add("<li>how many sessions you attend</li>");
-        collapsibleWillingnessToAttendMiam.add("<li>the type of issues you need to discuss</li>");
+        collapsibleWillingnessToAttendMiam.add("<li>where you live - welsh</li>");
+        collapsibleWillingnessToAttendMiam.add("<li>how many sessions you attend - welsh</li>");
+        collapsibleWillingnessToAttendMiam.add("<li>the type of issues you need to discuss - welsh</li>");
         collapsibleWillingnessToAttendMiam.add(ULPPTAG);
-        collapsibleWillingnessToAttendMiam.add("<p>You may be able to get legal aid if you are on a low income.</p>");
-        collapsibleWillingnessToAttendMiam.add("<h3 class=\"govuk-heading-s\">Family Mediation Voucher Scheme</h3>");
+        collapsibleWillingnessToAttendMiam.add("<p>You may be able to get legal aid if you are on a low income. - welsh</p>");
+        collapsibleWillingnessToAttendMiam.add("<h3 class=\"govuk-heading-s\">Family Mediation Voucher Scheme - welsh</h3>");
         collapsibleWillingnessToAttendMiam.add("<p>You may be eligible for the Family Mediation Voucher Scheme. This is a government scheme "
-            + "that provides up to £500 towards the cost of mediation for eligible cases.</p>");
+            + "that provides up to £500 towards the cost of mediation for eligible cases. - welsh</p>");
         collapsibleWillingnessToAttendMiam.add("<p>At your MIAM, your mediator will explain the voucher scheme and you may be offered a "
-            + "voucher if your case meets the scheme's criteria. Not all cases qualify for a voucher.</p>");
+            + "voucher if your case meets the scheme's criteria. Not all cases qualify for a voucher. - welsh</p>");
         collapsibleWillingnessToAttendMiam.add("<p>See the <a href=\"https://www.gov.uk\" class=\"govuk-link\""
             + "target=\"_blank\">GOV.UK</a> guidance on the <a href=\"https://www.gov.uk/guidance/family-mediation-voucher-scheme\" "
-            + "class=\"govuk-link\" target=\"_blank\">Family Mediation Voucher Scheme</a> for more information.</p>");
+            + "class=\"govuk-link\" target=\"_blank\">Family Mediation Voucher Scheme</a> for more information. - welsh</p>");
         collapsibleWillingnessToAttendMiam.add("<p>The MIAM process works, with agreement reached in over 70% of cases. - welsh</p>");
-        collapsibleWillingnessToAttendMiam.add("<h3 class=\"govuk-heading-s\">Exemptions</h3>");
+        collapsibleWillingnessToAttendMiam.add("<h3 class=\"govuk-heading-s\">Exemptions - welsh</h3>");
         collapsibleWillingnessToAttendMiam.add("<p>You don't need to attend a MIAM if you have a valid reason. For example, "
-            + "you or the children are at risk of harm.</p>");
+            + "you or the children are at risk of harm. - welsh</p>");
         collapsibleWillingnessToAttendMiam.add("<p>Find out more about <a href=\"https://apply-to-court-about-child-arrangements.service.justice.gov.uk/about/miam_exemptions\" "
             + "class=\"govuk-link\" target=\"_blank\">the reasons for a MIAM exemption</a>. - welsh</p>");
         collapsibleWillingnessToAttendMiam.add(DIV);
