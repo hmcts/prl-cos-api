279af2248c2d78cbc4d7ff281b6165be97ce5334
sunilrochani <sunil.rochani@hotmail.com>
2025-03-18 12:36:39 +0000

    FPVTL-97: 173978873547271017396569787874191739832019355066examples of HWF cases where there is no date submitted on the summary tab within exui and therefore no dat INC5692574

--- src/main/java/uk/gov/hmcts/reform/prl/services/HwfProcessUpdateCaseStateService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/HwfProcessUpdateCaseStateService.java
@@ -91,16 +91,12 @@ public class HwfProcessUpdateCaseStateService {
                             caseDetails.getId().toString(),
                             HWF_PROCESS_CASE_UPDATE.getValue()
                         );
-
                         caseDataUpdated.put("caseStatus", CaseStatus.builder().state(State.SUBMITTED_PAID.getLabel()).build());
-                        log.info("DateSubmitted is {} ", caseDataUpdated.get(DATE_SUBMITTED_FIELD));
                         if (caseDataUpdated.get(DATE_SUBMITTED_FIELD) == null) {
                             ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of(EUROPE_LONDON_TIME_ZONE));
                             log.info("DateTimeFormatter Date is {} ", DateTimeFormatter.ISO_LOCAL_DATE.format(zonedDateTime));
                             caseDataUpdated.put(DATE_SUBMITTED_FIELD, DateTimeFormatter.ISO_LOCAL_DATE.format(zonedDateTime));
-                            log.info("DateSubmitted is {} ", caseDataUpdated.get(DATE_SUBMITTED_FIELD));
                         }
-
                         //Save case data
                         allTabService.submitAllTabsUpdate(
                             startAllTabsUpdateDataContent.authorisation(),
