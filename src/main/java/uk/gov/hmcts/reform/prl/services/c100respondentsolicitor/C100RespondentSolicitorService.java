bf7f52ba2ee23fd2f6184b219f4683f0d3216fc3
GayatriDeviChadaram <Gayatri.Chadaram1@hmcts.net>
2025-03-26 09:37:42 +0000

    build issue fix

--- src/main/java/uk/gov/hmcts/reform/prl/services/c100respondentsolicitor/C100RespondentSolicitorService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/c100respondentsolicitor/C100RespondentSolicitorService.java
@@ -846,22 +846,18 @@ public class C100RespondentSolicitorService {
     }
 
     private Consent optimiseConsent(Consent consent) {
-        if (null != consent) {
-            String noConsentReason = consent.getNoConsentReason();
-            String courtOrderDetails = consent.getCourtOrderDetails();
-            if (YesOrNo.Yes.equals(consent.getConsentToTheApplication())) {
-                noConsentReason = null;
-            }
-            if (YesOrNo.No.equals(consent.getPermissionFromCourt())) {
-                courtOrderDetails = null;
-            }
-            return consent.toBuilder()
+        String noConsentReason = consent.getNoConsentReason();
+        String courtOrderDetails = consent.getCourtOrderDetails();
+        if (YesOrNo.Yes.equals(consent.getConsentToTheApplication())) {
+            noConsentReason = null;
+        }
+        if (YesOrNo.No.equals(consent.getPermissionFromCourt())) {
+            courtOrderDetails = null;
+        }
+        return consent.toBuilder()
                 .noConsentReason(noConsentReason)
                 .courtOrderDetails(courtOrderDetails)
                 .build();
-        } else {
-            return Consent.builder().build();
-        }
     }
 
     public Optional<SolicitorRole> getSolicitorRole(CallbackRequest callbackRequest) {
