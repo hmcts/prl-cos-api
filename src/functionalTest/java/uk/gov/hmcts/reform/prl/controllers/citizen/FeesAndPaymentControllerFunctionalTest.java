c8011a91d46f6caf96df26e56c2b5d61fa913d06
DelythJustice <delyth.jones11@justice.gov.uk>
2025-04-01 09:17:55 +0100

    FPVTL-409 for FPVTL-503 but need to go together and both null pointer exceptions point to correct place use junit5 and fee is harcoded in test!?

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/citizen/FeesAndPaymentControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/citizen/FeesAndPaymentControllerFunctionalTest.java
@@ -72,6 +72,5 @@ public class FeesAndPaymentControllerFunctionalTest {
 
         Assertions.assertNotNull(response);
         Assertions.assertNotNull(response.getAmount());
-        Assertions.assertEquals("263.00", response.getAmount());
     }
 }
