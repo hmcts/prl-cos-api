8e1660f92d0aebfbe7d55df3ba51c7713f13accb
DelythJustice <delyth.jones11@justice.gov.uk>
2025-04-01 13:47:36 +0100

    FPVTL-409 fix concat bit smarter

--- src/test/java/uk/gov/hmcts/reform/prl/models/dto/cafcass/CafCassCaseDataTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/models/dto/cafcass/CafCassCaseDataTest.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.reform.prl.models.dto.cafcass;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class CafCassCaseDataTest {
 
