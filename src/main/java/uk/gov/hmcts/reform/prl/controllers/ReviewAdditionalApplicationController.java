ead212f128a460cefbcf0ba8b94d491cf20a4307
sunilrochani <sunil.rochani@hotmail.com>
2025-05-19 09:36:53 +0100

    FPVTL-494: 'Review Additional Application' task update

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ReviewAdditionalApplicationController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ReviewAdditionalApplicationController.java
@@ -39,6 +39,7 @@ import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.AWP_ADDTIONAL_A
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.AWP_STATUS_CLOSED;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.AWP_STATUS_IN_REVIEW;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.INVALID_CLIENT;
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.YES;
 import static uk.gov.hmcts.reform.prl.enums.sendmessages.SendOrReply.REPLY;
 import static uk.gov.hmcts.reform.prl.enums.sendmessages.SendOrReply.SEND;
 import static uk.gov.hmcts.reform.prl.models.sendandreply.SendOrReplyMessage.temporaryFieldsAboutToStart;
@@ -119,43 +120,14 @@ public class ReviewAdditionalApplicationController extends AbstractCallbackContr
         CaseDetails caseDetails = callbackRequest.getCaseDetails();
         CaseData caseData = CaseUtils.getCaseData(caseDetails, objectMapper);
         Map<String, Object> caseDataMap = callbackRequest.getCaseDetails().getData();
-        if (caseData.getChooseSendOrReply().equals(SEND)) {
-            caseDataMap.put(MESSAGES, sendAndReplyService.addMessage(caseData, authorisation, caseDataMap));
-            String additionalApplicationCodeSelected = sendAndReplyService.fetchAdditionalApplicationCodeIfExist(
-                caseData, SEND
-            );
-
-            if (null != additionalApplicationCodeSelected) {
-                caseDataMap.put(
-                    AWP_ADDTIONAL_APPLICATION_BUNDLE,
-                    uploadAdditionalApplicationService
-                        .updateAwpApplicationStatus(
-                            additionalApplicationCodeSelected,
-                            caseData.getAdditionalApplicationsBundle(),
-                            AWP_STATUS_IN_REVIEW
-                        )
-                );
-            }
-
-            sendAndReplyService.sendNotificationToExternalParties(
-                caseData,
-                authorisation
-            );
-
-            //send emails in case of sending to others with emails
-            sendAndReplyService.sendNotificationEmailOther(caseData);
-            //WA - clear reply field in case of SEND
-            sendAndReplyService.removeTemporaryFields(caseDataMap, "replyMessageObject");
-        } else {
-            if (YesOrNo.No.equals(caseData.getSendOrReplyMessage().getRespondToMessage())) {
-                //Reply & close
-                caseDataMap.put(MESSAGES, sendAndReplyService.closeMessage(caseData, caseDataMap));
-
-                // Update status of Additional applications if selected to Closed
+        if (caseData.getReviewAdditionalApplicationWrapper() != null
+            && YES.equals(caseData.getReviewAdditionalApplicationWrapper().getIsAdditionalApplicationReviewed())) {
+            if (caseData.getChooseSendOrReply().equals(SEND)) {
+                caseDataMap.put(MESSAGES, sendAndReplyService.addMessage(caseData, authorisation, caseDataMap));
                 String additionalApplicationCodeSelected = sendAndReplyService.fetchAdditionalApplicationCodeIfExist(
-                    caseData, REPLY
+                    caseData, SEND
                 );
-                log.info("additionalApplicationCodeSelected while closing message {}", additionalApplicationCodeSelected);
+
                 if (null != additionalApplicationCodeSelected) {
                     caseDataMap.put(
                         AWP_ADDTIONAL_APPLICATION_BUNDLE,
@@ -163,19 +135,61 @@ public class ReviewAdditionalApplicationController extends AbstractCallbackContr
                             .updateAwpApplicationStatus(
                                 additionalApplicationCodeSelected,
                                 caseData.getAdditionalApplicationsBundle(),
-                                AWP_STATUS_CLOSED
+                                AWP_STATUS_IN_REVIEW
                             )
                     );
                 }
 
-                // in case of reply and close message, removing replymessageobject for wa
+                sendAndReplyService.sendNotificationToExternalParties(
+                    caseData,
+                    authorisation
+                );
+
+                //send emails in case of sending to others with emails
+                sendAndReplyService.sendNotificationEmailOther(caseData);
+                //WA - clear reply field in case of SEND
                 sendAndReplyService.removeTemporaryFields(caseDataMap, "replyMessageObject");
             } else {
-                //Reply & append history
-                caseDataMap.put(MESSAGES, sendAndReplyService.replyAndAppendMessageHistory(caseData, authorisation, caseDataMap));
+                if (YesOrNo.No.equals(caseData.getSendOrReplyMessage().getRespondToMessage())) {
+                    //Reply & close
+                    caseDataMap.put(MESSAGES, sendAndReplyService.closeMessage(caseData, caseDataMap));
+
+                    // Update status of Additional applications if selected to Closed
+                    String additionalApplicationCodeSelected = sendAndReplyService.fetchAdditionalApplicationCodeIfExist(
+                        caseData, REPLY
+                    );
+                    log.info(
+                        "additionalApplicationCodeSelected while closing message {}",
+                        additionalApplicationCodeSelected
+                    );
+                    if (null != additionalApplicationCodeSelected) {
+                        caseDataMap.put(
+                            AWP_ADDTIONAL_APPLICATION_BUNDLE,
+                            uploadAdditionalApplicationService
+                                .updateAwpApplicationStatus(
+                                    additionalApplicationCodeSelected,
+                                    caseData.getAdditionalApplicationsBundle(),
+                                    AWP_STATUS_CLOSED
+                                )
+                        );
+                    }
+
+                    // in case of reply and close message, removing replymessageobject for wa
+                    sendAndReplyService.removeTemporaryFields(caseDataMap, "replyMessageObject");
+                } else {
+                    //Reply & append history
+                    caseDataMap.put(
+                        MESSAGES,
+                        sendAndReplyService.replyAndAppendMessageHistory(
+                            caseData,
+                            authorisation,
+                            caseDataMap
+                        )
+                    );
+                }
+                //WA - clear send field in case of REPLY
+                sendAndReplyService.removeTemporaryFields(caseDataMap, "sendMessageObject");
             }
-            //WA - clear send field in case of REPLY
-            sendAndReplyService.removeTemporaryFields(caseDataMap, "sendMessageObject");
         }
 
         //clear temp fields
@@ -191,21 +205,24 @@ public class ReviewAdditionalApplicationController extends AbstractCallbackContr
                                                                                  @RequestBody CallbackRequest callbackRequest) {
         CaseData caseData = getCaseData(callbackRequest.getCaseDetails());
 
-        if (REPLY.equals(caseData.getChooseSendOrReply())
-            && YesOrNo.Yes.equals(caseData.getSendOrReplyMessage().getRespondToMessage())) {
-            return ok(SubmittedCallbackResponse.builder().confirmationBody(
-                REPLY_AND_CLOSE_MESSAGE
-            ).build());
-        }
+        if (caseData.getReviewAdditionalApplicationWrapper() != null
+            && YES.equals(caseData.getReviewAdditionalApplicationWrapper().getIsAdditionalApplicationReviewed())) {
+            if (REPLY.equals(caseData.getChooseSendOrReply())
+                && YesOrNo.Yes.equals(caseData.getSendOrReplyMessage().getRespondToMessage())) {
+                return ok(SubmittedCallbackResponse.builder().confirmationBody(
+                    REPLY_AND_CLOSE_MESSAGE
+                ).build());
+            }
 
-        if (SEND.equals(caseData.getChooseSendOrReply()) && InternalExternalMessageEnum.EXTERNAL.equals(
-            caseData.getSendOrReplyMessage().getSendMessageObject().getInternalOrExternalMessage())) {
-            return ok(SubmittedCallbackResponse.builder().confirmationBody(
-                SEND_AND_CLOSE_EXTERNAL_MESSAGE
-            ).build());
-        }
+            if (SEND.equals(caseData.getChooseSendOrReply()) && InternalExternalMessageEnum.EXTERNAL.equals(
+                caseData.getSendOrReplyMessage().getSendMessageObject().getInternalOrExternalMessage())) {
+                return ok(SubmittedCallbackResponse.builder().confirmationBody(
+                    SEND_AND_CLOSE_EXTERNAL_MESSAGE
+                ).build());
+            }
 
-        sendAndReplyService.closeAwPTask(caseData);
+            sendAndReplyService.closeAwPTask(caseData);
+        }
 
         return ok(SubmittedCallbackResponse.builder().build());
     }
---
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/ReviewAdditionalApplicationWrapper.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.reform.prl.models.dto.ccd;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import uk.gov.hmcts.reform.prl.enums.YesOrNo;
+import uk.gov.hmcts.reform.prl.models.complextypes.uploadadditionalapplication.AdditionalApplicationsBundle;
+
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ReviewAdditionalApplicationWrapper {
+
+    private YesOrNo isAdditionalApplicationReviewed;
+
+    private AdditionalApplicationsBundle selectedAdditionalApplicationsBundle;
+}
