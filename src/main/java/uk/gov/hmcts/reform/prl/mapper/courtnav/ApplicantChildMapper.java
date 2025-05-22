8e38b6154cdde49307f02d73c20f5ea70fe6ee81
Michael1142 <michael1142@hotmail.co.uk>
2025-05-22 14:32:01 +0100

    Fix sonar nags

--- src/main/java/uk/gov/hmcts/reform/prl/mapper/courtnav/ApplicantChildMapper.java
+++ src/main/java/uk/gov/hmcts/reform/prl/mapper/courtnav/ApplicantChildMapper.java
@@ -9,6 +9,8 @@ import uk.gov.hmcts.reform.prl.models.dto.ccd.courtnav.ProtectedChild;
 import java.time.LocalDate;
 import java.util.List;
 
+import static uk.gov.hmcts.reform.prl.enums.YesOrNo.No;
+import static uk.gov.hmcts.reform.prl.enums.YesOrNo.Yes;
 import static uk.gov.hmcts.reform.prl.utils.ElementUtils.element;
 
 @Mapper(componentModel = "spring")
@@ -20,13 +22,15 @@ public interface ApplicantChildMapper {
             .fullName(child.getFullName())
             .dateOfBirth(LocalDate.parse(child.getDateOfBirth().mergeDate()))
             .applicantChildRelationship(child.getRelationship())
-            .applicantRespondentShareParental(child.isParentalResponsibility() ? uk.gov.hmcts.reform.prl.enums.YesOrNo.Yes : uk.gov.hmcts.reform.prl.enums.YesOrNo.No)
+            .applicantRespondentShareParental(child.isParentalResponsibility() ? Yes : No)
             .respondentChildRelationship(child.getRespondentRelationship())
             .build();
     }
 
     default List<Element<ApplicantChild>> mapProtectedChildren(List<ProtectedChild> protectedChildren) {
-        if (protectedChildren == null) return null;
+        if (protectedChildren == null) {
+            return null;
+        }
 
         return protectedChildren.stream()
             .map(child -> element(toApplicantChild(child)))
