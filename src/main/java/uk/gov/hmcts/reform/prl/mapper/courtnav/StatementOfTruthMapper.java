7ba7df2688712b6d07e0dfa3262f8cc1afc63a4c
Michael1142 <michael1142@hotmail.co.uk>
2025-06-11 07:01:33 +0100

    Update mapper to use map struct

--- src/main/java/uk/gov/hmcts/reform/prl/mapper/courtnav/StatementOfTruthMapper.java
+++ src/main/java/uk/gov/hmcts/reform/prl/mapper/courtnav/StatementOfTruthMapper.java
@@ -1,34 +1,29 @@
 package uk.gov.hmcts.reform.prl.mapper.courtnav;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import uk.gov.hmcts.reform.prl.enums.FL401Consent;
 import uk.gov.hmcts.reform.prl.models.complextypes.StatementOfTruth;
-import uk.gov.hmcts.reform.prl.models.dto.ccd.courtnav.CourtNavFl401;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.courtnav.CourtNavStatementOfTruth;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.courtnav.enums.ConsentEnum;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", imports = {LocalDate.class})
 public interface StatementOfTruthMapper {
 
-    default StatementOfTruth map(CourtNavFl401 source) {
-        var statement = source.getFl401().getStatementOfTruth();
+    @Mapping(target = "applicantConsent", source = "applicantConsent", qualifiedByName = "mapConsentList")
+    @Mapping(target = "date", expression = "java(LocalDate.parse(source.getDate().mergeDate()))")
+    StatementOfTruth map(CourtNavStatementOfTruth source);
 
-        return StatementOfTruth.builder()
-            .applicantConsent(mapConsent(statement.getDeclaration()))
-            .signature(statement.getSignature())
-            .fullname(statement.getSignatureFullName())
-            .date(LocalDate.parse(statement.getSignatureDate().mergeDate()))
-            .nameOfFirm(statement.getRepresentativeFirmName())
-            .signOnBehalf(statement.getRepresentativePositionHeld())
-            .build();
-    }
-
-    private List<FL401Consent> mapConsent(List<ConsentEnum> declaration) {
-        return declaration.stream()
-            .map(ConsentEnum::getId)
-            .map(FL401Consent::getDisplayedValueFromEnumString)
-            .toList();
+    @Named("mapConsentList")
+    static List<FL401Consent> mapConsentList(List<ConsentEnum> declaration) {
+        return declaration == null ? null :
+            declaration.stream()
+                .map(ConsentEnum::getId)
+                .map(FL401Consent::getDisplayedValueFromEnumString)
+                .toList();
     }
 }
