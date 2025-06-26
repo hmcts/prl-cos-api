74be265cb69ea9097890089b89278928ee790112
Joshua Gray <joshgray91@gmail.com>
2025-06-26 13:45:24 +0100

    FPVTL-678 update ApplicantConfidentialityDetails

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/confidentiality/ApplicantConfidentialityDetails.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/confidentiality/ApplicantConfidentialityDetails.java
@@ -6,7 +6,7 @@ import uk.gov.hmcts.reform.prl.models.Address;
 
 @Builder
 @Data
-public class ApplicantsConfidentialityDetails {
+public class ApplicantConfidentialityDetails {
     private final String firstName;
     private final String lastName;
     private final String email;
