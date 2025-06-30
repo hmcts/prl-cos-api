edecb6d48a718ab66cee8587cf96d43bb5c322ba
SomeshRA <somesh.acharya1@hmcts.net>
2025-06-30 19:03:19 +0100

    FPVTL-493: Fixed checkstyle

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersController.java
@@ -15,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
