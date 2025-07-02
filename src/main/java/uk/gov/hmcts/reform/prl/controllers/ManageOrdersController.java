1c08eb02d89946b8101c22685d6d8f9383c8ff17
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2025-07-02 11:02:16 +0100

    FPVTL-493: improve MOC code

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersController.java
@@ -489,14 +489,6 @@ public class ManageOrdersController {
             } else {
                 caseDataUpdated.put(ORDERS_NEED_TO_BE_SERVED, No);
             }
-            boolean saveAsDraft = manageOrderService.isSaveAsDraft(caseData);
-
-            boolean present = ofNullable(caseData.getManageOrders().getOrdersHearingDetails())
-                .map(ElementUtils::unwrapElements)
-                .map(hearingData -> hearingData.getFirst().getHearingDateConfirmOptionEnum())
-                .filter(hearingDateConfirmOptionEnum ->
-                            HearingDateConfirmOptionEnum.dateConfirmedInHearingsTab.getId()
-                                .equals(hearingDateConfirmOptionEnum.getId())).isPresent();
 
             String encodedClientContext = CaseUtils.setTaskCompletion(
                 clientContext,
