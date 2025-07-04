0de0c4febb67db5c9db258048b9786669150c71c
SomeshRA <somesh.acharya1@hmcts.net>
2025-07-04 13:02:22 +0100

    FPVTL-915: Enabling Hearing hack on preview

---
+++ src/main/java/uk/gov/hmcts/reform/prl/clients/hearing/HearingApiHackClient.java
@@ -0,0 +1,102 @@
+package uk.gov.hmcts.reform.prl.clients.hearing;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestHeader;
+import uk.gov.hmcts.reform.prl.clients.HearingApiClient;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.AutomatedHearingCaseData;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.AutomatedHearingResponse;
+import uk.gov.hmcts.reform.prl.models.dto.hearingmanagement.NextHearingDetails;
+import uk.gov.hmcts.reform.prl.models.dto.hearings.CaseLinkedData;
+import uk.gov.hmcts.reform.prl.models.dto.hearings.CaseLinkedRequest;
+import uk.gov.hmcts.reform.prl.models.dto.hearings.Hearings;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Component
+@ConditionalOnProperty(name = "hearing.hack.enabled", havingValue = "true")
+public class HearingApiHackClient implements HearingApiClient {
+
+
+    public Hearings getHearingDetails(
+        @RequestHeader("Authorization") String authorisation,
+        @RequestHeader("ServiceAuthorization") String serviceAuthorization,
+        @RequestHeader("caseReference") String caseReference
+    ) throws RuntimeException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.findAndRegisterModules();
+
+        String hearingPayload = null;
+        try {
+            hearingPayload = Files.readString(Path.of("hearingHackResponse.json"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String updatedHearingPayload = hearingPayload.replace("<caseRef>", caseReference);
+
+        try {
+            return mapper.readValue(updatedHearingPayload, Hearings.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<CaseLinkedData> getCaseLinkedData(String authorisation,
+                                                  String serviceAuthorization,
+                                                  CaseLinkedRequest caseLinkedRequest) {
+        throw new UnsupportedOperationException("Feign call not supported from hack api");
+    }
+
+    @Override
+    public NextHearingDetails getNextHearingDate(String authorisation,
+                                                 String serviceAuthorization,
+                                                 String caseReference) {
+        throw new UnsupportedOperationException("Feign call not supported from hack api");
+    }
+
+    @Override
+    public Hearings getFutureHearings(String authorisation,
+                                      String serviceAuthorization,
+                                      String caseReference) {
+        throw new UnsupportedOperationException("Feign call not supported from hack api");
+    }
+
+    @Override
+    public List<Hearings> getHearingsByListOfCaseIds(String authorisation,
+                                                     String serviceAuthorization,
+                                                     Map<String, String> caseIdWithRegionIdMap) {
+        throw new UnsupportedOperationException("Feign call not supported from hack api");
+    }
+
+    @Override
+    public List<Hearings> getHearingsForAllCaseIdsWithCourtVenue(String authorisation,
+                                                                 String serviceAuthorization,
+                                                                 List<String> caseIds) {
+        throw new UnsupportedOperationException("Feign call not supported from hack api");
+    }
+
+    @Override
+    public Map<String, List<String>> getListedHearingsForAllCaseIdsOnCurrentDate(String authorisation,
+                                                                                 String serviceAuthorization,
+                                                                                 List<String> caseIds) {
+        throw new UnsupportedOperationException("Feign call not supported from hack api");
+    }
+
+    @Override
+    public ResponseEntity<AutomatedHearingResponse> createAutomatedHearing(String authorisation,
+                                                                           String serviceAuthorization,
+                                                                           AutomatedHearingCaseData automatedHearingCaseData) {
+        throw new UnsupportedOperationException("Feign call not supported from hack api");
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/testingsupport/HearingSupportController.java
@@ -0,0 +1,42 @@
+package uk.gov.hmcts.reform.prl.controllers.testingsupport;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+import uk.gov.hmcts.reform.prl.enums.State;
+import uk.gov.hmcts.reform.prl.models.dto.hearingmanagement.HearingRequest;
+import uk.gov.hmcts.reform.prl.services.hearingmanagement.HearingManagementService;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+@Slf4j
+@RestController
+@RequestMapping("/hearing-support/testing")
+@RequiredArgsConstructor
+@ConditionalOnProperty(name = "hearing.hack.enabled", havingValue = "true")
+public class HearingSupportController {
+    private final HearingManagementService hearingManagementService;
+
+    @PutMapping(path = "hearing-management-state-update/{caseState}", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
+    @Operation(description = "fis service call to update the state of case")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Callback processed.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CallbackResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)})
+    public void caseStateUpdateByHearingManagement(@RequestBody HearingRequest hearingRequest,
+                                                          @PathVariable("caseState") State caseState) throws Exception {
+        log.info("Hack to update hearing HMC response");
+        hearingManagementService.caseStateChangeForHearingManagement(hearingRequest,caseState);
+    }
+}
