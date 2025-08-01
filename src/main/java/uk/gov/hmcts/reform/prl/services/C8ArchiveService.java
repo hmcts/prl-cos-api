54a7281a647d6dfbb34cba067b73b9f00537017e
Jefex7 <duartepireslopes@gmail.com>
2025-08-01 08:52:32 +0100

    FPVTL-939: add upload time stamp to C8 Archived document.

--- src/main/java/uk/gov/hmcts/reform/prl/services/C8ArchiveService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/C8ArchiveService.java
@@ -14,6 +14,7 @@ import uk.gov.hmcts.reform.prl.models.documents.Document;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.utils.CaseUtils;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -104,6 +105,7 @@ public class C8ArchiveService {
                 .documentUrl(document.getDocumentUrl())
                 .documentBinaryUrl(document.getDocumentBinaryUrl())
                 .documentFileName(fileName)
+                .uploadTimeStamp(LocalDateTime.now())
                 .build();
 
             archivedDocuments.add(buildElement(archivedocument));
