8f20e46a60fbf0f59d036cbc2fb4ca85e7cd42eb
sunilrochani <sunil.rochani@hotmail.com>
2025-08-07 14:53:53 +0100

    FPVTL-841: Caseworker 'Remove barrister' submission callback

--- src/main/java/uk/gov/hmcts/reform/prl/clients/ccd/CaseAssignmentService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/clients/ccd/CaseAssignmentService.java
@@ -1,7 +1,5 @@
 package uk.gov.hmcts.reform.prl.clients.ccd;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.FeignException;
 import lombok.Builder;
@@ -366,39 +364,34 @@ public class CaseAssignmentService {
     }
 
     private Optional<String> getC100BarristerRole(Map<String, Object> data,
-                                                      CaseData caseData,
-                                                      String selectedPartyId) {
+                                                  CaseData caseData,
+                                                  String selectedPartyId) {
         PartyDetails c100Party = getC100Party(caseData, selectedPartyId);
         String nameKey = String.join("-", c100Party.getFirstName(), c100Party.getLastName());
+        record PartyInfo(String firstName, String lastName) {}
 
         return Arrays.stream(BarristerRole.RoleMapping.values())
-            .filter(roleMapping -> roleMapping.getRepresenting().equals(Representing.CAAPPLICANT)
-                || roleMapping.getRepresenting().equals(Representing.CARESPONDENT))
-            .filter(roleMap -> data.get(roleMap.getParty()) != null)
-            .filter(roleMap -> {
-                try {
-                    JsonNode jsonNode = objectMapper.readTree((String) data.get(roleMap.getParty()));
+                .filter(roleMapping -> roleMapping.getRepresenting().equals(BarristerRole.Representing.CAAPPLICANT)
+                        || roleMapping.getRepresenting().equals(BarristerRole.Representing.CARESPONDENT))
+                .filter(roleMap -> data.get(roleMap.getParty()) != null)
+                .filter(roleMap -> {
+                    PartyInfo partyInfo = objectMapper.convertValue(data.get(roleMap.getParty()), PartyInfo.class);
+
                     String partyKey = String.join(
-                        "-",
-                        jsonNode.get("firstName").asText(),
-                        jsonNode.get("lastName").asText()
+                            "-",
+                            partyInfo.firstName,
+                            partyInfo.lastName
                     );
+
                     return partyKey.equals(nameKey);
-                } catch (JsonProcessingException e) {
-                    throw new IllegalArgumentException(String.join(
-                        ":", "Error processing role for case data",
-                        String.valueOf(caseData.getId()), " and selected party id ",
-                        selectedPartyId)
-                    );
-                }
-            })
-            .findFirst()
-            .flatMap(roleMapping ->
-                         Arrays.stream(BarristerRole.values())
-                             .filter(barristerRole -> barristerRole.getRoleMapping().equals(roleMapping))
-                             .findFirst()
-            )
-            .map(BarristerRole::getCaseRoleLabel);
+                })
+                .findFirst()
+                .flatMap(roleMapping ->
+                        Arrays.stream(BarristerRole.values())
+                                .filter(barristerRole -> barristerRole.getRoleMapping().equals(roleMapping))
+                                .findFirst()
+                )
+                .map(BarristerRole::getCaseRoleLabel);
     }
 
     private void updatedPartyWithBarristerDetails(CaseData caseData,
