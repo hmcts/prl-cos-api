0cd52f3ff574779292ed2e6bc573ffb9a1ffa9a1
sunilrochani <sunil.rochani@hotmail.com>
2025-08-11 13:27:32 +0100

    FPVTL-841: Caseworker 'Remove barrister' submission callback

--- src/integrationTest/java/uk/gov/hmcts/reform/prl/controllers/CaseAssignmentControllerIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/prl/controllers/CaseAssignmentControllerIntegrationTest.java
@@ -30,7 +30,6 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
