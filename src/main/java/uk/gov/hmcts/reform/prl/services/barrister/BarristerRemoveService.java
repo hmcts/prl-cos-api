25e1bf88e5933dd8d50cd185b4a5ef9a23b90a26
Jefex7 <duartepireslopes@gmail.com>
2025-08-14 12:58:53 +0100

    FPVTL-842: Add filter for remove

--- src/main/java/uk/gov/hmcts/reform/prl/services/barrister/BarristerRemoveService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/barrister/BarristerRemoveService.java
@@ -30,7 +30,19 @@ public class BarristerRemoveService extends  AbstractBarristerService {
 
     @Override
     protected boolean isPartyApplicableForFiltering(boolean applicantOrRespondent, BarristerFilter barristerFilter, PartyDetails partyDetails) {
-        return hasBarrister(partyDetails) && partyDetails.getBarrister().getBarristerId() != null;
+        boolean isApplicable = hasBarrister(partyDetails) && partyDetails.getBarrister().getBarristerId() != null;
+
+        if (barristerFilter.isCaseworkerOrSolicitor()) {
+            return isApplicable;
+        } else {
+            if (partyDetails.getSolicitorOrg() == null || barristerFilter.getUserOrgIdentifier() == null) {
+                log.info("This party {} has an empty solicitor org or the user org identifier is empty", partyDetails.getPartyId());
+                return false;
+            }
+
+            return isApplicable
+                && barristerFilter.getUserOrgIdentifier().equals(partyDetails.getSolicitorOrg().getOrganisationID());
+        }
     }
 
     @Override
