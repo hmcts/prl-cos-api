1aeb047c2fa12217c06cebd27cc59029058e2bd4
SomeshRA <somesh.acharya1@hmcts.net>
2025-08-14 09:58:23 +0100

    FPVTL-811: Updated validation messages

--- src/test/java/uk/gov/hmcts/reform/prl/clients/ccd/CaseAssignmentServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/clients/ccd/CaseAssignmentServiceTest.java
@@ -532,7 +532,7 @@ class CaseAssignmentServiceTest {
                                                  AllocatedBarrister.builder().build(),
                                                  errorList);
         assertThat(errorList)
-            .contains("Could not find a registered barrister with the provided email address.");
+            .contains("Could not find a registered barrister with the email address provided");
     }
 
     @Test
@@ -881,7 +881,7 @@ class CaseAssignmentServiceTest {
         List<String> errors = new ArrayList<>();
         caseAssignmentService.validateBarristerOrgRelationship(c100CaseData, allocatedBarrister, errors);
         assertThat(errors)
-            .contains("Barrister doesn't belong to the selected organisation.");
+            .contains("Barrister is not registered with the selected organisation");
     }
 
     @Test
