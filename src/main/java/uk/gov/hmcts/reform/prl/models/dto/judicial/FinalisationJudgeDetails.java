8bff970c14cd94303c795ae7e879ad580a3a9262
Jefex7 <duartepireslopes@gmail.com>
2025-09-04 14:02:24 +0100

    FPVTL-1043: Amend class

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/judicial/FinalisationJudgeDetails.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/judicial/FinalisationJudgeDetails.java
@@ -4,15 +4,16 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.prl.enums.manageorders.JudgeOrMagistrateTitleEnum;
 
 
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class FinalisationJudgeDetails {
 
-
-    private final JudgeOrMagistrateTitleEnum judgeOrMagistrateTitle;
+    private  JudgeOrMagistrateTitleEnum judgeOrMagistrateTitle;
 }
