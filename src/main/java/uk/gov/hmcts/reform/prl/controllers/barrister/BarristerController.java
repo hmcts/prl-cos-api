a6ced8fceffbe59815b047011c0ecd1bfc4beba7
sanjayparekh-hmcts <36845702+sanjayparekh-hmcts@users.noreply.github.com>
2025-09-04 15:56:52 +0100

    Fpvtl 1086 add validation on startevent (#3283)
    * FPVTL-1086: incl validation in barrister start event
    
    * FPVTL-1086: incl validation in barrister start event
    
    * FPVTL-1086: merged from master

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/barrister/BarristerController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/barrister/BarristerController.java
@@ -51,7 +51,7 @@ public class BarristerController extends AbstractCallbackController {
     @PostMapping(path = "/add/about-to-start", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
     @Operation(description = "Callback to allocate a barrister on about-to-start")
     @SecurityRequirement(name = "Bearer Authentication")
-    public AboutToStartOrSubmitCallbackResponse handleMidEvent(
+    public AboutToStartOrSubmitCallbackResponse handleAddAboutToStartEvent(
         @RequestHeader(org.springframework.http.HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
         @RequestHeader(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER) String s2sToken,
         @RequestBody CallbackRequest callbackRequest) {
@@ -61,10 +61,17 @@ public class BarristerController extends AbstractCallbackController {
             CaseData caseData = CaseUtils.getCaseData(callbackRequest.getCaseDetails(), objectMapper);
 
             Map<String, Object> caseDataUpdated = callbackRequest.getCaseDetails().getData();
-            caseDataUpdated.put(ALLOCATED_BARRISTER, barristerAddService.getAllocatedBarrister(caseData, authorisation));
-
+            List<String> errorList = new ArrayList<>();
+            AllocatedBarrister barristerList = barristerAddService.getAllocatedBarrister(caseData, authorisation);
+            if (!barristerList.getPartyList().getListItems().isEmpty()) {
+                caseDataUpdated.put(ALLOCATED_BARRISTER, barristerList);
+            } else {
+                errorList.add("There are no solicitors currently assigned to any party on this case");
+            }
             AboutToStartOrSubmitCallbackResponse.AboutToStartOrSubmitCallbackResponseBuilder
-                builder = AboutToStartOrSubmitCallbackResponse.builder().data(caseDataUpdated);
+                builder = AboutToStartOrSubmitCallbackResponse.builder()
+                    .errors(errorList)
+                    .data(caseDataUpdated);
             return builder.build();
         } else {
             throw (new RuntimeException(INVALID_CLIENT));
