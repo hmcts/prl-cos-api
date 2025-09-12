99bcfaec17244a25ac8f0e321a99fca2b8048002
SomeshRA <somesh.acharya1@hmcts.net>
2025-09-12 10:39:13 +0100

    FPVTL-1116: Remove barrister case flags

--- src/test/java/uk/gov/hmcts/reform/prl/services/noticeofchange/NoticeOfChangePartiesServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/noticeofchange/NoticeOfChangePartiesServiceTest.java
@@ -93,7 +93,6 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
