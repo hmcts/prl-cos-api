a5a0ff1bc8e3b1d6601ac25da7f638eacd850197
dharmendra kumar <dharmendra.kumar@hmcts.net>
2025-10-07 09:16:23 +0100

    Remove retry on sftp to check

--- src/main/java/uk/gov/hmcts/reform/prl/services/acro/SftpService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/acro/SftpService.java
@@ -5,10 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.MessageHandlingException;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Recover;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -20,21 +16,10 @@ public class SftpService {
     @Autowired
     private MessageChannel toSftpChannel;
 
-    @Retryable(
-        retryFor = {MessageHandlingException.class},
-        maxAttempts = 4,
-        backoff = @Backoff(delay = 1000, multiplier = 3)
-    )
     public void uploadFile(File file) {
         log.info("Uploading file to SFTP");
         Message<File> message = MessageBuilder.withPayload(file).build();
         toSftpChannel.send(message);
         log.info("File {} uploaded to SFTP", file.getName());
-    }
-
-
-    @Recover
-    public void uploadFileRecovery(RuntimeException e, Message<File> message) {
-        log.error("Error while uploading file  {} to SFTP", message.getHeaders(), e);
     }
 }
