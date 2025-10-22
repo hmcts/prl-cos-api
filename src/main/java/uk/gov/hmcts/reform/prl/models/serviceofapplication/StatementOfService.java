b6b8528fb5286d5796630ecc4f492daa3c6582cd
Joshua Gray <joshgray91@gmail.com>
2025-10-22 18:42:37 +0100

    add servedOrderIds to StatementOfService

--- src/main/java/uk/gov/hmcts/reform/prl/models/serviceofapplication/StatementOfService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/serviceofapplication/StatementOfService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.prl.enums.serviceofapplication.StatementOfServiceWhatWasServed;
 import uk.gov.hmcts.reform.prl.models.Element;
 
@@ -11,6 +12,7 @@ import java.util.List;
 
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class StatementOfService {
@@ -23,5 +25,7 @@ public class StatementOfService {
     private List<Element<StmtOfServiceAddRecipient>> stmtOfServiceForOrder;
     @JsonProperty("stmtOfServiceForApplication")
     private List<Element<StmtOfServiceAddRecipient>> stmtOfServiceForApplication;
+    @JsonProperty("servedOrderIds")
+    @Builder.Default
+    private List<String> servedOrderIds = null;
 }
-
