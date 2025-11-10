b04504c002a4664c5a27d69a68d94ba6aaebfbdd
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2025-11-10 11:10:11 +0000

    Update CFTLib for local development (#3334)
    * Copy over cftlib role assignments, update cftlib library, add roles
    
    * Configure local dev environment
    
    * Revert idam config change
    
    * Bypass hearings api
    
    * Bump rse-cft-lib
    
    * Add TTL_profile role
    
    * Add cftlib scripts in separate bin subfolder
    
    * Add script to import CCD definitions
    
    * Minor doc changes
    
    * Update docs
    
    * Update cftlib plugin version
    
    ---------
    
    Co-authored-by: Daniel Catchpole <daniel.catchpole@justice.gov.uk>

--- src/cftlib/java/uk/gov/hmcts/reform/prl/CftLibConfig.java
+++ src/cftlib/java/uk/gov/hmcts/reform/prl/CftLibConfig.java
@@ -1,17 +1,26 @@
 package uk.gov.hmcts.reform.prl;
 
+import com.google.common.io.Resources;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.rse.ccd.lib.api.CFTLib;
 import uk.gov.hmcts.rse.ccd.lib.api.CFTLibConfigurer;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-
 @Component
 public class CftLibConfig implements CFTLibConfigurer {
+
     @Override
     public void configure(CFTLib lib) throws Exception {
+        createCcdRoles(lib);
+        configureRoleAssignments(lib);
+        importCcdDefinitions(lib);
+    }
+
+    private void createCcdRoles(CFTLib lib) {
         lib.createRoles(
             "caseworker-privatelaw-judge",
             "caseworker-privatelaw-courtadmin",
@@ -19,6 +28,7 @@ public class CftLibConfig implements CFTLibConfigurer {
             "caseworker-privatelaw-superuser",
             "caseworker-privatelaw-solicitor",
             "caseworker-privatelaw-systemupdate",
+            "caseworker-privatelaw-readonly",
             "citizen",
             "courtnav",
             "payments",
@@ -31,11 +41,27 @@ public class CftLibConfig implements CFTLibConfigurer {
             "pui-case-manager",
             "caseworker-approver",
             "caseworker-privatelaw-cafcass",
-            "caseworker-privatelaw-externaluser-viewonly"
+            "caseworker-privatelaw-externaluser-viewonly",
+            "tribunal-caseworker",
+            "allocated-magistrate",
+            "ctsc-team-leader",
+            "hearing-centre-team-leader",
+            "hearing-centre-admin",
+            "judge",
+            "senior-tribunal-caseworker",
+            "caseworker-privatelaw-courtadmin-casecreator",
+            "ctsc",
+            "TTL_profile"
         );
+    }
 
-        var def = Files.readAllBytes(Path.of("bin/ccd-config-PRL-local.xlsx"));
+    private void configureRoleAssignments(CFTLib lib) throws IOException {
+        var json = Resources.toString(Resources.getResource("cftlib-am-role-assignments.json"), StandardCharsets.UTF_8);
+        lib.configureRoleAssignments(json);
+    }
+
+    private void importCcdDefinitions(CFTLib lib) throws IOException {
+        var def = Files.readAllBytes(Path.of("build/definitionsToBeImported/ccd-config-PRL-local.xlsx"));
         lib.importDefinition(def);
     }
 }
-
