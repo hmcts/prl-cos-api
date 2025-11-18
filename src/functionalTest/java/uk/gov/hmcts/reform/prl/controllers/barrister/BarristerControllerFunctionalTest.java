005aa4fbc3a14636ad51bd6ff6cce4e015717245
DelythJustice <delyth.jones11@justice.gov.uk>
2025-11-18 18:07:22 +0000

    FPVTL-1115 add logging to failing functional test

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/barrister/BarristerControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/barrister/BarristerControllerFunctionalTest.java
@@ -1,8 +1,8 @@
 package uk.gov.hmcts.reform.prl.controllers.barrister;
 
 import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.RestAssured;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import lombok.extern.slf4j.Slf4j;
