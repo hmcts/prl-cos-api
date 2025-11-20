c2080142251ef2c1edd52797530ff646ee470a16
DelythJustice <delyth.jones11@justice.gov.uk>
2025-11-20 12:45:19 +0000

    FPVTL-1115 checkstyle fix

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/barrister/BarristerControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/barrister/BarristerControllerFunctionalTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.prl.controllers.barrister;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import lombok.extern.slf4j.Slf4j;
