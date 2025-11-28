bb1fe43b4f5a3745a86f7dd9f8bcf212c43f6155
DelythJustice <delyth.jones11@justice.gov.uk>
2025-11-28 08:36:15 +0000

    FPVTL-1115 fix test

--- src/test/java/uk/gov/hmcts/reform/prl/services/caseinitiation/CaseInitiationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/caseinitiation/CaseInitiationServiceTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
