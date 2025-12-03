c60e0bb6a9cb7315c9d747dfc1ea3db1ed8feb47
sunilrochani <sunil.rochani@hotmail.com>
2025-12-03 15:21:24 +0000

    FPVTL-1507: Call OS API to find Council for postcode

--- src/main/java/uk/gov/hmcts/reform/prl/clients/os/OsCourtFinderApi.java
+++ src/main/java/uk/gov/hmcts/reform/prl/clients/os/OsCourtFinderApi.java
@@ -1,15 +1,35 @@
 package uk.gov.hmcts.reform.prl.clients.os;
 
-
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import uk.gov.hmcts.reform.prl.config.OsPlacesFeignConfig;
 import uk.gov.hmcts.reform.prl.models.ordnancesurvey.OsPlacesResponse;
 
 @FeignClient(name = "os-court-finder-api", primary = false, url = "${postcodelookup.api.url}",
-    configuration = OsPlacesFeignConfig.class)
+    configuration = OsCourtFinderApi.FeignClientConfiguration.class)
 public interface OsCourtFinderApi {
+
+    class FeignClientConfiguration {
+
+        @Value("${postcodelookup.api.key}")
+        private String apiKey;
+
+        @Bean
+        public RequestInterceptor osPlacesRequestInterceptor() {
+
+            return new RequestInterceptor() {
+                @Override
+                public void apply(RequestTemplate template) {
+                    template.query("key", apiKey);
+                    template.query("maxresults", "1");
+                }
+            };
+        }
+    }
 
     @GetMapping(value = "/postcode", consumes = "application/json")
     OsPlacesResponse findCouncilByPostcode(@RequestParam("postcode") String postcode);
