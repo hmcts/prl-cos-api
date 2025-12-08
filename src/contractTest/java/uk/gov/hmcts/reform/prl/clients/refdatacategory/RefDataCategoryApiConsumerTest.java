b118698453a3a9c400172838272d62fe8b08f7fa
srochani <sunil.rochani@hmcts.net>
2025-12-08 15:46:40 +0000

    FPVTL-1507: Call OS API to find Council for postcode (#3413)
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode

--- src/contractTest/java/uk/gov/hmcts/reform/prl/clients/refdatacategory/RefDataCategoryApiConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/prl/clients/refdatacategory/RefDataCategoryApiConsumerTest.java
@@ -41,7 +41,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
         "pba.validation.service.api.baseurl=",
         "staffDetails.api.url=http://localhost:8899",
         "amRoleAssignment.api.url=",
-        "core_case_data.api.url="
+        "core_case_data.api.url=",
+        "postcodelookup.api.url="
     }
 )
 @ContextConfiguration(
