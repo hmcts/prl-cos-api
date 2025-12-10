856cc1a97fbf17f643634ac87bbad3b660cfe25b
DelythJustice <delyth.jones11@justice.gov.uk>
2025-12-10 14:01:29 +0000

    FPVTL-1115 set use user token to false when calling assigncaseaccess on user id

--- src/main/java/uk/gov/hmcts/reform/prl/services/caseaccess/AssignCaseAccessService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/caseaccess/AssignCaseAccessService.java
@@ -100,7 +100,7 @@ public class AssignCaseAccessService {
         assignCaseAccessClient.assignCaseAccess(
             sysUserToken,  // sysuser
             serviceToken,
-            true,
+            false,
             request
         );
     }
