e3e6f177453dcf04ff35d8f22f089575bb188793
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2025-12-11 12:28:36 +0000

    FPVTL-1676: Decouple deleting document from the rename process (#3421)
    * FPVTL-1676 Fixing conf doc rename.
    Fixed order of doc renaming.
    
    * FPVTL-1676: Fix ManageDocumentsServiceTests
    
    * FPVTL-1676: Delete documents after they have been reviewed as confidential
    
    * Remove vals
    
    * FPVTL-1676: Add tests for review documents submitted callbacks
    
    * FPVTL-1676: Add tests for manage documents
    
    * Addressing code review comments.
    
    ---------
    
    Co-authored-by: JohnnyEmbleton <john.embleton@justice.gov.uk>

--- src/main/java/uk/gov/hmcts/reform/prl/services/reviewdocument/ReviewDocumentService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/reviewdocument/ReviewDocumentService.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.prl.services.reviewdocument;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
@@ -19,9 +20,12 @@ import uk.gov.hmcts.reform.prl.models.complextypes.QuarantineLegalDoc;
 import uk.gov.hmcts.reform.prl.models.complextypes.ScannedDocument;
 import uk.gov.hmcts.reform.prl.models.documents.Document;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.ReviewDocuments;
+import uk.gov.hmcts.reform.prl.services.SystemUserService;
 import uk.gov.hmcts.reform.prl.services.managedocuments.ManageDocumentsService;
 import uk.gov.hmcts.reform.prl.services.tab.alltabs.AllTabServiceImpl;
 import uk.gov.hmcts.reform.prl.utils.CommonUtils;
+import uk.gov.hmcts.reform.prl.utils.DocumentUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +34,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static java.lang.String.format;
+import static java.util.Collections.emptyList;
 import static org.apache.commons.collections.CollectionUtils.isNotEmpty;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.BULK_SCAN;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.C100_CASE_TYPE;
@@ -44,6 +49,8 @@ import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.SOLICITOR;
 import static uk.gov.hmcts.reform.prl.enums.managedocuments.DocumentPartyEnum.CAFCASS_CYMRU;
 import static uk.gov.hmcts.reform.prl.utils.CommonUtils.formatDateTime;
 import static uk.gov.hmcts.reform.prl.utils.ElementUtils.element;
+import static uk.gov.hmcts.reform.prl.utils.ElementUtils.findElement;
+import static uk.gov.hmcts.reform.prl.utils.ElementUtils.nullSafeList;
 
 @Slf4j
 @Service
@@ -58,6 +65,8 @@ public class ReviewDocumentService {
 
     private final AllTabServiceImpl allTabService;
     private final ManageDocumentsService manageDocumentsService;
+    private final ObjectMapper objectMapper;
+    private final SystemUserService systemUserService;
 
     public static final String DOCUMENT_SUCCESSFULLY_REVIEWED = "# Document successfully reviewed";
     public static final String DOCUMENT_IN_REVIEW = "# Document review in progress";
@@ -630,6 +639,53 @@ public class ReviewDocumentService {
                 )
             ));
         }
+    }
+
+    /**
+     * This method deletes any previous - old - documents that have been replaced by a version that has been
+     * prefixed with 'Confidential'.
+     * @param currentCaseData This is the updated data that holds the renamed copy of the reviewed document.
+     * @param previousCaseData This is the data that contains documents ready to be reviewed.
+     */
+    public void cleanupOldCopyOfDocuments(CaseData currentCaseData, CaseData previousCaseData) {
+        Optional<ReviewDocuments> currentReviewDocs = Optional.ofNullable(currentCaseData.getReviewDocuments());
+        Optional<ReviewDocuments> previousReviewDocs = Optional.ofNullable(previousCaseData.getReviewDocuments());
+
+        cleanupConfidentialDocumentsList(
+            currentReviewDocs.map(ReviewDocuments::getConfidentialDocuments).orElse(emptyList()),
+            previousReviewDocs.map(ReviewDocuments::getConfidentialDocuments).orElse(emptyList()));
+
+        cleanupConfidentialDocumentsList(
+            currentReviewDocs.map(ReviewDocuments::getRestrictedDocuments).orElse(emptyList()),
+            previousReviewDocs.map(ReviewDocuments::getRestrictedDocuments).orElse(emptyList()));
+    }
+
+    private void cleanupConfidentialDocumentsList(List<Element<QuarantineLegalDoc>> currentConfidentialDocuments,
+                                                 List<Element<QuarantineLegalDoc>> previousConfidentialDocuments) {
+        nullSafeList(currentConfidentialDocuments).forEach(currentConfidentialDoc -> {
+            // check if it isn't in previous
+            if (findElement(currentConfidentialDoc.getId(), previousConfidentialDocuments).isEmpty()) {
+                log.info("Checking if we need to delete document {}",  currentConfidentialDoc.getId());
+                // new document, do check if we need to delete
+                String attributeName = DocumentUtils.populateAttributeNameFromCategoryId(
+                    currentConfidentialDoc.getValue().getCategoryId(),
+                    null
+                );
+                Document currentDocument = objectMapper.convertValue(
+                    objectMapper.convertValue(currentConfidentialDoc.getValue(), Map.class).get(attributeName),
+                    Document.class
+                );
+                String originalDocumentId = currentConfidentialDoc.getValue().getOriginalDocumentId();
+                if (originalDocumentId != null
+                    && !DocumentUtils.getDocumentId(currentDocument.getDocumentUrl()).equals(originalDocumentId)) {
+                    deleteDocumentById(originalDocumentId);
+                }
+            }
+        });
+    }
+
+    private void deleteDocumentById(String documentId) {
+        manageDocumentsService.deleteDocumentById(documentId);
     }
 
 }
