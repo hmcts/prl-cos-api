be6dc9839c2f8f4b6bcdbbc676407b560a6a1f04
srochani <sunil.rochani@hmcts.net>
2026-01-08 14:10:20 +0000

    Fpvtl 1508 lookup court from csv (#3416)
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1507: Call OS API to find Council for postcode
    
    * FPVTL-1508: Lookup Court info from csv, using OS LOCAL_CUSTODIAN value
    
    * FPVTL-1508: Lookup Court info from csv, using OS LOCAL_CUSTODIAN value
    
    * FPVTL-1508: Lookup Court info from csv, using OS LOCAL_CUSTODIAN value
    
    * FPVTL-1508: Lookup Court info from csv, using OS LOCAL_CUSTODIAN value
    
    * FPVTL-1508: Lookup Court info from csv, using OS LOCAL_CUSTODIAN value
    
    * FPVTL-1508: Lookup Court info from csv, using OS LOCAL_CUSTODIAN value
    
    * FPVTL-1508: Lookup Court info from csv, using OS LOCAL_CUSTODIAN value
    
    * FPVTL-1508: Lookup Court info from csv, using OS LOCAL_CUSTODIAN value
    
    * FPVTL-1508: Lookup Court info from csv, using OS LOCAL_CUSTODIAN value
    
    * FPVTL-1508: Lookup Court info from csv, using OS LOCAL_CUSTODIAN value
    
    * FPVTL-1508: Lookup Court info from csv, using OS LOCAL_CUSTODIAN value
    
    * FPVTL-1508: Lookup Court info from csv, using OS LOCAL_CUSTODIAN value
    
    * FPVTL-1508: Lookup Court info from csv, using OS LOCAL_CUSTODIAN value
    
    * FPVTL-1508: Lookup Court info from csv, using OS LOCAL_CUSTODIAN value
    
    ---------
    
    Co-authored-by: sanjayparekh-hmcts <36845702+sanjayparekh-hmcts@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/prl/services/OsCourtFinderService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/OsCourtFinderService.java
@@ -3,17 +3,73 @@ package uk.gov.hmcts.reform.prl.services;
 import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
+import uk.gov.hmcts.reform.prl.clients.CourtFinderApi;
 import uk.gov.hmcts.reform.prl.clients.os.OsCourtFinderApi;
+import uk.gov.hmcts.reform.prl.models.LocalAuthorityCourt;
+import uk.gov.hmcts.reform.prl.models.court.Court;
+import uk.gov.hmcts.reform.prl.models.court.CourtVenue;
 import uk.gov.hmcts.reform.prl.models.ordnancesurvey.OsPlacesResponse;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.util.CollectionUtils.isEmpty;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class OsCourtFinderService {
+
+    private final SystemUserService systemUserService;
     private final OsCourtFinderApi osCourtFinderApi;
+    private final LocalAuthorityCourtDataLoader localAuthorityCourtDataLoader;
+    private final LocationRefDataService locationRefDataService;
+    private final CourtFinderApi courtFinderApi;
+
+    public ImmutablePair<CourtVenue, Court> getC100NearestFamilyCourtAndVenue(String postcode) throws NotFoundException {
+        if (postcode == null || postcode.isEmpty()) {
+            throw new IllegalArgumentException("postcode is null or empty");
+        }
+        List<LocalAuthorityCourt> localAuthorityCourtList = localAuthorityCourtDataLoader.getLocalAuthorityCourtList();
+        // first check if the postcode is from specific post codes otherwise use os api to get local custodian code
+        Optional<LocalAuthorityCourt> selectedLocalAuthorityCourt = localAuthorityCourtList.stream()
+            .filter(court -> CollectionUtils.isNotEmpty(court.getSpecificPostCodes()))
+            .filter(court -> court.getSpecificPostCodes().stream().anyMatch(postcode::startsWith))
+            .findFirst();
+
+        if (selectedLocalAuthorityCourt.isEmpty()) {
+            String localCustodianCode = getLocalCustodianCodeByPostCode(postcode);
+            if (localCustodianCode != null) {
+                selectedLocalAuthorityCourt = localAuthorityCourtList.stream()
+                    .filter(court -> court.getLocalCustodianCode().equals(localCustodianCode))
+                    .findFirst();
+            }
+        }
+        if (selectedLocalAuthorityCourt.isPresent()) {
+            Optional<CourtVenue> courtVenue = locationRefDataService.getCourtDetailsFromEpimmsId(
+                selectedLocalAuthorityCourt.get().getEpimmsId(),
+                systemUserService.getSysUserToken()
+            );
+            if (courtVenue.isPresent()) {
+                log.info("Found court venue epimmsId {} for postcode {}", courtVenue.get().getCourtEpimmsId(), postcode);
+                return ImmutablePair.of(courtVenue.get(), getFactCourtDetails(courtVenue.get()));
+            }
+        }
+
+        return null;
+    }
+
+    public Court getC100NearestFamilyCourt(String postcode) throws NotFoundException {
+        ImmutablePair<CourtVenue, Court> courtCourtVenueMap = getC100NearestFamilyCourtAndVenue(postcode);
+        if (courtCourtVenueMap != null) {
+            return courtCourtVenueMap.getRight();
+        }
+        return null;
+    }
 
     public String getLocalCustodianCodeByPostCode(String postCode) throws NotFoundException {
         OsPlacesResponse osPlacesResponse = null;
@@ -23,9 +79,23 @@ public class OsCourtFinderService {
             log.info("OsCourtFinderService.getLocalCustodianCodeByPostCode() method is throwing exception : {}",e);
         }
         if (osPlacesResponse != null
-            && !CollectionUtils.isEmpty(osPlacesResponse.getResults())) {
+            && !isEmpty(osPlacesResponse.getResults())) {
             return osPlacesResponse.getResults().getFirst().getDpa().getLocalCustodianCode();
         }
         return null;
     }
+
+    private Court getFactCourtDetails(CourtVenue courtVenue) {
+        Court court = null;
+        String factUrl = courtVenue.getFactUrl();
+        if (factUrl != null && factUrl.split("/").length > 4) {
+            try {
+                court = courtFinderApi.getCourtDetails(factUrl.split("/")[4]);
+            } catch (Exception ex) {
+                log.error("Error fetching court details from Fact ", ex);
+            }
+        }
+        return court;
+    }
+
 }
