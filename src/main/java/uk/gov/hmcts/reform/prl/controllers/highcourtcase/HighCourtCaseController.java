b5a7e30a4fa255d41dd93c04af72f2f886686808
Yogesh Dhonde <Yogesh.Dhonde@HMCTS.NET>
2026-01-15 15:53:22 +0000

    FPVTL-1856

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/highcourtcase/HighCourtCaseController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/highcourtcase/HighCourtCaseController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,25 +23,27 @@ import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.reform.prl.constants.PrlAppsConstants;
 import uk.gov.hmcts.reform.prl.controllers.AbstractCallbackController;
-import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
+import uk.gov.hmcts.reform.prl.services.AuthorisationService;
 import uk.gov.hmcts.reform.prl.services.EventService;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
-import static java.util.Objects.nonNull;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.springframework.http.ResponseEntity.ok;
+import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.INVALID_CLIENT;
 
 @Slf4j
 @RestController
 @RequestMapping("/high-court-case")
 public class HighCourtCaseController  extends AbstractCallbackController {
 
+    private final AuthorisationService authorisationService;
 
-    public HighCourtCaseController(ObjectMapper objectMapper, EventService eventPublisher) {
+    @Autowired
+    public HighCourtCaseController(ObjectMapper objectMapper, EventService eventPublisher,
+                                   AuthorisationService authorisationService) {
         super(objectMapper, eventPublisher);
+        this.authorisationService = authorisationService;
     }
 
 
@@ -57,52 +60,32 @@ public class HighCourtCaseController  extends AbstractCallbackController {
         @RequestHeader(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER) String s2sToken,
         @RequestBody CallbackRequest callbackRequest) {
 
-        log.info("High court case about-to-submit");
-        final CaseDetails caseDetails = callbackRequest.getCaseDetails();
-        final CaseData caseData = getCaseData(caseDetails);
-        log.info("about-to-submit IsHighCourtCase {}", caseData.getIsHighCourtCase());
+        if (authorisationService.isAuthorized(authorisation,s2sToken)) {
+            final CaseDetails caseDetails = callbackRequest.getCaseDetails();
 
 
-        List<String> errors = new ArrayList<>();
-        return AboutToStartOrSubmitCallbackResponse.builder()
-            .errors(errors)
-            .data(caseDetails.getData())
-            .build();
+            List<String> errors = new ArrayList<>();
+            return AboutToStartOrSubmitCallbackResponse.builder()
+                .errors(errors)
+                .data(caseDetails.getData())
+                .build();
+        } else {
+            throw (new RuntimeException(INVALID_CLIENT));
+        }
     }
 
 
     @PostMapping("/submitted")
-    public ResponseEntity<SubmittedCallbackResponse> handleSubmitted(@RequestHeader("Authorization")
-                                                                     @Parameter(hidden = true) String authorisation,
-                                                                     @RequestBody CallbackRequest callbackRequest) {
-        log.info("High court case submitted");
-        final CaseDetails caseDetails = callbackRequest.getCaseDetails();
-        final CaseData caseData = getCaseData(caseDetails);
-        log.info("submitted IsHighCourtCase {}", caseData.getIsHighCourtCase());
-        return ok(SubmittedCallbackResponse.builder().build());
-    }
+    public ResponseEntity<SubmittedCallbackResponse> handleSubmitted(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
+        @RequestHeader(PrlAppsConstants.SERVICE_AUTHORIZATION_HEADER) String s2sToken,
+        @RequestBody CallbackRequest callbackRequest) {
 
-
-
-
-
-    private Map<String, Object> printCaseDataReceivedFromXui(CallbackRequest callbackRequest) {
-        final Map<String, Object> data = callbackRequest.getCaseDetails().getData();
-        if (nonNull(data)) {
-            for (Map.Entry<String, Object> entry : data.entrySet()) {
-                if (nonNull(entry.getKey()) && nonNull(entry.getValue())) {
-                    log.info("High Court Case data map {} ", entry.getKey() + " = " + entry.getValue());
-                } else if (nonNull(entry.getKey())) {
-                    log.info("High Court Case data map {} has null value", entry.getKey());
-                } else {
-                    log.info("High Court Case data map has null key for value {}", entry.getValue().toString());
-                }
-            }
-        } else {
-            log.info("High Court Case data map is null");
+        if (authorisationService.isAuthorized(authorisation, s2sToken)) {
+            return ok(SubmittedCallbackResponse.builder().build());
+        }  else {
+            throw (new RuntimeException(INVALID_CLIENT));
         }
-        return data;
     }
-
 
 }
