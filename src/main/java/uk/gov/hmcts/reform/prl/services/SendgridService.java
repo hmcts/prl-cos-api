82925ec005f893021820b6fddc635ed08d715c03
GEvans-HMCTS <gareth.evans4@justice.gov.uk>
2026-01-27 12:34:41 +0000

    adding logging for sendgrid service

--- src/main/java/uk/gov/hmcts/reform/prl/services/SendgridService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/SendgridService.java
@@ -174,6 +174,10 @@ public class SendgridService {
                 if (HttpStatus.valueOf(response.getStatusCode()).is2xxSuccessful()) {
                     log.info(NOTIFICATION_TO_PARTY_SENT_SUCCESSFULLY);
                 }
+                if (HttpStatus.valueOf(response.getStatusCode()).isError()) {
+                    log.error("Notification to parties failed for CASE ID: {}", emailProps.get(CASE_NUMBER));
+                    throw new IOException("Failed to send transfer case email");
+                }
             } catch (IOException ex) {
                 log.error("Notification to parties failed");
                 throw new IOException(ex.getMessage());
