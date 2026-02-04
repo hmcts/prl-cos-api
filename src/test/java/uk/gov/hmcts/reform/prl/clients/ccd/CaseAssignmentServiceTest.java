eab0d8622d4aa509117dc83603e960f9d0972d44
dharmendra kumar <dharmendra.kumar@hmcts.net>
2026-02-04 13:19:04 +0000

    Resolve failing test

--- src/test/java/uk/gov/hmcts/reform/prl/clients/ccd/CaseAssignmentServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/clients/ccd/CaseAssignmentServiceTest.java
@@ -961,7 +961,7 @@ class CaseAssignmentServiceTest {
                                                 FL401RESPONDENTBARRISTER.getCaseRoleLabel(),
                                                 errors);
         assertThat(errors)
-            .contains("A barrister is already associated with the case");
+            .contains("Requested user is already associated with the case");
     }
 
     @Test
