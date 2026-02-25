baf78cf1a9b0656d9161f8084e424888adaa39de
mrafeeqhmcts <mohammed.rafeeq@hmcts.net>
2026-02-25 10:26:49 +0000

    FPVTL-1731: integrated with jude branch

--- src/main/java/uk/gov/hmcts/reform/prl/constants/PrlAppsConstants.java
+++ src/main/java/uk/gov/hmcts/reform/prl/constants/PrlAppsConstants.java
@@ -806,7 +806,7 @@ public class PrlAppsConstants {
     public static final String HMC_STATUS_COMPLETED = "COMPLETED";
 
     public static final String CAFCASS = "Cafcass";
-    public  static final String LOCAL_AUTHORITY = "Local Authority";//TODO review the value later
+    public  static final String LOCAL_AUTHORITY = "LOCAL_AUTHORITY";//TODO review the value later
 
     public static final String APPLICANTS = "applicants";
     public static final String RESPONDENTS = "respondents";
