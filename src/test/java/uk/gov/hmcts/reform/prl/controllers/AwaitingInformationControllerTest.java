31c0a925e2d5efd0015ad361623785de55db781b
Hari Maddali <harikrshna.maddali@hmcts.net>
2026-02-25 10:00:11 +0000

    FPVTL-2022 review comments

--- src/test/java/uk/gov/hmcts/reform/prl/controllers/AwaitingInformationControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/AwaitingInformationControllerTest.java
@@ -26,9 +26,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.Silent.class)
 public class AwaitingInformationControllerTest {
