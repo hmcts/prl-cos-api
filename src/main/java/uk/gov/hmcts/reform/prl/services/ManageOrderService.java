440b040587d64a65efa1111f47640e91225970a6
smurugesan <Sunil.Murugesan@HMCTS.NET>
2026-03-03 12:18:46 +0000

    FPVTL-2052 - Add some log statement to debug logged in user details and role.

--- src/main/java/uk/gov/hmcts/reform/prl/services/ManageOrderService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ManageOrderService.java
@@ -2457,7 +2457,9 @@ public class ManageOrderService {
 
     public String getLoggedInUserType(String authorisation) {
         UserDetails userDetails = userService.getUserDetails(authorisation);
+        log.info("Debugging FPVTL-2052: User details: {}", userDetails);
         String loggedInUserType;
+        log.info("Debugging FPVTL-2052 : Is role-assignment-api-in-orders-journey enabled {}", launchDarklyClient.isFeatureEnabled(ROLE_ASSIGNMENT_API_IN_ORDERS_JOURNEY));
         if (launchDarklyClient.isFeatureEnabled(ROLE_ASSIGNMENT_API_IN_ORDERS_JOURNEY)) {
             //This would check for roles from AM for Judge/Legal advisor/Court admin
             //if it doesn't find then it will check for idam roles for rest of the users
