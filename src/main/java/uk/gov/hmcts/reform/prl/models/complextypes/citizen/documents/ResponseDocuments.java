29c94890b2c733371523a2abe1b566b5fe75f4da
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2026-03-06 10:25:39 +0000

    FPVTL-2381: Revert deprecation

--- src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/documents/ResponseDocuments.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/complextypes/citizen/documents/ResponseDocuments.java
@@ -12,16 +12,9 @@ import java.time.LocalDateTime;
 public class ResponseDocuments {
     private final String partyName;
     private final String createdBy;
+    private final LocalDate dateCreated;
     private final Document citizenDocument;
     private final Document respondentC8Document;
     private final Document respondentC8DocumentWelsh;
     private final LocalDateTime dateTimeCreated;
-
-    /**
-     * This field is being deprecated as part of FPVTL-2381,
-     * as existing logic uses dateTimeCreated.
-     * @deprecated in favour of dateTimeCreated
-     */
-    @Deprecated(forRemoval = false, since = "FPVTL-2381")
-    private final LocalDate dateCreated;
 }
