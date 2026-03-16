40a9db256bc2f339cb1f20612ad957947471a9f4
dharmendra kumar <dharmendra.kumar@hmcts.net>
2026-03-16 11:27:16 +0000

    update functional test

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/ManageOrdersControllerFunctionalTest.java
@@ -516,8 +516,8 @@ public class ManageOrdersControllerFunctionalTest {
             .body("data.cafcassCymruServedOptions", equalTo(null))
             .body("data.emailInformationCaOnlyC47a", equalTo(null))
             .body("data.localAuthoritySolicitorOrganisationPolicy", equalTo(null))
-            .body("data.localAuthority",
-                  equalTo("{isLocalAuthorityInvolvedInCase=No, localAuthoritySolicitorOrganisationName=null}"))
+            .body("data.localAuthority.isLocalAuthorityInvolvedInCase",equalTo("No"))
+            .body("data.localAuthority.localAuthoritySolicitorOrganisationName",      equalTo(null))
             .body("data.orderCollection[0].value.serveOrderDetails.cafcassCymruServed",
                   equalTo("Yes"))
             .body("data.orderCollection[0].value.serveOrderDetails.cafcassCymruEmail",
