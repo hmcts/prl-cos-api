ccf9a41c1d7acb6cd80f1f5a20039b97cb162eb8
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2026-03-17 11:13:39 +0000

    FPVTL-2278: minior correction2

--- src/main/java/uk/gov/hmcts/reform/prl/models/sendandreply/Message.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/sendandreply/Message.java
@@ -25,7 +25,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static java.util.Optional.ofNullable;
-import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.MAX_SUBJECT_LENGTH;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
