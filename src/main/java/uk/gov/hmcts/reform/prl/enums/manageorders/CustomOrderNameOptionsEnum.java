56b94d24eef5459364c67edc6c642d039d19aeca
DelythJustice <delyth.jones11@justice.gov.uk>
2026-03-26 19:23:00 +0000

    FPVTL-2249 add lombok @get incorp code review

--- src/main/java/uk/gov/hmcts/reform/prl/enums/manageorders/CustomOrderNameOptionsEnum.java
+++ src/main/java/uk/gov/hmcts/reform/prl/enums/manageorders/CustomOrderNameOptionsEnum.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.prl.enums.manageorders;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import uk.gov.hmcts.reform.prl.enums.CustomEnumSerializer;
 
@@ -34,16 +35,13 @@ public enum CustomOrderNameOptionsEnum {
     generalForm("generalForm", "General form of undertaking (N117)"),
     noticeOfProceedings("noticeOfProceedings", "Notice of proceedings (FL402)");
 
+    @Getter
     private final String id;
     private final String displayedValue;
 
     @JsonValue
     public String getDisplayedValue() {
         return displayedValue;
-    }
-
-    public String getId() {
-        return id;
     }
 
     @JsonCreator
