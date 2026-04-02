b5d6902a339ce5e007b6328baa8bce07e75f01db
mrafeeqhmcts <mohammed.rafeeq@hmcts.net>
2026-04-02 19:46:55 +0100

    FPVTL-2412 : added debug statement

--- src/main/java/uk/gov/hmcts/reform/prl/services/cafcass/CafcassUploadDocService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/cafcass/CafcassUploadDocService.java
@@ -77,7 +77,7 @@ public class CafcassUploadDocService {
 
     public void uploadDocument(String authorisation, MultipartFile document,
                                String typeOfDocument, String caseId) {
-        log.info("Cafcass document upload request received for caseId: {}, typeOfDocument: {}", caseId, typeOfDocument);
+        log.info("Cafcass document upload request received for caseId: {}, typeOfDocument: {}, filename{}", caseId, typeOfDocument,document.getOriginalFilename());
         if (isValidDocument(document, typeOfDocument)) {
             CaseDetails caseDetails = checkIfCasePresent(caseId, authorisation);
             if (caseDetails == null) {
