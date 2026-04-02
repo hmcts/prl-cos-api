a509644d3ab4e64ed3a9e7e08c3c573176748c8a
Hari Maddali <harikrshna.maddali@hmcts.net>
2026-04-02 11:39:44 +0100

    FPVTL-2085  fixed failing tests

--- src/integrationTest/java/uk/gov/hmcts/reform/prl/controllers/RequestFurtherInformationControllerIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/prl/controllers/RequestFurtherInformationControllerIntegrationTest.java
@@ -8,7 +8,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
