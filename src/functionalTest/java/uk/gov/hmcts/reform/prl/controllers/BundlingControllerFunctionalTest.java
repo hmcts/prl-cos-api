259bf6b1833311115315a5d6926853c181d25a6d
sunilrochani <sunil.rochani@hotmail.com>
2026-04-14 09:22:32 +0100

    FPVTL-2429: POC for Bundles using Category ID

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/BundlingControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/BundlingControllerFunctionalTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.prl.ResourceLoader;
@@ -18,6 +19,7 @@ import uk.gov.hmcts.reform.prl.utils.ServiceAuthenticationGenerator;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @ContextConfiguration
+@PropertySource(value = "classpath:application.yaml")
 public class BundlingControllerFunctionalTest {
 
     @Autowired
