b6939f030c8ac6b9f2db0fdfc8294f965bf47684
dharmendra kumar <dharmendra.kumar@hmcts.net>
2026-04-16 14:50:10 +0100

    Resolve merge conflicts

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/ServeOrderData.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/ServeOrderData.java
@@ -18,7 +18,6 @@ import java.util.List;
 @Data
 @Builder(toBuilder = true)
 @AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ServeOrderData {
