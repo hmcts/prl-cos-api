b8ea004ab2fc0daa04e89f8715a05c8a7aa40eea
Sahil Goonraz <sahil.goonraz@solirius.com>
2026-05-28 15:28:29 +0100

    FPVTL-2219: add integration test for retry

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/prl/CafcassCcdDataStoreService.java
@@ -0,0 +1,43 @@
+package uk.gov.hmcts.reform.prl;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import uk.gov.hmcts.reform.ccd.client.model.SearchResult;
+import uk.gov.hmcts.reform.prl.services.cafcass.CafcassCcdDataStoreService;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(properties = {
+    "core_case_data.api.url=http://localhost:${wiremock.server.port}",
+    // "launchdarkly.offline=true"
+})
+@AutoConfigureWireMock(port = 0)
+public class CafcassCcdDataStoreServiceIntegrationTest {
+
+    @Autowired
+    private CafcassCcdDataStoreService cafcassCcdDataStoreService;
+
+    @BeforeEach
+    public void setup() {
+        WireMock.reset();
+    }
+
+    @Test
+    public void shouldRetry3TimesAndReturnEmptyFallbackOn502() {
+        stubFor(post(urlPathEqualTo("/searchCases"))
+                    .willReturn(aResponse()
+                                    .withStatus(502)
+                                    .withHeader("Content-Type", "application/json")));
+
+        SearchResult result = cafcassCcdDataStoreService.searchCases("userToken", "{}", "s2sToken", "PRLAPPS");
+
+        assertEquals(0, result.getTotal());
+
+        WireMock.verify(3, postRequestedFor(urlPathEqualTo("/searchCases")));
+    }
+}
