54705e08b0fca38d9a17ecb95a32da546ca7d8ca
lawrence.reynolds <278118191+LawrenceReynolds-HMCTS@users.noreply.github.com>
2026-05-28 15:38:43 +0100

    Fixing bug with current user draft orders not appearing.

--- src/main/java/uk/gov/hmcts/reform/prl/services/ViewDraftOrdersService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/ViewDraftOrdersService.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.prl.services;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
@@ -11,7 +12,6 @@ import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.models.DraftOrder;
 import uk.gov.hmcts.reform.prl.models.Element;
-import uk.gov.hmcts.reform.prl.models.OrgSolicitors;
 import uk.gov.hmcts.reform.prl.models.Organisations;
 import uk.gov.hmcts.reform.prl.models.OtherDraftOrderDetails;
 import uk.gov.hmcts.reform.prl.models.SolicitorUser;
@@ -21,10 +21,12 @@ import uk.gov.hmcts.reform.prl.utils.CaseUtils;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -49,6 +51,10 @@ public class ViewDraftOrdersService {
             return filteredDraftOrders;
         }
 
+        String currentUserEmail = Optional.ofNullable(userService.getUserDetails(authorisation))
+            .map(UserDetails::getEmail)
+            .orElse(StringUtils.EMPTY);
+
         Optional<Organisations> currentUserOrganisationsOpt = organisationService.findUserOrganisation(authorisation);
         if (currentUserOrganisationsOpt.isPresent()) {
             /* Map the email of each user email to the list of orders they've created. */
@@ -56,13 +62,21 @@ public class ViewDraftOrdersService {
                 .collect(Collectors.groupingBy(e -> e.getValue().getOtherDetails().getOrderCreatedByEmailId()));
 
             /* Get the list of emails of the current user's organisation and iterate though to find which of the lists
-             * to include in the result. */
+             * to include in the result. (Adding in the current user as that can sometimes be excluded from
+             * organisationService.getOrganisationSolicitorDetails. */
             String currentUserOrganisationId = currentUserOrganisationsOpt.get().getOrganisationIdentifier();
             String systemAuthorisation = systemUserService.getSysUserToken();
-            OrgSolicitors orgSolicitors = organisationService.getOrganisationSolicitorDetails(systemAuthorisation, currentUserOrganisationId);
 
-            for (SolicitorUser solicitorUser : orgSolicitors.getUsers()) {
-                List<Element<DraftOrder>> userDraftOrders = createdEmailToDraftOrderMap.remove(solicitorUser.getEmail());
+            Set<String> orgSolicitorEmails = new HashSet<>();
+            orgSolicitorEmails.add(currentUserEmail);
+            orgSolicitorEmails.addAll(organisationService.getOrganisationSolicitorDetails(
+                    systemAuthorisation,
+                    currentUserOrganisationId
+                )
+                                          .getUsers().stream().map(SolicitorUser::getEmail).collect(Collectors.toSet()));
+
+            for (String orgSolicitorEmail : orgSolicitorEmails) {
+                List<Element<DraftOrder>> userDraftOrders = createdEmailToDraftOrderMap.remove(orgSolicitorEmail);
                 if (Objects.nonNull(userDraftOrders)) {
                     filteredDraftOrders.addAll(userDraftOrders);
                 }
@@ -81,9 +95,15 @@ public class ViewDraftOrdersService {
 
         } else {
             /* No organisation so just include draft orders that have been created by the current user. */
-            UserDetails currentUserDetails = userService.getUserDetails(authorisation);
             draftOrderCollection.stream()
-                .filter(e -> Objects.equals(currentUserDetails.getEmail(), e.getValue().getOtherDetails().getOrderCreatedByEmailId()))
+                .filter(e -> Objects.equals(
+                    currentUserEmail,
+                    Optional.ofNullable(e)
+                        .map(Element::getValue)
+                        .map(DraftOrder::getOtherDetails)
+                        .map(OtherDraftOrderDetails::getOrderCreatedByEmailId)
+                        .orElse(StringUtils.EMPTY)
+                ))
                 .forEach(filteredDraftOrders::add);
         }
 
@@ -98,6 +118,7 @@ public class ViewDraftOrdersService {
 
         return Map.of(
             FILTERED_DRAFT_ORDERS_CASE_FIELD, filteredDraftOrders,
-            FILTERED_DRAFT_ORDERS_PRESENT_YES_NO_FLAG_CASE_FIELD, filteredDraftOrdersPresentFlag);
+            FILTERED_DRAFT_ORDERS_PRESENT_YES_NO_FLAG_CASE_FIELD, filteredDraftOrdersPresentFlag
+        );
     }
 }
