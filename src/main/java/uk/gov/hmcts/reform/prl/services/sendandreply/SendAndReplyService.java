9cc457c68e0f1a6cc230e2bc84af53de8376dedd
Jefex7 <duartepireslopes@gmail.com>
2026-06-02 10:57:23 +0100

    FPVTL-2861: Test amend send and reply

--- src/main/java/uk/gov/hmcts/reform/prl/services/sendandreply/SendAndReplyService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/sendandreply/SendAndReplyService.java
@@ -1012,7 +1012,15 @@ public class SendAndReplyService {
                 .getMetadataForDocument(authorization, authTokenGenerator.generate(), UUID.fromString(documentId));
 
             if (document != null) {
-                return buildFromDocument(document);
+                buildFromDocument(document);
+                uk.gov.hmcts.reform.prl.models.documents.Document prlDocument = buildFromDocument(document);
+
+                final String[] labelParts = submittedDocumentList.getValue().getLabel().split(ARROW_SEPARATOR);
+                final String documentName = labelParts[documentPath.length - 1].trim();
+
+                return prlDocument.toBuilder()
+                    .documentFileName(documentName)
+                    .build();
             }
         }
         return null;
