08831542f14e9ef21879cb5253a9aa285d6db679
Jefex7 <duartepireslopes@gmail.com>
2026-06-02 10:12:27 +0100

    FPVTL-2861TestB : Add dto

---
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/RenameDocument.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.reform.prl.models.dto.ccd;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicList;
+
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RenameDocument {
+
+    private String newNameForDocument;
+
+    @JsonProperty("renameDocumentsList")
+    private DynamicList renameDocumentsList;
+
+    @JsonProperty("categoryDocumentsList")
+    private DynamicList categoryDocumentsList;
+}
