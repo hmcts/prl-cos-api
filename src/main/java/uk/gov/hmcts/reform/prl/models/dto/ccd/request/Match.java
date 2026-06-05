604647f0c0090b92165edbc343467e4a9c4dd192
SomeshRA <somesh.acharya1@hmcts.net>
2026-06-05 10:24:50 +0100

    FPVTL-2409: Reverted ignore json include nonnull

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/request/Match.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/request/Match.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.prl.models.dto.ccd.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,7 +14,6 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonInclude(JsonInclude.Include.NON_NULL)
 @Builder
 public class Match {
 
--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/request/QueryParam.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/request/QueryParam.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.prl.models.dto.ccd.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,7 +13,6 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonInclude(JsonInclude.Include.NON_NULL)
 @Builder
 public class QueryParam {
     private Query query;
