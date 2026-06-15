081583da93ed64e1c8b37d1e5d2d1641812cdde6
Yogesh Dhonde <Yogesh.Dhonde@HMCTS.net>
2026-06-15 09:31:49 +0100

    FPVTL-2408.

--- src/main/java/uk/gov/hmcts/reform/prl/utils/ClientContextUtils.java
+++ src/main/java/uk/gov/hmcts/reform/prl/utils/ClientContextUtils.java
@@ -3,14 +3,6 @@ package uk.gov.hmcts.reform.prl.utils;
 public class ClientContextUtils {
 
 
-    public static String extractTaskTriggeredByFromClientContext(String clientContext) {
-        if (clientContext == null || clientContext.isBlank()) {
-            return null;
-        }
-        return CaseUtils.getTaskTriggeredBy(CaseUtils.getWaMapper(clientContext));
-    }
-
-
     public static String extractHearingIdFromClientContext(String clientContext) {
         if (clientContext == null || clientContext.isBlank()) {
             return null;
