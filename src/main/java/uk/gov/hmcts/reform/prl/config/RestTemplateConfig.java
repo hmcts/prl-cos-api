62d07aa185fa9f381e15ced1d16ed414c183e3f3
James Turnbull <james.turnbull@justice.gov.uk>
2026-06-17 16:58:17 +0100

    Add timeout config for RestTemplate

--- src/main/java/uk/gov/hmcts/reform/prl/config/RestTemplateConfig.java
+++ src/main/java/uk/gov/hmcts/reform/prl/config/RestTemplateConfig.java
@@ -1,15 +1,19 @@
 package uk.gov.hmcts.reform.prl.config;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableConfigurationProperties(HttpProperties.class)
 public class RestTemplateConfig {
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    public RestTemplate restTemplate(RestTemplateBuilder builder, HttpProperties properties) {
         return builder
+            .connectTimeout(properties.getConnectTimeout())
+            .readTimeout(properties.getReadTimeout())
             .build();
     }
 }
