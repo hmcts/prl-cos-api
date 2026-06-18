1d0a1853643b5357ee7a6e04c85b309903cbdcfe
James Turnbull <james.turnbull@justice.gov.uk>
2026-06-18 11:59:18 +0100

    Improve error logging

--- src/main/java/uk/gov/hmcts/reform/prl/exception/ControllerExceptionHandler.java
+++ src/main/java/uk/gov/hmcts/reform/prl/exception/ControllerExceptionHandler.java
@@ -33,7 +33,7 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(DocumentGenerationException.class)
     public ResponseEntity<ErrorResponse> handleDocumentGenerationException(DocumentGenerationException ex) {
-        log.error("Exception occurred while document generation", ex);
+        log.error("Exception occurred when generating document", ex);
         ErrorResponse error = ErrorResponse.builder(ex, ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500),
                                                                                          ex.getMessage())).build();
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
