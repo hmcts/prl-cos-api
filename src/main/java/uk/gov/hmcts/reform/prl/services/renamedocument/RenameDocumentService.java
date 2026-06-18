09caa7b1c37be3837ce7d9833cfc5dab39173c4c
Jefex7 <duartepireslopes@gmail.com>
2026-06-18 17:55:27 +0100

    FPVTL-3075 : add to fields for SOD

--- src/main/java/uk/gov/hmcts/reform/prl/services/renamedocument/RenameDocumentService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/renamedocument/RenameDocumentService.java
@@ -48,6 +48,8 @@ public class RenameDocumentService {
         "finalServedApplicationDetailsList",
         "internalMessageAttachDocs",
         "externalMessageAttachDocs",
+        "servedDocumentsDetailsList",
+        "sodUnServedPack",
         "unServedApplicantPack",
         "unServedRespondentPack"
     );
