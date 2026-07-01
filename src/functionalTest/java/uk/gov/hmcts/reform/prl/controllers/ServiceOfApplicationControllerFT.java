15ce04ef0a22196aaea7f7d631769a7ed08b54bf
DelythJustice <delyth.jones11@justice.gov.uk>
2026-07-01 13:39:43 +0100

    FPVTL-1115 checkstyle

--- src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/ServiceOfApplicationControllerFT.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/prl/controllers/ServiceOfApplicationControllerFT.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
