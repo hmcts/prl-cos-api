71e81caf261e5d4a1e4156010751d1345a101673
SomeshRA <somesh.acharya1@hmcts.net>
2026-07-15 12:19:55 +0100

    FPVLT-3201: Fetch size reverted to 100

--- src/main/java/uk/gov/hmcts/reform/prl/services/UpdateHearingActualsService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/UpdateHearingActualsService.java
@@ -71,7 +71,7 @@ public class UpdateHearingActualsService {
 
     private final ObjectMapper objectMapper;
 
-    private static final String ES_PAGE_SIZE = "2";
+    private static final String ES_PAGE_SIZE = "100";
 
 
     public void updateHearingActuals() {
