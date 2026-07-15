af5930d9eaa74e827dfa1cd76ee9658b7596a497
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2026-07-15 13:41:14 +0100

    FPVTL-2836: reverting after proof obtained2

--- src/main/java/uk/gov/hmcts/reform/prl/services/TestingSupportFixtureLoader.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/TestingSupportFixtureLoader.java
@@ -15,9 +15,7 @@ public class TestingSupportFixtureLoader {
 
     public String loadJson(String filePath) throws Exception {
         String rawJson = ResourceLoader.loadJson(filePath);
-        log.info("Loading TestingSupport placeholders for filepath={} on env={}", filePath, environment.toString());
         String resolved = environment.resolvePlaceholders(rawJson);
-        log.info("Loaded TestingSupport placeholders for filepath={} as resolved={}", filePath, resolved);
         return resolved;
     }
 }
