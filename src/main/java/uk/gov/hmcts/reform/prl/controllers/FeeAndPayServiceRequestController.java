8ba6531d06bd4153acc7ed2a97624ecc15470ecf
Yogesh Dhonde <Yogesh.Dhonde@HMCTS.net>
2026-07-22 12:34:14 +0100

    FPVTL-3129.

--- src/main/java/uk/gov/hmcts/reform/prl/controllers/FeeAndPayServiceRequestController.java
+++ src/main/java/uk/gov/hmcts/reform/prl/controllers/FeeAndPayServiceRequestController.java
@@ -23,10 +23,15 @@ import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.enums.solicitoremailnotification.SolicitorEmailNotificationEventEnum;
 import uk.gov.hmcts.reform.prl.events.SolicitorNotificationEmailEvent;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CallbackRequest;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.CallbackResponse;
 import uk.gov.hmcts.reform.prl.services.AuthorisationService;
 import uk.gov.hmcts.reform.prl.services.EventService;
 import uk.gov.hmcts.reform.prl.services.SolicitorEmailService;
 import uk.gov.hmcts.reform.prl.services.caseflags.PartyLevelCaseFlagsService;
+import uk.gov.hmcts.reform.prl.services.payment.FeeAndPayServiceRequestService;
+import uk.gov.hmcts.reform.prl.utils.CaseUtils;
+
+import java.util.List;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.springframework.http.ResponseEntity.ok;
@@ -59,6 +64,7 @@ public class FeeAndPayServiceRequestController extends AbstractCallbackControlle
     public static final String CASE_DETAILS_URL = "/cases/case-details/";
 
     private final SolicitorEmailService solicitorEmailService;
+    private final FeeAndPayServiceRequestService feeAndPayServiceRequestService;
     private final AuthorisationService authorisationService;
     private final PartyLevelCaseFlagsService partyLevelCaseFlagsService;
 
@@ -66,10 +72,12 @@ public class FeeAndPayServiceRequestController extends AbstractCallbackControlle
     protected FeeAndPayServiceRequestController(ObjectMapper objectMapper,
                                                 EventService eventPublisher,
                                                 SolicitorEmailService solicitorEmailService,
+                                                FeeAndPayServiceRequestService feeAndPayServiceRequestService,
                                                 AuthorisationService authorisationService,
                                                 PartyLevelCaseFlagsService partyLevelCaseFlagsService) {
         super(objectMapper, eventPublisher);
         this.solicitorEmailService = solicitorEmailService;
+        this.feeAndPayServiceRequestService = feeAndPayServiceRequestService;
         this.authorisationService = authorisationService;
         this.partyLevelCaseFlagsService = partyLevelCaseFlagsService;
     }
@@ -112,6 +120,25 @@ public class FeeAndPayServiceRequestController extends AbstractCallbackControlle
         } else {
             throw (new RuntimeException(INVALID_CLIENT));
         }
+    }
+
+    @PostMapping(path = "/validate-help-with-fees", consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
+    @Operation(description = "Callback validate help with fees number .")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Callback processed.",
+            content = @Content(mediaType = "application/json",
+                schema = @Schema(implementation = uk.gov.hmcts.reform.ccd.client.model.CallbackResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)})
+    public CallbackResponse helpWithFeesValidator(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) @Parameter(hidden = true) String authorisation,
+        @RequestHeader(value = PrlAppsConstants.CLIENT_CONTEXT_HEADER_PARAMETER, required = false) String clientContext,
+        @RequestBody CallbackRequest callbackRequest
+    ) {
+        List<String> errorList =
+            feeAndPayServiceRequestService.validateSuppressedHelpWithFeesCheck(callbackRequest, CaseUtils.getLanguage(clientContext));
+        return CallbackResponse.builder()
+            .errors(errorList)
+            .build();
     }
 
     private SolicitorNotificationEmailEvent prepareAwaitingPaymentEvent(CallbackRequest callbackRequest) {
--- src/test/java/uk/gov/hmcts/reform/prl/controllers/FeeAndPayServiceRequestControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/controllers/FeeAndPayServiceRequestControllerTest.java
@@ -11,14 +11,23 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.ResponseEntity;
-import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.reform.prl.constants.PrlAppsConstants;
+import uk.gov.hmcts.reform.prl.enums.Event;
 import uk.gov.hmcts.reform.prl.enums.YesOrNo;
+import uk.gov.hmcts.reform.prl.models.FeeResponse;
+import uk.gov.hmcts.reform.prl.models.FeeType;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CallbackRequest;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseData;
 import uk.gov.hmcts.reform.prl.models.dto.ccd.CaseDetails;
+import uk.gov.hmcts.reform.prl.models.dto.ccd.UploadAdditionalApplicationData;
+import uk.gov.hmcts.reform.prl.models.dto.payment.PaymentServiceResponse;
 import uk.gov.hmcts.reform.prl.services.AuthorisationService;
 import uk.gov.hmcts.reform.prl.services.EventService;
 import uk.gov.hmcts.reform.prl.services.SolicitorEmailService;
+import uk.gov.hmcts.reform.prl.services.payment.FeeAndPayServiceRequestService;
+import uk.gov.hmcts.reform.prl.services.payment.FeeService;
+import uk.gov.hmcts.reform.prl.services.payment.PaymentRequestService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,40 +35,71 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 @PropertySource(value = "classpath:application.yaml")
 @RunWith(MockitoJUnitRunner.class)
 public class FeeAndPayServiceRequestControllerTest {
 
-    private static final String authToken = "Bearer TestAuthToken";
-    private static final String s2sToken = "s2s AuthToken";
+    private MockMvc mockMvc;
 
     @InjectMocks
     private FeeAndPayServiceRequestController feeAndPayServiceRequestController;
 
     @Mock
+    private PaymentRequestService paymentRequestService;
+
+    @Mock
     private AuthorisationService authorisationService;
 
+    @Mock
+    private FeeService feesService;
 
     @Mock
     private ObjectMapper objectMapper;
 
     @Mock
+    private FeeResponse feeResponse;
+
+    @Mock
+    private PaymentServiceResponse paymentServiceResponse;
+    @Mock
     private SolicitorEmailService solicitorEmailService;
+
+    @Mock
+    private FeeAndPayServiceRequestService feeAndPayServiceRequestService;
 
     @Mock
     private EventService eventPublisher;
 
     List<String> errorList;
 
-
+    public static final String authToken = "Bearer TestAuthToken";
+    public static final String s2sToken = "s2s AuthToken";
+    public static final String HWF_SUPPRESSION_ERROR_MESSAGE =
+        "Help with Fees is not yet available in Family Private Law digital service. Select 'No' to continue with your application";
 
     @Before
     public void setUp() {
         errorList = new ArrayList<>();
+        paymentServiceResponse = PaymentServiceResponse.builder()
+            .serviceRequestReference("2021-1638188893038")
+            .build();
+
+        feeResponse = FeeResponse.builder()
+            .code("FEE0325")
+            .build();
     }
 
+    @Test
+    public void testFeeServiceFeeCodeDetails() throws Exception {
+        FeeType feeType = null;
+
+        CallbackRequest callbackRequest = CallbackRequest.builder().build();
+
+        verifyNoMoreInteractions(feesService);
+    }
 
     @Test
     public void testCcdSubmittedHelpWithFeesYes() {
@@ -70,7 +110,7 @@ public class FeeAndPayServiceRequestControllerTest {
                                                             .helpWithFees(YesOrNo.Yes)
                                                             .build()).build()).build();
         when(authorisationService.isAuthorized(any(), any())).thenReturn(true);
-        ResponseEntity<SubmittedCallbackResponse> response = feeAndPayServiceRequestController.ccdSubmitted(authToken, s2sToken, callbackRequest);
+        ResponseEntity response = feeAndPayServiceRequestController.ccdSubmitted(authToken, s2sToken, callbackRequest);
         Assert.assertNotNull(response);
     }
 
@@ -83,11 +123,85 @@ public class FeeAndPayServiceRequestControllerTest {
                                                             .helpWithFees(YesOrNo.No)
                                                             .build()).build()).build();
         when(authorisationService.isAuthorized(any(), any())).thenReturn(true);
-        ResponseEntity<SubmittedCallbackResponse> response = feeAndPayServiceRequestController.ccdSubmitted(authToken, s2sToken, callbackRequest);
+        ResponseEntity response = feeAndPayServiceRequestController.ccdSubmitted(authToken, s2sToken, callbackRequest);
         Assert.assertNotNull(response);
     }
 
+    @Test
+    public void testHelpWithFeesValidatorNotValidForSubmitAndPay() {
+        CallbackRequest callbackRequest = CallbackRequest.builder()
+            .caseDetails(CaseDetails.builder().caseId("123")
+                             .state("PENDING").caseData(CaseData.builder()
+                                                            .applicantSolicitorEmailAddress("hello@gmail.com")
+                                                            .helpWithFees(YesOrNo.Yes)
+                                                            .helpWithFeesNumber("$$$$$$")
+                                                            .build()).build())
+            .eventId(Event.SUBMIT_AND_PAY.getId())
+            .build();
+        when(feeAndPayServiceRequestService.validateSuppressedHelpWithFeesCheck(any(), any())).thenCallRealMethod();
+        Assert.assertEquals(
+            HWF_SUPPRESSION_ERROR_MESSAGE,
+            feeAndPayServiceRequestController.helpWithFeesValidator(authToken, PrlAppsConstants.ENGLISH, callbackRequest).getErrors().get(0)
+        );
+    }
 
+    @Test
+    public void testHelpWithFeesValidatorNotValidForUploadAdditionalApplications() {
+        CallbackRequest callbackRequest = CallbackRequest.builder()
+            .caseDetails(CaseDetails.builder().caseId("123")
+                             .state("PENDING").caseData(CaseData.builder()
+                                                            .applicantSolicitorEmailAddress("hello@gmail.com")
+                                                            .helpWithFees(YesOrNo.Yes)
+                                                            .uploadAdditionalApplicationData(
+                                                                UploadAdditionalApplicationData.builder()
+                                                                    .additionalApplicationsHelpWithFees(YesOrNo.Yes)
+                                                                    .additionalApplicationsHelpWithFeesNumber("$$$$$$")
+                                                                    .build())
+                                                            .build()).build())
+            .eventId(Event.UPLOAD_ADDITIONAL_APPLICATIONS.getId())
+            .build();
+        when(feeAndPayServiceRequestService.validateSuppressedHelpWithFeesCheck(any(), any())).thenCallRealMethod();
+        Assert.assertEquals(
+            HWF_SUPPRESSION_ERROR_MESSAGE,
+            feeAndPayServiceRequestController.helpWithFeesValidator(authToken, PrlAppsConstants.ENGLISH, callbackRequest).getErrors().get(0)
+        );
+    }
+
+    //TODO: Help with fees validation has been suppressed for the time being
+    //    @Test
+    //    public void testHelpWithFeesValidatorExpression1() {
+    //        CallbackRequest callbackRequest = CallbackRequest.builder()
+    //            .caseDetails(CaseDetails.builder().caseId("123")
+    //                             .state("PENDING").caseData(CaseData.builder()
+    //                                                            .applicantSolicitorEmailAddress("hello@gmail.com")
+    //                                                            .helpWithFees(YesOrNo.Yes)
+    //                                                            .helpWithFeesNumber("w12-f34-z98")
+    //                                                            .build()).build()).build();
+    //        Assert.assertNotNull(feeAndPayServiceRequestController.helpWithFeesValidator(authToken, callbackRequest));
+    //    }
+    //
+    //    @Test
+    //    public void testhelpWithFeesValidatorExpression2() {
+    //        CallbackRequest callbackRequest = CallbackRequest.builder()
+    //            .caseDetails(CaseDetails.builder().caseId("123")
+    //                             .state("PENDING").caseData(CaseData.builder()
+    //                                                            .applicantSolicitorEmailAddress("hello@gmail.com")
+    //                                                            .helpWithFees(YesOrNo.Yes)
+    //                                                            .helpWithFeesNumber("aW34-123456")
+    //                                                            .build()).build()).build();
+    //        Assert.assertNotNull(feeAndPayServiceRequestController.helpWithFeesValidator(authToken, callbackRequest));
+    //    }
+    //
+    //    @Test
+    //    public void testHelpWithFeesNoSelected() {
+    //        CallbackRequest callbackRequest = CallbackRequest.builder()
+    //            .caseDetails(CaseDetails.builder().caseId("123")
+    //                             .state("PENDING").caseData(CaseData.builder()
+    //                                                            .applicantSolicitorEmailAddress("hello@gmail.com")
+    //                                                            .helpWithFees(YesOrNo.No)
+    //                                                            .build()).build()).build();
+    //        Assert.assertNotNull(feeAndPayServiceRequestController.helpWithFeesValidator(authToken, callbackRequest));
+    //    }
 
     protected <T extends Throwable> void assertExpectedException(ThrowingRunnable methodExpectedToFail, Class<T> expectedThrowableClass,
                                                                  String expectedMessage) {
