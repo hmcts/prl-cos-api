287e55f57ae56bde4893d3f7cde7c09c0307c12a
GEvans-HMCTS <gareth.evans4@justice.gov.uk>
2026-07-28 16:40:35 +0100

    fix unused import

--- src/test/java/uk/gov/hmcts/reform/prl/services/noticeofchange/NoticeOfChangePartiesServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/noticeofchange/NoticeOfChangePartiesServiceTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.prl.services.noticeofchange;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
