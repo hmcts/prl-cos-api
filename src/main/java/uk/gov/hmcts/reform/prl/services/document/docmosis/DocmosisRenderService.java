788646f772ffe45f558d0b3111e8229bf77a524e
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2026-07-29 14:48:58 +0100

    FPVTL-3392: more logging

--- src/main/java/uk/gov/hmcts/reform/prl/services/document/docmosis/DocmosisRenderService.java
+++ src/main/java/uk/gov/hmcts/reform/prl/services/document/docmosis/DocmosisRenderService.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.prl.services.document.docmosis;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.prl.clients.DocmosisClient;
@@ -42,8 +43,6 @@ public class DocmosisRenderService {
         String templateFilename = getTemplateFilename(generateDocumentRequest);
         log.info("Case ID {}: Generating document {}", generateDocumentRequest.getCaseId(), templateFilename);
         applyRtfValues(generateDocumentRequest);
-        log.info("generateDocumentRequest.rtfValue: {}", generateDocumentRequest.getValues().get("rtfValue"));
-
         Map<String, Object> placeholders = templateDataMapper.map(generateDocumentRequest.getValues());
         log.info("placeholders.rtfValue: {}", placeholders.get("rtfValue"));
         placeholders.put(CURRENT_DATE_KEY, dateTimeFormatter.format(ZonedDateTime.now(clock)));
@@ -58,20 +57,27 @@ public class DocmosisRenderService {
     }
 
     private void applyRtfValues(GenerateDocumentRequest generateDocumentRequest) {
-        String rftValue = "<p><strong>This is sample Bold text for demonstration purposes. "
-            + "It can be used to verify formatting, layout, and content rendering within the application.</strong></p>"
-            + "<p></p><p><em>This is sample Italic text for demonstration purposes. "
-            + "It can be used to verify formatting, layout, and content rendering within the application.</em></p>"
-            + "<p></p><p><u>This is sample Underline text for demonstration purposes. "
-            + "It can be used to verify formatting, layout, and content rendering within the application.</u></p>"
-            + "<p></p><p>This is sample paragraph text for demonstration purposes. "
-            + "It can be used to verify formatting, layout, and content rendering within the application.</p>"
-            + "<ol><li><p>Create a new case.</p></li><li><p>Enter the required details."
-            + "</p></li><li><p>Review the information.</p></li><li><p>Submit the application.</p>"
-            + "</li><li><p>Receive confirmation.</p></li></ol><p></p><ul><li><p>Gather requirements.</p></li></ul>"
-            + "<ul><li><p>Design the solution.</p></li><li><p>Implement the changes.</p></li><li><p>Perform testing.</p></li><"
-            + "li><p>Deploy to production.</p></li></ul><p></p>";
-        generateDocumentRequest.getValues().put("rtfValue", rftValue);
+        Map<String, Object> caseDataMap = ((Map<String, Object>)
+            (((Map<String, Object>) (generateDocumentRequest.getValues().get("caseDetails")))
+                .get("caseData")));
+        String rtfValue = caseDataMap.get("recitalsOrPreamble").toString();
+        if (StringUtils.isEmpty(rtfValue)) {
+            rtfValue = "<p><strong>This is sample Bold text for demonstration purposes. "
+                + "It can be used to verify formatting, layout, and content rendering within the application.</strong></p>"
+                + "<p></p><p><em>This is sample Italic text for demonstration purposes. "
+                + "It can be used to verify formatting, layout, and content rendering within the application.</em></p>"
+                + "<p></p><p><u>This is sample Underline text for demonstration purposes. "
+                + "It can be used to verify formatting, layout, and content rendering within the application.</u></p>"
+                + "<p></p><p>This is sample paragraph text for demonstration purposes. "
+                + "It can be used to verify formatting, layout, and content rendering within the application.</p>"
+                + "<ol><li><p>Create a new case.</p></li><li><p>Enter the required details."
+                + "</p></li><li><p>Review the information.</p></li><li><p>Submit the application.</p>"
+                + "</li><li><p>Receive confirmation.</p></li></ol><p></p><ul><li><p>Gather requirements.</p></li></ul>"
+                + "<ul><li><p>Design the solution.</p></li><li><p>Implement the changes.</p></li><li><p>Perform testing.</p></li><"
+                + "li><p>Deploy to production.</p></li></ul><p></p>";
+
+        }
+        caseDataMap.put("rtfValue", rtfValue);
     }
 
     private String getTemplateFilename(GenerateDocumentRequest request) {
