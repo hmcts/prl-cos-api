0e290054d3b6561c521535c07c677bc70d3e2d5e
scott.christie <s.christie496@gmail.com>
2026-08-26 17:28:01 +0100

    add initial model corresponding to ccd FieldType value

---
+++ src/main/java/uk/gov/hmcts/reform/prl/models/common/staff/StaffUser.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.reform.prl.models.common.staff;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder(toBuilder = true)
+public class StaffUser {
+
+    private String idamId;
+
+    public String getIdamId() {
+        return idamId;
+    }
+
+    public String getPersonalCode() {
+        return personalCode;
+    }
+
+    private String personalCode;
+
+    @JsonCreator
+    public StaffUser(@JsonProperty("idamId") String idamId,
+                     @JsonProperty("personalCode") String personalCode) {
+        this.idamId = idamId;
+        this.personalCode = personalCode;
+    }
+}
