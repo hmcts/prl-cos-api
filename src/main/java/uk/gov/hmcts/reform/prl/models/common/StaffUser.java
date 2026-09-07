276788088c065a03c72bed33b1f8bfd9dbd144d5
scott.christie <s.christie496@gmail.com>
2026-09-07 17:56:11 +0100

    Initial change to add staff user model

---
+++ src/main/java/uk/gov/hmcts/reform/prl/models/common/StaffUser.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.reform.prl.models.common;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder(toBuilder = true)
+public class StaffUser {
+    private String idamId;
+
+    public String getIdamId() {
+        return idamId;
+    }
+
+    @JsonCreator
+    public StaffUser(@JsonProperty("idamId") String idamId) {
+        this.idamId = idamId;
+    }
+}
