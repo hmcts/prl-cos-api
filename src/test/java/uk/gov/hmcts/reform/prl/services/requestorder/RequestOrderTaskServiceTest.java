00e3354f896916a881cd8da0c678cb65b38bbcab
sunilrochani <sunil.rochani@hotmail.com>
2026-09-08 16:13:21 +0100

    FPVTL-2408: Chase Solicitor reverted changes

--- src/test/java/uk/gov/hmcts/reform/prl/services/requestorder/RequestOrderTaskServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/services/requestorder/RequestOrderTaskServiceTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -58,7 +57,6 @@ import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.prl.constants.PrlAppsConstants.CASE_TYPE;
 import static uk.gov.hmcts.reform.prl.enums.CaseEvent.ENABLE_REQUEST_SOLICITOR_ORDER_TASK;
 
-@Disabled
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
 class RequestOrderTaskServiceTest {
