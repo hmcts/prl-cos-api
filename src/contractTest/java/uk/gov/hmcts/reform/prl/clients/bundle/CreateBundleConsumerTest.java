fe4127cc11c3d4351e8fb08aab0403a47c339680
DelythJustice <delyth.jones11@justice.gov.uk>
2026-09-14 13:32:00 +0100

    FPVTL-3517 try fix pact test

--- src/contractTest/java/uk/gov/hmcts/reform/prl/clients/bundle/CreateBundleConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/prl/clients/bundle/CreateBundleConsumerTest.java
@@ -42,7 +42,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 )
 @TestPropertySource(
     properties = {"bundle.api.url=http://localhost:8899","idam.api.url=localhost:5000","commonData.api.url=localhost:5000",
-        "fis_hearing.api.url=localhost:5000",
+        "hearing_component.api.feign-url=",
         "refdata.api.url=",
         "courtfinder.api.url=",
         "fees-register.api.url=",
