6e53026741f10133f78cf3858924a0f7f75a0e05
sanjayparekh-hmcts <36845702+sanjayparekh-hmcts@users.noreply.github.com>
2026-09-15 18:29:02 +0100

    FPVTL-2408: Chase Solicitor (#3950)
    * FPVTL-2408: Chase Solicitor
    
    * FPVTL-2409: updated chase policy
    
    * FPVTL-2409: updated chase policy2
    
    * FPVTL-2409: New Request Order Task
    
    * FPVTL-2409: add wa calls
    
    * Bumping chart version/ fixing aliases
    
    * FPVTL-2409: add wa calls2
    
    * FPVTL-2409: add wa calls - logs
    
    * FPVTL-2409: changed HCPolicy
    
    * FPVTL-2409: changed HCPolicy2
    
    * FPVTL-2409: changed HCPolicy2a
    
    * FPVTL-2409: Updates to handle task Done
    
    * FPVTL-2409: corrected checkstyleTest
    
    * FPVTL-2409: update Chase enums
    
    * FPVTL-2409: Check to prevent same task creation in a given day
    
    * FPVTL-2409: cleaned up logging
    
    * FPVTL-2409: updated logic
    
    * FPVTL-2409: updated HCP
    
    * FPVTL-2409: updated HCP2
    
    * FPVTL-3201: Update HearingsActuals to be batched
    
    * FPVTL-3201: Testing limit ES page size 2
    
    * FPVTL-3201: Refactored runCccSearch method
    
    * FPVTL-3201: Set fetch size to 100
    
    * FPVTL-3201: Testing set page size -2
    
    * FPVLT-3201: Updated Jenkins lib
    
    * FPVLT-3201: Fetch size reverted to 100
    
    * FPVTL-2408: Fixed merge issue
    
    * FPVTL-2408: Merged with FPVTL-3111
    
    * FPVTL-3201: Update jenkins
    
    * FPVTL-2408: Chase Solicitor
    
    * FPVTL-2408: Chase Solicitor
    
    * FPVTL-2408: Chase Solicitor
    
    * Fpvtl 2408 2409 extended (#4013)
    
    * FPVTL-2408-2409-extended: update qry and policy
    
    * FPVTL-2408-2409-extended: update qry and policy2
    
    * FPVTL-2408-2409-extended: update qry and policy3
    
    * FPVTL-2408-2409-extended: update qry and policy4
    
    * FPVTL-2408: Chase Solicitor
    
    * FPVTL-2408: Chase Solicitor fix
    
    * FPVTL-2408: Chase Solicitor fix
    
    * FPVTL-2408: Chase Solicitor fix
    
    * FPVTL-2408: Chase Solicitor fix
    
    * FPVTL-2408: Chase Solicitor
    For specific case 1787926974002019 only
    
    * FPVTL-2408: Chase Solicitor
    reverted changes
    
    * FPVTL-2408: Chase Solicitor
    reverted changes
    
    * FPVTL-2408: Chase Solicitor
    update cadence
    
    ---------
    
    Co-authored-by: SomeshRA <somesh.acharya1@hmcts.net>
    Co-authored-by: hmcts-jenkins-j-to-z <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>
    Co-authored-by: SomeshRA <69344273+SomeshRA@users.noreply.github.com>
    Co-authored-by: srochani <sunil.rochani@hmcts.net>
    Co-authored-by: sunilrochani <sunil.rochani@hotmail.com>

--- src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/BaseCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/prl/models/dto/ccd/BaseCaseData.java
@@ -19,6 +19,7 @@ import uk.gov.hmcts.reform.prl.enums.YesOrNo;
 import uk.gov.hmcts.reform.prl.enums.reopenclosedcases.ValidReopenClosedCasesStatusEnum;
 import uk.gov.hmcts.reform.prl.models.Element;
 import uk.gov.hmcts.reform.prl.models.caseaccess.OrganisationPolicy;
+import uk.gov.hmcts.reform.prl.models.common.dynamic.DynamicList;
 import uk.gov.hmcts.reform.prl.models.complextypes.RemovableDocument;
 import uk.gov.hmcts.reform.prl.models.complextypes.citizen.documents.ResponseDocuments;
 import uk.gov.hmcts.reform.prl.models.complextypes.confidentiality.ApplicantConfidentialityDetails;
@@ -197,8 +198,11 @@ public class BaseCaseData {
         return documentRemovalWrapper;
     }
 
+    private List<Element<RequestOrderHearingTracking>> requestOrderTaskTrackingByHearing;
+
     private List<Element<ApplicantConfidentialityDetails>> otherPeopleConfidentialDetails;
 
     private List<Element<ResponseDocuments>> otherPartyC8Documents;
     private List<Element<ResponseDocuments>> otherPartyC8DocumentsArchived;
+    private DynamicList customOrderHearingsType;
 }
